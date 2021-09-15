c8e632fbc15a250aa917da9f91c2d9bf8710e8c0
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-09-15 12:00:38 -0400

    cleanup for fresh start each run

--- packages/docs-preview/bin/index.sh
+++ packages/docs-preview/bin/index.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/env bash
+git clean -Xdf
 __dirname="$(CDPATH= cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 projectDir="$(cd "${__dirname}" && cd ../@hashicorp/docs-preview && pwd)"
 currentDir="$(pwd)"
@@ -19,10 +20,10 @@ cp -R "$projectDir/site/." "$currentDir/"
 # necessary for preview to work correctly
 cp ".env.$previewProduct" .env.local
 # Clean install
-# npm ci
+npm ci
 # Copy public assets from source repo into the
 # working Next.js directory
-# cp -r "../public/img" "./public"
+cp -r "../public/img" "./public"
 # Run next dev
-# NEXT_PUBLIC_CWD="$currentDir" npm run dev
+npm run dev
 # NEXT_PUBLIC_ALGOLIA_APP_ID=YY0FFNI7MF NEXT_PUBLIC_ALGOLIA_INDEX=product_WAYPOINT NEXT_PUBLIC_ALGOLIA_SEARCH_ONLY_API_KEY=5e4adfd8094367056501547d6fedb6c5 NEXT_PUBLIC_CWD="/Users/zachshilton/code/waypoint/website" npm run dev
