ce3937b48d7e323f9129132e4caeaa4f9131d65f
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-09-15 12:08:59 -0400

    use next-remote-watch

--- packages/docs-preview/site/pages/_app.js
+++ packages/docs-preview/site/pages/_app.js
@@ -1,19 +1,19 @@
 import './style.css'
 import Min100Layout from '@hashicorp/react-min-100-layout'
 import Placeholder from 'components/placeholder'
-import Link from "next/link"
+import Link from 'next/link'
 
 export default function App({ Component, pageProps }) {
   return (
     <Min100Layout footer={<Placeholder>Footer</Placeholder>}>
       <Placeholder>
-        <div style={{ display: "flex", flexWrap: "wrap" }}>
-          <span>Navigation</span>
+        <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+          <span style={{ padding: '0 0.5rem' }}>Navigation</span>
           <Link href="/docs">
-            <a>/docs</a>
+            <a style={{ padding: '0 0.5rem' }}>/docs</a>
           </Link>
           <Link href="/downloads">
-            <a>/downloads</a>
+            <a style={{ padding: '0 0.5rem' }}>/downloads</a>
           </Link>
         </div>
       </Placeholder>
