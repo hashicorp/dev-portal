bde59aaec1693c4fb37c59364fda8bb10d5510c6
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-09-15 12:43:15 -0400

    add mktg-logos as dep

--- packages/docs-preview/site/pages/downloads/index.jsx
+++ packages/docs-preview/site/pages/downloads/index.jsx
@@ -1,9 +1,10 @@
+import latestVersion from 'data/version'
 import productMetadata from 'data/metadata.json'
 import downloadPageProps from 'data/download-page-props'
 import ProductDownloadsPage from '@hashicorp/react-product-downloads-page'
 import { generateStaticProps } from '@hashicorp/react-product-downloads-page/server'
 
-function DownloadsPage({ releases, product, latestVersion }) {
+function DownloadsPage({ releases, product }) {
   return (
     <ProductDownloadsPage
       releases={releases}
@@ -17,7 +18,7 @@ function DownloadsPage({ releases, product, latestVersion }) {
 }
 
 export async function getStaticProps() {
-  const { product, latestVersion } = productMetadata
+  const { product } = productMetadata
   const staticProps = await generateStaticProps({
     product: product.slug,
     latestVersion,
