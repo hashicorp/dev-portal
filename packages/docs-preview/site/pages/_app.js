66603929686a9022cc55400fc0d51cfc166329a8
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-09-15 12:59:38 -0400

    add productName to nav placeholder

--- packages/docs-preview/site/pages/_app.js
+++ packages/docs-preview/site/pages/_app.js
@@ -2,13 +2,14 @@ import './style.css'
 import Min100Layout from '@hashicorp/react-min-100-layout'
 import Placeholder from 'components/placeholder'
 import Link from 'next/link'
+import { productName } from 'data/metadata'
 
 export default function App({ Component, pageProps }) {
   return (
     <Min100Layout footer={<Placeholder>Footer</Placeholder>}>
       <Placeholder>
         <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-          <span style={{ padding: '0 0.5rem' }}>Navigation</span>
+          <span style={{ padding: '0 0.5rem' }}>{productName}</span>
           <Link href="/docs">
             <a style={{ padding: '0 0.5rem' }}>/docs</a>
           </Link>
