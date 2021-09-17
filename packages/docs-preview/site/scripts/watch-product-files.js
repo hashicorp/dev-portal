740bc840f98a1ea2cca55ad3d8808ff98de9b0f8
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-09-17 15:19:57 -0400

    fix issue with sync

--- packages/docs-preview/site/scripts/watch-product-files.js
+++ packages/docs-preview/site/scripts/watch-product-files.js
@@ -2,32 +2,29 @@ const fs = require('fs')
 const path = require('path')
 const chokidar = require('chokidar')
 
-// sync the "data" folder outside the site
-// with the "data" folder within it
-const dataSourceDir = path.resolve(__dirname, '../../data')
-const dataDestDir = path.resolve(__dirname, '../data')
-chokidar.watch(dataSourceDir).on('all', generateSyncHandler(dataDestDir))
+// sync "content", "data", and "public" folders
+syncDirs('../../data', '../data')
+syncDirs('../../content', '../content')
+syncDirs('../../public', '../public')
 
-// sync the "content" folder outside the site
-// with the "content" folder within it
-const contentSourceDir = path.resolve(__dirname, '../../content')
-const contentDestDir = path.resolve(__dirname, '../content')
-chokidar.watch(contentSourceDir).on('all', generateSyncHandler(contentDestDir))
-
-// sync the "public" folder outside the site
-// with the "public" folder within it
-const publicSourceDir = path.resolve(__dirname, '../../public')
-const publicDestDir = path.resolve(__dirname, '../public')
-chokidar.watch(publicSourceDir).on('all', generateSyncHandler(publicDestDir))
-
-console.log({
-  dataSourceDir,
-  dataDestDir,
-  contentSourceDir,
-  contentDestDir,
-  publicSourceDir,
-  publicDestDir,
-})
+/**
+ * Given paths to a source and destination directory,
+ * both relative to this file, use chokidar to
+ * keep the destination directory in sync with the
+ * source directory.
+ *
+ * @param {string} sourceRelative
+ * @param {string} destRelative
+ */
+function syncDirs(sourceRelative, destRelative) {
+  const sourceDir = path.resolve(__dirname, sourceRelative)
+  const destDir = path.resolve(__dirname, destRelative)
+  console.log({
+    sourceDir,
+    destDir,
+  })
+  chokidar.watch(sourceDir).on('all', generateSyncHandler(sourceDir, destDir))
+}
 
 /**
  * Given an absolute path to destination directory,
@@ -41,14 +38,14 @@ console.log({
  * @param {string} destinationDir
  * @returns
  */
-function generateSyncHandler(destinationDir) {
+function generateSyncHandler(sourceDir, destinationDir) {
   return function handleSync(event, sourcePath) {
     const needsCopy = event === 'change' || event === 'add'
     const needsDelete = event === 'unlink'
     // early return if we don't need to do anything
     if (!needsCopy && !needsDelete) return
     // otherwise, handle the change, add, or unlink
-    const relativePath = path.relative(dataDir, sourcePath)
+    const relativePath = path.relative(sourceDir, sourcePath)
     const destinationPath = path.join(destinationDir, relativePath)
     if (needsCopy) {
       // for changes or additions, copy the updated source file
