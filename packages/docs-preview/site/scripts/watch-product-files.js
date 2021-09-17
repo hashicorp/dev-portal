6331abe42549c294182b4867b78078dd2dcff5ff
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-09-17 12:50:56 -0400

    test out watch script

---
+++ packages/docs-preview/site/scripts/watch-product-files.js
@@ -0,0 +1,19 @@
+const chokidar = require('chokidar')
+/*
+TO DO:
+
+build a script to watch product files, specifically:
+- the "content" and "data" dirs
+    - all file changes should be synced into the "site" dir
+- the "public" dir
+    - all file changes should be synced into the "site/public" dir
+*/
+
+console.log(
+  'TO DO: this Node script should be running a watcher at the same time as our dev script, but right now, it does nothing.'
+)
+
+// One-liner for current directory
+chokidar.watch('.').on('all', (event, path) => {
+  console.log(event, path)
+})
