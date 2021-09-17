be5cd1c6c11af9760e6d89b07d65137f4f1c38de
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-09-17 15:13:33 -0400

    implement watching of content, data, and public dirs

--- packages/docs-preview/site/scripts/watch-product-files.js
+++ packages/docs-preview/site/scripts/watch-product-files.js
@@ -1,28 +1,61 @@
+const fs = require('fs')
 const path = require('path')
 const chokidar = require('chokidar')
 
-const dataDir = path.resolve(__dirname, '../../data')
-const contentDir = path.resolve(__dirname, '../../content')
-const publicDir = path.resolve(__dirname, '../../public')
-console.log({ dataDir, contentDir, publicDir })
-/*
-TO DO:
+// sync the "data" folder outside the site
+// with the "data" folder within it
+const dataSourceDir = path.resolve(__dirname, '../../data')
+const dataDestDir = path.resolve(__dirname, '../data')
+chokidar.watch(dataSourceDir).on('all', generateSyncHandler(dataDestDir))
 
-build a script to watch product files, specifically:
-- the "content" and "data" dirs
-    - all file changes should be synced into the "site" dir
-- the "public" dir
-    - all file changes should be synced into the "site/public" dir
-*/
+// sync the "content" folder outside the site
+// with the "content" folder within it
+const contentSourceDir = path.resolve(__dirname, '../../content')
+const contentDestDir = path.resolve(__dirname, '../content')
+chokidar.watch(contentSourceDir).on('all', generateSyncHandler(contentDestDir))
 
-chokidar.watch(dataDir).on('all', (event, path) => {
-  if (event === 'change') console.log(event, path)
+// sync the "public" folder outside the site
+// with the "public" folder within it
+const publicSourceDir = path.resolve(__dirname, '../../public')
+const publicDestDir = path.resolve(__dirname, '../public')
+chokidar.watch(publicSourceDir).on('all', generateSyncHandler(publicDestDir))
+
+console.log({
+  dataSourceDir,
+  dataDestDir,
+  contentSourceDir,
+  contentDestDir,
+  publicSourceDir,
+  publicDestDir,
 })
 
-chokidar.watch(contentDir).on('all', (event, path) => {
-  if (event === 'change') console.log(event, path)
-})
-
-chokidar.watch(publicDir).on('all', (event, path) => {
-  if (event === 'change') console.log(event, path)
-})
+/**
+ * Given an absolute path to destination directory,
+ * return a function (event, sourcePath) => null
+ * suitable for use with chokidar.watch().on()
+ *
+ * For example:
+ * const syncHandler = generateSyncHandler(destDir)
+ * chokidar.watch(sourceDir).on("all", syncHandler)
+ *
+ * @param {string} destinationDir
+ * @returns
+ */
+function generateSyncHandler(destinationDir) {
+  return function handleSync(event, sourcePath) {
+    const needsCopy = event === 'change' || event === 'add'
+    const needsDelete = event === 'unlink'
+    // early return if we don't need to do anything
+    if (!needsCopy && !needsDelete) return
+    // otherwise, handle the change, add, or unlink
+    const relativePath = path.relative(dataDir, sourcePath)
+    const destinationPath = path.join(destinationDir, relativePath)
+    if (needsCopy) {
+      // for changes or additions, copy the updated source file
+      fs.copyFileSync(sourcePath, destinationPath)
+    } else if (needsDelete) {
+      // for deletions, delete the destination file
+      fs.unlinkSync(destinationPath)
+    }
+  }
+}
