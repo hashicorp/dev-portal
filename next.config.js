480b3864b3d795ff9c01473685cc176678b2a806
Bryce Kalow <bkalow@hashicorp.com>
2021-09-20 09:24:42 -0500

    fix svgo config and move files into src

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 const withHashicorp = require('@hashicorp/platform-nextjs-plugin')
 
-module.exports = withHashicorp({ nextOptimizedImages: true })()
+module.exports = withHashicorp({ nextOptimizedImages: true })({
+  svgo: {
+    plugins: [
+      {
+        removeViewBox: false,
+        collapseGroups: false,
+      },
+    ],
+  },
+})
