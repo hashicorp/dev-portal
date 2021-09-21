57ca5ea5a3fc8a72451da6419141dd7545974503
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-09-21 16:01:27 -0400

    add api-docs route

---
+++ packages/docs-preview/site/pages/api-docs/[[...page]].jsx
@@ -0,0 +1,61 @@
+import { productName, productSlug, mainBranch } from 'data/metadata'
+import DocsPage from '@hashicorp/react-docs-page'
+// additional components
+import Placement from 'components/waypoint/placement-table'
+import NestedNode from 'components/waypoint/nested-node'
+import ConfigEntryReference from 'components/consul/config-entry-reference'
+// imports below are used on server only
+import {
+  generateStaticPaths,
+  generateStaticProps,
+} from '@hashicorp/react-docs-page/server'
+
+// configures for this specific basepath
+const basePath = 'api-docs'
+
+// note: adds components for all possible product environments
+const additionalComponents = { ConfigEntryReference, Placement, NestedNode }
+const navDataFile = `./data/${basePath}-nav-data.json`
+const localContentDir = `./content/${basePath}`
+
+export default function DocsLayout({ product, staticProps }) {
+  return (
+    <DocsPage
+      product={product}
+      baseRoute={basePath}
+      staticProps={staticProps}
+      additionalComponents={additionalComponents}
+    />
+  )
+}
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: await generateStaticPaths({
+      navDataFile,
+      localContentDir,
+    }),
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const product = {
+    name: productName,
+    slug: productSlug,
+  }
+  const staticProps = await generateStaticProps({
+    navDataFile,
+    localContentDir,
+    mainBranch,
+    product,
+    params,
+    additionalComponents,
+  })
+  return {
+    props: {
+      product,
+      staticProps,
+    },
+  }
+}
