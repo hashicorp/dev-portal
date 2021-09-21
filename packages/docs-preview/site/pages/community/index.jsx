e558a0fc060b279d20d8dec3b07db476dd6e84cc
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-09-21 15:43:31 -0400

    simplify community page configuration

--- packages/docs-preview/site/pages/community/index.jsx
+++ packages/docs-preview/site/pages/community/index.jsx
@@ -3,22 +3,23 @@ import SectionHeader from '@hashicorp/react-section-header'
 import Head from 'next/head'
 import s from './style.module.css'
 import communityPageData from 'data/community-page-data'
+import { productName, productSlug } from 'data/metadata'
 
 export default function CommunityPage() {
-  const { pageTitle, heading, subheading, product, links } = communityPageData
+  const { subheading, links } = communityPageData
   return (
     <div className={s.communityPage}>
       <Head>
-        <title key="title">{pageTitle}</title>
+        <title key="title">Community | {productName} by HashiCorp</title>
       </Head>
       <div className={s.sectionHeaderWrapper}>
         <SectionHeader
-          headline={heading}
+          headline="Community"
           description={subheading}
           use_h1={true}
         />
       </div>
-      <VerticalTextBlockList product={product} data={links} />
+      <VerticalTextBlockList product={productSlug} data={links} />
     </div>
   )
 }
