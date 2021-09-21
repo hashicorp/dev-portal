d9f49390087636f27fdadd0927bbf4d3cc0dc4c6
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-09-21 15:33:53 -0400

    load community page data to support multiple products

--- packages/docs-preview/site/pages/community/index.jsx
+++ packages/docs-preview/site/pages/community/index.jsx
@@ -2,35 +2,23 @@ import VerticalTextBlockList from '@hashicorp/react-vertical-text-block-list'
 import SectionHeader from '@hashicorp/react-section-header'
 import Head from 'next/head'
 import s from './style.module.css'
+import communityPageData from 'data/community-page-data'
 
 export default function CommunityPage() {
+  const { pageTitle, heading, subheading, product, links } = communityPageData
   return (
     <div className={s.communityPage}>
       <Head>
-        <title key="title">Community | Waypoint by HashiCorp</title>
+        <title key="title">{pageTitle}</title>
       </Head>
       <div className={s.sectionHeaderWrapper}>
         <SectionHeader
-          headline="Community"
-          description="Waypoint is a newly-launched open source project. The project team depends on the community’s engagement and feedback. Get involved today."
+          headline={heading}
+          description={subheading}
           use_h1={true}
         />
       </div>
-      <VerticalTextBlockList
-        product="waypoint"
-        data={[
-          {
-            header: 'Community Forum',
-            body:
-              '<a href="https://discuss.hashicorp.com/c/waypoint">Waypoint Community Forum</a>',
-          },
-          {
-            header: 'Bug Tracker',
-            body:
-              '<a href="https://github.com/hashicorp/waypoint/issues">Issue tracker on GitHub</a>. Please only use this for reporting bugs. Do not ask for general help here; use the Community Form for that.',
-          },
-        ]}
-      />
+      <VerticalTextBlockList product={product} data={links} />
     </div>
   )
 }
