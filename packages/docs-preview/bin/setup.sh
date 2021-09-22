297d0e8566640aedd6bafbcbc4195f308ade4ac7
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-09-22 11:06:05 -0400

    clean up setup script

--- packages/docs-preview/bin/setup.sh
+++ packages/docs-preview/bin/setup.sh
@@ -6,39 +6,35 @@ parentDir="$(dirname "$currentDir")"
 previewProduct="$(basename "$parentDir")"
 previewDir="$currentDir/docs-preview"
 NEXT_PUBLIC_CWD="$currentDir"
-# echo "__dirname: $__dirname"
-# echo "projectDir: $projectDir"
-# echo "currentDir: $currentDir"
-# echo "previewProduct: $previewProduct"
 # Copy site source into working directory
-args=("$@")
-echo $# arguments passed
-echo ${args[0]} ${args[1]} ${args[2]}
 cp -R "$projectDir/site/." "$previewDir"
-# Copy content files into site source directory
+# cd into the site source directory,
+# and finish setting up the nextjs app directory
+cd "$previewDir"
+# CI-style install
+# (not yet installed via npx, since our
+# @hashicorp/docs-preview package has
+# a nested site folder and package.json
+# for the NextJS preview app.)
+npm ci
+# Write out product-specific files
+# necessary for preview to work as expected
+cp "./site-specific/.env.$previewProduct" .env.local
+cp "./site-specific/redirects.$previewProduct.js" redirects.js
+# Copy files into the working Next.js directory
+# (`npm run dev` script includes a watcher that
+# will keep these files synced)
 cp -R "$currentDir/content" "$previewDir/content"
 cp -R "$currentDir/data" "$previewDir/data"
 cp -R "$currentDir/public/." "$previewDir/public"
-# cd into the site source directory,
-# and prep to start the preview
-cd "$previewDir"
-# Write out product-specific files
-# necessary for preview to work correctly
-cp "./site-specific/.env.$previewProduct" .env.local
-cp "./site-specific/redirects.$previewProduct.js" redirects.js
-# Clean install
-npm ci
-# Copy public assets from source repo into the
-# working Next.js directory
-cp -r "../public/." "./public/"
-cp -r "../data/." "./data/"
-cp -r "../content/." "./content/"
-# run build, or dev
-if [ "${args[0]}" = "build" ]; then
-    echo "running build for vercel"
+# Run local dev preview by default,
+# but also allow and arg to be passed to
+# run a static export (trying this for PR deploy previews)
+isbuild="${args[0]}" = "build"
+if [ isbuild ]; then
+    echo "starting build for vercel deployment..."
     npm run static
 else
-    echo "running start script"
+    echo "starting local preview..."
     npm run dev
 fi
-# NEXT_PUBLIC_ALGOLIA_APP_ID=YY0FFNI7MF NEXT_PUBLIC_ALGOLIA_INDEX=product_WAYPOINT NEXT_PUBLIC_ALGOLIA_SEARCH_ONLY_API_KEY=5e4adfd8094367056501547d6fedb6c5 NEXT_PUBLIC_CWD="/Users/zachshilton/code/waypoint/website" npm run dev
