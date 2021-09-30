bffaaafe900a943979a04a1ff003f5f421427981
Kyle MacDonald <kmacdonald@hashicorp.com>
2021-09-30 13:31:55 -0400

    ✨ use process.env.ENABLE_VERSIONED_DOCS for fetch

---
+++ src/pages/waypoint/docs/[[...page]].tsx
@@ -0,0 +1,58 @@
+import DocsPage from '@hashicorp/react-docs-page'
+import {
+  generateStaticPaths,
+  generateStaticProps,
+} from '@hashicorp/react-docs-page/server'
+import Placement from 'components/author-primitives/placement-table'
+import NestedNode from 'components/author-primitives/nested-node'
+
+// because some of the util functions still require param arity, but we ignore
+// their values when process.env.ENABLE_VERSIONED_DOCS is set to true, we'll
+// just use this string to make it clear by using this k/v
+const temporary_noop = 'im just for show'
+
+const productName = 'Waypoint'
+const productSlug = 'waypoint'
+const basePath = 'docs'
+const additionalComponents = { Placement, NestedNode }
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export default function DocsLayout(props) {
+  return (
+    <DocsPage
+      product={{ name: productName, slug: productSlug }}
+      baseRoute={`${productSlug}/${basePath}`}
+      staticProps={props}
+      showVersionSelect={process.env.ENABLE_VERSIONED_DOCS}
+      additionalComponents={additionalComponents}
+    />
+  )
+}
+
+export async function getStaticPaths() {
+  const paths = await generateStaticPaths({
+    navDataFile: temporary_noop,
+    localContentDir: temporary_noop,
+    // new ----
+    product: { name: productName, slug: productSlug },
+    basePath,
+  })
+  return {
+    fallback: 'blocking',
+    paths,
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const props = await generateStaticProps({
+    navDataFile: temporary_noop,
+    localContentDir: temporary_noop,
+    product: { name: productName, slug: productSlug },
+    params,
+    basePath,
+  })
+  return {
+    props,
+    revalidate: 10,
+  }
+}
