37d3eea829a2a8476457b2796e2896b21af9ae2d
Kyle MacDonald <kmacdonald@hashicorp.com>
2021-10-01 10:58:08 -0400

    redirect _secret-homepage paths on prod since we’ve chosen to colocate these pages with other dev-portal code, this prevents the loading of the _secret-homepage path in production environments

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 const withHashicorp = require('@hashicorp/platform-nextjs-plugin')
+const redirectsConfig = require('./config/redirects/redirects.next.js')
 
 module.exports = withHashicorp({ nextOptimizedImages: true })({
+  async redirects() {
+    return redirectsConfig
+  },
   svgo: {
     plugins: [
       {
