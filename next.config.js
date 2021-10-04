56c7efb17f6566bab912bc4728f5823043a73197
Kyle MacDonald <macdonaldky@gmail.com>
2021-10-04 17:28:19 -0400

    add tmp noindex on all routes

--- next.config.js
+++ next.config.js
@@ -2,11 +2,25 @@ const withHashicorp = require('@hashicorp/platform-nextjs-plugin')
 const withSwingset = require('swingset')
 const redirectsConfig = require('./config/redirects/redirects.next.js')
 
+// temporary: set all paths as noindex, until we're serving from this project
+const temporary_hideDocsPaths = {
+  source: '/:path*',
+  headers: [
+    {
+      key: 'X-Robots-Tag',
+      value: 'noindex',
+    },
+  ],
+}
+
 module.exports = withSwingset({ componentsRoot: 'src/components/*' })(
   withHashicorp({
     nextOptimizedImages: true,
     transpileModules: ['swingset'],
   })({
+    async headers() {
+      return [temporary_hideDocsPaths]
+    },
     async redirects() {
       return redirectsConfig
     },
