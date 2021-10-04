f26331db7469f9b3e76e4a14ef100ae920caeab1
Kyle MacDonald <macdonaldky@gmail.com>
2021-10-04 14:56:00 -0400

    clean up the bad merge

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 const withHashicorp = require('@hashicorp/platform-nextjs-plugin')
-<<<<<<< HEAD
 const withSwingset = require('swingset')
 const redirectsConfig = require('./config/redirects/redirects.next.js')
 
@@ -21,20 +20,3 @@ module.exports = withSwingset({ componentsRoot: 'src/components/*' })(
     },
   })
 )
-=======
-const redirectsConfig = require('./config/redirects/redirects.next.js')
-
-module.exports = withHashicorp({ nextOptimizedImages: true })({
-  async redirects() {
-    return redirectsConfig
-  },
-  svgo: {
-    plugins: [
-      {
-        removeViewBox: false,
-        collapseGroups: false,
-      },
-    ],
-  },
-})
->>>>>>> 19a44f3 (add redirect for /:product/_secret-homepage on prod environments (#5))
