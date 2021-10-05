6ea347cd7d3a898fca917b3a15da403d8d64454e
Kyle MacDonald <macdonaldky@gmail.com>
2021-10-05 17:18:27 -0400

    port over remainder of waypoint layout code

--- next.config.js
+++ next.config.js
@@ -24,6 +24,11 @@ module.exports = withSwingset({ componentsRoot: 'src/components/*' })(
     async redirects() {
       return redirectsConfig
     },
+    env: {
+      HASHI_ENV: process.env.HASHI_ENV || 'development',
+      BUGSNAG_CLIENT_KEY: '06718db5e1d75829801baa0b4ca2fb7b',
+      BUGSNAG_SERVER_KEY: 'b32b4487b5dc72b32f51c8fe33641a43',
+    },
     svgo: {
       plugins: [
         {
