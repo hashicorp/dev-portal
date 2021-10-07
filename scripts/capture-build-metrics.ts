a9f676a3e8e7f65376beb6f93f7a69bd39844d4e
Bryce Kalow <bkalow@hashicorp.com>
2021-10-07 13:00:53 -0500

    get build metrics script corectly sending to DD

--- scripts/capture-build-metrics.ts
+++ scripts/capture-build-metrics.ts
@@ -1,24 +1,30 @@
 import path from 'path'
 import fs from 'fs'
+import { v1 } from '@datadog/datadog-api-client'
 
-const traceFilePath = './.next/trace'
+const configuration = v1.createConfiguration()
+const client = new v1.MetricsApi(configuration)
 
-async function readNextTrace(cwd: string) {
-  const filepath = path.join(cwd, traceFilePath)
-
-  const content = await fs.promises.readFile(filepath, { encoding: 'utf-8' })
-
-  const parts = content.trim().split('\n')
-
-  return parts
-}
-
-const events = [
+const TRACE_FILE_PATH = './.next/trace'
+const EVENTS = [
   'next-build',
   'run-webpack-compiler',
   'static-generation',
   'next-export',
 ]
+
+async function readNextTrace(cwd: string) {
+  const filepath = path.join(cwd, TRACE_FILE_PATH)
+
+  const content = await fs.promises.readFile(filepath, { encoding: 'utf-8' })
+
+  // The trace file consists of multiple JSON arrays separated by newlines
+  // This gives us an array of un-parsed JSON arrays
+  const parts = content.trim().split('\n')
+
+  // Parse the arrays from the trace and flatten the full array, giving us a flat list of events
+  return parts.map((part) => JSON.parse(part)).flat()
+}
 
 function captureMetric({
   name,
@@ -32,35 +38,43 @@ function captureMetric({
   tags?: string[]
 }) {
   return {
+    host: '',
     metric: name,
-    points: [[timestamp ?? Date.now(), duration]],
+    points: [[timestamp ?? Math.round(Date.now() / 1e3), duration]],
     tags,
     type: 'gauge',
   }
 }
 
 async function main() {
+  if (!process.env.CI) return
+
   const [, , appName] = process.argv
 
-  const timestamp = Date.now()
+  const timestamp = Math.round(Date.now() / 1e3)
   const trace = await readNextTrace(process.cwd())
 
-  const flatEvents = trace.map((part) => JSON.parse(part)).flat()
+  const filteredEvents = trace.filter((event) => EVENTS.includes(event.name))
 
-  const filteredEvents = flatEvents.filter((event) =>
-    events.includes(event.name)
+  const structuredMetrics = filteredEvents.map((event) =>
+    captureMetric({
+      name: `build.${event.name}`,
+      duration: Math.round(event.duration / 1e3),
+      timestamp,
+      tags: [`app:${appName}`, `environment:${process.env.HASHI_ENV}`],
+    })
   )
 
-  filteredEvents.forEach((event) => {
-    console.log(
-      captureMetric({
-        name: `build.${event.name}`,
-        duration: Math.round(event.duration / 1e3),
-        timestamp,
-        tags: [`app:${appName}`],
-      })
-    )
-  })
+  try {
+    await client.submitMetrics({
+      body: {
+        series: structuredMetrics,
+      },
+    })
+  } catch (error) {
+    // Swallow errors here, we don't want to impact the build or make it seem like there's been an error in the actual app
+    // if something goes wrong when sending metrics
+  }
 }
 
 main()
