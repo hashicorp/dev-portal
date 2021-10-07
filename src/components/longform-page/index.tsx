1770088d8f26659d13fd86a0c262aa23a7e24581
Kyle MacDonald <macdonaldky@gmail.com>
2021-10-07 12:06:49 -0400

    port over waypoint/security

---
+++ src/components/longform-page/index.tsx
@@ -0,0 +1,16 @@
+import classNames from 'classnames'
+import styles from './style.module.css'
+
+export default function LongformPage({ className, title, alert, children }) {
+  return (
+    <div className={classNames(styles.longformPage, className)}>
+      <div className="g-grid-container">
+        <div className={styles.longformWrapper}>
+          {alert && <div className={styles.alert}>{alert}</div>}
+          <h2 className="g-type-display-2">{title}</h2>
+          {children}
+        </div>
+      </div>
+    </div>
+  )
+}
