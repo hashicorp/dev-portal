9ce0ffc00dc52ee9d0e6ba8279c9d6b3a7d7690f
Bryce Kalow <bkalow@hashicorp.com>
2021-10-13 11:10:34 -0500

    remove unnecessary prop

--- src/pages/waypoint/docs/[[...page]].tsx
+++ src/pages/waypoint/docs/[[...page]].tsx
@@ -15,9 +15,6 @@ export default function DocsLayout(props) {
       product={{ name: productName, slug: productSlug }}
       baseRoute={`${productSlug}/${basePath}`}
       staticProps={props}
-      showVersionSelect={
-        process.env.ENABLE_VERSIONED_DOCS.toString() === 'true'
-      }
       additionalComponents={additionalComponents}
     />
   )
