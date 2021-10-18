d2bcb4500b906f74209230cd7b86e8c50708b768
Bryce Kalow <bkalow@hashicorp.com>
2021-10-18 12:07:36 -0500

    only run webpack build step

---
+++ scripts/next-build-webpack-only.ts
@@ -0,0 +1,22 @@
+import { spawn } from 'child_process'
+
+/**
+ * Wrap the next build command to only run the webpack step of the build, short-circuiting before static generation.
+ */
+async function main() {
+  const buildProc = spawn('./node_modules/.bin/next', ['build'], {
+    env: { ...process.env, FORCE_COLOR: 'true' },
+  })
+
+  buildProc.stdout.on('data', (data) => {
+    const value = data.toString()
+    if (value.includes('Collecting page data')) {
+      console.log('Webpack build finished, exiting.')
+      buildProc.kill('SIGINT')
+    } else {
+      console.log(value.trim())
+    }
+  })
+}
+
+main()
