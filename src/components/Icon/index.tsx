815a05dde92e6fe03abedef7237ec99874843778
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2021-10-19 09:36:17 -0400

    Adding Icon component

---
+++ src/components/Icon/index.tsx
@@ -0,0 +1,26 @@
+import classNames from 'classnames/bind'
+import InlineSvg from '@hashicorp/react-inline-svg'
+import s from './style.module.css'
+
+interface IconProps {
+  className?: string
+  name: string
+  size?: 16 | 24
+}
+
+const cx = classNames.bind(s)
+
+const Icon: React.FC<IconProps> = ({ className, name, size = 16 }) => {
+  const classes = cx('Icon', {
+    className,
+  })
+
+  return (
+    <InlineSvg
+      className={classes}
+      src={require(`@hashicorp/flight-icons/svg/${name}-${size}.svg?include`)}
+    />
+  )
+}
+
+export default Icon
