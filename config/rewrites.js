d7a85c847d744b4f80e7258b7b3466f01f80bce2
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-10-20 14:43:12 -0400

    add comment on rewrites

--- config/rewrites.js
+++ config/rewrites.js
@@ -1,5 +1,12 @@
 async function rewritesConfig() {
   return {
+    // We run our rewrites BEFORE letting NextJS check if there
+    // is a /pages file to be served, since there are conflicts, for example:
+    // - "/" should show _secret-io-homepage, even though there is a dev-portal landing page
+    // - "/docs" should show "/waypoint/docs", even if there is a dev-portal /docs page
+    // ref: https://nextjs.org/docs/api-reference/next.config.js/rewrites
+    // (we're using the option to return an object { beforeFiles, afterFiles, fallback }
+    // rather than a simple array)
     beforeFiles: [
       {
         source: '/',
