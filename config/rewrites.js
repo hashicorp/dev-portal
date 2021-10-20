f251649a685ffc5ab2d70a367f2217a13ea0920b
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-10-20 14:05:36 -0400

    test beforeFiles homepage rewrite

--- config/rewrites.js
+++ config/rewrites.js
@@ -1,28 +1,25 @@
-const waypointPathRewrites = [
-  {
-    source: '/',
-    destination: '/waypoint/_secret-io-homepage',
-    // has: [
-    //   {
-    //     type: 'host',
-    //     value: 'wp.snarglepuss.com',
-    //   },
-    // ],
-  },
-  // {
-  //   source: '/:path*',
-  //   destination: '/waypoint/:path*',
-  //   has: [
-  //     {
-  //       type: 'host',
-  //       value: 'wp.snarglepuss.com',
-  //     },
-  //   ],
-  // },
-]
+// const waypointPathRewrites = [
+//   {
+//     source: '/:path*',
+//     destination: '/waypoint/:path*',
+//     has: [
+//       {
+//         type: 'host',
+//         value: 'wp.snarglepuss.com',
+//       },
+//     ],
+//   },
+// ]
 
 async function rewritesConfig() {
-  return [...waypointPathRewrites]
+  return {
+    beforeFiles: [
+      {
+        source: '/',
+        destination: '/waypoint/_secret-io-homepage',
+      },
+    ],
+  }
 }
 
 module.exports = rewritesConfig
