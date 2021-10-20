72e28ff7369f8481d04e24e9acf2951cdf79da41
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-10-20 14:28:27 -0400

    remove duplicate (?) rewrite

--- config/rewrites.js
+++ config/rewrites.js
@@ -2,16 +2,6 @@ async function rewritesConfig() {
   return {
     beforeFiles: [
       {
-        source: '/',
-        destination: '/waypoint/_secret-io-homepage',
-        has: [
-          {
-            type: 'host',
-            value: 'wp.snarglepuss.com',
-          },
-        ],
-      },
-      {
         source: '/:path*',
         destination: '/waypoint/:path*',
         has: [
