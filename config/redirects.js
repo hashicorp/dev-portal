6f96a0b1490ecc218f7b9ec762686306c7a4e712
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-10-20 16:08:39 -0400

    attempt to fix redirects for non-waypoint dev-portal pages

--- config/redirects.js
+++ config/redirects.js
@@ -1,5 +1,7 @@
 const proxySettings = require('./proxy-settings')
 
+const DEV_PORTAL_DOMAIN = 'https://hashi-dev-portal.vercel.app'
+
 // Redirect all proxied Waypoint pages
 // to the Waypoint project domain
 // (we do this regardless of which domain we're serving from)
@@ -12,16 +14,39 @@ const proxySettings = require('./proxy-settings')
 // one way to achieve this would be with a "has" entry. However, for local dev
 // we'll likely be doing other env-based stuff, so it may be easier to just remove
 // these redirects in dev rather than list specific production hosts.
+const waypointHost = proxySettings.waypoint.host
 const waypointDomain = proxySettings.waypoint.domain
-const waypointProxyRedirects = proxySettings.waypoint.routesToProxy.map(
-  ({ proxiedRoute, projectPage }) => {
+const waypointProxyRedirects = proxySettings.waypoint.routesToProxy
+  .map(({ proxiedRoute, projectPage }) => {
     return {
       source: projectPage,
       destination: waypointDomain + proxiedRoute,
       permanent: false,
     }
-  }
-)
+  })
+  // If the route in question is NOT something we want
+  // to show on the proxied domain, then we should
+  // redirect to the live dev portal domain
+  .concat([
+    {
+      source: '/:path*',
+      destination: DEV_PORTAL_DOMAIN + '/:path*',
+      permanent: false,
+    },
+  ])
+  // For all these redirects, we want them to
+  // be specific to the Waypoint site hostname
+  .map((redirect) => {
+    return {
+      ...redirect,
+      has: [
+        {
+          type: 'host',
+          value: waypointHost,
+        },
+      ],
+    }
+  })
 
 async function redirectsConfig() {
   return [...waypointProxyRedirects]
