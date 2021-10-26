f39a7395b62acbfea262b35f8af2121cf11c8faa
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2021-10-26 18:13:33 -0400

    Handling active menu items

--- src/components/sidebar/components/menu-item/index.tsx
+++ src/components/sidebar/components/menu-item/index.tsx
@@ -2,11 +2,15 @@ import { MenuItem } from 'components/sidebar'
 import s from './style.module.css'
 
 interface SidebarMenuItemProps {
+  isActive: boolean
   item: MenuItem
 }
 
 // TODO: implement submenus (ref: https://app.asana.com/0/1201010428539925/1201265683986459/f)
-const SidebarMenuItem: React.FC<SidebarMenuItemProps> = ({ item }) => {
+const SidebarMenuItem: React.FC<SidebarMenuItemProps> = ({
+  isActive = false,
+  item,
+}) => {
   const { divider, title, path } = item
 
   // TODO: remove this once `divider` isn't in the data anymore
@@ -17,7 +21,7 @@ const SidebarMenuItem: React.FC<SidebarMenuItemProps> = ({ item }) => {
   return (
     <li className={s.sidebarMenuItem}>
       {/* TODO: conditionally render as button if submenu */}
-      <a href={path}>
+      <a aria-current={isActive ? 'page' : undefined} href={path}>
         <span>{title}</span>
         {/* TODO: this is for the submenu icon */}
         {/* <span></span> */}
--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,9 +1,13 @@
 import { useState } from 'react'
+import { useRouter } from 'next/router'
 import BackToLink from './components/back-to-link'
 import FilterInput from './components/filter-input'
 import SidebarMenuItem from './components/menu-item'
 import SidebarNav from './components/sidebar-nav'
 import s from './style.module.css'
+
+// TODO: store this in a Context that ProductChooser updates?
+const product = 'waypoint'
 
 export interface MenuItem {
   divider?: boolean
@@ -28,6 +32,7 @@ const getFilteredMenuItems = (items: MenuItem[], filterValue: string) => {
 }
 
 const Sidebar: React.FC<SidebarProps> = ({ menuItems }) => {
+  const router = useRouter()
   const [filterValue, setFilterValue] = useState('')
 
   const filteredMenuItems = getFilteredMenuItems(menuItems, filterValue)
@@ -37,14 +42,23 @@ const Sidebar: React.FC<SidebarProps> = ({ menuItems }) => {
       <BackToLink />
       <FilterInput value={filterValue} onChange={setFilterValue} />
       <SidebarNav>
-        {filteredMenuItems.map((item, index) => (
-          <SidebarMenuItem
-            item={item}
-            // TODO: come up with better alternative to index
-            // eslint-disable-next-line react/no-array-index-key
-            key={`sidebar-menu-item-${index}`}
-          />
-        ))}
+        {filteredMenuItems.map((item, index) => {
+          if (item.routes) {
+            return null
+          }
+
+          const path = `/${product}/docs/${item.path}`
+          const isActive = router.asPath === path
+          return (
+            <SidebarMenuItem
+              isActive={isActive}
+              item={{ ...item, path }}
+              // TODO: come up with better alternative to index
+              // eslint-disable-next-line react/no-array-index-key
+              key={`sidebar-menu-item-${index}`}
+            />
+          )
+        })}
       </SidebarNav>
     </div>
   )
