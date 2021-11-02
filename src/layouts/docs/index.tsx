1a775c65c7633d7ce1523283330126938229a9fd
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2021-11-02 12:55:53 -0400

    Making TOC more functional and improving layout

--- src/layouts/docs/index.tsx
+++ src/layouts/docs/index.tsx
@@ -1,5 +1,6 @@
 import NavigationHeader from 'components/navigation-header'
 import Sidebar, { MenuItem } from 'components/sidebar'
+import { useLayoutEffect, useState } from 'react'
 import s from './docs-layout.module.css'
 
 interface DocsLayoutProps {
@@ -7,29 +8,62 @@ interface DocsLayoutProps {
   navData: MenuItem[]
 }
 
-const DocsLayout: React.FC<DocsLayoutProps> = (props) => (
-  <div className={s.container}>
-    <NavigationHeader />
-    <div className={s.body}>
-      <Sidebar menuItems={props.navData} />
-      <div className={s.contentWrapper}>
-        <div className={s.content}>
-          {/* <div className={s.versionSwitcher}>VERSION SWITCHER</div> */}
-          <main className={s.main}>{props.children}</main>
-          <aside className={s.tableOfContents}>
-            <p className={s.tableOfContentsLabel}>Table of Contents</p>
-            <ul className={s.tableOfContentsList}>
-              <li className={s.tableOfContentsListItem}>Introduction</li>
-              <li className={s.tableOfContentsListItem}>Vault Documentation</li>
-              <li className={s.tableOfContentsListItem}>Quickstart</li>
-              <li className={s.tableOfContentsListItem}>Use cases</li>
-              <li className={s.tableOfContentsListItem}>Learning</li>
-            </ul>
-          </aside>
+// TODO: move to eventual TableOfContents component
+interface TableOfContentsHeading {
+  slug: string
+  text: string
+}
+
+const DocsLayout: React.FC<DocsLayoutProps> = (props) => {
+  const [tableOfContentsHeadings, setTableOfContentsHeadings] = useState<
+    TableOfContentsHeading[]
+  >([])
+
+  // This is extremely dependent on the HTML passed in
+  useLayoutEffect(() => {
+    const headingElements = document
+      .querySelector('main')
+      .querySelectorAll('h1,h2,h3,h4,h5,h6')
+
+    const headings = []
+    headingElements.forEach((heading) => {
+      const slug = heading.querySelectorAll('a')[1].id
+      const text = heading.textContent.substring(1)
+      headings.push({ slug, text })
+    })
+
+    setTableOfContentsHeadings(headings)
+  }, [])
+
+  return (
+    <div className={s.container}>
+      <NavigationHeader />
+      <div className={s.body}>
+        <Sidebar menuItems={props.navData} />
+        <div className={s.contentWrapper}>
+          <div className={s.content}>
+            {/* <div className={s.versionSwitcher}>VERSION SWITCHER</div> */}
+            <main className={s.main}>{props.children}</main>
+            {/* TODO: move to its own component & file */}
+            <aside className={s.tableOfContents}>
+              <p className={s.tableOfContentsLabel} id="table-of-contents">
+                Table of Contents
+              </p>
+              <nav aria-labelledby="table-of-contents">
+                <ul className={s.tableOfContentsList}>
+                  {tableOfContentsHeadings.map(({ slug, text }) => (
+                    <li className={s.tableOfContentsListItem} key={slug}>
+                      <a href={`#${slug}`}>{text}</a>
+                    </li>
+                  ))}
+                </ul>
+              </nav>
+            </aside>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default DocsLayout
