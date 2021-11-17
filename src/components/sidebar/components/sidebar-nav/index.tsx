75680e649d90b0b0c9ea4b2240a15fd18c2a4c8e
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2021-11-17 00:31:56 -0500

    Moving isActive calculation

--- src/components/sidebar/components/sidebar-nav/index.tsx
+++ src/components/sidebar/components/sidebar-nav/index.tsx
@@ -1,4 +1,3 @@
-import { useRouter } from 'next/router'
 import { MenuItem } from 'components/sidebar'
 import SidebarNavMenuItem from './sidebar-nav-menu-item'
 import s from './style.module.css'
@@ -14,9 +13,6 @@ interface SidebarNavProps {
 const product = 'waypoint'
 
 const SidebarNav: React.FC<SidebarNavProps> = ({ menuItems, title }) => {
-  const router = useRouter()
-
-  // TODO: not sure if we need this to be a nav element or not
   return (
     <nav aria-labelledby={SIDEBAR_LABEL_ID}>
       <p className={s.sidebarNavLabel} id={SIDEBAR_LABEL_ID}>
@@ -25,10 +21,8 @@ const SidebarNav: React.FC<SidebarNavProps> = ({ menuItems, title }) => {
       <ul className={s.sidebarNavList} role="menubar">
         {menuItems.map((item, index) => {
           const path = `/${product}/docs/${item.path}`
-          const isActive = router.asPath === path
           return (
             <SidebarNavMenuItem
-              isActive={isActive}
               item={{ ...item, path }}
               // TODO: come up with better alternative to index
               // eslint-disable-next-line react/no-array-index-key
