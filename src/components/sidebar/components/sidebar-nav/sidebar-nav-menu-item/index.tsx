5da5ed6bc9f702571936a367574f358d3042349d
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2021-11-17 00:32:18 -0500

    WIP adding submenus

--- src/components/sidebar/components/sidebar-nav/sidebar-nav-menu-item/index.tsx
+++ src/components/sidebar/components/sidebar-nav/sidebar-nav-menu-item/index.tsx
@@ -1,52 +1,84 @@
 import { useState } from 'react'
+import { useRouter } from 'next/router'
 import { MenuItem } from 'components/sidebar'
 import s from './style.module.css'
 
 interface SidebarMenuItemProps {
-  isActive: boolean
   item: MenuItem
 }
 
-const SidebarSubmenu: React.FC<{ item: MenuItem }> = ({ item }) => {
+const SidebarNavSubmenu: React.FC<{ currentPath: string; item: MenuItem }> = ({
+  currentPath,
+  item,
+}) => {
   const [isOpen, setIsOpen] = useState(false)
 
-  return null
+  return (
+    <li role="none">
+      <button
+        aria-expanded={isOpen}
+        aria-haspopup
+        onClick={() => setIsOpen((prevState) => !prevState)}
+        role="menuitem"
+        style={{ display: 'block' }}
+      >
+        {item.title}
+      </button>
+      {isOpen && (
+        <ul role="menu">
+          {item.routes.map((route) =>
+            route.routes ? (
+              <SidebarNavSubmenu currentPath={currentPath} item={route} />
+            ) : (
+              <li role="none">
+                <a
+                  aria-current={
+                    currentPath.endsWith(route.path) ? 'page' : undefined
+                  }
+                  role="menuitem"
+                  className={s.sidebarLink}
+                  href={route.path}
+                  dangerouslySetInnerHTML={{ __html: route.title }}
+                />
+              </li>
+            )
+          )}
+        </ul>
+      )}
+    </li>
+  )
 }
 
 // TODO: implement submenus (ref: https://app.asana.com/0/1201010428539925/1201265683986459/f)
-const SidebarNavMenuItem: React.FC<SidebarMenuItemProps> = ({
-  isActive = false,
-  item,
-}) => {
-  // TODO: handle the case when `href` is set
-  const { divider, title, path } = item
+const SidebarNavMenuItem: React.FC<SidebarMenuItemProps> = ({ item }) => {
+  const router = useRouter()
+  const currentPath = router.asPath
 
   // TODO: remove this once `divider` isn't in the data anymore
   // Design decided to remove the dividers in the new sidebar.
-  if (divider) {
+  if (item.divider) {
     return null
   }
 
-  if (item.routes) {
-    console.log(item)
-    return null
-  }
+  const renderMenuItem = () => {
+    if (item.routes) {
+      return <SidebarNavSubmenu currentPath={currentPath} item={item} />
+    }
 
-  return (
-    <li>
-      {/* TODO: conditionally render as button if submenu */}
+    const isActive = currentPath.endsWith(item.path)
+    return (
       <a
         aria-current={isActive ? 'page' : undefined}
         role="menuitem"
         className={s.sidebarLink}
-        href={path}
+        href={item.path}
       >
-        <span>{title}</span>
-        {/* TODO: this is for the submenu icon */}
-        {/* <span></span> */}
+        {item.title}
       </a>
-    </li>
-  )
+    )
+  }
+
+  return <li>{renderMenuItem()}</li>
 }
 
 export default SidebarNavMenuItem
