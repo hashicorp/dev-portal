7f7382f51a634d1e4ce8fc5e48f484cc8c525524
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-11-19 11:16:02 -0500

    fix: patch minor 404 page issues (#38)
    * fix: use apostrophes on 404
    
    * fix: remove console.log in env-checks
    
    * fix: remove other console.log
    
    * fix: remove commented-out function
    
    * docs: add comment to .env.local.example
    
    * fix: add domain matching to env-checks for 404 pages
    
    * fix: use public env for git ref
    
    * chore(waypoint): empty commit for waypoint deploy
    
    * chore(boundary): empty commit for boundary deploy
    
    * fix: add commitMsg fallback string
    
    * chore(waypoint): empty commit for waypoint deploy
    
    * chore(boundary): empty commit for boundary deploy

--- src/lib/env-checks.js
+++ src/lib/env-checks.js
@@ -1,12 +1,10 @@
+const proxyConfig = require('../../config/proxy-config')
+
 // NOTE: this module uses CommonJS exports,
 // as it must be required() into redirects and rewrites config,
 // neither of which are transpiled.
 
-const PROXIED_PRODUCTS = ['boundary', 'waypoint']
-
-// function isProduction() {
-//   return process.env.VERCEL_ENV == 'production'
-// }
+const PROXIED_PRODUCTS = Object.keys(proxyConfig)
 
 function isPreview() {
   return process.env.HASHI_ENV == 'preview'
@@ -17,24 +15,51 @@ function getProxiedProductSlug() {
     if (!acc && isProxiedProduct(slug)) return slug
     return acc
   }, false)
-  console.log({ proxiedProductSlug })
   return proxiedProductSlug
+}
+
+function getMatchedDomain(deployDomain) {
+  const domainProductSlug = PROXIED_PRODUCTS.reduce((acc, slug) => {
+    const productHost = proxyConfig[slug].host
+    if (!acc && deployDomain == productHost) return slug
+    return acc
+  }, false)
+  return domainProductSlug
 }
 
 function isProxiedProduct(productSlug) {
   const isDevEnvSet = process.env.DEV_IO_PROXY == productSlug
-  // Allow commit messages to trigger specific proxy settings...
+  // Allow commit messages to trigger specific proxy settings,
+  // but NOT if we're deploying off the main branch.
   const commitMsg =
     process.env.VERCEL_GIT_COMMIT_MESSAGE ||
     process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_MESSAGE ||
     ''
   const commitFirstLine = commitMsg.split('\n')[0]
-  const hasCommitFlag = commitFirstLine.indexOf(`(${productSlug})`) !== -1
+  const isCommitMatch = commitFirstLine.indexOf(`(${productSlug})`) !== -1
   // ... but only if NOT in production
-  const isOnMain = process.env.VERCEL_GIT_COMMIT_REF == 'main'
-  const isCommitMatch = !isOnMain && hasCommitFlag
-  console.log({ commitFirstLine, isDevEnvSet, isCommitMatch, isOnMain })
-  return isDevEnvSet || isCommitMatch
+  const commitRef =
+    process.env.VERCEL_GIT_COMMIT_REF ||
+    process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF
+  const isOnMain = commitRef == 'main'
+  // When deploying to specific proxied domains,
+  // this function should accurately reflect the proxied product
+  const deployDomain =
+    process.env.VERCEL_URL || process.env.NEXT_PUBLIC_VERCEL_URL
+  const isDomainMatch = productSlug == getMatchedDomain(deployDomain)
+  // Combine local and deployed settings
+  const isLocalMatch = isDevEnvSet
+  const isDeployedMatch = isDomainMatch || (isCommitMatch && !isOnMain)
+  console.log({
+    isLocalMatch,
+    isDeployedMatch,
+    isDomainMatch,
+    deployDomain,
+    commitFirstLine,
+    isCommitMatch,
+    isOnMain,
+  })
+  return isLocalMatch || isDeployedMatch
 }
 
 // TODO: still experimenting with deploy preview approach
