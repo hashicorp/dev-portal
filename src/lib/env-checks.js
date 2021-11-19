2903c4fd83cce088687d5429e9f93e1f22fb9150
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-11-19 13:40:44 -0500

    fix: implement getServerSide error page for proper 404 fix

--- src/lib/env-checks.js
+++ src/lib/env-checks.js
@@ -10,24 +10,24 @@ function isPreview() {
   return process.env.HASHI_ENV == 'preview'
 }
 
-function getProxiedProductSlug() {
+function getProxiedProductSlug(hostname) {
   const proxiedProductSlug = PROXIED_PRODUCTS.reduce((acc, slug) => {
-    if (!acc && isProxiedProduct(slug)) return slug
+    if (!acc && isProxiedProduct(slug, hostname)) return slug
     return acc
   }, false)
   return proxiedProductSlug
 }
 
-function getMatchedDomain(deployDomain) {
+function getMatchedDomain(hostname) {
   const domainProductSlug = PROXIED_PRODUCTS.reduce((acc, slug) => {
     const productHost = proxyConfig[slug].host
-    if (!acc && deployDomain == productHost) return slug
+    if (!acc && hostname == productHost) return slug
     return acc
   }, false)
   return domainProductSlug
 }
 
-function isProxiedProduct(productSlug) {
+function isProxiedProduct(productSlug, hostname) {
   const isDevEnvSet = process.env.DEV_IO_PROXY == productSlug
   // Allow commit messages to trigger specific proxy settings,
   // but NOT if we're deploying off the main branch.
@@ -44,25 +44,10 @@ function isProxiedProduct(productSlug) {
   const isOnMain = commitRef == 'main'
   // When deploying to specific proxied domains,
   // this function should accurately reflect the proxied product
-  // TODO: this doesn't work, the Vercel domain is the same
-  // TODO: regardless of which domain we're viewing the site through
-  // TODO: (eg test-bd.hashicorp.com & wp.snarglepuss.com both just
-  // TODO: point back to the same Vercel domain)
-  const deployDomain =
-    process.env.VERCEL_URL || process.env.NEXT_PUBLIC_VERCEL_URL
-  const isDomainMatch = productSlug == getMatchedDomain(deployDomain)
+  const isDomainMatch = productSlug == getMatchedDomain(hostname)
   // Combine local and deployed settings
   const isLocalMatch = isDevEnvSet
   const isDeployedMatch = isDomainMatch || (isCommitMatch && !isOnMain)
-  console.log({
-    isLocalMatch,
-    isDeployedMatch,
-    isDomainMatch,
-    deployDomain,
-    commitFirstLine,
-    isCommitMatch,
-    isOnMain,
-  })
   return isLocalMatch || isDeployedMatch
 }
 
