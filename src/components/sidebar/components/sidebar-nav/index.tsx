12d2eca2e9cdce15d878c704ee031958a678078d
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2021-11-23 18:00:38 -0500

    Moving addActiveStateMetadata to Sidebar to memoize

--- src/components/sidebar/components/sidebar-nav/index.tsx
+++ src/components/sidebar/components/sidebar-nav/index.tsx
@@ -1,4 +1,3 @@
-import { useRouter } from 'next/router'
 import { MenuItem } from 'components/sidebar'
 import SidebarNavMenuItem from './sidebar-nav-menu-item'
 import s from './style.module.css'
@@ -10,68 +9,25 @@ interface SidebarNavProps {
   title: string
 }
 
-// TODO: this might be a helpful to abstract as a util? The name feels looong
-const getCurrentPathWithoutParamsOrAnchors = (path: string): string => {
-  const matches = path.match(/.+?(?=(#|\?))/)
-  if (matches) {
-    return matches[0]
-  }
-
-  return path
-}
-
-/**
- * TODO: update & rename to add additional metadata
- *   - fullPath: the full subpath of a non-submenu item (currently handled in sidebar-nav-menu-item)
- *   - id: a generated slug based off of title and/or fullPath (depends if it's a submenu)
- */
-const addActiveStateMetadata = (currentPath: string, items: MenuItem[]) => {
-  let foundActiveItem = false
-
-  items.forEach((item) => {
-    if (foundActiveItem) {
-      item[item.routes ? 'hasActiveChild' : 'isActive'] = false
-      return
-    }
-
-    if (item.routes) {
-      foundActiveItem = addActiveStateMetadata(currentPath, item.routes)
-      item.hasActiveChild = foundActiveItem
-    } else {
-      foundActiveItem = currentPath.endsWith(item.path)
-      item.isActive = foundActiveItem
-    }
-  })
-
-  return foundActiveItem
-}
-
-const SidebarNav: React.FC<SidebarNavProps> = ({ menuItems, title }) => {
-  const router = useRouter()
-  const currentPath = getCurrentPathWithoutParamsOrAnchors(router.asPath)
-
-  addActiveStateMetadata(currentPath, menuItems)
-
-  return (
-    <nav aria-labelledby={SIDEBAR_LABEL_ID}>
-      <p className={s.sidebarNavLabel} id={SIDEBAR_LABEL_ID}>
-        {title}
-      </p>
-      <a className={s.skipToMainContent} href="#main">
-        Skip to main content
-      </a>
-      <ul className={s.sidebarNavList}>
-        {menuItems.map((item, index) => (
-          <SidebarNavMenuItem
-            item={item}
-            // TODO: use item.id when it has been added to the metadata (see above)
-            // eslint-disable-next-line react/no-array-index-key
-            key={`sidebar-nav-menu-item-${index}`}
-          />
-        ))}
-      </ul>
-    </nav>
-  )
-}
+const SidebarNav: React.FC<SidebarNavProps> = ({ menuItems, title }) => (
+  <nav aria-labelledby={SIDEBAR_LABEL_ID}>
+    <p className={s.sidebarNavLabel} id={SIDEBAR_LABEL_ID}>
+      {title}
+    </p>
+    <a className={s.skipToMainContent} href="#main">
+      Skip to main content
+    </a>
+    <ul className={s.sidebarNavList}>
+      {menuItems.map((item, index) => (
+        <SidebarNavMenuItem
+          item={item}
+          // TODO: use item.id when it has been added to the metadata (see above)
+          // eslint-disable-next-line react/no-array-index-key
+          key={`sidebar-nav-menu-item-${index}`}
+        />
+      ))}
+    </ul>
+  </nav>
+)
 
 export default SidebarNav
--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
+import { useRouter } from 'next/router'
 import SidebarBackToLink from './components/sidebar-back-to-link'
 import SidebarFilterInput from './components/sidebar-filter-input'
 import SidebarNav from './components/sidebar-nav'
@@ -24,6 +25,52 @@ interface SidebarProps {
   menuItems: MenuItem[]
 }
 
+/**
+ * TODO: update & rename to add additional metadata
+ *   - fullPath: the full subpath of a non-submenu item (currently handled in sidebar-nav-menu-item)
+ *   - id: a generated slug based off of title and/or fullPath (depends if it's a submenu)
+ */
+const addActiveStateMetadata = (
+  currentPath: string,
+  items: MenuItem[]
+): { foundActiveItem: boolean; itemsWithMetadata: MenuItem[] } => {
+  let foundActiveItem = false
+
+  const itemsWithMetadata = items.map((item) => {
+    const itemCopy = { ...item }
+
+    if (item.divider) {
+      return itemCopy
+    }
+
+    if (foundActiveItem) {
+      itemCopy[item.routes ? 'hasActiveChild' : 'isActive'] = false
+    } else if (item.routes) {
+      const result = addActiveStateMetadata(currentPath, item.routes)
+      foundActiveItem = result.foundActiveItem
+      itemCopy.routes = result.itemsWithMetadata
+      itemCopy.hasActiveChild = result.foundActiveItem
+    } else {
+      foundActiveItem = currentPath.endsWith(item.path)
+      itemCopy.isActive = foundActiveItem
+    }
+
+    return itemCopy
+  })
+
+  return { foundActiveItem, itemsWithMetadata }
+}
+
+// TODO: this might be a helpful to abstract as a util? The name feels looong
+const getCurrentPathWithoutParamsOrAnchors = (path: string): string => {
+  const matches = path.match(/.+?(?=(#|\?))/)
+  if (matches) {
+    return matches[0]
+  }
+
+  return path
+}
+
 // TODO: will need to recursively search submenus when they're implemented
 const getFilteredMenuItems = (items: MenuItem[], filterValue: string) => {
   if (!filterValue) {
@@ -36,8 +83,14 @@ const getFilteredMenuItems = (items: MenuItem[], filterValue: string) => {
 }
 
 const Sidebar: React.FC<SidebarProps> = ({ menuItems }) => {
+  const router = useRouter()
+  const currentPath = getCurrentPathWithoutParamsOrAnchors(router.asPath)
+  const { itemsWithMetadata } = useMemo(
+    () => addActiveStateMetadata(currentPath, menuItems),
+    [currentPath, menuItems]
+  )
   const [filterValue, setFilterValue] = useState('')
-  const filteredMenuItems = getFilteredMenuItems(menuItems, filterValue)
+  const filteredMenuItems = getFilteredMenuItems(itemsWithMetadata, filterValue)
 
   return (
     <div className={s.sidebar}>
