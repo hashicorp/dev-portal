27c239b06754c818e13a7f82353f68f985a0b92b
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2021-11-30 15:47:06 -0500

    Initializing default value more appropriately

--- src/contexts/use-window-size.tsx
+++ src/contexts/use-window-size.tsx
@@ -4,9 +4,43 @@ const DEFAULT_MOBILE_WIDTH = 728
 
 const DEFAULT_TABLET_WIDTH = 1000
 
+const getDerivedStateVariables = ({ mobileWidth, tabletWidth, width }) => {
+  let isMobile = false
+  let isTablet = false
+
+  if (width <= mobileWidth) {
+    isMobile = true
+  } else if (width <= tabletWidth) {
+    isTablet = true
+  }
+
+  return { isMobile, isTablet }
+}
+
+const getDefaultValue = ({
+  mobileWidth = DEFAULT_MOBILE_WIDTH,
+  tabletWidth = DEFAULT_TABLET_WIDTH,
+}) => {
+  let height: number | undefined
+  let width: number | undefined
+
+  if (typeof window !== 'undefined') {
+    height = window.innerHeight
+    width = window.innerWidth
+  }
+
+  const { isMobile, isTablet } = getDerivedStateVariables({
+    mobileWidth,
+    tabletWidth,
+    width,
+  })
+
+  return { height, isMobile, isTablet, mobileWidth, tabletWidth, width }
+}
+
 const DEFAULT_VALUE = {
-  height: undefined,
-  width: undefined,
+  height: typeof window === 'undefined' ? undefined : window.innerHeight,
+  width: typeof window === 'undefined' ? undefined : window.innerWidth,
 }
 
 interface WindowSizeProviderProps {
@@ -28,34 +62,37 @@ const WindowSizeProvider: React.FC<WindowSizeProviderProps> = ({
   mobileWidth = DEFAULT_MOBILE_WIDTH,
   tabletWidth = DEFAULT_TABLET_WIDTH,
 }) => {
-  const [value, setValue] = useState<WindowSize>(DEFAULT_VALUE)
+  const [value, setValue] = useState<WindowSize>(() => {
+    const defaultValue = getDefaultValue({ mobileWidth, tabletWidth })
+    console.log('default context value:', defaultValue)
+    return defaultValue
+  })
 
   useEffect(() => {
     const handleResize = () => {
       const height = window.innerHeight
       const width = window.innerWidth
+      const { isMobile, isTablet } = getDerivedStateVariables({
+        mobileWidth,
+        tabletWidth,
+        width,
+      })
 
-      // Derive additional state
-      let isMobile = false
-      let isTablet = false
-      if (width <= mobileWidth) {
-        isMobile = true
-      } else if (width <= tabletWidth) {
-        isTablet = true
-      }
-
-      setValue({ height, isMobile, isTablet, mobileWidth, tabletWidth, width })
+      setValue({
+        ...value,
+        height,
+        isMobile,
+        isTablet,
+        width,
+      })
     }
 
     // Add the resize event listener
     window.addEventListener('resize', handleResize)
 
-    // Call immediately to set the inital state values
-    handleResize()
-
     // Remove resize listener on cleanup
     return () => window.removeEventListener('resize', handleResize)
-  }, [mobileWidth, tabletWidth])
+  }, [mobileWidth, tabletWidth, value])
 
   return (
     <WindowSizeContext.Provider value={value}>
