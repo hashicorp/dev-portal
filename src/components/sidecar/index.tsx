e936ca53b17bab1d2e36f22426502e5c82f34e8e
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2021-12-03 14:36:19 -0500

    Starting to use IntersectionObserver

--- src/components/sidecar/index.tsx
+++ src/components/sidecar/index.tsx
@@ -1,3 +1,5 @@
+import { useLayoutEffect, useState } from 'react'
+import classNames from 'classnames'
 import { useDeviceSize } from 'contexts'
 import s from './style.module.css'
 
@@ -13,7 +15,41 @@ interface SidecarProps {
 // TODO: there are still a few things to do here. See https://app.asana.com/0/0/1201265683986463/f.
 const Sidecar: React.FC<SidecarProps> = ({ headings }) => {
   const { isMobile, isTablet } = useDeviceSize()
-  console.log('mobile?', isMobile, 'tablet?', isTablet)
+  const [activeSectionSlug, setActiveSectionSlug] = useState<string>()
+
+  // WIP: still modeling off of https://github.com/hashicorp/react-components/pull/325/files#diff-fb77bef81abf88c4e046f0ab25dfb955d3138b579b67143bad7124d5bcb04f6d
+  useLayoutEffect(() => {
+    if (isMobile || isTablet) {
+      return
+    }
+
+    const handleIntersection = (entries) => {
+      const visibleHeadingIds = []
+
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          visibleHeadingIds.push(entry.target.id)
+        }
+      })
+    }
+
+    const observer = new IntersectionObserver(handleIntersection)
+    headings.forEach((heading) => {
+      const headingLink = document.querySelector(`#${heading.slug}`)
+      if (headingLink) {
+        observer.observe(headingLink)
+      }
+    })
+
+    return () => {
+      headings.forEach((heading) => {
+        const headingLink = document.querySelector(`#${heading.slug}`)
+        if (headingLink) {
+          observer.unobserve(headingLink)
+        }
+      })
+    }
+  }, [headings, isMobile, isTablet])
 
   return (
     <aside className={`${s.sidecar} hide-on-mobile hide-on-tablet`}>
@@ -22,11 +58,20 @@ const Sidecar: React.FC<SidecarProps> = ({ headings }) => {
       </p>
       <nav aria-labelledby="table-of-contents">
         <ol className={s.sidecarList}>
-          {headings.map(({ slug, title }) => (
-            <li className={s.sidecarListItem} key={slug}>
-              <a href={`#${slug}`}>{title}</a>
-            </li>
-          ))}
+          {headings.map(({ slug, title }) => {
+            const isActive = slug === activeSectionSlug
+            return (
+              <li
+                className={classNames(s.sidecarListItem, {
+                  [s['sidecarListItem-active']]: isActive,
+                })}
+                key={slug}
+              >
+                <a href={`#${slug}`}>{title}</a>
+                {isActive && <span />}
+              </li>
+            )
+          })}
         </ol>
       </nav>
     </aside>
