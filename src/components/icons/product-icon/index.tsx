2f2b1e94ccac058c043fe4156388d75d1d8c4fea
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2021-12-09 18:29:56 -0500

    Scaffolding ProductSwitcher with logos and links

--- src/components/icons/product-icon/index.tsx
+++ src/components/icons/product-icon/index.tsx
@@ -1,12 +1,24 @@
 import InlineSvg from '@hashicorp/react-inline-svg'
+import { ProductCode } from 'common/types'
 import s from './style.module.css'
 
+// TODO: is there a programatic way to build this from productNamesToIcons?
 interface ProductIconProps {
-  product: 'vault'
+  product: ProductCode
 }
 
-const productNamesToIcons = {
-  vault: require('@hashicorp/mktg-logos/product/vault/logomark/white.svg?include'),
+// TODO: what logos for hcp & sentinel?
+const productNamesToIcons: { [key in ProductCode]: any } = {
+  boundary: require('@hashicorp/mktg-logos/product/boundary/logomark/color.svg?include'),
+  consul: require('@hashicorp/mktg-logos/product/consul/logomark/color.svg?include'),
+  hcp: require('@hashicorp/mktg-logos/corporate/hashicorp/logomark/black.svg?include'),
+  nomad: require('@hashicorp/mktg-logos/product/nomad/logomark/color.svg?include'),
+  packer: require('@hashicorp/mktg-logos/product/packer/logomark/color.svg?include'),
+  sentinel: require('@hashicorp/mktg-logos/corporate/hashicorp/logomark/black.svg?include'),
+  terraform: require('@hashicorp/mktg-logos/product/terraform/logomark/color.svg?include'),
+  vagrant: require('@hashicorp/mktg-logos/product/vagrant/logomark/color.svg?include'),
+  vault: require('@hashicorp/mktg-logos/product/vault/logomark/color.svg?include'),
+  waypoint: require('@hashicorp/mktg-logos/product/waypoint/logomark/color.svg?include'),
 }
 
 const ProductIcon: React.FC<ProductIconProps> = ({ product }) => (
--- src/components/product-switcher/index.tsx
+++ src/components/product-switcher/index.tsx
@@ -1,16 +1,94 @@
+import { useState } from 'react'
 import { IconCaret16 } from '@hashicorp/flight-icons/svg-react/caret-16'
+import { ProductCode } from 'common/types'
 import ProductIcon from 'components/icons/product-icon'
 import s from './style.module.css'
 
+const products: { name: string; code: ProductCode; url: string }[] = [
+  {
+    name: 'Terraform',
+    code: 'terraform',
+    url: 'https://www.terraform.io/docs/index.html',
+  },
+  {
+    name: 'Packer',
+    code: 'packer',
+    url: 'https://www.packer.io/docs',
+  },
+  {
+    name: 'Consul',
+    code: 'consul',
+    url: 'https://www.consul.io/docs',
+  },
+  {
+    name: 'Vault',
+    code: 'vault',
+    url: 'https://www.vaultproject.io/docs',
+  },
+  {
+    name: 'Boundary',
+    code: 'boundary',
+    url: 'https://www.boundaryproject.io/docs',
+  },
+  {
+    name: 'Nomad',
+    code: 'nomad',
+    url: 'https://www.nomadproject.io/docs',
+  },
+  {
+    name: 'Waypoint',
+    code: 'waypoint',
+    url: 'https://www.waypointproject.io/docs',
+  },
+  {
+    name: 'Vagrant',
+    code: 'vagrant',
+    url: 'https://www.vagrantup.com/docs',
+  },
+  {
+    name: 'Sentinel',
+    code: 'sentinel',
+    url: 'https://docs.hashicorp.com/sentinel',
+  },
+  {
+    name: 'HashiCorp Cloud Platform',
+    code: 'hcp',
+    url: 'https://cloud.hashicorp.com/docs/hcp',
+  },
+]
+
 // TODO: make this functional (ref: https://app.asana.com/0/1201010428539925/1201247589988629/f)
-const ProductSwitcher: React.FC = () => (
-  <div className={s.productSwitcher}>
-    <div className={s.iconAndNameContainer}>
-      <ProductIcon product="vault" />
-      <p>Vault</p>
+const ProductSwitcher: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <div className={s.productSwitcher}>
+      <button
+        onBlur={(e) => console.log('blur event', e)}
+        onClick={() => {
+          setIsOpen(!isOpen)
+        }}
+      >
+        <div className={s.iconAndNameContainer}>
+          <ProductIcon product="waypoint" />
+          <span>Waypoint</span>
+        </div>
+        <IconCaret16 />
+      </button>
+      {isOpen && (
+        <ul>
+          {products.map((product) => (
+            <li key={product.code}>
+              <a href={product.url}>
+                <ProductIcon product={product.code} />
+                <span>{product.name}</span>
+              </a>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
-    <IconCaret16 />
-  </div>
-)
+  )
+}
 
 export default ProductSwitcher
