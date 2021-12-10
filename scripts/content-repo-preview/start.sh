9b5561950869d9c4d3250d81a3d04cd4603db854
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-12-10 13:52:13 -0500

    fix: disable versioned docs for local preview

--- scripts/content-repo-preview/start.sh
+++ scripts/content-repo-preview/start.sh
@@ -7,4 +7,4 @@ fi
 cp -R ../public/** ./public/
 # REPO is set in each product repository's
 # call to this start script
-DEV_IO="$REPO" IS_CONTENT_PREVIEW=true ./node_modules/.bin/next
+DEV_IO="$REPO" IS_CONTENT_PREVIEW=true ENABLE_VERSIONED_DOCS=false ./node_modules/.bin/next
