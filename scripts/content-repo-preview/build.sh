d5f584cf061e904e946f941b978dd021f7de6a2a
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-12-10 13:04:44 -0500

    feat: refine content-repo build and preview scripts

--- scripts/content-repo-preview/build.sh
+++ scripts/content-repo-preview/build.sh
@@ -1,1 +1,40 @@
-echo "Hello build script!"
+npm i --production=false
+# Delete other products' docs pages,
+# these will just increase build times
+# Make a temporary directory for global CSS files
+mkdir ./temp-global-page-css
+# Boundary
+if [ "$REPO" != "boundary" ]; then
+    # Preserve page CSS files, otherwise global.css imports will break build
+    mv ./src/pages/_proxied-dot-io/boundary/home/style.css ./temp-global-page-css/boundary-home.css
+    # Delete all boundary dot-io pages
+    rm -rf ./src/pages/_proxied-dot-io/boundary
+    # Put the page CSS files back
+    mkdir ./src/pages/_proxied-dot-io/boundary
+    mkdir ./src/pages/_proxied-dot-io/boundary/home
+    mv ./temp-global-page-css/boundary-home.css ./src/pages/_proxied-dot-io/boundary/home/style.css
+fi
+# Sentinel
+if [ "$REPO" != "sentinel" ]; then
+    # Delete all sentinel dot-io pages
+    rm -rf ./src/pages/_proxied-dot-io/sentinel
+fi
+# Waypoint
+if [ "$REPO" != "waypoint" ]; then
+    # Delete all waypoint dot-io pages
+    rm -rf ./src/pages/_proxied-dot-io/waypoint
+fi
+# Build the site.
+# Note that DEV_IO and IS_CONTENT_PREVIEW are set
+# in Vercel configuration for the project.
+# (Those env vars are used both for next build,
+# and in error pages at request time, so Vercel
+# configuration seemed like the logical place)
+npm run build
+# Copy .next build output folder into project root,
+# so that Vercel's NextJS preset picks up on the build output
+echo "Listing files after build..."
+ls -a
+echo "Copying .next output to project root..."
+cp -R .next/ ../.next
+echo "Done."
--- scripts/content-repo-preview/start.sh
+++ scripts/content-repo-preview/start.sh
@@ -1,1 +1,12 @@
-echo "Hello start script!"
+# Install dependencies
+# TODO maybe some way to optimize this?
+# TODO or just remove it if node_modules is present?
+# TODO npm install seems unecessary on most runs...
+# TODO but it is useful during dev.
+npm i --production=false
+# TODO set up watcher to sync all files
+# TODO under website/public into website/website-preview/public
+cp -R ../public/img/** ./public/img/
+# REPO is set in each product repository's
+# call to this start script
+DEV_IO="$REPO" IS_CONTENT_PREVIEW=true ./node_modules/.bin/next
