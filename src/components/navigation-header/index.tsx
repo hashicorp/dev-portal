1eadd878f6a8e021d758de0f19098a3b11581ae9
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2021-12-13 19:00:19 -0500

    Adding initial release nav links

--- src/components/navigation-header/index.tsx
+++ src/components/navigation-header/index.tsx
@@ -1,8 +1,36 @@
+import Link from 'next/link'
 import HashiCorpLogo from '@hashicorp/mktg-logos/corporate/hashicorp/logomark/white.svg?include'
-import HeaderSearchInput from 'components/header-search-input'
 import InlineSvg from '@hashicorp/react-inline-svg'
+import HeaderSearchInput from 'components/header-search-input'
 import ProductChooser from 'components/product-chooser'
+// TODO: do we want to add a path alias for config? It currently lives above baseUrl (src)
+import { navigationLinks } from '../../../config/main-navigation'
 import s from './style.module.css'
+
+/**
+ * This is a temporary component. Once all of the nav links point to internal paths,
+ * it won't be needed anymore. Not planning to move into a separate file.
+ */
+const NavLink: React.FC<{
+  ariaCurrent: 'page' | undefined
+  href: string
+  label: string
+  path: string
+}> = ({ ariaCurrent, href, label, path }) => {
+  if (path) {
+    return (
+      <Link href={path}>
+        <a aria-current={ariaCurrent}>{label}</a>
+      </Link>
+    )
+  }
+
+  return (
+    <a aria-current={ariaCurrent} href={href}>
+      {label}
+    </a>
+  )
+}
 
 const NavigationHeader: React.FC = () => (
   <header className={s.navigationHeader}>
@@ -12,20 +40,18 @@ const NavigationHeader: React.FC = () => (
         <ProductChooser />
       </div>
       <div className={s.headerRight}>
-        {/* TODO: make these links functional (ref: https://app.asana.com/0/1201010428539925/1201247589988627/f) */}
         <ul className={s.navLinks}>
-          <li>
-            <a>Reference Docs</a>
-          </li>
-          <li>
-            <a>CLI</a>
-          </li>
-          <li>
-            <a>API</a>
-          </li>
-          <li>
-            <a>Downloads</a>
-          </li>
+          {navigationLinks.map((navLink, index) => (
+            <li key={navLink.id}>
+              {/* TODO: we'll use the router to determine the current link once they're all internal */}
+              <NavLink
+                ariaCurrent={index === 0 ? 'page' : undefined}
+                href={navLink.href}
+                label={navLink.label}
+                path={navLink.path}
+              />
+            </li>
+          ))}
         </ul>
         <HeaderSearchInput />
       </div>
