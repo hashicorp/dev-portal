5724607da8ee0422ce570fda0bc8168f1b4b583e
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2021-12-17 17:25:29 -0500

    Replacing downloads page with placeholder and nav header

--- src/pages/waypoint/downloads/index.jsx
+++ src/pages/waypoint/downloads/index.jsx
@@ -1,60 +1,19 @@
-import waypointConfig from '../../../../config/waypoint.json'
-import ProductDownloadsPage from '@hashicorp/react-product-downloads-page'
-import WaypointIoLayout from 'layouts/proxied-io-sites/waypoint'
-import styles from './style.module.css'
-// Imports below are server-side only
-import { generateStaticProps } from '@hashicorp/react-product-downloads-page/server'
+import NavigationHeader from 'components/navigation-header'
 
-function DownloadsPage({ releases }) {
+const WaypointDowloadsPage = () => {
   return (
-    <ProductDownloadsPage
-      releases={releases}
-      packageManagers={waypointConfig.packageManagers}
-      productName={waypointConfig.name}
-      productId={waypointConfig.slug}
-      latestVersion={waypointConfig.version} // temporary. switch to remote fetch
-      getStartedDescription="Follow step-by-step tutorials on AWS, Azure, GCP, and localhost."
-      getStartedLinks={[
-        {
-          label: 'Deploy to Docker',
-          href:
-            'https://learn.hashicorp.com/collections/waypoint/get-started-docker',
-        },
-        {
-          label: 'Deploy to Kubernetes',
-          href:
-            'https://learn.hashicorp.com/collections/waypoint/get-started-kubernetes',
-        },
-        {
-          label: 'Deploy to AWS',
-          href: 'https://learn.hashicorp.com/collections/waypoint/deploy-aws',
-        },
-        {
-          label: 'View all Waypoint tutorials',
-          href: 'https://learn.hashicorp.com/waypoint',
-        },
-      ]}
-      logo={
-        <img
-          className={styles.logo}
-          alt="Waypoint"
-          src={require('./img/waypoint-logo.svg')}
-        />
-      }
-      product={waypointConfig.slug}
-      tutorialLink={{
-        href: 'https://learn.hashicorp.com/waypoint',
-        label: 'View Tutorials at HashiCorp Learn',
-      }}
-    />
+    <>
+      <NavigationHeader />
+      <div className="g-grid-container">
+        <h1>Waypoint Downloads</h1>
+        <ul>
+          <li>This page is a work in progress</li>
+        </ul>
+      </div>
+    </>
   )
 }
 
-export const getStaticProps = () =>
-  generateStaticProps({
-    product: waypointConfig.slug,
-    latestVersion: waypointConfig.version, // temporary. switch to remote fetch
-  })
+// WaypointDowloadsPage.layout = WaypointIoLayout
 
-DownloadsPage.layout = WaypointIoLayout
-export default DownloadsPage
+export default WaypointDowloadsPage
