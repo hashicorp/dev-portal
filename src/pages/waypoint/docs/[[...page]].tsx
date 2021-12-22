fb4fa5a7e717b460cff602feec016f1ea8cfb81b
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2021-12-22 13:08:00 -0500

    Using docs page in waypoint/docs

--- src/pages/waypoint/docs/[[...page]].tsx
+++ src/pages/waypoint/docs/[[...page]].tsx
@@ -1,15 +1,11 @@
-// import DocsPage from '@hashicorp/react-docs-page'
 import {
   generateStaticPaths,
   generateStaticProps,
 } from '@hashicorp/react-docs-page/server'
-import defaultMdxComponents from '@hashicorp/platform-docs-mdx'
 import { anchorLinks } from '@hashicorp/remark-plugins'
-import { MDXRemote } from 'next-mdx-remote'
 import waypointConfig from '../../../../config/waypoint.json'
-import Placement from 'components/author-primitives/shared/placement-table'
-import NestedNode from 'components/author-primitives/waypoint/nested-node'
 import DocsLayout from 'layouts/docs'
+import DocsPage from 'components/docs-page'
 
 // because some of the util functions still require param arity, but we ignore
 // their values when process.env.ENABLE_VERSIONED_DOCS is set to true, we'll
@@ -19,22 +15,16 @@ const temporary_noop = 'im just for show'
 const productName = waypointConfig.name
 const productSlug = waypointConfig.slug
 const basePath = 'docs'
-const additionalComponents = defaultMdxComponents('waypoint', {
-  Placement,
-  NestedNode,
-})
 
-// TODO: inline styles will be removed in a follow-up layout task (ref: https://app.asana.com/0/0/1201217826547576/f)
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 function WaypointDocsPage(props) {
-  return <MDXRemote {...props.mdxSource} components={additionalComponents} />
+  return <DocsPage {...props.mdxSource} />
 }
 
 export async function getStaticPaths() {
   const paths = await generateStaticPaths({
     navDataFile: temporary_noop,
     localContentDir: temporary_noop,
-    // new ----
     product: { name: productName, slug: productSlug },
     basePath,
   })
@@ -68,7 +58,6 @@ export async function getStaticProps({ params }) {
   }
 }
 
-// Needs to be DocsLayout in the assembly-ui-v1 branch for now
 WaypointDocsPage.layout = DocsLayout
 
 export default WaypointDocsPage
