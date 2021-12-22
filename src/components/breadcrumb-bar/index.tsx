98e292fdfe0be8483814e033f6ae04604ce58a3e
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-12-22 10:19:19 -0500

    feat: ensure relative URLs in BreadcrumbBar

--- src/components/breadcrumb-bar/index.tsx
+++ src/components/breadcrumb-bar/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Link from 'next/link'
 import s from './style.module.css'
+import isAbsoluteUrl from './utils/is-absolute-url'
 
 interface BreadcrumbLink {
   /** Text to be shown for the link */
   title: string
-  /** The URL to link to. May be omitted for index-less routes. */
+  /** The URL to link to. Must be internal, ie must not be an absolute URL. May be omitted for index-less routes. */
   url?: string
   /** Whether this breadcrumb represents the current page */
   isCurrentPage?: boolean
@@ -16,6 +17,22 @@ function BreadcrumbBar({
 }: {
   links: BreadcrumbLink[]
 }): React.ReactElement {
+  // For now, we want to strictly require that all
+  // breadcrumb link URLs, if present, are relative rather
+  // than absolute links
+  links
+    .filter((l) => Boolean(l.url))
+    .forEach((l) => {
+      if (isAbsoluteUrl(l.url)) {
+        throw new Error(
+          `Absolute URL passed to BreadcrumbBar: "${JSON.stringify(
+            l
+          )}". Please ensure all "link.url" values are relative links.`
+        )
+      }
+    })
+  // Now that we're sure all links are relative,
+  // we can render the breadcrumb links
   return (
     <nav aria-label="Breadcrumb" className={s.root}>
       <ol className={s.listRoot}>
