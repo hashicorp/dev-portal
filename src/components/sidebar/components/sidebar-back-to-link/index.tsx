12d7b3b80add9f760cc7d5cea8202b5701493b01
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2021-12-22 17:31:55 -0500

    Making SidebarBackToLink functional

--- src/components/sidebar/components/sidebar-back-to-link/index.tsx
+++ src/components/sidebar/components/sidebar-back-to-link/index.tsx
@@ -1,12 +1,36 @@
+import Link from 'next/link'
 import { IconArrowLeft16 } from '@hashicorp/flight-icons/svg-react/arrow-left-16'
+import useCurrentPath from 'hooks/use-current-path'
 import s from './style.module.css'
 
-// TODO: design is still planning this functionality & states
-const SidebarBackToLink: React.FC = () => (
-  <a className={s.backToLink}>
-    <IconArrowLeft16 className={s.icon} />
-    <span>Back to lorem ipsum</span>
-  </a>
-)
+const subpagesToTitles = {
+  docs: 'Reference Docs',
+  commands: 'CLI',
+  plugins: 'Plugins',
+}
+
+const SidebarBackToLink: React.FC = () => {
+  const currentPath = useCurrentPath({ excludeHash: true, excludeSearch: true })
+  const currentPathSplit = currentPath.split('/')
+  const currentProductSlug = currentPathSplit[1]
+  const currentProductSubpage = currentPathSplit[2]
+  const backToPath = `/${currentProductSlug}/${currentProductSubpage}`
+
+  console.log(backToPath === currentPath)
+
+  return (
+    <Link href={backToPath}>
+      <a
+        className={s.backToLink}
+        style={{
+          visibility: backToPath === currentPath ? 'hidden' : 'visible',
+        }}
+      >
+        <IconArrowLeft16 className={s.icon} />
+        <span>Back to {subpagesToTitles[currentProductSubpage]}</span>
+      </a>
+    </Link>
+  )
+}
 
 export default SidebarBackToLink
