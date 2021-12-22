ea9b69df042de72227d90a1b3f56da80afc3ad68
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2021-12-21 20:13:24 -0500

    Adding base new layout to fix double scrollbar

--- src/pages/waypoint/index.tsx
+++ src/pages/waypoint/index.tsx
@@ -1,16 +1,15 @@
-import NavigationHeader from 'components/navigation-header'
+import { ReactElement } from 'react'
+import BaseNewLayout from 'layouts/base-new'
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export default function Homepage() {
-  return (
-    <>
-      <NavigationHeader />
-      <div className="g-grid-container">
-        <h1>Welcome to Waypoint</h1>
-        <ul>
-          <li>This page is a work in progress</li>
-        </ul>
-      </div>
-    </>
-  )
-}
+const WaypointHomePage = (): ReactElement => (
+  <div className="g-grid-container">
+    <h1>Welcome to Waypoint</h1>
+    <ul>
+      <li>This page is a work in progress</li>
+    </ul>
+  </div>
+)
+
+WaypointHomePage.layout = BaseNewLayout
+
+export default WaypointHomePage
