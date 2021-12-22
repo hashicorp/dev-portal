0945d067707a9b7d56c7411fe1429fe64d741ea2
Bryce Kalow <bkalow@hashicorp.com>
2021-12-22 14:58:02 -0600

    copy .next folder, then mv deps for speed

--- scripts/content-repo-preview/build.ts
+++ scripts/content-repo-preview/build.ts
@@ -84,13 +84,13 @@ async function main() {
   /** Build */
   execFileSync('npm', ['run', 'build'], { stdio: 'inherit' })
 
+  // Move the .next folder to the top-level so the site can be deployed and we can take advantage of the cache
+  await execFile('cp', ['-R', '.next', '../.next'])
+
   // Put node_modules into .next/cache so we can retrieve them on subsequent builds
-  execFileSync('mv', ['node_modules', '.next/cache/node_modules'], {
+  execFileSync('mv', ['node_modules', '../.next/cache/node_modules'], {
     stdio: 'inherit',
   })
-
-  // Move the .next folder to the top-level so the site can be deployed and we can take advantage of the cache
-  await execFile('mv', ['.next', '../.next'])
 }
 
 main()
