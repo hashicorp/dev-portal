3970c6c04f9bd0dc28974fab25082b0291c7d368
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2021-12-23 17:36:16 -0500

    Fixing defaultMdxComponents invocation

--- src/components/docs-page/index.tsx
+++ src/components/docs-page/index.tsx
@@ -6,19 +6,22 @@ import NestedNode from 'components/author-primitives/waypoint/nested-node'
 import makeDocsAnchor from 'components/docs-anchor'
 
 /**
- * TODO: this will eventually be configurable by all products, not just waypoint
- */
-const additionalComponents = defaultMdxComponents('waypoint', {
-  Placement,
-  NestedNode,
-  a: makeDocsAnchor('waypoint', ['docs', 'plugins', 'commands']),
-})
-
-/**
  * TODO: we may find that we want to allow additional components to be passed in
  * as a prop in the future
  */
 const DocsPage = (props): ReactElement => {
+  /**
+   * TODO: this will eventually be configurable by all products, not just waypoint
+   */
+  const additionalComponents = defaultMdxComponents({
+    product: 'waypoint',
+    additionalComponents: {
+      Placement,
+      NestedNode,
+      a: makeDocsAnchor('waypoint', ['docs', 'plugins', 'commands']),
+    },
+  })
+
   return <MDXRemote {...props} components={additionalComponents} />
 }
 
