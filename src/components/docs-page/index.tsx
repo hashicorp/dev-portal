2e3fce26028d06deae1e8c990357bec28425e453
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2021-12-23 13:59:32 -0500

    Adding TODO comments

--- src/components/docs-page/index.tsx
+++ src/components/docs-page/index.tsx
@@ -4,11 +4,18 @@ import defaultMdxComponents from '@hashicorp/platform-docs-mdx'
 import Placement from 'components/author-primitives/shared/placement-table'
 import NestedNode from 'components/author-primitives/waypoint/nested-node'
 
+/**
+ * TODO: this will eventually be configurable by all products, not just waypoint
+ */
 const additionalComponents = defaultMdxComponents('waypoint', {
   Placement,
   NestedNode,
 })
 
+/**
+ * TODO: we may find that we want to allow additional components to be passed in
+ * as a prop in the future
+ */
 const DocsPage = (props): ReactElement => {
   return <MDXRemote {...props} components={additionalComponents} />
 }
