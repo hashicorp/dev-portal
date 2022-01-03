34a0d92b58b5d46d0c91a8e48c071af5c8c32e74
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-01-03 14:09:26 -0500

    chore: revert changes to back-to-link, too opinionated

--- src/components/sidebar/components/sidebar-back-to-link/index.tsx
+++ src/components/sidebar/components/sidebar-back-to-link/index.tsx
@@ -1,15 +1,12 @@
 import { IconArrowLeft16 } from '@hashicorp/flight-icons/svg-react/arrow-left-16'
-import Link from 'next/link'
 import s from './style.module.css'
 
 // TODO: design is still planning this functionality & states
 const SidebarBackToLink: React.FC = () => (
-  <Link href="/">
-    <a className={s.backToLink}>
-      <IconArrowLeft16 className={s.icon} />
-      <span>Home</span>
-    </a>
-  </Link>
+  <a className={s.backToLink}>
+    <IconArrowLeft16 className={s.icon} />
+    <span>Back to lorem ipsum</span>
+  </a>
 )
 
 export default SidebarBackToLink
