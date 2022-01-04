b0ac529d1e8c5d3e5a385cbd1b13d919fcbed5ac
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-01-04 12:41:27 -0500

    style: add whitespace in waypoint home view page file
    Co-authored-by: Ashlee M Boyer <43934258+ashleemboyer@users.noreply.github.com>

--- src/pages/waypoint/index.tsx
+++ src/pages/waypoint/index.tsx
@@ -1,7 +1,9 @@
 import HomePage from 'views/waypoint/home'
 import server from 'views/waypoint/home/server'
+
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export async function getStaticProps() {
   return await server.getStaticProps()
 }
+
 export default HomePage
