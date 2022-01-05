4bceb9666d9f54627711c1415abb5e583e20a4ea
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-01-05 18:22:17 -0500

    Adding page title to solve pa11y-ci errors

--- src/layouts/base-new/index.tsx
+++ src/layouts/base-new/index.tsx
@@ -1,11 +1,18 @@
+import Head from 'next/head'
 import AlertBanner from 'components/alert-banner'
 import NavigationHeader from 'components/navigation-header'
-import Footer from 'components/footer'
 import s from './base-new-layout.module.css'
 
+/**
+ * TODO: page title needs to be dynamic, but this solves some pa11y-ci errors
+ * for the time being.
+ */
 const BaseNewLayout: React.FC = ({ children }) => {
   return (
     <div className={s.baseNewLayout}>
+      <Head>
+        <title>DevDot</title>
+      </Head>
       <AlertBanner type="highlight">
         <p>
           You are viewing an internal preview and work in progress version of
