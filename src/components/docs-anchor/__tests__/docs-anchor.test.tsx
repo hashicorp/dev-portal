bca6c7b4b0431debbd5cea573faf4259684e5446
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-01-07 17:14:50 -0500

    Updating DocsAnchor test for new context

--- src/components/docs-anchor/__tests__/docs-anchor.test.tsx
+++ src/components/docs-anchor/__tests__/docs-anchor.test.tsx
@@ -1,11 +1,18 @@
 import { render, screen } from '@testing-library/react'
-import makeDocsAnchor from '..'
+import { CurrentProductProvider } from 'contexts'
+import waypointData from 'data/waypoint.json'
+import { Product } from 'types/products'
+import DocsAnchor from '../'
+
+const product = waypointData as Product
 
 describe('DocsAnchor', () => {
   test('rewrites internal links based on props to the factory function', async () => {
-    const DocsAnchor = makeDocsAnchor('waypoint', ['docs'])
-
-    render(<DocsAnchor href="/docs/some/page">This is a link</DocsAnchor>)
+    render(
+      <CurrentProductProvider currentProduct={product}>
+        <DocsAnchor href="/docs/some/page">This is a link</DocsAnchor>
+      </CurrentProductProvider>
+    )
 
     const result = screen.queryByText('This is a link')
 
