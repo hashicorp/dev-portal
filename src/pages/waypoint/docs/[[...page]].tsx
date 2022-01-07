307ee0fa8c3d4d3d66d38677bb7065d377ad4054
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-01-07 16:31:47 -0500

    Using waypoint.json data and updating TS

--- src/pages/waypoint/docs/[[...page]].tsx
+++ src/pages/waypoint/docs/[[...page]].tsx
@@ -1,4 +1,6 @@
-import waypointConfig from '../../../../config/waypoint.json'
+import { ReactElement } from 'react'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import waypointData from 'data/waypoint.json'
 import { Product, ProductSlug } from 'types/products'
 import { generateStaticPaths, generateStaticProps } from 'layouts/docs/server'
 import DocsLayout from 'layouts/docs'
@@ -6,29 +8,30 @@ import DocsPage from 'components/docs-page'
 
 const basePath = 'docs'
 const product: Product = {
-  basePaths: waypointConfig.basePaths,
-  name: waypointConfig.name,
-  slug: waypointConfig.slug as ProductSlug,
+  basePaths: waypointData.basePaths,
+  name: waypointData.name,
+  slug: waypointData.slug as ProductSlug,
 }
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-function DocsView({ mdxSource }) {
+const WaypointDocsPage = ({ mdxSource }): ReactElement => {
   return <DocsPage {...mdxSource} />
 }
 
-export async function getStaticPaths() {
+export const getStaticPaths: GetStaticPaths = async () => {
   return {
     paths: await generateStaticPaths({ basePath, product }),
     fallback: 'blocking',
   }
 }
 
-export async function getStaticProps({ params }) {
+export const getStaticProps: GetStaticProps = async ({ params }) => {
   return {
     props: await generateStaticProps({ basePath, product, params }),
     revalidate: 10,
   }
 }
 
-DocsView.layout = DocsLayout
-export default DocsView
+WaypointDocsPage.layout = DocsLayout
+
+export default WaypointDocsPage
