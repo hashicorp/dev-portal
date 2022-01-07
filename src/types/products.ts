f3cde2444d8fdc7699f0adc5d14ca34b80dc3c7e
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-01-07 14:57:49 -0500

    Making basePaths optional in Product interface

--- src/types/products.ts
+++ src/types/products.ts
@@ -2,8 +2,11 @@ import { ProductMeta, Products } from '@hashicorp/platform-product-meta'
 
 type ProductSlug = Exclude<Products, 'hashicorp'> | 'hcp' | 'sentinel'
 
+/**
+ * TODO: put basePaths in a separate interface that extends the Product one?
+ */
 interface Product extends ProductMeta {
-  basePaths: string[]
+  basePaths?: string[]
   slug: ProductSlug
   url?: string
 }
