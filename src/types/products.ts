f42f16dcc3624af1e7bffbc7e96beede2ab734af
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-01-07 13:10:40 -0500

    Adding basePaths property to Product interface

--- src/types/products.ts
+++ src/types/products.ts
@@ -3,6 +3,7 @@ import { ProductMeta, Products } from '@hashicorp/platform-product-meta'
 type ProductSlug = Exclude<Products, 'hashicorp'> | 'hcp' | 'sentinel'
 
 interface Product extends ProductMeta {
+  basePaths: string[]
   slug: ProductSlug
   url?: string
 }
