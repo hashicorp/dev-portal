e6a320beee7a1cc9734ca22655ec163addd81e48
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-01-11 10:52:44 -0500

    docs: add clamp comment on min > max error

--- src/lib/clamp.ts
+++ src/lib/clamp.ts
@@ -3,6 +3,10 @@
  * Return the minimum value if the number is less than the minimum,
  * or the maximum value if the number is greater than the maximum,
  * or the number itself if it is greater than the minimum or less than the maximum.
+ *
+ * Note that if the minimum value provided is greater
+ * than the maximum value provided, this function
+ * will throw an error.
  */
 export default function clamp(
   /** The number to clamp. */
