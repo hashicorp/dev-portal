93dd369f9f6469d4e5e075f8d05caef65f646f47
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-01-12 17:26:08 -0500

    fix: fix up TSFixMes in sidebar-sidecar

--- src/layouts/sidebar-sidecar/index.tsx
+++ src/layouts/sidebar-sidecar/index.tsx
@@ -1,10 +1,15 @@
 import s from './style.module.css'
 
 interface SidebarSidecarLayoutProps {
-  header: $TSFixMe
-  sidebar: $TSFixMe
-  sidecar?: $TSFixMe
-  footer?: $TSFixMe
+  /** Header slot. Wrapped in a position: sticky container, elements in this area will always be visible. */
+  header: React.ReactNode
+  /** Sidebar slot, displayed in an elevated area at the left of the viewport. If the content in the sidebar is very tall, it will overflow. */
+  sidebar: React.ReactNode
+  /** Optional sidecar slot, displayed closely alongside the centered content area. If the content in the sidecar is very tall, it will extend the height of the page. */
+  sidecar?: React.ReactNode
+  /** Optional footer slot, to display below the sidebar + content + sidecar area. If the page content is shorter than 100vh, the footer will still be displayed at the bottom of the viewport.
+   * Note: Not used in current designs, though I think there's a possibility it will be used in the future, so wanted to ensure this implementation can accommodate it. */
+  footer?: React.ReactNode
 }
 
 const SidebarSidecarLayout: React.FC<SidebarSidecarLayoutProps> = ({
