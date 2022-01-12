e654eca27a9a51bfad85c4966fdd5a5424664646
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-01-12 17:18:07 -0500

    feat: fix up BaseNewLayout to demo sticky header, min100 layout

--- src/layouts/base-new/index.tsx
+++ src/layouts/base-new/index.tsx
@@ -1,17 +1,19 @@
+import Min100Layout from '@hashicorp/react-min-100-layout'
+import Footer from 'components/footer'
 import DevAlertBanner from 'components/dev-alert-banner'
 import NavigationHeader from 'components/navigation-header'
 import s from './base-new-layout.module.css'
 
 const BaseNewLayout: React.FC = ({ children }) => {
   return (
-    <>
+    <Min100Layout footer={<Footer />}>
       <div className={s.header}>
         <DevAlertBanner />
         <NavigationHeader />
       </div>
 
       {children}
-    </>
+    </Min100Layout>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,19 +2,15 @@ import { ReactElement } from 'react'
 import BaseNewLayout from 'layouts/base-new'
 import ProductCardGrid from 'components/product-card-grid'
 import HomepageHero from 'components/homepage-hero'
-import Footer from 'components/footer'
 import s from './index.module.css'
 
 function Homepage(): ReactElement {
   return (
-    <>
-      <div className={s.root}>
-        <HomepageHero className={s.hero} />
-        <h2 className={s.cardGridHeading}>Explore Product Documentation</h2>
-        <ProductCardGrid className={s.cardGrid} />
-      </div>
-      <Footer className={s.footer} />
-    </>
+    <div className={s.root}>
+      <HomepageHero className={s.hero} />
+      <h2 className={s.cardGridHeading}>Explore Product Documentation</h2>
+      <ProductCardGrid className={s.cardGrid} />
+    </div>
   )
 }
 
