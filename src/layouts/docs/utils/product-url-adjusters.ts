15357e194752c74824a23d15d6f6b2efcde5ee85
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-01-13 20:21:41 -0500

    style: use block rather than inline comments in product-url-adjusters
    Co-authored-by: Ashlee M Boyer <43934258+ashleemboyer@users.noreply.github.com>

--- src/layouts/docs/utils/product-url-adjusters.ts
+++ src/layouts/docs/utils/product-url-adjusters.ts
@@ -1,21 +1,24 @@
 import { Pluggable } from 'unified'
 import remarkPluginAdjustLinkUrls from 'lib/remark-plugin-adjust-link-urls'
 
+/**
+ * We must fix /api URLs, which should really be /api-docs. This would ideally
+ * be done in the source MDX. However, we need to support all past versions, so
+ * even if we corrected the latest source, we'd likely still want to run this on
+ * older content.
+ *
+ * Returns the modified URL.
+ *
+ * Note that further modification happens client-side, via our DocsAnchor custom
+ * MDX component.
+ */
 export const vaultUrlAdjuster: Pluggable = [
   remarkPluginAdjustLinkUrls,
   {
     urlAdjustFn: (sourceUrl: string): string => {
       let url = sourceUrl.slice()
-      // We must fix /api URLs, which should really be /api-docs.
-      // This would ideally be done in the source MDX.
-      // However, we need to support all past versions,
-      // so even if we corrected the latest source, we'd
-      // likely still want to run this on older content.
       const isBadApiUrl = url == '/api' || url.startsWith('/api/')
       if (isBadApiUrl) url = url.replace(/^\/api/, '/api-docs')
-      // Return the modified URL
-      // (Note that further modification happens client-side,
-      // via our DocsAnchor custom MDX component)
       return url
     },
   },
