d1328b9502da36e55688cfbefb22a20a7e7916ed
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-01-14 09:50:00 -0500

    feat: add vault docs views (#73)
    * feat: add in vault docs views
    
    * feat: add in columns and inline-tag components
    
    * feat: enable Vault custom MDX components
    
    * docs: add asana task comment
    
    * fix: use new DocsAnchor, already has context
    
    * fix: add missing vault basePaths data
    
    * refactor: add back docs-view, localize defaultMdxComponents
    
    * fix: fix baseName issue, add rewrites & redirects
    
    * fix: add back vault header nav items
    
    * refactor: use remark plugin to fix /api URLs, not rewrites
    
    * chore: revert unnecessary changes to next config
    
    * chore: remove duplicate vault landing nav data
    
    * style: use arrow functions for consistency
    
    * style: rm unused default export, export on one line
    
    * chore: clean up dead code in platform-docs-mdx
    
    * style: move remark plugin to lib for discoverability
    
    * chore: use Plugin, Pluggable types from unified
    
    * fix: unified types for remark-plugin
    
    * nit: name DocsView properly
    
    Co-authored-by: Bryce Kalow <bkalow@hashicorp.com>
    
    * fix: useCurrentProduct for switcher on vault/downloads
    
    Co-authored-by: Ashlee M Boyer <43934258+ashleemboyer@users.noreply.github.com>
    
    * style: use block rather than inline comments in product-url-adjusters
    
    Co-authored-by: Ashlee M Boyer <43934258+ashleemboyer@users.noreply.github.com>
    
    * fix: vaultData import
    
    Co-authored-by: Ashlee M Boyer <43934258+ashleemboyer@users.noreply.github.com>
    
    * fix: remove /api from basePaths, dupe of remark plugin fix
    
    Co-authored-by: Bryce Kalow <bkalow@hashicorp.com>
    Co-authored-by: Ashlee M Boyer <43934258+ashleemboyer@users.noreply.github.com>

---
+++ src/pages/vault/downloads/index.tsx
@@ -0,0 +1,28 @@
+import { ReactElement } from 'react'
+import { GetStaticProps } from 'next'
+import vaultData from 'data/vault.json'
+import { Product } from 'types/products'
+import BaseNewLayout from 'layouts/base-new'
+
+const product = vaultData as Product
+
+const VaultDownloadsPage = (): ReactElement => {
+  return (
+    <div className="g-grid-container">
+      <h1>Vault Downloads</h1>
+      <ul>
+        <li>This page is a work in progress</li>
+      </ul>
+    </div>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  return {
+    props: { product },
+    revalidate: 10,
+  }
+}
+
+VaultDownloadsPage.layout = BaseNewLayout
+export default VaultDownloadsPage
