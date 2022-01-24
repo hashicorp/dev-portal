8f57a2b6944d233a97edbf1d76175861fd2e3f76
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-01-24 09:45:18 -0500

    feat: add segment write key for ConsentManager

--- next.config.js
+++ next.config.js
@@ -38,6 +38,7 @@ module.exports = withSwingset({
       HASHI_ENV: process.env.HASHI_ENV || 'development',
       BUGSNAG_CLIENT_KEY: '06718db5e1d75829801baa0b4ca2fb7b',
       BUGSNAG_SERVER_KEY: 'b32b4487b5dc72b32f51c8fe33641a43',
+      SEGMENT_WRITE_KEY: process.env.SEGMENT_WRITE_KEY,
     },
     svgo: {
       plugins: [
