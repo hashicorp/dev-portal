77c42b725c4b00a316ff60cd0fb290f1487cf1c9
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-01-25 22:20:28 -0500

    Using CardLinks in main home page

--- src/components/product-card-grid/index.tsx
+++ src/components/product-card-grid/index.tsx
@@ -1,9 +1,9 @@
 import React, { ReactElement } from 'react'
-import Link from 'next/link'
 import classNames from 'classnames'
+import CardLink from 'components/card-link'
 import ProductIcon from 'components/product-icon'
-import s from './style.module.css'
 import CONTENT from './content.json'
+import s from './style.module.css'
 
 function ProductCardGrid({ className }: { className?: string }): ReactElement {
   return (
@@ -23,31 +23,33 @@ function CardGridSection({ title, products }): ReactElement {
       <span className={s.sectionBody}>
         {products.map(({ slug, hasLogo, headingIcon, heading, subheading }) => {
           return (
-            <Link key={slug} href={`/${slug}`}>
-              <a className={s.sectionBodyCardWrapper}>
-                <span className={s.sectionBodyCard}>
-                  {hasLogo && (
+            <div className={s.sectionBodyCardWrapper} key={slug}>
+              <CardLink
+                className={s.sectionBodyCard}
+                elevation="low"
+                href={`/${slug}`}
+              >
+                {hasLogo && (
+                  <ProductIcon
+                    className={s.sectionBodyCardLogo}
+                    product={slug}
+                  />
+                )}
+                <span className={s.sectionBodyCardHeading}>
+                  {headingIcon && (
                     <ProductIcon
-                      className={s.sectionBodyCardLogo}
+                      className={s.sectionBodyCardHeadingIcon}
                       product={slug}
                     />
                   )}
-                  <span className={s.sectionBodyCardHeading}>
-                    {headingIcon && (
-                      <ProductIcon
-                        className={s.sectionBodyCardHeadingIcon}
-                        product={slug}
-                      />
-                    )}
-                    {heading}
-                  </span>
-                  <span
-                    className={s.sectionBodyCardSubheading}
-                    dangerouslySetInnerHTML={{ __html: subheading }}
-                  />
+                  {heading}
                 </span>
-              </a>
-            </Link>
+                <span
+                  className={s.sectionBodyCardSubheading}
+                  dangerouslySetInnerHTML={{ __html: subheading }}
+                />
+              </CardLink>
+            </div>
           )
         })}
       </span>
