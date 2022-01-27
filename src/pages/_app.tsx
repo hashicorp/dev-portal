e4d049e6f852d5ad1131e92141050a2e0e379ef8
Bryce Kalow <bkalow@hashicorp.com>
2022-01-27 15:32:05 -0600

    use next/dynamic for the product switcher

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
 import '@hashicorp/platform-util/nprogress/style.css'
+import dynamic from 'next/dynamic'
 import { ErrorBoundary } from '@hashicorp/platform-runtime-error-monitoring'
 import useAnchorLinkAnalytics from '@hashicorp/platform-util/anchor-link-analytics'
 import BaseLayout from 'layouts/base'
-import PreviewProductSwitcher from 'components/_proxied-dot-io/common/preview-product-select'
 import './style.css'
 
 const isPreview = process.env.HASHI_ENV === 'preview'
+
+const PreviewProductSwitcher = dynamic(
+  () => import('components/_proxied-dot-io/common/preview-product-select'),
+  { ssr: false }
+)
 
 export default function App({ Component, pageProps }) {
   useAnchorLinkAnalytics()
