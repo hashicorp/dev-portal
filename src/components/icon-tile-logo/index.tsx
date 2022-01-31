d228f77e196c1886ae1b82430e96e15c405d5018
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-01-31 13:52:01 -0500

    refactor: refactor IconTile to accept children

--- src/components/icon-tile-logo/index.tsx
+++ src/components/icon-tile-logo/index.tsx
@@ -1,26 +1,30 @@
+import ProductIcon from 'components/product-icon'
+import { ProductSlug } from 'types/products'
 import IconTile from 'components/icon-tile'
 
+type ProductWithIconSlug = Exclude<ProductSlug, 'sentinel'>
 interface IconTileLogoProps {
-  product:
-    | 'boundary'
-    | 'consul'
-    | 'nomad'
-    | 'packer'
-    | 'terraform'
-    | 'vagrant'
-    | 'vault'
-    | 'waypoint'
-    | 'hcp'
+  product: ProductWithIconSlug
 }
 
+/**
+ * Use the IconTileLogo component when representing the product in our interfaces.
+ *
+ * Renders a product icon in a brand-colored tile. Note that this IconTileLogo
+ * is currently styled to match dev-portal designs, and differs in size and
+ * background coloration from the IconTileLogo documented in the
+ * HashiCorp Design System.
+ *
+ * ref: https://www.figma.com/file/noyY6dUMDYjmySpHcMjhkN/HDS-Product---Components-%5BWIP%5D?node-id=1377%3A11992
+ */
 function IconTileLogo({ product }: IconTileLogoProps): React.ReactElement {
   return (
     <IconTile
       size="extra-large"
-      icon={`${product}-color`}
-      forceIconColor={false}
       brandColor={product == 'hcp' ? 'neutral' : product}
-    />
+    >
+      <ProductIcon product={product} />
+    </IconTile>
   )
 }
 
--- src/components/icon-tile/index.tsx
+++ src/components/icon-tile/index.tsx
@@ -1,44 +1,44 @@
-import { useMemo } from 'react'
 import s from './style.module.css'
 import classNames from 'classnames'
+import { ProductSlug } from 'types/products'
+
+type ProductBrandColor =
+  | 'neutral'
+  | Exclude<Exclude<ProductSlug, 'sentinel'>, 'hcp'>
 
 interface IconTileProps {
-  icon: string
-  /** Note: the "extra-large" option is not documented in the design system. It's being used on the /{product} view pages. */
+  /** Pass a single child, which should be a Flight icon. For 'small' and 'medium' size, pass the 16px icon size; for other sizes pass the 24px icon size. Note that non-"color" icons will be colored using the "brandColor". */
+  children: React.ReactNode
+  /** Note: the "extra-large" option is not documented in the design system. It's being used for the IconTileLogo component, as used on the /{product} view pages. */
   size: 'small' | 'medium' | 'large' | 'extra-large'
-  brandColor?: string
-  forceIconColor?: boolean
+  /** Optional product slug to use for brand color theming. If not provided, defaults to "neutral". Note that "sentinel" and "hcp" are not supported. */
+  brandColor?: ProductBrandColor
 }
 
+/**
+ * IconTiles are used to represent objects and resources,
+ * either unrelated, or directly related to a product.
+ *
+ * Renders an icon in a brand-colored tile. Note that this IconTile does
+ * not fully implement the IconTile component documented in the
+ * HashiCorp Design System, as it does not support "secondary" icons.
+ *
+ * ref: https://www.figma.com/file/noyY6dUMDYjmySpHcMjhkN/HDS-Product---Components-%5BWIP%5D?node-id=1377%3A11992
+ */
 function IconTile({
-  icon,
+  children,
   size = 'medium',
   brandColor = 'neutral',
-  forceIconColor = true,
 }: IconTileProps): React.ReactElement {
-  const flightImportPath = `${icon}-${size == 'small' ? 16 : 24}`
-
-  const IconSvg = useMemo(() => {
-    try {
-      // eslint-disable-next-line @typescript-eslint/no-var-requires
-      const svgModule = require(`@hashicorp/flight-icons/svg-react/${flightImportPath}`)
-      return svgModule[Object.keys(svgModule)[0]]
-    } catch (e) {
-      console.error(e)
-      return () => null
-    }
-  }, [flightImportPath])
-
   return (
     <span
       className={classNames(
         s.root,
         s[`size-${size}`],
-        s[`color-${brandColor}`],
-        { [s.forceIconColor]: forceIconColor }
+        s[`color-${brandColor}`]
       )}
     >
-      <IconSvg />
+      {children}
     </span>
   )
 }
