52b5418f505d1b5d8d101d4a2938f2d5c5e8c0c5
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-02-01 20:14:47 -0500

    Styling Disclosure component

--- src/components/disclosure/index.tsx
+++ src/components/disclosure/index.tsx
@@ -1,31 +1,51 @@
-import { FC, useState } from 'react'
-import { IconChevronRight16 } from '@hashicorp/flight-icons/svg-react/chevron-right-16'
+import { FC, ReactNode, useState } from 'react'
+import classNames from 'classnames'
+import { IconChevronRight24 } from '@hashicorp/flight-icons/svg-react/chevron-right-24'
+import Text from 'components/text'
+import s from './disclosure.module.css'
 
 interface DisclosureProps {
-  title: string
+  children: ReactNode
   description?: string
   open?: boolean
+  title: string
 }
 
 const Disclosure: FC<DisclosureProps> = ({
-  title,
-  description,
   children,
+  description,
   open,
+  title,
 }) => {
   const [isOpen, setIsOpen] = useState(open)
+  const buttonClassNames = classNames(
+    s.button,
+    s[`button-${isOpen ? 'expanded' : 'collapsed'}`]
+  )
 
   return (
-    <>
-      <button onClick={() => setIsOpen(!isOpen)}>
-        <span>
-          <span>{title}</span>
-          {description && <span>{description}</span>}
+    <div className={s.root}>
+      <button
+        aria-expanded={isOpen}
+        className={buttonClassNames}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <span className={s.labelContainer}>
+          <Text asElement="span" className={s.title} weight="semibold">
+            {title}
+          </Text>
+          {description && (
+            <Text asElement="span" className={s.description} size={200}>
+              {title}
+            </Text>
+          )}
         </span>
-        <IconChevronRight16 />
+        <IconChevronRight24 />
       </button>
-      <div style={{ display: isOpen ? 'block' : 'none' }}>{children}</div>
-    </>
+      <div className={s.content} style={{ display: isOpen ? 'block' : 'none' }}>
+        {children}
+      </div>
+    </div>
   )
 }
 
