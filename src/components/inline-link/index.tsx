f9cea68b88d9577dc658ae30f8b139fd25eac73b
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-02-04 10:11:20 -0500

    First pass at InlineLink

---
+++ src/components/inline-link/index.tsx
@@ -0,0 +1,32 @@
+import { ReactElement } from 'react'
+import classNames from 'classnames'
+import MaybeInternalLink from 'components/maybe-internal-link'
+import s from './inline-link.module.css'
+import Text, { TextProps } from 'components/text'
+
+interface InlineLinkProps {
+  className?: string
+  href: string
+  size: 100 | 200 | 300
+  text: string
+  weight: TextProps['weight']
+}
+
+const InlineLink = ({
+  className,
+  href,
+  size,
+  text,
+}: InlineLinkProps): ReactElement => {
+  const classes = classNames(s.root, className)
+
+  return (
+    <MaybeInternalLink className={classes} href={href}>
+      <Text asElement="span" size={size}>
+        {text}
+      </Text>
+    </MaybeInternalLink>
+  )
+}
+
+export default InlineLink
