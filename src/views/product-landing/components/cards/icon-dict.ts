4340a78838d4d36bc2c668802e381dc4fe1d399c
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-02-07 12:37:21 -0500

    feat: add placeholder content for remaining products

--- src/views/product-landing/components/cards/icon-dict.ts
+++ src/views/product-landing/components/cards/icon-dict.ts
@@ -3,6 +3,7 @@ import { IconConsulColor16 } from '@hashicorp/flight-icons/svg-react/consul-colo
 import { IconDocs16 } from '@hashicorp/flight-icons/svg-react/docs-16'
 import { IconDownload16 } from '@hashicorp/flight-icons/svg-react/download-16'
 import { IconPackerColor16 } from '@hashicorp/flight-icons/svg-react/packer-color-16'
+import { IconProvider16 } from '@hashicorp/flight-icons/svg-react/provider-16'
 import { IconServer16 } from '@hashicorp/flight-icons/svg-react/server-16'
 import { IconTerminal16 } from '@hashicorp/flight-icons/svg-react/terminal-16'
 import { IconTerraformColor16 } from '@hashicorp/flight-icons/svg-react/terraform-color-16'
@@ -15,6 +16,7 @@ export default {
   IconDocs: IconDocs16,
   IconDownload: IconDownload16,
   IconPackerColor: IconPackerColor16,
+  IconProvider: IconProvider16,
   IconServer: IconServer16,
   IconTerminal: IconTerminal16,
   IconTerraformColor: IconTerraformColor16,
--- src/views/product-landing/components/cards/tag-icon-dict.js
+++ src/views/product-landing/components/cards/tag-icon-dict.js
@@ -1,7 +1,10 @@
 import { IconBoundaryColor16 } from '@hashicorp/flight-icons/svg-react/boundary-color-16'
 import { IconConsulColor16 } from '@hashicorp/flight-icons/svg-react/consul-color-16'
+import { IconNomadColor16 } from '@hashicorp/flight-icons/svg-react/nomad-color-16'
 import { IconPackerColor16 } from '@hashicorp/flight-icons/svg-react/packer-color-16'
 import { IconPlay16 } from '@hashicorp/flight-icons/svg-react/play-16'
+import { IconTerraformColor16 } from '@hashicorp/flight-icons/svg-react/terraform-color-16'
+import { IconVagrantColor16 } from '@hashicorp/flight-icons/svg-react/vagrant-color-16'
 import { IconVaultColor16 } from '@hashicorp/flight-icons/svg-react/vault-color-16'
 import { IconWaypointColor16 } from '@hashicorp/flight-icons/svg-react/waypoint-color-16'
 
@@ -9,7 +12,10 @@ import { IconWaypointColor16 } from '@hashicorp/flight-icons/svg-react/waypoint-
 export default {
   boundary: IconBoundaryColor16,
   consul: IconConsulColor16,
+  nomad: IconNomadColor16,
   packer: IconPackerColor16,
+  terraform: IconTerraformColor16,
+  vagrant: IconVagrantColor16,
   vault: IconVaultColor16,
   video: IconPlay16,
   waypoint: IconWaypointColor16,
