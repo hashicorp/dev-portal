253563ae51d517c6e7d857a769aa2ddd5c8680a6
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-02-07 11:46:36 -0500

    feat: add in boundary placeholder content

--- src/views/product-landing/components/cards/tag-icon-dict.js
+++ src/views/product-landing/components/cards/tag-icon-dict.js
@@ -1,3 +1,4 @@
+import { IconBoundaryColor16 } from '@hashicorp/flight-icons/svg-react/boundary-color-16'
 import { IconConsulColor16 } from '@hashicorp/flight-icons/svg-react/consul-color-16'
 import { IconPackerColor16 } from '@hashicorp/flight-icons/svg-react/packer-color-16'
 import { IconPlay16 } from '@hashicorp/flight-icons/svg-react/play-16'
@@ -6,9 +7,10 @@ import { IconWaypointColor16 } from '@hashicorp/flight-icons/svg-react/waypoint-
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
+  boundary: IconBoundaryColor16,
+  consul: IconConsulColor16,
+  packer: IconPackerColor16,
   vault: IconVaultColor16,
   video: IconPlay16,
   waypoint: IconWaypointColor16,
-  consul: IconConsulColor16,
-  packer: IconPackerColor16,
 }
