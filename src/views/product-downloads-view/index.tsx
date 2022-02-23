2a16e885cdda5c7d5a8b0527fdfc1e4a5dbe7c37
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-02-23 17:55:02 -0500

    WIP adding operating system card

--- src/views/product-downloads-view/index.tsx
+++ src/views/product-downloads-view/index.tsx
@@ -1,22 +1,28 @@
 import { ReactElement, useMemo, useState } from 'react'
+import CodeBlock from '@hashicorp/react-code-block'
 import semverRSort from 'semver/functions/rsort'
 import { useCurrentProduct } from 'contexts'
 import EmptyLayout from 'layouts/empty'
 import SidebarSidecarLayout from 'layouts/sidebar-sidecar'
+import Card from 'components/card'
 import Heading from 'components/heading'
 import IconTileLogo from 'components/icon-tile-logo'
+import Tabs, { Tab } from 'components/tabs'
 import Text from 'components/text'
 import {
   getPageSubtitle,
   initializeBackToLink,
   initializeBreadcrumbLinks,
   initializeNavData,
+  sortPlatforms,
+  prettyOs,
 } from './helpers'
 import { ProductDownloadsViewProps } from './types'
 import s from './product-downloads-view.module.css'
 
 const ProductDownloadsView = ({
   latestVersion,
+  pageContent,
   releases,
 }: ProductDownloadsViewProps): ReactElement => {
   const versionSwitcherOptions = useMemo(() => {
@@ -42,6 +48,25 @@ const ProductDownloadsView = ({
   const navData = useMemo(() => initializeNavData(currentProduct), [
     currentProduct,
   ])
+
+  const downloadsByOS = useMemo(
+    () => sortPlatforms(releases.versions[selectedVersion]),
+    [releases.versions, selectedVersion]
+  )
+  const packageManagersByOS = useMemo(() => {
+    const result = {}
+
+    pageContent.packageManagers.forEach((packageManager) => {
+      const { os } = packageManager
+      if (result[os]) {
+        result[os].push(packageManager)
+      } else {
+        result[os] = [packageManager]
+      }
+    })
+
+    return result
+  }, [pageContent.packageManagers])
 
   const pageTitle = `Install ${currentProduct.name}`
   const pageSubtitle = getPageSubtitle(
@@ -79,25 +104,68 @@ const ProductDownloadsView = ({
           </Text>
         </div>
       </div>
-      {
-        <>
-          <label style={{ display: 'block' }}>Version (temp switcher)</label>
-          <select
-            onChange={(e) => setSelectedVersion(e.target.value)}
-            value={selectedVersion}
-          >
-            {versionSwitcherOptions.map((option) => (
-              <option key={option.value} value={option.value}>
-                {option.label}
-              </option>
-            ))}
-          </select>
-        </>
-      }
-      <p>
-        This content should only show when the{' '}
-        <code>enable_new_downloads_view</code> flag is on
-      </p>
+      <div style={{ marginBottom: 48 }}>
+        {
+          <>
+            <label style={{ display: 'block' }}>Version (temp switcher)</label>
+            <select
+              onChange={(e) => setSelectedVersion(e.target.value)}
+              value={selectedVersion}
+            >
+              {versionSwitcherOptions.map((option) => (
+                <option key={option.value} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
+            </select>
+          </>
+        }
+      </div>
+      <Card elevation="base">
+        <Heading
+          className={s.operatingSystemTitle}
+          level={2}
+          size={300}
+          slug="operating-system"
+          weight="bold"
+        >
+          Operating System
+        </Heading>
+        <Tabs showAnchorLine>
+          {Object.keys(downloadsByOS).map((os) => {
+            const packageManagers = packageManagersByOS[os]
+            const hasOnePackageManager = packageManagers?.length === 1
+            const hasManyPackageManagers = packageManagers?.length > 1
+
+            const prettyOSName = prettyOs(os)
+            return (
+              <Tab heading={prettyOSName} key={os}>
+                <div className={s.tabContent}>
+                  {hasOnePackageManager && (
+                    <>
+                      <Heading
+                        level={3}
+                        size={200}
+                        slug={`package-manager-for-${prettyOSName}`}
+                        weight="semibold"
+                      >
+                        Package manager for {prettyOSName}
+                      </Heading>
+                      <CodeBlock
+                        code={packageManagers[0].commands
+                          .map((command) => `$ ${command}`)
+                          .join('\n')}
+                        options={{ showClipboard: true }}
+                      />
+                    </>
+                  )}
+                  {hasManyPackageManagers && <>{/* TODO: Tabs */}</>}
+                </div>
+              </Tab>
+            )
+          })}
+        </Tabs>
+      </Card>
     </SidebarSidecarLayout>
   )
 }
--- src/views/product-downloads-view/types.ts
+++ src/views/product-downloads-view/types.ts
@@ -2,5 +2,12 @@ import { ReleasesAPIResponse } from 'lib/fetch-release-data'
 
 export interface ProductDownloadsViewProps {
   latestVersion: string
+  pageContent: {
+    packageManagers: {
+      label: string
+      commands: string[]
+      os: string
+    }[]
+  }
   releases: ReleasesAPIResponse
 }
