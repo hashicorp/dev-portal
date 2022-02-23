d700d5b3694a55c04bb400b0c54f292abc548f49
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-02-23 12:41:54 -0500

    Conditionally showing downloads view on flag

--- src/pages/waypoint/downloads/index.tsx
+++ src/pages/waypoint/downloads/index.tsx
@@ -1,8 +1,22 @@
+import { ReactElement } from 'react'
 import { GetStaticProps } from 'next'
 import waypointData from 'data/waypoint.json'
 import { Product } from 'types/products'
+import { generateStaticProps, GeneratedProps } from 'lib/fetch-release-data'
+import EmptyLayout from 'layouts/empty'
 import ProductDownloadsView from 'views/product-downloads-view'
-import { generateStaticProps } from 'lib/fetch-release-data'
+import PlaceholderDownloadsView from 'views/placeholder-product-downloads-view'
+
+const WaypointDownloadsPage = (props: GeneratedProps): ReactElement => {
+  if (__config.flags.enable_new_downloads_view) {
+    const { latestVersion, releases } = props
+    return (
+      <ProductDownloadsView latestVersion={latestVersion} releases={releases} />
+    )
+  } else {
+    return <PlaceholderDownloadsView />
+  }
+}
 
 export const getStaticProps: GetStaticProps = async () => {
   const product = waypointData as Product
@@ -10,4 +24,6 @@ export const getStaticProps: GetStaticProps = async () => {
   return generateStaticProps(product)
 }
 
-export default ProductDownloadsView
+WaypointDownloadsPage.layout = EmptyLayout
+
+export default WaypointDownloadsPage
