5798c359645e97ec553ce0f6a2c1b9baf910b37f
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-02-24 16:22:59 -0500

    Creating helper components to clean up markup

--- src/views/product-downloads-view/components/downloads-section/index.tsx
+++ src/views/product-downloads-view/components/downloads-section/index.tsx
@@ -33,6 +33,171 @@ const groupPackageManagersByOS = (packageManagers: PackageManager[]) => {
   return result
 }
 
+const PackageManagerSection = ({ packageManagers, prettyOSName }) => {
+  const hasOnePackageManager = packageManagers?.length === 1
+  const hasManyPackageManagers = packageManagers?.length > 1
+  const hasPackageManagers = hasOnePackageManager || hasManyPackageManagers
+
+  return (
+    <>
+      {hasPackageManagers && (
+        <Heading
+          className={s.subHeading}
+          level={3}
+          size={200}
+          slug={`package-manager-for-${prettyOSName}`}
+          weight="semibold"
+        >
+          Package manager for {prettyOSName}
+        </Heading>
+      )}
+      {hasOnePackageManager && (
+        <CodeBlock
+          code={packageManagers[0].commands
+            .map((command: string) => `$ ${command}`)
+            .join('\n')}
+          language="shell-session"
+          options={{ showClipboard: true }}
+        />
+      )}
+      {hasManyPackageManagers && (
+        /**
+         * TODO: this will eventually be <CodeTabs> once a bug has
+         * been fixed.
+         *
+         * ref: https://app.asana.com/0/1201010428539925/1201881376116200/f
+         */
+        <Tabs showAnchorLine={false}>
+          {packageManagers.map((packageManager) => {
+            return (
+              <Tab heading={packageManager.label} key={packageManager.label}>
+                <CodeBlock
+                  className={s.codeTabsCodeBlock}
+                  code={packageManager.commands
+                    .map((command: string) => `$ ${command}`)
+                    .join('\n')}
+                  language="shell-session"
+                  options={{ showClipboard: true }}
+                />
+              </Tab>
+            )
+          })}
+        </Tabs>
+      )}
+    </>
+  )
+}
+
+const BinaryDownloadsSection = ({
+  downloadsByOS,
+  os,
+  prettyOSName,
+  selectedRelease,
+}) => {
+  return (
+    <>
+      <Heading
+        className={s.subHeading}
+        level={3}
+        size={200}
+        slug={`binary-download-for-${prettyOSName}`}
+        weight="semibold"
+      >
+        Binary download for {prettyOSName}
+      </Heading>
+      {Object.keys(downloadsByOS[os]).map((arch) => (
+        <Card className={s.downloadCard} elevation="base" key={arch}>
+          <div className={s.downloadCardText}>
+            <Text className={s.archNameLabel} size={200} weight="semibold">
+              {arch.toUpperCase()}
+            </Text>
+            <Text className={s.archVersionLabel} size={200} weight="regular">
+              Version: {selectedRelease.version}
+            </Text>
+          </div>
+          <DownloadStandaloneLink
+            ariaLabel="TODO"
+            href={downloadsByOS[os][arch]}
+          />
+        </Card>
+      ))}
+    </>
+  )
+}
+
+const ChangelogSection = ({ selectedRelease }) => {
+  return (
+    <>
+      <Heading
+        className={s.subHeading}
+        level={3}
+        size={200}
+        slug="release-information"
+        weight="semibold"
+      >
+        Release information
+      </Heading>
+      <Card className={s.downloadCard} elevation="base">
+        <div className={s.downloadCardText}>
+          <Text className={s.archNameLabel} size={200} weight="semibold">
+            Changelog
+          </Text>
+          <Text className={s.archVersionLabel} size={200} weight="regular">
+            Version: {selectedRelease.version}
+          </Text>
+        </div>
+        <StandaloneLink
+          ariaLabel="TODO"
+          href={`https://github.com/hashicorp/waypoint/blob/v${selectedRelease.version}/CHANGELOG.md`}
+          icon={<IconExternalLink16 />}
+          iconPosition="trailing"
+          openInNewTab
+          text="GitHub"
+          textSize={200}
+        />
+      </Card>
+    </>
+  )
+}
+
+const NotesSection = ({ selectedRelease }) => {
+  return (
+    <>
+      <Heading
+        className={s.subHeading}
+        level={3}
+        size={200}
+        slug="notes"
+        style={{ marginBottom: 8 }}
+        weight="semibold"
+      >
+        Notes
+      </Heading>
+      <Text size={200}>
+        You can find the{' '}
+        <InlineLink
+          href={`https://releases.hashicorp.com/${selectedRelease.name}/${selectedRelease.version}/${selectedRelease.shasums}`}
+          text={`SHA256 checksums for Waypoint ${selectedRelease.version}`}
+          textSize={200}
+        />{' '}
+        online and you can{' '}
+        <InlineLink
+          href={`https://releases.hashicorp.com/${selectedRelease.name}/${selectedRelease.version}/${selectedRelease.shasums_signature}`}
+          text="verify the checksums signature file"
+          textSize={200}
+        />{' '}
+        which has been signed using{' '}
+        <InlineLink
+          href="https://www.hashicorp.com/security"
+          text="HashiCorp's GPG key"
+          textSize={200}
+        />
+        .
+      </Text>
+    </>
+  )
+}
+
 const DownloadsSection = ({
   packageManagers,
   selectedRelease,
@@ -46,7 +211,7 @@ const DownloadsSection = ({
   )
 
   return (
-    <section>
+    <article>
       <Card elevation="base">
         <Heading
           className={s.operatingSystemTitle}
@@ -60,173 +225,30 @@ const DownloadsSection = ({
         <Tabs showAnchorLine>
           {Object.keys(downloadsByOS).map((os) => {
             const packageManagers: PackageManager[] = packageManagersByOS[os]
-            const hasOnePackageManager = packageManagers?.length === 1
-            const hasManyPackageManagers = packageManagers?.length > 1
-            const hasPackageManagers =
-              hasOnePackageManager || hasManyPackageManagers
-
             const prettyOSName = prettyOs(os)
+
             return (
               <Tab heading={prettyOSName} key={os}>
                 <div className={s.tabContent}>
-                  {hasPackageManagers && (
-                    <Heading
-                      className={s.subHeading}
-                      level={3}
-                      size={200}
-                      slug={`package-manager-for-${prettyOSName}`}
-                      weight="semibold"
-                    >
-                      Package manager for {prettyOSName}
-                    </Heading>
-                  )}
-                  {hasOnePackageManager && (
-                    <CodeBlock
-                      code={packageManagers[0].commands
-                        .map((command: string) => `$ ${command}`)
-                        .join('\n')}
-                      language="shell-session"
-                      options={{ showClipboard: true }}
-                    />
-                  )}
-                  {hasManyPackageManagers && (
-                    /**
-                     * TODO: this will eventually be <CodeTabs> once a bug has
-                     * been fixed.
-                     *
-                     * ref: https://app.asana.com/0/1201010428539925/1201881376116200/f
-                     */
-                    <Tabs showAnchorLine={false}>
-                      {packageManagers.map((packageManager) => {
-                        return (
-                          <Tab
-                            heading={packageManager.label}
-                            key={packageManager.label}
-                          >
-                            <CodeBlock
-                              className={s.codeTabsCodeBlock}
-                              code={packageManager.commands
-                                .map((command: string) => `$ ${command}`)
-                                .join('\n')}
-                              language="shell-session"
-                              options={{ showClipboard: true }}
-                            />
-                          </Tab>
-                        )
-                      })}
-                    </Tabs>
-                  )}
-                  <Heading
-                    className={s.subHeading}
-                    level={3}
-                    size={200}
-                    slug={`binary-download-for-${prettyOSName}`}
-                    weight="semibold"
-                  >
-                    Binary download for {prettyOSName}
-                  </Heading>
-                  {Object.keys(downloadsByOS[os]).map((arch) => (
-                    <Card
-                      className={s.downloadCard}
-                      elevation="base"
-                      key={arch}
-                    >
-                      <div className={s.downloadCardText}>
-                        <Text
-                          className={s.archNameLabel}
-                          size={200}
-                          weight="semibold"
-                        >
-                          {arch.toUpperCase()}
-                        </Text>
-                        <Text
-                          className={s.archVersionLabel}
-                          size={200}
-                          weight="regular"
-                        >
-                          Version: {selectedRelease.version}
-                        </Text>
-                      </div>
-                      <DownloadStandaloneLink
-                        ariaLabel="TODO"
-                        href={downloadsByOS[os][arch]}
-                      />
-                    </Card>
-                  ))}
-                  <Heading
-                    className={s.subHeading}
-                    level={3}
-                    size={200}
-                    slug="release-information"
-                    weight="semibold"
-                  >
-                    Release information
-                  </Heading>
-                  <Card className={s.downloadCard} elevation="base">
-                    <div className={s.downloadCardText}>
-                      <Text
-                        className={s.archNameLabel}
-                        size={200}
-                        weight="semibold"
-                      >
-                        Changelog
-                      </Text>
-                      <Text
-                        className={s.archVersionLabel}
-                        size={200}
-                        weight="regular"
-                      >
-                        Version: {selectedRelease.version}
-                      </Text>
-                    </div>
-                    <StandaloneLink
-                      ariaLabel="TODO"
-                      href={`https://github.com/hashicorp/waypoint/blob/v${selectedRelease.version}/CHANGELOG.md`}
-                      icon={<IconExternalLink16 />}
-                      iconPosition="trailing"
-                      openInNewTab
-                      text="GitHub"
-                      textSize={200}
-                    />
-                  </Card>
-                  <Heading
-                    className={s.subHeading}
-                    level={3}
-                    size={200}
-                    slug="notes"
-                    style={{ marginBottom: 8 }}
-                    weight="semibold"
-                  >
-                    Notes
-                  </Heading>
-                  <Text size={200}>
-                    You can find the{' '}
-                    <InlineLink
-                      href={`https://releases.hashicorp.com/${selectedRelease.name}/${selectedRelease.version}/${selectedRelease.shasums}`}
-                      text={`SHA256 checksums for Waypoint ${selectedRelease.version}`}
-                      textSize={200}
-                    />{' '}
-                    online and you can{' '}
-                    <InlineLink
-                      href={`https://releases.hashicorp.com/${selectedRelease.name}/${selectedRelease.version}/${selectedRelease.shasums_signature}`}
-                      text="verify the checksums signature file"
-                      textSize={200}
-                    />{' '}
-                    which has been signed using{' '}
-                    <InlineLink
-                      href="https://www.hashicorp.com/security"
-                      text="HashiCorp's GPG key"
-                      textSize={200}
-                    />
-                    .
-                  </Text>
+                  <PackageManagerSection
+                    packageManagers={packageManagers}
+                    prettyOSName={prettyOSName}
+                  />
+                  <BinaryDownloadsSection
+                    downloadsByOS={downloadsByOS}
+                    os={os}
+                    prettyOSName={prettyOSName}
+                    selectedRelease={selectedRelease}
+                  />
+                  <ChangelogSection selectedRelease={selectedRelease} />
+                  <NotesSection selectedRelease={selectedRelease} />
                 </div>
               </Tab>
             )
           })}
         </Tabs>
       </Card>
-    </section>
+    </article>
   )
 }
 
