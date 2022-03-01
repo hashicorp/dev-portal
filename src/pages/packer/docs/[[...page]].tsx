fcbed5aacbd53bb8364fb1da5caef52772ff7ebb
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-03-01 08:17:58 -0500

    feat: finish adding Packer /docs route

--- src/pages/packer/docs/[[...page]].tsx
+++ src/pages/packer/docs/[[...page]].tsx
@@ -3,37 +3,28 @@ import { Product } from 'types/products'
 import DocsView from 'views/docs-view'
 import SidebarSidecarLayout from 'layouts/sidebar-sidecar'
 import { getStaticGenerationFunctions } from 'layouts/sidebar-sidecar/server'
+// Author primitives
+import Badge from 'components/author-primitives/packer/badge'
+import BadgesHeader from 'components/author-primitives/packer/badges-header'
+import PluginBadge from 'components/author-primitives/packer/plugin-badge'
+import Checklist from 'components/author-primitives/packer/checklist'
 
 const basePath = 'docs'
 const baseName = 'Docs'
 const product = packerData as Product
+const additionalComponents = { Badge, BadgesHeader, PluginBadge, Checklist }
 
 const PackerDocsPage = ({ mdxSource }): React.ReactElement => {
-  return <DocsView {...mdxSource} />
+  return <DocsView {...mdxSource} additionalComponents={additionalComponents} />
 }
 
-const {
-  getStaticPaths: genGetStaticPaths,
-  getStaticProps: genGetStaticProps,
-} = getStaticGenerationFunctions({
+const { getStaticPaths, getStaticProps } = getStaticGenerationFunctions({
   product,
   basePath,
   baseName,
 })
 
-export async function getStaticPaths(ctx) {
-  const genPaths = await genGetStaticPaths(ctx)
-  console.log({ genPaths })
-  return genPaths
-}
-
-export async function getStaticProps({ params }) {
-  const genProps = await genGetStaticProps({ params })
-  console.log({ genProps })
-  return genProps
-}
-
 PackerDocsPage.layout = SidebarSidecarLayout
 
-// export { getStaticProps }
+export { getStaticProps, getStaticPaths }
 export default PackerDocsPage
