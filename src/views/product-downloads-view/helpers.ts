b7b30dd1aa8465d667f4521f24559ddf718e31f4
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-03-01 13:13:20 -0500

    Adding generateDefaultPackageManagers helper

--- src/views/product-downloads-view/helpers.ts
+++ src/views/product-downloads-view/helpers.ts
@@ -3,6 +3,7 @@ import { ReleaseVersion } from 'lib/fetch-release-data'
 import { SidebarSidecarLayoutProps } from 'layouts/sidebar-sidecar'
 import { BreadcrumbLink } from 'components/breadcrumb-bar'
 import { MenuItem } from 'components/sidebar'
+import { PackageManager } from './types'
 
 const PLATFORM_MAP = {
   Mac: 'darwin',
@@ -14,6 +15,78 @@ export interface SortedReleases {
   [os: string]: {
     [arch: string]: string
   }
+}
+
+export const generateDefaultPackageManagers = (
+  product: Product
+): PackageManager[] => {
+  const productSlug = product.slug
+
+  return [
+    {
+      label: 'Homebrew',
+      commands: [
+        `brew tap hashicorp/tap`,
+        `brew install hashicorp/tap/${productSlug}`,
+      ],
+      os: 'darwin',
+    },
+    {
+      label: 'Ubuntu/Debian',
+      commands: [
+        `curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -`,
+        `sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"`,
+        `sudo apt-get update && sudo apt-get install ${productSlug}`,
+      ],
+      os: 'linux',
+    },
+    {
+      label: 'CentOS/RHEL',
+      commands: [
+        `sudo yum install -y yum-utils`,
+        `sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo`,
+        `sudo yum -y install ${productSlug}`,
+      ],
+      os: 'linux',
+    },
+    {
+      label: 'Fedora',
+      commands: [
+        `sudo dnf install -y dnf-plugins-core`,
+        `sudo dnf config-manager --add-repo https://rpm.releases.hashicorp.com/fedora/hashicorp.repo`,
+        `sudo dnf -y install ${productSlug}`,
+      ],
+      os: 'linux',
+    },
+    {
+      label: 'Amazon Linux',
+      commands: [
+        `sudo yum install -y yum-utils`,
+        `sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo`,
+        `sudo yum -y install ${productSlug}`,
+      ],
+      os: 'linux',
+    },
+    {
+      label: 'Homebrew',
+      commands: [
+        `brew tap hashicorp/tap`,
+        `brew install hashicorp/tap/${productSlug}`,
+      ],
+      os: 'linux',
+    },
+  ]
+}
+
+export const getPageSubtitle = (
+  currentProduct: Product,
+  selectedVersion: string,
+  isLatestVersion: boolean
+): string => {
+  const versionText = `v${selectedVersion}${
+    isLatestVersion ? ' (latest version)' : ''
+  }`
+  return `Install or update to ${versionText} of ${currentProduct.name} to get started.`
 }
 
 export const initializeBackToLink = (
@@ -52,17 +125,6 @@ export const initializeNavData = (currentProduct: Product): MenuItem[] => {
     { divider: true },
     ...currentProduct.sidebar.resourcesNavData,
   ]
-}
-
-export const getPageSubtitle = (
-  currentProduct: Product,
-  selectedVersion: string,
-  isLatestVersion: boolean
-): string => {
-  const versionText = `v${selectedVersion}${
-    isLatestVersion ? ' (latest version)' : ''
-  }`
-  return `Install or update to ${versionText} of ${currentProduct.name} to get started.`
 }
 
 export const sortPlatforms = (releaseData: ReleaseVersion): SortedReleases => {
