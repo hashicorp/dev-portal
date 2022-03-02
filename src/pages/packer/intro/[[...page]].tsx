3c807eb7fea9ebe44b0d8dcbd64bc44aa61e143b
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-03-02 12:35:00 -0500

    feat: add packer docs routes (#141)
    * feat: WIP begin adding packer pages
    
    * feat: finish adding Packer /docs route
    
    * fix: mainBranch issue, add guides route
    
    * feat: add intro route
    
    * style: naming consistency for page component
    
    Co-authored-by: Ashlee M Boyer <43934258+ashleemboyer@users.noreply.github.com>
    
    * fix: PackerGuidesPage name typo
    
    * fix: PackerGuidesPage name typo again
    
    Co-authored-by: Ashlee M Boyer <43934258+ashleemboyer@users.noreply.github.com>

---
+++ src/pages/packer/intro/[[...page]].tsx
@@ -0,0 +1,26 @@
+import packerData from 'data/packer.json'
+import { Product } from 'types/products'
+import DocsView from 'views/docs-view'
+import SidebarSidecarLayout from 'layouts/sidebar-sidecar'
+import { getStaticGenerationFunctions } from 'layouts/sidebar-sidecar/server'
+
+const basePath = 'intro'
+const baseName = 'Intro'
+const product = packerData as Product
+const mainBranch = 'master'
+
+const PackerIntroPage = ({ mdxSource }): React.ReactElement => {
+  return <DocsView {...mdxSource} />
+}
+
+const { getStaticPaths, getStaticProps } = getStaticGenerationFunctions({
+  product,
+  basePath,
+  baseName,
+  mainBranch,
+})
+
+PackerIntroPage.layout = SidebarSidecarLayout
+
+export { getStaticProps, getStaticPaths }
+export default PackerIntroPage
