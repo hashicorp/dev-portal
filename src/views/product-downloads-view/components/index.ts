6e07780b4b78fb678b9796b41da27f66ff9bd1ba
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-03-04 16:19:51 -0500

    Adding note

--- src/views/product-downloads-view/components/index.ts
+++ src/views/product-downloads-view/components/index.ts
@@ -1,3 +1,11 @@
+/**
+ * This purpose of this barrel file is to help keep the import section of
+ * `ProductDownloadsView` clean. This is not a standard process used across the
+ * project because it affects build times when used in `src/components`, but is
+ * helpful where all components in the barrel file are always used together at
+ * the same time.
+ */
+
 import DownloadsSection from './downloads-section'
 import FeaturedTutorialsSection from './featured-tutorials-section'
 import OfficialReleasesSection from './official-releases-section'
