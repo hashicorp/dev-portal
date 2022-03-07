c7be54fc10c6a5c286ece4d5d59de4dc0d54fc9b
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-03-07 11:29:16 -0500

    style: comment formatting suggestions
    Co-authored-by: Ashlee M Boyer <43934258+ashleemboyer@users.noreply.github.com>

--- src/layouts/sidebar-sidecar/utils/product-url-adjusters.ts
+++ src/layouts/sidebar-sidecar/utils/product-url-adjusters.ts
@@ -50,27 +50,30 @@ export const sentinelUrlAdjuster: Pluggable = [
 function prefixSentinelDocsUrls(inputUrl: string): string {
   // We only want to adjust internal URLs, so we return absolute URLs as-is
   if (isAbsoluteUrl(inputUrl)) return inputUrl
+
   // We now know we're dealing with internal URLs only
   let outputUrl = inputUrl.slice()
-  // We gather some information on what link routes we're dealing with.
-  // Note that on the client-side, "components/docs-anchor" will modify
-  // URLs that start with any of the "basePath" strings defined in
-  // the "basePaths" array in "data/{product.json".
-  // Any such URLs will be prefixed with the product name, in this case
-  // "sentinel". However, unlike other product sites, links in the Sentinel
-  // docs already contain this prefix. So, we need to remove the prefix
-  // from any URLs that will later be modified based on "basePaths", while
-  // retaining it for URLs that will not match "basePaths" (namely
-  // "downloads" and "changelog").
-  // TODO: could this be simplified by doing all URL manipulations here
-  // TODO: at server time, rather than also prefixing some URLs on the
-  // TODO: client-side using "components/docs-anchor"?
-  // TODO: (May be useful longer term as well, eg if authors become
-  // TODO: accustomed to the new URL structure on dev-dot, and want to write
-  // TODO: with those URLs in mind, rather than the old dot-io URL structures,
-  // TODO: then we could apply these remark transformations to all content
-  // TODO: and remove the need for prefixing and manipulating.)
-  // TODO: Asana: https://app.asana.com/0/1201010428539925/1201921061548315/f
+
+  /**
+   * We gather some information on what link routes we're dealing with. Note
+   * that on the client-side, "components/docs-anchor" will modify URLs that
+   * start with any of the "basePath" strings defined in the "basePaths" array
+   * in "data/{product.json". Any such URLs will be prefixed with the product
+   * name, in this case "sentinel". However, unlike other product sites, links
+   * in the Sentinel docs already contain this prefix. So, we need to remove the
+   * prefix from any URLs that will later be modified based on "basePaths",
+   * while retaining it for URLs that will not match "basePaths" (namely
+   * "downloads" and "changelog").
+   *
+   * TODO: could this be simplified by doing all URL manipulations here at
+   * server time, rather than also prefixing some URLs on the client-side using
+   * "components/docs-anchor"? (May be useful longer term as well, eg if authors
+   * become accustomed to the new URL structure on dev-dot, and want to write
+   * with those URLs in mind, rather than the old dot-io URL structures, then we
+   * could apply these remark transformations to all content and remove the need
+   * for prefixing and manipulating.)
+   * Asana: https://app.asana.com/0/1201010428539925/1201921061548315/f
+   */
   const isBasePathExceptDocs = sentinelData.basePaths.reduce(
     (wasMatched, basePath) => {
       const isMatch =
@@ -80,21 +83,25 @@ function prefixSentinelDocsUrls(inputUrl: string): string {
     },
     false
   )
-  // Note that "docs" links will not be caught by the above check,
-  // as on the sentinel site they are found under `/sentinel/*`, rather
-  // than under `/sentinel/docs` as defined by our basePaths here in dev-dot.
-  // So, we assume that if an internal link prefixed with `/sentinel`
-  // it NOT one of the other two known links (downloads and changelog),
-  // then it must be a docs link.
+
+  /**
+   * Note that "docs" links will not be caught by the above check, as on the
+   * sentinel site they are found under `/sentinel/*`, rather than under
+   * `/sentinel/docs` as defined by our basePaths here in dev-dot. So, we assume
+   * that if an internal link prefixed with `/sentinel` it NOT one of the other
+   * two known links (downloads and changelog), then it must be a docs link.
+   */
   const isDownloadsUrl = outputUrl == '/sentinel/downloads'
   const isChangelogUrl = outputUrl == '/sentinel/changelog'
   const isDocsUrl = !isBasePathExceptDocs && !isDownloadsUrl && !isChangelogUrl
+
   // Now we have all the info we need, so we remove prefixes from
   // all our "basePath" URLs - including the assumed "docs" links.
   const hasSentinelPrefix = outputUrl.startsWith('/sentinel')
   const willBePrefixedOnClient = isBasePathExceptDocs || isDocsUrl
   const shouldRemovePrefix = hasSentinelPrefix && willBePrefixedOnClient
   if (shouldRemovePrefix) outputUrl = outputUrl.replace('/sentinel', '')
+
   // Finally, we also need to prefix "docs" URLs with "/docs" in order
   // to match the route structure we have here in dev-dot
   if (isDocsUrl) outputUrl = `/docs${outputUrl}`
