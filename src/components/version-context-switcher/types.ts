49e57dcd9a0f9564feddf76eb6583ab6a2c4692d
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-03-08 18:26:16 -0500

    Adding TODO

--- src/components/version-context-switcher/types.ts
+++ src/components/version-context-switcher/types.ts
@@ -1,5 +1,10 @@
 import { ChangeEventHandler, ReactElement } from 'react'
 
+/**
+ * TODO: this should be moved to exist in a ContextSwitcher component once we
+ * build one, it's not been added yet for the sake of time and because it's
+ * still a WIP component on the design system side.
+ */
 export interface ContextSwitcherOption {
   label: string
   value: string
