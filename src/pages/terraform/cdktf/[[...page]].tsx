76588a62d392cd5dd9bb9d6a6bba48dfc64ca2ba
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-03-09 12:51:21 -0500

    feat: add terraform/cdktf

---
+++ src/pages/terraform/cdktf/[[...page]].tsx
@@ -0,0 +1,30 @@
+import { ReactElement } from 'react'
+import terraformData from 'data/terraform.json'
+import { Product } from 'types/products'
+import { getStaticGenerationFunctions } from 'layouts/sidebar-sidecar/server'
+import SidebarSidecarLayout from 'layouts/sidebar-sidecar'
+import DocsView from 'views/docs-view'
+
+const basePath = 'cdktf'
+const baseName = 'CDKTF'
+const product = terraformData as Product
+const productSlugForLoader = 'terraform-website'
+
+const additionalComponents = {}
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+const TerraformCdktfPage = ({ mdxSource }): ReactElement => {
+  return <DocsView {...mdxSource} additionalComponents={additionalComponents} />
+}
+
+const { getStaticPaths, getStaticProps } = getStaticGenerationFunctions({
+  product,
+  productSlugForLoader,
+  basePath,
+  baseName,
+})
+
+TerraformCdktfPage.layout = SidebarSidecarLayout
+
+export { getStaticPaths, getStaticProps }
+export default TerraformCdktfPage
