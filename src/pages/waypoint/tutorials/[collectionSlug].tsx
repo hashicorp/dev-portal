6a3f95f800e71b6b6a177134576cfa204dce3b54
Kendall Strautman <kendallstrautman@gmail.com>
2022-03-09 14:56:57 -0800

    spike: adds colleciton data fetching

--- src/pages/waypoint/tutorials/[collectionSlug].tsx
+++ src/pages/waypoint/tutorials/[collectionSlug].tsx
@@ -2,15 +2,74 @@
  * TODO - create proper view
  */
 
+import {
+  getAllCollections,
+  getCollection,
+} from 'lib/learn-client/api/collection'
+import {
+  Collection as ClientCollection,
+  ProductOption,
+} from 'lib/learn-client/types'
+import { stripUndefinedProperties } from 'lib/strip-undefined-props'
 import Link from 'next/link'
+import { splitProductFromFilename } from 'views/tutorial-view/helpers'
 
-export default function WaypointCollectionPage() {
+export default function WaypointCollectionPage(props) {
   return (
     <>
-      <h1>Collection Heading</h1>
-      <Link href="/waypoint/tutorials/get-started-docker/get-started-intro">
-        <a>Go to this tutorial</a>
-      </Link>
+      <h1>{props.collection.name}</h1>
+      <p>{props.collection.description}</p>
+      {/** TODO shape these slugs here, insert collection */}
+      <ol>
+        {props.collection.tutorials.map((tutorial) => (
+          <li key={tutorial.id}>
+            <Link href={tutorial.slug}>
+              <>
+                <a>{tutorial.name}</a> <br />
+              </>
+            </Link>
+          </li>
+        ))}
+      </ol>
     </>
   )
+}
+
+export async function getStaticProps({
+  params,
+}): Promise<{ props: { collection: ClientCollection } }> {
+  const { collectionSlug } = params
+  const collection = await getCollection(
+    `${ProductOption['waypoint']}/${collectionSlug}`
+  )
+
+  return {
+    props: {
+      collection: stripUndefinedProperties(collection),
+    },
+  }
+}
+
+interface CollectionPagePaths {
+  params: {
+    collectionSlug: string
+  }
+}
+
+export async function getStaticPaths(): Promise<{
+  paths: CollectionPagePaths[]
+  fallback: boolean
+}> {
+  const collections = await getAllCollections({
+    product: ProductOption['waypoint'],
+  })
+  const paths = collections.map((collection) => ({
+    params: {
+      collectionSlug: splitProductFromFilename(collection.slug),
+    },
+  }))
+  return {
+    paths,
+    fallback: false,
+  }
 }
