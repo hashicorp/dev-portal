5066c158825b98b812fd16cde4e3705838a540d5
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-03-10 09:58:54 -0500

    feat: add terraform/cloud-docs

---
+++ src/pages/terraform/cloud-docs/[[...page]].tsx
@@ -0,0 +1,30 @@
+import { ReactElement } from 'react'
+import terraformData from 'data/terraform.json'
+import { Product } from 'types/products'
+import { getStaticGenerationFunctions } from 'layouts/sidebar-sidecar/server'
+import SidebarSidecarLayout from 'layouts/sidebar-sidecar'
+import DocsView from 'views/docs-view'
+
+const basePath = 'cloud-docs'
+const baseName = 'Cloud Docs'
+const product = terraformData as Product
+const productSlugForLoader = 'terraform-website'
+
+const additionalComponents = {}
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+const TerraformCloudDocsPage = ({ mdxSource }): ReactElement => {
+  return <DocsView {...mdxSource} additionalComponents={additionalComponents} />
+}
+
+const { getStaticPaths, getStaticProps } = getStaticGenerationFunctions({
+  product,
+  productSlugForLoader,
+  basePath,
+  baseName,
+})
+
+TerraformCloudDocsPage.layout = SidebarSidecarLayout
+
+export { getStaticPaths, getStaticProps }
+export default TerraformCloudDocsPage
