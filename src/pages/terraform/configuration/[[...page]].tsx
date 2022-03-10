56d62d404a33cb1bcf6040b0ee251ebee0f44d73
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-03-10 10:01:52 -0500

    feat: add terraform/configuration

---
+++ src/pages/terraform/configuration/[[...page]].tsx
@@ -0,0 +1,33 @@
+import { ReactElement } from 'react'
+import terraformData from 'data/terraform.json'
+import { Product } from 'types/products'
+import { getStaticGenerationFunctions } from 'layouts/sidebar-sidecar/server'
+import SidebarSidecarLayout from 'layouts/sidebar-sidecar'
+import DocsView from 'views/docs-view'
+
+const basePath = 'configuration'
+const baseName = 'Configuration'
+const product = terraformData as Product
+// TODO: productSlug should possibly actually be `terraform`,
+// TODO: but https://mktg-content-api.vercel.app/api/content/terraform/version-metadata?partial=true
+// TODO: does not return any version with "isLatest: true
+const productSlugForLoader = 'terraform-website'
+
+const additionalComponents = {}
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+const TerraformConfigPage = ({ mdxSource }): ReactElement => {
+  return <DocsView {...mdxSource} additionalComponents={additionalComponents} />
+}
+
+const { getStaticPaths, getStaticProps } = getStaticGenerationFunctions({
+  product,
+  productSlugForLoader,
+  basePath,
+  baseName,
+})
+
+TerraformConfigPage.layout = SidebarSidecarLayout
+
+export { getStaticPaths, getStaticProps }
+export default TerraformConfigPage
