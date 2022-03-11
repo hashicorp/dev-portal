30de07d2f5bf5eaa3b551a97479cf8b0a54a93ee
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-03-11 09:04:00 -0500

    feat: stub in terraform docs routes (#167)
    * feat: add terraform/language docs route
    
    * fix: rename route
    
    * feat: add skateboard top nav
    
    * feat: add terraform/cdktf
    
    * feat: add terraform/cli
    
    * chore: assess & fix some issues
    
    * feat: add terraform/cloud-docs
    
    * feat: add terraform/configuration
    
    * feat: add terraform/enterprise
    
    * feat: add terraform/guides
    
    * feat: add terraform/internals
    
    * feat: add terraform/intro
    
    * feat: add terraform/language
    
    * feat: add terraform/plugin
    
    * feat: add terraform/registry, misc cleanup
    
    * docs: add comment on guides page
    
    * fix: temporary fix to handle empty headings
    
    * docs: add comment on versioned docs
    
    * fix: configuration, not config
    
    Co-authored-by: Ashlee M Boyer <43934258+ashleemboyer@users.noreply.github.com>
    
    * style: fix comments, remove empty additionalComponents
    
    * fix: add all basePaths
    
    * chore: fix file i forgot to save & commit
    
    Co-authored-by: Ashlee M Boyer <43934258+ashleemboyer@users.noreply.github.com>

---
+++ src/pages/terraform/docs/[[...page]].tsx
@@ -0,0 +1,31 @@
+import { ReactElement } from 'react'
+import terraformData from 'data/terraform.json'
+import { Product } from 'types/products'
+import { getStaticGenerationFunctions } from 'layouts/sidebar-sidecar/server'
+import SidebarSidecarLayout from 'layouts/sidebar-sidecar'
+import DocsView from 'views/docs-view'
+import ProviderTable from 'components/author-primitives/terraform/provider-table'
+
+const basePath = 'docs'
+const baseName = 'Docs'
+const product = terraformData as Product
+const productSlugForLoader = 'terraform-website'
+
+const additionalComponents = { ProviderTable }
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+const TerraformDocsPage = ({ mdxSource }): ReactElement => {
+  return <DocsView {...mdxSource} additionalComponents={additionalComponents} />
+}
+
+const { getStaticPaths, getStaticProps } = getStaticGenerationFunctions({
+  product,
+  productSlugForLoader,
+  basePath,
+  baseName,
+})
+
+TerraformDocsPage.layout = SidebarSidecarLayout
+
+export { getStaticPaths, getStaticProps }
+export default TerraformDocsPage
---
+++ src/pages/terraform/registry/[[...page]].tsx
@@ -0,0 +1,28 @@
+import { ReactElement } from 'react'
+import terraformData from 'data/terraform.json'
+import { Product } from 'types/products'
+import { getStaticGenerationFunctions } from 'layouts/sidebar-sidecar/server'
+import SidebarSidecarLayout from 'layouts/sidebar-sidecar'
+import DocsView from 'views/docs-view'
+
+const basePath = 'registry'
+const baseName = 'Registry'
+const product = terraformData as Product
+const productSlugForLoader = 'terraform-website'
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+const TerraformRegistryPage = ({ mdxSource }): ReactElement => {
+  return <DocsView {...mdxSource} />
+}
+
+const { getStaticPaths, getStaticProps } = getStaticGenerationFunctions({
+  product,
+  productSlugForLoader,
+  basePath,
+  baseName,
+})
+
+TerraformRegistryPage.layout = SidebarSidecarLayout
+
+export { getStaticPaths, getStaticProps }
+export default TerraformRegistryPage
