eeaeff4000ee6e070f8f929b6f1800690349f9b0
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-03-16 11:37:50 -0400

    chore: clean up unecessary type annotations

--- src/components/card/types.ts
+++ src/components/card/types.ts
@@ -1,9 +1,7 @@
 import { ReactNode } from 'react'
 
 export interface CardProps {
-  /** The elements to render within the card body. */
   children: ReactNode
-  /** A string of one or more classnames attached to the card body element. */
   className?: string
   elevation?: 'base' | 'mid' | 'high'
 }
