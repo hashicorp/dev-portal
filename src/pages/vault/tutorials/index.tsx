fcff5d962181e98a65e9f009fccdbf4d42936799
Kendall Strautman <kendallstrautman@gmail.com>
2022-03-17 10:24:24 -0700

    feat: adds vault routes

---
+++ src/pages/vault/tutorials/index.tsx
@@ -0,0 +1,24 @@
+import { ProductOption } from 'lib/learn-client/types'
+import {
+  getProductTutorialsPageProps,
+  ProductTutorialsPageProps,
+} from 'views/product-tutorials-view/server'
+import ProductTutorialsView from 'views/product-tutorials-view'
+import BaseLayout from 'layouts/base-new'
+
+export function WaypointTutorialHubPage(
+  props: ProductTutorialsPageProps
+): React.ReactElement {
+  return <ProductTutorialsView {...props} />
+}
+
+export async function getStaticProps(): Promise<{
+  props: ProductTutorialsPageProps
+}> {
+  const props = await getProductTutorialsPageProps(ProductOption['vault'])
+
+  return props
+}
+
+WaypointTutorialHubPage.layout = BaseLayout
+export default WaypointTutorialHubPage
