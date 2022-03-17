d809aa7690ae873afb4cb1d82baaf3ade691d48e
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-03-17 13:45:00 -0400

    Renaming sidebar-filter-input style file

--- src/components/sidebar/components/sidebar-filter-input/index.tsx
+++ src/components/sidebar/components/sidebar-filter-input/index.tsx
@@ -1,7 +1,7 @@
 import { ChangeEvent, useRef } from 'react'
 import { IconFilter16 } from '@hashicorp/flight-icons/svg-react/filter-16'
 import { IconX16 } from '@hashicorp/flight-icons/svg-react/x-16'
-import s from './style.module.css'
+import s from './sidebar-filter-input.module.css'
 
 interface FilterInputProps {
   value: string
