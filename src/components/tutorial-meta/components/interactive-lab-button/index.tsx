51a92c6d15b58a510a7242df516553c00798f695
Kendall Strautman <kendallstrautman@gmail.com>
2022-03-21 15:23:43 -0700

    style: adds full button styles

--- src/components/tutorial-meta/components/interactive-lab-button/index.tsx
+++ src/components/tutorial-meta/components/interactive-lab-button/index.tsx
@@ -1,3 +1,4 @@
+import { IconTerminalScreen16 } from '@hashicorp/flight-icons/svg-react/terminal-screen-16'
 import s from './interactive-lab-button.module.css'
 
 interface InteractiveLabButtonProps {
@@ -10,5 +11,10 @@ export default function InteractiveLabButton({
   if (!showButton) {
     return null
   }
-  return <button className={s.labButton}>Show Terminal</button>
+  return (
+    <button className={s.labButton}>
+      <IconTerminalScreen16 className={s.terminalIcon} />
+      <span>Show Terminal</span>
+    </button>
+  )
 }
