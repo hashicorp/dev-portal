1ad3ee329b70f718f2ca41fe3cdc4f97a3498b00
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-03-23 17:03:59 -0400

    Wrapping author primitive imports with dynamic

--- src/views/docs-view/index.tsx
+++ src/views/docs-view/index.tsx
@@ -1,22 +1,43 @@
 import { ReactElement } from 'react'
+import dynamic from 'next/dynamic'
 import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote'
 import { useCurrentProduct } from 'contexts'
 import defaultMdxComponents from 'layouts/sidebar-sidecar/utils/_local_platform-docs-mdx'
 import SidebarSidecarLayout from 'layouts/sidebar-sidecar'
 
 // Author primitives
-import Badge from 'components/author-primitives/packer/badge'
-import BadgesHeader from 'components/author-primitives/packer/badges-header'
-import Button from '@hashicorp/react-button'
-import Checklist from 'components/author-primitives/packer/checklist'
-import Columns from 'components/author-primitives/vault/columns'
-import ConfigEntryReference from 'components/author-primitives/consul/config-entry-reference'
-import InlineTag from 'components/author-primitives/vault/inline-tag'
-import NestedNode from 'components/author-primitives/waypoint/nested-node'
-import Placement from 'components/author-primitives/shared/placement-table'
-import PluginBadge from 'components/author-primitives/packer/plugin-badge'
-import ProviderTable from 'components/author-primitives/terraform/provider-table'
-import SentinelEmbedded from '@hashicorp/react-sentinel-embedded'
+const Badge = dynamic(() => import('components/author-primitives/packer/badge'))
+const BadgesHeader = dynamic(
+  () => import('components/author-primitives/packer/badges-header')
+)
+const Button = dynamic(() => import('@hashicorp/react-button'))
+const Checklist = dynamic(
+  () => import('components/author-primitives/packer/checklist')
+)
+const Columns = dynamic(
+  () => import('components/author-primitives/vault/columns')
+)
+const ConfigEntryReference = dynamic(
+  () => import('components/author-primitives/consul/config-entry-reference')
+)
+const InlineTag = dynamic(
+  () => import('components/author-primitives/vault/inline-tag')
+)
+const NestedNode = dynamic(
+  () => import('components/author-primitives/waypoint/nested-node')
+)
+const Placement = dynamic(
+  () => import('components/author-primitives/shared/placement-table')
+)
+const PluginBadge = dynamic(
+  () => import('components/author-primitives/packer/plugin-badge')
+)
+const ProviderTable = dynamic(
+  () => import('components/author-primitives/terraform/provider-table')
+)
+const SentinelEmbedded = dynamic(
+  () => import('@hashicorp/react-sentinel-embedded')
+)
 
 const productsToAdditionalComponents = {
   consul: { ConfigEntryReference },
