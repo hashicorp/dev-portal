a1c481584615212c92d5c282ae713b5f8a1b8edd
Dylan Staley <88163+dstaley@users.noreply.github.com>
2022-03-24 10:45:47 -0700

    fix: replace next/head usage with HashiHead

--- src/pages/_proxied-dot-io/boundary/community/index.jsx
+++ src/pages/_proxied-dot-io/boundary/community/index.jsx
@@ -1,15 +1,16 @@
+import HashiHead from '@hashicorp/react-head'
 import BoundaryIoLayout from 'layouts/_proxied-dot-io/boundary'
 import VerticalTextBlockList from '@hashicorp/react-vertical-text-block-list'
 import SectionHeader from '@hashicorp/react-section-header'
-import Head from 'next/head'
 import s from './style.module.css'
 
 function CommunityPage() {
   return (
     <div className={s.root}>
-      <Head>
-        <title key="title">Community | Boundary by HashiCorp</title>
-      </Head>
+      <HashiHead
+        title="Community | Boundary by HashiCorp"
+        pageName="Community | Boundary by HashiCorp"
+      />
       <SectionHeader
         headline="Community"
         description="Boundary is a newly-launched open source project. The project team depends on the community’s engagement and feedback. Get involved today."
@@ -20,13 +21,11 @@ function CommunityPage() {
         data={[
           {
             header: 'Community Forum',
-            body:
-              '<a href="https://discuss.hashicorp.com/c/boundary">Boundary Community Forum</a>',
+            body: '<a href="https://discuss.hashicorp.com/c/boundary">Boundary Community Forum</a>',
           },
           {
             header: 'Bug Tracker',
-            body:
-              '<a href="https://github.com/hashicorp/boundary/issues">Issue tracker on GitHub</a>. Please only use this for reporting bugs. Do not ask for general help here; use the Community Form for that.',
+            body: '<a href="https://github.com/hashicorp/boundary/issues">Issue tracker on GitHub</a>. Please only use this for reporting bugs. Do not ask for general help here; use the Community Form for that.',
           },
         ]}
       />
