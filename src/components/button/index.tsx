e6fc66dbe879d92205b550015c0e3df0362ad132
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-03-24 10:52:10 -0400

    Handling icon and size props

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,10 +1,24 @@
-import s from './button.module.css'
 import { ButtonProps } from './types'
+import s from './button.module.css'
+import classNames from 'classnames'
 
-const Button = ({ onClick, text, type = 'button' }: ButtonProps) => {
+const Button = ({
+  icon,
+  iconPosition = 'leading',
+  onClick,
+  size = 'medium',
+  text,
+  type = 'button',
+}: ButtonProps) => {
+  const className = classNames(s.root, s[size])
+  const hasLeadingIcon = icon && iconPosition === 'leading'
+  const hasTrailingIcon = icon && iconPosition === 'trailing'
+
   return (
-    <button className={s.root} onClick={onClick} type={type}>
-      {text}
+    <button className={className} onClick={onClick} type={type}>
+      {hasLeadingIcon && icon}
+      <span className={s.text}>{text}</span>
+      {hasTrailingIcon && icon}
     </button>
   )
 }
--- src/components/button/types.ts
+++ src/components/button/types.ts
@@ -1,5 +1,10 @@
+import { ReactElement } from 'react'
+
 type ButtonElementProps = JSX.IntrinsicElements['button']
 
 export type ButtonProps = Pick<ButtonElementProps, 'onClick' | 'type'> & {
+  icon?: ReactElement
+  iconPosition?: 'leading' | 'trailing'
+  size?: 'small' | 'medium' | 'large'
   text: string
 }
