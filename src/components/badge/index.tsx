3f529c38514ef73f52d9affe9e184d323bb7b8a1
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-03-25 09:23:12 -0400

    Adding check for icon on status badges

--- src/components/badge/index.tsx
+++ src/components/badge/index.tsx
@@ -17,6 +17,14 @@ const Badge = ({
   const hasText = !!text
   const hasLabel = !!ariaDescribedBy || !!ariaLabel || !!ariaLabelledBy
   const isIconOnly = hasIcon && !hasText
+  const isStatusBadge =
+    color == 'success' || color == 'warning' || color == 'error'
+
+  if (isStatusBadge && !hasIcon) {
+    throw new Error(
+      '`Badge`s used for communicating status must have an icon to avoid relying on color alone.'
+    )
+  }
 
   if (!hasIcon && !hasText) {
     throw new Error(
