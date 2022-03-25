0a8d116dd282417f93f20437f477d6f38ec32157
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-03-25 10:39:34 -0400

    Adding CountBadge

---
+++ src/components/count-badge/index.tsx
@@ -0,0 +1,26 @@
+import classNames from 'classnames'
+import Badge from 'components/badge'
+import { CountBadgeProps } from './types'
+import s from './badge-count.module.css'
+
+const CountBadge = ({
+  color = 'neutral',
+  size = 'medium',
+  text,
+  type = 'filled',
+}: CountBadgeProps) => {
+  const classes = classNames(s.root, s[size])
+
+  return (
+    <Badge
+      className={classes}
+      color={color}
+      size={size}
+      text={text}
+      type={type}
+    />
+  )
+}
+
+export type { CountBadgeProps }
+export default CountBadge
