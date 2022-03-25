93f258b7a904da9a9437015b75e37da85002e871
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-03-24 23:22:45 -0400

    Adding checks for accessible labels

--- src/components/badge/index.tsx
+++ src/components/badge/index.tsx
@@ -13,6 +13,29 @@ const Badge = ({
   type = 'filled',
 }: BadgeProps) => {
   const className = classNames(s.root, s[size], s[`${type}-${color}`])
+  const hasIcon = !!icon
+  const hasText = !!text
+  const hasLabel = !!ariaDescribedBy || !!ariaLabel || !!ariaLabelledBy
+  const isIconOnly = hasIcon && !hasText
+
+  if (!hasIcon && !hasText) {
+    throw new Error(
+      '`Badge` must have either `text` or an `icon` with accessible labels.'
+    )
+  }
+
+  if (isIconOnly && !hasLabel) {
+    throw new Error(
+      'Icon-only `Badge`s require an accessible label. Either provide the `text` prop or one of: `ariaLabel`, `ariaLabelledBy`, `ariaDescribedBy`.'
+    )
+  }
+
+  if (ariaLabel && ariaLabelledBy) {
+    throw new Error(
+      '`Badge` does not accept both `ariaLabel` and `ariaLabelledBy`. Only provide one or the other.'
+    )
+  }
+
   return (
     <span
       aria-describedby={ariaDescribedBy}
