7a886b0ff7b3d759419d69592df3f0346a1ddc27
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-03-25 09:49:51 -0400

    Filling in props descriptions

--- src/components/badge/types.ts
+++ src/components/badge/types.ts
@@ -27,7 +27,8 @@ export interface BadgeProps {
   ariaLabelledBy?: string
 
   /**
-   * TODO: document other props
+   * The name of the color to apply styles to the badge. The default value is
+   * "neutral".
    */
   color?:
     | 'error'
@@ -36,8 +37,43 @@ export interface BadgeProps {
     | 'neutral'
     | 'success'
     | 'warning'
+
+  /**
+   * An icon from `@hashicorp/flight-icons` to render.
+   *
+   * Example:
+   *
+   * ```jsx
+   * import { IconAlertTriangle16 } from '@hashicorp/flight-icons/svg-react/alert-triangle-16'
+   *
+   * const MyComponent = () => {
+   *  return (
+   *    <Badge
+   *      color="warning"
+   *      icon={<IconAlertTriangle16 />}
+   *      text="Cost estimated"
+   *    />
+   *  )
+   * }
+   * ```
+   */
   icon?: ReactElement
+
+  /**
+   * The size of the badge, which mainly affects font size and padding. The
+   * default value is "medium".
+   */
   size?: 'small' | 'medium' | 'large'
+
+  /**
+   * The text to render inside of the badge. This is not required for icon-only
+   * badges.
+   */
   text?: string
+
+  /**
+   * The kind of style to apply to the badge, which mainly affects background
+   * color and border.
+   */
   type?: 'filled' | 'inverted' | 'outlined'
 }
