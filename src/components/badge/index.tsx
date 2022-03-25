c56178b1ad864f7812ccc8f997ce27ec08b9f37d
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-03-24 23:18:36 -0400

    Adding labeling props

--- src/components/badge/index.tsx
+++ src/components/badge/index.tsx
@@ -3,6 +3,9 @@ import { BadgeProps } from './types'
 import s from './badge.module.css'
 
 const Badge = ({
+  ariaDescribedBy,
+  ariaLabel,
+  ariaLabelledBy,
   color = 'neutral',
   icon,
   size = 'medium',
@@ -11,7 +14,12 @@ const Badge = ({
 }: BadgeProps) => {
   const className = classNames(s.root, s[size], s[`${type}-${color}`])
   return (
-    <span className={className}>
+    <span
+      aria-describedby={ariaDescribedBy}
+      aria-label={ariaLabel}
+      aria-labelledby={ariaLabelledBy}
+      className={className}
+    >
       {icon}
       {text && <span>{text}</span>}
     </span>
--- src/components/badge/types.ts
+++ src/components/badge/types.ts
@@ -1,6 +1,34 @@
 import { ReactElement } from 'react'
 
 export interface BadgeProps {
+  /**
+   * The value of the `id` of the element that describes the badge. The
+   * describing element does not have to be visible. If there are multiple
+   * labeling elements, this can be be a comma-separated list of `id`s.
+   *
+   * See: https://www.w3.org/TR/wai-aria-1.2/#aria-describedby
+   */
+  ariaDescribedBy?: string
+
+  /**
+   * A non-visual label accessible and descriptive label for the badge.
+   *
+   * See: https://www.w3.org/TR/wai-aria-1.2/#aria-label
+   */
+  ariaLabel?: string
+
+  /**
+   * The value of the `id` of the element that labels the badge. The labeling
+   * element does not have to be visible. If there are multiple labeling
+   * elements, this can be be a comma-separated list of `id`s.
+   *
+   * See: https://www.w3.org/TR/wai-aria-1.2/#aria-labelledby
+   */
+  ariaLabelledBy?: string
+
+  /**
+   * TODO: document other props
+   */
   color?:
     | 'error'
     | 'highlight'
