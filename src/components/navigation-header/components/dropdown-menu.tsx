13d2ae645d6a1e612b0632b6ed7e7f666b887c58
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-03-29 17:41:44 -0400

    Styling activator button

--- src/components/navigation-header/components/dropdown-menu.tsx
+++ src/components/navigation-header/components/dropdown-menu.tsx
@@ -8,17 +8,29 @@ const NavigationHeaderDropdownMenu = () => {
 
   return (
     <div className={s.root}>
-      <button
-        aria-expanded={isOpen}
-        className={s.activator}
-        onClick={() => setIsOpen(!isOpen)}
+      <div className={s.activatorWrapper}>
+        <button
+          aria-expanded={isOpen}
+          className={s.activator}
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <Text
+            asElement="span"
+            className={s.activatorText}
+            size={200}
+            weight="medium"
+          >
+            Documentation
+          </Text>
+          <IconChevronDown16 className={s.activatorIcon} />
+        </button>
+      </div>
+      <ul
+        className={s.dropdownList}
+        style={{ display: isOpen ? 'block' : 'none' }}
       >
-        <Text asElement="span" className={s.activatorText}>
-          activator text
-        </Text>
-        <IconChevronDown16 className={s.activatorIcon} />
-      </button>
-      <ul style={{ display: isOpen ? 'block' : 'none' }}>menu items</ul>
+        menu items
+      </ul>
     </div>
   )
 }
