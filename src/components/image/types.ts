c1136cd892ddc79f95de90d52c2397e878eb2760
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-03-29 15:58:34 -0400

    style: whitespace in types.ts files
    Co-authored-by: Ashlee M Boyer <43934258+ashleemboyer@users.noreply.github.com>

--- src/components/image/types.ts
+++ src/components/image/types.ts
@@ -3,6 +3,7 @@ export interface ImageProps {
    * Source attribute for the image tag.
    */
   src: JSX.IntrinsicElements['img']['src']
+
   /**
    * Alternate text for the image tag. Should be a text equivalent to the
    * information conveyed in the image. As an example, the alternate text
@@ -12,16 +13,19 @@ export interface ImageProps {
    * experience for all users regardless of ability.
    */
   alt: JSX.IntrinsicElements['img']['src']
+
   /**
    * Title attribute for the image. Typically only used if the image
    * is nested in an <a /> tag and needs to serve as text for the link.
    */
   title?: JSX.IntrinsicElements['img']['title']
+
   /**
    * Avoids adding margin to the component.
    * For use in other elements, such as ImageConfig
    */
   noMargin?: boolean
+
   /**
    * Avoids adding border to the component.
    * For use in other elements, such as ImageConfig
