4f5f48d4137567bf94ab49685b8002e993464fef
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-03-31 17:23:04 -0400

    docs: add link to ReactPlayerProps docs

--- src/components/video-embed/types.ts
+++ src/components/video-embed/types.ts
@@ -1,5 +1,9 @@
 import { ReactPlayerProps } from 'react-player'
 
+/**
+ * For ReactPlayerProps documentation, see:
+ * https://github.com/CookPete/react-player#props
+ */
 export interface VideoEmbedProps extends ReactPlayerProps {
   /**
    * Optional starting time for the video. Works with YouTube and Wistia video URLS.
