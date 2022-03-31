ed42f26537f191181552a0dfdb7350e2d20d0508
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-03-31 10:36:20 -0400

    refactor: make image factory

--- src/layouts/sidebar-sidecar/utils/_local_platform-docs-mdx/index.tsx
+++ src/layouts/sidebar-sidecar/utils/_local_platform-docs-mdx/index.tsx
@@ -9,6 +9,7 @@ import Tabs, { Tab } from 'components/tabs'
 import Text from 'components/text'
 import Image from 'components/image'
 import devDotStyles from 'components/dev-dot-content/dev-dot-content.module.css'
+import { ImageProps } from 'components/image/types'
 
 /**
  * Used by `makeHeadingElement`.
@@ -64,6 +65,22 @@ function makeHeadingElement(level, props) {
   return <Heading {...passableProps} />
 }
 
+/**
+ * Returns the Image component configured with specific default behaviour.
+ *
+ * In /docs, we want to hide image borders by default for now,
+ * to match existing behaviour. Note that in /tutorials, we want
+ * to show image borders by default. Later we may adjust these
+ * defaults; it would likely be ideal for /docs and /tutorials
+ * to have the same default behaviour.
+ */
+function makeImageElement({ noBorder }: { noBorder: ImageProps['noBorder'] }) {
+  // eslint-disable-next-line react/display-name
+  return ({ alt, src, title }: Pick<ImageProps, 'alt' | 'src' | 'title'>) => (
+    <Image alt={alt} src={src} title={title} noBorder={noBorder} />
+  )
+}
+
 // Purely for sharing between the two functions. Once `createMdxProvider` is
 // deprecated, this can be moved inline.
 function _defaultComponents() {
@@ -78,16 +95,7 @@ function _defaultComponents() {
     CodeTabs,
     pre,
     a: DocsAnchor,
-    /**
-     * In /docs, we want to hide image borders by default for now,
-     * to match existing behaviour. Note that in /tutorials, we want
-     * to show image borders by default. Later we may adjust these
-     * defaults; it would likely be ideal for /docs and /tutorials
-     * to have the same default behaviour.
-     */
-    img: ({ alt, src, title }: JSX.IntrinsicElements['img']) => (
-      <Image alt={alt} src={src} title={title} noBorder={true} />
-    ),
+    img: makeImageElement({ noBorder: true }),
     h1: (props) => makeHeadingElement(1, props),
     h2: (props) => makeHeadingElement(2, props),
     h3: (props) => makeHeadingElement(3, props),
