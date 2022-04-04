414e2fe10a70c95253f39f38fb35542482b80b88
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-04-04 13:34:50 -0400

    chore: scope out work to be done

--- src/components/dev-dot-content/index.tsx
+++ src/components/dev-dot-content/index.tsx
@@ -2,12 +2,17 @@ import { ReactElement } from 'react'
 import classNames from 'classnames'
 import { DevDotContentProps } from './types'
 import s from './dev-dot-content.module.css'
+import sAlerts from './custom-alert-boxes.module.css'
 
 const DevDotContent = ({
   className,
   children,
 }: DevDotContentProps): ReactElement => {
-  return <div className={classNames(s.root, className)}>{children}</div>
+  return (
+    <div className={classNames(s.root, sAlerts.root, className)}>
+      {children}
+    </div>
+  )
 }
 
 export default DevDotContent
