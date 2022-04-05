a4048fe048b6f491423c949637f2836078b3a904
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-04-05 17:51:54 -0400

    Adding shouldListen arg to useOnFocusOutside

--- src/hooks/use-on-focus-outside.js
+++ src/hooks/use-on-focus-outside.js
@@ -3,15 +3,18 @@ import { useEffect } from 'react'
 /**
  * Hook that runs a callback on focus outside the ref
  */
-function useOnFocusOutside(ref, handler) {
+function useOnFocusOutside(ref, handler, shouldListen = true) {
   useEffect(() => {
     function handleFocusChange(event) {
       const isOutside = ref.current && !ref.current.contains(event.target)
       if (isOutside) handler(event)
     }
 
-    // Bind the event listener
-    document.addEventListener('focusin', handleFocusChange)
+    // Bind the event listener if the listener should be applied via `shouldListen`
+    if (shouldListen) {
+      document.addEventListener('focusin', handleFocusChange)
+    }
+
     return () => {
       // Unbind the event listener on clean up
       document.removeEventListener('focusin', handleFocusChange)
