483c30e1db552c92d98fb2a440667d5c82f29428
Kendall Strautman <36613477+kendallstrautman@users.noreply.github.com>
2022-04-05 12:15:26 -0700

    refactor: use button component (#253)
    * refactor: use button component
    
    * style: adjust button primary box-shadow
    
    * chore: remove default props

--- src/components/tutorial-meta/components/interactive-lab-button/index.tsx
+++ src/components/tutorial-meta/components/interactive-lab-button/index.tsx
@@ -1,8 +1,6 @@
 import { IconTerminalScreen16 } from '@hashicorp/flight-icons/svg-react/terminal-screen-16'
+import Button from 'components/button'
 import { useInstruqtEmbed } from 'contexts/instruqt-lab'
-import s from './interactive-lab-button.module.css'
-
-// @TODO replace with Button component once implemented
 
 export default function InteractiveLabButton() {
   const ctx = useInstruqtEmbed()
@@ -14,9 +12,10 @@ export default function InteractiveLabButton() {
   const buttonText = `${ctx.active ? 'Hide' : 'Show'} Terminal`
 
   return (
-    <button className={s.labButton} onClick={() => ctx.setActive(!ctx.active)}>
-      <IconTerminalScreen16 className={s.terminalIcon} />
-      <span>{buttonText}</span>
-    </button>
+    <Button
+      text={buttonText}
+      onClick={() => ctx.setActive(!ctx.active)}
+      icon={<IconTerminalScreen16 />}
+    />
   )
 }
