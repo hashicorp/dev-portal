9cf3a6e0ad5a8611ffe159bf32a869ab2c9fa2a1
Kendall Strautman <kendallstrautman@gmail.com>
2022-04-05 09:24:12 -0700

    style: adjust button primary box-shadow

--- src/components/tutorial-meta/components/interactive-lab-button/index.tsx
+++ src/components/tutorial-meta/components/interactive-lab-button/index.tsx
@@ -2,10 +2,6 @@ import { IconTerminalScreen16 } from '@hashicorp/flight-icons/svg-react/terminal
 import Button from 'components/button'
 import { useInstruqtEmbed } from 'contexts/instruqt-lab'
 
-/**@TODO this button shows a drop-shadow elevation in the design,
- * adjust button to support or chat with design
- * */
-
 export default function InteractiveLabButton() {
   const ctx = useInstruqtEmbed()
 
