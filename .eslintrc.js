a8316dcd767593e25bd034f0be600ba7f4ad8d2b
Bryce Kalow <bkalow@hashicorp.com>
2022-04-05 16:42:16 -0500

    use eslint parser from eslint-config-next, adjust ts rules to overrides

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,24 @@
 module.exports = {
   root: true,
   extends: './node_modules/@hashicorp/platform-cli/config/.eslintrc.js',
+
   rules: {
     curly: 'error',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/typedef': [
-      'warn',
-      {
-        arrowParameter: true,
-        parameter: true,
-      },
-    ],
   },
+
+  overrides: [
+    {
+      files: ['**/*.ts?(x)'],
+      rules: {
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/typedef': [
+          'warn',
+          {
+            arrowParameter: true,
+            parameter: true,
+          },
+        ],
+      },
+    },
+  ],
 }
