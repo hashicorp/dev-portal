8198b897c0dc82b2fdfc5af88857ff6a017e3673
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-04-05 14:48:25 -0400

    fix: use consistent icon size

--- src/views/tutorial-view/components/next-previous/components/directional-link-box/index.tsx
+++ src/views/tutorial-view/components/next-previous/components/directional-link-box/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import classNames from 'classnames'
 import { IconArrowRight16 } from '@hashicorp/flight-icons/svg-react/arrow-right-16'
 import { IconArrowLeft16 } from '@hashicorp/flight-icons/svg-react/arrow-left-16'
-import { IconSliders24 } from '@hashicorp/flight-icons/svg-react/sliders-24'
+import { IconSliders16 } from '@hashicorp/flight-icons/svg-react/sliders-16'
 import CardLink from 'components/card-link'
 import Text from 'components/text'
 import { DirectionalLinkBoxProps, DirectionOption } from './types'
@@ -11,7 +11,7 @@ import s from './directional-link-box.module.css'
 const IconDict: { [k in DirectionOption]: typeof IconArrowRight16 } = {
   next: IconArrowRight16,
   previous: IconArrowLeft16,
-  final: IconSliders24,
+  final: IconSliders16,
 }
 
 function DirectionalLinkBox({
