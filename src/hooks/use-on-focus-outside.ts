34f468d01bc8702472eb3278db3bd813b7489472
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-04-06 10:34:30 -0400

    Aligning useOnFocusOutside more closely with useOnClickOutside

--- src/hooks/use-on-focus-outside.ts
+++ src/hooks/use-on-focus-outside.ts
@@ -1,30 +1,37 @@
-import { useEffect } from 'react'
+import { MutableRefObject, useEffect } from 'react'
 
 /**
  * Hook that runs a callback on focus outside the ref
  */
-function useOnFocusOutside(refs, handler, shouldListen = true) {
+export default function useOnFocusOutside(
+  refs: MutableRefObject<HTMLElement>[],
+  handler: (event?: FocusEvent) => void,
+  shouldListen: boolean = true
+) {
   useEffect(() => {
-    function handleFocusChange(event) {
-      const isFocusInside = refs.some((ref) =>
+    const listener = (event: FocusEvent) => {
+      // See if any provided refs had a focus event inside of them
+      const isFocusInside = refs.some((ref: MutableRefObject<HTMLElement>) =>
         ref?.current?.contains(event.target as Node)
       )
+
+      // Do nothing if the focus was inside any provided ref or its descendants
       if (isFocusInside) {
         return
       }
+
+      // Invoke handler if the focus was outside all provided refs
       handler(event)
     }
 
-    // Bind the event listener
+    // Listen for `focusin` events if `shouldListen` is true
     if (shouldListen) {
-      document.addEventListener('focusin', handleFocusChange)
+      document.addEventListener('focusin', listener)
     }
 
+    // Remove listener upon unmounting
     return () => {
-      // Unbind the event listener on clean up
-      document.removeEventListener('focusin', handleFocusChange)
+      document.removeEventListener('focusin', listener)
     }
   }, [refs, handler, shouldListen])
 }
-
-export default useOnFocusOutside
