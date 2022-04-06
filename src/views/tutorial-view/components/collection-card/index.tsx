254078f9fc0b49b6e8c228bf711f19d43c88a425
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-04-06 16:26:15 -0400

    feat: add comment re: getCollectionSlug

--- src/views/tutorial-view/components/collection-card/index.tsx
+++ src/views/tutorial-view/components/collection-card/index.tsx
@@ -8,6 +8,11 @@ import s from './collection-card.module.css'
  * TODO: styles and functionality dependent on design spec being finalized.
  * (currently "WIP" in Figma)
  * Figma: https://www.figma.com/file/VD7ahvXuXWJApeGnhbW4hv/Dev-Portal?node-id=6860%3A79233
+ *
+ * TODO: note as well that not all collection links will work, for now.
+ * This is because not all product collections are in dev-dot, yet.
+ * Asana task to address:
+ * https://app.asana.com/0/1201987349274776/1201991101510148
  */
 function CollectionCard({
   className,
