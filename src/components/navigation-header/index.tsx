b117a237fa0213372e43b2e4d26e8f80f64ebc17
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-04-06 20:49:47 -0400

    Stubbing out new NavigationHeader

--- src/components/navigation-header/index.tsx
+++ src/components/navigation-header/index.tsx
@@ -1,76 +1,29 @@
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import HashiCorpLogo from '@hashicorp/mktg-logos/corporate/hashicorp/logomark/white.svg?include'
-import InlineSvg from '@hashicorp/react-inline-svg'
+import { ReactNode } from 'react'
 import { useCurrentProduct } from 'contexts'
-import HeaderSearchInput from 'components/header-search-input'
-import ProductSwitcher from 'components/product-switcher'
+import useCurrentPath from 'hooks/use-current-path'
 import { NavigationHeaderItem } from './types'
 import s from './navigation-header.module.css'
 
-/**
- * Checks if a header navigation link's path matches the current route's path.
- * Useful for setting the `aria-current` property on <a> elements in the nav,
- * which is used as a CSS selector for applying active styles to links in the
- * header.
- */
-const isCurrentPage = (pagePath: string, currentPath: string): boolean => {
-  const currentPathSplit = currentPath.split('/')
-  const currentProductSlug = currentPathSplit[1]
-  const currentProductSubpage = currentPathSplit[2]
-
-  if (currentProductSubpage) {
-    return pagePath === `/${currentProductSlug}/${currentProductSubpage}`
-  } else if (currentProductSlug) {
-    return pagePath === `/${currentProductSlug}`
-  } else {
-    return pagePath === '/'
-  }
-}
-
 const NavigationHeader = () => {
-  const router = useRouter()
-  const currentPath = router.asPath
+  const currentPath = useCurrentPath({ excludeHash: true, excludeSearch: true })
   const currentProduct = useCurrentProduct()
-  const navItems = currentProduct?.navigationHeaderItems || []
-  const hasNavigationItems = navItems.length > 0
 
-  return (
+  const Header = ({ children }: { children: ReactNode }) => (
     <header className={s.navigationHeader}>
-      <nav>
-        <div className={s.headerLeft}>
-          <Link href="/">
-            <a>
-              <InlineSvg className={s.siteLogo} src={HashiCorpLogo} />
-            </a>
-          </Link>
-          <ProductSwitcher />
-        </div>
-        {hasNavigationItems && (
-          <div className={s.headerRight}>
-            <ul className={s.navLinks}>
-              {navItems.map((navLink: NavigationHeaderItem) => {
-                const isCurrent = isCurrentPage(navLink.path, currentPath)
-                return (
-                  <li className={s.navLinksListItem} key={navLink.id}>
-                    <Link href={navLink.path}>
-                      <a
-                        aria-current={isCurrent ? 'page' : undefined}
-                        className={s.navLinksAnchor}
-                      >
-                        {navLink.label}
-                      </a>
-                    </Link>
-                  </li>
-                )
-              })}
-            </ul>
-            <HeaderSearchInput theme="dark" />
-          </div>
-        )}
-      </nav>
+      <nav>{children}</nav>
     </header>
   )
+
+  if (currentPath === '/') {
+    return (
+      <Header>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img alt="" src="https://via.placeholder.com/232x32?text=LOGO" />
+      </Header>
+    )
+  } else {
+    return <Header>{`WIP: ${currentProduct.name} Header`}</Header>
+  }
 }
 
 export type { NavigationHeaderItem }
