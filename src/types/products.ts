84b546cf3722d4ff61588ace2957375f6c8b7548
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-04-07 14:53:14 -0400

    Adding LearnProductData interface

--- src/types/products.ts
+++ src/types/products.ts
@@ -1,4 +1,5 @@
 import { ProductMeta, Products } from '@hashicorp/platform-product-meta'
+import { ProductOption } from 'lib/learn-client/types'
 import { MenuItem } from 'components/sidebar'
 import { NavigationHeaderItem } from 'components/navigation-header'
 
@@ -21,6 +22,15 @@ interface Product extends ProductMeta {
   slug: ProductSlug
 }
 
+/**
+ * Learn does not support all of the products in `ProductSlug`, so this is the
+ * interface almost the same as `ProductData`, just with a limited set of
+ * options for `slug`.
+ */
+interface LearnProductData extends ProductData {
+  slug: ProductOption
+}
+
 interface ProductData extends Product {
   basePaths: string[]
   navigationHeaderItems: {
@@ -38,4 +48,11 @@ interface ProductData extends Product {
 
 type ProductGroup = Product[]
 
-export type { Product, ProductData, ProductGroup, ProductName, ProductSlug }
+export type {
+  LearnProductData,
+  Product,
+  ProductData,
+  ProductGroup,
+  ProductName,
+  ProductSlug,
+}
