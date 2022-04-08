109d90bb13f098791e0c9e07ce115dae890e5f66
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-04-07 20:16:36 -0400

    feat: implement InteractiveLabCallout component (#273)
    * feat: implement InteractiveLabCallout component
    
    * docs: remove comment and minor-jank-fix line-height
    
    * fix: tweak button size to match spec
    
    * fix: line-height issue
    
    * docs: make task for todo comment
    
    * docs: add comments on !important
    
    * docs: fix typo
    
    Co-authored-by: Ashlee M Boyer <43934258+ashleemboyer@users.noreply.github.com>
    
    * fix: remove unecessary !important
    
    * docs: add comment on data-heap-track
    
    * refactor: rename interactive-lab-callout
    
    * fix: use Card in callout, add surface-low support to Card
    
    * fix: use dev-dot media query
    
    * fix: use base elevation for card
    
    * fix: remove min-height from button, tweak padding
    
    Co-authored-by: Ashlee M Boyer <43934258+ashleemboyer@users.noreply.github.com>

---
+++ src/components/interactive-lab-callout/index.tsx
@@ -0,0 +1,46 @@
+import Image from 'next/image'
+import Button from 'components/button'
+import Card from 'components/card'
+import { useInstruqtEmbed } from 'contexts/instruqt-lab'
+import s from './interactive-lab-callout.module.css'
+
+export default function InteractiveLabCallout() {
+  const ctx = useInstruqtEmbed()
+
+  if (!ctx.labId) {
+    return null
+  }
+
+  return (
+    <Card className={s.interactiveCallout} elevation="base">
+      <div>
+        <p className={s.title}>Launch Terminal</p>
+        <p className={s.description}>
+          This tutorial includes a free interactive command-line lab that lets
+          you follow along on actual cloud infrastructure.
+        </p>
+        <div className={s.ctaButton}>
+          <Button
+            // TODO: <Button /> within dev-dot does not take "data-heap-track".
+            // Will handle in follow-up PR.
+            // https://app.asana.com/0/1201987349274776/1202096857327389/f
+            data-heap-track="InteractiveCalloutButton"
+            color="secondary"
+            text="Start interactive lab"
+            onClick={() => ctx.setActive(true)}
+            size="small"
+          />
+        </div>
+      </div>
+      <div className={s.image}>
+        <Image
+          src={require('./img/interactive-callout-visual.png')}
+          width={133}
+          height={100}
+          alt=""
+          layout="responsive"
+        />
+      </div>
+    </Card>
+  )
+}
