88880bdafef2c16a4dda29caae48353c339be539
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-04-12 16:21:34 -0400

    Placing sidebar heading classes in main stylesheet

--- src/components/sidebar/components/sidebar-section-heading/index.tsx
+++ src/components/sidebar/components/sidebar-section-heading/index.tsx
@@ -1,11 +1,11 @@
-import s from './sidebar-section-heading.module.css'
+import s from 'components/sidebar/sidebar.module.css'
 
 interface SidebarSectionHeadingProps {
   text: string
 }
 
 const SidebarSectionHeading = ({ text }: SidebarSectionHeadingProps) => {
-  return <h3 className={s.root}>{text}</h3>
+  return <h3 className={s.heading}>{text}</h3>
 }
 
 export default SidebarSectionHeading
