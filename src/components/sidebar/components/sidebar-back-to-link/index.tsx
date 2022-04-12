55046fad0ea43b263fae149a03304e459e42b59c
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-04-12 10:52:08 -0400

    Updating SidebarBackToLink styles

--- src/components/sidebar/components/sidebar-back-to-link/index.tsx
+++ src/components/sidebar/components/sidebar-back-to-link/index.tsx
@@ -1,8 +1,8 @@
 import { ReactElement } from 'react'
 import { IconArrowLeft16 } from '@hashicorp/flight-icons/svg-react/arrow-left-16'
+import StandaloneLink from 'components/standalone-link'
 import { SidebarBackToLinkProps } from './types'
 import s from './sidebar-back-to-link.module.css'
-import StandaloneLink from 'components/standalone-link'
 
 /**
  * TODO: Design is still planning this functionality & states. Since not all
@@ -15,6 +15,7 @@ const SidebarBackToLink = ({
 }: SidebarBackToLinkProps): ReactElement => {
   return (
     <StandaloneLink
+      className={s.root}
       href={url}
       icon={<IconArrowLeft16 className={s.icon} />}
       iconPosition="leading"
