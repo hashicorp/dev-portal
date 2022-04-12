edba0bf7c78bd2a71dd91ffdc095d0399c79117c
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-04-12 09:34:00 -0400

    chore: initial homepage setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,2 @@
-import { ReactElement } from 'react'
-import BaseNewLayout from 'layouts/base-new'
-import Heading from 'components/heading'
-import HomepageHero from 'components/homepage-hero'
-import ProductCardGrid from 'components/product-card-grid'
-import s from './index.module.css'
-
-function Homepage(): ReactElement {
-  return (
-    <div className={s.root}>
-      <HomepageHero className={s.hero} />
-      <Heading
-        className={s.cardGridHeading}
-        level={2}
-        size={400}
-        slug="explore-product-documentation"
-        weight="bold"
-      >
-        Explore Product Documentation
-      </Heading>
-      <ProductCardGrid className={s.cardGrid} />
-    </div>
-  )
-}
-
-Homepage.layout = BaseNewLayout
+import Homepage from 'views/homepage'
 export default Homepage
---
+++ src/views/homepage/index.tsx
@@ -0,0 +1,9 @@
+import { ReactElement } from 'react'
+import BaseNewLayout from 'layouts/base-new'
+
+function Homepage(): ReactElement {
+  return <>Homepage</>
+}
+
+Homepage.layout = BaseNewLayout
+export default Homepage
