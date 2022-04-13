d74c55bcfdc3295ce33842f95cf45128cfc90fc1
Kendall Strautman <kendallstrautman@gmail.com>
2022-04-13 15:47:07 -0700

    feat: adds regex to test for valid tutorials path

--- src/lib/__tests__/rewrite-tutorial-links.test.ts
+++ src/lib/__tests__/rewrite-tutorial-links.test.ts
@@ -10,22 +10,29 @@ import remark from 'remark'
 import { rewriteTutorialLinksPlugin } from 'lib/remark-plugins/rewrite-tutorial-links'
 
 // Matches the pattern /{beta-product}/tutorials/collection-slug/optional-tutorial-slug
-// @TODO load beta products from config, concat as string construction to dynamically load the products?
-const validDevPortalTutorialsPath = new RegExp(
-  /^\/(waypoint|vault)\/tutorials\/[a-z0-9]+(?:[-][a-z0-9]+)*(\/[a-z0-9]+(?:[-][a-z0-9]+)*)?/
+// @TODO load beta products from config
+const products = ['waypoint', 'vault']
+const slug = '[a-z0-9]+(?:[-][a-z0-9]+)*' // matches lower case letters, numbers and hyphens
+const devDotTutorialsPath = new RegExp(
+  `^/(${products.join('|')})/tutorials/${slug}(/${slug})?$`
 )
+const TEST_SLUGS = {
+  betaProductTutorial:
+    '[link to beta product tutorial](/tutorials/waypoint/get-started-ui)',
+}
 
 describe('rewriteTutorialLinks remark plugin', () => {
   test('Beta product tutorial links are rewritten to dev portal paths', async () => {
     // load beta product config
     const contents = await remark()
       .use(rewriteTutorialLinksPlugin)
-      .process(
-        '[link to beta product tutorial](/tutorials/waypoint/get-started-ui)'
-      )
+      .process(TEST_SLUGS.betaProductTutorial)
 
     const result = String(contents)
-    const isValidPath = validDevPortalTutorialsPath.test(result)
+    const path = result.split('(')[1].trim().slice(0, -1) // target the path within the md syntax
+
+    const isValidPath = devDotTutorialsPath.test(path)
+    console.log(path)
     expect(isValidPath).toEqual(true)
   })
 })
