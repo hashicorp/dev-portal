0f64597f5f6252ce6f3a19d1cc21e80fbb566417
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-04-13 12:55:24 -0400

    chore: make use of badge and cardlink

--- src/views/homepage/components/tutorial-card/index.tsx
+++ src/views/homepage/components/tutorial-card/index.tsx
@@ -5,6 +5,8 @@ import { productSlugs } from 'lib/products'
 import ProductIcon from 'components/product-icon'
 import { IconPlay16 } from '@hashicorp/flight-icons/svg-react/play-16'
 import { IconTerminalScreen16 } from '@hashicorp/flight-icons/svg-react/terminal-screen-16'
+import Badge from 'components/badge'
+import CardLink from 'components/card-link'
 import Heading from 'components/heading'
 import Text from 'components/text'
 import s from './tutorial-card.module.css'
@@ -39,36 +41,37 @@ export default function TutorialCard({
   badges,
 }: TutorialCardProps) {
   return (
-    <Link href={link}>
-      <a className={s.tutorialCard}>
-        {duration ? (
-          <Text className={s.duration} size={100} weight="medium">
-            {duration}
-          </Text>
-        ) : null}
-        <Heading
-          className={s.heading}
-          level={3}
-          size={200}
-          weight="semibold"
-          slug={heading}
-        >
-          {heading}
-        </Heading>
-        <Text className={s.description} size={100}>
-          {description}
+    <CardLink href="/" className={s.tutorialCard}>
+      {duration ? (
+        <Text className={s.duration} size={100} weight="medium">
+          {duration}
         </Text>
-        <ul className={s.badgesList}>
-          {badges.map((badge, index) => {
-            return (
-              <li key={index} className={s.badgesListItem}>
-                <VisuallyHidden>{badge}</VisuallyHidden>
-                <span className={s.badge}>{BADGE_ICON_MAP[badge]}</span>
-              </li>
-            )
-          })}
-        </ul>
-      </a>
-    </Link>
+      ) : null}
+      <Heading
+        className={s.heading}
+        level={3}
+        size={200}
+        weight="semibold"
+        slug={heading}
+      >
+        {heading}
+      </Heading>
+      <Text className={s.description} size={100}>
+        {description}
+      </Text>
+      <ul className={s.badgesList}>
+        {badges.map((badge, index) => {
+          return (
+            <li key={index} className={s.badgesListItem}>
+              <Badge
+                ariaLabel={badge}
+                icon={BADGE_ICON_MAP[badge]}
+                className={s.badge}
+              />
+            </li>
+          )
+        })}
+      </ul>
+    </CardLink>
   )
 }
