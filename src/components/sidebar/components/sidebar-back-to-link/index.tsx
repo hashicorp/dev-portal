27497e504a88b77463cebf9033ac9b94f0d5b200
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-04-13 13:34:46 -0400

    Wrapping SidebarBackToLink with div for styling

--- src/components/sidebar/components/sidebar-back-to-link/index.tsx
+++ src/components/sidebar/components/sidebar-back-to-link/index.tsx
@@ -4,23 +4,19 @@ import StandaloneLink from 'components/standalone-link'
 import { SidebarBackToLinkProps } from './types'
 import s from './sidebar-back-to-link.module.css'
 
-/**
- * TODO: Design is still planning this functionality & states. Since not all
- * cases are covered, we return a placeholder link if necessary props aren't
- * provided.
- */
 const SidebarBackToLink = ({
   text,
   url,
 }: SidebarBackToLinkProps): ReactElement => {
   return (
-    <StandaloneLink
-      className={s.root}
-      href={url}
-      icon={<IconArrowLeft16 className={s.icon} />}
-      iconPosition="leading"
-      text={text}
-    />
+    <div className={s.root}>
+      <StandaloneLink
+        href={url}
+        icon={<IconArrowLeft16 className={s.icon} />}
+        iconPosition="leading"
+        text={text}
+      />
+    </div>
   )
 }
 
