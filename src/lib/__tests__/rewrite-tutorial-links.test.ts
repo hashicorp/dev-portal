baabf82e693210ee1c80e1cc46fa5c69416f04d7
Kendall Strautman <kendallstrautman@gmail.com>
2022-04-13 16:38:51 -0700

    chore: use config products

--- src/lib/__tests__/rewrite-tutorial-links.test.ts
+++ src/lib/__tests__/rewrite-tutorial-links.test.ts
@@ -10,13 +10,13 @@ import remark from 'remark'
 import { rewriteTutorialLinksPlugin } from 'lib/remark-plugins/rewrite-tutorial-links'
 
 // Matches the pattern /{beta-product}/tutorials/collection-slug/optional-tutorial-slug
-// @TODO load beta products from config
-const products = ['waypoint', 'vault']
 const slug = '[a-z0-9]+(?:[-][a-z0-9]+)*' // matches lower case letters, numbers and hyphens
 const devDotTutorialsPath = new RegExp(
-  `^/(${products.join('|')})/tutorials/${slug}(/${slug})?$`
+  `^/(${__config.dev_dot.beta_product_slugs.join(
+    '|'
+  )})/tutorials/${slug}(/${slug})?$`
 )
-const TEST_SLUGS = {
+const TEST_MD_LINKS = {
   nonLearnLink: '[link to docs](https://www.waypointproject.io/docs)',
   betaProductTutorial:
     '[link to beta product tutorial](/tutorials/waypoint/get-started-ui)',
@@ -25,12 +25,12 @@ const TEST_SLUGS = {
 describe('rewriteTutorialLinks remark plugin', () => {
   test('Only internal Learn links are rewritten', async () => {
     const contentsWithoutPlugin = await remark().process(
-      TEST_SLUGS.nonLearnLink
+      TEST_MD_LINKS.nonLearnLink
     )
 
     const contentsWithPlugin = await remark()
       .use(rewriteTutorialLinksPlugin)
-      .process(TEST_SLUGS.nonLearnLink)
+      .process(TEST_MD_LINKS.nonLearnLink)
 
     expect(String(contentsWithPlugin)).toEqual(String(contentsWithoutPlugin))
   })
@@ -39,7 +39,7 @@ describe('rewriteTutorialLinks remark plugin', () => {
     // load beta product config
     const contents = await remark()
       .use(rewriteTutorialLinksPlugin)
-      .process(TEST_SLUGS.betaProductTutorial)
+      .process(TEST_MD_LINKS.betaProductTutorial)
 
     const result = String(contents)
     const path = result.split('(')[1].trim().slice(0, -1) // target the path within the md syntax
