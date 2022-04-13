17d6f9131067b7814d17b12faa4fb5c19ec16df1
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-04-13 16:57:51 -0400

    Updating SidebarNavLink to determine if external

--- src/components/sidebar/components/sidebar-nav-menu-item/index.tsx
+++ src/components/sidebar/components/sidebar-nav-menu-item/index.tsx
@@ -2,7 +2,7 @@ import { KeyboardEventHandler, useEffect, useRef, useState } from 'react'
 import Link from 'next/link'
 import { IconChevronRight16 } from '@hashicorp/flight-icons/svg-react/chevron-right-16'
 import { IconExternalLink16 } from '@hashicorp/flight-icons/svg-react/external-link-16'
-import MaybeInternalLink from 'components/maybe-internal-link'
+import isAbsoluteUrl from 'lib/is-absolute-url'
 import { MenuItem } from 'components/sidebar'
 import {
   SidebarHorizontalRule,
@@ -14,36 +14,21 @@ interface SidebarMenuItemProps {
   item: MenuItem
 }
 
-const SidebarNavLink = ({ item }) => {
-  if (item.fullPath) {
-    return (
-      <li>
-        <Link href={item.fullPath}>
-          <a
-            aria-current={item.isActive ? 'page' : undefined}
-            className={s.sidebarNavMenuItem}
-            // TODO: this might break some accessible labels, probably need aria-label
-            dangerouslySetInnerHTML={{ __html: item.title }}
-          />
-        </Link>
-      </li>
-    )
-  }
+const SidebarNavLink = ({ item }: { item: MenuItem }) => {
+  const href = item.fullPath || item.href
+  const isExternal = isAbsoluteUrl(href)
 
   return (
     <li>
-      <MaybeInternalLink
-        aria-current={item.isActive ? 'page' : undefined}
-        className={s.sidebarNavMenuItem}
-        href={item.href}
-      >
-        <span
-          className={s.navMenuItemLabel}
-          // TODO: this might break some accessible labels, probably need aria-label
-          dangerouslySetInnerHTML={{ __html: item.title }}
-        />
-        <IconExternalLink16 />
-      </MaybeInternalLink>
+      <Link href={href}>
+        <a aria-current={item.isActive} className={s.sidebarNavMenuItem}>
+          <span
+            className={s.navMenuItemLabel}
+            dangerouslySetInnerHTML={{ __html: item.title }}
+          />
+          {isExternal && <IconExternalLink16 />}
+        </a>
+      </Link>
     </li>
   )
 }
@@ -114,6 +99,7 @@ const SidebarNavMenuItem: React.FC<SidebarMenuItemProps> = ({ item }) => {
   if (item.divider) {
     return <SidebarHorizontalRule />
   }
+
   if (item.heading) {
     return <SidebarSectionHeading text={item.heading} />
   }
