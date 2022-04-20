ef49ea5737aa0658e080d99ac62b4c1ad068febc
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-04-20 10:39:53 -0400

    Renaming leadingIcon to leadingIconName

--- src/components/sidebar/helpers/generate-top-level-nav-items.ts
+++ src/components/sidebar/helpers/generate-top-level-nav-items.ts
@@ -4,7 +4,7 @@ import { productSlugsToNames } from 'lib/products'
 export const generateTopLevelNavItems = () => {
   const result = [
     {
-      leadingIcon: 'home',
+      leadingIconName: 'home',
       path: '/',
       title: 'Developer Home',
     },
@@ -16,10 +16,10 @@ export const generateTopLevelNavItems = () => {
   const betaProductSlugs = __config.dev_dot.beta_product_slugs
   betaProductSlugs.forEach((productSlug: ProductSlug) => {
     const title = productSlugsToNames[productSlug]
-    const leadingIcon = productSlug
+    const leadingIconName = productSlug
     const path = `/${productSlug}`
 
-    result.push({ leadingIcon, path, title })
+    result.push({ leadingIconName, path, title })
   })
 
   return result
