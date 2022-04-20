f243c16b995330a8709285ecec560bad01f69408
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-04-20 19:43:26 -0400

    Adjusting styles and using SidebarMobileControls

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,8 +1,6 @@
 import { ReactElement, useMemo, useState } from 'react'
+import { useDeviceSize } from 'contexts'
 import useCurrentPath from 'hooks/use-current-path'
-import Button from 'components/button'
-import SidebarBackToLink from './components/sidebar-back-to-link'
-import SidebarFilterInput from './components/sidebar-filter-input'
 import {
   SidebarNavLinkItem,
   SidebarNavMenuItem,
@@ -11,11 +9,10 @@ import {
 } from 'components/sidebar/components'
 import { FilteredNavItem, MenuItem, SidebarProps } from './types'
 import { addNavItemMetaData, getFilteredNavItems } from './helpers'
+import SidebarBackToLink from './components/sidebar-back-to-link'
+import SidebarFilterInput from './components/sidebar-filter-input'
+import SidebarMobileControls from './components/sidebar-mobile-controls'
 import s from './sidebar.module.css'
-import { IconChevronLeft16 } from '@hashicorp/flight-icons/svg-react/chevron-left-16'
-import { IconChevronRight16 } from '@hashicorp/flight-icons/svg-react/chevron-right-16'
-import { useDeviceSize } from 'contexts'
-import { useSidebarNavData } from 'layouts/sidebar-sidecar/contexts/sidebar-nav-data'
 
 const SIDEBAR_LABEL_ID = 'sidebar-label'
 
@@ -30,8 +27,7 @@ const Sidebar = ({
   visuallyHideTitle = false,
 }: SidebarProps): ReactElement => {
   const { isDesktop } = useDeviceSize()
-  const { hasManyLevels, isFirstLevel, isMiddleLevel, setCurrentLevel } =
-    useSidebarNavData()
+  const shouldRenderMobileControls = levelButtonProps && !isDesktop
   const currentPath = useCurrentPath({ excludeHash: true, excludeSearch: true })
   const [filterValue, setFilterValue] = useState('')
   const { itemsWithMetadata } = useMemo(
@@ -44,64 +40,44 @@ const Sidebar = ({
    */
 
   let backToElement
-  if (levelButtonProps && !isDesktop) {
-    const { iconPosition, text } = levelButtonProps
-
-    let icon
-    if (iconPosition === 'leading') {
-      icon = <IconChevronLeft16 />
-    } else {
-      icon = <IconChevronRight16 />
-    }
-
-    let backButton
-    if (hasManyLevels && !isFirstLevel) {
-      backButton = (
-        <Button
-          className={s.backButton}
-          color="tertiary"
-          icon={icon}
-          iconPosition={iconPosition}
-          text={text}
-          onClick={() => setCurrentLevel((prevLevel) => prevLevel - 1)}
-        />
-      )
-    }
-
-    let forwardButton
-    if (hasManyLevels && isFirstLevel) {
-      forwardButton = (
-        <Button
-          className={s.forwardButton}
-          color="tertiary"
-          icon={icon}
-          iconPosition={iconPosition}
-          text={text}
-          onClick={() => setCurrentLevel((prevLevel) => prevLevel + 1)}
-        />
-      )
-    } else if (hasManyLevels && isMiddleLevel) {
-      forwardButton = (
-        <Button
-          className={s.forwardButton}
-          color="tertiary"
-          icon={<IconChevronRight16 />}
-          iconPosition={'trailing'}
-          text="Previous"
-          onClick={() => setCurrentLevel((prevLevel) => prevLevel + 1)}
-        />
-      )
-    }
-
+  if (shouldRenderMobileControls) {
     backToElement = (
-      <div className={s.mobileControls}>
-        {backButton}
-        {forwardButton}
-      </div>
+      <SidebarMobileControls levelButtonProps={levelButtonProps} />
     )
   } else if (backToLinkProps) {
     const { text, href } = backToLinkProps
-    backToElement = <SidebarBackToLink text={text} href={href} />
+    backToElement = (
+      <div style={{ marginTop: 8 }}>
+        <SidebarBackToLink text={text} href={href} />
+      </div>
+    )
+  }
+
+  let sidebarFilterInput
+  if (showFilterInput) {
+    sidebarFilterInput = (
+      <div
+        style={{
+          marginTop: shouldRenderMobileControls ? 12 : 16,
+          marginBottom: 16,
+        }}
+      >
+        <SidebarFilterInput value={filterValue} onChange={setFilterValue} />
+      </div>
+    )
+  }
+
+  let overviewItem
+  if (overviewItemHref) {
+    overviewItem = (
+      <SidebarNavLinkItem
+        item={{
+          href: overviewItemHref,
+          title: 'Overview',
+          isActive: overviewItemHref === currentPath,
+        }}
+      />
+    )
   }
 
   let sidebarContent
@@ -121,25 +97,10 @@ const Sidebar = ({
     )
   }
 
-  let overviewItem
-  if (overviewItemHref) {
-    overviewItem = (
-      <SidebarNavLinkItem
-        item={{
-          href: overviewItemHref,
-          title: 'Overview',
-          isActive: overviewItemHref === currentPath,
-        }}
-      />
-    )
-  }
-
   return (
     <div className={s.sidebar}>
       {backToElement}
-      {showFilterInput && (
-        <SidebarFilterInput value={filterValue} onChange={setFilterValue} />
-      )}
+      {sidebarFilterInput}
       <nav aria-labelledby={SIDEBAR_LABEL_ID} className={s.nav}>
         <div className={visuallyHideTitle ? 'g-screen-reader-only' : undefined}>
           <SidebarTitleHeading text={title} id={SIDEBAR_LABEL_ID} />
