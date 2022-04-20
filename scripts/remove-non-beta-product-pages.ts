95f6bf51357318dce0a81b02511bc8292f5d74a8
Bryce Kalow <bkalow@hashicorp.com>
2022-04-20 16:43:37 -0500

    adds script to exclude non-beta product pages from our production build

---
+++ scripts/remove-non-beta-product-pages.ts
@@ -0,0 +1,39 @@
+import path from 'path'
+import fs from 'fs'
+import { loadHashiConfigForEnvironment } from '../config'
+
+/**
+ * In order to optimize builds, we're selectively not rendering pages for products which are
+ * not included in our public beta. This is accomplished by checking the products defined in
+ * config.dev_dot.beta_product_slugs and removing page folders which aren't included in the array.
+ */
+async function main() {
+  if (
+    !process.env.CI ||
+    process.env.HASHI_ENV !== 'production' ||
+    process.env.DEV_IO
+  ) {
+    return
+  }
+
+  const config = loadHashiConfigForEnvironment()
+  const betaProducts = config['dev_dot.beta_product_slugs']
+
+  const pagesDir = path.join(process.cwd(), 'src', 'pages')
+
+  const rootPagesDirs = (
+    await fs.promises.readdir(pagesDir, { withFileTypes: true })
+  ).filter((ent) => ent.isDirectory())
+
+  // Remove page files which are not for the beta products specified in our config
+  for (const dir of rootPagesDirs) {
+    if (!betaProducts.includes(dir.name)) {
+      console.log(`🧹 removing pages at /${dir.name}`)
+      await fs.promises.rm(path.join(pagesDir, dir.name), {
+        recursive: true,
+      })
+    }
+  }
+}
+
+main()
