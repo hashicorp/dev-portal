26b8b44e2049fee8028d43f06b6c5bba4760f218
Bryce Kalow <bkalow@hashicorp.com>
2022-04-21 18:13:48 -0500

    break out of autocomplete-js

--- src/views/docs-view/components/product-docs-search/index.tsx
+++ src/views/docs-view/components/product-docs-search/index.tsx
@@ -1,15 +1,65 @@
-import React, { createElement, Fragment, useEffect, useRef } from 'react'
-import { render } from 'react-dom'
+import React from 'react'
 import algoliasearch from 'algoliasearch'
-import { autocomplete } from '@algolia/autocomplete-js'
-import { getAlgoliaResults } from '@algolia/autocomplete-js'
+import {
+  AutocompleteOptions,
+  AutocompleteState,
+  createAutocomplete,
+} from '@algolia/autocomplete-core'
+import { Hit } from '@algolia/client-search'
+import {
+  getAlgoliaResults,
+  parseAlgoliaHitHighlight,
+} from '@algolia/autocomplete-preset-algolia'
 import { IconCornerDownLeft16 } from '@hashicorp/flight-icons/svg-react/corner-down-left-16'
+import { IconSearch16 } from '@hashicorp/flight-icons/svg-react/search-16'
+import { IconX16 } from '@hashicorp/flight-icons/svg-react/x-16'
 import Badge from 'components/badge'
 import s from './product-docs-search.module.css'
 
 const appId = process.env.NEXT_PUBLIC_ALGOLIA_APP_ID
 const apiKey = process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_ONLY_API_KEY
 const searchClient = algoliasearch(appId, apiKey)
+
+type HighlightHitParams<THit> = {
+  /**
+   * The Algolia hit whose attribute to retrieve the highlighted parts from.
+   */
+  hit: THit
+  /**
+   * The attribute to retrieve the highlighted parts from.
+   *
+   * You can use the array syntax to reference nested attributes.
+   */
+  attribute: keyof THit | string[]
+  /**
+   * The tag name to use for highlighted parts.
+   *
+   * @default "mark"
+   */
+  tagName?: string
+}
+
+export function Highlight<THit>({
+  hit,
+  attribute,
+  tagName = 'mark',
+}: HighlightHitParams<THit>): JSX.Element {
+  const Element = tagName as keyof JSX.IntrinsicElements
+
+  return (
+    <>
+      {parseAlgoliaHitHighlight<THit>({ hit, attribute }).map(
+        ({ value, isHighlighted }, index) => {
+          if (isHighlighted) {
+            return <Element key={index}>{value}</Element>
+          }
+
+          return value
+        }
+      )}
+    </>
+  )
+}
 
 function SearchResultsLegend() {
   return (
@@ -20,53 +70,152 @@ function SearchResultsLegend() {
   )
 }
 
-function Autocomplete(
-  props: Omit<Parameters<typeof autocomplete>[0], 'container'>
-) {
-  const containerRef = useRef(null)
+type AutocompleteItem = Hit<{
+  page_title: string
+  description: string
+  objectID: string
+}>
 
-  useEffect(() => {
-    if (!containerRef.current) {
+function Autocomplete(props: Partial<AutocompleteOptions<AutocompleteItem>>) {
+  const [autocompleteState, setAutocompleteState] = React.useState<
+    AutocompleteState<AutocompleteItem>
+  >({
+    collections: [],
+    completion: null,
+    context: {},
+    isOpen: false,
+    query: '',
+    activeItemId: null,
+    status: 'idle',
+  })
+  const autocomplete = React.useMemo(
+    () =>
+      createAutocomplete<
+        AutocompleteItem,
+        React.BaseSyntheticEvent,
+        React.MouseEvent,
+        React.KeyboardEvent
+      >({
+        onStateChange({ state }) {
+          setAutocompleteState(state)
+        },
+        ...props,
+      }),
+    [props]
+  )
+
+  const inputRef = React.useRef<HTMLInputElement>(null)
+  const formRef = React.useRef<HTMLFormElement>(null)
+  const panelRef = React.useRef<HTMLDivElement>(null)
+  const { getEnvironmentProps } = autocomplete
+
+  React.useEffect(() => {
+    if (!formRef.current || !panelRef.current || !inputRef.current) {
       return undefined
     }
 
-    const search = autocomplete({
-      container: containerRef.current,
-      renderer: {
-        createElement,
-        Fragment,
-        // @ts-expect-error - react-dom type mismatch, but matching the documentation
-        render,
-      },
-      render({ sections }, root) {
-        render(
-          <>
-            <div className={s.panelLayout}>{sections}</div>
-            <SearchResultsLegend />
-          </>,
-          root
-        )
-      },
-      ...props,
+    const { onTouchStart, onTouchMove } = getEnvironmentProps({
+      formElement: formRef.current,
+      inputElement: inputRef.current,
+      panelElement: panelRef.current,
     })
 
-    return () => {
-      search.destroy()
-    }
-  }, [props])
+    window.addEventListener('touchstart', onTouchStart)
+    window.addEventListener('touchmove', onTouchMove)
 
-  return <div ref={containerRef} />
+    return () => {
+      window.removeEventListener('touchstart', onTouchStart)
+      window.removeEventListener('touchmove', onTouchMove)
+    }
+  }, [getEnvironmentProps, formRef, inputRef, panelRef])
+
+  return (
+    <div className={s.root} {...autocomplete.getRootProps({})}>
+      <form
+        ref={formRef}
+        className={s.form}
+        {...autocomplete.getFormProps({ inputElement: inputRef.current })}
+      >
+        <div className={s.inputWrapperPrefix}>
+          <label {...autocomplete.getLabelProps({})}>
+            <button className={s.submitButton} type="submit" title="Submit">
+              <IconSearch16 />
+            </button>
+          </label>
+        </div>
+        <div className={s.inputWrapper}>
+          <input
+            className={s.input}
+            ref={inputRef}
+            {...autocomplete.getInputProps({ inputElement: inputRef.current })}
+          />
+        </div>
+        <div className={s.inputWrapperSuffix}>
+          <button
+            className={s.clearButton}
+            title="Clear"
+            type="reset"
+            hidden={!autocompleteState.query}
+          >
+            <IconX16 />
+          </button>
+        </div>
+      </form>
+
+      {autocompleteState.isOpen && (
+        <div
+          ref={panelRef}
+          className={[
+            s.panel,
+            'aa-Panel',
+            'aa-Panel--desktop',
+            autocompleteState.status === 'stalled' && 'aa-Panel--stalled',
+          ]
+            .filter(Boolean)
+            .join(' ')}
+          {...autocomplete.getPanelProps({})}
+        >
+          <div className={s.panelLayout}>
+            {autocompleteState.collections.map((collection, index) => {
+              const { source, items } = collection
+
+              return (
+                <section key={`source-${index}`} className="aa-Source">
+                  {items.length > 0 && (
+                    <ul className={s.list} {...autocomplete.getListProps()}>
+                      {items.map((item) => {
+                        return (
+                          <li
+                            key={item.objectID}
+                            className={s.item}
+                            {...autocomplete.getItemProps({ item, source })}
+                          >
+                            <ProductItem hit={item} />
+                          </li>
+                        )
+                      })}
+                    </ul>
+                  )}
+                </section>
+              )
+            })}
+          </div>
+          <SearchResultsLegend />
+        </div>
+      )}
+    </div>
+  )
 }
 
-function ProductItem({ hit, components }) {
+function ProductItem({ hit }) {
   return (
     <a href={`/waypoint/${hit.objectID}`} className={s.itemLink}>
       <div className={s.itemContent}>
         <div className={s.itemTitle}>
-          <components.Highlight hit={hit} attribute="page_title" />
+          <Highlight hit={hit} attribute="page_title" />
         </div>
         <div className={s.itemDescription}>
-          <components.Highlight hit={hit} attribute="description" />
+          <Highlight hit={hit} attribute="description" />
           <IconCornerDownLeft16 className={s.enterIcon} />
         </div>
       </div>
@@ -77,25 +226,8 @@ function ProductItem({ hit, components }) {
 export function ProductDocsSearch() {
   return (
     <Autocomplete
-      onStateChange={console.log}
-      classNames={{
-        root: s.root,
-        form: s.form,
-        inputWrapperPrefix: s.inputWrapperPrefix,
-        submitButton: s.submitButton,
-        inputWrapper: s.inputWrapper,
-        input: s.input,
-        inputWrapperSuffix: s.inputWrapperSuffix,
-        clearButton: s.clearButton,
-        panel: s.panel,
-        panelLayout: s.panelLayout,
-        list: s.list,
-        item: s.item,
-      }}
       openOnFocus={true}
       placeholder="Search"
-      debug
-      detachedMediaQuery="none"
       getSources={({ query }) => [
         {
           sourceId: 'products',
@@ -109,11 +241,6 @@ export function ProductDocsSearch() {
                 },
               ],
             })
-          },
-          templates: {
-            item({ item, components }) {
-              return <ProductItem hit={item} components={components} />
-            },
           },
         },
       ]}
