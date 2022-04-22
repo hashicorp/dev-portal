804d4d5b72aa34d0aa7ddacf11caa26cafc8af02
Bryce Kalow <bkalow@hashicorp.com>
2022-04-22 09:46:13 -0500

    fix formatting

--- scripts/remove-non-beta-product-pages.ts
+++ scripts/remove-non-beta-product-pages.ts
@@ -30,7 +30,11 @@ async function main() {
    * Ensure we retain _proxied-dot-io as it serves our production .io sites
    */
   for (const dir of rootPagesDirs) {
-    if (!betaProducts.includes(dir.name) && dir.name !== '_proxied-dot-io' && dir.name !== 'api') {
+    if (
+      !betaProducts.includes(dir.name) &&
+      dir.name !== '_proxied-dot-io' &&
+      dir.name !== 'api'
+    ) {
       console.log(`🧹 removing pages at /${dir.name}`)
       await fs.promises.rm(path.join(pagesDir, dir.name), {
         recursive: true,
