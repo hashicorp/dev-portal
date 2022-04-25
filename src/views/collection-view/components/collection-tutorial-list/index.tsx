a8b7985fe24071cb2a45c5911a3f171d10a6f761
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-04-25 11:15:30 -0400

    feat: build collection-tutorial-list for collection views (#363)
    * feat: implement formatTutorialCard helper
    
    * feat: implement CollectionTutorialList component
    
    * feat: use CollectionTutorialList in collection view
    
    * chore: fix typo
    
    * style: inline single-use const
    
    * fix: use getReadableTime from components/tutorial-meta
    
    * style: split interface to types.ts file

---
+++ src/views/collection-view/components/collection-tutorial-list/index.tsx
@@ -0,0 +1,34 @@
+import classNames from 'classnames'
+import TutorialCard from 'components/tutorial-card'
+import { TutorialCardPropsWithId } from 'components/tutorial-card/types'
+import { CollectionTutorialListProps } from './types'
+import s from './collection-tutorial-list.module.css'
+
+function CollectionTutorialList({
+  tutorials,
+  isOrdered,
+}: CollectionTutorialListProps) {
+  const ListRoot = isOrdered ? 'ol' : 'ul'
+
+  return (
+    <ListRoot className={classNames(s.listRoot, { [s.isOrdered]: isOrdered })}>
+      {tutorials.map((tutorial: TutorialCardPropsWithId) => {
+        return (
+          <li key={tutorial.id}>
+            <TutorialCard
+              description={tutorial.description}
+              duration={tutorial.duration}
+              hasInteractiveLab={tutorial.hasInteractiveLab}
+              hasVideo={tutorial.hasVideo}
+              heading={tutorial.heading}
+              url={tutorial.url}
+              productsUsed={tutorial.productsUsed}
+            />
+          </li>
+        )
+      })}
+    </ListRoot>
+  )
+}
+
+export default CollectionTutorialList
