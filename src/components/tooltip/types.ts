896d9d2881e48101140c28d3ecc184f464438521
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-04-25 19:13:08 -0400

    docs: update & format types.ts for tooltip

--- src/components/tooltip/types.ts
+++ src/components/tooltip/types.ts
@@ -1,27 +1,58 @@
 import { Position } from '@reach/tooltip'
 import { ReactElement, ReactNode } from 'react'
-
-export type PRect = Parameters<Position>[0]
-
 export interface TooltipProps {
-  /** Element that, when hovered, will display the tooltip. */
+  /**
+   * Element that, when hovered, will display the tooltip.
+   */
   children: ReactElement
-  /** Plain text, or render slot, to render in the tooltip */
+
+  /**
+   * Plain text, or `<ReactElement />`, to render in the tooltip.
+   */
   label: ReactNode
-  /** Optional alternate content for screen readers.
-   * Useful if the label prop is not suitable for screen reader announcement. */
+
+  /**
+   * Optional alternate content for screen readers.
+   * Useful if the label prop is not suitable for screen reader announcement,
+   * for example for icon-only tooltips.
+   */
   ariaLabel?: string
-  /** Minimum spacing from viewport edge */
-  collisionBuffer?: number
-  /** Theme for light or dark mode */
+
+  /**
+   * Theme for light or dark mode
+   */
   theme?: 'light' | 'dark'
-  /** Optional CSS override for background color. Foreground color can be overridden by passing a styled React node to the label prop.  */
-  themeBackground?: string
 }
 
+/**
+ * A partial DOMRect, describe further within @reach/tooltip
+ */
+export type PRect = Parameters<Position>[0]
+
 export interface TooltipArrowProps {
+  /**
+   * A partial DOMRect which describes the position of the tooltip trigger.
+   * Used to position the tooltip arrow.
+   * For use within <Tooltip /> only.
+   */
   triggerRect: PRect
+
+  /**
+   * The minimum pixel distance to maintain
+   * between the tooltip edge and the viewport edge.
+   * For use within <Tooltip /> only.
+   */
   collisionBuffer: number
+
+  /**
+   * A class shared within the component to alter appearance.
+   * For use within <Tooltip /> only.
+   */
   themeClass: string
+
+  /**
+   * The desired width of the tooltip arrow.
+   * For use within <Tooltip /> only.
+   */
   arrowWidth: number
 }
