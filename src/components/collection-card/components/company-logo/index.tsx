a71145d4988421a033e874314ecaa1802511257c
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-04-26 16:37:47 -0400

    refactor: explicit name to img map for type safety

--- src/components/collection-card/components/company-logo/index.tsx
+++ src/components/collection-card/components/company-logo/index.tsx
@@ -3,22 +3,28 @@ import classNames from 'classnames'
 import { CompanyLogoOption, CompanyLogoProps } from './types'
 import s from './company-logo.module.css'
 
+const LOGO_TO_SOURCE_MAP: Record<CompanyLogoOption, string> = {
+  'terraform-cloud':
+    '@hashicorp/mktg-logos/product/terraform-cloud/primary/color.svg',
+  oci: './img/oci.png',
+  docker: './img/docker',
+  github: './img/github',
+  'microsoft-azure': './img/microsoft-azure',
+  'google-cloud': './img/google-cloud',
+  aws: './img/aws',
+}
+
 export default function CompanyLogo({ name }: CompanyLogoProps): JSX.Element {
   if (!(name in CompanyLogoOption)) {
     console.log('Company Logo does not exist')
     return <></>
   }
 
-  const src =
-    name === 'terraform-cloud'
-      ? require('@hashicorp/mktg-logos/product/terraform-cloud/primary/color.svg')
-      : require(`./img/${name}.png`)
-
   return (
     <span className={s.root}>
       <span className={classNames(s.inner, s[`logo-${name}`])}>
         <Image
-          src={src}
+          src={require(LOGO_TO_SOURCE_MAP[name])}
           alt=""
           /**
            * Note: for use in CollectionCard, consumer should provide meaningful
