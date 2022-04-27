07d9464156786f76dcb9a9cf26615a26e3a9811d
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-04-27 17:45:00 -0400

    fix: remove duplicate tutorial-stack types

--- src/views/product-tutorials-view/components/product-view-content/components/tutorials-stack/types.ts
+++ src/views/product-tutorials-view/components/product-view-content/components/tutorials-stack/types.ts
@@ -1,68 +1,21 @@
-import { Collection, ProductOption } from 'lib/learn-client/types'
-
-/**
- * TODO
- * Many of these types were dropped in from the existing front-end repo.
- * Need to update to use the analogous types from lib/learn-client/types.
- * Intent is to make those updates during implementation of TutorialsStack
- */
-
-interface BaseTutorial {
-  id?: string
-  slug: string
-  name: string
-  description: string
-  readTime: number
-}
-
-type CollectionReference = Pick<
+import {
   Collection,
-  'id' | 'name' | 'shortName' | 'slug' | 'theme' | 'level' | 'ordered'
->
-
-type DefaultCollection = Omit<CollectionReference, 'level' | 'ordered'>
-
-enum VideoHostOption {
-  youtube = 'youtube',
-  wistia = 'wistia',
-}
-
-// @TODO delete katacoda option once all removed
-enum HandsOnLabProviderOption {
-  katacoda = 'katacoda',
-  instruqt = 'instruqt',
-}
-
-interface TutorialLabOptions {
-  handsOnLabId?: string
-  handsOnLabProvider?: HandsOnLabProviderOption
-}
-
-interface TutorialVideoOptions {
-  videoId?: string
-  videoHost?: VideoHostOption
-  videoInline: number // @TODO update to a boolean
-}
-
-export interface ProductUsedRow {
-  product: ProductOption
-  tutorial: string
-  isBeta: number
-  isPrimary: number
-  minVersion?: string
-  maxVersion?: string
-}
-
-//
-//
-//
+  ProductUsed,
+  Tutorial,
+  TutorialVideo,
+  TutorialHandsOnLab,
+} from 'lib/learn-client/types'
 
 export interface TutorialStackItem
-  extends BaseTutorial,
-    TutorialLabOptions,
-    Pick<TutorialVideoOptions, 'videoId'> {
-  defaultContext?: DefaultCollection
-  productsUsed?: ProductUsedRow[]
+  extends Pick<Tutorial, 'id' | 'slug' | 'name' | 'description' | 'readTime'> {
+  handsOnLabId?: TutorialHandsOnLab['id']
+  handsOnLabProvider?: TutorialHandsOnLab['provider']
+  videoId?: TutorialVideo['id']
+  defaultContext?: Pick<
+    Collection,
+    'id' | 'name' | 'shortName' | 'slug' | 'theme'
+  >
+  productsUsed?: ProductUsed[]
 }
 
 export interface TutorialsStackProps {
@@ -70,5 +23,5 @@ export interface TutorialsStackProps {
   heading: string
   /** Subheading to show above the tutorial cards. */
   subheading?: string
-  featuredTutorials: $TSFixMe[] // TutorialStackItem
+  featuredTutorials: TutorialStackItem[]
 }
