40f86454b94364c535d0c75feac710486212ce8b
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-04-28 13:05:52 -0400

    feat: implement LogoCardsList with CardsGridList

--- src/views/product-tutorials-view/components/product-view-content/components/logo-card-list/index.tsx
+++ src/views/product-tutorials-view/components/product-view-content/components/logo-card-list/index.tsx
@@ -1,13 +1,30 @@
-import s from './logo-card-list.module.css'
+import CardsGrid from 'components/cards-grid-list'
+import CollectionCard, {
+  CollectionCardPropsWithId,
+} from 'components/collection-card'
+import { formatCollectionCard } from 'components/collection-card/helpers'
 import { LogoCardListProps, LogoCardListItem } from './types'
 
 function LogoCardList({ items }: LogoCardListProps): JSX.Element {
+  /**
+   * TODO: Instead of passing full ClientCollection data to frontend,
+   * and trimming it down here to card data, it seems like we could
+   * do this trimming server side, and end up with a much smaller
+   * bundle of static props JSON.
+   * Asana task: https://app.asana.com/0/0/1202182325935203/f
+   */
+  const collectionCards = items.map((item: LogoCardListItem) => {
+    const { collection, logo } = item
+    return { ...formatCollectionCard(collection), logo }
+  }) as CollectionCardPropsWithId[]
+
   return (
-    <pre className={s.placeholder}>
-      <code>
-        {JSON.stringify({ component: 'LogoCardList', items }, null, 2)}
-      </code>
-    </pre>
+    <CardsGrid>
+      {collectionCards.map((cardPropsWithId: CollectionCardPropsWithId) => {
+        const { id, ...cardProps } = cardPropsWithId
+        return <CollectionCard key={id} {...cardProps} />
+      })}
+    </CardsGrid>
   )
 }
 
