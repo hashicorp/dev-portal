5bee4eae1a6f26d137f780187753e96a40e07ec5
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-05-03 18:37:46 -0400

    refactor: use work from rewrite-tutorial-links remark plugin

--- src/views/product-tutorials-view/helpers/__tests__/detect-and-reformat-learn-url.test.ts
+++ src/views/product-tutorials-view/helpers/__tests__/detect-and-reformat-learn-url.test.ts
@@ -1,6 +1,28 @@
+import nock from 'nock'
 import detectAndReformatLearnUrl from '../detect-and-reformat-learn-url'
 
+/**
+ * Mocks return value from 'api/tutorials-map' endpoint.
+ * Maps tutorial slugs to tutorial URLs which include the default collection.
+ * When adding new test cases, make sure the path is accounted for below
+ *
+ * [key: database tutorial slug]: value — dev dot absolute path
+ */
+const MOCK_TUTORIALS_MAP = {
+  'waypoint/aws-ecs': '/waypoint/tutorials/deploy-aws/aws-ecs',
+  'vault/getting-started-install':
+    '/vault/tutorials/getting-started/getting-started-install',
+}
+
 describe('detectAndReformatLearnUrl', () => {
+  beforeEach(async () => {
+    // the api base url defaults to localhost when no VERCEL_URL is provided
+    const scope = nock('http://localhost:3000/api/tutorials-map')
+      .persist()
+      .get(/.*/)
+      .reply(200, MOCK_TUTORIALS_MAP)
+  })
+
   it('returns non-Learn URLs back unmodified', async () => {
     const nonLearnUrls: string[] = [
       '/vault/docs',
@@ -29,7 +51,7 @@ describe('detectAndReformatLearnUrl', () => {
         input: '/collections/waypoint/deploy-aws',
         expected: '/waypoint/tutorials/deploy-aws',
       },
-      // Note: underlying getCollectionSlug() handles beta products
+      // Note: underlying rewriteTutorialsLink() handles beta products
       {
         input: '/collections/consul/kubernetes',
         expected: 'https://learn.hashicorp.com/collections/consul/kubernetes',
@@ -48,6 +70,10 @@ describe('detectAndReformatLearnUrl', () => {
       expected: string
     }
     const tutorialUrls: UrlTestCase[] = [
+      {
+        input: '/tutorials/vault/getting-started-install',
+        expected: '/vault/tutorials/getting-started/getting-started-install',
+      },
       {
         input:
           '/tutorials/vault/getting-started-install?in=vault/getting-started',
--- src/views/product-tutorials-view/helpers/detect-and-reformat-learn-url.ts
+++ src/views/product-tutorials-view/helpers/detect-and-reformat-learn-url.ts
@@ -1,8 +1,9 @@
 import { ProductOption, ThemeOption } from 'lib/learn-client/types'
-import { getCollectionSlug } from 'views/collection-view/helpers'
-import { getTutorialSlug } from 'views/collection-view/helpers'
-import { getTutorial } from 'lib/learn-client/api/tutorial'
 import isAbsoluteUrl from 'lib/is-absolute-url'
+import { rewriteTutorialsLink } from 'lib/remark-plugins/rewrite-tutorial-links'
+import { getTutorialMap } from 'lib/remark-plugins/rewrite-tutorial-links/utils'
+
+let TUTORIAL_MAP
 
 /**
  * Given a URL string,  detect whether it is a collection or tutorial URL
@@ -17,34 +18,12 @@ async function detectAndReformatLearnUrl(url: string): Promise<string> {
     return url
   }
   // Gather info on whether this is a collection or tutorial URL
-  const urlObject = new URL(url, 'https://learn.hashicorp.com')
-  const pathParts = urlObject.pathname.split('/')
-  const isTutorialUrl =
-    pathParts[1] === 'tutorials' && isThemeOrProduct(pathParts[2])
-  const isCollectionUrl =
-    pathParts[1] === 'collections' && isThemeOrProduct(pathParts[2])
-  if (isTutorialUrl) {
-    // For tutorial URLs, we need a collection slug
-    const tutorialSlugDb = `${pathParts[2]}/${pathParts[3]}`
-    // If there's an "?in=" param, we can use that as the collection slug
-    const inParam = urlObject.searchParams.get('in')
-    if (inParam) {
-      return getTutorialSlug(tutorialSlugDb, inParam)
-    } else {
-      // If not, we need to fetch the tutorial, & determine its default context
-      const tutorialData = await getTutorial(tutorialSlugDb)
-      if (!tutorialData) {
-        throw new Error(
-          `404: Could not find tutorial "${tutorialSlugDb}" in detectAndReformatLearnUrl.`
-        )
-      }
-      const { collectionCtx } = tutorialData
-      return getTutorialSlug(tutorialSlugDb, collectionCtx.default.slug)
-    }
-  } else if (isCollectionUrl) {
-    // For collection urls, we can use getCollection Slug
-    const collectionSlugDb = `${pathParts[2]}/${pathParts[3]}`
-    return getCollectionSlug(collectionSlugDb)
+  const [, p2, p3] = url.split('/')
+  const isTutorialUrl = p2 === 'tutorials' && isThemeOrProduct(p3)
+  const isCollectionUrl = p2 === 'collections' && isThemeOrProduct(p3)
+  if (isTutorialUrl || isCollectionUrl) {
+    TUTORIAL_MAP = await getTutorialMap()
+    return rewriteTutorialsLink(url, TUTORIAL_MAP)
   } else {
     // All other non-absolute URLs are returned unmodified
     return url
