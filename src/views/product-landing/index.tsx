4c9e9c04b53ad58f5f9d774b9633e4b0a84b726a
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-05-03 12:54:55 -0400

    feat: stub in redesigned page contents

--- src/views/product-landing/index.tsx
+++ src/views/product-landing/index.tsx
@@ -1,74 +1,40 @@
 /* eslint-disable react/no-array-index-key */
 import React, { ReactElement } from 'react'
-import slugify from 'slugify'
 import SidebarSidecarLayout from 'layouts/sidebar-sidecar'
-import Heading, { HeadingProps } from 'components/heading'
-import Text from 'components/text'
-import DevDotContent from 'components/dev-dot-content'
-import GetStarted, { GetStartedProps } from './components/get-started'
-import Cards, { CardProps } from './components/cards'
 import s from './product-landing.module.css'
-
-type Block =
-  | ({ type: 'heading' } & HeadingProps & { heading: string })
-  | ({ type: 'get_started' } & GetStartedProps)
-  | ({ type: 'cards' } & CardProps)
-
 interface ProductLandingProps {
   content: {
-    heading: string
-    subheading: string
-    blocks: Block[]
+    hero: $TSFixMe
+    overview: $TSFixMe
+    get_started: $TSFixMe
+    blocks: $TSFixMe[]
   }
 }
 
 function ProductLandingView({ content }: ProductLandingProps): ReactElement {
   return (
-    <DevDotContent>
-      <Heading
-        className={s.pageHeading}
-        size={500}
-        level={1}
-        slug={slugify(content.heading)}
-        weight="bold"
-      >
-        {content.heading}
-      </Heading>
-      {content.subheading && (
-        <Text className={s.pageSubheading}>{content.subheading}</Text>
-      )}
+    <div>
+      <pre className={s.placeholder}>
+        <code>
+          {JSON.stringify({ ...content, blocks: undefined }, null, 2)}
+        </code>
+      </pre>
       {content.blocks.map((block, idx) => {
         const { type } = block
-        if (type === 'heading') {
-          const { heading, slug, level, size } = block
-          return (
-            <Heading
-              className={s[`h${level}`]}
-              key={idx}
-              weight="bold"
-              {...{ slug, level, size }}
-            >
-              {heading}
-            </Heading>
-          )
-        } else if (type === 'get_started') {
-          const { heading, product, text, link } = block
-          return <GetStarted key={idx} {...{ product, heading, text, link }} />
-        } else if (type === 'cards') {
-          const { columns, cards } = block
-          return <Cards key={idx} {...{ columns, cards }} />
+        switch (type) {
+          default:
+            // If we don't have a recognized card type,
+            // return a dev-oriented debug view of the block data
+            // TODO: remove this for production, this is here
+            // TODO: temporarily as we work through demo-oriented implementation
+            return (
+              <pre key={idx} className={s.placeholder}>
+                <code>{JSON.stringify({ block }, null, 2)}</code>
+              </pre>
+            )
         }
-        // If we don't have a recognized card type,
-        // return a dev-oriented debug view of the block data
-        // TODO: remove this for production, this is here
-        // TODO: temporarily as we work through demo-oriented implementation
-        return (
-          <pre key={idx} style={{ border: '1px solid red' }}>
-            <code>{JSON.stringify({ type, block }, null, 2)}</code>
-          </pre>
-        )
       })}
-    </DevDotContent>
+    </div>
   )
 }
 
