27458d81c4012c7a2ad728eed38130f992a9df4f
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-05-04 14:14:17 -0400

    docs: add comment on extractHeadings

--- src/views/product-landing/helpers/extract-headings.ts
+++ src/views/product-landing/helpers/extract-headings.ts
@@ -2,6 +2,16 @@ import { TableOfContentsHeading } from 'layouts/sidebar-sidecar/components/table
 import { ProductLandingViewProps } from '../types'
 import { ProductLandingBlock } from '../components/product-landing-blocks/types'
 
+/**
+ * Extracts headings from product-landing page content .
+ *
+ * Note: could potentially use `traverse` here to make this more generic.
+ * We could target any & all objects that have { heading, headingSlug }
+ * However, right now that felt more opaque and cryptic than being more
+ * explicit around which headings are being used and in what order.
+ * As well, I think a generic implementation should better account for "level".
+ * So, deferred creating a generic implementation, for now.
+ */
 export function extractHeadings(
   content: ProductLandingViewProps['content']
 ): TableOfContentsHeading[] {
