256526035240728b9c60249724c5f14a04c38e1e
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-05-04 14:09:31 -0400

    chore: remove outdated comment

--- src/views/product-landing/helpers/extract-headings.ts
+++ src/views/product-landing/helpers/extract-headings.ts
@@ -5,18 +5,6 @@ import { ProductLandingBlock } from '../components/product-landing-blocks/types'
 export function extractHeadings(
   content: ProductLandingViewProps['content']
 ): TableOfContentsHeading[] {
-  /**
-   * TODO: content is raw placeholder for now
-   * - Need to add "headings" that aren't placeholders.
-   *   These will be derived from content blocks
-   *   (mainly heading blocks, but also others, based on design intent)
-   * - Need to augment things like tutorialSlugs to fill in that data
-   *   Will close: https://app.asana.com/0/1201010428539925/1201654639085764/f
-   * - Need to build out the client-side components, whose props APIs will
-   *   inform what content structure we'll want to return for use on the client.
-   *   Intent being to return the minimum data necessary to render
-   *   all content block components on the page.
-   */
   const headings: TableOfContentsHeading[] = [
     {
       title: content.overview.heading,
@@ -42,6 +30,5 @@ export function extractHeadings(
       []
     ),
   ]
-
   return headings
 }
