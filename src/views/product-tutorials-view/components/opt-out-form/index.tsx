8abc80bf9343b7beae9537be9e6b3823494ec921
Kendall Strautman <kendallstrautman@gmail.com>
2022-05-05 14:29:17 -0700

    basic form styles

---
+++ src/views/product-tutorials-view/components/opt-out-form/index.tsx
@@ -0,0 +1,72 @@
+import Button from 'components/button'
+import Text from 'components/text'
+import s from './opt-out-form.module.css'
+
+interface OptOutFormProps {
+  redirectUrl: string
+  onDismiss(): void
+  platform: 'vault' | 'waypoint' | 'learn'
+}
+
+enum platformOptions {
+  vault = 'Vault',
+  waypoint = 'Waypoint',
+  learn = 'Learn',
+}
+
+const optOutOptions = [
+  'Missing Content',
+  'Just checking it out',
+  'Something broke',
+  "I can't bookmark a tutorial",
+  'No user progress on tutorials',
+  'Something else?',
+]
+
+/**
+ *
+ * TODO - make the buttons, submit / reset inputs?
+ */
+
+export default function OptOutForm({
+  redirectUrl,
+  onDismiss,
+  platform,
+}: OptOutFormProps) {
+  return (
+    <form className={s.form}>
+      <label
+        htmlFor="opt-out-form"
+        className={s.heading}
+      >{`Why do you want to leave the ${platformOptions[platform]} Developer Beta?`}</label>
+      <Text className={s.subheading}>(optional)</Text>
+      <div className={s.feedback}>
+        <label htmlFor="opt-out-select" hidden>
+          Please select a reason
+        </label>
+        <select id="opt-out-select" className={s.select}>
+          <option>Select a reason</option>
+          {optOutOptions.map((reason: string) => (
+            <option key={reason.replaceAll(' ', '-')}>{reason}</option>
+          ))}
+        </select>
+        <label htmlFor="optional-feedback-text" hidden>
+          Enter additional feedback(optional)
+        </label>
+        <textarea
+          id="optional-feedback-text"
+          className={s.optionalText}
+          placeholder=" More details (optional)..."
+        />
+      </div>
+      <div className={s.ctaButtons}>
+        <Button
+          color="primary"
+          text="Leave Beta"
+          onClick={() => window.location.assign(redirectUrl)}
+        />
+        <Button color="secondary" text="Cancel" onClick={onDismiss} />
+      </div>
+    </form>
+  )
+}
