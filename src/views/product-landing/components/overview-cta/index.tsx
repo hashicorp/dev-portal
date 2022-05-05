971e0fa67f10d8737eff6ce1417971a067031074
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-05-05 19:37:05 -0400

    feat: implement overview-cta

--- src/views/product-landing/components/overview-cta/index.tsx
+++ src/views/product-landing/components/overview-cta/index.tsx
@@ -1,5 +1,9 @@
 import { OverviewCtaProps } from './types'
+import isAbsoluteUrl from 'lib/is-absolute-url'
+import { IconArrowRight16 } from '@hashicorp/flight-icons/svg-react/arrow-right-16'
+import { IconExternalLink16 } from '@hashicorp/flight-icons/svg-react/external-link-16'
 import s from './overview-cta.module.css'
+import StandaloneLink from 'components/standalone-link'
 
 function OverviewCta({
   heading,
@@ -9,20 +13,31 @@ function OverviewCta({
   image,
 }: OverviewCtaProps) {
   return (
-    <>
-      <pre className={s.placeholder}>
-        <code>
-          <h2 id={headingSlug} className={s.heading}>
-            {heading} [id={headingSlug}]
-          </h2>
-          {JSON.stringify(
-            { component: 'OverviewCta', heading, body, cta, image },
-            null,
-            2
-          )}
-        </code>
-      </pre>
-    </>
+    <div className={s.root}>
+      <div className={s.textPart}>
+        <h2 id={headingSlug} className={s.heading}>
+          {heading}
+        </h2>
+        <p className={s.body}>{body}</p>
+        <StandaloneLink
+          className={s.cta}
+          text={cta.text}
+          href={cta.url}
+          color="secondary"
+          icon={
+            isAbsoluteUrl(cta.url) ? (
+              <IconExternalLink16 />
+            ) : (
+              <IconArrowRight16 />
+            )
+          }
+          iconPosition="trailing"
+        />
+      </div>
+      <div className={s.imagePart}>
+        <img src={image} alt="" />
+      </div>
+    </div>
   )
 }
 
