cb2ec4c5f314294e0999b1a3d055d40a7c14a854
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-05-05 19:45:08 -0400

    feat: linked cards for product landing

--- src/views/product-landing/components/product-landing-blocks/blocks/linked-cards/index.tsx
+++ src/views/product-landing/components/product-landing-blocks/blocks/linked-cards/index.tsx
@@ -1,13 +1,20 @@
-import { LinkedCardsProps } from './types'
-import s from './linked-cards.module.css'
+import { LinkedCard, LinkedCardsProps } from './types'
+import CardsGridList from 'components/cards-grid-list'
+import CardLink from 'components/card-link'
+import { CardBody, CardHeading } from 'components/tutorial-collection-cards'
 
 function LinkedCards({ cards }: LinkedCardsProps) {
   return (
-    <pre className={s.placeholder}>
-      <code>
-        {JSON.stringify({ component: 'LinkedCards', cards }, null, 2)}
-      </code>
-    </pre>
+    <CardsGridList>
+      {cards.map((card: LinkedCard, idx: number) => {
+        return (
+          <CardLink key={idx} href={card.url}>
+            <CardHeading level={3} text={card.heading} />
+            <CardBody text={card.body} />
+          </CardLink>
+        )
+      })}
+    </CardsGridList>
   )
 }
 
