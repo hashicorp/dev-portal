c2b0c2ea4355c16ec1580a91385c40b80f0c789a
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-05-06 15:51:38 -0400

    feat: implement icon-cards for product landing (#400)
    * feat: hook up product landing sidecar
    
    * fix: use local product type, not learn one
    
    * chore: remove outdated comment
    
    * docs: add comment on extractHeadings
    
    * docs: add comment on makeHeadingSlugScope
    
    * feat: implement tutorial data fetching
    
    * feat: implement icon-cards for product landing
    
    * fix: revert not-yet-used spacing styles
    
    * fix: bad downloads path for Install link
    
    Co-authored-by: Kendall Strautman <36613477+kendallstrautman@users.noreply.github.com>
    
    Co-authored-by: Kendall Strautman <36613477+kendallstrautman@users.noreply.github.com>

---
+++ src/views/product-landing/components/icon-cards/get-icon-cards.tsx
@@ -0,0 +1,24 @@
+import { IconDocs16 } from '@hashicorp/flight-icons/svg-react/docs-16'
+import { IconLearn16 } from '@hashicorp/flight-icons/svg-react/learn-16'
+import { IconDownload16 } from '@hashicorp/flight-icons/svg-react/download-16'
+import { ProductSlug } from 'types/products'
+
+export function getIconCards(productSlug: ProductSlug) {
+  return [
+    {
+      icon: <IconDocs16 />,
+      text: 'Documentation',
+      url: `/${productSlug}/docs`,
+    },
+    {
+      icon: <IconLearn16 />,
+      text: 'Tutorials',
+      url: `/${productSlug}/tutorials`,
+    },
+    {
+      icon: <IconDownload16 />,
+      text: 'Install',
+      url: `/${productSlug}/downloads`,
+    },
+  ]
+}
