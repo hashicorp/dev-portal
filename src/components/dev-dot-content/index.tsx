31be291a66d129e2bb747026098b28843db6d298
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-05-11 12:11:22 -0600

    chore: revert alert change, start as assembly branch

--- src/components/dev-dot-content/index.tsx
+++ src/components/dev-dot-content/index.tsx
@@ -2,17 +2,12 @@ import { ReactElement } from 'react'
 import classNames from 'classnames'
 import { DevDotContentProps } from './types'
 import s from './dev-dot-content.module.css'
-import sAlerts from './custom-alert-boxes.module.css'
 
 const DevDotContent = ({
   className,
   children,
 }: DevDotContentProps): ReactElement => {
-  return (
-    <div className={classNames(s.root, sAlerts.root, className)}>
-      {children}
-    </div>
-  )
+  return <div className={classNames(s.root, className)}>{children}</div>
 }
 
 export default DevDotContent
