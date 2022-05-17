a6c0c921eeb431848cc97212f6f6701e78e5c0f8
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-05-17 18:38:41 -0400

    Rendering some MDX and marketing content in /waypoint/docs

--- src/pages/waypoint/docs/index.tsx
+++ src/pages/waypoint/docs/index.tsx
@@ -1,3 +1,11 @@
+// Third-party imports
+import { MDXRemoteSerializeResult } from 'next-mdx-remote'
+
+// HashiCorp imports
+import { IconBox16 } from '@hashicorp/flight-icons/svg-react/box-16'
+import { IconTerminalScreen16 } from '@hashicorp/flight-icons/svg-react/terminal-screen-16'
+
+// Global imports
 import waypointData from 'data/waypoint.json'
 import { ProductData } from 'types/products'
 import SidebarSidecarLayout from 'layouts/sidebar-sidecar'
@@ -6,10 +14,15 @@ import IconTileLogo from 'components/icon-tile-logo'
 import Heading from 'components/heading'
 import Text from 'components/text'
 import CardLink from 'components/card-link'
+import ProductDocsSearch from 'views/docs-view/components/product-docs-search'
+import DocsView from 'views/docs-view'
+import CardsGridList from 'components/cards-grid-list'
+import TruncateMaxLines from 'components/truncate-max-lines'
+import devDotStyles from 'components/dev-dot-content/dev-dot-content.module.css'
+
+// Local imports
 import pageContent from './content.json'
 import s from './style.module.css'
-import { IconBox16 } from '@hashicorp/flight-icons/svg-react/box-16'
-import { IconTerminalScreen16 } from '@hashicorp/flight-icons/svg-react/terminal-screen-16'
 
 const basePath = 'docs'
 const baseName = 'Docs'
@@ -33,11 +46,16 @@ const heroCards = [
   },
 ]
 
-const WaypointDocsLanding = () => {
+interface WaypointDocsLandingProps {
+  mdxSource: MDXRemoteSerializeResult
+}
+
+const WaypointDocsLanding = ({ mdxSource }: WaypointDocsLandingProps) => {
   const { pageSubtitle } = pageContent
 
   return (
     <>
+      {__config.flags.enable_product_docs_search ? <ProductDocsSearch /> : null}
       <div className={s.pageHeroWrapper}>
         <div className={s.pageHeroText}>
           <IconTileLogo productSlug="waypoint" />
@@ -66,8 +84,56 @@ const WaypointDocsLanding = () => {
           ))}
         </ul>
       </div>
-      <div>TODO - MDX content</div>
-      <div>TODO - marketing slot</div>
+      <div className={s.waypointMDXWrapper}>
+        <DocsView mdxSource={mdxSource} hideSearch />
+      </div>
+      <div>
+        {pageContent.marketingContentBlocks.map((block) => {
+          if (block.type === 'card-grid') {
+            // TODO move to a separate component
+            return (
+              <section>
+                <Heading
+                  className={devDotStyles.h2}
+                  level={2}
+                  size={400}
+                  weight="bold"
+                >
+                  {block.title}
+                </Heading>
+                {/* TODO add support for description text */}
+                <CardsGridList>
+                  {block.cards.map((card) => (
+                    <li key={card.url}>
+                      <CardLink className={s.cardGridCard} href={card.url}>
+                        <Text
+                          className={s.cardGridCardTitle}
+                          size={200}
+                          weight="semibold"
+                        >
+                          {card.title}
+                        </Text>
+                        <Text
+                          className={s.cardGridCardDescription}
+                          size={100}
+                          weight="regular"
+                        >
+                          <TruncateMaxLines
+                            maxLines={3}
+                            lineHeight="var(--token-typography-body-100-line-height)"
+                          >
+                            {card.description}
+                          </TruncateMaxLines>
+                        </Text>
+                      </CardLink>
+                    </li>
+                  ))}
+                </CardsGridList>
+              </section>
+            )
+          }
+        })}
+      </div>
     </>
   )
 }
