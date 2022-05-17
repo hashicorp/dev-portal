c5fb0b16a5603bae91c0a5fa11bddb762e99bbf7
Kendall Strautman <kendallstrautman@gmail.com>
2022-05-17 09:52:37 -0700

    feat: adds animation to dialog

--- src/components/dialog/index.tsx
+++ src/components/dialog/index.tsx
@@ -1,4 +1,5 @@
 import { DialogOverlay, DialogContent } from '@reach/dialog'
+import { AnimatePresence, motion } from 'framer-motion'
 import { DialogProps } from './types'
 import s from './dialog.module.css'
 
@@ -8,11 +9,35 @@ export default function Dialog({
   onDismiss,
   label,
 }: DialogProps) {
+  const AnimatedDialogOverlay = motion(DialogOverlay)
   return (
-    <DialogOverlay className={s.overlay} isOpen={isOpen} onDismiss={onDismiss}>
-      <DialogContent className={s.content} aria-label={label}>
-        {children}
-      </DialogContent>
-    </DialogOverlay>
+    <AnimatePresence>
+      {isOpen && (
+        <AnimatedDialogOverlay
+          className={s.dialogOverlay}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          onDismiss={onDismiss}
+        >
+          <DialogOverlay
+            className={s.overlay}
+            isOpen={isOpen}
+            onDismiss={onDismiss}
+          >
+            <motion.div
+              initial={{ y: 50 }}
+              animate={{ y: 0 }}
+              exit={{ y: 50 }}
+              transition={{ min: 0, max: 100, bounceDamping: 8 }}
+            >
+              <DialogContent className={s.content} aria-label={label}>
+                {children}
+              </DialogContent>
+            </motion.div>
+          </DialogOverlay>
+        </AnimatedDialogOverlay>
+      )}
+    </AnimatePresence>
   )
 }
