f88bf3805fdb72fe2193f16f7a5d6ed97e687efd
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-05-17 13:44:00 -0400

    chore: move back to brand color

--- src/components/_proxied-dot-io/common/io-home-inline-callout/index.tsx
+++ src/components/_proxied-dot-io/common/io-home-inline-callout/index.tsx
@@ -7,6 +7,7 @@ import { IconArrowRight16 } from '@hashicorp/flight-icons/svg-react/arrow-right-
 import s from './style.module.css'
 
 export interface IoHomeInlineCalloutProps {
+  brand: Products
   link: string
   heading: string
   description: string
@@ -20,15 +21,17 @@ export interface IoHomeInlineCalloutProps {
 }
 
 export default function IoHomeInlineCallout({
+  brand,
   link,
   heading,
   description,
   cta = 'Learn more',
   thumbnail,
 }: IoHomeInlineCalloutProps) {
+  const { themeClass } = useProductMeta(brand)
   return (
     <Link href={link}>
-      <a className={s.inlineCallout}>
+      <a className={classNames(s.inlineCallout, themeClass)}>
         <div className={s.media}>
           <Image
             src={thumbnail.src}
