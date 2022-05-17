40d38a259170c0fdd62868b42c65ce1a13c21132
Kendall Strautman <kendallstrautman@gmail.com>
2022-05-17 09:45:42 -0700

    refactor: extract out styles

--- src/components/dialog/index.tsx
+++ src/components/dialog/index.tsx
@@ -1,25 +1,6 @@
 import { DialogOverlay, DialogContent } from '@reach/dialog'
-import { ReactNode } from 'react'
-
-interface DialogProps {
-  isOpen: boolean
-  onDismiss(): void
-  children: ReactNode
-  label: string
-}
-
-const overlayStyles = {
-  background: 'var(--token-color-border-strong',
-  zIndex: 100,
-}
-
-const dialogStyles = {
-  boxShadow: 'var(--token-surface-higher-box-shadow)',
-  borderRadius: '6px',
-  padding: '24px',
-  maxWidth: '600px',
-  minWidth: '300px',
-}
+import { DialogProps } from './types'
+import s from './dialog.module.css'
 
 export default function Dialog({
   children,
@@ -28,8 +9,8 @@ export default function Dialog({
   label,
 }: DialogProps) {
   return (
-    <DialogOverlay style={overlayStyles} isOpen={isOpen} onDismiss={onDismiss}>
-      <DialogContent style={dialogStyles} aria-label={label}>
+    <DialogOverlay className={s.overlay} isOpen={isOpen} onDismiss={onDismiss}>
+      <DialogContent className={s.content} aria-label={label}>
         {children}
       </DialogContent>
     </DialogOverlay>
