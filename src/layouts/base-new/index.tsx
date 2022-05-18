97784a82091a00881ef73b8c60130df3064b87fc
Kendall Strautman <kendallstrautman@gmail.com>
2022-05-18 16:09:58 -0700

    refactor: remove delayed analytics capture

--- src/layouts/base-new/index.tsx
+++ src/layouts/base-new/index.tsx
@@ -3,16 +3,17 @@ import createConsentManager from '@hashicorp/react-consent-manager/loader'
 import DevAlertBanner from 'components/dev-alert-banner'
 import Footer from 'components/footer'
 import NavigationHeader from 'components/navigation-header'
-import { OptInPlatformOption } from 'components/opt-in-out/types'
-import { handleOptInAnalytics } from 'hooks/use-opt-in-analytics-tracking'
 import CoreDevDotLayout from 'layouts/core-dev-dot-layout'
-import { useRouter } from 'next/router'
 import s from './base-new-layout.module.css'
 
 interface BaseNewLayoutProps {
   /** Defaults to true. If true, the global footer will be shown at the bottom of the page. */
   showFooter?: boolean
 }
+
+const { ConsentManager, openConsentManager } = createConsentManager({
+  preset: 'oss',
+})
 
 const BaseNewLayout: React.FC<BaseNewLayoutProps> = ({
   children,
@@ -21,13 +22,6 @@ const BaseNewLayout: React.FC<BaseNewLayoutProps> = ({
   usePageviewAnalytics({
     siteId: process.env.NEXT_PUBLIC_FATHOM_SITE_ID,
     includedDomains: __config.dev_dot.analytics.included_domains,
-  })
-  const { query } = useRouter()
-  const { ConsentManager, openConsentManager } = createConsentManager({
-    preset: 'oss',
-    onAcceptAll: () => {
-      handleOptInAnalytics(query['optInFrom'] as OptInPlatformOption)
-    },
   })
 
   return (
