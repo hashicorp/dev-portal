f89de6f53912ac6e93888c17d302641c6e2fe655
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-05-19 08:59:20 -0400

    refactor: use safeAnalyticsTrack *praise emoji*

--- src/components/video-embed/helpers/analytics.ts
+++ src/components/video-embed/helpers/analytics.ts
@@ -1,3 +1,5 @@
+import { safeAnalyticsTrack } from 'lib/analytics'
+
 /**
  * video_played event
  *
@@ -10,29 +12,10 @@ function videoPlayedEvent({
   video_url: string
   video_progress: number
 }) {
-  if (checkAnalyticsConsent()) {
-    window.analytics.track('Video Played', {
-      video_url,
-      video_progress,
-    })
-  }
-}
-
-/**
- * Determine if window.analytics.track
- * is a function we can call.
- *
- * With consent, window.analytics.track is initialized in our
- * @hashicorp/react-consent-manager component
- * https://github.com/hashicorp/react-components/blob/c69c29bbcb9e2718c6864336326e4ddfb66822ba/packages/consent-manager/load.js#L132
- * https://segment.com/docs/connections/sources/catalog/libraries/website/javascript/#track
- *
- * @returns true if window.analytics.track is a function, false otherwise
- */
-function checkAnalyticsConsent() {
-  return (
-    window && window.analytics && typeof window.analytics.track == 'function'
-  )
+  safeAnalyticsTrack('Video Played', {
+    video_url,
+    video_progress,
+  })
 }
 
 export { videoPlayedEvent }
