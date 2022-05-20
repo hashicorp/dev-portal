ca11e3baa3a26bac091fbda3a9ce2a9e1d6841bf
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-05-19 21:42:58 -0400

    Using generateGetStaticProps helper in vault and waypoint

--- src/pages/vault/docs/index.tsx
+++ src/pages/vault/docs/index.tsx
@@ -1,39 +1,19 @@
 import vaultData from 'data/vault.json'
-import { ProductData } from 'types/products'
 import SidebarSidecarLayout from 'layouts/sidebar-sidecar'
-import { getStaticGenerationFunctions } from 'layouts/sidebar-sidecar/server'
 import ProductRootDocsPathLanding from 'views/product-root-docs-path-landing'
+import { generateGetStaticProps } from 'views/product-root-docs-path-landing/server'
 import pageContent from './content.json'
-
-const basePath = 'docs'
-const baseName = 'Docs'
-const product = vaultData as ProductData
 
 const VaultDocsLanding = () => {
   return <ProductRootDocsPathLanding pageContent={pageContent} />
 }
 
-const { getStaticProps: generatedGetStaticProps } =
-  getStaticGenerationFunctions({
-    product,
-    basePath,
-    baseName,
-  })
-
-export async function getStaticProps({ context }) {
-  // TODO: remove the any
-  const generatedProps = (await generatedGetStaticProps({
-    ...context,
-    params: { page: [] },
-  })) as any
-
-  generatedProps.props.layoutProps.githubFileUrl = null
-
-  // TODO handle rendering the sidecar in a follow-up PR
-  generatedProps.props.layoutProps.sidecarSlot = null
-
-  return generatedProps
-}
+const getStaticProps = generateGetStaticProps({
+  baseName: 'Docs',
+  basePath: 'docs',
+  product: vaultData,
+})
 
 VaultDocsLanding.layout = SidebarSidecarLayout
+export { getStaticProps }
 export default VaultDocsLanding
--- src/pages/waypoint/docs/index.tsx
+++ src/pages/waypoint/docs/index.tsx
@@ -1,16 +1,11 @@
 import { MDXRemoteSerializeResult } from 'next-mdx-remote'
 import waypointData from 'data/waypoint.json'
-import { ProductData } from 'types/products'
 import SidebarSidecarLayout from 'layouts/sidebar-sidecar'
-import { getStaticGenerationFunctions } from 'layouts/sidebar-sidecar/server'
 import DocsView from 'views/docs-view'
 import ProductRootDocsPathLanding from 'views/product-root-docs-path-landing'
+import { generateGetStaticProps } from 'views/product-root-docs-path-landing/server'
 import pageContent from './content.json'
 import s from './style.module.css'
-
-const basePath = 'docs'
-const baseName = 'Docs'
-const product = waypointData as ProductData
 
 interface WaypointDocsLandingProps {
   mdxSource: MDXRemoteSerializeResult
@@ -30,34 +25,11 @@ const WaypointDocsLanding = ({ mdxSource }: WaypointDocsLandingProps) => {
   )
 }
 
-const { getStaticProps: generatedGetStaticProps } =
-  getStaticGenerationFunctions({
-    product,
-    basePath,
-    baseName,
-  })
-
-/**
- * Wrapper for `generatedGetStaticProps`. Handles passing the correct `params`
- * property to `generatedGetStaticProps`.
- *
- * Also handles clearing the following `layoutProps` returned in the `props`
- * returned by `generatedGetStaticProps`: `githubFileUrl` and `sidecarSlot`.
- */
-async function getStaticProps({ context }) {
-  // TODO: remove the any
-  const generatedProps = (await generatedGetStaticProps({
-    ...context,
-    params: { page: [] },
-  })) as any
-
-  generatedProps.props.layoutProps.githubFileUrl = null
-
-  // TODO handle rendering the sidecar in a follow-up PR
-  generatedProps.props.layoutProps.sidecarSlot = null
-
-  return generatedProps
-}
+const getStaticProps = generateGetStaticProps({
+  baseName: 'Docs',
+  basePath: 'docs',
+  product: waypointData,
+})
 
 WaypointDocsLanding.layout = SidebarSidecarLayout
 export { getStaticProps }
