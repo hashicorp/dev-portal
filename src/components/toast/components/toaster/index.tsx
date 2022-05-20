685896233a58c32442651836ca0508f79dc9b4ec
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-05-19 22:43:44 -0400

    docs: add description on Toaster component

--- src/components/toast/components/toaster/index.tsx
+++ src/components/toast/components/toaster/index.tsx
@@ -1,8 +1,12 @@
 import { Toaster as ReactHotToaster } from 'react-hot-toast'
 
 /**
- * Toaster
- * @description toaster component with predefined settings for positioning and toast gutters. Removes default toast styling from react-hot-toast and manages styling within the Notification components.
+ * react-hot-toast's Toaster component, with predefined settings.
+ * Removes default toast styling from react-hot-toast,
+ * and manages styling within the Notification components.
+ *
+ * Note: largely duplicative of a similar component in @hashicorp/react-toast:
+ * https://github.com/hashicorp/react-components/blob/main/packages/toast/toaster/index.tsx
  */
 export default function Toaster() {
   return (
