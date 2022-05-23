9843b9dd625fed2d5a2e8b42f2b57b063be1c829
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-05-22 22:12:06 -0400

    feat: remove interactive toast, mv to other branch

--- src/components/toast/index.tsx
+++ src/components/toast/index.tsx
@@ -1,16 +1,18 @@
 import { useRouter } from 'next/router'
-import { Fragment, ReactNode, useCallback, useEffect } from 'react'
-import { DialogOverlay, DialogContent } from './_temp-forked-reach-dialog'
+import { useCallback, useEffect } from 'react'
 import { default as reactHotToast, Toast } from 'react-hot-toast'
 import Toaster from './components/toaster'
 import ToastDisplay from './components/toast-display'
 import { ToastDisplayProps, ToastColor } from './components/toast-display/types'
 
+const AUTO_DISMISS_DEFAULT = 4000
+
 interface toastOptions {
   /**
-   * Defaults to true. Set to false to prevent the toast from auto-dismissing.
+   * Set the number of milliseconds after which to auto-dismiss.
+   * Defaults to 4000. Set to "false" for show-till-dismissed toast.
    */
-  autoDismiss?: boolean
+  autoDismiss?: number | boolean
   /**
    * Flag that there is interactive content in the toast.
    * If the toast contains interactive content,
@@ -35,17 +37,15 @@ function toast({
   description,
   icon,
   title,
-  autoDismiss = true,
-  onDismissCallback = () => null,
-  isInteractive = Boolean(actions),
+  autoDismiss = AUTO_DISMISS_DEFAULT,
 }: Omit<ToastDisplayProps, 'onDismiss'> & toastOptions) {
   //
   return reactHotToast(
     (t: Toast) => {
       const router = useRouter()
 
+      // Allows the toast to dismiss itself
       const dismissSelf = useCallback(() => {
-        onDismissCallback()
         reactHotToast.remove(t.id)
       }, [t.id])
 
@@ -58,51 +58,26 @@ function toast({
         }
       }, [router.events, dismissSelf])
 
-      // TODO: reconsider what the dialog aria-label should be
-      const dialogAriaLabel =
-        title || (typeof description == 'string' ? description : 'Notification')
-
-      // For interactive content, we wrap the toast display component
-      // in a reach-ui dialog. This captures focus.
-      const Wrapper = isInteractive
-        ? ({ children }: { children: ReactNode }) => (
-            <DialogOverlay onDismiss={dismissSelf} dangerouslyBypassScrollLock>
-              <DialogContent aria-label={dialogAriaLabel}>
-                {children}
-              </DialogContent>
-            </DialogOverlay>
-          )
-        : Fragment
       return (
-        <Wrapper>
-          <ToastDisplay
-            actions={actions}
-            color={color}
-            description={description}
-            icon={icon}
-            onDismiss={dismissSelf}
-            title={title}
-          >
-            {children}
-          </ToastDisplay>
-        </Wrapper>
+        <ToastDisplay
+          actions={actions}
+          color={color}
+          description={description}
+          icon={icon}
+          onDismiss={dismissSelf}
+          title={title}
+        >
+          {children}
+        </ToastDisplay>
       )
     },
     {
-      duration: autoDismiss ? 4000 : Infinity,
-      ariaProps: {
-        'aria-live': isInteractive
-          ? 'off' // Interactive content has a dialog role, so no announcing
-          : color == 'critical' // Maybe there's some other heuristic here?
-          ? 'assertive'
-          : 'polite',
-        role: isInteractive
-          ? // Note: we use role="dialog" in an enclosed ReachUI Dialog
-            //  for interactive content. So want no aria-role on this element,
-            // but react-hot-toast's types don't allow it. So we force it.
-            (undefined as unknown as Toast['ariaProps']['role'])
-          : 'status',
-      },
+      duration:
+        autoDismiss == false
+          ? Infinity
+          : autoDismiss == true
+          ? AUTO_DISMISS_DEFAULT
+          : autoDismiss,
     }
   )
 }
