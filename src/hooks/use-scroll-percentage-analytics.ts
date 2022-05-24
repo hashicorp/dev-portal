ae2536f2fbf684be8f95524c29d8bc59b747de31
Bryce Kalow <bkalow@hashicorp.com>
2022-05-24 08:28:21 -0500

    simplify return of effect

--- src/hooks/use-scroll-percentage-analytics.ts
+++ src/hooks/use-scroll-percentage-analytics.ts
@@ -63,8 +63,6 @@ export default function useScrollPercentageAnalytics() {
 
     const cleanup = setupScrollPercentageTracking()
 
-    return () => {
-      cleanup()
-    }
+    return cleanup
   }, [])
 }
