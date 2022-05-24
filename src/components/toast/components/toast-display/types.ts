6ec6f78a502a6ed323834d8239c6c7503f09441a
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-05-24 10:05:00 -0400

    chore: alphabetize and format type comments

--- src/components/toast/components/toast-display/types.ts
+++ src/components/toast/components/toast-display/types.ts
@@ -10,26 +10,26 @@ export enum ToastColor {
 
 export interface ToastDisplayProps {
   /**
-   * Title text for the toast
+   * Color theme for the toast. Defaults to "neutral".
    */
-  title?: string
+  color?: ToastColor
+
   /**
    * Description text. Note that HTML is not yet supported, but may be in the
    * future. When a title is present, the description is shown below the title.
    */
   description?: string
+
   /**
    * A function to dismiss the toast that wraps this display component.
    */
   dismissSelf: () => void
-  /**
-   * Color theme for the toast. Defaults to "neutral".
-   */
-  color?: ToastColor
+
   /**
    * Optional icon to render beside the toast's content area.
    */
   icon?: ReactNode
+
   /**
    * Optional render prop for actions below the toast's title and description.
    * Expects one or many Button, ButtonLink, or StandaloneLink components.
@@ -37,4 +37,9 @@ export interface ToastDisplayProps {
    * to ensure correct spacing.
    */
   renderActions?: (props: { dismissSelf: () => void }) => ReactNode
+
+  /**
+   * Title text for the toast
+   */
+  title?: string
 }
