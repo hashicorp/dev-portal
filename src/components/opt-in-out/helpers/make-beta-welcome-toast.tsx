666b07634e427da41d48b38e945d0d619a4e2764
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-05-24 10:03:17 -0400

    feat: remove interactivity from opt-in toast

--- src/components/opt-in-out/helpers/make-beta-welcome-toast.tsx
+++ src/components/opt-in-out/helpers/make-beta-welcome-toast.tsx
@@ -1,7 +1,4 @@
-import { IconArrowUpRight16 } from '@hashicorp/flight-icons/svg-react/arrow-up-right-16'
 import { toast, ToastColor } from 'components/toast'
-import Button from 'components/button'
-import { ToastDisplayProps } from 'components/toast/components/toast-display/types'
 import { OptInPlatformOption, PlatformOptionTitles } from '../types'
 
 export default function makeBetaWelcomeToast(
@@ -16,28 +13,5 @@ export default function makeBetaWelcomeToast(
     title: `Welcome to the ${platformName} Website Beta!`,
     description:
       'You can leave the Beta at anytime by clicking the Leave Beta button near the start of the main content area.',
-    renderActions: ({
-      dismissSelf,
-    }: {
-      dismissSelf: ToastDisplayProps['dismissSelf']
-    }) => (
-      <>
-        <Button
-          color="secondary"
-          onClick={dismissSelf}
-          size="small"
-          text="Continue"
-        />
-        <Button
-          color="tertiary"
-          icon={<IconArrowUpRight16 />}
-          iconPosition="trailing"
-          onClick={handleOptOut}
-          size="small"
-          text="Leave beta"
-        />
-      </>
-    ),
-    autoDismiss: false,
   })
 }
