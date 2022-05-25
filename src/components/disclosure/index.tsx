62ee578ef7cc268be3329618554f3752b00d2e77
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-05-24 20:40:09 -0400

    Adding Disclosure and NavigationDisclosure stubs

---
+++ src/components/disclosure/index.tsx
@@ -0,0 +1,5 @@
+const Disclosure = () => {
+  return <h1>Hello from Disclosure!</h1>
+}
+
+export default Disclosure
---
+++ src/components/navigation-disclosure/index.tsx
@@ -0,0 +1,5 @@
+const NavigationDisclosure = () => {
+  return <h1>Hello from NavigationDisclosure!</h1>
+}
+
+export default NavigationDisclosure
