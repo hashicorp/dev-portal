56b1104cd48f5cb581878375e2a7cd7cb72d1b22
Brandon Romano <brandon@hashicorp.com>
2022-05-25 10:01:25 -0700

    Adds google search console site verificiation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -51,6 +51,10 @@ export default function App({ Component, pageProps, layoutProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="google-site-verification"
+          content="zRQZqfAsOX-ypXfU0mzAIzb5rUvj5fA4Zw2jWJRN-JI"
+        />
       </Head>
       <SSRProvider>
         <ErrorBoundary FallbackComponent={Error}>
