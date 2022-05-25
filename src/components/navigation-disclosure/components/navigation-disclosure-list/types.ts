85f80e866483b95c93b44a3f59d4e57857755f82
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-05-25 18:28:39 -0400

    Adding NavigationDisclosureList subcomponent

---
+++ src/components/navigation-disclosure/components/navigation-disclosure-list/types.ts
@@ -0,0 +1,8 @@
+type NativeULProps = JSX.IntrinsicElements['ul']
+
+interface NavigationDisclosureListProps {
+  children: NativeULProps['children']
+  className?: NativeULProps['className']
+}
+
+export type { NavigationDisclosureListProps }
