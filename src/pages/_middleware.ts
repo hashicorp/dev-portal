62a67719b748d4e487a5fcf1074f8f791ccc6285
Bryce Kalow <bkalow@hashicorp.com>
2022-05-25 15:43:36 -0500

    abstract out constant

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -2,34 +2,9 @@ import { NextResponse } from 'next/server'
 import type { NextFetchEvent, NextRequest } from 'next/server'
 import redirects from 'data/_redirects.generated.json'
 import { OptInPlatformOption } from 'components/opt-in-out/types'
+import { HOSTNAME_MAP } from 'constants/hostname-map'
 
 const OPT_IN_MAX_AGE = 60 * 60 * 24 * 180 // 180 days
-
-const HOSTNAME_MAP = {
-  'www.boundaryproject.io': 'boundary',
-  'test-bd.hashi-mktg.com': 'boundary',
-
-  'www.consul.io': 'consul',
-  'test-cs.hashi-mktg.com': 'consul',
-
-  'www.nomadproject.io': 'nomad',
-  'test-nm.hashi-mktg.com': 'nomad',
-
-  'www.packer.io': 'packer',
-  'test-pk.hashi-mktg.com': 'packer',
-
-  'docs.hashicorp.com': 'sentinel',
-  'test-st.hashi-mktg.com': 'sentinel',
-
-  'www.vagrantup.com': 'vagrant',
-  'test-vg.hashi-mktg.com': 'vagrant',
-
-  'www.vaultproject.io': 'vault',
-  'test-vt.hashi-mktg.com': 'vault',
-
-  'www.waypointproject.io': 'waypoint',
-  'test-wp.hashi-mktg.com': 'waypoint',
-}
 
 function determineProductSlug(req: NextRequest): string {
   // .io preview on dev portal
--- src/pages/_proxied-dot-io/_middleware.ts
+++ src/pages/_proxied-dot-io/_middleware.ts
@@ -1,30 +1,5 @@
 import { NextFetchEvent, NextRequest, NextResponse } from 'next/server'
-
-const HOSTNAME_MAP = {
-  'www.boundaryproject.io': 'boundary',
-  'test-bd.hashi-mktg.com': 'boundary',
-
-  'www.consul.io': 'consul',
-  'test-cs.hashi-mktg.com': 'consul',
-
-  'www.nomadproject.io': 'nomad',
-  'test-nm.hashi-mktg.com': 'nomad',
-
-  'www.packer.io': 'packer',
-  'test-pk.hashi-mktg.com': 'packer',
-
-  'docs.hashicorp.com': 'sentinel',
-  'test-st.hashi-mktg.com': 'sentinel',
-
-  'www.vagrantup.com': 'vagrant',
-  'test-vg.hashi-mktg.com': 'vagrant',
-
-  'www.vaultproject.io': 'vault',
-  'test-vt.hashi-mktg.com': 'vault',
-
-  'www.waypointproject.io': 'waypoint',
-  'test-wp.hashi-mktg.com': 'waypoint',
-}
+import { HOSTNAME_MAP } from 'constants/hostname-map'
 
 export function middleware(req: NextRequest, ev: NextFetchEvent): NextResponse {
   const params = req.nextUrl.searchParams
