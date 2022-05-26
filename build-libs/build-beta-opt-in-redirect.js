dde386f8fd615d191ec96745201628ca0ae42587
Bryce Kalow <bkalow@hashicorp.com>
2022-05-26 13:34:53 -0500

    Revert "test redirect for preview"
    This reverts commit 6788f924f59716de5bd83f0eb07fa7caff14ce18.

--- build-libs/build-beta-opt-in-redirect.js
+++ build-libs/build-beta-opt-in-redirect.js
@@ -1,6 +1,6 @@
 const { unflatten } = require('flat')
 const { loadHashiConfigForEnvironment } = require('../config')
-// const proxySettings = require('./proxy-settings')
+const proxySettings = require('./proxy-settings')
 
 const __config = unflatten(loadHashiConfigForEnvironment())
 
@@ -23,14 +23,9 @@ function buildBetaProductOptInRedirect(product, basePaths) {
         value: 'true',
       },
       {
-        type: 'cookie',
-        key: 'io_preview',
-        value: product,
+        type: 'host',
+        value: proxySettings[product].host,
       },
-      // {
-      //   type: 'host',
-      //   value: proxySettings[product].host,
-      // },
     ],
   }
 }
