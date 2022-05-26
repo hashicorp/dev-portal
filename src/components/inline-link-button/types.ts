d9961411624b37a50d47516942120b799b22f85c
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-05-26 17:45:50 -0400

    docs: format tweaks for inline-link-button types

--- src/components/inline-link-button/types.ts
+++ src/components/inline-link-button/types.ts
@@ -4,15 +4,14 @@ type NativeButtonProps = JSX.IntrinsicElements['button']
 
 export interface InlineLinkButtonProps {
   /**
-   * A string of one or more classnames. Is appended last to list of classnames
-   * passed to the button element.
-   */
-  className?: NativeButtonProps['className']
-
-  /**
    * Children to render within the `<button>` element.
    */
   children: ReactNode
+
+  /**
+   * A string of one or more classnames to append to the button element.
+   */
+  className?: NativeButtonProps['className']
 
   /**
    * The function invoked after the button is clicked. Passed directly to the
