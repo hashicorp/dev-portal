af2333041f34655f6adea2f6cba01257bedb6662
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-05-26 17:35:06 -0400

    Wrapping NavigationDisclosureActivator in forwardRef

--- src/components/navigation-disclosure/components/navigation-disclosure-activator/index.tsx
+++ src/components/navigation-disclosure/components/navigation-disclosure-activator/index.tsx
@@ -1,17 +1,37 @@
-import { DisclosureActivator } from 'components/disclosure'
+import { forwardRef } from 'react'
+import {
+  DisclosureActivator,
+  DisclosureActivatorForwardedRef,
+} from 'components/disclosure'
 import { NavigationDisclosureActivatorProps } from './types'
 
-const NavigationDisclosureActivator = ({
-  ariaLabel,
-  children,
-  className,
-}: NavigationDisclosureActivatorProps) => {
-  return (
-    <DisclosureActivator ariaLabel={ariaLabel} className={className}>
-      {children}
-    </DisclosureActivator>
-  )
-}
+/**
+ * Component for rendering the always-visible activator within a
+ * `NavigationDisclosure`.
+ *
+ * @see https://developer.hashi-mktg.com/swingset/components/disclosure
+ *
+ * @TODO
+ *  - add onKeyDown listener (for `NavigationHeaderDropdownMenu`)
+ *  - add onMouseEnter listener (for `NavigationHeaderDropdownMenu`)
+ */
+// eslint-disable-next-line react/display-name
+const NavigationDisclosureActivator = forwardRef(
+  (
+    { ariaLabel, children, className }: NavigationDisclosureActivatorProps,
+    ref: DisclosureActivatorForwardedRef
+  ) => {
+    return (
+      <DisclosureActivator
+        ariaLabel={ariaLabel}
+        className={className}
+        ref={ref}
+      >
+        {children}
+      </DisclosureActivator>
+    )
+  }
+)
 
 export type { NavigationDisclosureActivatorProps }
 export default NavigationDisclosureActivator
