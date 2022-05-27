d4574a93bb9061f0c86bd0b39b3803fdb2ea8e99
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-05-27 13:55:05 -0400

    Using NavigationDisclosure and friends

--- src/components/docs-version-switcher/index.tsx
+++ src/components/docs-version-switcher/index.tsx
@@ -1,17 +1,22 @@
 import { ProductWithCurrentRootDocsPath } from 'types/products'
 import { useCurrentProduct } from 'contexts'
 import useCurrentPath from 'hooks/use-current-path'
-// import NavigationDisclosure from 'components/navigation-disclosure'
+import NavigationDisclosure, {
+  NavigationDisclosureActivator,
+  NavigationDisclosureContent,
+  NavigationDisclosureLink,
+  NavigationDisclosureList,
+  NavigationDisclosureListItem,
+} from 'components/navigation-disclosure'
+import Text from 'components/text'
 import { DocsVersionSwitcherOption, DocsVersionSwitcherProps } from './types'
 import {
   getTargetPath,
   getVersionFromPath,
   removeVersionFromPath,
 } from './helpers'
-
-/**
- * !TODO - VERY WIP & COPY-PASTA'd from another branch
- */
+import { IconCaret16 } from '@hashicorp/flight-icons/svg-react/caret-16'
+import s from './docs-version-switcher.module.css'
 
 const DocsVersionSwitcher = ({ options }: DocsVersionSwitcherProps) => {
   const currentProduct = useCurrentProduct() as ProductWithCurrentRootDocsPath
@@ -31,13 +36,14 @@ const DocsVersionSwitcher = ({ options }: DocsVersionSwitcherProps) => {
     selectedOption = options[0]
   }
 
+  // TODO - do this "inline" so we're not iterating once for data and second for render
   const navigationDisclosureLinks = options.map(
     (option: DocsVersionSwitcherOption) => {
-      let url: string
+      let href: string
       if (option.isLatest) {
-        url = removeVersionFromPath(currentPath)
+        href = removeVersionFromPath(currentPath)
       } else {
-        url = getTargetPath({
+        href = getTargetPath({
           basePath: `${currentProduct.slug}/${currentProduct.currentRootDocsPath.path}`,
           asPath: currentPath,
           version: option.version,
@@ -46,59 +52,47 @@ const DocsVersionSwitcher = ({ options }: DocsVersionSwitcherProps) => {
 
       return {
         text: option.label,
-        url,
+        href,
       }
     }
   )
 
+  // TODO style the pieces
   return (
     <nav>
-      {/* <NavigationDisclosure links={navigationDisclosureLinks}>
-        {selectedOption.label}
-      </NavigationDisclosure> */}
+      <NavigationDisclosure>
+        {/* TODO add aria label */}
+        <NavigationDisclosureActivator>
+          <Text
+            asElement="span"
+            className={s.buttonText}
+            size={200}
+            weight="regular"
+          >
+            {selectedOption.label}
+          </Text>
+          <span className={s.buttonIcon}>
+            <IconCaret16 />
+          </span>
+        </NavigationDisclosureActivator>
+        <NavigationDisclosureContent>
+          <NavigationDisclosureList>
+            {navigationDisclosureLinks.map(({ href, text }) => {
+              return (
+                <NavigationDisclosureListItem key={href}>
+                  {/* TODO calculate isActive when the link array is built */}
+                  <NavigationDisclosureLink href={href} isActive={false}>
+                    {text}
+                  </NavigationDisclosureLink>
+                </NavigationDisclosureListItem>
+              )
+            })}
+          </NavigationDisclosureList>
+        </NavigationDisclosureContent>
+      </NavigationDisclosure>
     </nav>
   )
 }
 
 export type { DocsVersionSwitcherProps }
 export default DocsVersionSwitcher
-
-// import { IconCaret16 } from '@hashicorp/flight-icons/svg-react/caret-16'
-// import { ProductWithCurrentRootDocsPath } from 'types/products'
-// import { useCurrentProduct } from 'contexts'
-// import useCurrentPath from 'hooks/use-current-path'
-// import NavigationDisclosure from 'components/navigation-disclosure'
-// import Text from 'components/text'
-// import { DocsVersionSwitcherOption, DocsVersionSwitcherProps } from './types'
-// import {
-//   getTargetPath,
-//   getVersionFromPath,
-//   removeVersionFromPath,
-// } from './helpers'
-// import s from './docs-version-switcher.module.css'
-
-// const DocsVersionSwitcher = ({ options }: DocsVersionSwitcherProps) => {
-//   const currentProduct = useCurrentProduct() as ProductWithCurrentRootDocsPath
-// 	@@ -48,9 +51,22 @@ const DocsVersionSwitcher = ({ options }: DocsVersionSwitcherProps) => {
-//   )
-
-//   return (
-//     <nav className={s.root}>
-//       <NavigationDisclosure
-//         activatorClassName={s.button}
-//         links={navigationDisclosureLinks}
-//       >
-//         <Text
-//           asElement="span"
-//           className={s.buttonText}
-//           size={200}
-//           weight="regular"
-//         >
-//           {selectedOption.label}
-//         </Text>
-//         <span className={s.buttonIcon}>
-//           <IconCaret16 />
-//         </span>
-//       </NavigationDisclosure>
-//     </nav>
-//   )
