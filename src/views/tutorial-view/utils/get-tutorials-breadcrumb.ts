4b2920660f2a9ad448ecf0ce3db0313312cc35cf
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2022-05-27 16:47:39 -0400

    fix: prevent opt-in-slot shrink, truncate breadcrumbs (#492)
    * fix: add missing types packages
    
    * feat: WIP add truncation to breadcrumb items
    
    * fix: prevent opt-in-out slot from shrink
    
    * fix: add item truncation to breadcrumb-bar
    
    * docs: add comment on display: flex
    
    * fix: breadcrumb styles to match HDS
    
    * fix: isCurrentPage for tutorials, product landing, docs landing
    
    * fix: breadcrumb test slip up
    
    * fix: don't link current page items
    
    * fix: add padding to breadcrumb list items
    
    * fix: selected styling
    
    * fix: make current page link active

--- src/views/tutorial-view/utils/get-tutorials-breadcrumb.ts
+++ src/views/tutorial-view/utils/get-tutorials-breadcrumb.ts
@@ -16,22 +16,28 @@ export function getTutorialsBreadcrumb({
   collection,
   tutorial,
 }: TutorialsBreadcrumbOptions): BreadcrumbLink[] {
-  const paths = [
+  const paths: BreadcrumbLink[] = [
     { title: 'Developer', url: '/' },
     { title: product.name, url: `/${product.filename}` },
-    { title: 'Tutorials', url: `/${product.filename}/tutorials` },
+    {
+      title: 'Tutorials',
+      url: `/${product.filename}/tutorials`,
+      isCurrentPage: !collection, // If no collection, we're on product landing
+    },
   ]
 
   if (collection) {
     paths.push({
       title: collection.name,
       url: `/${product.filename}/tutorials/${collection.filename}`,
+      isCurrentPage: !tutorial, // If no tutorial, we're on collection
     })
 
     if (tutorial) {
       paths.push({
         title: tutorial.name,
         url: `/${product.filename}/tutorials/${collection.filename}/${tutorial.filename}`,
+        isCurrentPage: true,
       })
     }
   }
