14fd62834d94d345be9b2e1dea03d1cb47ace4d6
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-05-27 08:45:12 -0400

    refactor: remove inline-link-button from 404 page

--- src/views/error-views/dev-dot-404/index.tsx
+++ src/views/error-views/dev-dot-404/index.tsx
@@ -1,10 +1,8 @@
 import React, { ReactElement } from 'react'
-import { useRouter } from 'next/router'
 import { IconWaypointColor16 } from '@hashicorp/flight-icons/svg-react/waypoint-color-16'
 import { IconVaultColor16 } from '@hashicorp/flight-icons/svg-react/vault-color-16'
 import { IconHome16 } from '@hashicorp/flight-icons/svg-react/home-16'
 import { useErrorPageAnalytics } from '@hashicorp/react-error-view'
-import InlineLinkButton from 'components/inline-link-button'
 import CardsGridList from 'components/cards-grid-list'
 import IconCardLink, { IconCardLinkProps } from 'components/icon-card-link'
 import {
@@ -19,7 +17,6 @@ import s from './dev-dot-404.module.css'
  * Generic 404 error view content for use in dev-dot.
  */
 export function DevDot404(): ReactElement {
-  const router = useRouter()
   useErrorPageAnalytics(404)
 
   return (
@@ -28,9 +25,8 @@ export function DevDot404(): ReactElement {
         We couldn&apos;t find the page you&apos;re looking&nbsp;for.
       </ErrorViewH1>
       <ErrorViewParagraph>
-        Please check the url you entered for typos,{' '}
-        <InlineLinkButton onClick={router.back}>go back</InlineLinkButton> to
-        the page you came from, or go to one of the pages below.
+        Please check the url you entered for typos, go back to the page you came
+        from, or go to one of the pages below.
       </ErrorViewParagraph>
       <div className={s.cards}>
         <CardsGridList>
