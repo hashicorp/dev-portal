903333eddafc5592e457ff710959dfb2da61fb80
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-05-27 17:12:53 -0400

    Styling DocsVersionSwitcher

--- src/components/docs-version-switcher/index.tsx
+++ src/components/docs-version-switcher/index.tsx
@@ -1,3 +1,5 @@
+import classNames from 'classnames'
+import { IconCaret16 } from '@hashicorp/flight-icons/svg-react/caret-16'
 import { ProductWithCurrentRootDocsPath } from 'types/products'
 import { useCurrentProduct } from 'contexts'
 import useCurrentPath from 'hooks/use-current-path'
@@ -15,7 +17,6 @@ import {
   getVersionFromPath,
   removeVersionFromPath,
 } from './helpers'
-import { IconCaret16 } from '@hashicorp/flight-icons/svg-react/caret-16'
 import s from './docs-version-switcher.module.css'
 
 const DocsVersionSwitcher = ({ options }: DocsVersionSwitcherProps) => {
@@ -60,28 +61,40 @@ const DocsVersionSwitcher = ({ options }: DocsVersionSwitcherProps) => {
   // TODO style the pieces
   return (
     <nav>
-      <NavigationDisclosure>
+      <NavigationDisclosure
+        className={(isOpen: boolean) => {
+          return classNames(s.root, {
+            [s['root-expanded']]: isOpen,
+          })
+        }}
+      >
         {/* TODO add aria label */}
-        <NavigationDisclosureActivator>
-          <Text
-            asElement="span"
-            className={s.buttonText}
-            size={200}
-            weight="regular"
-          >
-            {selectedOption.label}
-          </Text>
-          <span className={s.buttonIcon}>
-            <IconCaret16 />
+        <NavigationDisclosureActivator className={s.activator}>
+          <span className={s.activatorTextWrapper}>
+            <Text
+              asElement="span"
+              className={s.activatorText}
+              size={200}
+              weight="regular"
+            >
+              {selectedOption.label}
+            </Text>
+            <span className={s.activatorIcon}>
+              <IconCaret16 />
+            </span>
           </span>
         </NavigationDisclosureActivator>
         <NavigationDisclosureContent>
-          <NavigationDisclosureList>
+          <NavigationDisclosureList className={s.optionList}>
             {navigationDisclosureLinks.map(({ href, text }) => {
               return (
                 <NavigationDisclosureListItem key={href}>
                   {/* TODO calculate isActive when the link array is built */}
-                  <NavigationDisclosureLink href={href} isActive={false}>
+                  <NavigationDisclosureLink
+                    className={s.optionLink}
+                    href={href}
+                    isActive={false}
+                  >
                     {text}
                   </NavigationDisclosureLink>
                 </NavigationDisclosureListItem>
