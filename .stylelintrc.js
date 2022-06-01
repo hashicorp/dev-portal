6967891bab503326791fe5ced617468408ec9974
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-06-01 18:23:08 -0400

    Preventing disables and fixes

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -6,8 +6,10 @@ module.exports = {
         outline: ['none', '/^0/'],
       },
       {
+        disableFix: true,
         message:
           'Do not remove outlines. Set them to be transparent instead. Removing outlines makes focus indicators inaccessible in high contrast modes.',
+        reportDisables: true,
       },
     ],
   },
