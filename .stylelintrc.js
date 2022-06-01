37c8addd3883e45cdd4933a3a20442857097f96c
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-06-01 15:48:31 -0400

    Adding declaration-property-value-disallowed-list for outline

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,3 +1,14 @@
 module.exports = {
   extends: ['@hashicorp/platform-cli/config/stylelint.config'],
+  rules: {
+    'declaration-property-value-disallowed-list': [
+      {
+        outline: ['none', '/^0/'],
+      },
+      {
+        message:
+          'Do not remove outlines. Set them to be transparent instead. Removing outlines makes focus indicators inaccessible in high contrast modes.',
+      },
+    ],
+  },
 }
