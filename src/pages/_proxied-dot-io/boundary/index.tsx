6e4d6f3cdeb85a882584426b9f375c374d71a2c7
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-06-02 16:49:11 -0400

    init

--- src/pages/_proxied-dot-io/boundary/index.tsx
+++ src/pages/_proxied-dot-io/boundary/index.tsx
@@ -14,162 +14,162 @@ import s from './home/home.module.css'
 import BoundaryIoLayout from 'layouts/_proxied-dot-io/boundary'
 
 export default function Homepage({ data }): React.ReactElement {
-  const {
-    seo,
-    heroHeading,
-    heroDescription,
-    heroCtas,
-    heroCards,
-    introHeading,
-    introDescription,
-    introFeatures,
-    introVideo,
-    inPracticeHeading,
-    inPracticeDescription,
-    inPracticeCards,
-    inPracticeCtaHeading,
-    inPracticeCtaDescription,
-    inPracticeCtaLink,
-    inPracticeCtaImage,
-    useCasesHeading,
-    useCasesDescription,
-    useCasesCards,
-    caseStudiesHeading,
-    caseStudiesDescription,
-    caseStudiesFeatured,
-    caseStudiesLinks,
-    callToActionHeading,
-    callToActionDescription,
-    callToActionCtas,
-    preFooterHeading,
-    preFooterDescription,
-    preFooterCtas,
-  } = data
-  const _introVideo = introVideo[0]
+	const {
+		seo,
+		heroHeading,
+		heroDescription,
+		heroCtas,
+		heroCards,
+		introHeading,
+		introDescription,
+		introFeatures,
+		introVideo,
+		inPracticeHeading,
+		inPracticeDescription,
+		inPracticeCards,
+		inPracticeCtaHeading,
+		inPracticeCtaDescription,
+		inPracticeCtaLink,
+		inPracticeCtaImage,
+		useCasesHeading,
+		useCasesDescription,
+		useCasesCards,
+		caseStudiesHeading,
+		caseStudiesDescription,
+		caseStudiesFeatured,
+		caseStudiesLinks,
+		callToActionHeading,
+		callToActionDescription,
+		callToActionCtas,
+		preFooterHeading,
+		preFooterDescription,
+		preFooterCtas,
+	} = data
+	const _introVideo = introVideo[0]
 
-  return (
-    <>
-      <Head>{renderMetaTags(seo)}</Head>
+	return (
+		<>
+			<Head>{renderMetaTags(seo)}</Head>
 
-      <IoHomeHero
-        pattern="/boundary/img/home-hero-pattern.svg"
-        brand="boundary"
-        heading={heroHeading}
-        description={heroDescription}
-        ctas={heroCtas}
-        cards={heroCards.map((card) => {
-          return {
-            ...card,
-            cta: card.cta[0],
-          }
-        })}
-      />
+			<IoHomeHero
+				pattern="/boundary/img/home-hero-pattern.svg"
+				brand="boundary"
+				heading={heroHeading}
+				description={heroDescription}
+				ctas={heroCtas}
+				cards={heroCards.map((card) => {
+					return {
+						...card,
+						cta: card.cta[0],
+					}
+				})}
+			/>
 
-      <IoHomeIntro
-        brand="boundary"
-        heading={introHeading}
-        description={introDescription}
-        features={introFeatures}
-        video={{
-          youtubeId: _introVideo?.youtubeId,
-          thumbnail: _introVideo?.thumbnail?.url,
-          heading: _introVideo?.heading,
-          description: _introVideo?.description,
-          person: {
-            name: _introVideo?.personName,
-            description: _introVideo?.personDescription,
-            avatar: _introVideo?.personAvatar?.url,
-          },
-        }}
-      />
+			<IoHomeIntro
+				brand="boundary"
+				heading={introHeading}
+				description={introDescription}
+				features={introFeatures}
+				video={{
+					youtubeId: _introVideo?.youtubeId,
+					thumbnail: _introVideo?.thumbnail?.url,
+					heading: _introVideo?.heading,
+					description: _introVideo?.description,
+					person: {
+						name: _introVideo?.personName,
+						description: _introVideo?.personDescription,
+						avatar: _introVideo?.personAvatar?.url,
+					},
+				}}
+			/>
 
-      <section className={s.useCases}>
-        <div className={s.container}>
-          <IoCardContainer
-            heading={useCasesHeading}
-            description={useCasesDescription}
-            cardsPerRow={4}
-            cards={useCasesCards.map((card) => {
-              return {
-                eyebrow: card.eyebrow,
-                link: {
-                  url: card.link,
-                  type: 'inbound',
-                },
-                heading: card.heading,
-                description: card.description,
-                products: card.products,
-              }
-            })}
-          />
-        </div>
-      </section>
+			<section className={s.useCases}>
+				<div className={s.container}>
+					<IoCardContainer
+						heading={useCasesHeading}
+						description={useCasesDescription}
+						cardsPerRow={4}
+						cards={useCasesCards.map((card) => {
+							return {
+								eyebrow: card.eyebrow,
+								link: {
+									url: card.link,
+									type: 'inbound',
+								},
+								heading: card.heading,
+								description: card.description,
+								products: card.products,
+							}
+						})}
+					/>
+				</div>
+			</section>
 
-      <IoHomeInPractice
-        brand="boundary"
-        pattern="/boundary/img/practice-pattern.svg"
-        heading={inPracticeHeading}
-        description={inPracticeDescription}
-        cards={inPracticeCards.map((card) => {
-          return {
-            eyebrow: card.eyebrow,
-            link: {
-              url: card.link,
-              type: 'inbound',
-            },
-            heading: card.heading,
-            description: card.description,
-            products: card.products,
-          }
-        })}
-        cta={{
-          heading: inPracticeCtaHeading,
-          description: inPracticeCtaDescription,
-          link: inPracticeCtaLink,
-          image: inPracticeCtaImage,
-        }}
-      />
+			<IoHomeInPractice
+				brand="boundary"
+				pattern="/boundary/img/practice-pattern.svg"
+				heading={inPracticeHeading}
+				description={inPracticeDescription}
+				cards={inPracticeCards.map((card) => {
+					return {
+						eyebrow: card.eyebrow,
+						link: {
+							url: card.link,
+							type: 'inbound',
+						},
+						heading: card.heading,
+						description: card.description,
+						products: card.products,
+					}
+				})}
+				cta={{
+					heading: inPracticeCtaHeading,
+					description: inPracticeCtaDescription,
+					link: inPracticeCtaLink,
+					image: inPracticeCtaImage,
+				}}
+			/>
 
-      {caseStudiesFeatured.length === 2 && caseStudiesLinks.length > 0 ? (
-        <IoHomeCaseStudies
-          heading={caseStudiesHeading}
-          description={caseStudiesDescription}
-          primary={caseStudiesFeatured}
-          secondary={caseStudiesLinks}
-        />
-      ) : null}
+			{caseStudiesFeatured.length === 2 && caseStudiesLinks.length > 0 ? (
+				<IoHomeCaseStudies
+					heading={caseStudiesHeading}
+					description={caseStudiesDescription}
+					primary={caseStudiesFeatured}
+					secondary={caseStudiesLinks}
+				/>
+			) : null}
 
-      <IoHomeCallToAction
-        brand="boundary"
-        heading={callToActionHeading}
-        content={callToActionDescription}
-        links={callToActionCtas}
-      />
+			<IoHomeCallToAction
+				brand="boundary"
+				heading={callToActionHeading}
+				content={callToActionDescription}
+				links={callToActionCtas}
+			/>
 
-      <IoHomePreFooter
-        brand="boundary"
-        heading={preFooterHeading}
-        description={preFooterDescription}
-        ctas={preFooterCtas}
-      />
-    </>
-  )
+			<IoHomePreFooter
+				brand="boundary"
+				heading={preFooterHeading}
+				description={preFooterDescription}
+				ctas={preFooterCtas}
+			/>
+		</>
+	)
 }
 Homepage.layout = BoundaryIoLayout
 
 export async function getStaticProps() {
-  const query = proxiedRivetClient('boundary')
-  const { boundaryHomepage } = await query({
-    query: homepageQuery,
-  })
+	const query = proxiedRivetClient('boundary')
+	const { boundaryHomepage } = await query({
+		query: homepageQuery,
+	})
 
-  return {
-    props: {
-      data: boundaryHomepage,
-    },
-    revalidate:
-      process.env.HASHI_ENV === 'production'
-        ? process.env.GLOBAL_REVALIDATE
-        : 10,
-  }
+	return {
+		props: {
+			data: boundaryHomepage,
+		},
+		revalidate:
+			process.env.HASHI_ENV === 'production'
+				? process.env.GLOBAL_REVALIDATE
+				: 10,
+	}
 }
