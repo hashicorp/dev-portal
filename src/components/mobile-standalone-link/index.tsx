10272f8314b6ad390c1e80fb050a7b87bafe3b7a
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-06-02 17:13:45 -0400

    Using CSS for MobileStandaloneLink

--- src/components/mobile-standalone-link/index.tsx
+++ src/components/mobile-standalone-link/index.tsx
@@ -1,6 +1,6 @@
-import { useDeviceSize } from 'contexts'
 import StandaloneLink from 'components/standalone-link'
 import { MobileStandaloneLinkProps } from './types'
+import s from './mobile-standalone-link.module.css'
 
 /**
  * Handles rendering different content based on whether or not the current
@@ -12,21 +12,19 @@ const MobileStandaloneLink = ({
   text,
   ...rest
 }: MobileStandaloneLinkProps) => {
-  const { isMobile } = useDeviceSize()
-
-  // Use a larger icon if we're at mobile width
-  let icon = size16Icon
-  if (isMobile) {
-    icon = size24Icon
-  }
-
-  // Don't render with text if we're at a mobile width
-  let linkText
-  if (!isMobile) {
-    linkText = text
-  }
-
-  return <StandaloneLink {...rest} icon={icon} text={linkText} />
+  return (
+    <StandaloneLink
+      {...rest}
+      icon={
+        <>
+          <span className={s.notMobileIcon}>{size16Icon}</span>
+          <span className={s.mobileIcon}>{size24Icon}</span>
+        </>
+      }
+      text={text}
+      textClassName={s.text}
+    />
+  )
 }
 
 export type { MobileStandaloneLinkProps }
