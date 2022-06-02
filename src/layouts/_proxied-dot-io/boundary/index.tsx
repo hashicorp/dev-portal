4efdcc1495f79ab5d196373e0c1fd841c36e3122
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-06-02 16:49:11 -0400

    init

--- src/layouts/_proxied-dot-io/boundary/index.tsx
+++ src/layouts/_proxied-dot-io/boundary/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import HashiHead from '@hashicorp/react-head'
-import HashiStackMenu from '@hashicorp/react-hashi-stack-menu'
 import AlertBanner from '@hashicorp/react-alert-banner'
 import Min100Layout from '@hashicorp/react-min-100-layout'
 import useProductMeta, { Products } from '@hashicorp/platform-product-meta'
@@ -11,6 +10,18 @@ import localConsentManagerServices from 'lib/consent-manager-services/io-sites'
 import Footer from 'components/_proxied-dot-io/boundary/footer'
 import ProductSubnav from 'components/_proxied-dot-io/boundary/subnav'
 import productData from 'data/boundary.json'
+import query from './query.graphql'
+
+interface Props {
+	/**
+	 * Data from data which may contain nav items for the use cases nav
+	 */
+	data: {
+		useCaseNavItems: Array<{ url: string; text: string }>
+	}
+	/** Page contents to render in the layout */
+	children: React.ReactNode
+}
 
 const { ConsentManager, openConsentManager } = createConsentManager({
 	segmentWriteKey: productData.analyticsConfig.segmentWriteKey,
@@ -18,17 +29,13 @@ const { ConsentManager, openConsentManager } = createConsentManager({
 	otherServices: [...localConsentManagerServices],
 })
 
-function BoundaryIoLayout({
-	children,
-}: {
-	/** Page contents to render in the layout */
-	children: React.ReactNode
-}): React.ReactElement {
+function BoundaryIoLayout({ children, data }: Props): React.ReactElement {
 	usePageviewAnalytics({
 		siteId: process.env.NEXT_PUBLIC_FATHOM_SITE_ID_BOUNDARY,
 		includedDomains: productData.analyticsConfig.includedDomains,
 	})
 	const { themeClass } = useProductMeta(productData.name as Products)
+	const { useCaseNavItems } = data
 
 	return (
 		<>
@@ -49,13 +56,59 @@ function BoundaryIoLayout({
 						hideOnMobile
 					/>
 				)}
-				<HashiStackMenu onPanelChange={() => null} />
-				<ProductSubnav />
+				<ProductSubnav
+					menuItems={[
+						{
+							text: 'Overview',
+							url: '/',
+							type: 'inbound',
+						},
+						'divider',
+						useCaseNavItems.length > 0
+							? {
+									text: 'Use Cases',
+									submenu: [
+										...useCaseNavItems.map((item) => {
+											return {
+												text: item.text,
+												url: `/use-cases/${item.url}`,
+											}
+										}),
+									].sort((a, b) => a.text.localeCompare(b.text)),
+							  }
+							: undefined,
+						{
+							text: 'Tutorials',
+							url: 'https://learn.hashicorp.com/boundary',
+							type: 'inbound',
+						},
+						{
+							text: 'Docs',
+							url: '/docs',
+							type: 'inbound',
+						},
+						{
+							text: 'API',
+							url: '/api-docs',
+							type: 'inbound',
+						},
+						{
+							text: 'Community',
+							url: '/community',
+							type: 'inbound',
+						},
+					].filter(Boolean)}
+				/>
 				<div className={themeClass}>{children}</div>
 			</Min100Layout>
 			<ConsentManager />
 		</>
 	)
+}
+
+BoundaryIoLayout.rivetParams = {
+	query,
+	dependencies: [],
 }
 
 export default BoundaryIoLayout
