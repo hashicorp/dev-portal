feae99da3115c3298fbaf60738314cb298ff5777
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-06-02 11:53:58 -0400

    Removing id, organizing imports

--- src/views/tutorial-view/utils/mdx-components.tsx
+++ src/views/tutorial-view/utils/mdx-components.tsx
@@ -1,22 +1,22 @@
+// Third-party imports
 import { ReactNode } from 'react'
-import codeBlockPrimitives from '@hashicorp/react-code-block/mdx'
-import { makeHeadingElement } from 'layouts/sidebar-sidecar/utils/_local_platform-docs-mdx'
-import Tabs, { Tab } from 'components/tabs'
-import ImageConfig from 'components/image-config'
-import Image from 'components/image'
-import InteractiveLabCallout from 'components/interactive-lab-callout'
-import VideoEmbed from 'components/video-embed'
-import Text from 'components/text'
-import devDotStyles from 'components/dev-dot-content/dev-dot-content.module.css'
-import s from './mdx-components.module.css'
-import AccordionDisclosure from 'components/accordion-disclosure'
-import slugify from 'slugify'
 
-/**
- * @TODO move over these components from learn, update to new spec
- *
- * import Accordion from 'components/accordion'
- */
+// HashiCorp imports
+import codeBlockPrimitives from '@hashicorp/react-code-block/mdx'
+
+// Global imports
+import { makeHeadingElement } from 'layouts/sidebar-sidecar/utils/_local_platform-docs-mdx'
+import AccordionDisclosure from 'components/accordion-disclosure'
+import devDotStyles from 'components/dev-dot-content/dev-dot-content.module.css'
+import Image from 'components/image'
+import ImageConfig from 'components/image-config'
+import InteractiveLabCallout from 'components/interactive-lab-callout'
+import Tabs, { Tab } from 'components/tabs'
+import Text from 'components/text'
+import VideoEmbed from 'components/video-embed'
+
+// Local imports
+import s from './mdx-components.module.css'
 
 const { CodeBlockConfig, CodeTabs, pre } = codeBlockPrimitives({
   theme: 'dark',
@@ -44,11 +44,7 @@ function TabsWrapper({ children }: { children: ReactNode }) {
  *   - pass classname with a margin-top setting for when there are multiple?
  */
 const AccordionWrapper = ({ children, collapse, heading }) => {
-  return (
-    <AccordionDisclosure id={slugify(heading)} title={heading}>
-      {children}
-    </AccordionDisclosure>
-  )
+  return <AccordionDisclosure title={heading}>{children}</AccordionDisclosure>
 }
 
 //  these components are automatically imported into scope within MDX content
