fe1d776966731f7cc3cc07c4148007d3fda758d8
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-06-03 08:06:12 -0400

    fix: use CSS to switch between buttons

--- src/components/navigation-header/components/give-feedback-button/index.tsx
+++ src/components/navigation-header/components/give-feedback-button/index.tsx
@@ -3,37 +3,35 @@ import classNames from 'classnames'
 import { IconMessageSquareFill24 } from '@hashicorp/flight-icons/svg-react/message-square-fill-24'
 import { IconExternalLink16 } from '@hashicorp/flight-icons/svg-react/external-link-16'
 import ButtonLink from 'components/button-link'
-import { useDeviceSize } from 'contexts'
 import s from './give-feedback-button.module.css'
 
 const FORM_URL = 'https://forms.gle/fnHLuNahLEhjuKvE6'
 
 function GiveFeedbackButton(): ReactElement {
-  const { isMobile } = useDeviceSize()
-
-  if (isMobile) {
-    return (
-      <a
-        href={FORM_URL}
-        aria-label="Give beta feedback"
-        className={classNames(s.iconButton, s.primary)}
-        target="_blank"
-        rel="noreferrer"
-      >
-        <IconMessageSquareFill24 />
-      </a>
-    )
-  } else {
-    return (
-      <ButtonLink
-        openInNewTab={true}
-        href={FORM_URL}
-        text="Give beta feedback"
-        icon={<IconExternalLink16 />}
-        iconPosition="trailing"
-      />
-    )
-  }
+  return (
+    <>
+      <span className={s.iconButtonContainer}>
+        <a
+          href={FORM_URL}
+          aria-label="Give beta feedback"
+          className={classNames(s.iconButton, s.primary)}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <IconMessageSquareFill24 />
+        </a>
+      </span>
+      <span className={s.textButtonContainers}>
+        <ButtonLink
+          openInNewTab={true}
+          href={FORM_URL}
+          text="Give beta feedback"
+          icon={<IconExternalLink16 />}
+          iconPosition="trailing"
+        />
+      </span>
+    </>
+  )
 }
 
 export default GiveFeedbackButton
