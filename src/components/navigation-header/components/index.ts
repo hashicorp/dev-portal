3444cbb7909561f6a0c71ccaa992666e70f8cfdd
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2022-06-03 09:22:02 -0400

    feat: add give feedback button to top nav (#530)
    * feat: add give feedback button to top nav
    
    * fix: refine icon button for mobile
    
    * fix: open link in new tab
    
    * fix: set target and rel for open in new tab
    
    * fix: use CSS to switch between buttons
    
    * fix: typo in className
    
    * fix: ButtonLink reference in error msg
    
    * fix: update aria-label for both links
    
    * style: apply suggestions from code review
    
    Co-authored-by: Ashlee M Boyer <43934258+ashleemboyer@users.noreply.github.com>
    
    Co-authored-by: Ashlee M Boyer <43934258+ashleemboyer@users.noreply.github.com>

--- src/components/navigation-header/components/index.ts
+++ src/components/navigation-header/components/index.ts
@@ -1,3 +1,4 @@
+import GiveFeedbackButton from './give-feedback-button'
 import HomePageHeaderContent from './home-page-content'
 import NavigationHeaderDropdownMenu from './dropdown-menu'
 import PrimaryNavLink from './primary-nav-link'
@@ -5,6 +6,7 @@ import PrimaryNavSubmenu from './primary-nav-submenu'
 import ProductPageHeaderContent from './product-page-content'
 
 export {
+  GiveFeedbackButton,
   HomePageHeaderContent,
   NavigationHeaderDropdownMenu,
   PrimaryNavLink,
