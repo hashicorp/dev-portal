0bef3a09f2bc4189758779a3b7ba4eaf4c8ee473
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-06-07 12:20:29 -0400

    Naming file to match rule name

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: ['@hashicorp/platform-cli/config/stylelint.config'],
-  plugins: ['./stylelint-rules/no-undeclared-tokens.js'],
+  plugins: ['./stylelint-rules/no-undeclared-hds-color-tokens.js'],
   rules: {
     'digital-plugin/no-undeclared-hds-color-tokens': true,
     'declaration-property-value-disallowed-list': [
