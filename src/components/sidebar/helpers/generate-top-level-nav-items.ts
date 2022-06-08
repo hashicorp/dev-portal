57c79ee284e5b97b27ba636a858ed733d867cfab
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-06-08 13:02:21 -0400

    Updating comments

--- src/components/sidebar/helpers/generate-top-level-nav-items.ts
+++ src/components/sidebar/helpers/generate-top-level-nav-items.ts
@@ -4,9 +4,6 @@ import { generateTopLevelSubNavItems } from 'lib/generate-top-level-sub-nav-item
 /**
  * Generates the top-level website nav data for rendering in `Sidebar` as the
  * top-level of the mobile navigation experience.
- *
- * Depends on the `__config.dev_dot.beta_product_slugs` variable to be set in
- * the current environment config.
  */
 export const generateTopLevelSidebarNavData = (productName: ProductName) => {
   const levelButtonProps = {
--- src/lib/generate-top-level-sub-nav-items.ts
+++ src/lib/generate-top-level-sub-nav-items.ts
@@ -2,6 +2,14 @@ import { ProductSlug } from 'types/products'
 import getIsBetaProduct from 'lib/get-is-beta-product'
 import { productSlugsToNames } from 'lib/products'
 
+/**
+ * Generates the top-level nav items to be rendered in various places in the
+ * app, including `Sidebar`, the home page mobile menu, and the main nav H menu
+ * that shows on non-home pages.
+ *
+ * Depends on the `__config.dev_dot.beta_product_slugs` variable to be set in
+ * the current environment config.
+ */
 export const generateTopLevelSubNavItems = () => {
   const betaProductItems = []
   const futureProductItems = []
