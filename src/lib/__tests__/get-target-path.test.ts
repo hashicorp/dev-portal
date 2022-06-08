fce96a4a8d2c3c0b634f4e263e8f7541b9a00899
Kevin Wang <kwangsan@gmail.com>
2022-06-08 16:09:07 -0400

    make tests relevant

--- src/lib/__tests__/get-target-path.test.ts
+++ src/lib/__tests__/get-target-path.test.ts
@@ -3,53 +3,46 @@ import { getTargetPath } from '../get-target-path'
 describe('getTargetPath', () => {
   describe('with a nested path', () => {
     it('should return a target path while on "latest"', () => {
-      {
-        const input = {
-          basePath: 'sentinel/intro',
-          asPath: '/sentinel/intro/some/nested/article',
-          version: 'v1.10.x',
-        }
-        const target = getTargetPath(input)
-        expect(target).toEqual('/sentinel/intro/v1.10.x/some/nested/article')
+      const input = {
+        basePath: 'vault/docs',
+        asPath: '/vault/docs/secrets/kv',
+        version: 'v1.9.x',
       }
+
+      const target = getTargetPath(input)
+      expect(target).toEqual('/vault/docs/v1.9.x/secrets/kv')
     })
 
     it('should return a target path while on an older version', () => {
-      {
-        const input = {
-          basePath: 'sentinel/intro',
-          asPath: '/sentinel/intro/v1.8.x/some/nested/article',
-          version: 'v1.10.x',
-        }
-        const target = getTargetPath(input)
-        expect(target).toEqual('/sentinel/intro/v1.10.x/some/nested/article')
+      const input = {
+        basePath: 'vault/docs',
+        asPath: '/vault/docs/v1.9.x/secrets/kv',
+        version: 'v1.10.x',
       }
+      const target = getTargetPath(input)
+      expect(target).toEqual('/vault/docs/v1.10.x/secrets/kv')
     })
   })
 
   describe('with a non-nested path', () => {
     it('should return a target path while on "latest"', () => {
-      {
-        const input = {
-          basePath: 'sentinel/intro',
-          asPath: '/sentinel/intro',
-          version: 'v1.10.x',
-        }
-        const target = getTargetPath(input)
-        expect(target).toEqual('/sentinel/intro/v1.10.x')
+      const input = {
+        basePath: 'vault/docs',
+        asPath: '/vault/docs',
+        version: 'v1.10.x',
       }
+      const target = getTargetPath(input)
+      expect(target).toEqual('/vault/docs/v1.10.x')
     })
 
     it('should return a target path while on an older version', () => {
-      {
-        const input = {
-          basePath: 'sentinel/intro',
-          asPath: '/sentinel/intro/v1.8.x',
-          version: 'v1.10.x',
-        }
-        const target = getTargetPath(input)
-        expect(target).toEqual('/sentinel/intro/v1.10.x')
+      const input = {
+        basePath: 'vault/docs',
+        asPath: '/vault/docs/v1.8.x',
+        version: 'v1.10.x',
       }
+      const target = getTargetPath(input)
+      expect(target).toEqual('/vault/docs/v1.10.x')
     })
   })
 })
