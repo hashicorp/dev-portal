9a5be988446b6586dac96e2be496a10ff7f97374
Bryce Kalow <bkalow@hashicorp.com>
2022-06-08 14:15:43 -0500

    simplify implementation

--- src/components/collection-card/components/company-logo/index.tsx
+++ src/components/collection-card/components/company-logo/index.tsx
@@ -9,125 +9,26 @@ export default function CompanyLogo({ name }: CompanyLogoProps): JSX.Element {
     return <></>
   }
 
-  let image = null
-  switch (name) {
-    case 'aws': {
-      image = (
-        <Image
-          src={require(`./img/aws.png?url`)}
-          alt=""
-          /**
-           * Note: for use in CollectionCard, consumer should provide meaningful
-           * text using the card heading element.
-           */
-          objectPosition="left center"
-          layout="fill"
-          objectFit="contain"
-        />
-      )
-      break
-    }
-    case 'docker': {
-      image = (
-        <Image
-          src={require(`./img/docker.png?url`)}
-          alt=""
-          /**
-           * Note: for use in CollectionCard, consumer should provide meaningful
-           * text using the card heading element.
-           */
-          objectPosition="left center"
-          layout="fill"
-          objectFit="contain"
-        />
-      )
-      break
-    }
-    case 'github': {
-      image = (
-        <Image
-          src={require(`./img/github.png?url`)}
-          alt=""
-          /**
-           * Note: for use in CollectionCard, consumer should provide meaningful
-           * text using the card heading element.
-           */
-          objectPosition="left center"
-          layout="fill"
-          objectFit="contain"
-        />
-      )
-      break
-    }
-    case 'google-cloud': {
-      image = (
-        <Image
-          src={require(`./img/google-cloud.png?url`)}
-          alt=""
-          /**
-           * Note: for use in CollectionCard, consumer should provide meaningful
-           * text using the card heading element.
-           */
-          objectPosition="left center"
-          layout="fill"
-          objectFit="contain"
-        />
-      )
-      break
-    }
-    case 'microsoft-azure': {
-      image = (
-        <Image
-          src={require(`./img/microsoft-azure.png?url`)}
-          alt=""
-          /**
-           * Note: for use in CollectionCard, consumer should provide meaningful
-           * text using the card heading element.
-           */
-          objectPosition="left center"
-          layout="fill"
-          objectFit="contain"
-        />
-      )
-      break
-    }
-    case 'oci': {
-      image = (
-        <Image
-          src={require(`./img/oci.png?url`)}
-          alt=""
-          /**
-           * Note: for use in CollectionCard, consumer should provide meaningful
-           * text using the card heading element.
-           */
-          objectPosition="left center"
-          layout="fill"
-          objectFit="contain"
-        />
-      )
-      break
-    }
-    case 'terraform-cloud': {
-      image = (
-        <Image
-          src={require('@hashicorp/mktg-logos/product/terraform-cloud/primary/color.svg?url')}
-          alt=""
-          /**
-           * Note: for use in CollectionCard, consumer should provide meaningful
-           * text using the card heading element.
-           */
-          objectPosition="left center"
-          layout="fill"
-          objectFit="contain"
-        />
-      )
-      break
-    }
-  }
+  const src =
+    name === 'terraform-cloud'
+      ? require('@hashicorp/mktg-logos/product/terraform-cloud/primary/color.svg?url')
+      : require(`./img/${name}.png?url`)
 
   return (
     <span className={s.root}>
-      <span className={classNames(s.inner, s[`logo-${name}`])}>{image}</span>
+      <span className={classNames(s.inner, s[`logo-${name}`])}>
+        <Image
+          src={src}
+          alt=""
+          /**
+           * Note: for use in CollectionCard, consumer should provide meaningful
+           * text using the card heading element.
+           */
+          objectPosition="left center"
+          layout="fill"
+          objectFit="contain"
+        />
+      </span>
     </span>
   )
 }
