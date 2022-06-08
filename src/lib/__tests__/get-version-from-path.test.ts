13f2fceab02285a49c3105ac8673e0b8324cae34
Kevin Wang <kwangsan@gmail.com>
2022-06-08 15:51:37 -0400

    make tests relevant

--- src/lib/__tests__/get-version-from-path.test.ts
+++ src/lib/__tests__/get-version-from-path.test.ts
@@ -3,49 +3,31 @@ import { getVersionFromPath } from '../get-version-from-path'
 describe('getVersionFromPath', () => {
   it('should return the version if it is present', () => {
     {
-      const path =
-        'https://waypointproject.io/docs/v10.2.x/waypoint-hcl/variables/deploy'
+      const path = 'https://developer.hashicorp.com/vault/docs/v1.8.x/platform'
 
       const version = getVersionFromPath(path)
-      expect(version).toEqual('v10.2.x')
+      expect(version).toEqual('v1.8.x')
     }
 
     {
       const path =
-        'https://waypointproject.io/docs/v9000.9000.x/waypoint-hcl/variables/deploy'
+        'https://developer.hashicorp.com/waypoint/docs/v0.7.x/getting-started'
 
       const version = getVersionFromPath(path)
-      expect(version).toEqual('v9000.9000.x')
-    }
-
-    {
-      const path =
-        'https://waypointproject.io/docs/v0.5.x/waypoint-hcl/variables/deploy'
-
-      const version = getVersionFromPath(path)
-      expect(version).toEqual('v0.5.x')
+      expect(version).toEqual('v0.7.x')
     }
   })
 
   it('should return `undefined` if no version is present', () => {
-    const path = 'https://waypointproject.io/docs/waypoint-hcl/variables/deploy'
+    const path = 'https://developer.hashicorp.com/waypoint/docs/getting-started'
     const version = getVersionFromPath(path)
     expect(version).toBeUndefined()
   })
 
   it('should return `undefined` if version is not formatted properly', () => {
-    {
-      const path =
-        'https://waypointproject.io/docs/10.2.4-canary.6/waypoint-hcl/variables/deploy'
-      const version = getVersionFromPath(path)
-      expect(version).toBeUndefined()
-    }
-
-    {
-      const path =
-        'https://waypointproject.io/docs/v10.2/waypoint-hcl/variables/deploy'
-      const version = getVersionFromPath(path)
-      expect(version).toBeUndefined()
-    }
+    const path =
+      'https://developer.hashicorp.com/waypoint/docs/0.7.x/getting-started'
+    const version = getVersionFromPath(path)
+    expect(version).toBeUndefined()
   })
 })
