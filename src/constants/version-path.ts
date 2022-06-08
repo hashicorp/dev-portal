165acf2cfd3bbc7a79512b1dad0330403586868a
Kevin Wang <kwangsan@gmail.com>
2022-06-08 15:12:14 -0400

    chore: parse TFE version

--- src/constants/version-path.ts
+++ src/constants/version-path.ts
@@ -1,3 +1,5 @@
-const VERSION_IN_PATH_REGEX = /^v\d+\.\d+\.(\d+|\w+)$/i
+/** This REGEX is used to parse a product version from a URL */
+export const VERSION_IN_PATH_REGEX = /^v\d+\.\d+\.(\d+|\w+)$/i
 
-export { VERSION_IN_PATH_REGEX }
+/** This REGEX is used to parse a Terraform Enterprise version from a URL */
+export const TFE_VERSION_IN_PATH_REGEXP = /v[0-9]{6}-\d+/i
