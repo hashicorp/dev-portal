c55cf061727e31941cfa27acf25f0fa965f04617
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-06-08 10:45:22 -0400

    Adding MobileMenuProvider to CoreDevDotLayout

--- src/layouts/core-dev-dot-layout/index.tsx
+++ src/layouts/core-dev-dot-layout/index.tsx
@@ -1,17 +1,21 @@
-import { DatadogHeadTag, DatadogScriptTag } from 'lib/datadog'
 import Head from 'next/head'
+import { DatadogHeadTag, DatadogScriptTag } from 'lib/datadog'
+import { MobileMenuProvider } from 'contexts'
+import { CoreDevDotLayoutProps } from './types'
 import s from './core-dev-dot-layout.module.css'
 
-const CoreDevDotLayout: React.FC = ({ children }) => (
-  <>
-    <Head>
-      <link rel="icon" href="/favicon.ico" sizes="any" />
-      <link rel="icon" href="/icon.svg" type="image/svg+xml" />
-      <DatadogHeadTag />
-    </Head>
-    <div className={s.root}>{children}</div>
-    <DatadogScriptTag />
-  </>
-)
+const CoreDevDotLayout = ({ children }: CoreDevDotLayoutProps) => {
+  return (
+    <MobileMenuProvider>
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <DatadogHeadTag />
+      </Head>
+      <div className={s.root}>{children}</div>
+      <DatadogScriptTag />
+    </MobileMenuProvider>
+  )
+}
 
 export default CoreDevDotLayout
