3a96f01cc819459de2f8595d9abee1031fc571a2
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-06-14 17:21:56 -0400

    Adding comments

--- src/components/sidebar/helpers/generate-resources-nav-items.ts
+++ src/components/sidebar/helpers/generate-resources-nav-items.ts
@@ -7,14 +7,24 @@ const COMMUNITY_FORUM_LINKS_BY_SLUG = {
   waypoint: 'https://discuss.hashicorp.com/c/waypoint/51',
 }
 
-const generateCommunityForumNavItem = (productSlug: ProductSlug) => {
+/**
+ * Generates the sidebar nav item for the Community Forum link. If given a
+ * Product slug specified in `COMMUNITY_FORUM_LINKS_BY_SLUG`, that Product's
+ * link will be used. Otherwise, a general default link is used.
+ */
+const generateCommunityForumNavItem = (productSlug?: ProductSlug) => {
   const title = 'Community Forum'
   const href =
     COMMUNITY_FORUM_LINKS_BY_SLUG[productSlug] || DEFAULT_COMMUNITY_FORUM_LINK
   return { href, title }
 }
 
-const generateResourcesNavItems = (productSlug: ProductSlug) => {
+/**
+ * Generates the sidebar nav items for the Resources section of the sidebar.
+ * Optionally accepts a Product slug for customization of links. Invokes
+ * additional helper functions for each nav item as needed.
+ */
+const generateResourcesNavItems = (productSlug?: ProductSlug) => {
   return [
     { heading: 'Resources' },
     { title: 'All Tutorials', href: 'https://learn.hashicorp.com/search' },
