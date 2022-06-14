86a5b780ca0133f22c1c5999eefd2145454297e8
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2022-06-14 10:44:24 -0400

    feat: add tabs nested styling (#558)
    * feat: stub in isNested styles
    
    * feat: nesting tabpanel styles
    
    * feat: style nested tabs-as-buttons
    
    * feat: style tabs-as-dropdown nested
    
    * docs: add comments to CSS
    
    * style: rename prop, clarify comments
    
    * feat: shared Mdx Tabs for docs and tutorials
    
    * docs: update comment on defaultTabIdx
    
    * style: don't not not do double-negative
    
    * style: retain [tabpanel] selector
    
    * style: reorder imports, css import last
    
    Co-authored-by: Ashlee M Boyer <43934258+ashleemboyer@users.noreply.github.com>
    
    * fix: updated NestedTabContext default value
    
    Co-authored-by: Bryce Kalow <bkalow@hashicorp.com>
    
    * style: use import alias for simpler import-export
    
    Co-authored-by: Ashlee M Boyer <43934258+ashleemboyer@users.noreply.github.com>
    
    * fix: update type for nested tab context
    
    * chore: clean up unused CSS file
    
    * docs: add comment on TabNestingProvider
    
    * hotfix: consul stable-website reference
    
    * docs: add comments on use of ::after
    
    * feat: update border-bottom on tabs-as-dropdown
    
    * fix: lint whitespace
    
    * fix: revert hotfix for consul stable-website rm
    
    * docs: add comment on not-important
    
    Co-authored-by: Ashlee M Boyer <43934258+ashleemboyer@users.noreply.github.com>
    Co-authored-by: Bryce Kalow <bkalow@hashicorp.com>

---
+++ src/components/dev-dot-content/mdx-components/mdx-tabs/index.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from 'react'
+import Tabs, { Tab as MdxTab } from 'components/tabs'
+import s from './mdx-tabs.module.css'
+
+/**
+ * Note: at one time, we supported a `defaultTabIdx` prop on `<Tabs />`.
+ * However, this option had no known use in Docs or Tutorials content,
+ * so it has since been removed.
+ */
+function MdxTabs({ children }: { children: ReactNode }) {
+  return (
+    <div className={s.tabsWrapper}>
+      <Tabs allowNestedStyles>{children}</Tabs>
+    </div>
+  )
+}
+
+export { MdxTabs, MdxTab }
