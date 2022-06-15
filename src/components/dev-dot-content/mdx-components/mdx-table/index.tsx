654990b32dd7cc9b26c63e2fb020c56aaa500762
Bryce Kalow <bkalow@hashicorp.com>
2022-06-15 10:23:00 -0500

    adds a comment

--- src/components/dev-dot-content/mdx-components/mdx-table/index.tsx
+++ src/components/dev-dot-content/mdx-components/mdx-table/index.tsx
@@ -1,5 +1,9 @@
 import s from './mdx-table.module.css'
 
+/**
+ * Lightweight wrapper around the native table element to encapsulate styles and enable horizontal overflow scrolling
+ * along with full-width tables.
+ */
 export default function MDXTable(props) {
   return (
     <div className={s.tableWrapper}>
