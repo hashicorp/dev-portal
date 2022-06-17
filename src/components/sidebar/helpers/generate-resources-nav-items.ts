871c9e8062b8c0364b48a472f4a4a34b071c848a
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-06-17 12:31:53 -0400

    style: DEFAULT_GITHUB_LINK const

--- src/components/sidebar/helpers/generate-resources-nav-items.ts
+++ src/components/sidebar/helpers/generate-resources-nav-items.ts
@@ -1,6 +1,7 @@
 import { ProductSlug } from 'types/products'
 
 const DEFAULT_COMMUNITY_FORUM_LINK = 'https://discuss.hashicorp.com/'
+const DEFAULT_GITHUB_LINK = 'https://github.com/hashicorp'
 
 const COMMUNITY_FORUM_LINKS_BY_SLUG = {
   vault: 'https://discuss.hashicorp.com/c/vault/30',
@@ -25,10 +26,10 @@ const generateCommunityForumNavItem = (productSlug?: ProductSlug) => {
 const GITHUB_LINKS_BY_SLUG: { [K in ProductSlug]: string } = {
   boundary: 'https://github.com/hashicorp/boundary',
   consul: 'https://github.com/hashicorp/consul',
-  hcp: 'https://github.com/hashicorp', // No specific repo to link to
+  hcp: DEFAULT_GITHUB_LINK,
   nomad: 'https://github.com/hashicorp/nomad',
   packer: 'https://github.com/hashicorp/packer',
-  sentinel: 'https://github.com/hashicorp', // No specific repo to link to
+  sentinel: DEFAULT_GITHUB_LINK,
   terraform: 'https://github.com/hashicorp/terraform',
   vagrant: 'https://github.com/hashicorp/vagrant',
   vault: 'https://github.com/hashicorp/vault',
@@ -40,10 +41,11 @@ const GITHUB_LINKS_BY_SLUG: { [K in ProductSlug]: string } = {
  * Product slug specified in `COMMUNITY_FORUM_LINKS_BY_SLUG`, that Product's
  * link will be used. Otherwise, a general default link is used.
  */
+
 function generateGitHubNavItem(productSlug?: ProductSlug) {
   return {
     title: 'GitHub',
-    href: GITHUB_LINKS_BY_SLUG[productSlug] || 'https://github.com/hashicorp',
+    href: GITHUB_LINKS_BY_SLUG[productSlug] || DEFAULT_GITHUB_LINK,
   }
 }
 
