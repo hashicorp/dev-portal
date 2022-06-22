77a879a9903e8df5b52421eeeddb930a62fbd778
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-06-21 20:11:16 -0400

    Removing CardLink references to MaybeInternalLink

--- src/components/card-link/types.ts
+++ src/components/card-link/types.ts
@@ -1,28 +1,29 @@
 import { ReactNode } from 'react'
-import { MaybeInternalLinkProps } from 'components/maybe-internal-link'
+
+type NativeAnchorProps = JSX.IntrinsicElements['a']
 
 export interface CardLinkProps {
   /**
-   * The element(s) to render within the `CardLink` body.
+   * An optional string value that labels the link. For use when content visible
+   * in the DOM meant to give the link meaning is either missing, or does not
+   * accurately describe the link.
+   */
+  ariaLabel?: NativeAnchorProps['aria-label']
+
+  /**
+   * The content to render within the `CardLink` body.
    */
   children: ReactNode
 
   /**
    * A string of one or more classnames passed to the inner `Card` component.
    */
-  className?: string
+  className?: NativeAnchorProps['className']
 
   /**
    * The destination of the link.
    */
-  href: string
+  href: NativeAnchorProps['href']
 
-  /**
-   * An optional string value that labels the link. For use when content visible
-   * in the DOM meant to give the link meaning is either missing, or does not
-   * accurately describe the link.
-   */
-  ariaLabel?: MaybeInternalLinkProps['aria-label']
-
-  target?: HTMLAnchorElement['target']
+  target?: NativeAnchorProps['target']
 }
