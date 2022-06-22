00b6ee501da72a2c05f1f5a1b72c45e7a6c54dd6
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-06-22 11:56:25 -0400

    fix: add required meta tags

--- src/pages/test-with-twitter-description.tsx
+++ src/pages/test-with-twitter-description.tsx
@@ -4,14 +4,17 @@ function TestPageWithTwitterDescription() {
   return (
     <>
       <Head>
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="og:title" content="Test With Twitter Description" />
+        <meta property="og:type" content="website" />
         <meta
           name="description"
           property="og:description"
-          content="This is the og:description for a test page."
+          content="This is the og:description for a test page with twitter description."
         />
         <meta
           name="twitter:description"
-          content="This is the twitter:description for a test page."
+          content="This is the twitter:description for a test page with twitter description."
         />
       </Head>
       <p>
--- src/pages/test-without-twitter-description.tsx
+++ src/pages/test-without-twitter-description.tsx
@@ -4,10 +4,13 @@ function TestPageWithoutTwitterDescription() {
   return (
     <>
       <Head>
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="og:title" content="Test Without Twitter Description" />
+        <meta property="og:type" content="website" />
         <meta
           name="description"
           property="og:description"
-          content="This is the og:description for a test page."
+          content="This is the og:description for a test page without twitter description."
         />
       </Head>
       <p>
