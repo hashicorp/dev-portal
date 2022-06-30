4548cc31a2188e299a45c30ad4807875d31fc60d
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-06-30 15:10:12 -0400

    fix: use includeMdxSource, not isHcpDocs

--- src/views/product-root-docs-path-landing/index.tsx
+++ src/views/product-root-docs-path-landing/index.tsx
@@ -20,20 +20,8 @@ const ProductRootDocsPathLanding = ({
   const showProductDocsSearch = __config.flags.enable_product_docs_search
 
   let mdxSlot: ReactElement
-  /**
-   * TODO: cloud.hashicorp.com/docs redirects to cloud.hashicorp.com/docs/hcp,
-   * and the content for the former URL is empty (just a heading).
-   * This is a workaround to prevent the empty content from being rendered.
-   * https://github.com/hashicorp/cloud.hashicorp.com/blob/main/content/docs/index.mdx
-   *
-   * Related redirects-in-dev-dot issues:
-   * - Load redirects from private repos:
-   *   https://app.asana.com/0/1202097197789424/1202532915796679/f
-   * - Get remotely sourced redirects working for dev-dot routes:
-   *   https://app.asana.com/0/1201987349274776/1201662082096106/f
-   */
-  const isNotHcpDocs = product.slug != 'hcp'
-  if (mdxSource && isNotHcpDocs) {
+
+  if (mdxSource) {
     const classes = classNames(s[`${product.slug}MDXWrapper`], s.mdxSlotWrapper)
     mdxSlot = (
       <div className={classes}>
