fcdf37dce9911e9151124e61948c95b868eb496c
Dylan Staley <88163+dstaley@users.noreply.github.com>
2022-07-05 13:25:00 -0700

    fix: update ubuntu/debian install instructions

--- src/views/product-downloads-view/helpers.ts
+++ src/views/product-downloads-view/helpers.ts
@@ -31,9 +31,9 @@ export const generateDefaultPackageManagers = (
 		{
 			label: 'Ubuntu/Debian',
 			commands: [
-				`curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -`,
-				`sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"`,
-				`sudo apt-get update && sudo apt-get install ${productSlug}`,
+				`wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg`,
+				`echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list`,
+				`sudo apt update && sudo apt install ${productSlug}`,
 			],
 			os: 'linux',
 		},
