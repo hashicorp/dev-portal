771758a273bd28a69215221b39eab800ec5eb9de
Noel Quiles <3746694+EnMod@users.noreply.github.com>
2022-07-06 13:29:29 -0400

    refactor: spaces to tabs

--- src/pages/_proxied-dot-io/packer/index.tsx
+++ src/pages/_proxied-dot-io/packer/index.tsx
@@ -14,160 +14,160 @@ import homepageQuery from './query.graphql'
 import s from './style.module.css'
 
 export default function PackerHomepage({ data }): React.ReactElement {
-  const {
-    seo,
-    heroHeading,
-    heroDescription,
-    heroCtas,
-    heroCards,
-    introHeading,
-    introDescription,
-    introFeatures,
-    introVideo,
-    inPracticeHeading,
-    inPracticeDescription,
-    inPracticeCards,
-    inPracticeCtaHeading,
-    inPracticeCtaDescription,
-    inPracticeCtaLink,
-    inPracticeCtaImage,
-    useCasesHeading,
-    useCasesDescription,
-    useCasesCards,
-    caseStudiesHeading,
-    caseStudiesDescription,
-    caseStudiesFeatured,
-    caseStudiesLinks,
-    callToActionHeading,
-    callToActionDescription,
-    callToActionCtas,
-    preFooterHeading,
-    preFooterDescription,
-    preFooterCtas,
-  } = data
-  const _introVideo = introVideo[0]
+	const {
+		seo,
+		heroHeading,
+		heroDescription,
+		heroCtas,
+		heroCards,
+		introHeading,
+		introDescription,
+		introFeatures,
+		introVideo,
+		inPracticeHeading,
+		inPracticeDescription,
+		inPracticeCards,
+		inPracticeCtaHeading,
+		inPracticeCtaDescription,
+		inPracticeCtaLink,
+		inPracticeCtaImage,
+		useCasesHeading,
+		useCasesDescription,
+		useCasesCards,
+		caseStudiesHeading,
+		caseStudiesDescription,
+		caseStudiesFeatured,
+		caseStudiesLinks,
+		callToActionHeading,
+		callToActionDescription,
+		callToActionCtas,
+		preFooterHeading,
+		preFooterDescription,
+		preFooterCtas,
+	} = data
+	const _introVideo = introVideo[0]
 
-  return (
-    <>
-      <Head>{renderMetaTags(seo)}</Head>
+	return (
+		<>
+			<Head>{renderMetaTags(seo)}</Head>
 
-      <IoHomeHero
-        pattern={require('/public/packer/img/home-hero-pattern.svg')}
-        brand="packer"
-        heading={heroHeading}
-        description={heroDescription}
-        ctas={heroCtas}
-        cards={heroCards.map((card) => {
-          return {
-            ...card,
-            cta: card.cta[0],
-          }
-        })}
-      />
+			<IoHomeHero
+				pattern={require('/public/packer/img/home-hero-pattern.svg')}
+				brand="packer"
+				heading={heroHeading}
+				description={heroDescription}
+				ctas={heroCtas}
+				cards={heroCards.map((card) => {
+					return {
+						...card,
+						cta: card.cta[0],
+					}
+				})}
+			/>
 
-      <IoHomeIntro
-        brand="packer"
-        heading={introHeading}
-        description={introDescription}
-        features={introFeatures}
-        video={{
-          youtubeId: _introVideo?.youtubeId,
-          thumbnail: _introVideo?.thumbnail?.url,
-          heading: _introVideo?.heading,
-          description: _introVideo?.description,
-          person: {
-            name: _introVideo?.personName,
-            description: _introVideo?.personDescription,
-            avatar: _introVideo?.personAvatar?.url,
-          },
-        }}
-      />
+			<IoHomeIntro
+				brand="packer"
+				heading={introHeading}
+				description={introDescription}
+				features={introFeatures}
+				video={{
+					youtubeId: _introVideo?.youtubeId,
+					thumbnail: _introVideo?.thumbnail?.url,
+					heading: _introVideo?.heading,
+					description: _introVideo?.description,
+					person: {
+						name: _introVideo?.personName,
+						description: _introVideo?.personDescription,
+						avatar: _introVideo?.personAvatar?.url,
+					},
+				}}
+			/>
 
-      <section className={s.useCases}>
-        <div className={s.container}>
-          <IoCardContainer
-            heading={useCasesHeading}
-            description={useCasesDescription}
-            cardsPerRow={4}
-            cards={useCasesCards.map((card) => {
-              return {
-                eyebrow: card.eyebrow,
-                link: {
-                  url: card.link,
-                  type: 'inbound',
-                },
-                heading: card.heading,
-                description: card.description,
-                products: card.products,
-              }
-            })}
-          />
-        </div>
-      </section>
+			<section className={s.useCases}>
+				<div className={s.container}>
+					<IoCardContainer
+						heading={useCasesHeading}
+						description={useCasesDescription}
+						cardsPerRow={4}
+						cards={useCasesCards.map((card) => {
+							return {
+								eyebrow: card.eyebrow,
+								link: {
+									url: card.link,
+									type: 'inbound',
+								},
+								heading: card.heading,
+								description: card.description,
+								products: card.products,
+							}
+						})}
+					/>
+				</div>
+			</section>
 
-      <IoHomeInPractice
-        brand="packer"
-        pattern={require('/public/packer/img/practice-pattern.svg')}
-        heading={inPracticeHeading}
-        description={inPracticeDescription}
-        cards={inPracticeCards.map((card) => {
-          return {
-            eyebrow: card.eyebrow,
-            link: {
-              url: card.link,
-              type: 'inbound',
-            },
-            heading: card.heading,
-            description: card.description,
-            products: card.products,
-          }
-        })}
-        cta={{
-          heading: inPracticeCtaHeading,
-          description: inPracticeCtaDescription,
-          link: inPracticeCtaLink,
-          image: inPracticeCtaImage,
-        }}
-      />
+			<IoHomeInPractice
+				brand="packer"
+				pattern={require('/public/packer/img/practice-pattern.svg')}
+				heading={inPracticeHeading}
+				description={inPracticeDescription}
+				cards={inPracticeCards.map((card) => {
+					return {
+						eyebrow: card.eyebrow,
+						link: {
+							url: card.link,
+							type: 'inbound',
+						},
+						heading: card.heading,
+						description: card.description,
+						products: card.products,
+					}
+				})}
+				cta={{
+					heading: inPracticeCtaHeading,
+					description: inPracticeCtaDescription,
+					link: inPracticeCtaLink,
+					image: inPracticeCtaImage,
+				}}
+			/>
 
-      <IoHomeCaseStudies
-        heading={caseStudiesHeading}
-        description={caseStudiesDescription}
-        primary={caseStudiesFeatured}
-        secondary={caseStudiesLinks}
-      />
+			<IoHomeCaseStudies
+				heading={caseStudiesHeading}
+				description={caseStudiesDescription}
+				primary={caseStudiesFeatured}
+				secondary={caseStudiesLinks}
+			/>
 
-      <IoHomeCallToAction
-        brand="packer"
-        heading={callToActionHeading}
-        content={callToActionDescription}
-        links={callToActionCtas}
-      />
+			<IoHomeCallToAction
+				brand="packer"
+				heading={callToActionHeading}
+				content={callToActionDescription}
+				links={callToActionCtas}
+			/>
 
-      <IoHomePreFooter
-        brand="packer"
-        heading={preFooterHeading}
-        description={preFooterDescription}
-        ctas={preFooterCtas}
-      />
-    </>
-  )
+			<IoHomePreFooter
+				brand="packer"
+				heading={preFooterHeading}
+				description={preFooterDescription}
+				ctas={preFooterCtas}
+			/>
+		</>
+	)
 }
 PackerHomepage.layout = PackerIoLayout
 
 export async function getStaticProps() {
-  const query = proxiedRivetClient('packer')
-  const { packerHomepage } = await query({
-    query: homepageQuery,
-  })
+	const query = proxiedRivetClient('packer')
+	const { packerHomepage } = await query({
+		query: homepageQuery,
+	})
 
-  return {
-    props: {
-      data: packerHomepage,
-    },
-    revalidate:
-      process.env.HASHI_ENV === 'production'
-        ? process.env.GLOBAL_REVALIDATE
-        : 10,
-  }
+	return {
+		props: {
+			data: packerHomepage,
+		},
+		revalidate:
+			process.env.HASHI_ENV === 'production'
+				? process.env.GLOBAL_REVALIDATE
+				: 10,
+	}
 }
--- src/pages/_proxied-dot-io/packer/use-cases/[slug].tsx
+++ src/pages/_proxied-dot-io/packer/use-cases/[slug].tsx
@@ -13,228 +13,228 @@ import useCasesQuery from './query.graphql'
 import s from './style.module.css'
 
 export default function UseCasePage({ data }) {
-  const {
-    seo,
-    heroHeading,
-    heroDescription,
-    challengeHeading,
-    challengeDescription,
-    challengeImage,
-    challengeLink,
-    solutionHeading,
-    solutionDescription,
-    solutionImage,
-    solutionLink,
-    customerCaseStudy,
-    cardsHeading,
-    cardsDescription,
-    tutorialsLink,
-    tutorialCards,
-    documentationLink,
-    documentationCards,
-    callToActionHeading,
-    callToActionDescription,
-    callToActionLinks,
-    videoCallout,
-  } = data
-  const _customerCaseStudy = customerCaseStudy[0]
-  const _videoCallout = videoCallout[0]
+	const {
+		seo,
+		heroHeading,
+		heroDescription,
+		challengeHeading,
+		challengeDescription,
+		challengeImage,
+		challengeLink,
+		solutionHeading,
+		solutionDescription,
+		solutionImage,
+		solutionLink,
+		customerCaseStudy,
+		cardsHeading,
+		cardsDescription,
+		tutorialsLink,
+		tutorialCards,
+		documentationLink,
+		documentationCards,
+		callToActionHeading,
+		callToActionDescription,
+		callToActionLinks,
+		videoCallout,
+	} = data
+	const _customerCaseStudy = customerCaseStudy[0]
+	const _videoCallout = videoCallout[0]
 
-  return (
-    <>
-      <Head>{renderMetaTags(seo)}</Head>
+	return (
+		<>
+			<Head>{renderMetaTags(seo)}</Head>
 
-      <IoUsecaseHero
-        eyebrow="Use case"
-        heading={heroHeading}
-        description={heroDescription}
-        pattern={require('/public/packer/img/usecase-hero-pattern.svg')}
-      />
+			<IoUsecaseHero
+				eyebrow="Use case"
+				heading={heroHeading}
+				description={heroDescription}
+				pattern={require('/public/packer/img/usecase-hero-pattern.svg')}
+			/>
 
-      <IoUsecaseSection
-        brand="packer"
-        eyebrow="Challenge"
-        heading={challengeHeading}
-        description={challengeDescription}
-        media={{
-          src: challengeImage?.url,
-          width: challengeImage?.width,
-          height: challengeImage?.height,
-          alt: challengeImage?.alt,
-        }}
-        cta={{
-          text: 'Learn more',
-          link: challengeLink,
-        }}
-      />
+			<IoUsecaseSection
+				brand="packer"
+				eyebrow="Challenge"
+				heading={challengeHeading}
+				description={challengeDescription}
+				media={{
+					src: challengeImage?.url,
+					width: challengeImage?.width,
+					height: challengeImage?.height,
+					alt: challengeImage?.alt,
+				}}
+				cta={{
+					text: 'Learn more',
+					link: challengeLink,
+				}}
+			/>
 
-      <IoUsecaseSection
-        brand="packer"
-        bottomIsFlush={_customerCaseStudy}
-        eyebrow="Solution"
-        heading={solutionHeading}
-        description={solutionDescription}
-        media={{
-          src: solutionImage?.url,
-          width: solutionImage?.width,
-          height: solutionImage?.height,
-          alt: solutionImage?.alt,
-        }}
-        cta={{
-          text: 'Learn more',
-          link: solutionLink,
-        }}
-      />
+			<IoUsecaseSection
+				brand="packer"
+				bottomIsFlush={_customerCaseStudy}
+				eyebrow="Solution"
+				heading={solutionHeading}
+				description={solutionDescription}
+				media={{
+					src: solutionImage?.url,
+					width: solutionImage?.width,
+					height: solutionImage?.height,
+					alt: solutionImage?.alt,
+				}}
+				cta={{
+					text: 'Learn more',
+					link: solutionLink,
+				}}
+			/>
 
-      {_customerCaseStudy ? (
-        <IoUsecaseCustomer
-          link={_customerCaseStudy.link}
-          media={{
-            src: _customerCaseStudy.image.url,
-            width: _customerCaseStudy.image.width,
-            height: _customerCaseStudy.image.height,
-            alt: _customerCaseStudy.image.alt,
-          }}
-          logo={{
-            src: _customerCaseStudy.logo.url,
-            width: _customerCaseStudy.logo.width,
-            height: _customerCaseStudy.logo.height,
-            alt: _customerCaseStudy.logo.alt,
-          }}
-          heading={_customerCaseStudy.heading}
-          description={_customerCaseStudy.description}
-          stats={_customerCaseStudy.stats.map((stat) => {
-            return {
-              value: stat.value,
-              key: stat.label,
-            }
-          })}
-        />
-      ) : null}
+			{_customerCaseStudy ? (
+				<IoUsecaseCustomer
+					link={_customerCaseStudy.link}
+					media={{
+						src: _customerCaseStudy.image.url,
+						width: _customerCaseStudy.image.width,
+						height: _customerCaseStudy.image.height,
+						alt: _customerCaseStudy.image.alt,
+					}}
+					logo={{
+						src: _customerCaseStudy.logo.url,
+						width: _customerCaseStudy.logo.width,
+						height: _customerCaseStudy.logo.height,
+						alt: _customerCaseStudy.logo.alt,
+					}}
+					heading={_customerCaseStudy.heading}
+					description={_customerCaseStudy.description}
+					stats={_customerCaseStudy.stats.map((stat) => {
+						return {
+							value: stat.value,
+							key: stat.label,
+						}
+					})}
+				/>
+			) : null}
 
-      <div className={s.cards}>
-        <IoCardContainer
-          heading={cardsHeading}
-          description={cardsDescription}
-          label="Docs"
-          cta={{
-            url: documentationLink ? documentationLink : '/docs',
-            text: 'Explore all',
-          }}
-          cardsPerRow={3}
-          cards={documentationCards.map((card) => {
-            return {
-              eyebrow: card.eyebrow,
-              link: {
-                url: card.link,
-                type: 'inbound',
-              },
-              heading: card.heading,
-              description: card.description,
-              products: card.products,
-            }
-          })}
-        />
+			<div className={s.cards}>
+				<IoCardContainer
+					heading={cardsHeading}
+					description={cardsDescription}
+					label="Docs"
+					cta={{
+						url: documentationLink ? documentationLink : '/docs',
+						text: 'Explore all',
+					}}
+					cardsPerRow={3}
+					cards={documentationCards.map((card) => {
+						return {
+							eyebrow: card.eyebrow,
+							link: {
+								url: card.link,
+								type: 'inbound',
+							},
+							heading: card.heading,
+							description: card.description,
+							products: card.products,
+						}
+					})}
+				/>
 
-        <IoCardContainer
-          label="Tutorials"
-          cta={{
-            url: tutorialsLink
-              ? tutorialsLink
-              : 'https://learn.hashicorp.com/packer',
-            text: 'Explore all',
-          }}
-          cardsPerRow={3}
-          cards={tutorialCards.map((card) => {
-            return {
-              eyebrow: card.eyebrow,
-              link: {
-                url: card.link,
-                type: 'inbound',
-              },
-              heading: card.heading,
-              description: card.description,
-              products: card.products,
-            }
-          })}
-        />
-      </div>
+				<IoCardContainer
+					label="Tutorials"
+					cta={{
+						url: tutorialsLink
+							? tutorialsLink
+							: 'https://learn.hashicorp.com/packer',
+						text: 'Explore all',
+					}}
+					cardsPerRow={3}
+					cards={tutorialCards.map((card) => {
+						return {
+							eyebrow: card.eyebrow,
+							link: {
+								url: card.link,
+								type: 'inbound',
+							},
+							heading: card.heading,
+							description: card.description,
+							products: card.products,
+						}
+					})}
+				/>
+			</div>
 
-      <div className={s.callToAction}>
-        <IoUsecaseCallToAction
-          theme="light"
-          brand="packer"
-          heading={callToActionHeading}
-          description={callToActionDescription}
-          links={callToActionLinks.map((link) => {
-            return {
-              text: link.title,
-              url: link.link,
-            }
-          })}
-          pattern={require('/public/packer/img/usecase-callout-pattern.svg')}
-        />
-      </div>
+			<div className={s.callToAction}>
+				<IoUsecaseCallToAction
+					theme="light"
+					brand="packer"
+					heading={callToActionHeading}
+					description={callToActionDescription}
+					links={callToActionLinks.map((link) => {
+						return {
+							text: link.title,
+							url: link.link,
+						}
+					})}
+					pattern={require('/public/packer/img/usecase-callout-pattern.svg')}
+				/>
+			</div>
 
-      {_videoCallout ? (
-        <div className={s.videoCallout}>
-          <IoVideoCallout
-            youtubeId={_videoCallout.youtubeId}
-            thumbnail={_videoCallout.thumbnail.url}
-            heading={_videoCallout.heading}
-            description={_videoCallout.description}
-            person={{
-              avatar: _videoCallout.personAvatar?.url,
-              name: _videoCallout.personName,
-              description: _videoCallout.personDescription,
-            }}
-          />
-        </div>
-      ) : null}
-    </>
-  )
+			{_videoCallout ? (
+				<div className={s.videoCallout}>
+					<IoVideoCallout
+						youtubeId={_videoCallout.youtubeId}
+						thumbnail={_videoCallout.thumbnail.url}
+						heading={_videoCallout.heading}
+						description={_videoCallout.description}
+						person={{
+							avatar: _videoCallout.personAvatar?.url,
+							name: _videoCallout.personName,
+							description: _videoCallout.personDescription,
+						}}
+					/>
+				</div>
+			) : null}
+		</>
+	)
 }
 UseCasePage.layout = PackerIoLayout
 
 export async function getStaticPaths() {
-  const query = proxiedRivetClient('nomad')
-  const { allPackerUseCases } = await query({
-    query: useCasesQuery,
-  })
+	const query = proxiedRivetClient('nomad')
+	const { allPackerUseCases } = await query({
+		query: useCasesQuery,
+	})
 
-  return {
-    paths: allPackerUseCases.map((page) => {
-      return {
-        params: {
-          slug: page.slug,
-        },
-      }
-    }),
-    fallback: 'blocking',
-  }
+	return {
+		paths: allPackerUseCases.map((page) => {
+			return {
+				params: {
+					slug: page.slug,
+				},
+			}
+		}),
+		fallback: 'blocking',
+	}
 }
 
 export async function getStaticProps({ params }) {
-  const { slug } = params
+	const { slug } = params
 
-  const query = proxiedRivetClient('packer')
-  const { allPackerUseCases } = await query({
-    query: useCasesQuery,
-  })
+	const query = proxiedRivetClient('packer')
+	const { allPackerUseCases } = await query({
+		query: useCasesQuery,
+	})
 
-  const page = allPackerUseCases.find((page) => page.slug === slug)
+	const page = allPackerUseCases.find((page) => page.slug === slug)
 
-  if (!page) {
-    return { notFound: true }
-  }
+	if (!page) {
+		return { notFound: true }
+	}
 
-  return {
-    props: {
-      data: page,
-    },
-    revalidate:
-      process.env.HASHI_ENV === 'production'
-        ? process.env.GLOBAL_REVALIDATE
-        : 10,
-  }
+	return {
+		props: {
+			data: page,
+		},
+		revalidate:
+			process.env.HASHI_ENV === 'production'
+				? process.env.GLOBAL_REVALIDATE
+				: 10,
+	}
 }
