4c5203d716d11e5e44532ebf51503452c5d7877c
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2022-07-11 09:57:42 -0400

    refactor: add back permalink check

--- src/components/inline-link/index.tsx
+++ src/components/inline-link/index.tsx
@@ -19,25 +19,8 @@ const InlineLink = ({
 		className
 	)
 
-	/**
-	 * An undefined "href" is possible for some of the extra "target" <a> elements
-	 * that are added by our `anchor-links` remark plugin. These elements have
-	 * an "id", and act as a target for anchor links, but do not have an href.
-	 *
-	 * For now, we still want to render these elements, as they're needed
-	 * for some anchor links to function.
-	 *
-	 * FUTURE TODOs
-	 *   - update the plugin to no longer add empty <a> elements
-	 *   - update this to throw an error if href is falsy
-	 *
-	 * Asana task:
-	 * https://app.asana.com/0/1100423001970639/1202560954440296/f
-	 */
-	const hrefWithFallback = href || '#'
-
 	return (
-		<Link href={hrefWithFallback}>
+		<Link href={href}>
 			<a {...rest} className={classes}>
 				{children}
 			</a>
