bfebaa7c4f6340bdc3dbd132fa2d6318c8248065
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-07-12 07:33:06 -0400

    refactor: set up generateStaticFunctions to reduce boilerplate

--- src/pages/vault/tutorials/[...tutorialSlug]/index.tsx
+++ src/pages/vault/tutorials/[...tutorialSlug]/index.tsx
@@ -1,38 +1,12 @@
-import { GetStaticPathsResult, GetStaticPropsResult } from 'next'
-import vaultData from 'data/vault.json'
 import { LearnProductData } from 'types/products'
-import { ProductOption } from 'lib/learn-client/types'
 import TutorialView from 'views/tutorial-view'
-import {
-	getTutorialPagePaths,
-	getTutorialPageProps,
-	TutorialPageProps,
-	TutorialPagePaths,
-} from 'views/tutorial-view/server'
+import { generateStaticFunctions } from 'views/tutorial-view/server'
+// product data
+import vaultData from 'data/vault.json'
 
-export async function getStaticProps({
-	params,
-}): Promise<GetStaticPropsResult<TutorialPageProps>> {
-	const product = vaultData as LearnProductData
-	const props = await getTutorialPageProps(product, params.tutorialSlug)
+const { getStaticPaths, getStaticProps } = generateStaticFunctions(
+	vaultData as LearnProductData
+)
 
-	// If the tutorial doesn't exist, hit the 404
-	if (!props) {
-		return { notFound: true }
-	}
-
-	return props
-}
-
-export async function getStaticPaths(): Promise<
-	GetStaticPathsResult<TutorialPagePaths['params']>
-> {
-	const paths = await getTutorialPagePaths(ProductOption['vault'])
-
-	return {
-		paths: paths.slice(0, __config.learn.max_static_paths ?? 0),
-		fallback: 'blocking',
-	}
-}
-
+export { getStaticPaths, getStaticProps }
 export default TutorialView
--- src/pages/waypoint/tutorials/[...tutorialSlug]/index.tsx
+++ src/pages/waypoint/tutorials/[...tutorialSlug]/index.tsx
@@ -1,37 +1,12 @@
-import { GetStaticPathsResult, GetStaticPropsResult } from 'next'
-import waypointData from 'data/waypoint.json'
 import { LearnProductData } from 'types/products'
-import { ProductOption } from 'lib/learn-client/types'
 import TutorialView from 'views/tutorial-view'
-import {
-	getTutorialPagePaths,
-	getTutorialPageProps,
-	TutorialPageProps,
-	TutorialPagePaths,
-} from 'views/tutorial-view/server'
+import { generateStaticFunctions } from 'views/tutorial-view/server'
+// product data
+import waypointData from 'data/waypoint.json'
 
-export async function getStaticProps({
-	params,
-}): Promise<GetStaticPropsResult<TutorialPageProps>> {
-	const product = waypointData as LearnProductData
-	const props = await getTutorialPageProps(product, params.tutorialSlug)
+const { getStaticPaths, getStaticProps } = generateStaticFunctions(
+	waypointData as LearnProductData
+)
 
-	// If the tutorial doesn't exist, hit the 404
-	if (!props) {
-		return { notFound: true }
-	}
-
-	return props
-}
-
-export async function getStaticPaths(): Promise<
-	GetStaticPathsResult<TutorialPagePaths['params']>
-> {
-	const paths = await getTutorialPagePaths(ProductOption['waypoint'])
-	return {
-		paths: paths.slice(0, __config.learn.max_static_paths ?? 0),
-		fallback: 'blocking',
-	}
-}
-
+export { getStaticPaths, getStaticProps }
 export default TutorialView
