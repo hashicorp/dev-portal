ed68782a38afc29b0bfd898140721144c8d43766
Kendall Strautman <kendallstrautman@gmail.com>
2022-07-13 14:30:18 -0700

    refactors to reduce boiler plate

--- src/pages/terraform/tutorials/[...tutorialSlug]/index.tsx
+++ src/pages/terraform/tutorials/[...tutorialSlug]/index.tsx
@@ -1,48 +1,12 @@
-import { GetStaticPathsResult, GetStaticPropsResult } from 'next'
-import terraformData from 'data/terraform.json'
 import { LearnProductData } from 'types/products'
-import { ProductOption } from 'lib/learn-client/types'
 import TutorialView from 'views/tutorial-view'
-import {
-	getTutorialPagePaths,
-	getTutorialPageProps,
-	TutorialPageProps,
-	TutorialPagePaths,
-} from 'views/tutorial-view/server'
+import { generateStaticFunctions } from 'views/tutorial-view/server'
+// product data
+import terraformData from 'data/terraform.json'
 
-export function TerraformTutorialPage({
-	layoutProps,
-	tutorial,
-	product,
-}: TutorialPageProps): React.ReactElement {
-	return (
-		<TutorialView layout={layoutProps} tutorial={tutorial} product={product} />
-	)
-}
+const { getStaticPaths, getStaticProps } = generateStaticFunctions(
+	terraformData as LearnProductData
+)
 
-export async function getStaticProps({
-	params,
-}): Promise<GetStaticPropsResult<TutorialPageProps>> {
-	const product = terraformData as LearnProductData
-	const props = await getTutorialPageProps(product, params.tutorialSlug)
-
-	// If the tutorial doesn't exist, hit the 404
-	if (!props) {
-		return { notFound: true }
-	}
-
-	return props
-}
-
-export async function getStaticPaths(): Promise<
-	GetStaticPathsResult<TutorialPagePaths['params']>
-> {
-	const paths = await getTutorialPagePaths(ProductOption['vault'])
-
-	return {
-		paths: paths.slice(0, __config.learn.max_static_paths ?? 0),
-		fallback: 'blocking',
-	}
-}
-
-export default TerraformTutorialPage
+export { getStaticPaths, getStaticProps }
+export default TutorialView
--- src/pages/terraform/tutorials/[collectionSlug].tsx
+++ src/pages/terraform/tutorials/[collectionSlug].tsx
@@ -1,45 +1,12 @@
-import { GetStaticPropsResult } from 'next'
-import terraformData from 'data/terraform.json'
 import { LearnProductData } from 'types/products'
-import { ProductOption } from 'lib/learn-client/types'
 import CollectionView from 'views/collection-view'
-import {
-	getCollectionPageProps,
-	getCollectionPaths,
-	CollectionPageProps,
-} from 'views/collection-view/server'
+import { generateStaticFunctions } from 'views/collection-view/server'
+// product data
+import terraformData from 'data/terraform.json'
 
-export async function getStaticProps({
-	params,
-}): Promise<GetStaticPropsResult<CollectionPageProps>> {
-	const { collectionSlug } = params
-	const product = terraformData as LearnProductData
-
-	const props = await getCollectionPageProps(product, collectionSlug)
-
-	// If the collection doesn't exist, hit the 404
-	if (!props) {
-		return { notFound: true }
-	}
-
-	return props
-}
-
-interface CollectionPagePaths {
-	params: {
-		collectionSlug: string
-	}
-}
-
-export async function getStaticPaths(): Promise<{
-	paths: CollectionPagePaths[]
-	fallback: boolean
-}> {
-	const paths = await getCollectionPaths(ProductOption['terraform'])
-	return {
-		paths,
-		fallback: false,
-	}
-}
+const { getStaticPaths, getStaticProps } = generateStaticFunctions(
+	terraformData as LearnProductData
+)
+export { getStaticPaths, getStaticProps }
 
 export default CollectionView
