e3d359cce4dc69a7a240975b1b2ce125a802edbe
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-07-13 14:40:39 -0400

    feat: add remaining opt-in-out PLATFORM_OPTIONS

--- src/components/opt-in-out/types.ts
+++ src/components/opt-in-out/types.ts
@@ -1,7 +1,15 @@
 export enum PlatformOptionTitles {
-	'vault-io' = 'Vault',
-	'waypoint-io' = 'Waypoint',
 	learn = 'Learn',
+	'boundary-io' = 'Boundary',
+	'consul-io' = 'Consul',
+	'hcp-io' = 'HashiCorp Cloud Platform',
+	'nomad-io' = 'Nomad',
+	'packer-io' = 'Packer',
+	'sentinel-io' = 'Sentinel',
+	'terraform-io' = 'Terraform',
+	'vault-io' = 'Vault',
+	'vagrant-io' = 'Vagrant',
+	'waypoint-io' = 'Waypoint',
 }
 
 export type OptInPlatformOption = keyof typeof PlatformOptionTitles
@@ -21,12 +29,14 @@ export interface OptInOutProps {
 	redirectPath?: string
 }
 
+export interface RedirectData {
+	base_url: string
+	getRedirectPath: (currentPath?: string) => string
+	cookieKey: string
+	cookieAnalyticsKey: string
+}
+
 export type PlatformOptionRedirectData = Record<
 	OptInPlatformOption,
-	{
-		base_url: string
-		getRedirectPath: (currentPath?: string) => string
-		cookieKey: string
-		cookieAnalyticsKey: string
-	}
+	RedirectData
 >
