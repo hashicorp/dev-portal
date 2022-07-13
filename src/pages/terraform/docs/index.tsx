68cd0428e7c67caba27413886092f8ac9ed687fd
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-07-13 18:31:35 -0400

    feat: add /terraform/docs custom landing page

--- src/pages/terraform/docs/index.tsx
+++ src/pages/terraform/docs/index.tsx
@@ -1,37 +1,13 @@
-/**
- * TODO: should have "custom" docs landing page here.
- */
+import terraformData from 'data/terraform.json'
+import { ProductData } from 'types/products'
+import ProductRootDocsPathLanding from 'views/product-root-docs-path-landing'
+import { generateGetStaticProps } from 'views/product-root-docs-path-landing/server'
+import pageContent from './content.json'
 
-// import terraformData from 'data/terraform.json'
-// import { ProductData } from 'types/products'
-// import { getStaticGenerationFunctions } from 'layouts/sidebar-sidecar/server'
-// import DocsView from 'views/docs-view'
+const getStaticProps = generateGetStaticProps({
+	pageContent,
+	product: terraformData as ProductData,
+})
 
-// const basePath = 'docs'
-// const baseName = 'Docs'
-// const product = terraformData as ProductData
-// const productSlugForLoader = 'terraform-website'
-
-function DocsView(props) {
-	return (
-		<>
-			<pre>
-				<code>{JSON.stringify(props, null, 2)}</code>
-			</pre>
-		</>
-	)
-}
-
-// const { getStaticPaths, getStaticProps } = getStaticGenerationFunctions({
-// 	product,
-// 	productSlugForLoader,
-// 	basePath,
-// 	baseName,
-// })
-
-// export { getStaticPaths, getStaticProps }
-
-export async function getStaticProps() {
-	return { props: { foo: 'shouldBeACustomDocsPageBar' } }
-}
-export default DocsView
+export { getStaticProps }
+export default ProductRootDocsPathLanding
