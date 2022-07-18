6ed9a07219a10e847bc7f62d93cd585f7a6c1f1f
Kendall Strautman <36613477+kendallstrautman@users.noreply.github.com>
2022-07-18 14:00:04 -0700

    refactor: collapse all tutorials routes to dynamic (#707)
    * spike: tutorials dynamic routes
    
    * add tutorials to dynamic route
    
    * chore: cleanup
    
    * chore: fix path desctructuring
    
    * chore: Adds product to path
    
    * chore: refactor back to server file
    
    * chore: Adjust comment
    
    * chore:adjust comment again
    
    * chore: adjust types
    
    * chore: refactor product data to use fs
    
    * refactor: use shared get product data utility
    
    * refactor: use fs for base tutorials base product data
    
    * refactor: the product from collection slug gen
    
    * refactor: move up def
    
    * refactor: remove if theme isn't a beta product yet
    
    * refactor: rename product --> productSlug
    
    * chore: refactor productSlug map gen
    
    * fix: update productSlug name in all functions
    
    * refactor: move getStaticGen funcs into page files
    
    * refactor: move get product data func into lib
    
    * refactor: build a new collection path array
    
    * chore; fix type
    
    * refactor: move type def out of inline
    
    * chore: cleanup comments and types
    
    * Refactor: use forEach
    
    * chore: adds new comment / formatting
    
    * refactor: extract intent with conditional
    
    * chore: remove nomad tutorial routes

---
+++ src/pages/[productSlug]/tutorials/index.tsx
@@ -0,0 +1,43 @@
+import { GetStaticPropsContext } from 'next'
+import { LearnProductData, LearnProductSlug } from 'types/products'
+import {
+	getProductTutorialsViewProps,
+	ProductTutorialsViewProps,
+} from 'views/product-tutorials-view/server'
+import ProductTutorialsView from 'views/product-tutorials-view'
+import { cachedGetProductData } from 'lib/get-product-data'
+
+/**
+ * Based on the array of beta product slugs,
+ * generate each product tutorials route
+ * i.e. /vault/tutorials
+ */
+function generateProductTutorialHomePaths() {
+	return __config.dev_dot.beta_product_slugs.map(
+		(productSlug: LearnProductSlug) => ({
+			params: { productSlug },
+		})
+	)
+}
+
+export async function getStaticProps({
+	params,
+}: GetStaticPropsContext<{ productSlug: LearnProductSlug }>): Promise<{
+	props: ProductTutorialsViewProps
+}> {
+	const productData = cachedGetProductData(params.productSlug)
+	const props = await getProductTutorialsViewProps(
+		productData as LearnProductData
+	)
+
+	return props
+}
+
+export async function getStaticPaths() {
+	return {
+		paths: generateProductTutorialHomePaths(),
+		fallback: false,
+	}
+}
+
+export default ProductTutorialsView
