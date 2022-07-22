d4316c3a31c3bb47fc67f0a36f04488bfbf3bd94
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-07-22 13:36:46 -0400

    Simplifying test methods used

--- src/__tests__/e2e/docs-landing-icon-card-links.spec.ts
+++ src/__tests__/e2e/docs-landing-icon-card-links.spec.ts
@@ -4,7 +4,7 @@ import waypointData from '../../data/waypoint.json'
 // Run all the tests generated in this file in parallel
 test.describe.configure({ mode: 'parallel' })
 
-// Generate a tests for each root docs path other than `docs`
+// Generate a test for each root docs path other than `docs`
 waypointData.rootDocsPaths.forEach(({ path, name }) => {
 	// There is no IconCardLink for the docs root path, ignore it
 	if (path === 'docs') {
@@ -14,26 +14,19 @@ waypointData.rootDocsPaths.forEach(({ path, name }) => {
 	// Create a test for this product + path
 	const testName = `[/waypoint/docs] ${name} \`IconCardLink\` goes to /waypoint/${path}`
 	test(testName, async ({ baseURL, page }) => {
-		// Triple the available time for the test to ru
-		test.slow()
-
 		// Build expected page url from baseURL, product slug, and path
 		const expectedPageUrl = `${baseURL}/waypoint/${path}`
 
-		// Load the /docs landing page
+		// Load the /waypoint/docs landing page before each test
 		await page.goto(`${baseURL}/waypoint/docs`)
 
-		// Get and click the anchor element rendered by IconCardLink
-		await page
-			.locator(`main header a[href="/waypoint/${path}"]`, {
-				hasText: name,
-			})
-			.click()
+		// Click the `IconCardLink`'s anchor element
+		await page.click(`main header a[href="/waypoint/${path}"]`)
 
-		// Wait for new url to load
+		// Wait for the expected page URL to load
 		await page.waitForURL(expectedPageUrl)
 
 		// Assert the correct page loaded
-		await expect(page).toHaveURL(expectedPageUrl)
+		expect(page.url()).toBe(expectedPageUrl)
 	})
 })
