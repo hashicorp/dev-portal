5ce9a28f4fbc5b26151601e3ef695ca8f9836120
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-07-27 19:32:39 -0400

    Using developmentToast in DropdownDisclosure

--- src/components/dropdown-disclosure/index.tsx
+++ src/components/dropdown-disclosure/index.tsx
@@ -1,5 +1,7 @@
+import { useEffect } from 'react'
 import classNames from 'classnames'
 import Disclosure, { DisclosureContent } from 'components/disclosure'
+import { developmentToast, ToastColor } from 'components/toast'
 import {
 	DropdownDisclosureActivator,
 	DropdownDisclosureButtonItem,
@@ -12,6 +14,19 @@ import { DropdownDisclosureProps } from './types'
 import { validateDropdownDisclosureChildren } from './helpers'
 import s from './dropdown-disclosure.module.css'
 
+/**
+ * Logs an error message to the console, then renders a critical development
+ * with the same message toast.
+ */
+const handleError = (message: string) => {
+	console.error(message)
+	developmentToast({
+		color: ToastColor.critical,
+		title: 'Error in DropdownDisclosure',
+		description: message,
+	})
+}
+
 const DropdownDisclosure = ({
 	'aria-label': ariaLabel,
 	children,
@@ -19,36 +34,38 @@ const DropdownDisclosure = ({
 	color = 'primary',
 	hideChevron = false,
 	icon,
-	text,
 	listPosition = 'left',
+	text,
 }: DropdownDisclosureProps) => {
 	// Check if `children` are valid
 	validateDropdownDisclosureChildren(children)
 
 	// Check for invalid props
-	const hasIcon = !!icon
-	const hasText = !!text
-	const hasLabel = !!ariaLabel
-	const isIconOnly = hasIcon && !hasText
-	const isTextOnly = !hasIcon && hasText
+	useEffect(() => {
+		const hasIcon = !!icon
+		const hasText = !!text
+		const hasLabel = !!ariaLabel
+		const isIconOnly = hasIcon && !hasText
+		const isTextOnly = !hasIcon && hasText
 
-	if (hasIcon && hasText) {
-		throw new Error(
-			'`DropdownDisclosure` does not accept both `icon` and `label`. Only provide one or the other.'
-		)
-	}
+		if (hasIcon && hasText) {
+			const message =
+				'`DropdownDisclosure` does not accept both `icon` and `label`. Only provide one or the other.'
+			handleError(message)
+		}
 
-	if (isIconOnly && !hasLabel) {
-		throw new Error(
-			'Icon-only `DropdownDisclosure`s require an accessible label. Provide an `aria-label` or replace the `icon` prop with the `text` prop.'
-		)
-	}
+		if (isIconOnly && !hasLabel) {
+			const message =
+				'Icon-only `DropdownDisclosure`s require an accessible label. Provide an `aria-label` or replace the `icon` prop with the `text` prop.'
+			handleError(message)
+		}
 
-	if (isTextOnly && hideChevron) {
-		throw new Error(
-			'`DropdownDisclosure`s with the `text` prop require `hideChevron` to be `false`.'
-		)
-	}
+		if (isTextOnly && hideChevron) {
+			const message =
+				'`DropdownDisclosure`s with the `text` prop require `hideChevron` to be `false`.'
+			handleError(message)
+		}
+	}, [ariaLabel, hideChevron, icon, text])
 
 	return (
 		<Disclosure
