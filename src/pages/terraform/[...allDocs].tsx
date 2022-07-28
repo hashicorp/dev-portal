da8ede6bf7c96a061f0706c19cf9ab3866e2f79e
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-07-28 16:05:41 -0400

    docs: add comment on custom docs page

--- src/pages/terraform/[...allDocs].tsx
+++ src/pages/terraform/[...allDocs].tsx
@@ -3,6 +3,17 @@ import { ProductData } from 'types/products'
 import { getStaticGenerationFunctions } from 'views/docs-view/utils/all-docs-server'
 import DocsView from 'views/docs-view'
 
+/**
+ * TODO: does not work as expected with custom docs routes (I think?)
+ * The combination of this page file (pages/terraform/[...allDocs].tsx)
+ * with the custom /docs page file (pages/[productSlug]/docs/index.tsx)
+ * does not seem to work as expected. I've seen some deploy previews
+ * with the custom /docs page rendering; and others where the default
+ * docs page is rendered instead.
+ *
+ * Need to investigate further, outside the scope of initial Terraform spike
+ * work, as custom docs page work is on hold while we finalize our approach.
+ */
 const { getStaticPaths, getStaticProps } = getStaticGenerationFunctions({
 	productData: terraformData as ProductData,
 })
