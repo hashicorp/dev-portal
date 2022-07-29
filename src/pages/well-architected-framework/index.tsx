2ed0398643ac49151f9065b0b47e68f13bdca8b7
Kendall Strautman <kendallstrautman@gmail.com>
2022-07-29 16:19:12 -0700

    spiking out index page using sidebar layout

--- src/pages/well-architected-framework/index.tsx
+++ src/pages/well-architected-framework/index.tsx
@@ -1,3 +1,20 @@
+import SidebarSidecarLayout from 'layouts/sidebar-sidecar'
+import {
+	generateProductLandingSidebarNavData,
+	generateTopLevelSidebarNavData,
+} from 'components/sidebar/helpers'
+import TutorialsSidebar, {
+	CollectionViewSidebarContent,
+} from 'components/tutorials-sidebar'
+import { ProductTutorialsSitemap } from 'views/product-tutorials-view/components'
+import { ProductTutorialsViewProps } from 'views/product-tutorials-view/server'
+import OptInOut from 'components/opt-in-out'
+import { useOptInAnalyticsTracking } from 'hooks/use-opt-in-analytics-tracking'
+
+import {
+	getOverviewHeading,
+	buildLayoutHeadings,
+} from 'views/product-tutorials-view/helpers/heading-helpers'
 import Link from 'next/link'
 import BaseNewLayout from 'layouts/base-new'
 import getProductPageContent from 'views/product-tutorials-view/helpers/get-product-page-content'
@@ -5,25 +22,33 @@ import Heading from 'components/heading'
 import ProductViewContent from 'views/product-tutorials-view/components/product-view-content'
 import { getCollectionsBySection } from 'lib/learn-client/api/collection'
 import { stripUndefinedProperties } from 'lib/strip-undefined-props'
+import processPageData from 'views/product-tutorials-view/helpers/process-page-data'
+import { getTutorialsBreadcrumb } from 'views/tutorial-view/utils/get-tutorials-breadcrumb'
 
 export const WAF_SLUG = 'well-architected-framework'
 
 // TODO - figure out betta API solution to serve sidebar data
 // right now its fetching ALL collections and filtering based on the slug
 export default function WellArchitectedFrameworkLanding(props) {
-	const { pageData, inlineCollections, inlineTutorials, allTopicCollections } =
-		props.content
+	const { pageData, layoutProps } = props
+
+	const PageHeading = () => {
+		const { title, level, slug } = getOverviewHeading()
+		return (
+			<Heading id={slug} level={level} size={500} weight="bold">
+				{title}
+			</Heading>
+		)
+	}
+
 	return (
 		<>
-			<Heading level={1} size={500} weight="bold">
-				{pageData.title}
-			</Heading>
+			<PageHeading />
 			<ProductViewContent
 				blocks={pageData.blocks}
-				inlineCollections={inlineCollections}
-				inlineTutorials={inlineTutorials}
+				inlineCollections={pageData.inlineCollections}
+				inlineTutorials={pageData.inlineTutorials}
 			/>
-			<_tempCollectionSidebarPlaceholder collections={allTopicCollections} />
 		</>
 	)
 }
@@ -48,12 +73,32 @@ export function _tempCollectionSidebarPlaceholder({ collections }) {
 }
 
 export async function getStaticProps() {
-	const pageContent = await getProductPageContent(WAF_SLUG)
+	const {
+		pageData: rawPageData,
+		inlineCollections,
+		inlineTutorials,
+	} = await getProductPageContent(WAF_SLUG)
+	const { pageData } = await processPageData(rawPageData)
 	const wafCollections = await getCollectionsBySection(WAF_SLUG)
+	const layoutProps = {
+		headings: buildLayoutHeadings(pageData),
+		breadcrumbLinks: getTutorialsBreadcrumb({
+			product: { name: 'Well Architected Framework', filename: WAF_SLUG },
+		}),
+		sidebarSections: wafCollections,
+	}
 
 	return {
 		props: stripUndefinedProperties({
-			content: { ...pageContent, allTopicCollections: wafCollections },
+			metadata: {
+				title: 'Tutorials',
+			},
+			data: {
+				pageData,
+				inlineCollections,
+				inlineTutorials,
+			},
+			layoutProps,
 		}),
 	}
 }
