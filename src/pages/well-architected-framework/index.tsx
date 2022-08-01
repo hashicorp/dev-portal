97927814c51708cd30369bba1d44283a9350bc1a
Kendall Strautman <kendallstrautman@gmail.com>
2022-08-01 11:02:46 -0700

    sort sidebar alphabetically

--- src/pages/well-architected-framework/index.tsx
+++ src/pages/well-architected-framework/index.tsx
@@ -13,12 +13,38 @@ import processPageData from 'views/product-tutorials-view/helpers/process-page-d
 import { getTutorialsBreadcrumb } from 'views/tutorial-view/utils/get-tutorials-breadcrumb'
 import { SidebarProps } from 'components/sidebar'
 import { PageSlugOption } from 'lib/learn-client/api/page'
+import { sortAlphabetically } from 'views/product-tutorials-view/helpers'
 
 export const wafData = {
 	slug: 'well-architected-framework',
 	name: 'Well Architected Framework',
+	/**
+	 * TODO: figure out where this data should live. It could be here for now as a workaround
+	 * Problem: we don't have a way to generate these sidebar categories currently
+	 * via the tutorials repo data. We could add these to the generic waf page data, or create a new
+	 * data file in dev dot to store this data for now.
+	 */
+	sidebarCategories: [
+		{
+			name: 'Framework Pillars',
+			collections: [
+				'well-architected-framework/operational-excellence',
+				'well-architected-framework/reliability',
+			],
+		},
+		{
+			name: 'Reference Architecture',
+			collections: [
+				'well-architected-framework/nomad',
+				'well-architected-framework/terraform',
+				'well-architected-framework/zero-trust-networking',
+				'well-architected-framework/zer-trust-security',
+			],
+		},
+	],
 }
 
+// @TODO - add the heading 'callout' and the description section with picture...
 export default function WellArchitectedFrameworkLanding(props) {
 	const { data, layoutProps } = props
 	const { pageData, inlineCollections, inlineTutorials } = data
@@ -44,10 +70,18 @@ export default function WellArchitectedFrameworkLanding(props) {
 						levelUpButtonText: 'Main Menu',
 						levelDownButtonText: 'Previous',
 					},
-					menuItems: layoutProps.sidebarSections.map((section) => ({
-						title: section.name,
-						fullPath: section.slug,
-					})),
+					menuItems: [
+						/**@TODO currently these sort alphabetically. We should
+						 * add sidebar categorization for non-product specific collections
+						 * */
+						{ title: 'Overview', isActive: true, fullPath: `${wafData.slug}` },
+						...layoutProps.sidebarSections
+							.sort(sortAlphabetically('name'))
+							.map((section) => ({
+								title: section.name,
+								fullPath: section.slug,
+							})),
+					],
 					showFilterInput: false,
 				},
 			]}
