e7cd012c40d827847805ab7c19e70e41cc78571e
Kendall Strautman <kendallstrautman@gmail.com>
2022-08-01 18:00:03 -0700

    chore: remove unused types

--- src/types/products.ts
+++ src/types/products.ts
@@ -35,6 +35,10 @@ type LearnProductName = Exclude<
 	'HashiCorp Cloud Platform' | 'Sentinel'
 >
 
+export enum Topics {
+	'well-architected-framework' = 'Well Architected Framework',
+}
+
 /**
  * Learn does not support all of the products in `ProductSlug`, so this is the
  * interface almost the same as `ProductData`, just with a limited set of
