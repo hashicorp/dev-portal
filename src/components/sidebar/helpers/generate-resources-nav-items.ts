fc6ea960d3f193a23666edca579ee66ce83f5e64
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-08-05 13:43:53 -0400

    Updating generateResourcesNavItems and maps

--- src/components/sidebar/helpers/generate-resources-nav-items.ts
+++ src/components/sidebar/helpers/generate-resources-nav-items.ts
@@ -1,47 +1,59 @@
 import { ProductSlug } from 'types/products'
-import { developmentToast, ToastColor } from 'components/toast'
 
-const DEFAULT_ITEMS = [
-	{
-		title: 'All Tutorials',
-		href: 'https://learn.hashicorp.com/',
-	},
-	{
-		title: 'Community Forum',
-		href: 'https://discuss.hashicorp.com/',
-	},
-	{
-		title: 'Support',
-		href: 'https://www.hashicorp.com/customer-success',
-	},
-	{
-		title: 'GitHub',
-		href: 'https://github.com/hashicorp',
-	},
-]
+const DEFAULT_COMMUNITIY_FORUM_LINK = 'https://discuss.hashicorp.com/'
+const DEFAULT_GITHUB_LINK = 'https://github.com/hashicorp'
+const DEFAULT_SUPPORT_LINK = 'https://www.hashicorp.com/customer-success'
+
+const COMMUNITY_LINKS_BY_PRODUCT: { [key in ProductSlug]: string } = {
+	boundary: 'https://discuss.hashicorp.com/c/boundary/50',
+	consul: 'https://discuss.hashicorp.com/c/consul/29',
+	hcp: 'https://discuss.hashicorp.com/c/hcp/54',
+	nomad: 'https://discuss.hashicorp.com/c/nomad/28',
+	packer: 'https://discuss.hashicorp.com/c/packer/23',
+	sentinel: 'https://discuss.hashicorp.com/c/sentinel/25',
+	terraform: 'https://discuss.hashicorp.com/c/terraform-core/27',
+	vagrant: 'https://discuss.hashicorp.com/c/vagrant/24',
+	vault: 'https://discuss.hashicorp.com/c/vault/30',
+	waypoint: 'https://discuss.hashicorp.com/c/waypoint/51',
+}
+
+const GITHUB_LINKS_BY_PRODUCT_SLUG: { [key in ProductSlug]: string } = {
+	boundary: 'https://github.com/hashicorp/boundary',
+	consul: 'https://github.com/hashicorp/consul',
+	hcp: DEFAULT_GITHUB_LINK,
+	nomad: 'https://github.com/hashicorp/nomad',
+	packer: 'https://github.com/hashicorp/packer',
+	sentinel: DEFAULT_GITHUB_LINK,
+	terraform: 'https://github.com/hashicorp/terraform',
+	vagrant: 'https://github.com/hashicorp/vagrant',
+	vault: 'https://github.com/hashicorp/vault',
+	waypoint: 'https://github.com/hashicorp/waypoint',
+}
 
 /**
  * Generates the sidebar nav items for the Resources section of the sidebar.
- * Optionally accepts a Product slug for customization of links. Invokes
- * additional helper functions for each nav item as needed.
+ * Optionally accepts a Product slug for customization of links.
  */
 const generateResourcesNavItems = (productSlug?: ProductSlug) => {
-	let resourceItems = DEFAULT_ITEMS
-	if (productSlug) {
-		try {
-			// eslint-disable-next-line @typescript-eslint/no-var-requires
-			const data = require(`content/${productSlug}/sidebar-resources.json`)
-			resourceItems = data.items
-		} catch {
-			developmentToast({
-				color: ToastColor.critical,
-				title: 'Error in `generateResourcesNavItems`',
-				description: `Could not load "sidebar-resources.json" content file for "${productSlug}". Rendering default Resources items instead.`,
-			})
-		}
-	}
-
-	return [{ heading: 'Resources' }, ...resourceItems]
+	return [
+		{ heading: 'Resources' },
+		{
+			title: 'Community Forum',
+			href: productSlug
+				? COMMUNITY_LINKS_BY_PRODUCT[productSlug]
+				: DEFAULT_COMMUNITIY_FORUM_LINK,
+		},
+		{
+			title: 'Support',
+			href: DEFAULT_SUPPORT_LINK,
+		},
+		{
+			title: 'GitHub',
+			href: productSlug
+				? GITHUB_LINKS_BY_PRODUCT_SLUG[productSlug]
+				: DEFAULT_GITHUB_LINK,
+		},
+	]
 }
 
 export { generateResourcesNavItems }
