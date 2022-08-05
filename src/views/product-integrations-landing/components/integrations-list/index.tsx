576805c347d5fc1e266b0d557fa3cca6aeb8f21f
BrandonRomano <brandon@hashicorp.com>
2022-08-04 17:33:32 -0700

    Adds basic card layout

--- src/views/product-integrations-landing/components/integrations-list/index.tsx
+++ src/views/product-integrations-landing/components/integrations-list/index.tsx
@@ -1,9 +1,35 @@
+import CardsGridList from 'components/cards-grid-list'
+import CardLink from 'components/card-link'
+import {
+	CardEyebrow,
+	CardHeading,
+	CardBody,
+} from 'components/tutorial-collection-cards'
+
 export default function IntegrationsList({ integrations }) {
 	return (
-		<ul>
+		<CardsGridList>
 			{integrations.map((integration) => {
-				return <li key={integration.id}>{integration.name}</li>
+				return (
+					<IntegrationCard
+						key={integration.id}
+						title={integration.name}
+						description={integration.description}
+						organization={integration.organization.slug}
+						repoUrl={integration.repo_url}
+					/>
+				)
 			})}
-		</ul>
+		</CardsGridList>
+	)
+}
+
+function IntegrationCard({ title, description, organization, repoUrl }) {
+	return (
+		<CardLink href={repoUrl} openInNewTab={true}>
+			<CardHeading level={3} text={title} />
+			<CardEyebrow text={`@${organization}`} />
+			<CardBody text={description} />
+		</CardLink>
 	)
 }
