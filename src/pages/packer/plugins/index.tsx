50ca94f40645f6ce07054d15c62f3dbc126b847d
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2022-08-10 14:50:05 -0400

    feat: finish stubbing in /packer/plugins landing

--- src/pages/packer/plugins/index.tsx
+++ src/pages/packer/plugins/index.tsx
@@ -1,11 +1,42 @@
-function DocsView() {
-	return (
-		<>
-			<pre>
-				<code>/packer/plugins</code>
-			</pre>
-		</>
-	)
+import DocsView from 'views/docs-view'
+// Imports below are used server-side
+import { getRootDocsPathGenerationFunctions } from 'views/docs-view/utils/get-root-docs-path-generation-functions'
+import { appendRemotePluginsNavData } from 'components/_proxied-dot-io/packer/remote-plugin-docs/server'
+
+/**
+ * Path to the plugins manifest relative to the `website` directory
+ * of the `hashicorp/packer` GitHub repo.
+ *
+ * Note: local preview is not yet supported for the Dev Dot UI,
+ * we pass remotePluginsFile to avoid having to refactor
+ * appendRemotePluginsNavData (for now).
+ */
+const remotePluginsFile = 'data/plugins-manifest.json'
+
+// We use the same getStaticProps function as all other Dev Dot docs routes
+const { getStaticProps: baseGetStaticProps } =
+	getRootDocsPathGenerationFunctions('packer', 'plugins')
+
+/**
+ * We tack on some extra plugin data to the result of the base getStaticProps,
+ * in order to get the sidebar on this page showing links to all remote plugins.
+ */
+async function getStaticProps(ctx) {
+	const staticProps = await baseGetStaticProps({ params: {}, ...ctx })
+	// Merge in remote plugin data sidebar items
+	if ('props' in staticProps) {
+		const navData = await appendRemotePluginsNavData(
+			remotePluginsFile,
+			staticProps.props.layoutProps.sidebarNavDataLevels[2].menuItems,
+			''
+		)
+		staticProps.props.layoutProps.sidebarNavDataLevels[2].menuItems = navData
+	}
+	// Return the modified static props
+	return staticProps
 }
+
+// Export getStatic functions
+export { getStaticProps }
 
 export default DocsView
