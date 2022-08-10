3141c727191b1750d9c79de82ef30982c7c422fd
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-08-10 13:09:19 -0400

    Adding getBookmark method

---
+++ src/lib/learn-client/api/bookmark/get-bookmark.ts
@@ -0,0 +1,44 @@
+import { SessionData } from 'types/auth'
+import { ApiBookmark, ApiTutorial } from 'lib/learn-client/api/api-types'
+import { get, toError } from 'lib/learn-client'
+import { errorDevelopmentToast } from 'components/toast'
+import { BOOKMARK_API_ROUTE } from '.'
+
+type GetBookmarkResult = null | ApiBookmark
+
+/**
+ * Fetches a single bookmark for the given tutorialId using the
+ * /bookmarks/:tutorialId GET endpoint.
+ */
+const getBookmark = async ({
+	tutorialId,
+	accessToken,
+}: {
+	tutorialId: ApiTutorial['id']
+	accessToken: SessionData['accessToken']
+}): Promise<GetBookmarkResult> => {
+	// Make the GET request
+	const requestResult = await get(
+		`${BOOKMARK_API_ROUTE}/${tutorialId}`,
+		accessToken
+	)
+
+	// Return null if the request status is 404
+	if (requestResult.status === 404) {
+		return null
+	}
+
+	// Return the result as JSON if the request status is OK
+	if (requestResult.ok) {
+		const { result } = await requestResult.json()
+		return result
+	}
+
+	// Render an error development toast request status is not OK
+	// TODO - log to Datadog
+	const error = await toError(requestResult)
+	errorDevelopmentToast('Error in `getBookmark`', error.toString())
+}
+
+export type { GetBookmarkResult }
+export { getBookmark }
--- src/lib/learn-client/api/bookmark/index.ts
+++ src/lib/learn-client/api/bookmark/index.ts
@@ -1,7 +1,8 @@
 import { createBookmark } from './create-bookmark'
 import { deleteBookmark } from './delete-bookmark'
+import { getBookmark } from './get-bookmark'
 import { getBookmarks } from './get-bookmarks'
 
 export const BOOKMARK_API_ROUTE = '/bookmarks'
 
-export { createBookmark, deleteBookmark, getBookmarks }
+export { createBookmark, deleteBookmark, getBookmark, getBookmarks }
