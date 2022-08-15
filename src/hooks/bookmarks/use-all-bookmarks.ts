0c82ec0e09b4b66a8b6879191968a7d4037daa28
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-08-15 17:49:33 -0400

    Adding initial useAllBookmarks

---
+++ src/hooks/bookmarks/use-all-bookmarks.ts
@@ -0,0 +1,42 @@
+import { useQueries, useQuery, UseQueryResult } from '@tanstack/react-query'
+import { ApiBookmark } from 'lib/learn-client/api/api-types'
+import { getAllBookmarks } from 'lib/learn-client/api/bookmark'
+import useAuthentication from 'hooks/use-authentication'
+
+interface UseAllBookmarksResult extends Omit<UseQueryResult, 'data'> {
+	bookmarks: undefined | ApiBookmark[]
+}
+
+/**
+ * @TODO document
+ */
+const useAllBookmarks = (): UseAllBookmarksResult => {
+	// Get the current user's access token
+	const { session } = useAuthentication()
+	const accessToken = session?.accessToken
+
+	// Fetch all bookmarks with the access token
+	const { data: bookmarks, ...restQueryResult } = useQuery(
+		['bookmarks'],
+		() => getAllBookmarks({ accessToken }),
+		{ enabled: !!accessToken }
+	)
+
+	// Prime a query for each bookmark fetched
+	useQueries({
+		queries: bookmarks
+			? bookmarks.map((bookmark: ApiBookmark) => ({
+					queryKey: ['bookmark', bookmark.tutorial_id],
+					queryFn: () => bookmark,
+			  }))
+			: [],
+	})
+
+	return {
+		bookmarks,
+		...restQueryResult,
+	}
+}
+
+export type { UseAllBookmarksResult }
+export { useAllBookmarks }
