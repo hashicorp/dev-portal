6e452ee3f25e83c48d43ea6232090e88f6e9c9b0
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-08-15 17:49:57 -0400

    Add initial useBookmarksByTutorialIds

---
+++ src/hooks/bookmarks/use-bookmarks-by-tutorial-ids.ts
@@ -0,0 +1,51 @@
+import { useQueries, useQuery, UseQueryResult } from '@tanstack/react-query'
+import { ApiBookmark, ApiTutorial } from 'lib/learn-client/api/api-types'
+import { getBookmarksByTutorialIds } from 'lib/learn-client/api/bookmark'
+import useAuthentication from 'hooks/use-authentication'
+
+interface UseBookmarksByTutorialIdsOptions {
+	tutorialIds: ApiTutorial['id'][]
+}
+
+interface UseBookmarksByTutorialIdsResult extends Omit<UseQueryResult, 'data'> {
+	bookmarks: undefined | ApiBookmark[]
+}
+
+/**
+ * @TODO document
+ */
+const useBookmarksByTutorialIds = ({
+	tutorialIds,
+}: UseBookmarksByTutorialIdsOptions): UseBookmarksByTutorialIdsResult => {
+	// Get the current user's access token
+	const { session } = useAuthentication()
+	const accessToken = session?.accessToken
+
+	// Fetch all bookmarks with the access token
+	const { data: bookmarks, ...restQueryResult } = useQuery(
+		['bookmarks'],
+		() => getBookmarksByTutorialIds({ accessToken, tutorialIds }),
+		{ enabled: !!accessToken }
+	)
+
+	// Prime a query for each bookmark fetched
+	useQueries({
+		queries: bookmarks
+			? bookmarks.map((bookmark: ApiBookmark) => ({
+					queryKey: ['bookmark', bookmark.tutorial_id],
+					queryFn: () => bookmark,
+			  }))
+			: [],
+	})
+
+	return {
+		bookmarks,
+		...restQueryResult,
+	}
+}
+
+export type {
+	UseBookmarksByTutorialIdsOptions,
+	UseBookmarksByTutorialIdsResult,
+}
+export { useBookmarksByTutorialIds }
