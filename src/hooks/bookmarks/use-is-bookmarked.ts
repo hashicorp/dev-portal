cae846992f12e5f127b30c0b89abd74c6db9b3fa
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-08-15 18:06:13 -0400

    Adding initial useIsBookmarked

---
+++ src/hooks/bookmarks/use-is-bookmarked.ts
@@ -0,0 +1,37 @@
+import { useQuery, UseQueryResult } from '@tanstack/react-query'
+import { ApiTutorial } from 'lib/learn-client/api/api-types'
+import { useAllBookmarks } from './use-all-bookmarks'
+
+interface UseIsBookmarkedOptions {
+	tutorialId: ApiTutorial['id']
+}
+
+interface UseIsBookmarkedResult extends Omit<UseQueryResult, 'data'> {
+	isBookmarked: null | boolean
+}
+
+/**
+ * @TODO document
+ */
+const useIsBookmarked = ({
+	tutorialId,
+}: UseIsBookmarkedOptions): UseIsBookmarkedResult => {
+	// Load up all bookmarks, since we know it primes queries & handles auth
+	const { isFetched: bookmarksAreFetched } = useAllBookmarks()
+
+	// After bookmarks are fetched, query for bookmark for the given `tutorialId`
+	// Disable query until bookmarks have finished fetching
+	const { data: bookmark, ...restQueryResult } = useQuery(
+		['bookmark', tutorialId],
+		{ enabled: bookmarksAreFetched }
+	)
+
+	// If `bookmark` is `undefined`, then it is still loading
+	const isBookmarked = bookmark === undefined ? null : !!bookmark
+	return {
+		isBookmarked,
+		...restQueryResult,
+	}
+}
+
+export { useIsBookmarked }
