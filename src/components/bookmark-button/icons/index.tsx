cf5d15558332c0c4d9e3407baa11e346696d2161
Kendall Strautman <kendallstrautman@gmail.com>
2022-08-16 11:19:05 -0700

    refactor: css vars

--- src/components/bookmark-button/icons/index.tsx
+++ src/components/bookmark-button/icons/index.tsx
@@ -1,15 +1,15 @@
+import classNames from 'classnames'
 import { IconBookmark16 } from '@hashicorp/flight-icons/svg-react/bookmark-16'
 import { IconBookmarkAdd16 } from '@hashicorp/flight-icons/svg-react/bookmark-add-16'
 import { IconBookmarkFill16 } from '@hashicorp/flight-icons/svg-react/bookmark-fill-16'
 import { IconBookmarkRemoveFill16 } from '@hashicorp/flight-icons/svg-react/bookmark-remove-fill-16'
-import classNames from 'classnames'
 import s from './icons.module.css'
 
 export function AddBookmarkIcon() {
 	return (
 		<>
-			<IconBookmark16 className={classNames(s.icon, s.base)} />
-			<IconBookmarkAdd16 className={classNames(s.icon, s.hover)} />
+			<IconBookmark16 className={classNames(s.position, s.base)} />
+			<IconBookmarkAdd16 className={classNames(s.position, s.hover)} />
 		</>
 	)
 }
@@ -17,8 +17,8 @@ export function AddBookmarkIcon() {
 export function RemoveBookmarkIcon() {
 	return (
 		<>
-			<IconBookmarkFill16 className={classNames(s.icon, s.base)} />
-			<IconBookmarkRemoveFill16 className={classNames(s.icon, s.hover)} />
+			<IconBookmarkFill16 className={classNames(s.position, s.base)} />
+			<IconBookmarkRemoveFill16 className={classNames(s.position, s.hover)} />
 		</>
 	)
 }
