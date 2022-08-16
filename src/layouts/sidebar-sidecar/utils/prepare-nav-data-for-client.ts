3d3447297f851d508a3226dd675b657193751243
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2022-08-16 12:13:44 -0400

    test: add tests for fullPathFromRelativeHref

--- src/layouts/sidebar-sidecar/utils/prepare-nav-data-for-client.ts
+++ src/layouts/sidebar-sidecar/utils/prepare-nav-data-for-client.ts
@@ -159,28 +159,7 @@ function prepareNavNodeForClient({
 			 * If we have a non-absolute NavDirectLink,
 			 * convert it to a NavLeaf node, and treat it similarly.
 			 */
-			let fullPath
-			if (node.href.startsWith(`/${basePaths[0]}/`)) {
-				/**
-				 * If the path already starts with the basePaths[0], the product slug,
-				 * we use the href as the fullPath directly.
-				 */
-				fullPath = node.href
-			} else if (node.href.startsWith('/')) {
-				/**
-				 * If the path starts with a slash, we treat it as relative
-				 * to the previous dot-io setup. We prefix it with basePaths[0],
-				 * which should be the product slug.
-				 */
-				fullPath = `/${path.join(basePaths[0], node.href)}`
-			} else {
-				/**
-				 * If the path does not start with a slash, we treat it as relative
-				 * to the combined current basePath.
-				 */
-				fullPath = `/${path.join(basePaths.join('/'), node.href)}`
-			}
-
+			const fullPath = fullPathFromRelativeHref(node.href, basePaths)
 			const preparedItem = {
 				...node,
 				fullPath,
@@ -202,4 +181,38 @@ function prepareNavNodeForClient({
 	}
 }
 
+/**
+ * Given a relative `href`, expected to be constructed for dot-io contexts,
+ * as well as the current `basePaths`,
+ * Return a `fullPath` for use with the Dev Dot URL structure.
+ *
+ * @param href A relative URL
+ * @param basePaths The current set of basePaths
+ */
+function fullPathFromRelativeHref(href: string, basePaths: string[]): string {
+	let fullPath
+	if (href.startsWith(`/${basePaths[0]}/`)) {
+		/**
+		 * If the path already starts with the basePaths[0], the product slug,
+		 * we use the href as the fullPath directly.
+		 */
+		fullPath = href
+	} else if (href.startsWith('/')) {
+		/**
+		 * If the path starts with a slash, we treat it as relative
+		 * to the previous dot-io setup. We prefix it with basePaths[0],
+		 * which should be the product slug.
+		 */
+		fullPath = `/${path.join(basePaths[0], href)}`
+	} else {
+		/**
+		 * If the path does not start with a slash, we treat it as relative
+		 * to the combined current basePath.
+		 */
+		fullPath = `/${path.join(basePaths.join('/'), href)}`
+	}
+	return fullPath
+}
+
+export { fullPathFromRelativeHref }
 export default prepareNavDataForClient
