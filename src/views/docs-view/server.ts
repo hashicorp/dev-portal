f9f2f45aa6839c41c7af5486f59a8519e73bf067
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2022-08-16 13:49:25 -0400

    docs: add TODO comment

--- src/views/docs-view/server.ts
+++ src/views/docs-view/server.ts
@@ -266,6 +266,16 @@ export function getStaticGenerationFunctions<
 			const sidebarNavDataLevels = [
 				generateTopLevelSidebarNavData(product.name),
 				generateProductLandingSidebarNavData(product),
+				/**
+				 * TODO: for nested rootDocsPaths, the backToLink props should
+				 * lead the previous rootDocsPath.
+				 *
+				 * A rootDocsPath is considered "nested" if its path contains a slash.
+				 * If we have a "nested" rootDocsPath, we should try to find the parent
+				 * rootDocsPath. If we can't find a "parent" rootDocsPath,
+				 * or if this is not a "nested" rootDocsPath, then we should use
+				 * a back-to "<product> Home" link.
+				 */
 				{
 					backToLinkProps: {
 						text: `${product.name} Home`,
