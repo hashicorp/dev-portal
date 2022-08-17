888f05fe2c77aeaf05ab4981b9f4e9a9388ee886
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-08-17 10:48:36 -0400

    Adding support for get_started.iconCardLinks

--- src/components/get-started-card/index.tsx
+++ src/components/get-started-card/index.tsx
@@ -1,41 +1,82 @@
-import Card from 'components/card'
 import { IconArrowRight16 } from '@hashicorp/flight-icons/svg-react/arrow-right-16'
+import { IconHcp16 } from '@hashicorp/flight-icons/svg-react/hcp-16'
+import { IconVmware16 } from '@hashicorp/flight-icons/svg-react/vmware-16'
+import { IconKubernetesColor16 } from '@hashicorp/flight-icons/svg-react/kubernetes-color-16'
 import ButtonLink from 'components/button-link'
+import Card from 'components/card'
+import IconCardLink from 'components/icon-card-link'
 import StandaloneLink from 'components/standalone-link'
 import { GetStartedCardProps } from './types'
 import s from './get-started-card.module.css'
+import { developmentToast, ToastColor } from 'components/toast'
+
+// TODO use shared map after other PR is merged
+const SUPPORTED_ICONS = {
+	hcp: <IconHcp16 />,
+	vmware: <IconVmware16 />,
+	'kubernetes-color': <IconKubernetesColor16 />,
+}
 
 function GetStartedCard({
 	heading,
 	headingSlug,
 	body,
 	ctas,
+	iconCardLinks,
 }: GetStartedCardProps) {
+	const hasCtas = ctas !== undefined && ctas !== null
+	const hasIconCardLinks = iconCardLinks !== undefined && iconCardLinks !== null
+	if (hasCtas && hasIconCardLinks) {
+		developmentToast({
+			color: ToastColor.critical,
+			title: 'Error in GetStartedCard',
+			description:
+				'Both `ctas` and `iconCardLinks` were passed to GetStartedCard. Only provide one.',
+		})
+	}
+
 	return (
 		<Card className={s.card}>
 			<h2 id={headingSlug} className={s.heading}>
 				{heading}
 			</h2>
 			<p className={s.body}>{body}</p>
-			<div className={s.ctas}>
-				{ctas.map((cta, idx) => {
-					if (idx == 0) {
-						// eslint-disable-next-line react/no-array-index-key
-						return <ButtonLink key={idx} href={cta.url} text={cta.text} />
-					} else {
+			{ctas && ctas.length ? (
+				<div className={s.ctas}>
+					{ctas.map((cta, idx) => {
+						if (idx == 0) {
+							// eslint-disable-next-line react/no-array-index-key
+							return <ButtonLink key={idx} href={cta.url} text={cta.text} />
+						} else {
+							return (
+								<StandaloneLink
+									// eslint-disable-next-line react/no-array-index-key
+									key={idx}
+									href={cta.url}
+									text={cta.text}
+									icon={<IconArrowRight16 />}
+									iconPosition="trailing"
+								/>
+							)
+						}
+					})}
+				</div>
+			) : null}
+			{iconCardLinks && iconCardLinks.length ? (
+				<ul className={s.iconCardLinks}>
+					{iconCardLinks.map(({ icon, text, url }) => {
 						return (
-							<StandaloneLink
-								// eslint-disable-next-line react/no-array-index-key
-								key={idx}
-								href={cta.url}
-								text={cta.text}
-								icon={<IconArrowRight16 />}
-								iconPosition="trailing"
-							/>
+							<li key={url}>
+								<IconCardLink
+									icon={SUPPORTED_ICONS[icon]}
+									text={text}
+									url={url}
+								/>
+							</li>
 						)
-					}
-				})}
-			</div>
+					})}
+				</ul>
+			) : null}
 		</Card>
 	)
 }
--- src/components/get-started-card/types.ts
+++ src/components/get-started-card/types.ts
@@ -1,9 +1,17 @@
+import { IconCardLinkProps } from 'components/icon-card-link'
+
 export interface GetStartedCardProps {
 	heading: string
 	headingSlug: string
 	body: string
-	ctas: {
+	ctas?: {
 		text: string
 		url: string
+	}[]
+	iconCardLinks?: {
+		// TODO reference SUPPORTED_ICONS
+		icon: string
+		text: IconCardLinkProps['text']
+		url: IconCardLinkProps['url']
 	}[]
 }
