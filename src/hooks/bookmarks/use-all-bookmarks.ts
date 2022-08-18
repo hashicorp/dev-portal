9050eb5f6d06e0009660abdbdc326363f589a900
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-08-18 19:41:53 -0400

    Priming queries in useAllBookmarks

--- src/hooks/bookmarks/use-all-bookmarks.ts
+++ src/hooks/bookmarks/use-all-bookmarks.ts
@@ -1,25 +1,50 @@
-import { useQuery, UseQueryResult } from '@tanstack/react-query'
-import { ApiBookmark } from 'lib/learn-client/api/api-types'
-import { getAllBookmarks } from 'lib/learn-client/api/bookmark'
+import { useQuery, useQueryClient, UseQueryResult } from '@tanstack/react-query'
+import {
+	getAllBookmarks,
+	GetAllBookmarksResult,
+} from 'lib/learn-client/api/bookmark'
 import useAuthentication from 'hooks/use-authentication'
 
-interface UseAllBookmarksResult extends Omit<UseQueryResult, 'data'> {
-	bookmarks: undefined | ApiBookmark[]
+type QueryDataType = GetAllBookmarksResult
+
+interface UseAllBookmarksOptions {
+	enabled?: boolean
+}
+
+interface UseAllBookmarksResult
+	extends Omit<UseQueryResult<QueryDataType>, 'data'> {
+	bookmarks: UseQueryResult<QueryDataType>['data']
 }
 
 /**
  * Handles fetching and storing all bookmarks with React Query.
  */
-const useAllBookmarks = (): UseAllBookmarksResult => {
+const useAllBookmarks = ({
+	enabled,
+}: UseAllBookmarksOptions): UseAllBookmarksResult => {
+	// Get the current Query Client
+	const queryClient = useQueryClient()
+
 	// Get the current user's access token
 	const { session } = useAuthentication()
 	const accessToken = session?.accessToken
 
+	// Set up the `onSuccess` callback
+	const onSuccess = (data: QueryDataType) => {
+		// Prime each individual bookmark query by tutorial id
+		data.forEach((bookmark) => {
+			queryClient.setQueryData(['isBookmarked', bookmark.tutorial_id], true)
+		})
+	}
+
 	// Fetch all bookmarks with the access token
-	const { data: bookmarks, ...restQueryResult } = useQuery(
+	const { data: bookmarks, ...restQueryResult } = useQuery<QueryDataType>(
 		['bookmarks'],
 		() => getAllBookmarks({ accessToken }),
-		{ enabled: !!accessToken }
+		{
+			enabled: enabled && !!accessToken,
+			onSuccess,
+		}
 	)
 
 	return {
@@ -28,5 +53,5 @@ const useAllBookmarks = (): UseAllBookmarksResult => {
 	}
 }
 
-export type { UseAllBookmarksResult }
+export type { UseAllBookmarksOptions, UseAllBookmarksResult }
 export { useAllBookmarks }
