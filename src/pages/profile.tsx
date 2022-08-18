02acddd12f9ccef15c67fb9fea6e43b3babb5aeb
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-08-18 19:45:59 -0400

    Turning profile page into bookmarks testing

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,11 +1,8 @@
-import useAuthentication from 'hooks/use-authentication'
 import BaseNewLayout from 'layouts/base-new'
 import ProfileView from 'views/profile-view'
 
 const ProfilePage = () => {
-	const { user } = useAuthentication()
-
-	return <ProfileView user={user} />
+	return <ProfileView />
 }
 
 ProfilePage.layout = BaseNewLayout
