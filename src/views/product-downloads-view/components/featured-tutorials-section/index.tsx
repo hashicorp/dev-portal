ba6ce3cfe99675aee4bebeeb13fef76b727949c0
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-08-19 08:15:42 -0400

    Consolidating some shared styles

--- src/views/product-downloads-view/components/featured-tutorials-section/index.tsx
+++ src/views/product-downloads-view/components/featured-tutorials-section/index.tsx
@@ -4,7 +4,7 @@ import Heading from 'components/heading'
 import CardsGridList from 'components/cards-grid-list'
 import CollectionCard from 'components/collection-card'
 import TutorialCard from 'components/tutorial-card'
-import s from './featured-tutorials-section.module.css'
+import viewStyles from 'views/product-downloads-view/product-downloads-view.module.css'
 
 interface FeaturedTutorialsSectionProps {
 	featuredLearnCards: FeaturedLearnCard[]
@@ -16,34 +16,32 @@ const FeaturedTutorialsSection = ({
 	return (
 		<>
 			<Heading
-				className={s.sectionHeading}
+				className={viewStyles.heading2}
+				id="featured-tutorials"
 				level={2}
 				size={300}
-				id="featured-tutorials"
 				weight="bold"
 			>
 				Featured Tutorials
 			</Heading>
-			<div className={s.cardGrid}>
-				<CardsGridList>
-					{featuredLearnCards.map((cardProps: FeaturedLearnCard) => {
-						const { id, type } = cardProps
-						if (type == 'collection') {
-							return (
-								<li key={id}>
-									<CollectionCard {...cardProps} />
-								</li>
-							)
-						} else if (type == 'tutorial') {
-							return (
-								<li key={id}>
-									<TutorialCard {...cardProps} />
-								</li>
-							)
-						}
-					})}
-				</CardsGridList>
-			</div>
+			<CardsGridList>
+				{featuredLearnCards.map((cardProps: FeaturedLearnCard) => {
+					const { id, type } = cardProps
+					if (type == 'collection') {
+						return (
+							<li key={id}>
+								<CollectionCard {...cardProps} />
+							</li>
+						)
+					} else if (type == 'tutorial') {
+						return (
+							<li key={id}>
+								<TutorialCard {...cardProps} />
+							</li>
+						)
+					}
+				})}
+			</CardsGridList>
 		</>
 	)
 }
