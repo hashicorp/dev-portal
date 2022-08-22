a333d642b6dc5273ed66b6670fca44f69173ab3b
Kendall Strautman <kendallstrautman@gmail.com>
2022-08-22 16:47:37 -0700

    spike: adds confirmation dialog

--- src/views/profile/bookmarks-view/helpers/render-bookmark-cards.tsx
+++ src/views/profile/bookmarks-view/helpers/render-bookmark-cards.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { getTutorialSlug } from 'views/collection-view/helpers'
 import { formatTutorialData } from 'lib/learn-client/api/tutorial/formatting'
 import { ProductUsed } from 'lib/learn-client/types'
@@ -5,6 +6,7 @@ import { ApiBookmark } from 'lib/learn-client/api/api-types'
 import TutorialCard from 'components/tutorial-card'
 import getReadableTime from 'components/tutorial-meta/components/badges/helpers'
 import { BookmarkButtonIconOnly } from 'components/bookmark-button'
+import Dialog from 'components/dialog'
 
 /**
  * TODO: the bookmark button `handleClick` should
@@ -24,6 +26,7 @@ export default function renderBookmarkCard(bookmark: ApiBookmark) {
 		productsUsed,
 	} = formatTutorialData(bookmark.tutorial)
 	const duration = getReadableTime(readTime)
+
 	return (
 		<li key={id}>
 			<TutorialCard
@@ -37,13 +40,29 @@ export default function renderBookmarkCard(bookmark: ApiBookmark) {
 				eyebrowSlot={
 					<>
 						<span>{duration}</span>
-						<BookmarkButtonIconOnly
-							isBookmarked={true}
-							handleOnClick={() => console.log('Trigger confirmation dialog!')}
-						/>
+						<BookmarkButtonWithRemoveDialog />
 					</>
 				}
 			/>
 		</li>
+	)
+}
+
+function BookmarkButtonWithRemoveDialog() {
+	const [showDialog, setShowDialog] = useState(false)
+	const openDialog = () => setShowDialog(true)
+	const closeDialog = () => setShowDialog(false)
+
+	return (
+		<>
+			<BookmarkButtonIconOnly isBookmarked={true} handleOnClick={openDialog} />
+			<Dialog
+				onDismiss={closeDialog}
+				isOpen={showDialog}
+				label="Confirm remove bookmark"
+			>
+				<h1>hello! do you really want to unbookmark?</h1>
+			</Dialog>
+		</>
 	)
 }
