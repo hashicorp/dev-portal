31fb0d593676b0b1e06bb88999bf3424cdda44c8
Kendall Strautman <kendallstrautman@gmail.com>
2022-08-24 16:13:07 -0700

    oops: fix import

--- src/components/sidebar/components/nav-list/index.tsx
+++ src/components/sidebar/components/nav-list/index.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from 'react'
-import s from '../../style.module.css'
+import s from '../../sidebar.module.css'
 
 export default function NavList({ children }: { children: ReactNode }) {
 	return <ul className={s.navList}>{children}</ul>
