5ba48eacb2ea31d8b79aef0632cb2ac391b392d6
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-08-24 17:32:37 -0400

    Adding Errors component

--- src/components/form/components/index.ts
+++ src/components/form/components/index.ts
@@ -1,5 +1,6 @@
+import Errors, { ErrorsProps } from './errors'
 import HelperText, { HelperTextProps } from './helper-text'
 import Label, { LabelProps } from './label'
 
-export type { HelperTextProps, LabelProps }
-export { HelperText, Label }
+export type { ErrorsProps, HelperTextProps, LabelProps }
+export { Errors, HelperText, Label }
