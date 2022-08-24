154844e0c18b4d23be6111d60a5b7b388754d011
Kendall Strautman <kendallstrautman@gmail.com>
2022-08-24 16:31:39 -0700

    refactor: separate sidebar into own file

--- src/views/profile/bookmarks-view/index.tsx
+++ src/views/profile/bookmarks-view/index.tsx
@@ -1,21 +1,13 @@
 import { useAllBookmarks } from 'hooks/bookmarks'
-import BaseNewLayout from 'layouts/base-new'
+import SidebarSidecarLayout from 'layouts/sidebar-sidecar'
 import AuthenticatedView from 'views/authenticated-view'
 import CardsGridList from 'components/cards-grid-list'
 import Text from 'components/text'
 import Heading from 'components/heading'
 import BookmarksEmptyState from './components/empty-state'
+import { ProfileBookmarksSidebar } from './components/sidebar'
 import renderBookmarkCard from './helpers/render-bookmark-cards'
 import s from './bookmarks-view.module.css'
-import SidebarSidecarLayout from 'layouts/sidebar-sidecar'
-import { generateTopLevelSubNavItems } from 'lib/generate-top-level-sub-nav-items'
-import { SidebarNavMenuItem } from 'components/sidebar/components'
-import Sidebar from 'components/sidebar'
-import { IconSignOut16 } from '@hashicorp/flight-icons/svg-react/sign-out-16'
-import { SidebarNavMenuButton } from 'components/sidebar/components/sidebar-nav-menu-item'
-import useAuthentication from 'hooks/use-authentication'
-import { useDeviceSize } from 'contexts'
-import NavList from 'components/sidebar/components/nav-list'
 
 /**
  * The exported view component that handles wrapping the view content in
@@ -41,51 +33,6 @@ const ProfileBookmarksView = () => {
 				<ProfileBookmarksViewContent />
 			</SidebarSidecarLayout>
 		</AuthenticatedView>
-	)
-}
-
-function ProfileBookmarksSidebar() {
-	const { signOut } = useAuthentication()
-	const { isDesktop } = useDeviceSize()
-
-	return (
-		<Sidebar
-			title={isDesktop ? 'Profile' : 'Main Menu'}
-			showFilterInput={false}
-		>
-			<NavList>
-				{isDesktop ? (
-					<>
-						<SidebarNavMenuItem
-							item={{
-								title: 'Bookmarks',
-								fullPath: '/profile/bookmarks',
-								isActive: true,
-							}}
-						/>
-						<SidebarNavMenuItem
-							item={{
-								title: 'Account Settings',
-								href: 'https://portal.cloud.hashicorp.com/account-settings',
-							}}
-						/>
-						<li>
-							<SidebarNavMenuButton
-								item={{
-									title: 'Sign Out',
-									onClick: () => signOut(),
-									icon: <IconSignOut16 />,
-								}}
-							/>
-						</li>
-					</>
-				) : (
-					generateTopLevelSubNavItems().map((item, index) => (
-						<SidebarNavMenuItem item={item} key={index} />
-					))
-				)}
-			</NavList>
-		</Sidebar>
 	)
 }
 
@@ -129,5 +76,4 @@ const ProfileBookmarksViewContent = () => {
 	)
 }
 
-ProfileBookmarksView.layout = BaseNewLayout
 export default ProfileBookmarksView
