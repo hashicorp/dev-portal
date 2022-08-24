596fec3ca47fccbf96c07e87f6d59faaac285b31
Bryce Kalow <bkalow@hashicorp.com>
2022-08-24 12:09:07 -0500

    adjust types

--- src/views/tutorial-library/utils/router-state.ts
+++ src/views/tutorial-library/utils/router-state.ts
@@ -1,28 +1,37 @@
+import { ParsedUrlQuery } from 'querystring'
+import { InstantSearchProps } from 'react-instantsearch-hooks-web'
 import { stripUndefinedProperties } from 'lib/strip-undefined-props'
 import { INDEX_NAME } from '../constants'
 
 /**
  * Converts query parameters to a structured search state object
  */
-export function routerStateToSearchState(routeState) {
+export function routerStateToSearchState(
+	routeState: ParsedUrlQuery
+): InstantSearchProps['initialUiState'] {
 	return {
 		[INDEX_NAME]: {
-			query: routeState.query,
+			query: routeState.query as string,
 			refinementList: {
-				products: routeState?.product?.split?.(',') ?? [],
+				products: (routeState?.product as string)?.split?.(',') ?? [],
 			},
 			menu: {
-				edition: routeState.edition,
+				edition: routeState.edition as string,
 			},
 			toggle: {
-				hasVideo: routeState.hasVideo,
-				isInteractive: routeState.isInteractive,
+				hasVideo: routeState.hasVideo === 'true',
+				isInteractive: routeState.isInteractive === 'true',
 			},
 		},
 	}
 }
 
-export function searchStateToRouteState(searchState) {
+/**
+ * Converts structured search state into a object to be set as query parameters
+ */
+export function searchStateToRouteState(
+	searchState: InstantSearchProps['initialUiState']
+) {
 	const state = searchState[INDEX_NAME]
 	const result = {
 		query: state.query,
