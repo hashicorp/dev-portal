ad20c284f31fdfa8f853962133bea36d24b36029
Kendall Strautman <kendallstrautman@gmail.com>
2022-08-25 13:20:44 -0700

    chore: fix sort

--- src/views/profile/bookmarks-view/helpers/card-sort-data.ts
+++ src/views/profile/bookmarks-view/helpers/card-sort-data.ts
@@ -4,13 +4,13 @@ export const SortData = {
 	newest: {
 		text: 'Newest',
 		sort: (a: ApiBookmark, b: ApiBookmark) => {
-			return new Date(a.created_at).getTime() - new Date(b.created_at).getTime()
+			return new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
 		},
 	},
 	oldest: {
 		text: 'Oldest',
 		sort: (a: ApiBookmark, b: ApiBookmark) => {
-			return new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
+			return new Date(a.created_at).getTime() - new Date(b.created_at).getTime()
 		},
 	},
 }
