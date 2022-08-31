a02a4f33cb1017ef1a32590ec04545946506e8ec
Kevin Wang <kwangsan@gmail.com>
2022-08-31 10:41:07 -0400

    feat(open-api-page): redesign WIP

--- src/components/open-api-page/partials/operation-object/index.tsx
+++ src/components/open-api-page/partials/operation-object/index.tsx
@@ -1,4 +1,10 @@
 import Collapsible from '../collapsible'
+import Tabs, { Tab } from 'components/tabs'
+import AccordionDisclosure from 'components/accordion-disclosure'
+import Disclosure, {
+	DisclosureActivator,
+	DisclosureContent,
+} from 'components/disclosure'
 import ResponseObject from '../response-object'
 import Parameter from '../parameter'
 import { capitalCase } from 'change-case'
@@ -12,6 +18,7 @@ import s from './style.module.css'
 import { OperationObjectType } from '../../types'
 import { LegacyRef } from 'react'
 import { MdxInlineCode } from 'components/dev-dot-content/mdx-components'
+import Card from 'components/card'
 
 function Parameters({ title, params }) {
 	return (
@@ -36,7 +43,7 @@ function Parameters({ title, params }) {
 export interface OperationObjectProps {
 	/** The path to the endpoint where this operation is executed */
 	path: string
-	/** The type of operation */
+	/** The HTTP verb */
 	type: 'get' | 'put' | 'post' | 'delete' | 'options' | 'head' | 'patch'
 	/** A subset of [Operation Object](https://swagger.io/specification/v2/#operation-object) relevant to the UI */
 	data: OperationObjectType
@@ -70,46 +77,31 @@ function OperationObject({
 	const bodyProps = bodyParam ? getBodyParamProps(bodyParam) : []
 
 	return (
-		<div
-			className={classNames(s.root, {
-				[s.isHeaderHovered]: isHeaderHovered,
-			})}
-		>
-			<button
-				className={s.header}
-				ref={headerRef as LegacyRef<HTMLButtonElement>}
-				onClick={() => setIsCollapsed(!isCollapsed)}
-			>
-				<span className={s.meta}>
-					<span className={s.title}>{title}</span>
-					<span className={s.endpoint}>
-						<MdxInlineCode className={s.method}>
-							{type.toUpperCase()}
-						</MdxInlineCode>
-						<MdxInlineCode className={s.path}>{path}</MdxInlineCode>
+		<div className={classNames(s.root)}>
+			<AccordionDisclosure
+				title={title}
+				description={
+					<span className={s.meta}>
+						<span className={s.endpoint}>
+							<MdxInlineCode className={s.method}>
+								{type.toUpperCase()}
+							</MdxInlineCode>
+							<MdxInlineCode className={s.method}>{path}</MdxInlineCode>
+						</span>
 					</span>
-				</span>
-				<span className={s.toggleText}>
-					{isCollapsed ? 'Expand' : 'Collapse'}
-					<InlineSvg
-						className={classNames(s.toggleIcon, {
-							[s.isCollapsed]: isCollapsed,
-						})}
-						src={svgChevronDown}
-					/>
-				</span>
-			</button>
-			<Collapsible isCollapsed={isCollapsed}>
+				}
+			>
+				<hr />
 				<div className={s.details}>
 					{renderOperationIntro ? renderOperationIntro({ data }) : null}
 					<div
 						className={s.summary}
 						dangerouslySetInnerHTML={{ __html: summary ?? '' }}
 					/>
-					<TwoColumnLayout
-						columnOne={
-							<div>
-								<p className={s.columnHeading}>Request</p>
+					<Tabs showAnchorLine={false}>
+						<Tab heading="Request">
+							<span className={s.spacer} />
+							<Card elevation="base">
 								{pathParams.length > 0 ? (
 									<Parameters title="Path Parameters" params={pathParams} />
 								) : null}
@@ -119,11 +111,12 @@ function OperationObject({
 								{bodyProps.length > 0 ? (
 									<Parameters title="Body Parameters" params={bodyProps} />
 								) : null}
-							</div>
-						}
-						columnTwo={
-							<div>
-								<p className={s.columnHeading}>Response</p>
+							</Card>
+						</Tab>
+
+						<Tab heading="Response">
+							<span className={s.spacer} />
+							<Card elevation="base">
 								{successResponse ? (
 									<>
 										<p className={s.columnSectionHeading}>
@@ -134,11 +127,11 @@ function OperationObject({
 								) : (
 									<p>No response has been defined.</p>
 								)}
-							</div>
-						}
-					/>
+							</Card>
+						</Tab>
+					</Tabs>
 				</div>
-			</Collapsible>
+			</AccordionDisclosure>
 		</div>
 	)
 }
