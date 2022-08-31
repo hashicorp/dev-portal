c3fb7b9f8de36a7838506db85901dfea604b866c
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-08-31 14:46:41 -0400

    Adding generateEnterprisePackageManagers helper

--- src/views/product-downloads-view/helpers.ts
+++ src/views/product-downloads-view/helpers.ts
@@ -74,6 +74,51 @@ export const generateDefaultPackageManagers = (
 	]
 }
 
+export function generateEnterprisePackageManagers(
+	product: Pick<ProductData, 'slug'>
+): PackageManager[] {
+	const productSlug = product.slug
+
+	return [
+		{
+			label: 'Ubuntu/Debian',
+			commands: [
+				`wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg`,
+				`echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list`,
+				`sudo apt update && sudo apt install ${productSlug}-enterprise`,
+			],
+			os: 'linux',
+		},
+		{
+			label: 'CentOS/RHEL',
+			commands: [
+				`sudo yum install -y yum-utils`,
+				`sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo`,
+				`sudo yum -y install ${productSlug}-enterprise`,
+			],
+			os: 'linux',
+		},
+		{
+			label: 'Fedora',
+			commands: [
+				`sudo dnf install -y dnf-plugins-core`,
+				`sudo dnf config-manager --add-repo https://rpm.releases.hashicorp.com/fedora/hashicorp.repo`,
+				`sudo dnf -y install ${productSlug}-enterprise`,
+			],
+			os: 'linux',
+		},
+		{
+			label: 'Amazon Linux',
+			commands: [
+				`sudo yum install -y yum-utils`,
+				`sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo`,
+				`sudo yum -y install ${productSlug}-enterprise`,
+			],
+			os: 'linux',
+		},
+	]
+}
+
 export const generatePackageManagers = ({
 	defaultPackageManagers,
 	packageManagerOverrides,
