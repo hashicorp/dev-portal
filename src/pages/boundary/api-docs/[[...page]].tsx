06937b9b391539ab4d1970cde04d3f9019d37630
Kevin Wang <kwangsan@gmail.com>
2022-09-01 12:20:38 -0400

    chore: refactor `boundary/api-docs`

--- src/pages/boundary/api-docs/[[...page]].tsx
+++ src/pages/boundary/api-docs/[[...page]].tsx
@@ -1,3 +1,5 @@
+import { InferGetStaticPropsType } from 'next'
+
 import SidebarSidecarLayout from 'layouts/sidebar-sidecar'
 import { OpenApiPageContents } from 'components/open-api-page'
 /* Used server-side only */
@@ -21,15 +23,21 @@ const targetFile = {
 // The path to read from when running local preview in the context of the boundary repository
 const targetLocalFile = '../../internal/gen/controller.swagger.json'
 
-function ApiDocsView({ layoutProps, apiPageProps }) {
+import { SidebarSidecarLayoutProps } from 'layouts/sidebar-sidecar'
+import type { PageWithLayout } from 'pages/_app'
+
+function ApiDocsLayout(props: SidebarSidecarLayoutProps) {
+	return <SidebarSidecarLayout {...props} />
+}
+
+type ApiDocsViewProps = InferGetStaticPropsType<typeof getStaticProps>
+const ApiDocsView: PageWithLayout<ApiDocsViewProps> = ({ apiPageProps }) => {
 	return (
-		<SidebarSidecarLayout {...layoutProps} sidecarSlot={null}>
-			<OpenApiPageContents
-				info={apiPageProps.info}
-				operationCategory={apiPageProps.operationCategory}
-				renderOperationIntro={apiPageProps.renderOperationIntro}
-			/>
-		</SidebarSidecarLayout>
+		<OpenApiPageContents
+			info={apiPageProps.info}
+			operationCategory={apiPageProps.operationCategory}
+			renderOperationIntro={null}
+		/>
 	)
 }
 
@@ -135,4 +143,5 @@ export async function getStaticProps({ params }) {
 	}
 }
 
+ApiDocsView.layout = ApiDocsLayout
 export default ApiDocsView
