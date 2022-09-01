486cf59155055c9fac992e1b3da74e4106b8a823
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2022-09-01 09:32:48 -0400

    docs: add comment on rightIcon

--- src/components/sidebar/components/sidebar-nav-menu-item/types.ts
+++ src/components/sidebar/components/sidebar-nav-menu-item/types.ts
@@ -25,6 +25,12 @@ interface SidebarNavMenuItemProps {
 type SupportedIconName = 'home' & ProductSlug
 interface SidebarNavLinkItem extends MenuItem {
 	leadingIconName?: SupportedIconName
+	/**
+	 * Optional icon to display at right of the menu item.
+	 * Note that if this menu item is an external link,
+	 * the `rightIcon` will be ignored and an external link
+	 * icon will be shown instead.
+	 */
 	rightIcon?: ReactElement
 }
 
