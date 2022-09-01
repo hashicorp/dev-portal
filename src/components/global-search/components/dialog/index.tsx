f98c7d646ebf6ef1e75c72ce0dd811a647ce807c
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-08-31 21:27:55 -0400

    Rendering product tag when currentProduct

--- src/components/global-search/components/dialog/index.tsx
+++ src/components/global-search/components/dialog/index.tsx
@@ -1,28 +1,51 @@
+import { useMemo, useState } from 'react'
+import { IconSearch24 } from '@hashicorp/flight-icons/svg-react/search-24'
 import { useCurrentProduct, useGlobalSearch } from 'contexts'
 import Dialog from 'components/dialog'
 import SuggestedPagesList from '../suggested-pages-list'
 import { getSuggestedPages } from './helpers/get-suggested-pages'
 import s from './global-search-dialog.module.css'
+import Tag from 'components/tag'
+import { ProductData } from 'types/products'
 
 const GlobalSearchDialog = () => {
 	const currentProduct = useCurrentProduct()
 	const { isGlobalSearchEnabled, searchIsOpen, toggleSearchIsOpen } =
 		useGlobalSearch()
 
+	// TODO put in a Context
+	const [productFilter, setProductFilter] =
+		useState<ProductData>(currentProduct)
+
+	// variables based on state
+	const suggestedPages = useMemo(
+		() => getSuggestedPages(productFilter),
+		[productFilter]
+	)
+
 	if (!isGlobalSearchEnabled) {
 		return null
 	}
-
-	const suggestedPages = getSuggestedPages(currentProduct)
 
 	return (
 		<Dialog
 			contentClassName={s.content}
 			isOpen={searchIsOpen}
-			onDismiss={() => toggleSearchIsOpen()}
+			onDismiss={() => {
+				toggleSearchIsOpen()
+				setProductFilter(currentProduct)
+			}}
 		>
 			<div className={s.contentInner}>
-				<div className={s.header}>header</div>
+				<div className={s.header}>
+					<IconSearch24 />
+					{productFilter ? (
+						<Tag
+							onRemove={() => setProductFilter(null)}
+							text={productFilter.name}
+						/>
+					) : null}
+				</div>
 				<div className={s.body}>
 					<SuggestedPagesList suggestedPages={suggestedPages} />
 				</div>
