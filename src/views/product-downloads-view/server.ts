b337903517d9ef5ba0042b655ecff15caa0d0510
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-09-02 17:18:37 -0400

    Using generateFeaturedTutorialsCards for featuredLearnCards

--- src/views/product-downloads-view/server.ts
+++ src/views/product-downloads-view/server.ts
@@ -3,14 +3,15 @@ import path from 'path'
 import { ProductData } from 'types/products'
 import { generateStaticProps as generateReleaseStaticProps } from 'lib/fetch-release-data'
 import { stripUndefinedProperties } from 'lib/strip-undefined-props'
-import { formatTutorialCard } from 'components/tutorial-card/helpers'
 import {
 	ProductDownloadsViewStaticProps,
 	RawProductDownloadsViewContent,
 	FeaturedLearnCard,
 } from './types'
-import { getInlineTutorials } from 'views/product-tutorials-view/helpers/get-inline-content'
-import { sortAndFilterReleaseVersions } from './helpers'
+import {
+	generateFeaturedTutorialsCards,
+	sortAndFilterReleaseVersions,
+} from './helpers'
 
 interface GenerateStaticPropsOptions {
 	isEnterpriseMode?: boolean
@@ -59,60 +60,10 @@ const generateGetStaticProps = (
 		})
 
 		/**
-		 * The `featuredTutorialsSlugs` array allows two formats of slugs:
-		 *   - <product slug>/<collection slug>/<tutorial slug>
-		 *   - <product slug>/<tutorial slug>
-		 */
-		const splitSlugs = (slug) => {
-			const slugParts = slug.split('/')
-			if (slugParts.length === 3) {
-				// Slug format is: <product slug>/<collection slug>/<tutorial slug>
-				return {
-					productSlug: slugParts[0],
-					collectionSlug: slugParts[1],
-					tutorialSlug: slugParts[2],
-				}
-			} else if (slugParts.length === 2) {
-				// Slug format is: <product slug>/<tutorial slug>
-				return { productSlug: slugParts[0], tutorialSlug: slugParts[1] }
-			} else {
-				console.error(
-					'Found string `featuredTutorialsSlugs` width incorrect number of slash-separated parts:',
-					slug
-				)
-			}
-		}
-
-		/**
-		 * Gather tutorials and collections based on slugs used
-		 */
-		const tutorialSlugs = featuredTutorialsSlugs.map((slug) => {
-			const { productSlug, tutorialSlug } = splitSlugs(slug)
-			return `${productSlug}/${tutorialSlug}`
-		})
-		const inlineTutorials = await getInlineTutorials(tutorialSlugs)
-
-		/**
 		 * Transform feature tutorial and collection entries into card data
 		 */
-		const featuredLearnCards: FeaturedLearnCard[] = featuredTutorialsSlugs.map(
-			(slug: string) => {
-				const { productSlug, collectionSlug, tutorialSlug } = splitSlugs(slug)
-				const tutorialData = inlineTutorials[`${productSlug}/${tutorialSlug}`]
-				const defaultContext = tutorialData.collectionCtx.default
-				const tutorialLiteCompat = { ...tutorialData, defaultContext }
-
-				const formattedTutorialCard = formatTutorialCard(tutorialLiteCompat)
-				if (collectionSlug) {
-					formattedTutorialCard.url = `/${productSlug}/tutorials/${collectionSlug}/${tutorialSlug}`
-				}
-
-				return {
-					type: 'tutorial',
-					...formattedTutorialCard,
-				}
-			}
-		)
+		const featuredLearnCards: FeaturedLearnCard[] =
+			await generateFeaturedTutorialsCards(featuredTutorialsSlugs)
 
 		/**
 		 * Combine release data and page content
