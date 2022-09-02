832d16e32e8de3bbded503d406147089a62e0e43
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-09-01 22:36:12 -0400

    Adding Consul enterprise download page

---
+++ src/pages/consul/downloads/enterprise.tsx
@@ -0,0 +1,11 @@
+import consulData from 'data/consul.json'
+import { ProductData } from 'types/products'
+import ProductDownloadsView from 'views/product-downloads-view'
+import { generateGetStaticProps } from 'views/product-downloads-view/server'
+
+const getStaticProps = generateGetStaticProps(consulData as ProductData, {
+	isEnterpriseMode: true,
+})
+
+export { getStaticProps }
+export default ProductDownloadsView
