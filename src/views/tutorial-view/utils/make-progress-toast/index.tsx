ec0abd649b85e2bef82d6aee1753f35f28eb9a92
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-09-06 10:55:04 -0400

    fix: use correct toast icon and icon colour

--- src/views/tutorial-view/utils/make-progress-toast/index.tsx
+++ src/views/tutorial-view/utils/make-progress-toast/index.tsx
@@ -3,7 +3,7 @@ import {
 	ToastColor,
 } from 'components/toast/components/toast-display/types'
 import { IconCheckCircle24 } from '@hashicorp/flight-icons/svg-react/check-circle-24'
-import { IconCheck24 } from '@hashicorp/flight-icons/svg-react/check-24'
+import { IconCheckCircleFill24 } from '@hashicorp/flight-icons/svg-react/check-circle-fill-24'
 import { toast } from 'components/toast'
 import s from './progress-toast.module.css'
 
@@ -18,7 +18,7 @@ function generateProgressToast(
 		return {
 			title: 'Collection complete!',
 			description: `Great job, keep up the momentum!`,
-			icon: <IconCheck24 className={s.collectionCompleteIcon} />,
+			icon: <IconCheckCircleFill24 className={s.collectionCompleteIcon} />,
 			color: ToastColor.success,
 		}
 	} else {
