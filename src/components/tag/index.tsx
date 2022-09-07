601ef8da6deac5cd435d2ab8349e44fbca48d67a
Bryce Kalow <bkalow@hashicorp.com>
2022-09-07 16:48:30 -0500

    integrate tags instead of custom buttons

--- src/components/tag/index.tsx
+++ src/components/tag/index.tsx
@@ -2,10 +2,18 @@ import { IconX16 } from '@hashicorp/flight-icons/svg-react/x-16'
 import { TagProps } from './types'
 import s from './tag.module.css'
 
-const Tag = ({ text, onRemove }: TagProps) => {
+const Tag = ({ text, onRemove, buttonAriaLabel }: TagProps) => {
+	const buttonAriaProps = {
+		...(buttonAriaLabel ? { 'aria-label': buttonAriaLabel } : {}),
+	}
+
 	return (
 		<span className={s.root}>
-			<button className={s.removeButton} onClick={onRemove}>
+			<button
+				className={s.removeButton}
+				onClick={onRemove}
+				{...buttonAriaProps}
+			>
 				<IconX16 className={s.removeIcon} />
 			</button>
 			<span className={s.text}>{text}</span>
--- src/components/tag/types.ts
+++ src/components/tag/types.ts
@@ -3,6 +3,7 @@ type NativeButtonProps = JSX.IntrinsicElements['button']
 interface TagProps {
 	onRemove?: NativeButtonProps['onClick']
 	text: string
+	buttonAriaLabel?: string
 }
 
 export type { TagProps }
