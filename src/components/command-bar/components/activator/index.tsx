37efb20669fe4f9e1eb9510a85043996b9ef4f21
Ashlee M Boyer <43934258+ashleemboyer@users.noreply.github.com>
2022-09-07 16:57:02 -0400

    [CommandBarActivator] Add keyboard shortcut Badges (#943)
    * Adding key Badges
    
    * Adding getIsUserAgentMacOs
    
    * Using getIsUserAgentMacOs for key badge
    
    * Only showing cmd key

--- src/components/command-bar/components/activator/index.tsx
+++ src/components/command-bar/components/activator/index.tsx
@@ -1,3 +1,5 @@
+import { IconCommand16 } from '@hashicorp/flight-icons/svg-react/command-16'
+import Badge from 'components/badge'
 import { useCommandBar } from 'components/command-bar'
 import Text from 'components/text'
 import { CommandBarActivatorProps } from './types'
@@ -15,12 +17,28 @@ const CommandBarActivator = ({
 			className={s.root}
 			onClick={() => toggleIsOpen()}
 		>
-			{leadingIcon ? (
-				<span className={s.leadingIcon}>{leadingIcon}</span>
-			) : null}
-			<Text asElement="span" className={s.text} size={200} weight="regular">
-				{visualLabel}
-			</Text>
+			<span className={s.left}>
+				{leadingIcon ? (
+					<span className={s.leadingIcon}>{leadingIcon}</span>
+				) : null}
+				<Text asElement="span" className={s.text} size={200} weight="regular">
+					{visualLabel}
+				</Text>
+			</span>
+			<span className={s.right}>
+				<Badge
+					ariaLabel="Command key"
+					color="neutral-dark-mode"
+					icon={<IconCommand16 />}
+					size="small"
+				/>
+				<Badge
+					ariaLabel="K key"
+					color="neutral-dark-mode"
+					size="small"
+					text="K"
+				/>
+			</span>
 		</button>
 	)
 }
