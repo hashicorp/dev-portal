52048b129f90aa4c04c2e97b5846cc0ca25860f4
Ashlee M Boyer <43934258+ashleemboyer@users.noreply.github.com>
2022-09-08 11:45:55 -0400

    [CommandBarContext] Update to track current tags (#951)
    * Preventing Tag from wrapping text
    
    * Adding currentTags to state
    
    * Removing broken LOC, not necessary anyways

--- src/components/command-bar/components/dialog/index.tsx
+++ src/components/command-bar/components/dialog/index.tsx
@@ -1,16 +1,29 @@
+import { useState } from 'react'
 import { IconCommand16 } from '@hashicorp/flight-icons/svg-react/command-16'
 import Badge from 'components/badge'
-import { useCommandBar, SupportedCommand } from 'components/command-bar'
+import { useCommandBar, CommandBarTag } from 'components/command-bar'
 import Dialog from 'components/dialog'
+import Tag from 'components/tag'
 import { CommandBarDialogFooterProps, CommandBarDialogProps } from './types'
 import s from './command-bar-dialog.module.css'
 
 const CommandBarDialogHeader = () => {
-	const { currentCommand } = useCommandBar()
+	const { currentCommand, currentTags, removeTag } = useCommandBar()
 
 	return (
 		<div className={s.header}>
 			<div className={s.icon}>{currentCommand.icon}</div>
+			{currentTags.length > 0 ? (
+				<div className={s.headerTags}>
+					{currentTags.map((tag: CommandBarTag) => (
+						<Tag
+							key={tag.id}
+							text={tag.text}
+							onRemove={() => removeTag(tag.id)}
+						/>
+					))}
+				</div>
+			) : null}
 			<input
 				className={s.input}
 				placeholder={currentCommand.inputProps.placeholder}
@@ -36,24 +49,19 @@ const CommandBarDialogHeader = () => {
 }
 
 const CommandBarDialogBody = () => {
-	const { currentCommand, setCurrentCommand } = useCommandBar()
+	const { addTag, currentTags } = useCommandBar()
+	const [inputValue, setInputValue] = useState('')
 
 	return (
 		<div className={s.body}>
+			<label>Tag text:</label>
+			<input onChange={(e) => setInputValue(e.target.value)} />
 			<button
-				onClick={() =>
-					setCurrentCommand(
-						currentCommand.name === SupportedCommand.search
-							? SupportedCommand.settings
-							: SupportedCommand.search
-					)
-				}
+				onClick={() => {
+					addTag({ id: currentTags.length.toString(), text: inputValue })
+				}}
 			>
-				Use{' '}
-				{currentCommand.name === SupportedCommand.search
-					? 'settings'
-					: 'search'}{' '}
-				command
+				add tag
 			</button>
 		</div>
 	)
