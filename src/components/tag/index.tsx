90f02af5b4ffeb76b573a63a873a6d681bdc17dc
Bryce Kalow <bkalow@hashicorp.com>
2022-09-07 19:30:10 -0500

    refactor default value for removeButtonAriaLabel

--- src/components/tag/index.tsx
+++ src/components/tag/index.tsx
@@ -2,13 +2,17 @@ import { IconX16 } from '@hashicorp/flight-icons/svg-react/x-16'
 import { TagProps } from './types'
 import s from './tag.module.css'
 
-const Tag = ({ text, onRemove, removeButtonAriaLabel }: TagProps) => {
+const Tag = ({
+	text,
+	onRemove,
+	removeButtonAriaLabel = `Dismiss ${text}`,
+}: TagProps) => {
 	return (
 		<span className={s.root}>
 			<button
 				className={s.removeButton}
 				onClick={onRemove}
-				aria-label={removeButtonAriaLabel ?? `Dismiss ${text}`}
+				aria-label={removeButtonAriaLabel}
 			>
 				<IconX16 className={s.removeIcon} />
 			</button>
