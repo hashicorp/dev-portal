7ed1f9b2edf2e5d76584d857560303ac7d24d7ce
Kevin Wang <kwangsan@gmail.com>
2022-09-08 13:12:17 -0400

    fix sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,6 +2,6 @@
 module.exports = {
 	siteUrl: process.env.SITE_URL || 'https://developer.hashicorp.com',
 	generateRobotsTxt: true, // (optional)
-	exclude: ['/swingset'],
+	exclude: ['/swingset/*'],
 	// ...other options
 }
