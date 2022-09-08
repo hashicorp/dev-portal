efe66407da5f8fd65c48200b1205ee2cf853b454
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-09-08 13:28:18 -0400

    Documenting CurrentContentTypeProvider

--- src/contexts/current-content-type.tsx
+++ src/contexts/current-content-type.tsx
@@ -9,6 +9,18 @@ interface CurrentContentTypeProviderProps {
 	currentContentType: CurrentContentType
 }
 
+/**
+ * Stores the current content type being viewed in a page. This information is
+ * useful when we want to show a different UI based on the type of content a
+ * user is actively viewing.
+ *
+ * Example: Global Search will automatically show tutorial search results before
+ * docs search when `currentContentType` is "tutorials".
+ *
+ * To change the value of `currentContentType`, the `contentType` property can
+ * be added to a component that is rendered as a page. See `CustomPageComponent`
+ * for reference.
+ */
 const CurrentContentTypeProvider = ({
 	children,
 	currentContentType,
