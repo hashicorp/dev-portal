5ea6a40dbc74d2e0895acb4f3dd41021706bdbec
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-09-08 20:28:52 -0400

    Rendering DialogBody and adding toggle button for demo

--- src/components/command-bar/components/dialog/body.tsx
+++ src/components/command-bar/components/dialog/body.tsx
@@ -1,12 +1,24 @@
-import { useCurrentContentType } from 'contexts'
+import { SupportedCommand, useCommandBar } from 'components/command-bar'
 import s from './command-bar-dialog.module.css'
 
 const CommandBarDialogBody = () => {
-	const currentContentType = useCurrentContentType()
+	const { currentCommand, setCurrentCommand } = useCommandBar()
+	const { DialogBody } = currentCommand
 
 	return (
 		<div className={s.body}>
-			<div>Current content type: {currentContentType}</div>
+			<button
+				onClick={() => {
+					if (currentCommand.name === SupportedCommand.search) {
+						setCurrentCommand(SupportedCommand.settings)
+					} else {
+						setCurrentCommand(SupportedCommand.search)
+					}
+				}}
+			>
+				toggle command
+			</button>
+			<DialogBody />
 		</div>
 	)
 }
