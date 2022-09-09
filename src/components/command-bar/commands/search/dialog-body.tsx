be3b0f1fe3c517fc9a1a6e5e7480d323def41ea9
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-09-09 12:01:55 -0400

    Rendering demo in Search dialog body

--- src/components/command-bar/commands/search/dialog-body.tsx
+++ src/components/command-bar/commands/search/dialog-body.tsx
@@ -1,7 +1,15 @@
 import { useCallback, useMemo } from 'react'
+import { IconHistory16 } from '@hashicorp/flight-icons/svg-react/history-16'
 import { useCurrentProduct } from 'contexts'
-import { useSetUpAndCleanUpCommandState } from 'components/command-bar/hooks'
 import { useCommandBar } from 'components/command-bar'
+import { useSetUpAndCleanUpCommandState } from 'components/command-bar/hooks'
+import {
+	CommandBarButtonListItem,
+	CommandBarDivider,
+	CommandBarLinkListItem,
+	CommandBarList,
+} from 'components/command-bar/components'
+import ProductIcon from 'components/product-icon'
 
 const SearchCommandBarDialogBody = () => {
 	const { addTag, removeTag } = useCommandBar()
@@ -39,7 +47,72 @@ const SearchCommandBarDialogBody = () => {
 	 */
 	useSetUpAndCleanUpCommandState(setUpCommandState, cleanUpCommandState)
 
-	return <h1>Search Command Stuff</h1>
+	return (
+		<>
+			<CommandBarList label="Search Results">
+				<CommandBarLinkListItem
+					title="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+					badges={['Terraform', 'Packer', 'Nomad']}
+					description="The AWS secrets engine generates AWS access credentials dynamically based on IAM policies. The AWS secrets engine generates AWS access credentials dynamically based on IAM policies."
+					url="/"
+				/>
+				<CommandBarLinkListItem
+					title="AMI Builder (EBS backed)"
+					badges={['Packer']}
+					description="The AWS secrets engine generates AWS access credentials dynamically based on IAM policies. The AWS secrets engine generates AWS access credentials dynamically based on IAM policies."
+					url="/"
+				/>
+				<CommandBarLinkListItem
+					title="AWS ECS"
+					badges={['Consul']}
+					description="The AWS secrets engine generates AWS access credentials dynamically based on IAM policies. The AWS secrets engine generates AWS access credentials dynamically based on IAM policies."
+					url="/"
+				/>
+			</CommandBarList>
+			<CommandBarDivider />
+			<CommandBarList label="Recent Searches">
+				<CommandBarButtonListItem
+					icon={<IconHistory16 />}
+					title="AWS"
+					onClick={() => {
+						console.log('TODO: set search query to "AWS"')
+					}}
+				/>
+				<CommandBarButtonListItem
+					icon={<IconHistory16 />}
+					title="terraform install"
+					onClick={() => {
+						console.log('TODO: set search query to "terraform install"')
+					}}
+				/>
+				<CommandBarButtonListItem
+					icon={<IconHistory16 />}
+					title="Get started"
+					onClick={() => {
+						console.log('TODO: set search query to "Get started"')
+					}}
+				/>
+			</CommandBarList>
+			<CommandBarDivider />
+			<CommandBarList label="Suggested Pages">
+				<CommandBarLinkListItem
+					icon={<ProductIcon productSlug="hcp" />}
+					title="HashiCorp Cloud Platform"
+					url="/hcp"
+				/>
+				<CommandBarLinkListItem
+					icon={<ProductIcon productSlug="terraform" />}
+					title="Terraform"
+					url="/terraform"
+				/>
+				<CommandBarLinkListItem
+					icon={<ProductIcon productSlug="vault" />}
+					title="Vault"
+					url="/vault"
+				/>
+			</CommandBarList>
+		</>
+	)
 }
 
 export default SearchCommandBarDialogBody
