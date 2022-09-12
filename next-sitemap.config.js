a04ea3b503c6decb48b1e9e0be015a3a5bfa94c5
Kevin Wang <kwangsan@gmail.com>
2022-09-12 14:40:30 -0400

    sitemap: ignore `/profile*`

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,6 +2,6 @@
 module.exports = {
 	siteUrl: process.env.SITE_URL || 'https://developer.hashicorp.com',
 	generateRobotsTxt: true, // (optional)
-	exclude: ['/swingset*', '/onboarding/*'],
+	exclude: ['/swingset*', '/onboarding/*', '/profile*'],
 	// ...other options
 }
