45648e935bfbbe10c73ca5fab7777406bb7d383b
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-09-12 14:26:30 -0400

    Adding docs and tutorials tabs empty states

--- src/components/command-bar/commands/search/dialog-body.tsx
+++ src/components/command-bar/commands/search/dialog-body.tsx
@@ -1,14 +1,88 @@
-import { useCallback, useMemo } from 'react'
+import { useCallback, useMemo, useState } from 'react'
+import { IconGuide16 } from '@hashicorp/flight-icons/svg-react/guide-16'
+import { IconArrowRight16 } from '@hashicorp/flight-icons/svg-react/arrow-right-16'
 import { ProductSlug } from 'types/products'
-import { useCurrentProduct } from 'contexts'
-import { CommandBarTag, useCommandBar } from 'components/command-bar'
+import { useCurrentContentType, useCurrentProduct } from 'contexts'
+import Card from 'components/card'
+import { useCommandBar } from 'components/command-bar'
 import { useSetUpAndCleanUpCommandState } from 'components/command-bar/hooks'
+import { CommandBarDivider } from 'components/command-bar/components'
+import StandaloneLink from 'components/standalone-link'
+import Tabs, { Tab } from 'components/tabs'
 import { generateSuggestedPages } from './helpers/generate-suggested-pages'
 import SuggestedPages, { SuggestedPage } from './suggested-pages'
+import s from './dialog-body.module.css'
+import Text from 'components/text'
+import { getCurrentProductTag } from './helpers/get-current-product-tag'
+
+const DEFAULT_SEARCH_RESULTS = {
+	docs: [],
+	tutorials: [],
+}
+
+const NoResultsMessage = () => {
+	return <p className={s.noResultsMessage}>No results match your search.</p>
+}
+
+const DocumentationTabContents = ({ searchResults, suggestedPages }) => {
+	return searchResults.length === 0 ? (
+		<>
+			<NoResultsMessage />
+			<CommandBarDivider className={s.noDocsResultsDivider} />
+			<SuggestedPages pages={suggestedPages} />
+		</>
+	) : (
+		<p>TODO show docs results</p>
+	)
+}
+
+const TutorialsTabContents = ({ searchResults, tutorialLibraryCta }) => {
+	return searchResults.length === 0 ? (
+		<>
+			<NoResultsMessage />
+			<Card className={s.tutorialLibraryCta} elevation="base">
+				<IconGuide16 className={s.tutorialLibraryCtaIcon} />
+				<Text
+					asElement="span"
+					className={s.tutorialLibraryCtaText}
+					size={200}
+					weight="medium"
+				>
+					{tutorialLibraryCta.text}
+				</Text>
+				<StandaloneLink
+					href={tutorialLibraryCta.href}
+					icon={<IconArrowRight16 />}
+					iconPosition="trailing"
+					size="small"
+					text="Explore"
+				/>
+			</Card>
+		</>
+	) : (
+		<p>TODO show tutorials results</p>
+	)
+}
 
 const SearchCommandBarDialogBody = () => {
 	const { addTag, currentInputValue, currentTags, removeTag } = useCommandBar()
 	const currentProduct = useCurrentProduct()
+	const contentType = useCurrentContentType()
+
+	// TODO `setSearchResults` will be used soon
+	const [searchResults, setSearchResults] = useState(DEFAULT_SEARCH_RESULTS)
+
+	/**
+	 * Get the CommandBarTag object for the current product if it's present.
+	 */
+	const currentProductTag = useMemo(
+		() =>
+			getCurrentProductTag({
+				currentProduct,
+				currentTags,
+			}),
+		[currentProduct, currentTags]
+	)
 
 	/**
 	 * Generate suggested pages, memoized.
@@ -17,11 +91,8 @@ const SearchCommandBarDialogBody = () => {
 	 * waiting to abstract that far for now.
 	 */
 	const suggestedPages = useMemo<SuggestedPage[]>(() => {
-		const currentProductTag = currentProduct
-			? currentTags.find((tag: CommandBarTag) => tag.id === currentProduct.slug)
-			: undefined
 		return generateSuggestedPages(currentProductTag?.id as ProductSlug)
-	}, [currentProduct, currentTags])
+	}, [currentProductTag])
 
 	/**
 	 * Create callback for setting up this command's state.
@@ -49,7 +120,30 @@ const SearchCommandBarDialogBody = () => {
 	return (
 		<>
 			{currentInputValue ? (
-				<p>Current search query: {currentInputValue}</p>
+				<Tabs
+					showAnchorLine={false}
+					initialActiveIndex={contentType === 'tutorials' ? 1 : 0}
+				>
+					<Tab heading="Documentation">
+						<DocumentationTabContents
+							searchResults={searchResults.docs}
+							suggestedPages={suggestedPages}
+						/>
+					</Tab>
+					<Tab heading="Tutorials">
+						<TutorialsTabContents
+							searchResults={searchResults.tutorials}
+							tutorialLibraryCta={{
+								href: currentProductTag
+									? `/tutorials/library?product=${currentProductTag.id}`
+									: '/tutorials/library',
+								text: currentProductTag
+									? `See all ${currentProductTag.text} tutorials in the Tutorial Library`
+									: 'See all tutorials in the Tutorial Library',
+							}}
+						/>
+					</Tab>
+				</Tabs>
 			) : (
 				<SuggestedPages pages={suggestedPages} />
 			)}
