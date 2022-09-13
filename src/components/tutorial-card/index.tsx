888bde3401b3f65f04524547a73268b1b988af00
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2022-09-13 09:46:50 -0400

    feat: tutorial card progress display (#971)
    * refactor: format-tutorial-card requires ctx lite
    
    * refactor: require collectionId on tutorial-card
    
    * feat: stub in tutorial card with auth elements
    
    * fix: type in useTutorialProgress
    
    * refactor: support eyebrow slot aria label
    
    * feat: style tutorial progress icon
    
    * chore: delete unused component
    
    * chore: split out progress-icon-and-label
    
    * docs: update comment w task link
    
    * style: avoid coercing enum to number
    
    Co-authored-by: Bryce Kalow <bkalow@hashicorp.com>
    
    * chore: formatting
    
    Co-authored-by: Bryce Kalow <bkalow@hashicorp.com>

--- src/components/tutorial-card/index.tsx
+++ src/components/tutorial-card/index.tsx
@@ -6,7 +6,11 @@ import {
 	CardBadges,
 	CardBadgeOption,
 } from 'components/tutorial-collection-cards'
-import { buildAriaLabel, TutorialCardWithBookmark } from './helpers'
+import {
+	buildAriaLabel,
+	getSpeakableDuration,
+	TutorialCardWithAuthElements,
+} from './helpers'
 import { TutorialCardProps, TutorialCardPropsWithId } from './types'
 import s from './tutorial-card.module.css'
 
@@ -22,6 +26,7 @@ function TutorialCard({
 	hasVideo,
 	hasInteractiveLab,
 	eyebrowSlot,
+	eyebrowSlotAriaLabel = getSpeakableDuration(duration),
 }: TutorialCardProps) {
 	/**
 	 * Build the array of badges to show at the bottom of the card.
@@ -41,6 +46,7 @@ function TutorialCard({
 		productsUsed,
 		hasVideo,
 		hasInteractiveLab,
+		eyebrowSlotAriaLabel,
 	})
 
 	return (
@@ -54,5 +60,5 @@ function TutorialCard({
 }
 
 export type { TutorialCardProps, TutorialCardPropsWithId }
-export { TutorialCardWithBookmark }
+export { TutorialCardWithAuthElements }
 export default TutorialCard
