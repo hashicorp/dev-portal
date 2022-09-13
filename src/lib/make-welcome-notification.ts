148c878db3b4a570fd22b7ecc71b2575e5db1508
Pamela Bortnick <pbortnick@gmail.com>
2022-09-13 15:12:11 -0400

    Set expiration times on cookies and use cookie for session check

--- src/lib/make-welcome-notification.ts
+++ src/lib/make-welcome-notification.ts
@@ -1,42 +1,52 @@
 import Cookies from 'js-cookie'
 import { toast, ToastColor } from 'components/toast'
 
-const WELCOME_DATE_COOKIE = 'dev-dot-welcome'
+const WELCOME_COOKIE = 'dev-dot-first-welcome-notification'
+const INITIALIZATION_COOKIE = 'dev-dot-welcome-notification-initialized'
 const DISMISS_COOKIE = 'dev-dot-dismiss-welcome'
-const SESSION_ITEM = 'in-dev-dot-welcome-session'
-
-const getCurrentMinutes = () => Date.now() / 60000
+const SESSION_COOKIE = 'dev-dot-welcome-session'
 
 function permanentlyDismiss() {
-	Cookies.set(DISMISS_COOKIE, true)
-	Cookies.remove(WELCOME_DATE_COOKIE)
+	// Set dismiss cookie that expires after a year assuming we won't use these notifications in a year from now (9/13/22)
+	Cookies.set(DISMISS_COOKIE, true, {
+		expires: 365,
+	})
+	Cookies.remove(INITIALIZATION_COOKIE)
+	Cookies.remove(WELCOME_COOKIE)
 }
 
 export function makeWelcomeToast() {
 	const dismissWelcomeCookie = Cookies.get(DISMISS_COOKIE)
-	const inSession = sessionStorage.getItem(SESSION_ITEM)
+	const inSession = Cookies.get(SESSION_COOKIE)
 
 	// Don't show toast if it has been dismissed or session is still active
 	if (dismissWelcomeCookie || inSession) {
 		return
 	}
 
-	const welcomeCookie = Cookies.get(WELCOME_DATE_COOKIE)
+	const welcomeCookie = Cookies.get(WELCOME_COOKIE)
+	const initializationCookie = Cookies.get(INITIALIZATION_COOKIE)
 
-	// If toast was first shown 3 months ago (in minutes), dismiss & don't show again
-	if (welcomeCookie && getCurrentMinutes() - welcomeCookie >= 131400) {
+	// Permanently remove the notification after 3 months by checking if notification has ever been viewed
+	// and if the initial cookie expired (max 3 months)
+	if (initializationCookie && !welcomeCookie) {
 		permanentlyDismiss()
-		return
 	}
 
-	// If no cookie has been set, set cookie as current DateTime in minutes
-	if (!welcomeCookie) {
-		Cookies.set(WELCOME_DATE_COOKIE, getCurrentMinutes())
+	// If no cookie has been set, set cookie that expires after 3 months
+	// Set cookie that shows initialization of notification
+	if (!welcomeCookie && !initializationCookie) {
+		Cookies.set(INITIALIZATION_COOKIE, true, {
+			expires: 180,
+		})
+		Cookies.set(WELCOME_COOKIE, true, {
+			expires: 90,
+		})
 	}
 
-	// Set session item to detect whether or not to display
+	// Set cookie that expires at end of session to detect whether or not to display
 	// We don't need a condition here since we will have returned if session item is defined (line 15)
-	sessionStorage.setItem(SESSION_ITEM, 'yes')
+	Cookies.set(SESSION_COOKIE, true)
 
 	toast({
 		color: ToastColor.highlight,
