550f9ac3b7aeb3d265ef150cc2c34835181700c5
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2022-09-13 22:06:52 -0400

    docs: add ProgressBar description

--- src/components/progress-bar/index.tsx
+++ src/components/progress-bar/index.tsx
@@ -1,6 +1,15 @@
 import { CSSProperties } from 'react'
 import s from './progress-bar.module.css'
 
+/**
+ * Displays a bar that fills to the percent provided
+ * through the `percentDone` prop.
+ *
+ * `ProgressBar` is always `4px` tall, and grows to fill the width of its
+ * parent container. It also accepts an optional `rounded` boolean prop,
+ * which defaults to `true`. If `rounded` is set to `false`, the ends of
+ * the progress bar will end in sharp corners.
+ */
 function ProgressBar({
 	percentDone,
 	rounded = true,
