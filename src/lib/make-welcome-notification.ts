ae0880d80d822ead9916d41e611a41e61595f0bf
Pamela Bortnick <pbortnick@gmail.com>
2022-09-14 17:18:32 -0400

    Return without toast if current date is greater than the latest date it should appear

--- src/lib/make-welcome-notification.ts
+++ src/lib/make-welcome-notification.ts
@@ -2,52 +2,39 @@ import Cookies from 'js-cookie'
 import { toast, ToastColor } from 'components/toast'
 
 const WELCOME_COOKIE = 'dev-dot-first-welcome-notification'
-const INITIALIZATION_COOKIE = 'dev-dot-welcome-notification-initialized'
+
 const DISMISS_COOKIE = 'dev-dot-dismiss-welcome'
 const SESSION_COOKIE = 'dev-dot-welcome-session'
-const TOAST_EXPIRATION = 90
 const AUTO_DISMISS = 15000 // 15 seconds
-const MAX_DATE = new Date('11/1/2023')
+const MAX_DATE = new Date('1/31/2023')
 
 function permanentlyDismiss() {
 	// Set dismiss cookie that expires after a year assuming we won't need these notifications in a year after GA
 	Cookies.set(DISMISS_COOKIE, true, {
 		expires: MAX_DATE,
 	})
-	Cookies.remove(INITIALIZATION_COOKIE)
 	Cookies.remove(WELCOME_COOKIE)
 	Cookies.remove(SESSION_COOKIE)
 }
 
 export function makeWelcomeToast() {
-	const dismissWelcomeToast = Cookies.get(DISMISS_COOKIE)
-	const inSession = Cookies.get(SESSION_COOKIE)
+	// Return if current date is later than the max date
+	if (new Date() > MAX_DATE) {
+		return
+	}
 
 	// Don't show toast if it has been dismissed or session is still active
+	const dismissWelcomeToast = Cookies.get(DISMISS_COOKIE)
+	const inSession = Cookies.get(SESSION_COOKIE)
 	if (dismissWelcomeToast || inSession) {
 		return
 	}
 
+	// If no cookies have been set, set cookie that expires after 3 months from GA
 	const welcomeToastCookie = Cookies.get(WELCOME_COOKIE)
-	const toastInitialized = Cookies.get(INITIALIZATION_COOKIE)
-
-	// Permanently remove the notification after 3 months by checking if:
-	// 1. the notification has ever been viewed and
-	// 2. the welcome cookie expired
-	// 3. the current date is later than the max date we want to display the notifications
-	if ((toastInitialized && !welcomeToastCookie) || new Date() > MAX_DATE) {
-		permanentlyDismiss()
-		return
-	}
-
-	// If no cookies have been set, set cookie that expires after 3 months
-	// Also set cookie that shows initialization of notification
-	if (!welcomeToastCookie && !toastInitialized) {
-		Cookies.set(INITIALIZATION_COOKIE, true, {
-			expires: MAX_DATE,
-		})
+	if (!welcomeToastCookie) {
 		Cookies.set(WELCOME_COOKIE, true, {
-			expires: TOAST_EXPIRATION,
+			expires: MAX_DATE,
 		})
 	}
 
