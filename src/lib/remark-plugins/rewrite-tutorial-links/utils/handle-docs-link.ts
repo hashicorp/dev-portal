ad419b76fbed80f90278fc6d1423c31de3040e17
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-09-15 14:57:59 -0400

    Adding comments

--- src/lib/remark-plugins/rewrite-tutorial-links/utils/handle-docs-link.ts
+++ src/lib/remark-plugins/rewrite-tutorial-links/utils/handle-docs-link.ts
@@ -2,23 +2,35 @@ import path from 'path'
 import { ProductSlug } from 'types/products'
 
 /**
+ * Handles rewriting the `pathname` for a given URL object, which is intended to
+ * be a URL to docs content.
  *
- * This function handles rewrites for docs links in learn content
- * Refer to this whimsical for full mapping - https://whimsical.com/url-remaps-TqyEmfG6gYyiAZR1HWSWEL
+ * Refer to this whimsical for full mapping:
+ * https://whimsical.com/url-remaps-TqyEmfG6gYyiAZR1HWSWEL
  *
- * It accept a nodePath, which should be the url pathname only (e.g. just /docs, not vaultproject.io/docs)
+ * Examples:
  *
- * /docs								-->	/waypoint/docs
- * /api									--> /vault/api-docs
- * /docs/some-doc.html	--> /waypoint/docs/some-doc
- * /api/index.html			--> /waypoint/api-docs
+ *   /docs								-->	/waypoint/docs
+ *   /api									--> /vault/api-docs
+ *   /docs/some-doc.html	--> /waypoint/docs/some-doc
+ *   /api/index.html			--> /waypoint/api-docs
  */
-
 export function handleDocsLink(urlObject: URL, product: ProductSlug) {
+	/**
+	 * Separate the different parts of the URL so they are analyzed in silos.
+	 */
 	const { pathname, search, hash } = urlObject
+
+	/**
+	 * @TODO validate that the pathname parts are valid? (Is any input invalid?)
+	 */
 	const pathnameParts = pathname.split('/')
 	const [, basePath, ...restParts] = pathnameParts
 
+	/**
+	 * Remove `index.html` or a trailing `.html` from the last part of the given
+	 * URL's pathname.
+	 */
 	const numRestParts = restParts.length
 	if (numRestParts > 0) {
 		const lastPart = restParts[numRestParts - 1]
@@ -27,7 +39,20 @@ export function handleDocsLink(urlObject: URL, product: ProductSlug) {
 			.replace('.html', '')
 	}
 
+	/**
+	 * If the basePath (the first part of the given pathname) is `api`, then
+	 * replace it with `api-docs`.
+	 */
 	const finalBasePath = basePath === 'api' ? 'api-docs' : basePath
+
+	/**
+	 * Join all the path parts back together. Using `path` to prevent leading or
+	 * trailing `/` characters.
+	 */
 	const joinedParts = path.join(product, finalBasePath, ...restParts)
+
+	/**
+	 * Piece the URL parts together.
+	 */
 	return `/${joinedParts}${search}${hash}`
 }
