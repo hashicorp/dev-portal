5a2ae98b3211b13fa185090ad55d032031232504
Pamela Bortnick <pbortnick@gmail.com>
2022-09-15 14:11:52 -0400

    Compare dates instaed of cookie check

--- src/lib/make-welcome-notification.ts
+++ src/lib/make-welcome-notification.ts
@@ -1,7 +1,6 @@
 import Cookies from 'js-cookie'
 import { toast, ToastColor } from 'components/toast'
 
-const WELCOME_COOKIE = 'dev-dot-first-welcome-notification'
 const DISMISS_COOKIE = 'dev-dot-dismiss-welcome'
 const SESSION_COOKIE = 'dev-dot-welcome-session'
 const AUTO_DISMISS = 15000 // 15 seconds
@@ -12,13 +11,15 @@ function permanentlyDismiss() {
 	Cookies.set(DISMISS_COOKIE, true, {
 		expires: MAX_DATE,
 	})
-	Cookies.remove(WELCOME_COOKIE)
 	Cookies.remove(SESSION_COOKIE)
 }
 
-const hasWelcomeToast = () => Cookies.get(WELCOME_COOKIE)
-
 export function makeWelcomeToast() {
+	// If the current date is later than the last day we want to show the notifications, return
+	if (new Date() > MAX_DATE) {
+		return
+	}
+
 	// Don't show toast if it has been dismissed or session is still active
 	const hasDismissedWelcomeToast = Cookies.get(DISMISS_COOKIE)
 	const inSession = Cookies.get(SESSION_COOKIE)
@@ -26,26 +27,17 @@ export function makeWelcomeToast() {
 		return
 	}
 
-	// If no cookies have been set, set cookie that expires after 3 months from GA
-	if (!hasWelcomeToast()) {
-		Cookies.set(WELCOME_COOKIE, true, {
-			expires: MAX_DATE,
-		})
-	}
-
 	// Set cookie that expires at end of session to detect whether or not to display
 	// We don't need a condition here since we will have already returned if session is active
 	Cookies.set(SESSION_COOKIE, true)
 
 	// If welcome toast cookie has not expired and all other checks have passed, render toast
-	if (hasWelcomeToast()) {
-		toast({
-			color: ToastColor.highlight,
-			title: `Welcome to HashiCorp Developer!`,
-			description: 'Your destination for documentation and tutorials.',
-			autoDismiss: AUTO_DISMISS,
-			onDismissCallback: permanentlyDismiss,
-			dismissOnRouteChange: false,
-		})
-	}
+	toast({
+		color: ToastColor.highlight,
+		title: `Welcome to HashiCorp Developer!`,
+		description: 'Your destination for documentation and tutorials.',
+		autoDismiss: AUTO_DISMISS,
+		onDismissCallback: permanentlyDismiss,
+		dismissOnRouteChange: false,
+	})
 }
