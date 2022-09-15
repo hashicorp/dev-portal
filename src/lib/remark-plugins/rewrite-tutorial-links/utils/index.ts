950144dec28b3b8b072f32dc2c8d2d778d10d132
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-09-14 20:21:35 -0400

    Renaming file

--- src/lib/remark-plugins/rewrite-tutorial-links/utils/index.ts
+++ src/lib/remark-plugins/rewrite-tutorial-links/utils/index.ts
@@ -1,4 +1,4 @@
 export { getTutorialMap } from './get-tutorial-map'
 export { handleCollectionLink } from './handle-collection-link'
 export { handleDocsLink } from './handle-docs-links'
-export { handleTutorialLink } from './handle-learn-links'
+export { handleTutorialLink } from './handle-tutorial-link'
