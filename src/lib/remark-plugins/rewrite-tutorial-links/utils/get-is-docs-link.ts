dc7de255997cca1f8eadda679b4f3cca10346a81
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-09-16 16:37:23 -0400

    Refactoring getIsDocsLink based on test cases

--- src/lib/remark-plugins/rewrite-tutorial-links/utils/get-is-docs-link.ts
+++ src/lib/remark-plugins/rewrite-tutorial-links/utils/get-is-docs-link.ts
@@ -1,36 +1,71 @@
 import { productSlugsToHostNames } from 'lib/products'
-
-const ACCEPTED_DOCS_PATHNAMES = [
-	'docs',
-	'api',
-	'api-docs',
-	'commands',
-	'plugins',
-	'tools',
-	'vagrant-cloud',
-	'intro',
-	'cdktf',
-	'cli',
-	'cloud-docs',
-	'enterprise',
-	'internals',
-	'language',
-	'plugin',
-	'registry',
-]
-
-const DOCS_LINK_REGEX = new RegExp(
-	`(${Object.values(productSlugsToHostNames).join(
-		'|'
-	)})/(${ACCEPTED_DOCS_PATHNAMES.join('|')})`
-)
+import { ProductData, ProductSlug } from 'types/products'
 
 /**
  * Determine whether the given `link` is a URL referencing product docs sites
  * that are external to the Learn platform.
  */
 const getIsDocsLink = (link: string): boolean => {
-	return DOCS_LINK_REGEX.test(link)
+	try {
+		const urlObject = new URL(link)
+		const { origin, pathname } = urlObject
+		const basePath = pathname.split('/')[1]
+
+		/**
+		 * Try to parse the product slug from the URL origin.
+		 */
+		const productSlug = Object.keys(productSlugsToHostNames).find(
+			(productSlug: ProductSlug) => {
+				const productHostname = productSlugsToHostNames[productSlug]
+				return origin.includes(productHostname)
+			}
+		)
+
+		/**
+		 * If a product slug couldn't be parsed, then it's not a docs link.
+		 */
+		const isDocsHostname = !!productSlug
+		if (!isDocsHostname) {
+			return false
+		}
+
+		/**
+		 * If there is no base path, then it's a docs site home page.
+		 */
+		if (basePath === '') {
+			return true
+		}
+
+		/**
+		 * Load the `basePaths` configured for the parsed product, and allow `api`
+		 * as one of the product's accepted base paths if `api-docs` is accepted.
+		 */
+		// eslint-disable-next-line @typescript-eslint/no-var-requires
+		const productData = require(`data/${productSlug}.json`) as ProductData
+		const acceptedBasePaths = productData.basePaths
+		if (acceptedBasePaths.includes('api-docs')) {
+			acceptedBasePaths.push('api')
+		}
+
+		/**
+		 * Check if the base path is one of the product's accepted base paths.
+		 */
+		const isBasePathAccepted =
+			acceptedBasePaths.length === 0 ||
+			acceptedBasePaths.some((acceptedBasePath: string) => {
+				return basePath.startsWith(acceptedBasePath)
+			})
+
+		/**
+		 * Return whether or not the base path is accepted.
+		 */
+		return isBasePathAccepted
+	} catch (error) {
+		/**
+		 * If `link` isn't a fully valid URL, then it's not an external docs link.
+		 */
+		return false
+	}
 }
 
 export { getIsDocsLink }
