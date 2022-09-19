88f72d3f58f1e7bc8850a284f8e128d51839e603
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2022-09-19 11:45:21 -0400

    feat: remove use of addOverviewItem

--- src/types/products.ts
+++ src/types/products.ts
@@ -124,15 +124,6 @@ interface RootDocsPath {
 	visuallyHideSidebarTitle?: boolean
 
 	/**
-	 * An optional property to add an "overview" item to the sidebar.
-	 * By default, an overview item will be added. This property must be
-	 * explicitly set to `false` to prevent an overview item from being added.
-	 * The `href` for this item will lead to the root docs path,
-	 * and will dynamically account for version context.
-	 */
-	addOverviewItem?: boolean
-
-	/**
 	 * An optional description for this category of documentation.
 	 * Shown as the subtitle of the docs landing hero element.
 	 * If omitted, falls back to the page's authored frontMatter.description,
