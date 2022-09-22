bd3235c8c9885e1a9a50913699d96b3af0c0485f
Kevin Wang <kwangsan@gmail.com>
2022-09-22 17:49:51 -0400

    revert signOutWrapper

--- src/hooks/use-authentication/helpers/sign-out-wrapper.ts
+++ src/hooks/use-authentication/helpers/sign-out-wrapper.ts
@@ -1,26 +1,15 @@
 import { signOut, SignOutParams } from 'next-auth/react'
 
-// global state to dedupe multiple signOut requests
-let isCalling = false
-
 /**
  * A minimal wrapper around next-auth/react's `signOut` function. Purpose is to
  * handle invoking the wrapped function with a default value.
  *
  * https://next-auth.js.org/getting-started/client#signout
  */
-const signOutWrapper = async (options: SignOutParams = {}) => {
+const signOutWrapper = (options: SignOutParams = {}) => {
 	const { callbackUrl = '/', redirect = true, ...restOptions } = options
 
-	if (isCalling) {
-		return
-	}
-	try {
-		isCalling = true
-		await signOut({ callbackUrl, redirect, ...restOptions })
-	} finally {
-		isCalling = false
-	}
+	return signOut({ callbackUrl, redirect, ...restOptions })
 }
 
 export { signOutWrapper }
