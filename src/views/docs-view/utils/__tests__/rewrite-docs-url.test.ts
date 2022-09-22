dd482eb686a06c5155e29973ee9a692ae0b35692
Kendall Strautman <36613477+kendallstrautman@users.noreply.github.com>
2022-09-22 09:26:49 -0700

    patch: external .io links (#1066)

--- src/views/docs-view/utils/__tests__/rewrite-docs-url.test.ts
+++ src/views/docs-view/utils/__tests__/rewrite-docs-url.test.ts
@@ -4,13 +4,15 @@ import { rewriteDocsUrl } from '../product-url-adjusters'
 
 describe('rewriteDocsUrl', () => {
 	describe('/downloads links', () => {
-		test.each(productSlugs)(
+		// there is no downloads link for hcp
+		const productsToTest = productSlugs.filter((slug) => slug !== 'hcp')
+
+		test.each(productsToTest)(
 			'when currentProduct is %p',
 			(productSlug: ProductSlug) => {
 				// eslint-disable-next-line @typescript-eslint/no-var-requires
 				const productData = require(`data/${productSlug}.json`)
-				const expectedOutput =
-					productSlug === 'hcp' ? '/downloads' : `/${productSlug}/downloads`
+				const expectedOutput = `/${productSlug}/downloads`
 				expect(rewriteDocsUrl('/downloads', productData)).toBe(expectedOutput)
 
 				// special case for vagrant vmware utility downloads page
@@ -21,5 +23,23 @@ describe('rewriteDocsUrl', () => {
 				}
 			}
 		)
+	})
+
+	describe('Rewrite non-docs base paths to external links', () => {
+		const testData = [
+			{ input: '/pricing', expected: 'https://cloud.hashicorp.com/pricing' },
+			{ input: '/sla', expected: 'https://cloud.hashicorp.com/sla' },
+			{
+				input: '/products/vault',
+				expected: 'https://cloud.hashicorp.com/products/vault',
+			},
+			{ input: '/consul/docs/some-path', expected: '/consul/docs/some-path' }, // base case, it shouldn't get rewritten
+		]
+		// eslint-disable-next-line @typescript-eslint/no-var-requires
+		const hcpProductData = require(`data/hcp.json`)
+
+		test.each(testData)('Testing subpath', (item) => {
+			expect(rewriteDocsUrl(item.input, hcpProductData)).toBe(item.expected)
+		})
 	})
 })
