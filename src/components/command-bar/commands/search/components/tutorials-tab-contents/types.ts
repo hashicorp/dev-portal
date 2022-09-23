d9db190eaf9522edc4e1150e15729c33ccffc953
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-09-23 12:45:08 -0400

    Adding TutorialHit

--- src/components/command-bar/commands/search/components/tutorials-tab-contents/types.ts
+++ src/components/command-bar/commands/search/components/tutorials-tab-contents/types.ts
@@ -1,9 +1,29 @@
 interface TutorialsTabContentsProps {
-	searchResults: $TSFixMe[]
 	tutorialLibraryCta: {
 		href: string
 		text: string
 	}
 }
 
-export type { TutorialsTabContentsProps }
+interface TutorialHitObject extends Record<string, unknown> {
+	_highlightResult: {
+		description: {
+			value: string
+		}
+		name: {
+			value: string
+		}
+	}
+	defaultContext: {
+		slug: string
+	}
+	id: string
+	products: string[]
+	slug: string
+}
+
+interface TutorialHitProps {
+	hit: TutorialHitObject
+}
+
+export type { TutorialHitObject, TutorialHitProps, TutorialsTabContentsProps }
