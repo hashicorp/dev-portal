7c31f7ecb79e77ec145249d12e0acc58f40a8f9e
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-09-23 12:44:43 -0400

    Adding DocumentationHit

---
+++ src/components/command-bar/commands/search/components/documentation-tab-contents/documentation-hit.tsx
@@ -0,0 +1,22 @@
+import { productSlugsToNames } from 'lib/products'
+import { CommandBarLinkListItem } from 'components/command-bar/components'
+import { DocumentationHitObject, DocumentationHitProps } from './types'
+
+const DocumentationHit = ({ hit }: DocumentationHitProps) => {
+	const { objectID, _highlightResult, product } = hit
+	const { page_title, description } = _highlightResult
+	const productName = product === 'hcp' ? 'HCP' : productSlugsToNames[product]
+
+	return (
+		<CommandBarLinkListItem
+			key={objectID}
+			title={page_title?.value}
+			description={description?.value}
+			url={`/${objectID}`}
+			badges={productName ? [productName] : undefined}
+		/>
+	)
+}
+
+export type { DocumentationHitObject, DocumentationHitProps }
+export default DocumentationHit
--- src/components/command-bar/commands/search/components/documentation-tab-contents/types.ts
+++ src/components/command-bar/commands/search/components/documentation-tab-contents/types.ts
@@ -1,8 +1,28 @@
-import { SuggestedPagesProps } from '../../types'
+import { SuggestedPagesProps } from '../suggested-pages/types'
 
 interface DocumentationTabContentsProps {
-	searchResults: $TSFixMe[]
 	suggestedPages: SuggestedPagesProps['pages']
 }
 
-export type { DocumentationTabContentsProps }
+interface DocumentationHitObject extends Record<string, unknown> {
+	objectID: string
+	_highlightResult: {
+		page_title: {
+			value: string
+		}
+		description: {
+			value: string
+		}
+	}
+	product: string
+}
+
+interface DocumentationHitProps {
+	hit: DocumentationHitObject
+}
+
+export type {
+	DocumentationHitObject,
+	DocumentationHitProps,
+	DocumentationTabContentsProps,
+}
