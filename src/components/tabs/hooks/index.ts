ee02ae7d8f126e7371ab6c9501c9c328283f1cf6
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-09-23 17:10:23 -0400

    Removing useSyncedTabGroups

--- src/components/tabs/hooks/index.ts
+++ src/components/tabs/hooks/index.ts
@@ -1,5 +1,4 @@
 import useHasOverflow from './use-has-overflow'
-import useSyncedTabGroups from './use-synced-tab-groups'
 import useTabItems from './use-tab-items'
 
-export { useHasOverflow, useSyncedTabGroups, useTabItems }
+export { useHasOverflow, useTabItems }
