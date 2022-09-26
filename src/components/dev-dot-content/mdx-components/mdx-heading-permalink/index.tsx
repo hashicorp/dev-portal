f212deb63e3c6da211e18510447b5080f2389957
Pamela Bortnick <pbortnick@gmail.com>
2022-09-26 16:15:11 -0400

    Add clipboard copy

--- src/components/dev-dot-content/mdx-components/mdx-heading-permalink/index.tsx
+++ src/components/dev-dot-content/mdx-components/mdx-heading-permalink/index.tsx
@@ -5,8 +5,16 @@ import s from './mdx-heading-permalink.module.css'
 export default function MdxHeadingPermalink(props) {
 	const { className, ...rest } = props
 
+	function copyLinkToClipboard(e) {
+		navigator.clipboard.writeText(e.target.closest('a').href)
+	}
+
 	return (
-		<a className={classNames(s.root, className)} {...rest}>
+		<a
+			className={classNames(s.root, className)}
+			onClick={copyLinkToClipboard}
+			{...rest}
+		>
 			<div className={s.icon}>
 				<IconLink16 color="var(--token-color-foreground-faint)" />
 			</div>
