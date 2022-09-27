355959fcd57f7fa6cc9a9f45d2e0d6e2de045ff5
Pamela Bortnick <pbortnick@gmail.com>
2022-09-27 11:54:53 -0400

    Remove click to copy

--- src/components/dev-dot-content/mdx-components/mdx-heading-permalink/index.tsx
+++ src/components/dev-dot-content/mdx-components/mdx-heading-permalink/index.tsx
@@ -1,34 +1,15 @@
 import classNames from 'classnames'
 import { IconLink16 } from '@hashicorp/flight-icons/svg-react/link-16'
-import Tooltip from 'components/tooltip'
 import s from './mdx-heading-permalink.module.css'
 
 export default function MdxHeadingPermalink(props) {
 	const { className, ...rest } = props
 
-	function copyLinkToClipboard(e) {
-		const href = e.target.closest('a').href
-		navigator.clipboard.writeText(href).then(
-			() => {
-				console.log(`Successfully copied ${href} to clipboard`)
-			},
-			() => {
-				console.error(`Error copying ${href} to clipboard`)
-			}
-		)
-	}
-
 	return (
-		<Tooltip theme="light" label="Clicking will copy link to clipboard">
-			<a
-				className={classNames(s.root, className)}
-				onClick={copyLinkToClipboard}
-				{...rest}
-			>
-				<div className={s.icon}>
-					<IconLink16 color="var(--token-color-foreground-faint)" />
-				</div>
-			</a>
-		</Tooltip>
+		<a className={classNames(s.root, className)} {...rest}>
+			<div className={s.icon}>
+				<IconLink16 color="var(--token-color-foreground-faint)" />
+			</div>
+		</a>
 	)
 }
