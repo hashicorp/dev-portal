3f38ba8ce610f4d1853024a8559fb8c05446b141
Pamela Bortnick <pbortnick@gmail.com>
2022-09-27 11:26:44 -0400

    Add tooltip and writeText error handling

--- src/components/dev-dot-content/mdx-components/mdx-heading-permalink/index.tsx
+++ src/components/dev-dot-content/mdx-components/mdx-heading-permalink/index.tsx
@@ -1,23 +1,34 @@
 import classNames from 'classnames'
 import { IconLink16 } from '@hashicorp/flight-icons/svg-react/link-16'
+import Tooltip from 'components/tooltip'
 import s from './mdx-heading-permalink.module.css'
 
 export default function MdxHeadingPermalink(props) {
 	const { className, ...rest } = props
 
 	function copyLinkToClipboard(e) {
-		navigator.clipboard.writeText(e.target.closest('a').href)
+		const href = e.target.closest('a').href
+		navigator.clipboard.writeText(href).then(
+			() => {
+				console.log(`Successfully copied  ${href} to clipboard`)
+			},
+			() => {
+				console.error(`Error copying ${href} to clipboard`)
+			}
+		)
 	}
 
 	return (
-		<a
-			className={classNames(s.root, className)}
-			onClick={copyLinkToClipboard}
-			{...rest}
-		>
-			<div className={s.icon}>
-				<IconLink16 color="var(--token-color-foreground-faint)" />
-			</div>
-		</a>
+		<Tooltip theme="light" label="Clicking will copy link to clipboard">
+			<a
+				className={classNames(s.root, className)}
+				onClick={copyLinkToClipboard}
+				{...rest}
+			>
+				<div className={s.icon}>
+					<IconLink16 color="var(--token-color-foreground-faint)" />
+				</div>
+			</a>
+		</Tooltip>
 	)
 }
