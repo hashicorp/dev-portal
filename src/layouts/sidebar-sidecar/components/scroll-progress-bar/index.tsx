424d970b458585ae66889cf55dd8281ef016c75f
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2022-09-28 16:50:26 -0400

    Revert "feat: use framer-motion directly"
    This reverts commit e8df6c25c31c37ab2d002f79f7948a3fecefe72a.

--- src/layouts/sidebar-sidecar/components/scroll-progress-bar/index.tsx
+++ src/layouts/sidebar-sidecar/components/scroll-progress-bar/index.tsx
@@ -1,15 +1,13 @@
-// import ProgressBar from 'components/progress-bar'
-import { m, MotionValue } from 'framer-motion'
+import ProgressBar from 'components/progress-bar'
 import s from './scroll-progress-bar.module.css'
 
 /**
  * Displays a scroll progress bar at the bottom of SidebarSidecarLayout.
  */
-function ScrollProgressBar({ progress }: { progress: MotionValue<number> }) {
+function ScrollProgressBar({ progress }: { progress: number }) {
 	return (
 		<div className={s.root}>
-			{/* <ProgressBar percentDone={progress * 100} rounded={false} /> */}
-			<m.div className={s.progress} style={{ scaleX: progress }} />
+			<ProgressBar percentDone={progress * 100} rounded={false} />
 		</div>
 	)
 }
