fba612609e094d82b44c27ba9de4653685205dbb
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-09-28 14:01:05 -0400

    Using local storage for recent searches

--- src/components/command-bar/commands/search/components/dialog-body/index.tsx
+++ src/components/command-bar/commands/search/components/dialog-body/index.tsx
@@ -1,12 +1,18 @@
-import { useCallback, useMemo } from 'react'
+import { useCallback, useEffect, useMemo, useState } from 'react'
 import algoliasearch from 'algoliasearch'
 import { Configure, InstantSearch } from 'react-instantsearch-hooks-web'
 import { IconDocs16 } from '@hashicorp/flight-icons/svg-react/docs-16'
+import { IconHistory16 } from '@hashicorp/flight-icons/svg-react/history-16'
 import { IconLearn16 } from '@hashicorp/flight-icons/svg-react/learn-16'
 import { ProductSlug } from 'types/products'
 import { useCurrentContentType, useCurrentProduct } from 'contexts'
 import { CommandBarTag, useCommandBar } from 'components/command-bar'
 import { useSetUpAndCleanUpCommandState } from 'components/command-bar/hooks'
+import {
+	CommandBarButtonListItem,
+	CommandBarDivider,
+	CommandBarList,
+} from 'components/command-bar/components'
 import Tabs, { Tab } from 'components/tabs'
 import {
 	generateSuggestedPages,
@@ -41,6 +47,33 @@ const SearchCommandBarDialogBodyContent = ({
 		return generateSuggestedPages(currentProductTag?.id as ProductSlug)
 	}, [currentProductTag])
 
+	let recentSearches
+	if (typeof window !== 'undefined') {
+		const storageValue = JSON.parse(
+			window.localStorage.getItem('RECENT_SEARCHES')
+		) as string[]
+
+		if (storageValue.length > 0) {
+			recentSearches = (
+				<>
+					<CommandBarList label="Recent Searches">
+						{storageValue.reverse().map((recent) => {
+							return (
+								<CommandBarButtonListItem
+									key={recent}
+									icon={<IconHistory16 />}
+									onClick={() => console.log('clicked', recent)}
+									title={recent}
+								/>
+							)
+						})}
+					</CommandBarList>
+					<CommandBarDivider />
+				</>
+			)
+		}
+	}
+
 	return currentInputValue ? (
 		<div className={s.tabsWrapper}>
 			<Tabs
@@ -64,6 +97,7 @@ const SearchCommandBarDialogBodyContent = ({
 		</div>
 	) : (
 		<div className={s.suggestedPagesWrapper}>
+			{recentSearches}
 			<SuggestedPages pages={suggestedPages} />
 		</div>
 	)
@@ -72,6 +106,50 @@ const SearchCommandBarDialogBodyContent = ({
 const SearchCommandBarDialogBody = () => {
 	const currentProduct = useCurrentProduct()
 	const { addTag, currentInputValue, currentTags, removeTag } = useCommandBar()
+	const [searchQuery, setSearchQuery] = useState(undefined)
+
+	/**
+	 * Delay sending off search queries while the user is typing.
+	 */
+	useEffect(() => {
+		const typingDebounce = setTimeout(() => {
+			setSearchQuery(currentInputValue)
+		}, 300)
+
+		return () => clearTimeout(typingDebounce)
+	}, [currentInputValue])
+
+	/**
+	 * Handle updating local storage when the `searchQuery` updates.
+	 */
+	useEffect(() => {
+		if (typeof window === 'undefined' || !searchQuery) {
+			return
+		}
+
+		const localStorage = window.localStorage
+		const rawValue = localStorage.getItem('RECENT_SEARCHES')
+		const recentSearches = rawValue ? JSON.parse(rawValue) : []
+
+		if (!recentSearches || recentSearches.length === 0) {
+			localStorage.setItem('RECENT_SEARCHES', JSON.stringify([searchQuery]))
+			return
+		}
+
+		const indexOfQuery = recentSearches.indexOf(searchQuery)
+		if (indexOfQuery >= 0) {
+			recentSearches.splice(indexOfQuery, 1)
+		}
+
+		recentSearches.push(searchQuery)
+
+		let newValue = recentSearches
+		if (recentSearches.length > 3) {
+			newValue = recentSearches.slice(-3)
+		}
+
+		localStorage.setItem('RECENT_SEARCHES', JSON.stringify(newValue))
+	}, [searchQuery])
 
 	/**
 	 * Create callback for setting up this command's state.
@@ -113,7 +191,7 @@ const SearchCommandBarDialogBody = () => {
 			indexName={__config.dev_dot.algolia.tutorialsIndexName}
 			searchClient={searchClient}
 		>
-			<Configure query={currentInputValue} />
+			<Configure query={searchQuery} />
 			<SearchCommandBarDialogBodyContent
 				currentProductTag={currentProductTag}
 			/>
