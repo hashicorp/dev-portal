3fbf61d97dac30d0186061f218870b739650be9a
Pamela Bortnick <pbortnick@gmail.com>
2022-09-28 10:42:21 -0400

    Use useWindowSize

--- src/components/command-bar/components/dialog/index.tsx
+++ src/components/command-bar/components/dialog/index.tsx
@@ -4,17 +4,21 @@ import CommandBarDialogHeader from './header'
 import CommandBarDialogFooter from './footer'
 import CommandBarDialogBody from './body'
 import s from './command-bar-dialog.module.css'
+import useWindowSize from 'hooks/use-window-size'
 
 const CommandBarDialog = ({
 	isOpen = false,
 	onDismiss = () => null,
 }: CommandBarDialogProps) => {
+	const { width } = useWindowSize()
+
 	return (
 		<Dialog
 			contentClassName={s.content}
-			contentWrapperClassName={s.contentWrapper}
+			// contentWrapperClassName={s.contentWrapper}
 			isOpen={isOpen}
 			onDismiss={onDismiss}
+			variant={width <= 728 ? 'bottom' : null}
 		>
 			<div className={s.contentInner}>
 				<CommandBarDialogHeader />
