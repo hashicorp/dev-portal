2cd7cceda256aa99c44f9007c8391a251c15ee98
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-09-29 19:37:16 -0400

    Only removing trailing /index for now

--- src/components/command-bar/commands/search/components/documentation-tab-contents/documentation-hit.tsx
+++ src/components/command-bar/commands/search/components/documentation-tab-contents/documentation-hit.tsx
@@ -5,64 +5,10 @@ import { DocumentationHitObject, DocumentationHitProps } from './types'
 const IS_DEV = process.env.NODE_ENV !== 'production'
 
 /**
- * @TODO import this from the source:
- * https://github.com/hashicorp/web-platform-packages/blob/main/packages/remark-plugins/util/generate-slug.js
+ * @TODO - does not yet handle linking to specific headings.
  */
-const generateHeadingSlug = (heading, allHeadingLinks = []) => {
-	let slug = heading
-		.toLowerCase()
-		.trim()
-		.replace(/<\/?[^>]*>/g, '') // Strip links
-		.replace(/\(\(#.*?\)\)/g, '') // Strip anchor link aliases
-		.replace(/\W+/g, '-') // Whitespace to '-'
-		.replace(/-+/g, '-') // Collapse more than one '-'
-		.replace(/^-/g, '') // Remove leading '-'
-		.replace(/-$/g, '') // Remove trailing '-'
-
-	// count if there are any duplicates on the page
-	const dupeCount = allHeadingLinks.reduce((m, i) => {
-		if (slug === i) {
-			m++
-		}
-		return m
-	}, 0)
-	allHeadingLinks.push(slug)
-
-	// append the count to the end of the slug if necessary
-	if (dupeCount > 0) {
-		slug = `${slug}-${dupeCount}`
-	}
-
-	return slug
-}
-
 const generateDocumentationHitUrl = (hit: DocumentationHitObject) => {
-	let url = `/${hit.objectID.replace(/\/index$/, '')}`
-
-	const titleDoesNotMatch =
-		hit._highlightResult?.page_title?.matchLevel === 'none'
-	const descriptionDoesNotMatch =
-		hit._highlightResult?.description?.matchLevel === 'none'
-	if (titleDoesNotMatch && descriptionDoesNotMatch) {
-		const headingSlugs = []
-		const matchingHeadingSlugs = []
-
-		hit._highlightResult?.headings?.forEach((headingHighlightResult, index) => {
-			const headingSlug = generateHeadingSlug(
-				hit?.headings[index],
-				headingSlugs
-			)
-			if (headingHighlightResult?.matchLevel !== 'none') {
-				matchingHeadingSlugs.push(headingSlug)
-			}
-		})
-
-		if (matchingHeadingSlugs?.length === 1) {
-			url = `${url}#${generateHeadingSlug(matchingHeadingSlugs[0])}`
-		}
-	}
-
-	return url
+	return `/${hit.objectID.replace(/\/index$/, '')}`
 }
 
 const DocumentationHit = ({ hit }: DocumentationHitProps) => {
