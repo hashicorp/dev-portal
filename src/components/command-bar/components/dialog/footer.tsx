2983a3126373316b8060d66877b7c1dd59bf8cd2
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-09-28 23:46:36 -0400

    Removing arrow key instructions

--- src/components/command-bar/components/dialog/footer.tsx
+++ src/components/command-bar/components/dialog/footer.tsx
@@ -1,5 +1,3 @@
-import { IconArrowDown16 } from '@hashicorp/flight-icons/svg-react/arrow-down-16'
-import { IconArrowUp16 } from '@hashicorp/flight-icons/svg-react/arrow-up-16'
 import { IconCornerDownLeft16 } from '@hashicorp/flight-icons/svg-react/corner-down-left-16'
 import { IconExternalLink16 } from '@hashicorp/flight-icons/svg-react/external-link-16'
 import Badge from 'components/badge'
@@ -7,32 +5,12 @@ import Text from 'components/text'
 import { FORM_URL } from 'components/navigation-header/components/give-feedback-button'
 import StandaloneLink from 'components/standalone-link'
 import s from './command-bar-dialog.module.css'
-import { IconCornerDownLeft16 } from '@hashicorp/flight-icons/svg-react/corner-down-left-16'
 
 const CommandBarDialogFooter = () => {
 	return (
 		<div className={s.footer}>
 			<div className={s.footerLeft}>
 				<Badge color="neutral" text="Tab" type="outlined" size="small" />
-				<Text asElement="span" size={100} weight="regular">
-					and
-				</Text>
-				<div className={s.footerLeftArrowKeys}>
-					<Badge
-						ariaLabel="Up arrow key"
-						color="neutral"
-						icon={<IconArrowUp16 />}
-						type="outlined"
-						size="small"
-					/>
-					<Badge
-						ariaLabel="Down arrow key"
-						color="neutral"
-						icon={<IconArrowDown16 />}
-						type="outlined"
-						size="small"
-					/>
-				</div>
 				<Text asElement="span" size={100} weight="regular">
 					to navigate,
 				</Text>
