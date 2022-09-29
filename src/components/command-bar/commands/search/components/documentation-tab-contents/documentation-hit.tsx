4a5222e5e1733947ed4efe4c207fa4f01ee9fd30
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-09-29 17:16:55 -0400

    Return null and warn if _highlightResult is falsy

--- src/components/command-bar/commands/search/components/documentation-tab-contents/documentation-hit.tsx
+++ src/components/command-bar/commands/search/components/documentation-tab-contents/documentation-hit.tsx
@@ -2,8 +2,26 @@ import { productSlugsToNames } from 'lib/products'
 import { CommandBarLinkListItem } from 'components/command-bar/components'
 import { DocumentationHitObject, DocumentationHitProps } from './types'
 
+const IS_DEV = process.env.NODE_ENV !== 'production'
+
 const DocumentationHit = ({ hit }: DocumentationHitProps) => {
 	const { objectID, _highlightResult, product } = hit
+
+	/**
+	 * If no _highlightResult, the hit is likely invalid and links to a page that
+	 * doesn't exist. So we log a dev warning and return `null`.
+	 */
+	if (!_highlightResult) {
+		if (IS_DEV) {
+			console.warn(
+				'[DocumentationHit] Found a `hit` with no `_highlightResult`:\n',
+				JSON.stringify(hit, null, 2)
+			)
+		}
+
+		return null
+	}
+
 	const { page_title, description } = _highlightResult
 	const productName = product === 'hcp' ? 'HCP' : productSlugsToNames[product]
 
