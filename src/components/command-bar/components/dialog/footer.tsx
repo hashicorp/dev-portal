f8ddfc3093215b4b4703d71e60125f928efa21ca
Ashlee M Boyer <43934258+ashleemboyer@users.noreply.github.com>
2022-09-30 09:42:33 -0500

    [Global Search] Keyboard Navigation (#1095)
    * Adding missing enter key badge
    
    * Changing "or" to "and"
    
    * Skateboarding keyboard nav
    
    * Removing tabindex 0 from tabpanel
    
    * Removing tabindex -1 from command bar list items
    
    * Removing arrow key instructions
    
    * Adjusting focus styles
    
    * Removing no longer needed tests
    
    * Undoing arrow key changes
    
    * Undoing label change
    
    * Fixing CommandBarList aria-labelledby

--- src/components/command-bar/components/dialog/footer.tsx
+++ src/components/command-bar/components/dialog/footer.tsx
@@ -1,37 +1,16 @@
-import { IconArrowDown16 } from '@hashicorp/flight-icons/svg-react/arrow-down-16'
-import { IconArrowUp16 } from '@hashicorp/flight-icons/svg-react/arrow-up-16'
+import { IconCornerDownLeft16 } from '@hashicorp/flight-icons/svg-react/corner-down-left-16'
 import { IconExternalLink16 } from '@hashicorp/flight-icons/svg-react/external-link-16'
 import Badge from 'components/badge'
 import Text from 'components/text'
 import { FORM_URL } from 'components/navigation-header/components/give-feedback-button'
 import StandaloneLink from 'components/standalone-link'
 import s from './command-bar-dialog.module.css'
-import { IconCornerDownLeft16 } from '@hashicorp/flight-icons/svg-react/corner-down-left-16'
 
 const CommandBarDialogFooter = () => {
 	return (
 		<div className={s.footer}>
 			<div className={s.footerLeft}>
 				<Badge color="neutral" text="Tab" type="filled" size="small" />
-				<Text asElement="span" size={100} weight="regular">
-					or
-				</Text>
-				<div className={s.footerLeftArrowKeys}>
-					<Badge
-						ariaLabel="Up arrow key"
-						color="neutral"
-						icon={<IconArrowUp16 />}
-						type="filled"
-						size="small"
-					/>
-					<Badge
-						ariaLabel="Down arrow key"
-						color="neutral"
-						icon={<IconArrowDown16 />}
-						type="filled"
-						size="small"
-					/>
-				</div>
 				<Text asElement="span" size={100} weight="regular">
 					to navigate,
 				</Text>
