d1bdb6f51db6e218ad699b3488a7b0cf6cbcab04
Pamela Bortnick <pbortnick@gmail.com>
2022-09-30 16:11:01 -0400

    Remove unrecognized props from permalink component

--- src/components/dev-dot-content/mdx-components/mdx-heading-permalink/index.tsx
+++ src/components/dev-dot-content/mdx-components/mdx-heading-permalink/index.tsx
@@ -3,10 +3,14 @@ import { IconLink16 } from '@hashicorp/flight-icons/svg-react/link-16'
 import s from './mdx-heading-permalink.module.css'
 
 export default function MdxHeadingPermalink(props) {
-	const { className, level, ...rest } = props
+	const { className, level, href, 'aria-label': ariaLabel } = props
 
 	return (
-		<a className={classNames(s.root, className, s[`h${level}`])} {...rest}>
+		<a
+			className={classNames(s.root, className, s[`h${level}`])}
+			aria-label={ariaLabel}
+			href={href}
+		>
 			<IconLink16 className={s.icon} />
 		</a>
 	)
