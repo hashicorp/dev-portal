ad513cbc80ae06765bcb15463938bd792d0e6350
Kevin Wang <kwangsan@gmail.com>
2022-10-03 15:00:22 -0400

    refactor code

--- src/views/docs-view/utils/get-deploy-preview-loader.ts
+++ src/views/docs-view/utils/get-deploy-preview-loader.ts
@@ -67,37 +67,38 @@ export function getDeployPreviewLoader({
 					? loaderOptions.remarkPlugins(params)
 					: loaderOptions.remarkPlugins
 
+			// Custom handling for TF projects whose images reside in `website/img`
+			// rather than `website/public` (Next.js serves static assets from `public`)
+			const remarkTerraformPlugins = []
+			if (currentRootDocsPath.productSlugForLoader?.includes('terraform')) {
+				remarkTerraformPlugins.push(
+					remarkRewriteAssets({
+						product: currentRootDocsPath.productSlugForLoader,
+						version: process.env.CURRENT_GIT_BRANCH,
+						getAssetPathParts: (nodeUrl) => {
+							// special case for CDKTF's relative paths ('./image.png')
+							if (
+								currentRootDocsPath.productSlugForLoader === 'terraform-cdk'
+							) {
+								return Array.isArray(params.page)
+									? [
+											'website/docs/cdktf',
+											...params.page,
+											nodeUrl.startsWith('.') ? `.${nodeUrl}` : `../${nodeUrl}`,
+									  ]
+									: ['website/docs/cdktf', nodeUrl]
+							}
+							// all other TF subprojects
+							return ['website', nodeUrl]
+						},
+					})
+				)
+			}
+
 			return [
 				...remarkPluginsFromExtraOptions,
 				...remarkPluginsForFileSystemContent,
-				// Custom handling for TF projects whose images reside in `website/img`
-				// rather than `website/public` (Next.js serves static assets from `public`)
-				...(currentRootDocsPath.productSlugForLoader?.includes('terraform')
-					? [
-							remarkRewriteAssets({
-								product: currentRootDocsPath.productSlugForLoader,
-								version: process.env.CURRENT_GIT_BRANCH,
-								getAssetPathParts: (nodeUrl) => {
-									// special case for CDKTF's relative paths ('./image.png')
-									if (
-										currentRootDocsPath.productSlugForLoader === 'terraform-cdk'
-									) {
-										return Array.isArray(params.page)
-											? [
-													'website/docs/cdktf',
-													...params.page,
-													nodeUrl.startsWith('.')
-														? `.${nodeUrl}`
-														: `../${nodeUrl}`,
-											  ]
-											: ['website/docs/cdktf', nodeUrl]
-									}
-									// all other TF subprojects
-									return ['website', nodeUrl]
-								},
-							}),
-					  ]
-					: []),
+				...remarkTerraformPlugins,
 			]
 		},
 	})
