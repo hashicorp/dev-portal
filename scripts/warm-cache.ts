1c0b32132e50c9204ab7251469bfcf019f75b348
Bryce Kalow <bkalow@hashicorp.com>
2022-10-05 16:44:53 -0500

    feat: update cache warmer to use the revalidate endpoint for developer docs paths (#1177)

--- scripts/warm-cache.ts
+++ scripts/warm-cache.ts
@@ -9,7 +9,7 @@ import {
 } from 'lib/learn-client/api/collection'
 import { splitProductFromFilename } from 'views/tutorial-view/utils'
 import config from '../config/base.json'
-import { productSlugs } from 'lib/products'
+import { activeProductSlugs } from 'lib/products'
 import { ProductSlug } from 'types/products'
 
 interface StaticPathsResponse {
@@ -28,7 +28,8 @@ interface StaticPathsResponse {
 
 const DEV_PORTAL_URL = config.dev_dot.canonical_base_url
 
-const fetch = createFetch()
+// the socket timeout here matches Vercel's serverless function execution timeout (900s) as defined here: https://vercel.com/docs/concepts/limits/overview#general-limits
+const fetch = createFetch(null, { timeout: 900 * 1000 })
 
 /**
  * Returns a string representing a date that's `daysAgo` in the past, truncated
@@ -70,6 +71,42 @@ async function getUrlsToCache(product: string): Promise<string[]> {
 	return data.result.paths.map((p) => {
 		return [baseUrl, p.params.page.join('/')].join('/')
 	})
+}
+
+/**
+ * Uses the /api/revalidate endpoint to statically generate all docs pages for each product
+ */
+async function warmDeveloperDocsCache() {
+	const url = new URL('/api/revalidate', DEV_PORTAL_URL)
+
+	for (const productSlug of activeProductSlugs) {
+		const body = JSON.stringify({ product: productSlug })
+
+		try {
+			console.log(`triggering revalidate for ${productSlug}...`)
+			const startTime = Date.now()
+
+			await fetch(url.toString(), {
+				method: 'POST',
+				body,
+				headers: {
+					'content-type': 'application/json',
+					authorization: `Bearer ${process.env.REVALIDATE_TOKEN}`,
+				},
+			})
+
+			const elapsedTime = Date.now() - startTime
+			console.log(
+				`successfully warmed the cache for ${productSlug}'s docs routes (${elapsedTime}ms)`
+			)
+		} catch (error) {
+			console.log(
+				'failed to trigger revalidate for product:',
+				productSlug,
+				error
+			)
+		}
+	}
 }
 
 // Fetch all tutorial paths per beta product
@@ -114,7 +151,7 @@ async function getTutorialUrlsToCache(product: ProductSlug): Promise<string[]> {
 
 		const tutorialUrls = (
 			await Promise.all(
-				productSlugs.map((product: ProductSlug) =>
+				activeProductSlugs.map((product: ProductSlug) =>
 					getTutorialUrlsToCache(product)
 				)
 			)
@@ -138,6 +175,10 @@ async function getTutorialUrlsToCache(product: ProductSlug): Promise<string[]> {
 			},
 			{ concurrency: 16, stopOnError: false }
 		)
+
+		console.log('Triggering revalidate for documentation pages')
+		await warmDeveloperDocsCache()
+
 		process.exit()
 	} catch (err) {
 		console.error(err)
