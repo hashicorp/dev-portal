1b55dca5f962b321c7b544eb304247086035ed8f
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-10-06 14:17:12 -0400

    Separating CardTitle text styles

--- src/components/card/components/card-title/index.tsx
+++ src/components/card/components/card-title/index.tsx
@@ -1,9 +1,12 @@
-import classNames from 'classnames'
 import { CardTitleProps } from './types'
 import s from './card-title.module.css'
 
 const CardTitle = ({ className, text }: CardTitleProps) => {
-	return <div className={classNames(s.root, className)}>{text}</div>
+	return (
+		<div className={className}>
+			<span className={s.text}>{text}</span>
+		</div>
+	)
 }
 
 export type { CardTitleProps }
