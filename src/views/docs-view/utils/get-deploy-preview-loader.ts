1130edb79a44e9d01627e0d469cb93105e098ccd
Kevin Wang <kwangsan@gmail.com>
2022-10-07 16:41:24 -0400

    Update src/views/docs-view/utils/get-deploy-preview-loader.ts

--- src/views/docs-view/utils/get-deploy-preview-loader.ts
+++ src/views/docs-view/utils/get-deploy-preview-loader.ts
@@ -72,7 +72,7 @@ export function getDeployPreviewLoader({
 			// - images reside in `website/img` rather than `website/public` (Next.js serves static assets from `public`)
 			// - TFE/C content exclusion markdown comments are used
 			const remarkTerraformPlugins = []
-			if (currentRootDocsPath.productSlugForLoader?.match(/(^terraform-)/i)) {
+			if (currentRootDocsPath.productSlugForLoader?.match(/^terraform/i)) {
 				remarkTerraformPlugins.push(
 					remarkRewriteAssets({
 						product: currentRootDocsPath.productSlugForLoader,
