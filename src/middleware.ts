25c4f28806b7e9858771ec96e3de03bc0dc918f3
Kevin Wang <kwangsan@gmail.com>
2022-10-12 12:29:57 -0400

    Revert "feat: middleware time logging + custom matcher (#1206)"
    This reverts commit 0964f82067279f41170679582cc1b2a9b965f363.

--- src/middleware.ts
+++ src/middleware.ts
@@ -34,8 +34,6 @@ function determineProductSlug(req: NextRequest): string {
  * - Handling the opt in for cookie setting
  */
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
-	const label = `[middleware] ${req.nextUrl.pathname}`
-	console.time(label)
 	// Handle redirects
 	const product = determineProductSlug(req)
 	// Sets a cookie named hc_geo on the response
@@ -52,7 +50,6 @@ export function middleware(req: NextRequest, ev: NextFetchEvent) {
 			)
 		}
 		if (destination.startsWith('http')) {
-			console.timeEnd(label)
 			return NextResponse.redirect(destination, permanent ? 308 : 307)
 		}
 
@@ -60,7 +57,6 @@ export function middleware(req: NextRequest, ev: NextFetchEvent) {
 		// request URL to adjust the pathname in an absolute URL
 		const url = req.nextUrl.clone()
 		url.pathname = destination
-		console.timeEnd(label)
 		return NextResponse.redirect(url, permanent ? 308 : 307)
 	}
 
@@ -78,7 +74,6 @@ export function middleware(req: NextRequest, ev: NextFetchEvent) {
 
 		deleteCookie(req, response, `${product}-io-beta-opt-in`)
 
-		console.timeEnd(label)
 		return response
 	}
 
@@ -110,7 +105,6 @@ export function middleware(req: NextRequest, ev: NextFetchEvent) {
 
 			const response = NextResponse.redirect(redirectUrl, redirectStatus)
 
-			console.timeEnd(label)
 			return response
 		}
 	}
@@ -139,22 +133,6 @@ export function middleware(req: NextRequest, ev: NextFetchEvent) {
 		})
 	}
 
-	console.timeEnd(label)
 	// Continue request processing
 	return response
-}
-
-export const config = {
-	matcher: [
-		/*
-		 * Match all request paths except for the ones starting with:
-		 * - api (API routes)
-		 * - static (static files)
-		 * - _next/static (Next.js static files)
-		 * - img (image assets)
-		 * - favicon.ico (favicon file)
-		 * - icon (hashicorp logo)
-		 */
-		'/((?!api|static|_next\\/static|img|favicon.ico|icon).*)',
-	],
 }
