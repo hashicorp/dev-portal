29d69e7aef8b557f60e7e4ecaa9b833f6de94f17
Bryce Kalow <bkalow@hashicorp.com>
2022-10-14 11:09:23 -0500

    adds static paths from analytics for learn

--- src/pages/[productSlug]/tutorials/[...tutorialSlug]/index.tsx
+++ src/pages/[productSlug]/tutorials/[...tutorialSlug]/index.tsx
@@ -5,6 +5,7 @@ import {
 } from 'next'
 import { LearnProductData, LearnProductSlug } from 'types/products'
 import { cachedGetProductData } from 'lib/get-product-data'
+import { getStaticPathsFromAnalytics } from 'lib/get-static-paths-from-analytics'
 import TutorialView from 'views/tutorial-view'
 import {
 	TutorialPageProps,
@@ -12,13 +13,38 @@ import {
 	getTutorialPagePaths,
 	getTutorialPageProps,
 } from 'views/tutorial-view/server'
+import { activeProductSlugs } from 'lib/products'
 
 async function getStaticPaths(): Promise<
 	GetStaticPathsResult<TutorialPagePaths['params']>
 > {
-	const paths = await getTutorialPagePaths()
+	const validPaths = await getTutorialPagePaths()
+
+	const paths = (
+		await Promise.all(
+			activeProductSlugs.map(async (productSlug) => {
+				// fetch paths from analytics for each product
+				const analyticsPaths = await getStaticPathsFromAnalytics({
+					param: 'tutorialSlug',
+					limit: __config.learn.max_static_paths ?? 0,
+					pathPrefix: `/${productSlug}/tutorials`,
+					validPaths,
+				})
+
+				// add the productSlug param to the resulting params object
+				return analyticsPaths.map((result) => {
+					// @ts-expect-error - this is okay
+					result.params.productSlug = productSlug
+
+					return result
+				})
+			})
+		)
+	).flat()
+
 	return {
-		paths: paths.slice(0, __config.learn.max_static_paths ?? 0),
+		// @ts-expect-error - TODO need to refine the types here
+		paths,
 		fallback: 'blocking',
 	}
 }
