32dc6f69e078aa9729d26e8fb90b1f702f494043
Kevin Wang <kwangsan@gmail.com>
2022-10-17 16:30:09 -0400

    fix(breadcrumb-bar): drop URL-less items

--- src/components/breadcrumb-bar/index.tsx
+++ src/components/breadcrumb-bar/index.tsx
@@ -19,6 +19,7 @@ function BreadcrumbBar({
 }: {
 	links: BreadcrumbLink[]
 }): React.ReactElement {
+	console.log('links', links)
 	// For now, we want to strictly require that all
 	// breadcrumb link URLs, if present, are relative rather
 	// than absolute links
