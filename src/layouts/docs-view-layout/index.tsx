bdd219f2a19bfceef7910cccec0a069daf9e279c
Bryce Kalow <bkalow@hashicorp.com>
2022-10-20 09:33:01 -0500

    try to fix client-side error on error page

--- src/layouts/docs-view-layout/index.tsx
+++ src/layouts/docs-view-layout/index.tsx
@@ -13,10 +13,10 @@ import getIsBetaProduct from 'lib/get-is-beta-product'
 const DocsViewLayout = (props: SidebarSidecarLayoutProps) => {
 	const currentProduct = useCurrentProduct()
 	// Products will be removed from the beta array as we shift them to GA, which will hide the opt out button
-	const isBetaProduct = getIsBetaProduct(currentProduct.slug)
+	const isBetaProduct = getIsBetaProduct(currentProduct?.slug)
 
 	const optInOutSlot = isBetaProduct ? (
-		<OptInOut platform={`${currentProduct.slug}-io`} />
+		<OptInOut platform={`${currentProduct?.slug}-io`} />
 	) : null
 	return <SidebarSidecarLayout {...props} optInOutSlot={optInOutSlot} />
 }
