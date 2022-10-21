ac0808dcc586c17f8bb6292bf01af9523ce50383
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-10-21 10:22:26 -0400

    feat: Packer GA

--- build-libs/__tests__/build-beta-opt-in-redirect.test.ts
+++ build-libs/__tests__/build-beta-opt-in-redirect.test.ts
@@ -5,22 +5,6 @@ describe('buildBetaOptInRedirect', () => {
 		expect(buildBetaProductOptInRedirect()).toMatchInlineSnapshot(`
 		Array [
 		  Object {
-		    "destination": "https://developer.hashicorp.com/packer/:base/:path*",
-		    "has": Array [
-		      Object {
-		        "key": "packer-io-beta-opt-in",
-		        "type": "cookie",
-		        "value": "true",
-		      },
-		      Object {
-		        "type": "host",
-		        "value": "(www\\\\.packer\\\\.io|test-pk\\\\.hashi-mktg\\\\.com)",
-		      },
-		    ],
-		    "permanent": false,
-		    "source": "/:base(docs|guides|intro|plugins|downloads)/:path*",
-		  },
-		  Object {
 		    "destination": "https://developer.hashicorp.com/vagrant/:base/:path*",
 		    "has": Array [
 		      Object {
