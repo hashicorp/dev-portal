c603174775ae4f62b7aeaade5c13a05843c43ff5
Manish Honnatti <manish.honnatti@hashicorp.com>
2022-10-24 12:59:24 -0700

    chore: add '/_next/' paths to 'disallow' in robots.txt (#1286)
    * chore: add '/_next' paths to 'disallow' in robots.txt
    
    * reduce scope to '/_next/'

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,6 +2,15 @@
 module.exports = {
 	siteUrl: process.env.SITE_URL || 'https://developer.hashicorp.com',
 	generateRobotsTxt: true, // (optional)
+	robotsTxtOptions: {
+		policies: [
+			{
+				userAgent: '*',
+				allow: '/',
+				disallow: '/_next/',
+			},
+		],
+	},
 	exclude: ['/swingset*', '/onboarding/*', '/profile*'],
 	// ...other options
 }
