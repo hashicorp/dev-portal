1da756013ec18fa0426fef29248920724aaeb8a6
Manish Honnatti <manish.honnatti@hashicorp.com>
2022-10-25 06:47:15 -0700

    chore: add Nextjs JSON paths to 'disallow' in robots.txt (#1292)

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,5 +3,14 @@ module.exports = {
 	siteUrl: process.env.SITE_URL || 'https://developer.hashicorp.com',
 	generateRobotsTxt: true, // (optional)
 	exclude: ['/swingset*', '/onboarding/*', '/profile*'],
+	robotsTxtOptions: {
+		policies: [
+			{
+				userAgent: '*',
+				allow: '/',
+				disallow: '/_next/data/*.json',
+			},
+		],
+	},
 	// ...other options
 }
