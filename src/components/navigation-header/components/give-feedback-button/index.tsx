d57bdd01e5f2f741e0a34bb06cbedfc3aee83452
Kendall Strautman <36613477+kendallstrautman@users.noreply.github.com>
2022-10-25 10:32:08 -0700

    chore: remove beta language (#1291)

--- src/components/navigation-header/components/give-feedback-button/index.tsx
+++ src/components/navigation-header/components/give-feedback-button/index.tsx
@@ -2,9 +2,9 @@ import classNames from 'classnames'
 import Image from 'next/image'
 import { IconExternalLink16 } from '@hashicorp/flight-icons/svg-react/external-link-16'
 import ButtonLink from 'components/button-link'
+import { FEEDBACK_FORM_URL } from 'constants/feedback-form'
 import s from './give-feedback-button.module.css'
 
-export const FORM_URL = 'https://forms.gle/fnHLuNahLEhjuKvE6'
 const LINK_TEXT = 'Give beta feedback'
 const LINK_ARIA_LABEL = 'Give beta feedback (opens in new tab)'
 
@@ -20,7 +20,7 @@ function GiveFeedbackButton({
 			{allowIconOnly ? (
 				<span className={s.iconButtonContainer}>
 					<a
-						href={FORM_URL}
+						href={FEEDBACK_FORM_URL}
 						aria-label={LINK_ARIA_LABEL}
 						className={classNames(s.iconButton, s.primary)}
 						target="_blank"
@@ -38,7 +38,7 @@ function GiveFeedbackButton({
 			<span className={allowIconOnly ? s.textButtonContainer : undefined}>
 				<ButtonLink
 					aria-label={LINK_ARIA_LABEL}
-					href={FORM_URL}
+					href={FEEDBACK_FORM_URL}
 					icon={<IconExternalLink16 />}
 					iconPosition="trailing"
 					openInNewTab
