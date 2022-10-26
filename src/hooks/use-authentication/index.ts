08a5cb65b2571c581e1618103147e6bf3f61526c
Pamela Bortnick <pbortnick@gmail.com>
2022-10-26 13:24:15 -0400

    Set Segment id to accessToken

--- src/hooks/use-authentication/index.ts
+++ src/hooks/use-authentication/index.ts
@@ -88,7 +88,7 @@ const useAuthentication = (
 		const segmentUserId = safeGetSegmentId()
 		if (segmentUserId !== session.accessToken) {
 			// 	analytics?.accessToken(
-			// 		session.id,
+			// 		session.accessToken,
 			// 		{
 			// 			email: user.email,
 			// 			leadSource: 'DevPortal Sign Up',
