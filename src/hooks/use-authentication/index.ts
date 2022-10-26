7d00b319fb0179c2648ce162c5992bb4a9ce4b02
Pamela Bortnick <pbortnick@gmail.com>
2022-10-26 10:35:45 -0400

    Use accessToken

--- src/hooks/use-authentication/index.ts
+++ src/hooks/use-authentication/index.ts
@@ -86,8 +86,8 @@ const useAuthentication = (
 		delete session.user
 
 		const segmentUserId = safeGetSegmentId()
-		if (segmentUserId !== session.id) {
-			// 	analytics?.identify(
+		if (segmentUserId !== session.accessToken) {
+			// 	analytics?.accessToken(
 			// 		session.id,
 			// 		{
 			// 			email: user.email,
