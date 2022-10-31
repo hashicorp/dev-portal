3272bb37d006ea6dd9d069e6e324732a6a56852c
Pamela Bortnick <pbortnick@gmail.com>
2022-10-31 12:56:23 -0400

    update: use NextSteps component within IoHomePreFooter

--- src/components/_proxied-dot-io/common/io-home-pre-footer/index.tsx
+++ src/components/_proxied-dot-io/common/io-home-pre-footer/index.tsx
@@ -1,14 +1,20 @@
-import * as React from 'react'
-import classNames from 'classnames'
+import NextSteps from '@hashicorp/react-next-steps'
+import type { NextStepsProps } from '@hashicorp/react-next-steps/types'
 import { Products } from '@hashicorp/platform-product-meta'
-import { IconArrowRight16 } from '@hashicorp/flight-icons/svg-react/arrow-right-16'
 import s from './style.module.css'
+
+type CTA = {
+	heading: string
+	description: string
+	cta: string
+	link: string
+}
 
 interface IoHomePreFooterProps {
 	brand: Products
 	heading: string
 	description: string
-	ctas: [IoHomePreFooterCard, IoHomePreFooterCard, IoHomePreFooterCard]
+	ctas: [CTA] | [CTA, CTA] | [CTA, CTA, CTA]
 }
 
 export default function IoHomePreFooter({
@@ -17,63 +23,26 @@ export default function IoHomePreFooter({
 	description,
 	ctas,
 }: IoHomePreFooterProps) {
+	console.log(ctas)
 	return (
-		<div className={classNames(s.preFooter, s[brand])}>
-			<div className={s.container}>
-				<div className={s.content}>
-					<h2 className={s.heading}>{heading}</h2>
-					<p className={s.description}>{description}</p>
-				</div>
-				<div className={s.cards}>
-					{ctas.map((cta, index) => {
-						return (
-							<IoHomePreFooterCard
-								key={index}
-								brand={brand}
-								link={cta.link}
-								heading={cta.heading}
-								description={cta.description}
-								cta={cta.cta}
-							/>
-						)
-					})}
-				</div>
-			</div>
+		<div className={s.nextSteps}>
+			<NextSteps
+				theme={brand}
+				heading={heading}
+				description={description}
+				steps={
+					ctas.map((cta: CTA) => {
+						return {
+							heading: cta.heading,
+							description: cta.description,
+							cta: {
+								title: cta.cta,
+								url: cta.link,
+							},
+						}
+					}) as NextStepsProps['steps']
+				}
+			/>
 		</div>
-	)
-}
-
-interface IoHomePreFooterCard {
-	brand?: string
-	link: string
-	heading: string
-	description: string
-	cta: string
-}
-
-function IoHomePreFooterCard({
-	brand,
-	link,
-	heading,
-	description,
-	cta,
-}: IoHomePreFooterCard): React.ReactElement {
-	return (
-		<a
-			href={link}
-			className={s.card}
-			style={
-				{
-					'--primary': `var(--${brand})`,
-					'--secondary': `var(--${brand}-secondary)`,
-				} as React.CSSProperties
-			}
-		>
-			<h3 className={s.cardHeading}>{heading}</h3>
-			<p className={s.cardDescription}>{description}</p>
-			<span className={s.cardCta}>
-				{cta} <IconArrowRight16 />
-			</span>
-		</a>
 	)
 }
