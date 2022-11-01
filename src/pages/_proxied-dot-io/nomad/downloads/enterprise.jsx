79f5f770195b565f7497a19428ba11dd1bb87828
Kendall Strautman <36613477+kendallstrautman@users.noreply.github.com>
2022-11-01 09:35:31 -0700

    Update src/pages/_proxied-dot-io/nomad/downloads/enterprise.jsx

--- src/pages/_proxied-dot-io/nomad/downloads/enterprise.jsx
+++ src/pages/_proxied-dot-io/nomad/downloads/enterprise.jsx
@@ -38,7 +38,7 @@ function DownloadsPage({ product, releases, latestVersion }) {
 			}
 			tutorialLink={{
 				href: 'https://developer.hashicorp.com/nomad/tutorials',
-				label: 'View Tutorials at HashiCorp Learn',
+				label: 'View Tutorials',
 			}}
 			merchandisingSlot={
 				<p className={s.legalNotice}>
