2598d9ba21d27e7eb2a8b3a9fe8aa6b06104432c
Pamela Bortnick (she/her) <pbortnick@gmail.com>
2022-11-04 12:19:40 -0400

    feat: run `saveAndLoadAnalytics()` after signin (#1317)
    * Add saveAndLoadAnalytics to api callback
    
    * Add loadAll in saveAndLoad function
    
    * Use canary version of consent manager
    
    * Delete console log
    
    * Update consent manager package
    
    * Add mock graphql to jest config
    
    * Add gql mock file
    
    * Add svg moduleNameMapper in jest config
    
    * Add svg jest-raw-loader to transform in jest config
    
    * Install jest-raw-loader
    
    * Move jest-raw-loader to dev dependencies

--- src/hooks/use-authentication/index.ts
+++ src/hooks/use-authentication/index.ts
@@ -1,5 +1,7 @@
 import { useEffect } from 'react'
 import { useSession } from 'next-auth/react'
+import { saveAndLoadAnalytics } from '@hashicorp/react-consent-manager'
+import { preferencesSavedAndLoaded } from '@hashicorp/react-consent-manager/util/cookies'
 import {
 	AuthErrors,
 	SessionData,
@@ -42,6 +44,16 @@ const useAuthentication = (
 	const isAuthenticated = status === 'authenticated'
 	const showAuthenticatedUI = isAuthenticated
 	const showUnauthenticatedUI = !isLoading && !isAuthenticated
+	const preferencesLoaded = preferencesSavedAndLoaded()
+
+	// We accept consent manager on the user's behalf. As per Legal & Compliance,
+	// signing-in means a user is accepting our privacy policy and so we can
+	// enable tracking. Should only be ran if not already set & loaded.
+	useEffect(() => {
+		if (isAuthenticated && !preferencesLoaded) {
+			saveAndLoadAnalytics({ loadAll: true })
+		}
+	}, [isAuthenticated, preferencesLoaded])
 
 	// Separating user and session data
 	let session: SessionData, user: UserData
