8199f05f895b2894d453a30588d7fefa098e702b
BrandonRomano <brandon@hashicorp.com>
2022-11-16 15:34:25 -0800

    Lift header into a component

--- src/views/product-integrations-landing/components/tier-badge/index.tsx
+++ src/views/product-integrations-landing/components/tier-badge/index.tsx
@@ -3,18 +3,31 @@ import classNames from 'classnames'
 import { IconAward16 } from '@hashicorp/flight-icons/svg-react/award-16'
 import { IconHandshake16 } from '@hashicorp/flight-icons/svg-react/handshake-16'
 import { Tier } from 'lib/integrations-api-client'
+import { ProductSlug } from 'types/products'
 
 interface TierBadgeStyles extends React.CSSProperties {
 	'--badge-color': string
 }
 
-export default function TierBadge({ tier, productSlug, size }) {
+interface TierBadgeProps {
+	tier: Tier
+	productSlug: ProductSlug
+	size: 'small' | 'large'
+	className?: string
+}
+
+export default function TierBadge({
+	tier,
+	productSlug,
+	size,
+	className,
+}: TierBadgeProps) {
 	const styles: TierBadgeStyles = {
 		'--badge-color': `var(--token-color-${productSlug}-surface)`,
 	}
 	return (
 		<span
-			className={classNames(s.tierBadge, {
+			className={classNames(s.tierBadge, className, {
 				[s.large]: size === 'large',
 			})}
 			style={styles}
