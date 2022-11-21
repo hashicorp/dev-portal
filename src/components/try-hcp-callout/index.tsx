df6b003727aeb992bae2b8a2cd80f58ec6aa550a
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-11-21 10:23:15 -0500

    chore: remove hcp-callout-compact-prebuilt

--- src/components/try-hcp-callout/index.tsx
+++ src/components/try-hcp-callout/index.tsx
@@ -1,4 +1,3 @@
 export * from './components/try-hcp-callout'
 export * from './components/try-hcp-callout-compact'
-export * from './components/try-hcp-callout-compact-prebuilt'
 export * from './components/try-hcp-callout-prebuilt'
