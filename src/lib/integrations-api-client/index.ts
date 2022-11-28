91b32a65025ccfab47b963904526fa220f7a646d
BrandonRomano <brandon@hashicorp.com>
2022-11-28 14:16:41 -0800

    Fetch _all_ integrations

--- src/lib/integrations-api-client/index.ts
+++ src/lib/integrations-api-client/index.ts
@@ -77,6 +77,47 @@ export async function fetchProductIntegrations(
 	)
 }
 
+export async function fetchAllProductIntegrations(
+	product: ProductSlug,
+	fetchedIntegrations?: Integration[],
+	after?: string
+): Promise<Array<Integration>> {
+	// Set the query batch size
+	const BATCH_SIZE = 100
+
+	// Set the base array if it's the first call
+	if (typeof fetchedIntegrations === 'undefined') {
+		fetchedIntegrations = []
+	}
+
+	// Fetch them from the API, recursively
+	const fetchResult = await fetchProductIntegrations(product, BATCH_SIZE, after)
+	if (
+		fetchResult.meta.status_code < 200 ||
+		fetchResult.meta.status_code >= 300
+	) {
+		// Just throwing an error here, if something goes wrong
+		// there's not anything we can do to handle.
+		throw new Error(
+			`Failed to fetch Integrations. ${JSON.stringify(fetchResult)}`
+		)
+	}
+
+	if (fetchResult.result.length < BATCH_SIZE) {
+		// If there's less than BATCH_SIZE integrations fetched (less than
+		// the limit), that means that we've fetched everything.
+		return fetchedIntegrations.concat(fetchResult.result)
+	} else {
+		return fetchAllProductIntegrations(
+			product,
+			// Concat the result with the previous
+			fetchedIntegrations.concat(fetchResult.result),
+			// The last ID
+			fetchResult.result[fetchResult.result.length - 1].id
+		)
+	}
+}
+
 export async function fetchIntegration(
 	product: ProductSlug,
 	identifier: string
--- src/pages/[productSlug]/integrations/index.tsx
+++ src/pages/[productSlug]/integrations/index.tsx
@@ -1,11 +1,10 @@
-import { ProductSlug } from 'types/products'
 import { cachedGetProductData } from 'lib/get-product-data'
 import {
-	fetchProductIntegrations,
+	fetchAllProductIntegrations,
 	Integration,
 } from 'lib/integrations-api-client'
+import { ProductSlug } from 'types/products'
 import ProductIntegrationsLanding from 'views/product-integrations-landing'
-import { ApiResponse } from 'lib/integrations-api-client/standard-client'
 
 // The products that we are enabling for this Integrations POC
 export const enabledProducts: Array<ProductSlug> = [
@@ -22,11 +21,12 @@ export async function getServerSideProps({ params }) {
 		}
 	}
 
-	const integrationResult: ApiResponse<Integration[]> =
-		await fetchProductIntegrations(params.productSlug)
+	const integrations: Integration[] = await fetchAllProductIntegrations(
+		params.productSlug
+	)
 	return {
 		props: {
-			integrations: integrationResult.result,
+			integrations: integrations,
 			product: {
 				...cachedGetProductData(params.productSlug),
 			},
