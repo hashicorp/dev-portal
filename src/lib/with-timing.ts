e58e36efaab029fdb940e532ad88495eddbbe4b4
Kevin Wang <kwangsan@gmail.com>
2022-11-28 17:47:22 -0500

    feat: with-timing

---
+++ src/lib/with-timing.ts
@@ -0,0 +1,24 @@
+/**
+ * A utility method to wrap a function, and log execution time.
+ */
+export async function withTiming<T>(
+	label: string,
+	fn: () => Promise<T>
+): Promise<T> {
+	if (process.env.HC_DEBUG_TIMINGS === '1') {
+		console.group(label)
+		console.time(label)
+
+		let result: T
+		try {
+			result = await fn()
+		} finally {
+			console.groupEnd()
+			console.timeEnd(label)
+		}
+		return result
+	} else {
+		// passthrough if env var is not specified
+		return await fn()
+	}
+}
