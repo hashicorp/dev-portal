36f14fb921178105a9694c19bc917bdc8f68dbae
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-12-01 15:33:58 -0500

    feat: add content area component for shared grid

--- src/views/certifications/[slug]/index.tsx
+++ src/views/certifications/[slug]/index.tsx
@@ -1,24 +1,29 @@
+import BaseNewLayout from 'layouts/base-new'
 import { CertificationsNav } from '../components'
+import { CertificationsContentArea } from '../components/certifications-content-area'
 import { CertificationPageProps } from './types'
 
 function CertificationPage({ navProps, pageContent }: CertificationPageProps) {
 	return (
 		<>
-			<h1>{pageContent.title}</h1>
-			<CertificationsNav {...navProps} />
-			<h2>{`[stub]`} Page Content</h2>
-			<pre
-				className="hds-typography-code-100"
-				style={{
-					whiteSpace: 'pre-wrap',
-					background: '#EEEEEE',
-					padding: '2rem',
-				}}
-			>
-				<code>{JSON.stringify({ pageContent }, null, 2)}</code>
-			</pre>
+			<CertificationsContentArea>
+				<h1>{pageContent.title}</h1>
+				<CertificationsNav {...navProps} />
+				<h2>{`[stub]`} Page Content</h2>
+				<pre
+					className="hds-typography-code-100"
+					style={{
+						whiteSpace: 'pre-wrap',
+						background: '#EEEEEE',
+						padding: '2rem',
+					}}
+				>
+					<code>{JSON.stringify({ pageContent }, null, 2)}</code>
+				</pre>
+			</CertificationsContentArea>
 		</>
 	)
 }
 
+CertificationPage.layout = BaseNewLayout
 export default CertificationPage
