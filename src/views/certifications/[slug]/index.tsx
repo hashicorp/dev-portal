57200f23b72474eedfec056875ad0403bcb4e072
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-12-06 17:30:56 -0500

    chore: stub in more components

--- src/views/certifications/[slug]/index.tsx
+++ src/views/certifications/[slug]/index.tsx
@@ -1,20 +1,28 @@
+// Global
 import BaseNewLayout from 'layouts/base-new'
+// Shared view components
 import {
 	CertificationsContentArea,
 	CertificationsNav,
 	CertificationsHero,
 } from '../components'
+// Local view components
+import { CertificationProgramDetails } from './components'
 import { CertificationPageProps } from './types'
 
 function CertificationPage({ navProps, pageContent }: CertificationPageProps) {
 	return (
 		<>
 			<CertificationsNav {...navProps} />
+			<CertificationsHero
+				heading={pageContent.title}
+				description={
+					'Brief intro. Short but enough to help practitioners take that next step. Suggestion is a max of 5 lines of content. Verify your basic infrastructure automation skills. If you have passed the Terraform Associate wait until the new version comes out to re-certify.'
+				}
+			/>
+			<CertificationProgramDetails title="Terraform Associate 002" />
+			<CertificationProgramDetails title="Terraform Associate 003" />
 			<CertificationsContentArea>
-				<CertificationsHero
-					heading={pageContent.title}
-					description={pageContent.heroIntro}
-				/>
 				<h2>{`[stub]`} Page Content</h2>
 				<pre
 					className="hds-typography-code-100"
