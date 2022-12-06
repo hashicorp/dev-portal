f60693b55fe69d26b7c721568619d4ce83ab6bab
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-12-06 11:22:59 -0500

    feat: stub in revised certifications landing

--- src/views/certifications/landing/server.ts
+++ src/views/certifications/landing/server.ts
@@ -1,26 +1,28 @@
-import rivetQuery from '@hashicorp/platform-cms'
+import fs from 'fs'
+import path from 'path'
 import { GetStaticPropsResult } from 'next'
-import { formatCertificationsNavProps } from '../components/certifications-nav/helpers'
-import pageQuery from './page-query.graphql'
 import { CertificationLandingProps } from './types'
 
 /**
- * Note: starting by fetching Dato content for now, because it's what we have
- * so far, and lists all sub-pages too. Could pull down and write into JSON
- * or YAML files or whatnot if we'd prefer to have it local to dev dot.
+ * Read in local content from the filesystem.
  */
 export async function getStaticProps(): Promise<
 	GetStaticPropsResult<CertificationLandingProps>
 > {
-	const pageContent = await rivetQuery({
-		query: pageQuery,
-	})
-
-	const navProps = formatCertificationsNavProps(
-		pageContent.allProductCertificationPages
+	const pageContent = JSON.parse(
+		readLocalFile('src/content/certifications/landing.json')
 	)
 
 	return {
-		props: { pageContent, navProps },
+		props: { pageContent },
 	}
+}
+
+/**
+ * Read a local file from getStaticProps context,
+ * using `process.cwd()` to resolve the provided filePath.
+ */
+function readLocalFile(filePath: string) {
+	const fullPath = path.join(process.cwd(), filePath)
+	return fs.readFileSync(fullPath, 'utf8')
 }
