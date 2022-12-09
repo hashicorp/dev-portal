56aa3f2b70f53497bf89dd325c488131818ed151
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-12-09 11:49:24 -0500

    Adding failing test case

---
+++ src/views/docs-view/__tests__/multiple-remark-plugins.test.ts
@@ -0,0 +1,27 @@
+import remark from 'remark'
+import { cachedGetProductData } from 'lib/get-product-data'
+import { rewriteTutorialLinksPlugin } from 'lib/remark-plugins/rewrite-tutorial-links'
+import remarkPluginAdjustLinkUrls from 'lib/remark-plugin-adjust-link-urls'
+import { getProductUrlAdjuster } from '../utils/product-url-adjusters'
+
+describe('multiple remark plugins', () => {
+	let waypointProduct
+	let productUrlAdjuster
+
+	beforeAll(() => {
+		waypointProduct = cachedGetProductData('waypoint')
+		productUrlAdjuster = getProductUrlAdjuster(waypointProduct)
+	})
+
+	test('rewriteTutorialLinksPlugin, then remarkPluginAdjustLinkUrls', async () => {
+		const linkToProcess = '[Waypoint](/waypoint)'
+
+		const result = await remark()
+			.use(rewriteTutorialLinksPlugin, { tutorialMap: {} })
+			.use(remarkPluginAdjustLinkUrls, { urlAdjustFn: productUrlAdjuster })
+			.process(linkToProcess)
+
+		// The link should be unchanged
+		expect(String(result)).toBe(linkToProcess)
+	})
+})
