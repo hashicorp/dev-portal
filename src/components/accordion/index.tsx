16f28438ebe8656ea3bf6efad8dee9fd4a93ef00
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-12-08 19:49:27 -0500

    feat: implement Accordion

--- src/components/accordion/index.tsx
+++ src/components/accordion/index.tsx
@@ -1,7 +1,29 @@
-import s from './accordion.module.css'
+import AccordionDisclosure from 'components/accordion-disclosure'
+import type { AccordionProps, AccordionContentItem } from './types'
 
-function Accordion() {
-	return <div className={s.root}>Hello Accordion</div>
+/**
+ * Renders a group of AccordionDisclosure items.
+ */
+function Accordion({ className, items }: AccordionProps) {
+	return (
+		<div className={className}>
+			{items.map((item: AccordionContentItem, idx: number) => {
+				const { title, content } = item
+				const isFirstItem = idx === 0
+				const isLastItem = idx === items.length - 1
+				return (
+					<AccordionDisclosure
+						key={title}
+						title={title}
+						isFirstItem={isFirstItem}
+						isLastItem={isLastItem}
+					>
+						{content}
+					</AccordionDisclosure>
+				)
+			})}
+		</div>
+	)
 }
 
 export default Accordion
