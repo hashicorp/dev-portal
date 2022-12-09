fbc50f46af4ce616f63eff67eb7190b0127af233
Brandon Romano <brandon@hashicorp.com>
2022-12-08 19:06:45 -0800

    Small UI improvements for integrations demo (#1426)
    * Remove categorized list, fix at 2 columns
    
    * Sort alphabetically & improve types

--- src/views/product-integrations-landing/components/integrations-list/index.tsx
+++ src/views/product-integrations-landing/components/integrations-list/index.tsx
@@ -1,33 +1,62 @@
-import s from './style.module.css'
-import CardsGridList from 'components/cards-grid-list'
 import CardLink from 'components/card-link'
+import CardsGridList from 'components/cards-grid-list'
 import TierBadge from '../tier-badge'
+import s from './style.module.css'
+import { ProductSlug } from 'types/products'
 
-export default function IntegrationsList({ integrations }) {
+import { Integration, Tier } from 'lib/integrations-api-client/integration'
+
+interface IntegrationsListProps {
+	integrations: Array<Integration>
+}
+
+export default function IntegrationsList({
+	integrations,
+}: IntegrationsListProps) {
 	return (
-		<CardsGridList>
-			{integrations.map((integration) => {
-				const url = integration.external_only
-					? integration.external_url.replace(
-							/^https:\/\/developer.hashicorp.com/,
-							''
-					  )
-					: `/${integration.product.slug}/integrations/${integration.slug}`
-				return (
-					<IntegrationCard
-						key={integration.id}
-						title={integration.name}
-						description={integration.description}
-						latestVersion={integration.versions[0]}
-						organization={integration.organization.slug}
-						productSlug={integration.product.slug}
-						tier={integration.tier}
-						url={url}
-					/>
-				)
-			})}
+		<CardsGridList fixedColumns={2}>
+			{integrations
+				.sort((a: Integration, b: Integration): number => {
+					if (a.name < b.name) {
+						return -1
+					} else if (a.name > b.name) {
+						return 1
+					} else {
+						return 0
+					}
+				})
+				.map((integration: Integration) => {
+					const url = integration.external_only
+						? integration.external_url.replace(
+								/^https:\/\/developer.hashicorp.com/,
+								''
+						  )
+						: `/${integration.product.slug}/integrations/${integration.slug}`
+					return (
+						<IntegrationCard
+							key={integration.id}
+							title={integration.name}
+							description={integration.description}
+							latestVersion={integration.versions[0]}
+							organization={integration.organization.slug}
+							productSlug={integration.product.slug}
+							tier={integration.tier}
+							url={url}
+						/>
+					)
+				})}
 		</CardsGridList>
 	)
+}
+
+interface IntegrationCardProps {
+	title: string
+	latestVersion: string
+	description: string
+	organization: string
+	tier: Tier
+	productSlug: ProductSlug
+	url: string
 }
 
 function IntegrationCard({
@@ -38,7 +67,7 @@ function IntegrationCard({
 	tier,
 	productSlug,
 	url,
-}) {
+}: IntegrationCardProps) {
 	return (
 		<CardLink ariaLabel="TODO" className={s.integrationCard} href={url}>
 			<div className={s.header}>
@@ -48,7 +77,6 @@ function IntegrationCard({
 				</div>
 				<TierBadge tier={tier} productSlug={productSlug} size="small" />
 			</div>
-
 			<span className={s.organization}>{`@${organization}`}</span>
 			<p className={s.body}>{description}</p>
 		</CardLink>
