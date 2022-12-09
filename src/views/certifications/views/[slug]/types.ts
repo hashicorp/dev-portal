2fe92cab644bd7eb2b6490839bdc32a767010d59
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-12-09 00:21:39 -0500

    feat: implement individual page hero bgs

--- src/views/certifications/views/[slug]/types.ts
+++ src/views/certifications/views/[slug]/types.ts
@@ -2,6 +2,7 @@ import { CertificationProgram } from 'views/certifications/types'
 import { CertificationsNavProps } from '../../components/certifications-nav/types'
 
 export interface CertificationPageProps {
+	slug: string
 	navProps: CertificationsNavProps
 	pageContent: CertificationProgram
 }
