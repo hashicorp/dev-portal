7161e0507f4a4a1516ef91f957fb08eba5294049
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-12-09 15:56:23 -0500

    docs: add comment on heading-wrapper

--- src/components/disclosure/components/disclosure-activator/components/heading-wrapper/index.tsx
+++ src/components/disclosure/components/disclosure-activator/components/heading-wrapper/index.tsx
@@ -6,6 +6,10 @@ import s from './heading-wrapper.module.css'
  * Given a heading level,
  * Return a heading component at that level that will wrap children
  * without adding styles.
+ *
+ * Note: we're opting not to use `components/heading` here, as for this
+ * case, we want to wrap elements in <h{n} /> without adding any styles
+ * at all, leaving the styling up to the provided `children`.
  */
 export function getHeadingWrapper(level: HeadingLevel) {
 	const Heading = level as React.ElementType
