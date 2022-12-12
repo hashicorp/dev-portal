65dd22a8125eba5075b97e7843f209b0392d7286
Kevin Wang <kwangsan@gmail.com>
2022-12-12 15:13:30 -0500

    categorized-variable-group-list: partition by root

--- src/views/product-integration/components/categorized-variable-group-list/index.tsx
+++ src/views/product-integration/components/categorized-variable-group-list/index.tsx
@@ -1,4 +1,5 @@
 import {
+	Variable,
 	VariableGroupList,
 	VariableGroupListProps,
 } from '../variable-group-list'
@@ -21,6 +22,38 @@ export default function CategorizedVariableGroupList({
 		}
 	})
 
+	// Partition variables into "required" and "optional",
+	// by root-level nodes only. Any sub-nodes `required`
+	// property will be ignored, and will follow the
+	// parent node's `required` property.
+	const partitions = variables.reduce(
+		(acc, v) => {
+			const isRoot = v.key.split('.').length === 1
+			const isRequired = v.required
+
+			// root
+			if (isRoot) {
+				// update internal map for sub-nodes' to lookup
+				acc._roots[v.key] = isRequired
+				// push to correct partition
+				isRequired ? acc.required.push(v) : acc.optional.push(v)
+			} else {
+				// sub-node
+				// lookup parent node's `required` property
+				const rootKey = v.key.split('.')[0]
+				// push to correct partition
+				acc._roots[rootKey] ? acc.required.push(v) : acc.optional.push(v)
+			}
+
+			return acc
+		},
+		{
+			_roots: {} as Record<string, boolean>,
+			required: [] as Variable[],
+			optional: [] as Variable[],
+		}
+	)
+
 	return (
 		<>
 			{!onlyHasNull ? (
@@ -28,17 +61,13 @@ export default function CategorizedVariableGroupList({
 					{hasRequired && (
 						<>
 							<h3>Required</h3>
-							<VariableGroupList
-								variables={variables.filter((v) => v.required)}
-							/>
+							<VariableGroupList variables={partitions.required} />
 						</>
 					)}
 					{hasOptional && (
 						<>
 							<h3>Optional</h3>
-							<VariableGroupList
-								variables={variables.filter((v) => !v.required)}
-							/>
+							<VariableGroupList variables={partitions.optional} />
 						</>
 					)}
 				</>
