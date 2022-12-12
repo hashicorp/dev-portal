940f2dd92d447683d1a5312d698d9d423dede4d6
BrandonRomano <brandon@hashicorp.com>
2022-12-12 10:49:22 -0800

    Remove sorting from integrations-list

--- src/views/product-integrations-landing/components/integrations-list/index.tsx
+++ src/views/product-integrations-landing/components/integrations-list/index.tsx
@@ -1,10 +1,9 @@
 import CardLink from 'components/card-link'
 import CardsGridList from 'components/cards-grid-list'
+import { Integration, Tier } from 'lib/integrations-api-client/integration'
+import { ProductSlug } from 'types/products'
 import TierBadge from '../tier-badge'
 import s from './style.module.css'
-import { ProductSlug } from 'types/products'
-
-import { Integration, Tier } from 'lib/integrations-api-client/integration'
 
 interface IntegrationsListProps {
 	integrations: Array<Integration>
@@ -15,36 +14,26 @@ export default function IntegrationsList({
 }: IntegrationsListProps) {
 	return (
 		<CardsGridList fixedColumns={2}>
-			{integrations
-				.sort((a: Integration, b: Integration): number => {
-					if (a.name < b.name) {
-						return -1
-					} else if (a.name > b.name) {
-						return 1
-					} else {
-						return 0
-					}
-				})
-				.map((integration: Integration) => {
-					const url = integration.external_only
-						? integration.external_url.replace(
-								/^https:\/\/developer.hashicorp.com/,
-								''
-						  )
-						: `/${integration.product.slug}/integrations/${integration.slug}`
-					return (
-						<IntegrationCard
-							key={integration.id}
-							title={integration.name}
-							description={integration.description}
-							latestVersion={integration.versions[0]}
-							organization={integration.organization.slug}
-							productSlug={integration.product.slug}
-							tier={integration.tier}
-							url={url}
-						/>
-					)
-				})}
+			{integrations.map((integration: Integration) => {
+				const url = integration.external_only
+					? integration.external_url.replace(
+							/^https:\/\/developer.hashicorp.com/,
+							''
+					  )
+					: `/${integration.product.slug}/integrations/${integration.slug}`
+				return (
+					<IntegrationCard
+						key={integration.id}
+						title={integration.name}
+						description={integration.description}
+						latestVersion={integration.versions[0]}
+						organization={integration.organization.slug}
+						productSlug={integration.product.slug}
+						tier={integration.tier}
+						url={url}
+					/>
+				)
+			})}
 		</CardsGridList>
 	)
 }
