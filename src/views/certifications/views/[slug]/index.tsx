f5de7794eba05fae448b249b27c05b2ff9932d74
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-12-13 15:02:05 -0500

    feat: use ProgramHero in program view

--- src/views/certifications/views/[slug]/index.tsx
+++ src/views/certifications/views/[slug]/index.tsx
@@ -1,20 +1,34 @@
+// Global
+import BaseNewLayout from 'layouts/base-new'
+// Local
+import { ProgramHero } from './components'
 import { CertificationProgramViewProps } from './types'
 
 function CertificationProgramView({
 	pageContent,
 	slug,
 }: CertificationProgramViewProps) {
+	const { hero } = pageContent
+
 	return (
-		<pre style={{ border: '1px solid magenta' }}>
-			<code>
-				{JSON.stringify(
-					{ note: 'Program page placeholder', slug, pageContent },
-					null,
-					2
-				)}
-			</code>
-		</pre>
+		<>
+			<ProgramHero
+				heading={hero.heading}
+				description={hero.description}
+				slug={slug}
+			/>
+			<pre style={{ border: '1px solid magenta' }}>
+				<code>
+					{JSON.stringify(
+						{ note: 'Program page placeholder', slug, pageContent },
+						null,
+						2
+					)}
+				</code>
+			</pre>
+		</>
 	)
 }
 
+CertificationProgramView.layout = BaseNewLayout
 export default CertificationProgramView
