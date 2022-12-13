c906744f2efaff6882cb6e234b0a26b67f79898c
Brandon Romano <brandon@hashicorp.com>
2022-12-12 16:06:18 -0800

    Integrations Pagination (#1438)
    * Adds pagination to the integrations landing
    
    * collapsable pagination
    
    * Remove sorting from integrations-list
    
    * Adds top-level comment
    
    * Adds alphabetical comment

--- src/views/product-integrations-landing/components/searchable-integrations-list/index.tsx
+++ src/views/product-integrations-landing/components/searchable-integrations-list/index.tsx
@@ -1,25 +1,33 @@
-import { useState } from 'react'
-import classNames from 'classnames'
-import s from './style.module.css'
 import { IconSearch16 } from '@hashicorp/flight-icons/svg-react/search-16'
-import IntegrationsList from '../integrations-list'
+import classNames from 'classnames'
+import { Integration } from 'lib/integrations-api-client/integration'
+import { useState } from 'react'
+import PaginatedIntegrationsList from '../paginated-integrations-list'
+import s from './style.module.css'
+
+interface SearchableIntegrationsListProps {
+	integrations: Array<Integration>
+	className: string
+}
 
 export default function SearchableIntegrationsList({
 	integrations,
 	className,
-}) {
+}: SearchableIntegrationsListProps) {
 	const [searchQuery, setSearchQuery] = useState('')
-	const filteredIntegrations = integrations.filter((integration) => {
-		return (
-			integration.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
-			integration.description
-				.toLowerCase()
-				.includes(searchQuery.toLowerCase()) ||
-			integration.organization.slug
-				.toLowerCase()
-				.includes(searchQuery.toLowerCase())
-		)
-	})
+	const filteredIntegrations = integrations.filter(
+		(integration: Integration) => {
+			return (
+				integration.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+				integration.description
+					.toLowerCase()
+					.includes(searchQuery.toLowerCase()) ||
+				integration.organization.slug
+					.toLowerCase()
+					.includes(searchQuery.toLowerCase())
+			)
+		}
+	)
 
 	return (
 		<div className={classNames(s.searchableIntegrationsList, className)}>
@@ -31,7 +39,7 @@ export default function SearchableIntegrationsList({
 				{filteredIntegrations.length}{' '}
 				{filteredIntegrations.length === 1 ? 'Result' : 'Results'}
 			</p>
-			<IntegrationsList integrations={filteredIntegrations} />
+			<PaginatedIntegrationsList integrations={filteredIntegrations} />
 		</div>
 	)
 }
