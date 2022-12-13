721bd82d976e898673368dea0ae859c7c7f5051e
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-12-13 13:02:28 -0500

    chore: stub in new page files, as assembly base

---
+++ src/views/certifications/views/landing/index.tsx
@@ -0,0 +1,9 @@
+function CertificationsLandingView() {
+	return (
+		<div style={{ border: '1px solid magenta' }}>
+			Certification Landing placeholder
+		</div>
+	)
+}
+
+export default CertificationsLandingView
