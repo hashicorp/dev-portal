9b2d7915cb3fb2654eb38e206c62222114e2df49
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-12-13 16:40:11 -0500

    feat: add FAQ MDX content utils

--- src/views/certifications/types.ts
+++ src/views/certifications/types.ts
@@ -1,4 +1,14 @@
 import type { RawCertificationProgram } from './content/schemas/certification-program'
+import { MDXRemoteSerializeResult } from 'next-mdx-remote'
+
+/**
+ * An FAQ item consists of a title representing the questions,
+ * and some serialized MDX representing the answer content.
+ */
+export interface FaqItem {
+	title: string
+	mdxSource: MDXRemoteSerializeResult
+}
 
 /**
  * Program slugs are mainly used for stylistic tweaks. This type, and related
