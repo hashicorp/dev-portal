ae8570f8630fb61a770de9dace282c0ef3d4a566
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-12-12 22:57:59 -0500

    Refactoring preAdjustUrl to no longer need regex

--- src/lib/remark-plugin-adjust-link-urls.ts
+++ src/lib/remark-plugin-adjust-link-urls.ts
@@ -14,36 +14,49 @@ const getIsInternalPath = (url: string) => {
 /**
  * Pre-adjusts urls that start with a path part of the given `currentPath`. See
  * examples in: `src/lib/__tests__/remark-plugin-adjust-link-urls.test.ts`.
- *
- * NOTE: urls that start with: `.`, `/`, `?`, or `#` are not pre-adjusted. These
- * do not need to be pre-adjusted because they are assumed to be handled by the
- * `urlAdjustFn` given to `remarkPluginAdjustLinkUrls`.
  */
 export const preAdjustUrl = ({ currentPath, url }): string => {
-	const isInternalPath = getIsInternalPath(url)
-	const needsPreAdjustment = isInternalPath && !/^[./?#]/.test(url)
-	if (!needsPreAdjustment) {
+	// Do nothing if currentPath or url are falsy (empty, null, undefined, etc.)
+	if (!currentPath || !url) {
 		return url
 	}
 
+	// Do nothing if url is not an internal path
+	const isInternalPath = getIsInternalPath(url)
+	if (!isInternalPath) {
+		return url
+	}
+
+	// Do nothing if url is a top-level path
+	if (url.startsWith('/')) {
+		return url
+	}
+
+	// Since they're worth checking, split up url and currentPath
+	const urlParts = url.split('/')
 	const currentPathParts = currentPath.split('/')
-	const urlToChangeParts = url.split('/')
 
-	const newPathPrefixParts = []
-	currentPathParts.find((currentPathPart) => {
-		if (!currentPathPart) {
-			return false
-		}
+	// Search for first part of url within currentPath
+	const indexInCurrentPath = currentPathParts.indexOf(urlParts[0])
 
-		const matchesUrlToChangeFirstPart = currentPathPart === urlToChangeParts[0]
-		if (!matchesUrlToChangeFirstPart) {
-			newPathPrefixParts.push(currentPathPart)
-		}
+	// Do nothing if currentPath does not have the first part of url
+	if (indexInCurrentPath === -1) {
+		return url
+	}
 
-		return matchesUrlToChangeFirstPart
-	})
+	// Prefix the url with a slash if it starts with the first part of currentPath
+	if (indexInCurrentPath === 0) {
+		return `/${url}`
+	}
 
-	return `/${newPathPrefixParts.join('/')}/${url}`
+	// Retain parts of currentPath before the one that matches first part of url
+	const currentPathPartsToRetain = currentPathParts.slice(0, indexInCurrentPath)
+
+	// Join the retained parts of currentPath and all parts of url
+	const allJoinedParts = [...currentPathPartsToRetain, ...urlParts].join('/')
+
+	// Return all of the joined parts
+	return allJoinedParts
 }
 
 const remarkPluginAdjustLinkUrls: Plugin = ({
@@ -53,7 +66,6 @@ const remarkPluginAdjustLinkUrls: Plugin = ({
 	currentPath: string
 	urlAdjustFn: (url: string) => string
 }): Transformer => {
-	console.log(currentPath)
 	return function transformer(tree) {
 		visit(tree, ['link', 'definition'], (node: Link | Definition) => {
 			const urlToAdjust = preAdjustUrl({ currentPath, url: node.url })
