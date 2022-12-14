c425cec93a306a07d6d23b3b2ab2331ac477c25a
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-12-14 11:35:55 -0500

    docs: better docs for program hero foreground

--- src/views/certifications/views/[slug]/components/program-hero/index.tsx
+++ src/views/certifications/views/[slug]/components/program-hero/index.tsx
@@ -5,6 +5,20 @@ import { ProgramSlug } from 'views/certifications/types'
 // Styles
 import s from './program-hero.module.css'
 
+/**
+ * Note: foreground and background are both set based on the program `slug`.
+ * When new programs are added, this enum and the `theme-` classes in the
+ * adjacent `program-hero.module.css` should likely be expanded.
+ *
+ * If a slug is not explicitly supported, the CertificationsHero will default
+ * to dark foreground text, and a plain white background.
+ */
+enum ProgramHeroForeground {
+	'security-automation' = 'dark',
+	'infrastructure-automation' = 'light',
+	'networking-automation' = 'light',
+}
+
 function ProgramHero({
 	heading,
 	description,
@@ -18,7 +32,7 @@ function ProgramHero({
 		<CertificationsHero
 			heading={heading}
 			description={description}
-			foreground={slug === 'security-automation' ? 'dark' : 'light'}
+			foreground={ProgramHeroForeground[slug]}
 			backgroundSlot={
 				<div className={classNames(s.heroBackground, s[`theme-${slug}`])} />
 			}
