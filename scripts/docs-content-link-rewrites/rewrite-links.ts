d1f8724c04ad2f227e07a85681965a65c3fe5e45
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-12-15 11:55:40 -0500

    Adding more logs for debugging

--- scripts/docs-content-link-rewrites/rewrite-links.ts
+++ scripts/docs-content-link-rewrites/rewrite-links.ts
@@ -43,11 +43,16 @@ const main = async () => {
 	}
 
 	// If there files to check, start pulling .io and learn data needed
-	const dotIoToDevDotPaths = await getDocsToDevDotUrlMap()
+	console.log('Loading dotIoToDevDotPaths...')
+	const dotIoToDevDotPaths = getDocsToDevDotUrlMap()
+	console.log('Loading learnToDevDotPaths...')
 	const learnToDevDotPaths = await getLearnToDevDotUrlMap()
-	const normalizedProductSlug = normalizeRemoteLoaderSlug(REPO)
 
 	// Invoke the helper that checks MDX and JSON files for rewriteable links
+	const normalizedProductSlug = normalizeRemoteLoaderSlug(REPO)
+	console.log(
+		`Processing ${changedMdxFiles.length} .mdx files and ${changedNavDataJsonFiles.length} -nav-data.json files`
+	)
 	const { allLinksToRewrite, allUnrewriteableLinks } =
 		await getAllLinksToRewrite({
 			filePaths: changedMdxFiles.map((filePath: string) =>
@@ -66,12 +71,14 @@ const main = async () => {
 		// Throw an error if configured to, such as in a legacy link format checker
 		if (ERROR_IF_LINKS_TO_REWRITE === 'true') {
 			throw new Error(message)
+		} else {
+			console.log(message)
 		}
 
 		// Otherwise, update the files determined to have rewriteable links
 		filesWithLinksToRewrite.forEach((filePath: string) => {
 			// Attempt to rewrite the file content string
-			const linksToRewrite = filesWithLinksToRewrite[filePath]
+			const linksToRewrite = allLinksToRewrite[filePath]
 			const fileContent = fs.readFileSync(filePath, 'utf-8')
 			const updatedContent = rewriteFileContentString(
 				fileContent,
@@ -90,6 +97,7 @@ const main = async () => {
 			}
 
 			// Otherwise, update the file with the rewritten links
+			console.log(`Updating links in ${filePath}...`)
 			fs.writeFileSync(filePath, updatedContent)
 		})
 	}
@@ -118,6 +126,7 @@ const main = async () => {
 			rewrittenLinksDirectory,
 			`${REPO}.json`
 		)
+		console.log(`Generating ${rewrittenLinksFile}...`)
 		fs.writeFileSync(
 			rewrittenLinksFile,
 			JSON.stringify(allLinksToRewrite, null, 2)
@@ -143,6 +152,7 @@ const main = async () => {
 			unrewriteableLinksDirectory,
 			`${REPO}.json`
 		)
+		console.log(`Generating ${unrewriteableLinksFile}...`)
 		fs.writeFileSync(
 			unrewriteableLinksFile,
 			JSON.stringify(allUnrewriteableLinks.sort(), null, 2)
