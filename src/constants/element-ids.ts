2d7ff3114dd24437ab72d1c5747885d5eb494b18
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-12-14 19:41:48 -0500

    Adding element id constants

---
+++ src/constants/element-ids.ts
@@ -0,0 +1,5 @@
+const MAIN_ELEMENT_ID = 'main'
+const SIDEBAR_LABEL_ID = 'sidebar-label'
+const SIDEBAR_NAV_ELEMENT_ID = 'sidebar-nav'
+
+export { MAIN_ELEMENT_ID, SIDEBAR_LABEL_ID, SIDEBAR_NAV_ELEMENT_ID }
