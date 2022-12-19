4fdb17e826e74f1a6a2ba3a3d2fcaee87fb5704f
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2022-12-19 14:10:13 -0500

    fix: revert text color change in MDX FAQs (#1472)
    * fix: revert text color change in MDX FAQs
    
    * chore: removed unused css file

--- src/components/dev-dot-content/mdx-components/mdx-p/index.tsx
+++ src/components/dev-dot-content/mdx-components/mdx-p/index.tsx
@@ -1,10 +1,8 @@
-import classNames from 'classnames'
 import Text from 'components/text'
 import s from './mdx-p.module.css'
 
 function MdxP(props) {
-	const { className, ...restProps } = props
-	return <Text {...restProps} className={classNames(s.p, className)} />
+	return <Text {...props} className={s.p} />
 }
 
 export { MdxP }
