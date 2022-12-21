177c67ca272ebcd96562679ea9282e3cb772ee7a
Kevin Wang <kwangsan@gmail.com>
2022-12-21 13:45:17 -0500

    chore: move filtering out of sidebar (#1490)
    * chore: move filtering out of sidebar
    
    * chore: gen data serverside

--- src/views/product-integrations-landing/components/searchable-integrations-list/index.tsx
+++ src/views/product-integrations-landing/components/searchable-integrations-list/index.tsx
@@ -1,30 +1,23 @@
 import { IconSearch16 } from '@hashicorp/flight-icons/svg-react/search-16'
 import classNames from 'classnames'
-import { Integration } from 'lib/integrations-api-client/integration'
+import { Integration, Tier } from 'lib/integrations-api-client/integration'
 import { useState } from 'react'
-import { useMobileDrawerContext } from 'views/product-integrations-landing/contexts/mobile-drawer-context'
 import { useIntegrationsSearchContext } from 'views/product-integrations-landing/contexts/integrations-search-context'
 import PaginatedIntegrationsList from '../paginated-integrations-list'
 import s from './style.module.css'
-import Button from 'components/button'
-import { IconSliders16 } from '@hashicorp/flight-icons/svg-react/sliders-16'
+import capitalize from '@hashicorp/platform-util/text/capitalize'
+import { IconX16 } from '@hashicorp/flight-icons/svg-react/x-16'
+import DropdownDisclosure, {
+	DropdownDisclosureButtonItem,
+} from 'components/dropdown-disclosure'
 
 interface SearchableIntegrationsListProps {
 	className: string
 }
 
-interface CustomHeaderStyles extends React.CSSProperties {
-	'--background-image': string
-}
-
-const headerStyleVars: CustomHeaderStyles = {
-	'--background-image': `url('${require('./img/header-background.svg?url')}')`,
-}
-
 export default function SearchableIntegrationsList({
 	className,
 }: SearchableIntegrationsListProps) {
-	const { setDialogOpen } = useMobileDrawerContext()
 	const { filteredIntegrations: integrations } = useIntegrationsSearchContext()
 
 	const [searchQuery, setSearchQuery] = useState('')
@@ -42,21 +35,109 @@ export default function SearchableIntegrationsList({
 		}
 	)
 
+	const {
+		tierOptions,
+		matchingOfficial,
+		officialChecked,
+		setOfficialChecked,
+		matchingVerified,
+		partnerChecked,
+		setPartnerChecked,
+		matchingCommunity,
+		communityChecked,
+		setCommunityChecked,
+		sortedComponents,
+		componentCheckedArray,
+		setComponentCheckedArray,
+		atLeastOneFacetSelected,
+	} = useIntegrationsSearchContext()
+
 	return (
 		<div className={classNames(s.searchableIntegrationsList, className)}>
-			<div className={s.header} style={headerStyleVars}>
+			<div className={s.header}>
 				<h1>Integrations</h1>
 				<SearchBar
 					searchQuery={searchQuery}
 					onChange={(e) => setSearchQuery(e.target.value)}
 				/>
-				<Button
-					text="Filters"
-					icon={<IconSliders16 />}
-					color="secondary"
-					className={classNames('g-show-with-mobile-menu', s.filterButton)}
-					onClick={() => setDialogOpen(true)}
-				/>
+
+				<div className={s.selectStack}>
+					<DropdownDisclosure color="secondary" text="Tier">
+						{tierOptions.map((e) => {
+							const checked =
+								(e === Tier.OFFICIAL && officialChecked) ||
+								(e === Tier.PARTNER && partnerChecked) ||
+								(e === Tier.COMMUNITY && communityChecked)
+							return (
+								<DropdownDisclosureButtonItem
+									key={e}
+									onClick={() => {
+										switch (e) {
+											case Tier.OFFICIAL:
+												return setOfficialChecked((p) => !p)
+											case Tier.PARTNER:
+												return setPartnerChecked((p) => !p)
+											case Tier.COMMUNITY:
+												return setCommunityChecked((p) => !p)
+										}
+									}}
+								>
+									{checked ? '✅ ' : ''}
+									{capitalize(e)}
+								</DropdownDisclosureButtonItem>
+							)
+						})}
+					</DropdownDisclosure>
+					<DropdownDisclosure color="secondary" text="Components">
+						{sortedComponents.map((e, i) => (
+							<DropdownDisclosureButtonItem
+								key={e.id}
+								onClick={() =>
+									setComponentCheckedArray((prev) => {
+										const next = [...prev]
+										next[i] = !next[i]
+										return next
+									})
+								}
+							>
+								{componentCheckedArray[i] ? '✅ ' : ''}
+								{capitalize(e.plural_name)}
+							</DropdownDisclosureButtonItem>
+						))}
+					</DropdownDisclosure>
+
+					<DropdownDisclosure color="secondary" text="Attributes">
+						<DropdownDisclosureButtonItem onClick={() => {}}>
+							Todo
+						</DropdownDisclosureButtonItem>
+						<DropdownDisclosureButtonItem onClick={() => {}}>
+							Todo
+						</DropdownDisclosureButtonItem>
+						<DropdownDisclosureButtonItem onClick={() => {}}>
+							Todo
+						</DropdownDisclosureButtonItem>
+					</DropdownDisclosure>
+
+					{atLeastOneFacetSelected && (
+						<button
+							title="Clear filters"
+							className={s.clearFilters}
+							onClick={(e) => {
+								setOfficialChecked(false)
+								setPartnerChecked(false)
+								setCommunityChecked(false)
+								setComponentCheckedArray(
+									componentCheckedArray.map((v, i) => {
+										return false
+									})
+								)
+							}}
+						>
+							<IconX16 />
+							<span>Clear filters</span>
+						</button>
+					)}
+				</div>
 			</div>
 
 			<p className={s.results}>
