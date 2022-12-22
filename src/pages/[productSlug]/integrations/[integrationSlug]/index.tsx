e269d5149c9699e15ab87dd3b860c3894c66d004
Brandon Romano <brandon@hashicorp.com>
2022-12-22 12:43:49 -0800

    Multi Page Integrations (#1493)
    * Layout WIP
    
    * Add noindex to non-latest
    
    * Move the current integration page temporarily
    
    * Move our nested page to use a URL param
    
    * Rename old integrations paths
    
    * Adds header component
    
    * Move Header component to layout
    
    * Clean up data fetch, abstract serialization
    
    * Render versioned pages
    
    * Adds back withTimng
    
    * Render component page
    
    * Switch README sidebar title to Overview
    
    * Plug in content for component view
    
    * Move components to new component-view
    
    * Delete old components / sections
    
    * Adds basic page title
    
    * Generate integration library nav level for mobile
    
    * Adds Integration page metadata
    
    * Specify product on integrations landing
    
    * Don't export integrationComponentBreadcrumbLinks
    
    Co-authored-by: Kevin Wang <kwangsan@gmail.com>
    
    Co-authored-by: Kevin Wang <kwangsan@gmail.com>

---
+++ src/pages/[productSlug]/integrations/[integrationSlug]/index.tsx
@@ -0,0 +1,111 @@
+import { BreadcrumbLink } from 'components/breadcrumb-bar'
+import { ENABLED_INTEGRATION_PRODUCTS } from 'lib/enabled-integration-products'
+import { cachedGetProductData } from 'lib/get-product-data'
+import {
+	Integration,
+	fetchIntegration,
+} from 'lib/integrations-api-client/integration'
+import { fetchIntegrationRelease } from 'lib/integrations-api-client/release'
+import serializeIntegrationMarkdown from 'lib/serialize-integration-markdown'
+import { withTiming } from 'lib/with-timing'
+import { ProductData, ProductSlug } from 'types/products'
+import ProductIntegrationReadmeView from 'views/product-integration/readme-view'
+
+interface PathParams {
+	productSlug: ProductSlug
+	integrationSlug: string
+}
+
+async function _getServerSideProps({
+	params: { productSlug, integrationSlug },
+}: {
+	params: PathParams
+}) {
+	// Pull out the Product Config
+	const product = cachedGetProductData(productSlug)
+
+	// 404 early if the product is not enabled for integrations
+	if (!ENABLED_INTEGRATION_PRODUCTS.includes(product.slug)) {
+		return {
+			notFound: true,
+		}
+	}
+
+	// Fetch the Integration
+	const integrationResponse = await fetchIntegration(
+		product.slug,
+		integrationSlug
+	)
+	if (integrationResponse.meta.status_code != 200) {
+		console.warn('Could not fetch Integration', integrationResponse)
+		return { notFound: true }
+	}
+	const integration = integrationResponse.result
+
+	// If the integration is external only, we shouldn't render this page
+	if (integration.external_only) {
+		return {
+			notFound: true,
+		}
+	}
+
+	// Fetch the Latest Release
+	const activeReleaseResponse = await fetchIntegrationRelease(
+		product.slug,
+		integrationSlug,
+		integrationResponse.result.versions[0] // Always the latest release
+	)
+	if (activeReleaseResponse.meta.status_code != 200) {
+		console.warn('Could not fetch Release', activeReleaseResponse)
+		return { notFound: true }
+	}
+	const activeRelease = activeReleaseResponse.result
+
+	return {
+		props: {
+			metadata: {
+				title: `${integration.name} | Integrations`,
+				description: integration.description,
+			},
+			product,
+			integration,
+			activeRelease,
+			breadcrumbLinks: integrationBreadcrumbLinks(product, integration, true),
+			serializedREADME: await serializeIntegrationMarkdown(
+				activeRelease.readme
+			),
+		},
+	}
+}
+
+export function integrationBreadcrumbLinks(
+	product: ProductData,
+	integration: Integration,
+	finalBreadcrumbSegments: boolean
+): Array<BreadcrumbLink> {
+	return [
+		{
+			title: 'Developer',
+			url: '/',
+		},
+		{
+			title: product.name,
+			url: `/${product.slug}`,
+		},
+		{
+			title: 'Integrations',
+			url: `/${product.slug}/integrations`,
+		},
+		{
+			title: integration.name,
+			url: `/${product.slug}/integrations/${integration.slug}`,
+			isCurrentPage: finalBreadcrumbSegments,
+		},
+	]
+}
+
+const label = '[productSlug]/integrations/[integrationSlug]'
+export const getServerSideProps = async (ctx) => {
+	return withTiming(label, () => _getServerSideProps(ctx))
+}
+export default ProductIntegrationReadmeView
