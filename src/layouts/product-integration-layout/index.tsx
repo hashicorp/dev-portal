c093495fa1d610c3ae5584f526e3355239ca5e57
Brandon Romano <brandon@hashicorp.com>
2022-12-22 12:43:49 -0800

    Multi Page Integrations (#1493)
    * Layout WIP
    
    * Add noindex to non-latest
    
    * Move the current integration page temporarily
    
    * Move our nested page to use a URL param
    
    * Rename old integrations paths
    
    * Adds header component
    
    * Move Header component to layout
    
    * Clean up data fetch, abstract serialization
    
    * Render versioned pages
    
    * Adds back withTimng
    
    * Render component page
    
    * Switch README sidebar title to Overview
    
    * Plug in content for component view
    
    * Move components to new component-view
    
    * Delete old components / sections
    
    * Adds basic page title
    
    * Generate integration library nav level for mobile
    
    * Adds Integration page metadata
    
    * Specify product on integrations landing
    
    * Don't export integrationComponentBreadcrumbLinks
    
    Co-authored-by: Kevin Wang <kwangsan@gmail.com>
    
    Co-authored-by: Kevin Wang <kwangsan@gmail.com>

---
+++ src/layouts/product-integration-layout/index.tsx
@@ -0,0 +1,121 @@
+import HashiHead from '@hashicorp/react-head'
+import { BreadcrumbLink } from 'components/breadcrumb-bar'
+import {
+	generateProductLandingSidebarNavData,
+	generateTopLevelSidebarNavData,
+} from 'components/sidebar/helpers'
+import SidebarSidecarLayout from 'layouts/sidebar-sidecar'
+import { Integration } from 'lib/integrations-api-client/integration'
+import { Release, ReleaseComponent } from 'lib/integrations-api-client/release'
+import { generateProductIntegrationLibrarySidebarNavData } from 'pages/[productSlug]/integrations'
+import { ProductData } from 'types/products'
+import Header from './components/header'
+import s from './style.module.css'
+
+interface ProductIntegrationLayoutProps {
+	title: string
+	className?: string
+	currentProduct: ProductData
+	integration: Integration
+	activeRelease: Release
+	breadcrumbLinks: BreadcrumbLink[]
+	// When the version is changed on a page using this layout,
+	// this function should calculate the new URL that we
+	// should be redirected to.
+	getVersionChangedURL: (version: string) => string
+	children: React.ReactNode
+}
+
+/**
+ * This is a layout component that manages the SidebarSidecarLayout of an integration
+ * page. This layout is coupled to the URL structure, and assumes this integration
+ * page is rendering in the `/{product}/integrations/{integration}` context.
+ */
+export default function ProductIntegrationLayout({
+	className,
+	title,
+	currentProduct,
+	integration,
+	activeRelease,
+	breadcrumbLinks,
+	getVersionChangedURL,
+	children,
+}: ProductIntegrationLayoutProps) {
+	// Determine if we're on the lastest version, as that will slightly adjust the URLs
+	const onLatestVersion = integration.versions[0] === activeRelease.version
+
+	// Components that have READMEs or Variable Groups get a page
+	const componentsWithPages: Array<ReleaseComponent> =
+		activeRelease.components.filter((rc: ReleaseComponent) => {
+			return rc.readme || rc.variable_groups.length
+		})
+
+	// Calculate the Sidebar
+	const sidebarNavDataLevels = [
+		generateTopLevelSidebarNavData(currentProduct.name),
+		generateProductLandingSidebarNavData(currentProduct),
+		generateProductIntegrationLibrarySidebarNavData(currentProduct),
+		{
+			backToLinkProps: {
+				text: `${currentProduct.name} Integrations`,
+				href: `/${currentProduct.slug}/integrations`,
+			},
+			levelButtonProps: {
+				levelUpButtonText: `${currentProduct.name} Integrations`,
+				levelDownButtonText: 'Previous',
+			},
+			showFilterInput: false,
+			title: integration.name,
+			menuItems: [
+				{
+					title: 'Overview',
+					fullPath: onLatestVersion
+						? `/${currentProduct.slug}/integrations/${integration.slug}`
+						: `/${currentProduct.slug}/integrations/${integration.slug}/${activeRelease.version}`,
+				},
+				componentsWithPages.length
+					? {
+							title: 'Components',
+							isOpen: true,
+							routes: componentsWithPages.map((rc: ReleaseComponent) => {
+								return {
+									title: rc.component.name,
+									fullPath: onLatestVersion
+										? `/${currentProduct.slug}/integrations/${integration.slug}/latest/components/${rc.component.slug}`
+										: `/${currentProduct.slug}/integrations/${integration.slug}/${activeRelease.version}/components/${rc.component.slug}`,
+								}
+							}),
+					  }
+					: undefined,
+			],
+		},
+	]
+
+	return (
+		<SidebarSidecarLayout
+			// TODO: see if we can fix the navDataLevels type
+			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+			// @ts-ignore
+			sidebarNavDataLevels={sidebarNavDataLevels}
+			breadcrumbLinks={breadcrumbLinks}
+			sidecarSlot={<></>}
+		>
+			{!onLatestVersion && (
+				<HashiHead>
+					<meta name="robots" content="noindex, nofollow" />
+				</HashiHead>
+			)}
+			<Header
+				className={s.header}
+				integration={integration}
+				activeRelease={activeRelease}
+				getVersionChangedURL={getVersionChangedURL}
+				onInstallClicked={() => {
+					console.log('TODO, probably remove this')
+				}}
+			/>
+			<h1>{title}</h1>
+			<div className={className}>{children}</div>
+		</SidebarSidecarLayout>
+	)
+}
---
+++ src/pages/[productSlug]/integrations/[integrationSlug]/[integrationVersion]/components/[componentSlug]/index.tsx
@@ -0,0 +1,162 @@
+import { BreadcrumbLink } from 'components/breadcrumb-bar'
+import { ENABLED_INTEGRATION_PRODUCTS } from 'lib/enabled-integration-products'
+import { cachedGetProductData } from 'lib/get-product-data'
+import {
+	Integration,
+	fetchIntegration,
+} from 'lib/integrations-api-client/integration'
+import {
+	Release,
+	ReleaseComponent,
+	fetchIntegrationRelease,
+} from 'lib/integrations-api-client/release'
+import serializeIntegrationMarkdown from 'lib/serialize-integration-markdown'
+import { withTiming } from 'lib/with-timing'
+import { ProductData, ProductSlug } from 'types/products'
+import ProductIntegrationComponentView from 'views/product-integration/component-view'
+import { integrationVersionBreadcrumbLinks } from '../..'
+
+interface PathParams {
+	productSlug: ProductSlug
+	integrationSlug: string
+	integrationVersion: string
+	componentSlug: string
+}
+
+async function _getServerSideProps({
+	params: { productSlug, integrationSlug, integrationVersion, componentSlug },
+}: {
+	params: PathParams
+}) {
+	// Pull out the Product Config
+	const product = cachedGetProductData(productSlug)
+
+	// 404 early if the product is not enabled for integrations
+	if (!ENABLED_INTEGRATION_PRODUCTS.includes(product.slug)) {
+		return {
+			notFound: true,
+		}
+	}
+
+	// Fetch the Integration
+	const integrationResponse = await fetchIntegration(
+		product.slug,
+		integrationSlug
+	)
+	if (integrationResponse.meta.status_code != 200) {
+		console.warn('Could not fetch Integration', integrationResponse)
+		return { notFound: true }
+	}
+	const integration = integrationResponse.result
+
+	// If the integration is external only, we shouldn't render this page
+	if (integration.external_only) {
+		return {
+			notFound: true,
+		}
+	}
+
+	// Fetch the Release
+	const activeReleaseResponse = await fetchIntegrationRelease(
+		product.slug,
+		integrationSlug,
+		integrationVersion === 'latest'
+			? integration.versions[0]
+			: integrationVersion
+	)
+	if (activeReleaseResponse.meta.status_code != 200) {
+		console.warn('Could not fetch Release', activeReleaseResponse)
+		return { notFound: true }
+	}
+	const activeRelease = activeReleaseResponse.result
+
+	// Grab the specific release component
+	const releaseComponent: ReleaseComponent = activeRelease.components
+		.filter((rc: ReleaseComponent) => {
+			// Only release components w/ either a README or variable groups get a page
+			return rc.readme || rc.variable_groups.length
+		})
+		.find((rc: ReleaseComponent) => {
+			return rc.component.slug === componentSlug
+		})
+	if (!releaseComponent) {
+		return { notFound: true }
+	}
+
+	// If the integration version is the latest version, redirect using `latest`
+	if (integrationVersion === integration.versions[0]) {
+		return {
+			redirect: {
+				destination: `/${product.slug}/integrations/${integration.slug}/latest/components/${releaseComponent.component.slug}`,
+				// Not permanent as a new release in the future will turn the
+				// latest release into an older release which should render!
+				permanent: false,
+			},
+		}
+	}
+
+	// Compute the string for the inclusion of the version in the title
+	const titleVersion =
+		activeRelease.version === integration.versions[0]
+			? ''
+			: ` (${activeRelease.version})`
+
+	return {
+		props: {
+			metadata: {
+				title: `${integration.name} ${releaseComponent.component.name}${titleVersion} | Integrations`,
+			},
+			product,
+			integration,
+			activeRelease,
+			component: releaseComponent,
+			serializedREADME: releaseComponent.readme
+				? await serializeIntegrationMarkdown(releaseComponent.readme)
+				: undefined,
+			breadcrumbLinks: integrationComponentBreadcrumbLinks(
+				product,
+				integration,
+				activeRelease,
+				releaseComponent,
+				true
+			),
+		},
+	}
+}
+
+function integrationComponentBreadcrumbLinks(
+	product: ProductData,
+	integration: Integration,
+	activeRelease: Release,
+	releaseComponent: ReleaseComponent,
+	finalBreadcrumbSegments: boolean
+): Array<BreadcrumbLink> {
+	const versionSlug =
+		activeRelease.version === integration.versions[0]
+			? 'latest'
+			: activeRelease.version
+
+	return [
+		...integrationVersionBreadcrumbLinks(
+			product,
+			integration,
+			activeRelease,
+			false
+		),
+		{
+			title: 'Components',
+		},
+		{
+			title: releaseComponent.component.name,
+			url: `/${product.slug}/integrations/${integration.slug}/${versionSlug}/components/${releaseComponent.component.slug}`,
+			isCurrentPage: finalBreadcrumbSegments,
+		},
+	]
+}
+
+const label =
+	'[productSlug]/integrations/[integrationSlug]/[integrationVersion]/components/[component]'
+export const getServerSideProps = async (ctx) => {
+	return withTiming(label, () => _getServerSideProps(ctx))
+}
+export default ProductIntegrationComponentView
