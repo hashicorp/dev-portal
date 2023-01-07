35c6351919a38d37ee7f865362e2aa4a255f1c55
Kevin Wang <kwangsan@gmail.com>
2023-01-07 16:36:58 -0500

    chore: update swingset with useState

--- src/pages/swingset/[[...swingset]].tsx
+++ src/pages/swingset/[[...swingset]].tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import createPage from 'swingset/page'
 import { createStaticProps, createStaticPaths } from 'swingset/server'
 import remarkGfm from 'remark-gfm'
@@ -13,6 +14,7 @@ import SwingsetColorToken from '__swingset-components/swingset-color-token'
 import SwingsetTestIcon from '__swingset-components/swingset-test-icon'
 
 const components = {
+	useState,
 	InstruqtProvider,
 	Search,
 	SearchProvider,
