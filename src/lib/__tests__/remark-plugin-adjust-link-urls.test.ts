915d9062619c747d8c827159b4d555d6b0644782
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2023-01-10 12:40:53 -0500

    Adding plugin test cases for .io sites

--- src/lib/__tests__/remark-plugin-adjust-link-urls.test.ts
+++ src/lib/__tests__/remark-plugin-adjust-link-urls.test.ts
@@ -1,7 +1,14 @@
-import { preAdjustUrl } from 'lib/remark-plugin-adjust-link-urls'
+import remark from 'remark'
+import { ProductData, ProductSlug, RootDocsPath } from 'types/products'
+import { cachedGetProductData } from 'lib/get-product-data'
+import { productSlugs, productSlugsToHostNames } from 'lib/products'
+import remarkPluginAdjustLinkUrls, {
+	preAdjustUrl,
+} from 'lib/remark-plugin-adjust-link-urls'
+import { getProductUrlAdjuster } from 'views/docs-view/utils/product-url-adjusters'
 
 describe('remarkPluginAdjustLinkUrls', () => {
-	describe('preAdjustUrl', () => {
+	describe('preAdjustUrl helper', () => {
 		describe('does not pre-adjust urls that start with: `.`, `/`, `?`, `#`', () => {
 			const mockCurrentPath = '/some/docs/path'
 			const urlsToTest = [
@@ -41,6 +48,77 @@ describe('remarkPluginAdjustLinkUrls', () => {
 			test.each(urlsToTest)('"%s" is not pre-adjusted', (url: string) => {
 				expect(preAdjustUrl({ currentPath: mockCurrentPath, url })).toEqual(url)
 			})
+		})
+	})
+
+	describe('plugin tests', () => {
+		const URL_SUFFIXES = [
+			'',
+			'?param=value',
+			'#heading',
+			'?param=value#heading',
+		]
+
+		interface TestCase {
+			input: string
+			expected: string
+		}
+
+		const testEachCase = (product: ProductData, testCases: TestCase[]) => {
+			const productUrlAdjuster = getProductUrlAdjuster(product)
+
+			// Add a test case for each URL_SUFFIX
+			const allTestCases = []
+			testCases.forEach(({ input, expected }: TestCase) => {
+				URL_SUFFIXES.forEach((suffix: string) => {
+					allTestCases.push({
+						input: `${input}${suffix}`,
+						expected: `${expected}${suffix}`,
+					})
+				})
+			})
+
+			test.each(allTestCases)(
+				'$input -> $expected',
+				async ({ input, expected }: TestCase) => {
+					const result = await remark()
+						.use(remarkPluginAdjustLinkUrls, {
+							urlAdjustFn: productUrlAdjuster,
+						})
+						.process(`[test](${input})`)
+					expect(result.contents).toMatch(`[test](${expected})`)
+				}
+			)
+		}
+
+		describe('does not change external .io links', () => {
+			Object.entries(productSlugsToHostNames).forEach(
+				([productSlug, hostname]: [ProductSlug, string]) => {
+					// TODO add cases for sentinel?
+					if (productSlug === 'sentinel') {
+						return
+					}
+
+					const productData = cachedGetProductData(productSlug)
+					describe(productSlug, () => {
+						const testCases = []
+
+						productData.rootDocsPaths.forEach((rootDocsPath: RootDocsPath) => {
+							const url = `https://${hostname}/${rootDocsPath.path}`
+							testCases.push({
+								input: url,
+								expected: url,
+							})
+							testCases.push({
+								input: `${url}/some/path`,
+								expected: `${url}/some/path`,
+							})
+						})
+
+						testEachCase(productData, testCases)
+					})
+				}
+			)
 		})
 	})
 })
