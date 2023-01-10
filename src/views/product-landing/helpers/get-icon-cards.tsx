34d601816b628a3e7b11c153c83c4559777a743d
Brandon Romano <brandon@hashicorp.com>
2023-01-10 12:45:04 -0800

    Adds Integration Config settings (#1514)
    * Adds Integration Config settings
    
    * Adds section header
    
    * Update src/views/product-landing/helpers/get-icon-cards.tsx
    
    Co-authored-by: Kevin Wang <kwangsan@gmail.com>
    
    Co-authored-by: Kevin Wang <kwangsan@gmail.com>

--- src/views/product-landing/helpers/get-icon-cards.tsx
+++ src/views/product-landing/helpers/get-icon-cards.tsx
@@ -2,36 +2,35 @@ import { IconApi16 } from '@hashicorp/flight-icons/svg-react/api-16'
 import { IconDocs16 } from '@hashicorp/flight-icons/svg-react/docs-16'
 import { IconDownload16 } from '@hashicorp/flight-icons/svg-react/download-16'
 import { IconLearn16 } from '@hashicorp/flight-icons/svg-react/learn-16'
-import { ENABLED_INTEGRATION_PRODUCTS } from 'lib/enabled-integration-products'
-import { ProductSlug } from 'types/products'
+import { ProductData } from 'types/products'
 
-export function getIconCards(productSlug: ProductSlug) {
+export function getIconCards(product: ProductData) {
 	const iconCards = [
 		{
 			icon: <IconDocs16 />,
 			text: 'Documentation',
-			url: `/${productSlug}/docs`,
+			url: `/${product.slug}/docs`,
 		},
 		{
 			icon: <IconLearn16 />,
 			text: 'Tutorials',
-			url: `/${productSlug}/tutorials`,
+			url: `/${product.slug}/tutorials`,
 		},
 	]
-	if (productSlug !== 'hcp') {
+	if (product.slug !== 'hcp') {
 		iconCards.push({
 			icon: <IconDownload16 />,
 			text: 'Install',
-			url: `/${productSlug}/downloads`,
+			url: `/${product.slug}/downloads`,
 		})
 	}
 
 	// Add Integrations card if it's enabled for this product
-	if (ENABLED_INTEGRATION_PRODUCTS.includes(productSlug)) {
+	if (product.integrationsConfig.enabled) {
 		iconCards.push({
 			icon: <IconApi16 />,
 			text: 'Integrations',
-			url: `/${productSlug}/integrations`,
+			url: `/${product.slug}/integrations`,
 		})
 	}
 
