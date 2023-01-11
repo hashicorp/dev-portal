31a4602d850d094163534706b1280a5879985e65
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2023-01-10 20:35:25 -0500

    Adding cases for folder-relative urls

--- src/lib/__tests__/remark-plugin-adjust-link-urls.test.ts
+++ src/lib/__tests__/remark-plugin-adjust-link-urls.test.ts
@@ -2,18 +2,68 @@ import { preAdjustUrl } from 'lib/remark-plugin-adjust-link-urls'
 
 describe('remarkPluginAdjustLinkUrls', () => {
 	describe('preAdjustUrl', () => {
-		describe('does not pre-adjust urls that start with: `.`, `/`, `?`, `#`', () => {
+		describe('does not pre-adjust query strings or anchors', () => {
 			const mockCurrentPath = '/some/docs/path'
 			const urlsToTest = [
-				'../oneLevelUp',
-				'./differentPageAtCurrentLevel',
-				'/root/level/page',
-				'?paramKey=paramValue',
-				'#page-anchor',
+				'.',
+				'.invalid/folder/relative',
+				'?param=value',
+				'#heading',
+				'?param=value#heading',
 			]
-
 			test.each(urlsToTest)('"%s" is not pre-adjusted', (url: string) => {
 				expect(preAdjustUrl({ currentPath: mockCurrentPath, url })).toEqual(url)
+			})
+		})
+
+		describe('pre-adjusts folder-relative urls starting with `../`', () => {
+			const testCases = [
+				{
+					input: {
+						currentPath: '/docs/plugins/go-plugins/guests',
+						url: '../guest-capabilities',
+					},
+					expected: '/docs/plugins/guest-capabilities',
+				},
+				{
+					input: {
+						currentPath: '/docs/plugins/go-plugins/guests',
+						url: '../../multi-machine',
+					},
+					expected: '/docs/multi-machine',
+				},
+				{
+					input: {
+						currentPath: '/docs/plugins/go-plugins/guests',
+						url: '../../share/connect',
+					},
+					expected: '/docs/share/connect',
+				},
+			]
+			test.each(testCases)('$input.url -> $expected', ({ input, expected }) => {
+				expect(preAdjustUrl(input)).toBe(expected)
+			})
+		})
+
+		describe('pre-adjusts folder-relative urls starting with `./`', () => {
+			const testCases = [
+				{
+					input: {
+						currentPath: '/docs/disks',
+						url: './configuration',
+					},
+					expected: '/docs/disks/configuration',
+				},
+				{
+					input: {
+						currentPath: '/docs/disks',
+						url: './hyperv/common-issues',
+					},
+					expected: '/docs/disks/hyperv/common-issues',
+				},
+			]
+			test.each(testCases)('$input.url -> $expected', ({ input, expected }) => {
+				expect(preAdjustUrl(input)).toBe(expected)
 			})
 		})
 
