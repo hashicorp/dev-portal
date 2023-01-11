0c6e745edb52e411f883f59fd2a259664bc45a8f
Ashlee M Boyer <43934258+ashleemboyer@users.noreply.github.com>
2023-01-11 14:45:17 -0500

    Fix DocsView handling of some links with query params and/or anchors (#1515)
    * Adding plugin test cases for .io sites
    
    * Abstracting testEachCase helper
    
    * Adding query param and anchor failing test cases
    
    * Adding query params and anchors to every test case
    
    * Fixing some cases with query params and anchors
    
    * Flattening a describe
    
    * Fixing case in handling external learn links
    
    * Undoing some changes

--- src/lib/remark-plugins/rewrite-tutorial-links/__tests__/rewrite-external-docs-link.test.ts
+++ src/lib/remark-plugins/rewrite-tutorial-links/__tests__/rewrite-external-docs-link.test.ts
@@ -6,14 +6,23 @@ const getTestURLObject = (url: string) => {
 	return new URL(url, __config.dev_dot.canonical_base_url)
 }
 
+const URL_SUFFIXES = ['', '?param=value', '#heading', '?param=value#heading']
+
 const testEachCase = (cases: [string, string][]) => {
-	test.each(cases)(
-		'rewriteExternalDocsLink(%p) returns %p',
-		(input: string, expectedOutput: string) => {
-			const testUrlObject = getTestURLObject(input)
-			expect(rewriteExternalDocsLink(testUrlObject)).toBe(expectedOutput)
-		}
-	)
+	const allCases = []
+	cases.forEach(([input, expected]) => {
+		URL_SUFFIXES.forEach((suffix) => {
+			allCases.push([
+				typeof input === 'string' ? `${input}${suffix}` : input,
+				typeof expected === 'string' ? `${expected}${suffix}` : expected,
+			])
+		})
+	})
+
+	test.each(allCases)('%p -> %p', (input: string, expectedOutput: string) => {
+		const testUrlObject = getTestURLObject(input)
+		expect(rewriteExternalDocsLink(testUrlObject)).toBe(expectedOutput)
+	})
 }
 
 describe('rewriteExternalDocsLink', () => {
@@ -50,135 +59,49 @@ describe('rewriteExternalDocsLink', () => {
 		)
 	})
 
-	describe('when neither `search` nor `hash` are present', () => {
-		describe('when the base path is not "api"', () => {
-			testEachCase([
-				['https://vaultproject.io/docs', '/vault/docs'],
-				['https://waypointproject.io/docs', '/waypoint/docs'],
-				['https://vaultproject.io/docs/some/path', '/vault/docs/some/path'],
-				[
-					'https://waypointproject.io/docs/some/path',
-					'/waypoint/docs/some/path',
-				],
-			])
-		})
-
-		describe('when the base path is "api"', () => {
-			testEachCase([
-				['https://vaultproject.io/api', '/vault/api-docs'],
-				['https://vaultproject.io/api/some/path', '/vault/api-docs/some/path'],
-			])
-		})
-
-		describe('when the page is "index.html"', () => {
-			testEachCase([
-				['https://waypointproject.io/docs/index.html', '/waypoint/docs'],
-				[
-					'https://waypointproject.io/docs/some/path/index.html',
-					'/waypoint/docs/some/path',
-				],
-				['https://vaultproject.io/api/index.html', '/vault/api-docs'],
-				[
-					'https://vaultproject.io/api/some/path/index.html',
-					'/vault/api-docs/some/path',
-				],
-			])
-		})
-
-		describe('when the page ends with ".html"', () => {
-			testEachCase([
-				['https://www.terraform.io/downloads.html', '/terraform/downloads'],
-				['https://waypointproject.io/docs/page.html', '/waypoint/docs/page'],
-				[
-					'https://waypointproject.io/docs/some/path.html',
-					'/waypoint/docs/some/path',
-				],
-				['https://vaultproject.io/api/page.html', '/vault/api-docs/page'],
-				[
-					'https://vaultproject.io/api/some/path.html',
-					'/vault/api-docs/some/path',
-				],
-			])
-		})
+	describe('when the base path is not "api"', () => {
+		testEachCase([
+			['https://vaultproject.io/docs', '/vault/docs'],
+			['https://waypointproject.io/docs', '/waypoint/docs'],
+			['https://vaultproject.io/docs/some/path', '/vault/docs/some/path'],
+			['https://waypointproject.io/docs/some/path', '/waypoint/docs/some/path'],
+		])
 	})
 
-	describe('when `search` is present, and `hash` is NOT present', () => {
+	describe('when the base path is "api"', () => {
 		testEachCase([
+			['https://vaultproject.io/api', '/vault/api-docs'],
+			['https://vaultproject.io/api/some/path', '/vault/api-docs/some/path'],
+		])
+	})
+
+	describe('when the page is "index.html"', () => {
+		testEachCase([
+			['https://waypointproject.io/docs/index.html', '/waypoint/docs'],
 			[
-				'https://waypointproject.io/docs?paramA=valueA',
-				'/waypoint/docs?paramA=valueA',
+				'https://waypointproject.io/docs/some/path/index.html',
+				'/waypoint/docs/some/path',
 			],
+			['https://vaultproject.io/api/index.html', '/vault/api-docs'],
 			[
-				'https://vaultproject.io/api?paramA=valueA',
-				'/vault/api-docs?paramA=valueA',
-			],
-			[
-				'https://waypointproject.io/docs/index.html?paramA=valueA',
-				'/waypoint/docs?paramA=valueA',
-			],
-			[
-				'https://vaultproject.io/api/index.html?paramA=valueA',
-				'/vault/api-docs?paramA=valueA',
-			],
-			[
-				'https://waypointproject.io/docs/page.html?paramA=valueA',
-				'/waypoint/docs/page?paramA=valueA',
-			],
-			[
-				'https://vaultproject.io/api/page.html?paramA=valueA',
-				'/vault/api-docs/page?paramA=valueA',
+				'https://vaultproject.io/api/some/path/index.html',
+				'/vault/api-docs/some/path',
 			],
 		])
 	})
 
-	describe('when `search` is NOT present, and `hash` is present', () => {
+	describe('when the page ends with ".html"', () => {
 		testEachCase([
-			['https://vaultproject.io/docs#test-hash', '/vault/docs#test-hash'],
-			['https://vaultproject.io/api#test-hash', '/vault/api-docs#test-hash'],
+			['https://www.terraform.io/downloads.html', '/terraform/downloads'],
+			['https://waypointproject.io/docs/page.html', '/waypoint/docs/page'],
 			[
-				'https://vaultproject.io/docs/index.html#test-hash',
-				'/vault/docs#test-hash',
+				'https://waypointproject.io/docs/some/path.html',
+				'/waypoint/docs/some/path',
 			],
+			['https://vaultproject.io/api/page.html', '/vault/api-docs/page'],
 			[
-				'https://vaultproject.io/api/index.html#test-hash',
-				'/vault/api-docs#test-hash',
-			],
-			[
-				'https://vaultproject.io/docs/page.html#test-hash',
-				'/vault/docs/page#test-hash',
-			],
-			[
-				'https://vaultproject.io/api/page.html#test-hash',
-				'/vault/api-docs/page#test-hash',
-			],
-		])
-	})
-
-	describe('when both `search` and `hash` are present', () => {
-		testEachCase([
-			[
-				'https://vaultproject.io/docs?paramA=valueA#test-hash',
-				'/vault/docs?paramA=valueA#test-hash',
-			],
-			[
-				'https://vaultproject.io/api?paramA=valueA#test-hash',
-				'/vault/api-docs?paramA=valueA#test-hash',
-			],
-			[
-				'https://vaultproject.io/docs/index.html?paramA=valueA#test-hash',
-				'/vault/docs?paramA=valueA#test-hash',
-			],
-			[
-				'https://vaultproject.io/api/index.html?paramA=valueA#test-hash',
-				'/vault/api-docs?paramA=valueA#test-hash',
-			],
-			[
-				'https://vaultproject.io/docs/page.html?paramA=valueA#test-hash',
-				'/vault/docs/page?paramA=valueA#test-hash',
-			],
-			[
-				'https://vaultproject.io/api/page.html?paramA=valueA#test-hash',
-				'/vault/api-docs/page?paramA=valueA#test-hash',
+				'https://vaultproject.io/api/some/path.html',
+				'/vault/api-docs/some/path',
 			],
 		])
 	})
