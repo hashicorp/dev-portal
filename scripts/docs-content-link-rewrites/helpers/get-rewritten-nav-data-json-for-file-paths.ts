7ab7a4c86396808558fd92822620af244bfcdd29
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2023-01-12 18:58:21 -0500

    WIP commenting out problematic code

--- scripts/docs-content-link-rewrites/helpers/get-rewritten-nav-data-json-for-file-paths.ts
+++ scripts/docs-content-link-rewrites/helpers/get-rewritten-nav-data-json-for-file-paths.ts
@@ -2,59 +2,59 @@ import fs from 'fs'
 import path from 'path'
 import { ProductSlug } from 'types/products'
 import { cachedGetProductData } from 'lib/get-product-data'
-import { getRewrittenNonRelativeUrl } from '../rewrite-links-plugin'
+// import { getRewrittenNonRelativeUrl } from '../rewrite-links-plugin'
 import { getBasePathForNavDataFile } from './get-base-path-for-nav-data-file'
 
-const recursivelyUpdateNavItem = ({
-	currentPath,
-	item,
-	learnToDevDotPaths,
-	dotIoToDevDotPaths,
-}) => {
-	const isPathItem = item.hasOwnProperty('path')
-	const isHrefItem = item.hasOwnProperty('href')
-	const isRoutesItem = item.hasOwnProperty('routes')
+// const recursivelyUpdateNavItem = ({
+// 	currentPath,
+// 	item,
+// 	learnToDevDotPaths,
+// 	dotIoToDevDotPaths,
+// }) => {
+// 	const isPathItem = item.hasOwnProperty('path')
+// 	const isHrefItem = item.hasOwnProperty('href')
+// 	const isRoutesItem = item.hasOwnProperty('routes')
 
-	if (isPathItem || isHrefItem) {
-		const itemPropertyName = isPathItem ? 'path' : 'href'
-		const url = item[itemPropertyName]
+// 	if (isPathItem || isHrefItem) {
+// 		const itemPropertyName = isPathItem ? 'path' : 'href'
+// 		const url = item[itemPropertyName]
 
-		try {
-			const urlObject = new URL(url)
-			const rewrittenUrl = getRewrittenNonRelativeUrl({
-				dotIoToDevDotPaths,
-				learnToDevDotPaths,
-				urlObject,
-			})
-			if (rewrittenUrl) {
-				item[itemPropertyName] = rewrittenUrl
-			}
-		} catch (e) {
-			/**
-			 * Assume the url is already rewritten
-			 *
-			 * @TODO (ashleemboyer) Update after holiday break.
-			 * This will not 100% work for everything, but does work for Waypoint.
-			 */
-			if (url.startsWith('/')) {
-				return
-			}
+// 		try {
+// 			const urlObject = new URL(url)
+// 			const rewrittenUrl = getRewrittenNonRelativeUrl({
+// 				dotIoToDevDotPaths,
+// 				learnToDevDotPaths,
+// 				urlObject,
+// 			})
+// 			if (rewrittenUrl) {
+// 				item[itemPropertyName] = rewrittenUrl
+// 			}
+// 		} catch (e) {
+// 			/**
+// 			 * Assume the url is already rewritten
+// 			 *
+// 			 * @TODO (ashleemboyer) Update after holiday break.
+// 			 * This will not 100% work for everything, but does work for Waypoint.
+// 			 */
+// 			if (url.startsWith('/')) {
+// 				return
+// 			}
 
-			item[itemPropertyName] = path.join(currentPath, url)
-		}
-	}
+// 			item[itemPropertyName] = path.join(currentPath, url)
+// 		}
+// 	}
 
-	if (isRoutesItem) {
-		item.routes.forEach((routeItem) => {
-			recursivelyUpdateNavItem({
-				currentPath,
-				item: routeItem,
-				learnToDevDotPaths,
-				dotIoToDevDotPaths,
-			})
-		})
-	}
-}
+// 	if (isRoutesItem) {
+// 		item.routes.forEach((routeItem) => {
+// 			recursivelyUpdateNavItem({
+// 				currentPath,
+// 				item: routeItem,
+// 				learnToDevDotPaths,
+// 				dotIoToDevDotPaths,
+// 			})
+// 		})
+// 	}
+// }
 
 const getRewrittenNavDataJsonForFilePaths = async ({
 	filePaths,
@@ -69,28 +69,28 @@ const getRewrittenNavDataJsonForFilePaths = async ({
 }) => {
 	const allRewrittenNavData = {}
 
-	const productData = cachedGetProductData(normalizedProductSlug)
-	filePaths.forEach((filePath) => {
-		if (!filePath.endsWith('-nav-data.json')) {
-			return
-		}
+	// const productData = cachedGetProductData(normalizedProductSlug)
+	// filePaths.forEach((filePath) => {
+	// 	if (!filePath.endsWith('-nav-data.json')) {
+	// 		return
+	// 	}
 
-		const basePath = getBasePathForNavDataFile({ filePath, productData })
-		const fileContent = fs.readFileSync(filePath, 'utf-8')
-		const navData = JSON.parse(fileContent)
+	// 	const basePath = getBasePathForNavDataFile({ filePath, productData })
+	// 	const fileContent = fs.readFileSync(filePath, 'utf-8')
+	// 	const navData = JSON.parse(fileContent)
 
-		const newNavData = navData.map((item) => {
-			const itemCopy = { ...item }
-			recursivelyUpdateNavItem({
-				currentPath: `/${productData.slug}/${basePath}`,
-				item: itemCopy,
-				learnToDevDotPaths,
-				dotIoToDevDotPaths,
-			})
-			return itemCopy
-		})
-		allRewrittenNavData[filePath] = newNavData
-	})
+	// 	const newNavData = navData.map((item) => {
+	// 		const itemCopy = { ...item }
+	// 		recursivelyUpdateNavItem({
+	// 			currentPath: `/${productData.slug}/${basePath}`,
+	// 			item: itemCopy,
+	// 			learnToDevDotPaths,
+	// 			dotIoToDevDotPaths,
+	// 		})
+	// 		return itemCopy
+	// 	})
+	// 	allRewrittenNavData[filePath] = newNavData
+	// })
 
 	return allRewrittenNavData
 }
