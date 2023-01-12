1598c5cc9acfcefbf5fb9a90e16483d32f1f30cf
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2023-01-12 17:50:07 -0500

    Leveraging rewriteTutorialsLink & tracking stats

--- scripts/docs-content-link-rewrites/rewrite-links-plugin.ts
+++ scripts/docs-content-link-rewrites/rewrite-links-plugin.ts
@@ -2,6 +2,10 @@ import { Definition, Link } from 'mdast'
 import { Plugin } from 'unified'
 import visit from 'unist-util-visit'
 import { processDocsNode } from 'lib/remark-plugins/remark-plugin-adjust-link-urls/helpers'
+import { rewriteTutorialsLink } from 'lib/remark-plugins/rewrite-tutorial-links/utils/rewrite-tutorials-link'
+import { getTutorialMap } from 'lib/remark-plugins/rewrite-tutorial-links/utils'
+
+let TUTORIAL_MAP
 
 const rewriteLinksPlugin: Plugin = ({
 	urlAdjustFn,
@@ -16,8 +20,30 @@ const rewriteLinksPlugin: Plugin = ({
 	urlAdjustFn: (url: string) => string
 }) => {
 	return async function transformer(tree) {
+		if (!TUTORIAL_MAP) {
+			TUTORIAL_MAP = await getTutorialMap()
+		}
 		return visit(tree, ['link', 'definition'], (node: Link | Definition) => {
-			processDocsNode({ node, currentPath, urlAdjustFn, statistics })
+			const originalUrl = `${node.url}`
+
+			// Only adjust if the URL is not empty
+			if (node.url !== '') {
+				// Apply changes made by the tutorials link rewriter
+				node.url = rewriteTutorialsLink(originalUrl, TUTORIAL_MAP, 'docs')
+
+				// Apply changes on top of that with the docs link rewriter
+				// (this helper modifies `node.url`, does not return new url)
+				processDocsNode({ node, currentPath, urlAdjustFn })
+			}
+
+			// Record the URL in `statistics` based on whether or not it changed
+			const finalUrl = `${node.url}`
+			const { linksToRewrite = {}, unrewriteableLinks = [] } = statistics
+			if (finalUrl !== originalUrl) {
+				linksToRewrite[originalUrl] = finalUrl
+			} else {
+				unrewriteableLinks.push(originalUrl)
+			}
 		})
 	}
 }
