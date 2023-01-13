bf53ad39599996e1271b2860348541830b02e126
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2023-01-13 16:38:54 -0500

    feat: BrandedHeaderCard for integrations landing

---
+++ src/views/product-integrations-landing/components/branded-header-card/index.tsx
@@ -0,0 +1,18 @@
+import BrandedCard from 'components/branded-card'
+import { BrandedHeaderCardProps } from './types'
+import s from './branded-header-card.module.css'
+
+export function BrandedHeaderCard({
+	heading,
+	description,
+	productSlug,
+}: BrandedHeaderCardProps) {
+	return (
+		<BrandedCard productSlug={productSlug}>
+			<div className={s.cardPadding}>
+				<h1 className={s.heading}>{heading}</h1>
+				<p className={s.description}>{description}</p>
+			</div>
+		</BrandedCard>
+	)
+}
---
+++ src/views/product-integrations-landing/components/branded-header-card/types.ts
@@ -0,0 +1,7 @@
+import { ProductSlug } from 'types/products'
+
+export interface BrandedHeaderCardProps {
+	heading: string
+	description: string
+	productSlug?: ProductSlug
+}
