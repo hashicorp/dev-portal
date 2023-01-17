4247639b0798927e99852ef866490d77dd7ae694
Kevin Wang <kwangsan@gmail.com>
2023-01-17 16:02:00 -0500

    feat: update mobile filtering UI

--- src/views/product-integrations-landing/components/searchable-integrations-list/index.tsx
+++ src/views/product-integrations-landing/components/searchable-integrations-list/index.tsx
@@ -6,12 +6,17 @@ import { useIntegrationsSearchContext } from 'views/product-integrations-landing
 import PaginatedIntegrationsList from '../paginated-integrations-list'
 import s from './style.module.css'
 import capitalize from '@hashicorp/platform-util/text/capitalize'
-import { IconX16 } from '@hashicorp/flight-icons/svg-react/x-16'
 import { IconCheck16 } from '@hashicorp/flight-icons/svg-react/check-16'
+import { IconFilter16 } from '@hashicorp/flight-icons/svg-react/filter-16'
 import DropdownDisclosure, {
 	DropdownDisclosureButtonItem,
 } from 'components/dropdown-disclosure'
+import Button from 'components/button'
 import Tag from 'components/tag'
+import Dialog from 'components/dialog'
+import { IconX16 } from '@hashicorp/flight-icons/svg-react/x-16'
+import { CheckboxField } from 'components/form/field-controls'
+import Legend from 'components/form/components/legend'
 
 interface SearchableIntegrationsListProps {
 	className: string
@@ -93,6 +98,11 @@ export default function SearchableIntegrationsList({
 		})
 	}
 
+	const [isFilterDialogOpen, setIsFilterDialogOpen] = useState(false)
+
+	const resultText = `${filteredIntegrations.length} ${
+		filteredIntegrations.length === 1 ? 'result found' : 'results found'
+	}`
 	return (
 		<div className={classNames(s.searchableIntegrationsList, className)}>
 			<div className={s.header}>
@@ -102,7 +112,8 @@ export default function SearchableIntegrationsList({
 				/>
 
 				<div className={s.filterOptions}>
-					<div className={s.selectStack}>
+					{/* tablet_up */}
+					<div className={classNames(s.selectStack, s.tablet_up)}>
 						<DropdownDisclosure color="secondary" text="Tier">
 							{tierOptions.map((e) => {
 								const checked =
@@ -158,28 +169,24 @@ export default function SearchableIntegrationsList({
 							})}
 						</DropdownDisclosure>
 					</div>
-					<span className={s.results}>
-						{filteredIntegrations.length}{' '}
-						{filteredIntegrations.length === 1
-							? 'result found'
-							: 'results found'}
+					<span className={classNames(s.results, s.tablet_up)}>
+						{resultText}
 					</span>
+
+					{/* mobile_only */}
+					<div className={classNames(s.row, s.mobile_only)}>
+						<Button
+							className={s.row_fill}
+							text="Filters"
+							color="secondary"
+							icon={<IconFilter16 />}
+							iconPosition="trailing"
+							onClick={() => setIsFilterDialogOpen(true)}
+						/>
+					</div>
 				</div>
 
 				<div className={s.filterInfo}>
-					{atLeastOneFacetSelected ? (
-						<button
-							title="Clear filters"
-							className={s.clearFilters}
-							onClick={handleClearFilters}
-						>
-							<IconX16 />
-							<span>Clear filters</span>
-						</button>
-					) : (
-						<span className={s.noFilters}>No filters selected</span>
-					)}
-
 					{/* Render x-tags for tiers */}
 					{tierOptions.map((e) => {
 						const checked =
@@ -222,11 +229,167 @@ export default function SearchableIntegrationsList({
 							)
 						)
 					})}
+
+					{atLeastOneFacetSelected ? (
+						<button
+							title="Clear filters"
+							className={classNames(s.clearFilters, s.tablet_up)}
+							onClick={handleClearFilters}
+						>
+							<span>Clear filters</span>
+						</button>
+					) : (
+						<span className={s.noFilters}>No filters selected</span>
+					)}
 				</div>
 			</div>
 
+			<div className={classNames(s.results, s.mobile_only)}>{resultText}</div>
+
 			<PaginatedIntegrationsList integrations={filteredIntegrations} />
+
+			{/* 
+				Mobile Filter Dialog
+				- This dialog is rendered in a portal, so where it
+					exists in JSX markup is not super important.
+				*/}
+			<Dialog
+				isOpen={isFilterDialogOpen}
+				label="Tutorial filters"
+				onDismiss={() => setIsFilterDialogOpen(false)}
+				variant="bottom"
+			>
+				<div className={classNames(s.row, s.justifyEnd)}>
+					<button
+						className={s.exitIcon}
+						onClick={() => setIsFilterDialogOpen(false)}
+						aria-label="Cancel"
+						type="button"
+					>
+						<IconX16 />
+					</button>
+				</div>
+				<div className={s.row}>
+					<Button
+						text="Done"
+						onClick={() => setIsFilterDialogOpen(false)}
+						className={s.row_fill}
+					/>
+					<Button
+						disabled={!atLeastOneFacetSelected}
+						text="Reset Filters"
+						icon={<IconX16 />}
+						color="tertiary"
+						onClick={handleClearFilters}
+						className={s.row_fill}
+					/>
+				</div>
+
+				<div className={s.mobileFilters}>
+					<MobileFilters />
+				</div>
+			</Dialog>
 		</div>
+	)
+}
+
+// Renders Tier/Component/Flags checkboxes
+function MobileFilters() {
+	const {
+		tierOptions,
+		officialChecked,
+		setOfficialChecked,
+		partnerChecked,
+		setPartnerChecked,
+		communityChecked,
+		setCommunityChecked,
+		sortedComponents,
+		componentCheckedArray,
+		setComponentCheckedArray,
+		flags,
+		flagsCheckedArray,
+		setFlagsCheckedArray,
+	} = useIntegrationsSearchContext()
+
+	const makeToggleTierHandler = (tier: Tier) => () => {
+		switch (tier) {
+			case Tier.OFFICIAL:
+				setOfficialChecked((s) => !s)
+				break
+			case Tier.PARTNER:
+				setPartnerChecked((s) => !s)
+				break
+			case Tier.COMMUNITY:
+				setCommunityChecked((s) => !s)
+				break
+		}
+	}
+
+	const makeToggleComponentHandler = (index: number) => () => {
+		const newComponentCheckedArray = [...componentCheckedArray]
+		newComponentCheckedArray[index] = !newComponentCheckedArray[index]
+		setComponentCheckedArray(newComponentCheckedArray)
+	}
+
+	const makeToggleFlagHandler = (index: number) => () => {
+		const newFlagsCheckedArray = [...flagsCheckedArray]
+		newFlagsCheckedArray[index] = !newFlagsCheckedArray[index]
+		setFlagsCheckedArray(newFlagsCheckedArray)
+	}
+
+	return (
+		<>
+			<div className={s.optionsContainer}>
+				<Legend>Tier</Legend>
+				{tierOptions.map((e) => {
+					const checked =
+						(e === Tier.OFFICIAL && officialChecked) ||
+						(e === Tier.PARTNER && partnerChecked) ||
+						(e === Tier.COMMUNITY && communityChecked)
+					return (
+						<CheckboxField
+							key={e}
+							labelFontWeight="regular"
+							label={capitalize(e)}
+							checked={checked}
+							onChange={makeToggleTierHandler(e)}
+						/>
+					)
+				})}
+			</div>
+
+			<div className={s.optionsContainer}>
+				<Legend>Component</Legend>
+				{sortedComponents.map((e, i) => {
+					const checked = componentCheckedArray[i]
+					return (
+						<CheckboxField
+							key={e.id}
+							labelFontWeight="regular"
+							label={capitalize(e.plural_name)}
+							checked={checked}
+							onChange={makeToggleComponentHandler(i)}
+						/>
+					)
+				})}
+			</div>
+
+			<div className={s.optionsContainer}>
+				<Legend>Flags</Legend>
+				{flags.map((e, i) => {
+					const checked = flagsCheckedArray[i]
+					return (
+						<CheckboxField
+							key={e.id}
+							labelFontWeight="regular"
+							label={e.name}
+							checked={checked}
+							onChange={makeToggleFlagHandler(i)}
+						/>
+					)
+				})}
+			</div>
+		</>
 	)
 }
 
@@ -234,6 +397,7 @@ function FilterBar({ filterQuery, onChange }) {
 	return (
 		<div className={s.searchBar}>
 			<IconSearch16 />
+			{/* TODO: consider <FilterInput/> */}
 			<input
 				type="text"
 				placeholder="Filter integrations"
