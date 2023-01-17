9d4ee6793327a03524659fe05ade66d90ef8c53e
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2023-01-17 18:24:16 -0500

    feat: add required-only boolean filter (#1531)
    * feat: stub in checkbox control
    
    * feat: implement required-only filtering
    
    * chore: clean up filtering logic, split to helpers
    
    * feat: implement basic mobile styling
    
    * feat: only show isRequired control if relevant
    
    * fix: fix up and tweak styling
    
    * fix: tweak spacing
    
    * fix: split out includeMatchAncestors, cleanup
    
    * fix: highlight only if filters are applied
    
    * fix: only highlight query matches

--- src/views/product-integration/component-view/components/searchable-variable-group-list/index.tsx
+++ src/views/product-integration/component-view/components/searchable-variable-group-list/index.tsx
@@ -1,6 +1,12 @@
 import { IconSearch16 } from '@hashicorp/flight-icons/svg-react/search-16'
+import { CheckboxField } from 'components/form/field-controls'
 import { useState } from 'react'
 import { Variable, VariableGroupList } from '../variable-group-list'
+import {
+	applyQueryFilter,
+	applyRequiredFilter,
+	includeMatchAncestors,
+} from './helpers'
 import s from './style.module.css'
 
 interface SearchableVariableGroupListProps {
@@ -13,69 +19,66 @@ export default function SearchableVariableGroupList({
 	variables,
 }: SearchableVariableGroupListProps) {
 	const [searchQuery, setSearchQuery] = useState('')
-	let filteredVariables: Array<Variable> = []
-	let numMatches: number
+	const [requiredOnly, setRequiredOnly] = useState(false)
 
-	if (searchQuery.length < 2) {
-		filteredVariables = variables
-		numMatches = filteredVariables.length
-	} else {
-		// Filter all the variables that have been directly matched via the Search Query
-		const directMatches = variables.filter((variable: Variable) => {
-			return (
-				variable.key.toLowerCase().includes(searchQuery.toLowerCase()) ||
-				variable.description?.toLowerCase().includes(searchQuery.toLowerCase())
-			)
-		})
-		numMatches = directMatches.length
+	/**
+	 * We only want to render the `requiredOnly` filtering input if the
+	 * required property is relevant for the variables we're rendering.
+	 *
+	 * We consider the required property to be relevant if any of the variables
+	 * have their required property set to `true` or `false`, rather than
+	 * the default `null` setting.
+	 */
+	const isRequiredRelevant = variables.find(
+		({ required }: Variable) => required === true || required === false
+	)
 
-		// Determine all of the variables that need to be included in the results
-		// This includes all of the ones matched above, but also their parents
-		const allKeys = []
-		directMatches
-			.map((variable: Variable) => variable.key)
-			.forEach((matchedKey: string) => {
-				const segments = matchedKey.split('.')
-				segments.forEach((segment: string, i: number) => {
-					allKeys.push(
-						matchedKey
-							.split('.')
-							.slice(0, i + 1)
-							.join('.')
-					)
-				})
-			})
-		const allKeysUnique = Array.from(new Set(allKeys))
-		filteredVariables = variables
-			.filter((variable: Variable) => allKeysUnique.includes(variable.key))
-			.map((variable: Variable) => {
-				// If a variable was a direct match, specify highlight as a prop
-				if (
-					directMatches
-						.map((variable: Variable) => variable.key)
-						.includes(variable.key)
-				) {
-					return {
-						...variable,
-						highlight: true,
-					}
-				} else {
-					return variable
-				}
-			})
+	/**
+	 * Apply the requiredOnly & searchQuery filters.
+	 * Then, include ancestor variables for all direct matches.
+	 *
+	 * Note: We do not include unmatched ancestor variables in the results count.
+	 * Instead, we display the results count as the number of direct matches only.
+	 * As well, we only highlight direct matches, and only if filters are applied.
+	 */
+	let matches = variables
+	// Apply the required filter, if applicable
+	const applyRequired = requiredOnly === true
+	if (applyRequired) {
+		matches = applyRequiredFilter(matches)
 	}
+	// Apply the query filter, if applicable. Query matches are highlighted.
+	const applyQuery = searchQuery.length > 2
+	if (applyQuery) {
+		matches = applyQueryFilter(searchQuery, matches)
+	}
+	// Include ancestors, not only direct matches.
+	const matchesWithAncestors = includeMatchAncestors(matches, variables)
+	// Count direct matches only
+	const numMatches = matches.length
 
 	return (
 		<div>
-			<SearchBar
-				groupName={groupName}
-				searchQuery={searchQuery}
-				onChange={(e) => setSearchQuery(e.target.value)}
-			/>
+			<div className={s.searchRow}>
+				<SearchBar
+					groupName={groupName}
+					searchQuery={searchQuery}
+					onChange={(e) => setSearchQuery(e.target.value)}
+				/>
+				{isRequiredRelevant ? (
+					<div className={s.checkboxContainer}>
+						<CheckboxField
+							label="Required only"
+							checked={requiredOnly}
+							onChange={() => setRequiredOnly(!requiredOnly)}
+						/>
+					</div>
+				) : null}
+			</div>
 			<p className={s.results}>
 				{numMatches} {numMatches === 1 ? 'Result' : 'Results'}
 			</p>
-			<VariableGroupList variables={filteredVariables} />
+			<VariableGroupList variables={matchesWithAncestors} />
 		</div>
 	)
 }
