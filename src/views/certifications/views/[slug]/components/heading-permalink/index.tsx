6cf36466bf93a3a8b408e55996c7e2ae14464775
Kendall Strautman <36613477+kendallstrautman@users.noreply.github.com>
2023-01-18 16:47:32 -0800

    Update src/views/certifications/views/[slug]/components/heading-permalink/index.tsx

--- src/views/certifications/views/[slug]/components/heading-permalink/index.tsx
+++ src/views/certifications/views/[slug]/components/heading-permalink/index.tsx
@@ -1,8 +1,10 @@
 import slugify from 'slugify'
 import { IconLink16 } from '@hashicorp/flight-icons/svg-react/link-16'
 import s from './heading-permalink.module.css'
+
 // Future thought: refactor this to use a shared `HeadingPermalink`
 // base component that can also be used in the mdx heading component
+
 export function HeadingPermalink({ heading }: { heading: string }) {
 	const id = `${slugify(heading, {
 		lower: true,
