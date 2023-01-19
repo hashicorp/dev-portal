cda5a621a0df74a0689a21e9a8042f1c10b3e7e2
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2023-01-19 12:31:04 -0500

    feat: add integrations library event helpers

---
+++ src/views/product-integrations-landing/components/searchable-integrations-list/helpers/analytics.ts
@@ -0,0 +1,42 @@
+import { safeAnalyticsTrack } from 'lib/analytics'
+
+/**
+ * Track "Integration Library Searched" events.
+ *
+ * @see /analytics/spec/events/integration_library_searched.yml
+ */
+function integrationLibrarySearchedEvent({
+	search_query,
+	results_count,
+}: {
+	search_query: string
+	results_count: number
+}) {
+	safeAnalyticsTrack('Integration Library Filtered Selected', {
+		search_query,
+		results_count,
+	})
+}
+
+/**
+ * Track "Integration Library Filtered Selected" events.
+ *
+ * @see /analytics/spec/events/integration_library_filter_selected.yml
+ */
+function integrationLibraryFilterSelectedEvent({
+	filter_category,
+	filter_value,
+}: {
+	filter_category: 'tier' | 'component' | 'flag'
+	filter_value: number
+}) {
+	safeAnalyticsTrack('Integration Library Filtered Selected', {
+		filter_category,
+		filter_value,
+	})
+}
+
+export {
+	integrationLibrarySearchedEvent,
+	integrationLibraryFilterSelectedEvent,
+}
