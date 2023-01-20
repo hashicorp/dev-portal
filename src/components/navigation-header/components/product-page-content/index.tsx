0e42a7aee48570f9d69539b5d5c9894c859771f6
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2023-01-20 14:48:03 -0500

    feat: implement product-less nav, clean up

--- src/components/navigation-header/components/product-page-content/index.tsx
+++ src/components/navigation-header/components/product-page-content/index.tsx
@@ -2,81 +2,42 @@
 import { IconHashicorp24 } from '@hashicorp/flight-icons/svg-react/hashicorp-24'
 
 // Global imports
-import { ProductSlug } from 'types/products'
-import { productSlugsToNames } from 'lib/products'
 import { useCurrentProduct } from 'contexts'
-import { NavigationHeaderItemGroup } from 'components/navigation-header/types'
 
 // Local imports
 import {
+	NavBarListContainer,
 	NavigationHeaderDropdownMenu,
 	PrimaryNavLink,
 	PrimaryNavSubmenu,
 } from '..'
 import { ProductIconTextLink } from './components'
-import { getNavItems } from './utils/get-nav-items'
+import { getNavItems, getProductsDropdownItems, NavItem } from './utils'
 import s from './product-page-content.module.css'
 
 const ProductPageHeaderContent = () => {
 	const currentProduct = useCurrentProduct()
-
-	// Build menu items
-	const productItems = []
-	Object.keys(productSlugsToNames).forEach((productSlug: ProductSlug) => {
-		// Exclude Sentinel for now
-		if (productSlug === 'sentinel') {
-			return
-		}
-
-		// Generate properties of each menu item
-		const icon = productSlug
-		const label = productSlugsToNames[productSlug]
-		const path = `/${productSlug}`
-
-		// Push the menu item to the correct array
-		productItems.push({
-			icon,
-			label,
-			path,
-		})
-	})
-	const homeMenuItem = {
-		// TODO as string was not accepted
-		icon: 'home' as $TSFixMe,
-		label: 'HashiCorp Developer',
-		path: '/',
-	}
-
-	// Construct item groups for the dropdown, avoid adding empty groups
-	const allMainMenuItems: NavigationHeaderItemGroup[] = [
-		{ items: [homeMenuItem] },
-	]
-	if (productItems.length) {
-		allMainMenuItems.push({ label: 'Products', items: productItems })
-	}
+	const allProductsItems = getProductsDropdownItems()
+	const productNavItems = getNavItems(currentProduct)
 
 	return (
-		<div className={s.root}>
-			<div className={s.leftSideContainer}>
-				<div className={s.hashicorpIconDropdown}>
-					<NavigationHeaderDropdownMenu
-						ariaLabel="Main menu"
-						buttonClassName={s.companyLogoMenuButton}
-						dropdownClassName={s.companyLogoMenuButtonDropdown}
-						itemGroups={allMainMenuItems}
-						leadingIcon={<IconHashicorp24 className={s.hashiLogo} />}
-					/>
-				</div>
-				<div className={s.productIconTextLink}>
-					<ProductIconTextLink
-						name={currentProduct.name}
-						slug={currentProduct.slug}
-					/>
-				</div>
+		<>
+			<div className={s.productsDropdown}>
+				<NavigationHeaderDropdownMenu
+					ariaLabel="Main menu"
+					buttonClassName={s.productsDropdownButton}
+					dropdownClassName={s.productsDropdownPane}
+					itemGroups={allProductsItems}
+					leadingIcon={<IconHashicorp24 className={s.productsDropdownIcon} />}
+				/>
 			</div>
-			<nav className={s.nav}>
-				<ul className={s.navList}>
-					{getNavItems(currentProduct).map((navItem) => {
+			<div className={s.productLinkAndNav}>
+				<ProductIconTextLink
+					name={currentProduct.name}
+					slug={currentProduct.slug}
+				/>
+				<NavBarListContainer>
+					{productNavItems.map((navItem: NavItem) => {
 						const ariaLabel = `${currentProduct.name} ${navItem.label}`
 						const isSubmenu = 'items' in navItem
 
@@ -90,9 +51,9 @@ const ProductPageHeaderContent = () => {
 							</li>
 						)
 					})}
-				</ul>
-			</nav>
-		</div>
+				</NavBarListContainer>
+			</div>
+		</>
 	)
 }
 
