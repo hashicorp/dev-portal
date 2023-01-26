16c94fd2bdcadab9aeb38b192fa82d5094b5f5b7
Kendall Strautman <kendallstrautman@gmail.com>
2023-01-25 16:01:40 -0800

    chore: use badge

--- src/layouts/product-integration-layout/components/header/index.tsx
+++ src/layouts/product-integration-layout/components/header/index.tsx
@@ -6,10 +6,13 @@ import DropdownDisclosure, {
 	DropdownDisclosureLinkItem,
 } from 'components/dropdown-disclosure'
 import IconTileLogo from 'components/icon-tile-logo'
+import Badge from 'components/badge'
+import Tooltip from 'components/tooltip'
 import { Flag, Integration } from 'lib/integrations-api-client/integration'
 import { Release } from 'lib/integrations-api-client/release'
-import TagList, {
+import {
 	GetIntegrationTags,
+	Tag,
 } from 'views/product-integrations-landing/components/tag-list'
 import s from './style.module.css'
 
@@ -50,6 +53,8 @@ export default function Header({
 		.map((f: Flag) => f.slug)
 		.includes('builtin')
 
+	const tags = GetIntegrationTags(integration, true)
+
 	console.log({ integration })
 
 	return (
@@ -85,7 +90,18 @@ export default function Header({
 
 			<div>
 				{/**swap tags for badges */}
-				<TagList size="medium" tags={GetIntegrationTags(integration, true)} />
+				{tags.map((tag: Tag) => (
+					<Tooltip key={tag.name} label={tag.description}>
+						<Badge
+							className={s.tagContent}
+							icon={tag.icon as React.ReactElement}
+							text={tag.name}
+							type="outlined"
+							size="medium"
+						/>
+					</Tooltip>
+				))}
+
 				{shouldShowInstallButton && (
 					<Button
 						text="Install"
