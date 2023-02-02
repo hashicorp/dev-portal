391b98430ac89a5e958cd00232c7637a358ee595
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2023-02-02 10:39:19 -0500

    chore: enable waypoint, demo code block

--- src/views/product-integration/component-view/helpers/get-processed-variables-markdown.ts
+++ src/views/product-integration/component-view/helpers/get-processed-variables-markdown.ts
@@ -27,7 +27,8 @@ export async function getProcessedVariablesMarkdown(
 			processedVariablesMarkdown[uniqueKey] = {}
 			if (description !== null) {
 				// TODO: remove dev_markdown test values here
-				const dev_markdown = `**Test bold**, _emphasis_, also [a link](https://dev.hashicorp.com/not-visited-yet), and \`some inline code\`, heck maybe [\`linked code\`](https://dev.hashicorp.com/yoink)`
+				const dev_markdown = `**Test bold**, _emphasis_, also [a link](https://dev.hashicorp.com/not-visited-yet), and \`some inline code\`, heck maybe [\`linked code\`](https://dev.hashicorp.com/yoink).\n\n\`\`\`bash\necho "something" # like this\n
+				echo "wow"\n\`\`\`\n\n`
 				const dev_description = `${dev_markdown}. Original: ${description}`
 				const mdxSource = await serializeIntegrationMarkdown(dev_description)
 				processedVariablesMarkdown[uniqueKey] = { description: mdxSource }
