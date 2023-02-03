1445acf7e7ce65785fc51625d0b1b37da139929d
BrandonRomano <brandon@hashicorp.com>
2023-02-03 15:03:44 -0800

    Revert "Fix tests / remove plugins"
    This reverts commit 76a7ba70702bf4b3b675380917305b19ed4ff115.

--- src/components/navigation-header/components/product-page-content/utils/__tests__/get-nav-items.test.ts
+++ src/components/navigation-header/components/product-page-content/utils/__tests__/get-nav-items.test.ts
@@ -18,6 +18,11 @@ describe('getNavItems', () => {
 					name: 'CLI',
 					path: 'commands',
 				},
+				{
+					iconName: 'plug',
+					name: 'Plugins',
+					path: 'plugins',
+				},
 			],
 		} as ProductData
 		expect(getNavItems(testWaypointData)).toMatchInlineSnapshot(`
@@ -37,6 +42,10 @@ describe('getNavItems', () => {
 		  Object {
 		    "label": "CLI",
 		    "url": "/waypoint/commands",
+		  },
+		  Object {
+		    "label": "Plugins",
+		    "url": "/waypoint/plugins",
 		  },
 		  Object {
 		    "label": "Try Cloud",
