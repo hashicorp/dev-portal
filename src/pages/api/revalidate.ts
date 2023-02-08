1d858f962f6de3d7001644b94dfacf1aa882fe5c
Bryce Kalow <bkalow@hashicorp.com>
2023-02-08 12:34:17 -0600

    handle case where nav data does not exist

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -61,7 +61,7 @@ async function handler(request: NextApiRequest, response: NextApiResponse) {
 					)
 					const { result } = await response.json()
 
-					if (!result.navData) {
+					if (!result || !result.navData) {
 						console.log(
 							`[revalidate] failed to find nav data for path: ${path}. It is possible that this path does not have nav data, this is likely safe to ignore.`
 						)
@@ -90,7 +90,7 @@ async function handler(request: NextApiRequest, response: NextApiResponse) {
 					.replace(/\/$/, '')
 
 				console.log('[revalidate]', pathToRevalidate)
-				revalidatePromises.push(response.revalidate(pathToRevalidate))
+				// revalidatePromises.push(response.revalidate(pathToRevalidate))
 			}
 		})
 	}
