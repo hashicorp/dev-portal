36cdaf751dbb12e7912f12bdb21b3ae7fd54e671
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2023-02-21 11:43:22 -0500

    fix: use of new URL

--- src/components/outline-nav/utils/get-item-slugs.ts
+++ src/components/outline-nav/utils/get-item-slugs.ts
@@ -5,9 +5,12 @@ import { OutlineLinkItem } from '../types'
  */
 function getItemSlugs(items: OutlineLinkItem[]) {
 	return items.reduce((acc: string[], item: OutlineLinkItem) => {
-		const urlObject = new URL(item.url)
+		// Note that baseUrl isn't important, we're just using it to get the hash
+		const urlObject = new URL(item.url, 'https://www.example.com')
 		const slug = urlObject.hash.replace('#', '') // get hash without leading #
-		acc.push(slug)
+		if (slug !== '') {
+			acc.push(slug)
+		}
 		if ('items' in item) {
 			acc = acc.concat(getItemSlugs(item.items))
 		}
