ee5beceacfd108443951a7a00c0482345db4d73c
Nick Ayres <nickolas.ayres@hashicorp.com>
2023-02-22 13:12:15 -0700

    Add sitemap xml page

---
+++ src/pages/sitemap.xml.tsx
@@ -0,0 +1,33 @@
+import { GetServerSidePropsContext } from 'next'
+
+export default function SiteMap() {
+	return undefined
+}
+
+export async function getServerSideProps({ res }: GetServerSidePropsContext) {
+	const response = await fetch(
+		`https://content.hashicorp.com/api/all-docs-paths`
+	)
+	const { result } = await response.json()
+	console.log(result)
+	const createSitemap = () => `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        ${result
+					.map(
+						(sitePath: { path: string; created_at: string }) =>
+							`<url><loc>${
+								process.env.SITE_URL || 'https://developer.hashicorp.com'
+							}/${sitePath.path}</loc><lastmod>${
+								sitePath.created_at
+							}</lastmod><changefreq>daily</changefreq><priority>1.0</priority></url>`
+					)
+					.join('')}
+    </urlset>
+    `
+	res.setHeader('Content-Type', 'text/xml')
+	res.write(createSitemap())
+	res.end()
+	return {
+		props: {},
+	}
+}
