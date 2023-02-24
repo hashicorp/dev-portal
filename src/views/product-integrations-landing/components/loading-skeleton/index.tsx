45ad7e5d572e524e5371ce6a5fbe73568e4805e3
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2023-02-23 19:51:53 -0500

    Using full width rows and lowering opacity

--- src/views/product-integrations-landing/components/loading-skeleton/index.tsx
+++ src/views/product-integrations-landing/components/loading-skeleton/index.tsx
@@ -1,42 +1,4 @@
-import classNames from 'classnames'
 import s from './integrations-loading-skeleton.module.css'
-
-const FiltersRow = () => {
-	return (
-		<div className={s.filtersRow}>
-			{[1, 2, 3, 4, 5].map((_, index) => (
-				<div className={s.tag} key={index} />
-			))}
-			<div className={classNames('g-hide-on-mobile', s.clearFiltersButton)} />
-		</div>
-	)
-}
-
-const WideViewportContent = () => {
-	return (
-		<div className="g-hide-on-mobile">
-			<div className={s.filterControls}>
-				<div className={s.multiSelectsGroup}>
-					<div className={s.multiSelect} />
-					<div className={s.multiSelect} />
-					<div className={s.multiSelect} />
-				</div>
-				<div className={s.resultsText} />
-			</div>
-			<FiltersRow />
-		</div>
-	)
-}
-
-const NarrowViewportContent = () => {
-	return (
-		<div className="g-hide-on-tablet g-hide-on-desktop">
-			<div className={s.mobileFiltersButton} />
-			<FiltersRow />
-			<div className={s.resultsText} />
-		</div>
-	)
-}
 
 /**
  * @TODO replace with a more standard system when it's ready.
@@ -46,8 +8,15 @@ const LoadingSkeleton = () => {
 	return (
 		<div className={s.root}>
 			<div className={s.filterInput} />
-			<NarrowViewportContent />
-			<WideViewportContent />
+			<div className="g-hide-on-mobile">
+				<div className={s.filterControlsRow} />
+				<div className={s.tagsRow} />
+			</div>
+			<div className="g-hide-on-tablet g-hide-on-desktop">
+				<div className={s.filtersButton} />
+				<div className={s.tagsRow} />
+				<div className={s.resultsCountRow} />
+			</div>
 			<div className={s.cardsGrid}>
 				{[1, 2, 3, 4].map((_, index) => (
 					<div className={s.card} key={index} />
