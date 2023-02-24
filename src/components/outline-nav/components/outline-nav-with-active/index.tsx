b714a30441a1ee845c3efae4b9dbfc96a0d2c17c
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2023-02-24 12:12:24 -0500

    feat: useOnScreen for enableActiveSection

--- src/components/outline-nav/components/outline-nav-with-active/index.tsx
+++ src/components/outline-nav/components/outline-nav-with-active/index.tsx
@@ -1,11 +1,11 @@
-import { useMemo } from 'react'
+import { useMemo, useRef } from 'react'
 import OutlineNav from 'components/outline-nav'
+import useOnScreen from 'lib/hooks/use-on-screen'
 import { OutlineLinkItem } from 'components/outline-nav/types'
 import {
 	getItemSlugs,
 	highlightActiveItems,
 } from 'components/outline-nav/utils'
-import { useDeviceSize } from 'contexts'
 import { useActiveSection } from 'lib/hash-links/use-active-section'
 
 /**
@@ -17,21 +17,8 @@ function OutlineNavWithActive({
 }: {
 	items: Omit<OutlineLinkItem, 'isActive'>[]
 }) {
-	/**
-	 * We useDeviceSize to avoid running active heading calculations
-	 * if we're on a viewport where we won't render the nav anyways.
-	 *
-	 * TODO: isDesktop isn't fully accurate, we may want to adjust this
-	 * based on the specific breakpoint at which we show the sidecar.
-	 *
-	 * Thought: could we monitor the visibility of `OutlineNav` instead,
-	 * ie the root element we're rendering in this component?
-	 * If it's visible in CSS, then we should enable useActiveSection.
-	 * If it's not visible, then we should NOT enable useActiveSection.
-	 * Maybe a `useVisibility` hook or something would be useful here.
-	 * Maybe this could reduce reliance on specific breakpoint metrics.
-	 */
-	const { isDesktop } = useDeviceSize()
+	const rootRef = useRef()
+	const isOnScreen = useOnScreen(rootRef)
 
 	/**
 	 * Determine the active section. Note we only enable this when the sidecar
@@ -39,7 +26,7 @@ function OutlineNavWithActive({
 	 */
 	const itemSlugs = useMemo(() => getItemSlugs(items), [items])
 	const hasMultipleItems = itemSlugs.length > 1
-	const enableActiveSection = isDesktop && hasMultipleItems
+	const enableActiveSection = isOnScreen && hasMultipleItems
 	const activeSection = useActiveSection(itemSlugs, enableActiveSection)
 
 	/**
@@ -59,7 +46,11 @@ function OutlineNavWithActive({
 		return null
 	}
 
-	return <OutlineNav items={itemsWithActive} />
+	return (
+		<div ref={rootRef}>
+			<OutlineNav items={itemsWithActive} />
+		</div>
+	)
 }
 
 export { OutlineNavWithActive }
