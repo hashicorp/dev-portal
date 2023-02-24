5ac5e247166914be9db3ac87a574a75c9c4d0363
Kendall Strautman <kendallstrautman@gmail.com>
2023-02-24 11:40:36 -0800

    feat: adds tutorial and collection paths

--- src/pages/api/revalidate-all-tutorials.ts
+++ src/pages/api/revalidate-all-tutorials.ts
@@ -3,18 +3,21 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
+import moize, { Options } from 'moize'
 import type { NextApiRequest, NextApiResponse } from 'next/types'
 import { StatusCodes } from 'http-status-codes'
+
 import { validateToken } from 'lib/api-validate-token'
+import { getAllCollections } from 'lib/learn-client/api/collection'
 import {
-	ApiFeaturedCollection,
-	ApiProductsUsed,
-	ApiTutorialLite,
-} from 'lib/learn-client/api/api-types'
-import { getTutorialSlug } from 'views/collection-view/helpers'
-import { getCollectionSlug } from 'views/collection-view/helpers'
+	Collection as ClientCollection,
+	TutorialLite as ClientTutorialLite,
+} from 'lib/learn-client/types'
 import { activeProductSlugs } from 'lib/products'
 import { SectionOption } from 'lib/learn-client/types'
+import { getTutorialSlug } from 'views/collection-view/helpers'
+import { getCollectionSlug } from 'views/collection-view/helpers'
+import { ProductSlug } from 'types/products'
 
 /**
  * Accepts a POST request with a tutorial object, triggers revalidation for all paths associated with that tutorial
@@ -28,9 +31,9 @@ async function handler(request: NextApiRequest, response: NextApiResponse) {
 
 	try {
 		const revalidatePromises = []
-		const productTutorialPaths = getProductTutorialPathsToRevalidate()
-		const sectionTutorialPaths = getSectionTutorialPathsToRevalidate()
-		const paths = [...productTutorialPaths, ...sectionTutorialPaths]
+		const tutorialLandingPaths = getTutorialLandingPaths()
+		const collectionAndTutorialPaths = await getCollectionAndTutorialPaths()
+		const paths = [...tutorialLandingPaths, ...collectionAndTutorialPaths]
 
 		if (!paths || paths.length === 0) {
 			response.status(200).end()
@@ -58,50 +61,53 @@ export default validateToken(handler, {
 	onlyMethods: ['POST'],
 })
 
-function getProductTutorialPathsToRevalidate(): string[] {
+const moizeOpts: Options = {
+	isPromise: true,
+	maxSize: Infinity,
+	isDeepEqual: true,
+}
+
+// limit the expensive call for collections who all have the same product
+// TODO check how this functions, will this cache properly on the edge?
+// cache the collection paths in the res header?
+const cachedGetAllCollections = moize(getAllCollections, moizeOpts)
+
+function getTutorialLandingPaths(): string[] {
 	const paths = []
 
 	// build all product tutorial landing paths
-	activeProductSlugs.forEach((productSlug) => {
+	activeProductSlugs.forEach((productSlug: ProductSlug) => {
 		paths.push(`/${productSlug}/tutorials`)
 	})
 
-	// build all collection paths
-
-	// build all tutorial paths
-
-	return paths
-}
-
-// for waf and onboarding routes that are top level
-function getSectionTutorialPathsToRevalidate() {
-	const paths = []
-
-	// build all product tutorial landing paths
+	// for waf and onboarding routes that are top level
 	Object.values(SectionOption).forEach((slug: SectionOption) => {
 		paths.push(`/${slug}`)
 	})
 
-	// build all collection paths
+	return paths
+}
 
-	// build all tutorial paths
+async function getCollectionAndTutorialPaths() {
+	const paths = []
+
+	const allCollections = await cachedGetAllCollections()
+
+	allCollections.forEach((collection: ClientCollection) => {
+		// build collection path
+		paths.push(getCollectionSlug(collection.slug))
+
+		// build tutorial path
+		collection.tutorials.forEach((tutorial: ClientTutorialLite) => {
+			paths.push(getTutorialSlug(tutorial.slug, collection.slug))
+		})
+	})
 
 	return paths
 }
 
 /**
  * write as out file to test the output
+ *
+ *  TODO check on the cloud / theme handling
  */
-
-// tutorial.featured_collections.map((collection: ApiFeaturedCollection) => {
-// 	// add individual tutorial path per collection
-// 	paths.push(getTutorialSlug(tutorial.slug, collection.slug))
-
-// 	// add each collection path
-// 	paths.push(getCollectionSlug(collection.slug))
-// })
-
-// // add product tutorial landing paths for each product used
-// tutorial.products_used.map((product: ApiProductsUsed) =>
-// 	paths.push(`/${product.product.slug}/tutorials`)
-// )
