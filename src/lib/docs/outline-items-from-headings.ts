53fc3b4b822acd7b036ada6541ef3f079ea3cfd1
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2023-02-27 10:20:14 -0500

    chore: extract outline-from-headings util & type

---
+++ src/lib/docs/outline-items-from-headings.ts
@@ -0,0 +1,30 @@
+import { OutlineLinkItem } from 'components/outline-nav/types'
+
+/**
+ * Heading type coming from our remark anchor-links plugin.
+ */
+export interface AnchorLinksPluginHeading {
+	title: string
+	slug: string
+	level: 1 | 2 | 3 | 4 | 5 | 6
+}
+
+/**
+ * Given a flat array of headings,
+ * Return an array of outline items, for use with OutlineNav components.
+ */
+function outlineItemsFromHeadings(
+	headings: AnchorLinksPluginHeading[]
+): OutlineLinkItem[] {
+	return headings.map((heading: AnchorLinksPluginHeading, index: number) => {
+		/* TODO: maybe consider resolving the backticks issue closer to server? */
+		const titleWithoutBackticks = heading.title.replace(/`/g, '')
+		return {
+			title: titleWithoutBackticks,
+			url: `#${heading.slug}`,
+			dataHeapTrack: `toc-list-item-index-${index}`,
+		}
+	})
+}
+
+export default outlineItemsFromHeadings
