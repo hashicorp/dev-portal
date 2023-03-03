eecd43813e939c80427845cd32fec3bd873f4459
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2023-03-03 10:50:21 -0500

    docs: add comment on cross-view style import

--- src/views/product-root-docs-path-landing/index.tsx
+++ src/views/product-root-docs-path-landing/index.tsx
@@ -38,6 +38,14 @@ const ProductRootDocsPathLanding = ({
 		const classes = classNames(
 			s[`${product.slug}MDXWrapper`],
 			s.mdxSlotWrapper,
+			/**
+			 * TODO: docsViewStyles.mdxContent might make sense to apply to
+			 * DevDotContent, since the related concerns (iframe overflow, and
+			 * removing the margin off the first item) seem to come up everywhere
+			 * that DevDotContent is used.
+			 * Task:
+			 * https://app.asana.com/0/1202097197789424/1204107433878314/f
+			 */
 			docsViewStyles.mdxContent
 		)
 		mdxSlot = (
