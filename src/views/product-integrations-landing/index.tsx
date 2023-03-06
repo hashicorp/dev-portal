a6f62be2874a47518eb120c461afcca559426dd2
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2023-03-06 18:45:16 -0500

    Ordering imports and alphabetizing props

--- src/views/product-integrations-landing/index.tsx
+++ src/views/product-integrations-landing/index.tsx
@@ -3,41 +3,40 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
-import s from './style.module.css'
-import SearchableIntegrationsList from './components/searchable-integrations-list'
-import { IntegrationsSearchProvider } from './contexts/integrations-search-context'
-import { type Integration } from 'lib/integrations-api-client/integration'
-
-import SidebarSidecarLayout from 'layouts/sidebar-sidecar'
-import { type SidebarProps } from 'components/sidebar'
-import { type BreadcrumbLink } from 'components/breadcrumb-bar'
 import { ProductData } from 'types/products'
+import { type Integration } from 'lib/integrations-api-client/integration'
+import SidebarSidecarLayout from 'layouts/sidebar-sidecar'
+import { type BreadcrumbLink } from 'components/breadcrumb-bar'
+import { type SidebarProps } from 'components/sidebar'
 import { BrandedHeaderCard } from './components/branded-header-card'
+import { IntegrationsSearchProvider } from './contexts/integrations-search-context'
+import SearchableIntegrationsList from './components/searchable-integrations-list'
+import s from './style.module.css'
 
 export interface ViewProps {
-	integrations: Array<Integration>
-	sidebarNavDataLevels: Array<SidebarProps>
 	breadcrumbLinks: Array<BreadcrumbLink>
+	integrations: Array<Integration>
 	product: ProductData
+	sidebarNavDataLevels: Array<SidebarProps>
 }
 
 export default function ProductIntegrationsLanding({
-	integrations,
-	sidebarNavDataLevels,
 	breadcrumbLinks,
+	integrations,
 	product,
+	sidebarNavDataLevels,
 }: ViewProps) {
 	return (
 		<IntegrationsSearchProvider integrations={integrations}>
 			<SidebarSidecarLayout
-				sidebarNavDataLevels={sidebarNavDataLevels}
 				breadcrumbLinks={breadcrumbLinks}
+				sidebarNavDataLevels={sidebarNavDataLevels}
 			>
 				<div className={s.mainArea}>
 					<BrandedHeaderCard
-						productSlug={product.slug}
-						heading={`${product.name} Integrations`}
 						description={product.integrationsConfig.description}
+						heading={`${product.name} Integrations`}
+						productSlug={product.slug}
 					/>
 					<SearchableIntegrationsList className={s.searchList} />
 				</div>
