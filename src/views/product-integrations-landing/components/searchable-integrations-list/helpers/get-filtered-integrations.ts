0de513a2f8f52bef713642722a44306ef3ad4a77
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2023-03-06 22:41:22 -0500

    Abstracting and exporting getDoesMatchFilterQuery

--- src/views/product-integrations-landing/components/searchable-integrations-list/helpers/get-filtered-integrations.ts
+++ src/views/product-integrations-landing/components/searchable-integrations-list/helpers/get-filtered-integrations.ts
@@ -5,6 +5,22 @@
 
 import { Integration } from 'lib/integrations-api-client/integration'
 
+const getDoesMatchFilterQuery = ({
+	integration,
+	filterQuery,
+}: {
+	integration: Integration
+	filterQuery: string
+}) => {
+	return (
+		integration.name.toLowerCase().includes(filterQuery.toLowerCase()) ||
+		integration.description.toLowerCase().includes(filterQuery.toLowerCase()) ||
+		integration.organization.slug
+			.toLowerCase()
+			.includes(filterQuery.toLowerCase())
+	)
+}
+
 const getFilteredIntegrations = ({
 	integrations,
 	filterQuery,
@@ -13,16 +29,8 @@ const getFilteredIntegrations = ({
 	filterQuery: string
 }) => {
 	return integrations.filter((integration: Integration) => {
-		return (
-			integration.name.toLowerCase().includes(filterQuery.toLowerCase()) ||
-			integration.description
-				.toLowerCase()
-				.includes(filterQuery.toLowerCase()) ||
-			integration.organization.slug
-				.toLowerCase()
-				.includes(filterQuery.toLowerCase())
-		)
+		return getDoesMatchFilterQuery({ integration, filterQuery })
 	})
 }
 
-export { getFilteredIntegrations }
+export { getDoesMatchFilterQuery, getFilteredIntegrations }
