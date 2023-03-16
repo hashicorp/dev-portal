1cec8b1e379e32010341912221d6455433f846a1
Kendall Strautman <kendallstrautman@gmail.com>
2023-03-16 13:25:55 -0700

    chore: update comment

--- src/components/themed-image/index.tsx
+++ src/components/themed-image/index.tsx
@@ -1,11 +1,20 @@
 import { ThemedImageProps } from './types'
 
 /**
+ * This component can be used in views where we need to
+ * conditionally render an image based on the theme state.
+ *
  * See styles/themes/global.css for the attribute handler
  * that uses `display:none` to conditionally render content
  * based on the theme state with css
  */
 export default function ThemedImage({ src, alt = '' }: ThemedImageProps) {
+	/**
+	 * @TODO could add protection here to throw
+	 * when used outside of the theme context. Since the
+	 * entire app is wrapped with the provier when using the
+	 * core devdot layout, it's currently low-risk
+	 * */
 	return (
 		<>
 			{/* When the theme is dark, hide this div */}
