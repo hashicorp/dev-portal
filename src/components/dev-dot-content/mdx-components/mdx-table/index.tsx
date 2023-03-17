ff2d99c44adf5433a24cd99971f06b0766277878
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2023-03-17 17:44:19 -0400

    Adding focus ring for table

--- src/components/dev-dot-content/mdx-components/mdx-table/index.tsx
+++ src/components/dev-dot-content/mdx-components/mdx-table/index.tsx
@@ -6,13 +6,17 @@
 import s from './mdx-table.module.css'
 
 /**
- * Lightweight wrapper around the native table element to encapsulate styles and enable horizontal overflow scrolling
- * along with full-width tables.
+ * Lightweight wrapper around the native <table> element. Encapsulates styles,
+ * enables scrolling when tables are too tall or wide, and uses `tabindex="0"`
+ * to enable keyboard-only users to access the overflowed content.
  */
-export function MdxTable(props) {
+export function MdxTable(props: JSX.IntrinsicElements['table']) {
 	return (
-		<div className={s.tableWrapper}>
-			<table {...props} />
+		<div className={s.root}>
+			<div className={s.tableWrapper} tabIndex={0}>
+				<table {...props} />
+			</div>
+			<div className={s.tableFocusRing} />
 		</div>
 	)
 }
