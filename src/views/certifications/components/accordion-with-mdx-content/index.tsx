24d0b2e935b31ea1c2d013bdad20f64cda96811a
Kendall Strautman <kendallstrautman@gmail.com>
2023-03-28 10:46:36 -0700

    spike: adds themed image for certifications

--- src/views/certifications/components/accordion-with-mdx-content/index.tsx
+++ src/views/certifications/components/accordion-with-mdx-content/index.tsx
@@ -2,13 +2,38 @@
  * Copyright (c) HashiCorp, Inc.
  * SPDX-License-Identifier: MPL-2.0
  */
-
+import NextImage from 'next/image'
 import Accordion from 'components/accordion'
 import DevDotContent from 'components/dev-dot-content'
 import Image from 'components/image'
 import { ImageProps } from 'components/image/types'
 import { AccordionWithMdxContentProps, AccordionMdxItem } from './types'
 import s from './accordion-with-mdx-content.module.css'
+
+function ThemedMdxImage({ alt, src, title, width, height }) {
+	return (
+		<>
+			<span data-hide-on-theme="dark">
+				<NextImage
+					src={src.light}
+					alt={alt}
+					title={title}
+					width={width}
+					height={height}
+				/>
+			</span>
+			<span data-hide-on-theme="light">
+				<NextImage
+					src={src.dark}
+					alt={alt}
+					title={title}
+					width={width}
+					height={height}
+				/>
+			</span>
+		</>
+	)
+}
 
 function MdxImage({
 	alt,
@@ -20,6 +45,7 @@ function MdxImage({
 
 const MDX_COMPONENTS = {
 	img: MdxImage,
+	ThemedImage: ThemedMdxImage,
 }
 
 export function AccordionWithMdxContent({
