95797eaa6b49c15f0d83c2227291b1def8389987
Kendall Strautman <kendallstrautman@gmail.com>
2023-03-30 11:14:57 -0700

    style out with icons

--- src/components/theme-switch/index.tsx
+++ src/components/theme-switch/index.tsx
@@ -4,6 +4,9 @@
  */
 
 import { IconCaret16 } from '@hashicorp/flight-icons/svg-react/caret-16'
+import { IconMonitor16 } from '@hashicorp/flight-icons/svg-react/monitor-16'
+import { IconMoon16 } from '@hashicorp/flight-icons/svg-react/moon-16'
+import { IconSun16 } from '@hashicorp/flight-icons/svg-react/sun-16'
 import { useState, useEffect } from 'react'
 import { useTheme } from 'next-themes'
 import s from './theme-switch.module.css'
@@ -15,6 +18,12 @@ import s from './theme-switch.module.css'
 
 interface ThemeSelectProps {
 	labelledById?: string
+}
+
+const themeIcons = {
+	system: <IconMonitor16 />,
+	dark: <IconMoon16 />,
+	light: <IconSun16 />,
 }
 
 export default function ThemeSelect({ labelledById }: ThemeSelectProps) {
@@ -33,9 +42,7 @@ export default function ThemeSelect({ labelledById }: ThemeSelectProps) {
 
 	return (
 		<div className={s.root}>
-			<span className={s.productIcon}>
-				<IconCaret16 />
-			</span>
+			<span className={s.productIcon}>{themeIcons[theme]}</span>
 			<select
 				className={s.select}
 				id={labelledById}
