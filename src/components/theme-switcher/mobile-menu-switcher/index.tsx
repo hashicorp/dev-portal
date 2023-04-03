637e954d54149a05932365bcd33da5f48c7dd24c
Kendall Strautman <36613477+kendallstrautman@users.noreply.github.com>
2023-04-03 14:01:22 -0700

    Update src/components/theme-switcher/mobile-menu-switcher/index.tsx

--- src/components/theme-switcher/mobile-menu-switcher/index.tsx
+++ src/components/theme-switcher/mobile-menu-switcher/index.tsx
@@ -2,7 +2,7 @@ import ThemeSwitcher from 'components/theme-switcher'
 import s from './mobile-menu-switcher.module.css'
 
 export default function MobileMenuThemeSwitcher() {
-	const switcherId = 'theme-switcher-mobile-menu'
+	const switcherId = 'theme-switcher-unauthenticated-mobile-menu'
 	return (
 		<div className={s.root}>
 			<label className={s.label} htmlFor={switcherId}>
