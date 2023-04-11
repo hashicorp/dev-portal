6f48fc1193ffbc388b92364612a47aa1ec94522f
Kendall Strautman <kendallstrautman@gmail.com>
2023-04-11 14:25:41 -0700

    refactor to use array.includes

--- src/lib/isThemedPath.ts
+++ src/lib/isThemedPath.ts
@@ -1,7 +1,3 @@
 export default function isThemedPath(currentPath: string): boolean {
-	return (
-		__config.dev_dot.non_themed_paths.findIndex(
-			(nonThemedPath: string) => nonThemedPath === currentPath
-		) === -1
-	)
+	return !__config.dev_dot.non_themed_paths.includes(currentPath)
 }
