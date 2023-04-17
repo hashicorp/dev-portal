a388f2b85437f42bcf0a45c095b5a9669edb8a9f
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2023-04-17 09:17:16 -0400

    chore: fix up TSFixMe type

--- src/views/product-root-docs-path-landing/components/hero/types.ts
+++ src/views/product-root-docs-path-landing/components/hero/types.ts
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
+import { VersionSelectItem } from '@hashicorp/react-docs-page/server/loaders/remote-content'
 import {
 	IconCardGridItem,
 	ProductRootDocsPathLandingProps,
@@ -12,6 +13,6 @@ export interface ProductRootDocsPathLandingHeroProps {
 	pageHeading: ProductRootDocsPathLandingProps['pageHeading']
 	pageSubtitle: string
 	iconCardGridItems?: IconCardGridItem[]
-	versions: $TSFixMe
+	versions: VersionSelectItem[]
 	projectName?: string
 }
