ef366d811d4cc58aabc294dd18bdda9487b47191
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2023-04-26 20:19:21 -0400

    Adding note about react-aria/useMove

--- src/hooks/use-on-swipe.ts
+++ src/hooks/use-on-swipe.ts
@@ -13,6 +13,12 @@ const DEFAULT_CLIENT_X_DIFF_THRESHOLD = 20
  * Given a ref associated with a swipeable HTML element and callback functions
  * swiping left and swiping right, invokes the appropriate callback when a swipe
  * is detected.
+ *
+ * Note: if we find the need to listen for more than touch events in the future,
+ * React Aria's useMove hook may be something we can leverage. At the time of
+ * writing, we do not yet have a need for all of its functionality.
+ *
+ * https://react-spectrum.adobe.com/react-aria/useMove.html
  */
 const useOnSwipe = ({
 	diffThreshold = DEFAULT_CLIENT_X_DIFF_THRESHOLD,
