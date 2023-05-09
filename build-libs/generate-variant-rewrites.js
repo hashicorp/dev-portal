e8d26838f8586aa29228b093230a858ec2c23325
Kendall Strautman <kendallstrautman@gmail.com>
2023-05-09 12:58:54 -0700

    cleanup generate rewrites function

--- build-libs/generate-variant-rewrites.js
+++ build-libs/generate-variant-rewrites.js
@@ -4,29 +4,6 @@ const variantData = require('../src/content/variants.json')
 
 const MAX_LIMIT = '100' // Defaults to API max
 
-function get(path) {
-	const options = {
-		method: 'GET',
-	}
-
-	return fetch(`${process.env.NEXT_PUBLIC_LEARN_API_BASE_URL}${path}`, options)
-}
-
-async function toError(errorResponse) {
-	let json
-	try {
-		json = await errorResponse.json()
-	} catch (err) {
-		// Do nothing if the response is not json
-	}
-
-	return new Error(
-		`${errorResponse.status} ${errorResponse.statusText}${
-			json?.error ? ` - ${json.error}` : ''
-		}`
-	)
-}
-
 async function fetchAll({ after, fetchedTutorials }) {
 	// Set the base array if it's the first call
 	if (typeof fetchedTutorials === 'undefined') {
@@ -68,16 +45,23 @@ async function fetchTutorials(after) {
 
 	const queryStr = `?${params.toString()}`
 	const route = '/tutorials' + queryStr
-	const response = await get(route)
+	const response = await fetch(
+		`${process.env.NEXT_PUBLIC_LEARN_API_BASE_URL}${route}`,
+		{
+			method: 'GET',
+		}
+	)
 
 	if (response.ok) {
 		return response
+	} else {
+		return new Error(`${response.status} ${response.statusText}`)
 	}
-
-	const error = await toError(response)
-	throw error
 }
 
+// use /:product/tutorials/:collection/tutorial?variants="slug:optionSlug"
+// with cookie
+// to /:product/tutorials/:collection/tutorial/{slug:optionSlug}
 async function getVariantRewrites() {
 	// get all tutorial paths with variants
 	const allTutorials = await fetchAll({})
@@ -87,31 +71,17 @@ async function getVariantRewrites() {
 		Boolean(tutorial.variant)
 	)
 
-	console.log(tutorialVariants.length, 'VARIANTS')
-
 	// build each path with the correct cookie
 	return tutorialVariants.flatMap((tutorial) => {
 		const variant = variantData[tutorial.variant]
-		console.log({ variant })
 		const tutorialFilename = tutorial.slug.split('/')[1]
 
 		return variant.map((variantOption) => ({
 			source: `/:product/tutorials/:collection/${tutorialFilename}`,
 			destination: `/:product/tutorials/:collection/${tutorialFilename}/${variantOption.id}`,
-			has: [
-				{ type: 'cookie', key: tutorial.variant, value: variantOption.id },
-				// {
-				// 	type: 'query',
-				// 	key: 'variant',
-				// 	value: variantOption.id,
-				// },
-			],
+			has: [{ type: 'cookie', key: tutorial.variant, value: variantOption.id }],
 		}))
 	})
-
-	// use /:product/tutorials/:collection/tutorial?variants="slug:optionSlug"
-	// with cookie
-	// to /:product/tutorials/:collection/tutorial/{slug:optionSlug}
 }
 
 module.exports = { getVariantRewrites }
