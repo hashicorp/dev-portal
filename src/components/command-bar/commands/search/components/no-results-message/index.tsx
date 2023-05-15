d24d102a069840d70c2ada996ef624bc5c87ee3d
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2023-05-15 15:24:06 -0400

    feat: stub data in place for no results state

--- src/components/command-bar/commands/search/components/no-results-message/index.tsx
+++ src/components/command-bar/commands/search/components/no-results-message/index.tsx
@@ -4,12 +4,17 @@
  */
 
 import Text from 'components/text'
+import { useHitsContext } from '../../helpers'
 import s from './no-results-message.module.css'
 
 const NoResultsMessage = () => {
+	const [hitCounts] = useHitsContext()
 	return (
 		<Text asElement="p" className={s.root} size={300} weight="medium">
 			No results match your search.
+			<pre>
+				<code>{JSON.stringify({ hitCounts }, null, 2)}</code>
+			</pre>
 		</Text>
 	)
 }
