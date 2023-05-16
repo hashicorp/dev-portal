76a4ca5a9818a7a798bf6a328b5bf6d738fc31f0
Kendall Strautman <kendallstrautman@gmail.com>
2023-05-16 15:00:21 -0700

    refactor to use passed data

--- src/views/tutorial-view/components/variants-dropdown-disclosure/index.tsx
+++ src/views/tutorial-view/components/variants-dropdown-disclosure/index.tsx
@@ -1,44 +1,53 @@
+import { useRouter } from 'next/router'
 import DropdownDisclosure, {
 	DropdownDisclosureLinkItem,
 } from 'components/dropdown-disclosure'
 import Text from 'components/text'
 import s from './variants-dropdown-disclosure.module.css'
 
-const STUB_VARIANTS_DATA = {
-	slug: 'deployment',
-	name: 'Deployment',
-	options: [
-		{
-			slug: 'hcp',
-			name: 'Hashicorp Cloud Platform (HCP)',
-		},
-		{ slug: 'self-managed', name: 'Self Managed' },
-	],
-}
-
-function _tmpStubGetVariantPath(slug: string) {
-	return `?variants=${STUB_VARIANTS_DATA.slug}:${slug}`
-}
-
 // @TODO add aria-labelledBy
 
-export function VariantsDropdownDisclosure() {
+// @TODO lift these to bonafide util to get this path once we have real data
+function _tmpGetVariantPath(path: string, variantType: string) {
+	const url = new URL(path, 'https://developer.hashicorp.com')
+
+	// if the variant is not defined, or if it is defined in the path already, use that
+	if (!variantType || url.searchParams.get('variants') === variantType) {
+		return path
+	}
+
+	// otherwise just add the variant to the path
+	url.searchParams.set('variants', variantType)
+
+	return url.pathname.toString() + url.search.toString()
+}
+
+function getVariantParam(slug: string, optionSlug: string) {
+	return `${slug}:${optionSlug}`
+}
+
+export function VariantsDropdownDisclosure({ variant }: { variant: $TSFixMe }) {
 	// @TODO hook this into useVariants hook once data is wired
-	const activeOption = STUB_VARIANTS_DATA.options[0]
+	const { asPath } = useRouter()
+	const activeOption = variant.options[0]
+
 	return (
 		<div className={s.root}>
 			<Text weight="semibold" size={100} className={s.label}>
-				{STUB_VARIANTS_DATA.name}
+				{variant.name}
 			</Text>
 			<DropdownDisclosure
-				aria-label={STUB_VARIANTS_DATA.name}
+				aria-label={variant.name}
 				color="secondary"
 				text={activeOption.name}
 			>
-				{STUB_VARIANTS_DATA.options.map((option) => (
+				{variant.options.map((option) => (
 					<DropdownDisclosureLinkItem
 						key={option.slug}
-						href={_tmpStubGetVariantPath(option.slug)}
+						href={_tmpGetVariantPath(
+							asPath,
+							getVariantParam(variant.slug, option.slug)
+						)}
 					>
 						{option.name}
 					</DropdownDisclosureLinkItem>
