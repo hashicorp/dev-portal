0ad1a925721b7eae7643c98d5d49002d76daefea
Kevin Wang <kwangsan@gmail.com>
2023-05-16 14:30:54 -0400

    opentel

--- next.config.js
+++ next.config.js
@@ -144,6 +144,7 @@ module.exports = withSwingset({
 			contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
 		},
 		experimental: {
+			instrumentationHook: true,
 			largePageDataBytes: 512 * 1000, // 512KB
 			// TODO: not using transpilePackages here until https://github.com/vercel/next.js/pull/43546 lands
 			// transpilePackages: [
