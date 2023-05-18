80adc3050cdeb810bcb5900455ca874b13b9957a
Kendall Strautman <kendallstrautman@gmail.com>
2023-05-18 10:27:27 -0700

    adds wrapping calss

--- src/views/tutorial-view/components/variant-dropdown-disclosure/mobile/index.tsx
+++ src/views/tutorial-view/components/variant-dropdown-disclosure/mobile/index.tsx
@@ -4,14 +4,17 @@ import s from './mobile-variant-dropdown-disclosure.module.css'
 
 export function MobileVariantDropdownDisclosure({
 	variant,
+	className,
 }: VariantDropdownDisclosureProps) {
 	return (
-		<VariantDropdownDisclosure
-			variant={variant}
-			classNames={{
-				dropdownRoot: s.dropdownDisclosure,
-				dropdownActivator: s.dropdownActivator,
-			}}
-		/>
+		<div className={className}>
+			<VariantDropdownDisclosure
+				variant={variant}
+				classNames={{
+					dropdownRoot: s.dropdownDisclosure,
+					dropdownActivator: s.dropdownActivator,
+				}}
+			/>
+		</div>
 	)
 }
