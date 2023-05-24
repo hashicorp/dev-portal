ecc4f6eec39cd6253fe02ff1efa295f7575378e7
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2023-05-24 14:58:00 -0400

    Adding styles and image url options

--- src/components/tutorials-landing-view/tutorials-landing-content-card-link/index.tsx
+++ src/components/tutorials-landing-view/tutorials-landing-content-card-link/index.tsx
@@ -1,9 +1,9 @@
+import { useId } from '@react-aria/utils'
 import { IconArrowRight24 } from '@hashicorp/flight-icons/svg-react/arrow-right-24'
+import Badge from 'components/badge'
 import CardLink from 'components/card-link'
 import Tooltip from 'components/tooltip'
-import Badge from 'components/badge'
 import s from './tutorials-landing-content-card-link.module.css'
-import { useId } from 'react'
 
 type ContentCardLinkEyebrowPart = string
 
@@ -13,17 +13,21 @@ interface ContentCardLinkBadge {
 }
 
 interface ContentCardLinkProps {
+	backgroundImageUrl?: string
+	badges: ContentCardLinkBadge[]
+	description: string
+	eyebrowParts: ContentCardLinkEyebrowPart[]
+	headerImageUrl?: string
 	href: string
 	title: string
-	eyebrowParts: ContentCardLinkEyebrowPart[]
-	description: string
-	badges: ContentCardLinkBadge[]
 }
 
 const TutorialsLandingContentCardLink = ({
+	backgroundImageUrl,
 	badges,
 	description,
 	eyebrowParts,
+	headerImageUrl,
 	href,
 	title,
 }: ContentCardLinkProps) => {
@@ -31,25 +35,66 @@ const TutorialsLandingContentCardLink = ({
 
 	return (
 		<CardLink ariaLabel={title} className={s.root} href={href}>
-			<div className={s.eyebrow}>
-				{eyebrowParts.map(
-					(eyebrowPart: ContentCardLinkEyebrowPart, index: number) => (
-						// eslint-disable-next-line react/no-array-index-key
-						<span key={`${uniqueId}-eyebrowPart-${index}`}>{eyebrowPart}</span>
-					)
-				)}
-			</div>
-			<h3 className={s.title}>{title}</h3>
-			<p className={s.description}>{description}</p>
-			<ul className={s.badges}>
-				{badges.map(({ icon, label }: ContentCardLinkBadge) => (
-					<Tooltip key={`${uniqueId}-badge-${label}`} label={label}>
-						<Badge ariaLabel={label} icon={icon} size="small" />
-					</Tooltip>
-				))}
-			</ul>
-			<div className={s.icon}>
-				<IconArrowRight24 />
+			{headerImageUrl ? (
+				<div
+					className={s.headerImage}
+					style={{
+						backgroundImage: `url(${headerImageUrl})`,
+					}}
+				/>
+			) : null}
+			<div
+				className={s.content}
+				style={
+					backgroundImageUrl
+						? {
+								// TODO uncomment when there are svgs to send through
+								// background: `url(${backgroundImageUrl})`,
+								backgroundColor: 'lightgray',
+								paddingTop: 44,
+								height: '100%',
+						  }
+						: null
+				}
+			>
+				<div>
+					{eyebrowParts?.length > 0 ? (
+						<div className={s.eyebrow}>
+							{eyebrowParts.map(
+								(eyebrowPart: ContentCardLinkEyebrowPart, index: number) => (
+									<span
+										className={s.eyebrowPart}
+										// eslint-disable-next-line react/no-array-index-key
+										key={`${uniqueId}-eyebrowPart-${index}`}
+									>
+										{eyebrowPart}
+									</span>
+								)
+							)}
+						</div>
+					) : null}
+					<h3 className={s.title}>{title}</h3>
+					<p className={s.description}>{description}</p>
+				</div>
+				<div className={s.footer}>
+					{badges?.length > 0 ? (
+						<ul className={s.badgeList}>
+							{badges.map(({ icon, label }: ContentCardLinkBadge) => (
+								<li
+									key={`${uniqueId}-badge-${label}`}
+									className={s.badgeListItem}
+								>
+									<Tooltip label={label}>
+										<Badge ariaLabel={label} icon={icon} size="small" />
+									</Tooltip>
+								</li>
+							))}
+						</ul>
+					) : null}
+					<div className={s.icon}>
+						<IconArrowRight24 />
+					</div>
+				</div>
 			</div>
 		</CardLink>
 	)
