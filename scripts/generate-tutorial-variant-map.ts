c6fe5cf8311e2a60ead24ec930f84db69093b6fc
Kendall Strautman <kendallstrautman@gmail.com>
2023-05-24 11:26:34 -0700

    Refactor to use withVariants param

--- scripts/generate-tutorial-variant-map.ts
+++ scripts/generate-tutorial-variant-map.ts
@@ -1,49 +1,16 @@
 import createFetch from '@vercel/fetch'
 import path from 'path'
 import fs from 'fs'
+import {
+	ApiFeaturedCollection,
+	ApiTutorialLite,
+	ApiTutorialVariantOption,
+} from 'lib/learn-client/api/api-types'
+
 const fetch = createFetch()
 
-const MAX_LIMIT = '100' // Defaults to API max
-
-async function fetchAll({ after, fetchedTutorials }) {
-	// Set the base array if it's the first call
-	if (typeof fetchedTutorials === 'undefined') {
-		fetchedTutorials = []
-	}
-
-	const response = await fetchTutorials(after)
-
-	if ('ok' in response && response.ok) {
-		const data = await response.json()
-		const allTutorials = [...fetchedTutorials, ...data.result]
-
-		/*
-		 ** If recurse is false, we only want the # of tuts specific by the limit.
-		 ** If there's less than 100 tutorials fetched (less than the max limit),
-		 ** that means that we've fetched everything.
-		 */
-		if (data.result.length < Number(MAX_LIMIT)) {
-			return allTutorials
-		}
-
-		// Otherwise, recurse to get the next batch of tutorials
-		return fetchAll({
-			// The last ID
-			after: data.result[data.result.length - 1].id,
-			// Pass the accumulated tutorials
-			fetchedTutorials: allTutorials,
-		})
-	}
-}
-
-async function fetchTutorials(after) {
-	const limit = MAX_LIMIT
-	const params = new URLSearchParams({ limit })
-
-	if (after) {
-		params.append('after', after)
-	}
-
+async function fetchTutorialsWithVariants() {
+	const params = new URLSearchParams({ hasVariants: '1' })
 	const queryStr = `?${params.toString()}`
 	const route = '/tutorials' + queryStr
 	const response = await fetch(
@@ -54,7 +21,11 @@ async function fetchTutorials(after) {
 	)
 
 	if (response.ok) {
-		return response
+		const data = await response.json()
+		return data.result
+	} else if (response.status === 404) {
+		console.log('[generateTutorialVariantMap] 404: No variant tutorials found')
+		return null
 	} else {
 		return new Error(`${response.status} ${response.statusText}`)
 	}
@@ -76,33 +47,33 @@ async function fetchTutorials(after) {
  */
 async function getVariantRewrites() {
 	// get all tutorial paths with variants
-	const allTutorials = await fetchAll({
-		after: undefined,
-		fetchedTutorials: undefined,
-	})
-
-	// filter out those that don't have variants
-	const tutorialVariants = allTutorials.filter(
-		(tutorial) => tutorial.variants?.length > 0
-	)
+	const tutorialsWithVariants = await fetchTutorialsWithVariants()
 	const variantsObj = {}
 
+	// return early if no tutorials have variants
+	if (tutorialsWithVariants === null) {
+		return variantsObj
+	}
+
 	// for each tutorial that has variants defined
-	tutorialVariants.map((tutorial) => {
+	tutorialsWithVariants.map((tutorial: ApiTutorialLite) => {
 		const tutorialFilename = tutorial.slug.split('/')[1]
 		const variant = tutorial.variants[0] // we only support 1 variant right now
 
 		// map over the collection paths to build up each tutorial path with the variant options
-		tutorial.featured_collections.map((collection) => {
+		tutorial.featured_collections.map((collection: ApiFeaturedCollection) => {
 			const [product, collectionFilename] = collection.slug.split('/')
 			const path = `/${product}/tutorials/${collectionFilename}/${tutorialFilename}`
+			const defaultOption = variant.options.find(
+				(option: ApiTutorialVariantOption) => option.display_order === 1
+			)
 
 			variantsObj[path] = {
 				slug: variant.slug,
-				options: variant.options.map(({ slug }) => slug),
-				defaultOption: variant.options.find(
-					(option) => option.display_order === 1
+				options: variant.options.map(
+					({ slug }: ApiTutorialVariantOption) => slug
 				),
+				defaultOption: defaultOption.slug,
 			}
 		})
 	})
