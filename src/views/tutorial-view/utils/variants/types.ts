c35df9915e57f20f2051676355475933aff089dd
Kendall Strautman <36613477+kendallstrautman@users.noreply.github.com>
2023-05-26 13:42:56 -0700

    Update src/views/tutorial-view/utils/variants/types.ts

--- src/views/tutorial-view/utils/variants/types.ts
+++ src/views/tutorial-view/utils/variants/types.ts
@@ -3,7 +3,6 @@ import {
 	TutorialVariantOption as ClientTutorialVariantOption,
 } from 'lib/learn-client/types'
 
-// @TODO rename to TutorialViewVariant? Wait until rebase to avoid conflicts
 
 export interface TutorialVariant extends ClientTutorialVariant {
 	activeOption: TutorialVariantOption
