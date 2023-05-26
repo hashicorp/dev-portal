da2501206f4c75e5485bb206005bde7286ebccd5
Kendall Strautman <36613477+kendallstrautman@users.noreply.github.com>
2023-05-26 13:38:36 -0700

    Update src/views/tutorial-view/components/variant-dropdown-disclosure/types.ts

--- src/views/tutorial-view/components/variant-dropdown-disclosure/types.ts
+++ src/views/tutorial-view/components/variant-dropdown-disclosure/types.ts
@@ -2,9 +2,5 @@ import { TutorialVariant } from 'views/tutorial-view/utils/variants/types'
 
 export interface VariantDropdownDisclosureProps {
 	variant: TutorialVariant
-	classNames?: {
-		dropdownRoot: string
-		dropdownActivator: string
-	}
 	isFullWidth?: boolean
 }
