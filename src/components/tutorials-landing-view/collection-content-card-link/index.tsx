41eaf1099a2ce39c565b26af0fffa0f1222929ec
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2023-05-30 13:54:45 -0400

    Cleaning up types

--- src/components/tutorials-landing-view/collection-content-card-link/index.tsx
+++ src/components/tutorials-landing-view/collection-content-card-link/index.tsx
@@ -1,3 +1,4 @@
+import { ProductUsed, TutorialLite } from 'lib/learn-client/types'
 import { getCollectionSlug } from 'views/collection-view/helpers'
 import { type CollectionContentCardLinkProps } from '../types'
 import ContentCardLink from '../content-card-link'
@@ -22,20 +23,20 @@ const CollectionContentCardLink = ({
 	let hasLab = false
 	const productsUsed = new Set([productSlug])
 
-	collection.tutorials.forEach((tutorial) => {
+	collection.tutorials.forEach((tutorial: TutorialLite) => {
 		if (!hasVideo && Boolean(tutorial.video)) {
 			hasVideo = true
 		}
 		if (!hasLab && Boolean(tutorial.handsOnLab)) {
 			hasLab = true
 		}
-		tutorial.productsUsed.forEach((productUsed) => {
+		tutorial.productsUsed.forEach((productUsed: ProductUsed) => {
 			productsUsed.add(productUsed.product.slug)
 		})
 	})
 
 	const badges = []
-	productsUsed.forEach((productUsed) =>
+	productsUsed.forEach((productUsed: ProductUsed['product']['slug']) =>
 		badges.push(BADGE_ICON_MAP[productUsed])
 	)
 	if (hasVideo) {
