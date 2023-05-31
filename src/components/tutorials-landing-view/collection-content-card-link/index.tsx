265a3bb78fe2bf92064e3a73fea720ad85de4001
Ashlee M Boyer <43934258+ashleemboyer@users.noreply.github.com>
2023-05-30 19:04:41 -0500

    [Tutorials Front Door] ContentCardLink, CollectionContentCardLink, CertificationContentCardLink (#1957)
    * Stubbing TutorialsLandingContentCardLink
    
    * Adding styles and image url options
    
    * Updating backgroundImageUrl usage
    
    * Fixing Badge size
    
    * Updating Swingset page markup
    
    * Updating CSS background properties
    
    * Adding IconPlay16 and IconTerminalScreen16
    
    * Adding hasHeaderImage class
    
    * Adding TutorialsLandingCollectionContentCardLink
    
    * Handling additional color and padding styles
    
    * Adding TutorialsLandingCertificationContentCardLink
    
    * Adding collection and certification card examples
    
    * Moving ContentCardLink types to new file
    
    * Adding CollectionContentCardLinkProps
    
    * Adding CertificationContentCardLinkProps
    
    * Renaming subcomponents and folders
    
    * Removing ContentCardLink example
    
    * Adding dev toast for ambiguous args
    
    * Removing usage of useId
    
    * Fixing last parts of renamign
    
    * Moving BADGE_ICON_MAP
    
    * Passing collection to CollectionContentCardLink
    
    * Cleaning up types
    
    * Removing CollectionContentCardLinkProps['productSlug']
    
    * Fixing pluralization
    
    * Normalizing productSlug

---
+++ src/components/tutorials-landing-view/collection-content-card-link/index.tsx
@@ -0,0 +1,62 @@
+import { ProductUsed, TutorialLite } from 'lib/learn-client/types'
+import { normalizeSlugForDevDot } from 'lib/tutorials/normalize-product-like-slug'
+import { getCollectionSlug } from 'views/collection-view/helpers'
+import { type CollectionContentCardLinkProps } from '../types'
+import ContentCardLink from '../content-card-link'
+import { BADGE_ICON_MAP, PRODUCT_SLUGS_TO_HEADER_IMAGES } from './constants'
+
+const CollectionContentCardLink = ({
+	collection,
+}: CollectionContentCardLinkProps) => {
+	const productSlug = normalizeSlugForDevDot(collection.slug.split('/')[0])
+	const title = collection.name
+	const description = collection.description
+	const headerImageUrl = PRODUCT_SLUGS_TO_HEADER_IMAGES[productSlug]
+	const href = getCollectionSlug(collection.slug)
+
+	const tutorialCount = collection.tutorials.length
+	const eyebrowParts = [
+		'Learning path',
+		`${tutorialCount} tutorial${tutorialCount != 1 ? 's' : ''}`,
+	]
+
+	let hasVideo = false
+	let hasLab = false
+	const productsUsed = new Set([productSlug])
+
+	collection.tutorials.forEach((tutorial: TutorialLite) => {
+		if (!hasVideo && Boolean(tutorial.video)) {
+			hasVideo = true
+		}
+		if (!hasLab && Boolean(tutorial.handsOnLab)) {
+			hasLab = true
+		}
+		tutorial.productsUsed.forEach((productUsed: ProductUsed) => {
+			productsUsed.add(productUsed.product.slug)
+		})
+	})
+
+	const badges = []
+	productsUsed.forEach((productUsed: ProductUsed['product']['slug']) =>
+		badges.push(BADGE_ICON_MAP[productUsed])
+	)
+	if (hasVideo) {
+		badges.push(BADGE_ICON_MAP.video)
+	}
+	if (hasLab) {
+		badges.push(BADGE_ICON_MAP.interactive)
+	}
+
+	return (
+		<ContentCardLink
+			badges={badges}
+			description={description}
+			headerImageUrl={headerImageUrl}
+			href={href}
+			title={title}
+			eyebrowParts={eyebrowParts}
+		/>
+	)
+}
+
+export default CollectionContentCardLink
