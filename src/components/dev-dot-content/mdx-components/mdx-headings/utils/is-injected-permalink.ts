e9ad7037f1b87ce3eab7cf43c98ed061afb2b11f
Kevin Wang <kwangsan@gmail.com>
2023-06-01 10:08:48 -0400

    chore: update type for Next.js upgrade - this gets ahead of a breaking type, observed when updating `next`

--- src/components/dev-dot-content/mdx-components/mdx-headings/utils/is-injected-permalink.ts
+++ src/components/dev-dot-content/mdx-components/mdx-headings/utils/is-injected-permalink.ts
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
-import { ReactChild, ReactFragment, ReactPortal } from 'react'
+import { Children } from 'react'
 
 /**
  * Given a React child, determine if it a permalink element injected by our
@@ -16,7 +16,7 @@ import { ReactChild, ReactFragment, ReactPortal } from 'react'
  * Ref: https://github.com/hashicorp/remark-plugins/blob/d7869b253b3ae07116f280f95fe2b414145594f1/plugins/anchor-links/index.js#L155
  */
 export function isInjectedPermalink(
-	child: ReactChild | ReactFragment | ReactPortal
+	child: ReturnType<typeof Children.toArray>[number]
 ): boolean {
 	const isLiteral = typeof child === 'string' || typeof child === 'number'
 	if (isLiteral) {
