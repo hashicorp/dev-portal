06350cb70e872f4919e746b6045226061c0fc245
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2023-06-02 17:37:33 -0400

    chore: flag and placeholder for unified search

---
+++ src/components/command-bar/commands/search/unified-search/components/dialog-body/index.tsx
@@ -0,0 +1,9 @@
+function UnifiedSearchCommandBarDialogBody() {
+	return (
+		<div style={{ border: '2px solid magenta' }}>
+			Unified search results pane will go here.
+		</div>
+	)
+}
+
+export { UnifiedSearchCommandBarDialogBody }
