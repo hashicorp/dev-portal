3eb676ab4caa6438767f1201397d3884893b1ddf
Kendall Strautman <kendallstrautman@gmail.com>
2023-06-22 15:08:02 -0700

    adds tests for overriding width / height

--- src/lib/remark-plugins/remark-image-dimensions/__tests__/index.test.tsx
+++ src/lib/remark-plugins/remark-image-dimensions/__tests__/index.test.tsx
@@ -37,6 +37,20 @@ const sourceWithDifferntProps = `
 />
 `
 
+const sourceWithWidthHeightOverride = `
+# Hello
+
+<ThemedImage
+    src={{
+        light: 'img/boundary/boundary-desktop-cluster-url.png',
+        dark: 'img/boundary/boundary-components-min.png'
+    }}
+    alt='themed image'
+    width='500'
+    height='300'
+/>
+`
+
 describe('themed image dimensions remark plugin', () => {
 	it('should rewrite src urls, width, height for previews', async () => {
 		process.env.VERCEL_ENV = 'preview'
@@ -200,16 +214,67 @@ describe('themed image dimensions remark plugin', () => {
 		</div>
 	`)
 	})
+
+	it('should allow width / height to be overridden via props', async () => {
+		process.env.VERCEL_ENV = 'preview'
+
+		const mdxSource = await serialize(sourceWithWidthHeightOverride, {
+			mdxOptions: {
+				remarkPlugins: [remarkPluginCalculateImageDimensions],
+			},
+		})
+
+		const { container } = render(
+			<MDXRemote {...mdxSource} components={{ ThemedImage: MdxThemedImage }} />
+		)
+
+		// we expect to fallback to the plain `img` tag, without width and height defined
+		expect(container).toMatchInlineSnapshot(`
+		<div>
+		  <h1>
+		    Hello
+		  </h1>
+		  <span
+		    class="root"
+		    data-hide-on-theme="dark"
+		  >
+		    <img
+		      alt="themed image"
+		      class="image"
+		      data-nimg="1"
+		      decoding="async"
+		      height="300"
+		      loading="lazy"
+		      src="/_next/image?url=https%3A%2F%2Fcontent.hashicorp.com%2Fapi%2Fassets%3Fproduct%3Dtutorials%26version%3Dmain%26asset%3Dpublic%252Fimg%252Fboundary%252Fboundary-desktop-cluster-url.png&w=1080&q=75"
+		      srcset="/_next/image?url=https%3A%2F%2Fcontent.hashicorp.com%2Fapi%2Fassets%3Fproduct%3Dtutorials%26version%3Dmain%26asset%3Dpublic%252Fimg%252Fboundary%252Fboundary-desktop-cluster-url.png&w=640&q=75 1x, /_next/image?url=https%3A%2F%2Fcontent.hashicorp.com%2Fapi%2Fassets%3Fproduct%3Dtutorials%26version%3Dmain%26asset%3Dpublic%252Fimg%252Fboundary%252Fboundary-desktop-cluster-url.png&w=1080&q=75 2x"
+		      style="color: transparent;"
+		      width="500"
+		    />
+		  </span>
+		  <span
+		    class="root"
+		    data-hide-on-theme="light"
+		  >
+		    <img
+		      alt="themed image"
+		      class="image"
+		      data-nimg="1"
+		      decoding="async"
+		      height="300"
+		      loading="lazy"
+		      src="/_next/image?url=https%3A%2F%2Fcontent.hashicorp.com%2Fapi%2Fassets%3Fproduct%3Dtutorials%26version%3Dmain%26asset%3Dpublic%252Fimg%252Fboundary%252Fboundary-components-min.png&w=1080&q=75"
+		      srcset="/_next/image?url=https%3A%2F%2Fcontent.hashicorp.com%2Fapi%2Fassets%3Fproduct%3Dtutorials%26version%3Dmain%26asset%3Dpublic%252Fimg%252Fboundary%252Fboundary-components-min.png&w=640&q=75 1x, /_next/image?url=https%3A%2F%2Fcontent.hashicorp.com%2Fapi%2Fassets%3Fproduct%3Dtutorials%26version%3Dmain%26asset%3Dpublic%252Fimg%252Fboundary%252Fboundary-components-min.png&w=1080&q=75 2x"
+		      style="color: transparent;"
+		      width="500"
+		    />
+		  </span>
+		</div>
+	`)
+	})
 })
 
 /** *
  * test if THEMED IMAGE JSX
  *
- * test if light and dark are swapped
- *
- * test if asset 404s
- *
  * test various tabs / spacing setups
- *
- * test if width /height are passed that it overrides
  */
