c27fa08071ae4eb537e2a7012c77069fdbdfac52
Kendall Strautman <kendallstrautman@gmail.com>
2023-06-26 18:47:45 -0700

    adjust tests

--- src/components/image/index.test.tsx
+++ src/components/image/index.test.tsx
@@ -4,59 +4,35 @@
  */
 
 import { render } from '@testing-library/react'
-
-import { rewriteStaticAssetsPlugin } from 'lib/remark-plugins/rewrite-static-tutorials-assets'
-
-import { MDXRemote } from 'next-mdx-remote'
-import { serialize } from 'next-mdx-remote/serialize'
 import Image from 'components/image'
 
-// FIXTURES -----------------------------------------
-
-const source = `
-# Hello
-
-How goes it
-
-![dark theme](/img/themed/dark-test.png#hide-on-light)
-![light theme](/img/themed/light-test.png#hide-on-dark)
-
-![another image](/img/something.jpg)
-`
-
-const MDX_COMPONENTS = { img: Image as any }
-const SERIALIZE_OPTIONS = {
-	mdxOptions: {
-		remarkPlugins: [rewriteStaticAssetsPlugin],
-	},
-}
-
-// ASSERTIONS -----------------------------------------------------------
-
 describe('image component', () => {
-	it('should add data-hide-on-theme attribute when theme hash is passed', async () => {
-		const mdxSource = await serialize(source, SERIALIZE_OPTIONS)
-		const { container } = render(
-			<MDXRemote {...mdxSource} components={MDX_COMPONENTS} />
+	it('adds a `data-hide-on-theme` attribute when hash is passed', async () => {
+		const { getByAltText } = render(
+			<Image
+				src="/img/themed/dark-test.png#hide-on-light"
+				alt="dark theme test"
+			/>
 		)
+		const image = getByAltText('dark theme test')
+		expect(image.parentElement).toHaveAttribute('data-hide-on-theme', 'light')
+	})
 
-		expect(
-			container.querySelector('[data-hide-on-theme="dark"]')
-		).toBeInTheDocument()
-		expect(
-			container.querySelector('[data-hide-on-theme="light"]')
-		).toBeInTheDocument()
+	it('only matches the `hide-on-{theme}` hash pattern', async () => {
+		const { getByAltText } = render(
+			<Image src="/img/themed/dark-test.png#bora-bora-theme" alt="test" />
+		)
+		const image = getByAltText('test')
+		expect(image.parentElement).not.toHaveAttribute('data-hide-on-theme')
 	})
 
 	it('renders a plain image without the data-hide-on-theme attribute ', async () => {
-		const mdxSource = await serialize(source, SERIALIZE_OPTIONS)
 		const { getByAltText } = render(
-			<MDXRemote {...mdxSource} components={MDX_COMPONENTS} />
+			<Image src="/img/themed/dark-test.png" alt="plain test" />
 		)
+		const image = getByAltText('plain test')
 
-		expect(getByAltText('another image')).toBeInTheDocument()
-		expect(getByAltText('another image')).not.toHaveAttribute(
-			'data-hide-on-theme'
-		)
+		expect(image).toBeInTheDocument()
+		expect(image.parentElement).not.toHaveAttribute('data-hide-on-theme')
 	})
 })
