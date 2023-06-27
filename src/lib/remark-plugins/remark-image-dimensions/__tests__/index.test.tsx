83dc66a652374eb6fe6b60682f5ac44c014c53c2
Kendall Strautman <kendallstrautman@gmail.com>
2023-06-27 15:10:39 -0700

    source --> src

--- src/lib/remark-plugins/remark-image-dimensions/__tests__/index.test.tsx
+++ src/lib/remark-plugins/remark-image-dimensions/__tests__/index.test.tsx
@@ -16,23 +16,23 @@ jest.mock('probe-image-size', () => {
 
 describe('remarkPluginInjectImageDimensions', () => {
 	it('wont add width / height if no protocol in src', async () => {
-		const source = `/img/themed/test.png`
-		const url = await getUrlWithDimensions(source)
+		const src = `/img/themed/test.png`
+		const url = await getUrlWithDimensions(src)
 
 		expect(url).toBe(undefined)
 	})
 
 	it('adds dimensions when protocol is in src', async () => {
-		const source = `https://content.hashicorp.com/api/assets/img/themed/test-placeholder.png`
-		const url = await getUrlWithDimensions(source)
+		const src = `https://content.hashicorp.com/api/assets/img/themed/test-placeholder.png`
+		const url = await getUrlWithDimensions(src)
 
 		expect(url).toContain(`width=${probeDimensions.width}`)
 		expect(url).toContain(`height=${probeDimensions.height}`)
 	})
 
 	it('passes through hash', async () => {
-		const source = `https://content.hashicorp.com/api/assets/img/themed/test.png#hide-on-dark`
-		const url = await getUrlWithDimensions(source)
+		const src = `https://content.hashicorp.com/api/assets/img/themed/test.png#hide-on-dark`
+		const url = await getUrlWithDimensions(src)
 
 		expect(url).toContain(`width=${probeDimensions.width}`)
 		expect(url).toContain(`height=${probeDimensions.height}`)
