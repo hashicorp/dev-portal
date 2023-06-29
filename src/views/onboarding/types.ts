f8e5eaff1c3a87385857626c59dba6b81716572f
Bryce Kalow <bkalow@hashicorp.com>
2023-06-29 13:27:48 -0500

    Revert "chore: update next and platform-nextjs-plugin to latest (#2036)"
    This reverts commit 4a6bd15e0fd6c89fa685ee34164f74add14b9cfa.

--- src/views/onboarding/types.ts
+++ src/views/onboarding/types.ts
@@ -13,7 +13,7 @@ import { SidebarProps } from 'components/sidebar/types'
 import { CollectionContext, TutorialViewProps } from 'views/tutorial-view'
 import { NextPreviousProps } from 'views/tutorial-view/components'
 import { OutlineLinkItem } from 'components/outline-nav/types'
-import { MDXRemoteSerializeResult } from 'next-mdx-remote-v1'
+import { MDXRemoteSerializeResult } from 'next-mdx-remote'
 
 export interface OnboardingCollectionViewProps {
 	metadata: {
--- src/views/product-integration/component-view/components/variable-group-list/index.tsx
+++ src/views/product-integration/component-view/components/variable-group-list/index.tsx
@@ -14,7 +14,7 @@ import {
 	MdxInlineCode,
 	MdxP,
 } from 'components/dev-dot-content/mdx-components'
-import { MDXRemoteSerializeResult } from 'next-mdx-remote-v1'
+import { MDXRemoteSerializeResult } from 'next-mdx-remote'
 import s from './style.module.css'
 
 /**
