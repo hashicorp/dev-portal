2e0e82d1aa4b3d5310ee2deba9d16cade356ada1
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2023-07-10 12:39:59 -0400

    chore: move dev swagger.json upload out of the way

--- src/views/open-api-docs-demo/components/input-section/index.tsx
+++ src/views/open-api-docs-demo/components/input-section/index.tsx
@@ -1,9 +1,13 @@
+import { useState } from 'react'
+// Icons
+import { IconCode16 } from '@hashicorp/flight-icons/svg-react/code-16'
+// Components
 import Text from 'components/text'
-import AccordionDisclosure from 'components/accordion-disclosure'
 // Local
 import { FileStringInput } from '../'
 // Styles
 import s from './input-section.module.css'
+import classNames from 'classnames'
 
 /**
  * Render some basic UI to allow uploading or pasting an OpenAPI spec file.
@@ -15,26 +19,29 @@ export function InputSection({
 	fileString: string
 	setFileString: (fileString: string) => void
 }) {
+	const [shown, setShown] = useState(false)
 	return (
-		<div className={s.root}>
-			<AccordionDisclosure title="Preview your .swagger.json" initialOpen>
-				<div className={s.container}>
-					<div className={s.instructions}>
-						<Text>
-							Upload an OpenAPI .swagger.json file, or paste the file contents
-							into the text area.
-						</Text>
-						<span className={s.fileInput}>
-							<FileStringInput accept=".json" setFileString={setFileString} />
-						</span>
-					</div>
-					<textarea
-						className={s.textArea}
-						value={fileString}
-						onChange={(e) => setFileString(e.target.value)}
-					/>
+		<div className={classNames(s.root, { [s.shown]: shown })}>
+			<button className={s.hideShow} onClick={() => setShown(!shown)}>
+				<IconCode16 />
+				<Text>.swagger.json</Text>
+			</button>
+			<div className={s.container}>
+				<div className={s.instructions}>
+					<Text>
+						Upload an OpenAPI .swagger.json file, or paste the file contents
+						into the text area.
+					</Text>
+					<span className={s.fileInput}>
+						<FileStringInput accept=".json" setFileString={setFileString} />
+					</span>
 				</div>
-			</AccordionDisclosure>
+				<textarea
+					className={s.textArea}
+					value={fileString}
+					onChange={(e) => setFileString(e.target.value)}
+				/>
+			</div>
 		</div>
 	)
 }
