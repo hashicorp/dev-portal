1d6e1d8a9767210c56e464bc5cf824948d71e602
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2023-07-19 13:07:19 -0400

    feat: add OperationsSections component

--- src/views/open-api-docs-view/components/index.tsx
+++ src/views/open-api-docs-view/components/index.tsx
@@ -1,2 +1,3 @@
 export { OpenApiDocsMobileMenuLevels } from './open-api-docs-mobile-menu-levels'
 export { DevCodeBlock } from './dev-code-block'
+export { OperationSections } from './operation-sections'
