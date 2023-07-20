7f6bcb2d4c725a1ac4a18ee95c3279b5f71e5bbb
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2023-07-20 12:51:58 -0400

    feat: parse and validate OpenAPI docs data (#2092)
    * chore: stub in parse-and-validate fn
    
    * chore: enable flags
    
    * chore: install oas-normalize
    
    * refactor: refactor gSP so more data can be passed
    
    * chore: add fixture for HCP Vault Secrets
    
    * feat: use oas-normalize in parse-and-validate
    
    * chore: render placeholder data in the view
    
    * feat: load and validate schema from GitHub
    
    * chore: remove unused placeholder JSON
    
    * chore: clean up stray type
    
    * chore: install openapi-types as direct dep
    
    * style: rename targetFile to sourceFile
    
    * style: avoid export * from
    
    * docs: expand comment on de-referencing
    
    * fix: issue with sidebar-layout CSS
    
    * chore: revert flags used for dev & review

---
+++ src/views/open-api-docs-view/utils/find-latest-stable-version.ts
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import { OpenApiDocsVersionData } from '../types'
+import { sortDateVersionData } from './sort-date-version-data'
+
+/**
+ * Given an array of version data,
+ * Return the latest `stable` version.
+ *
+ * Note: only supports date-based version formats, for example "2023-01-15".
+ * We'd need to update the sort logic in order to support other formats.
+ */
+function findLatestStableVersion(
+	versionData: OpenApiDocsVersionData[]
+): OpenApiDocsVersionData {
+	// If we have exactly one version, we assume it's the latest stable version.
+	if (versionData.length === 1) {
+		return versionData[0]
+	}
+	// Sort versions in descending order
+	const versionsDescending = sortDateVersionData(versionData)
+	// Return the first 'stable' release we can find in descending versions
+	return versionsDescending.find((v) => v.releaseStage === 'stable')
+}
+
+export { findLatestStableVersion }
