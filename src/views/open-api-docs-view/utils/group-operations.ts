bfb429dd064d7f9a93ae944418ae17d5f020ef4a
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2023-07-21 10:04:07 -0400

    docs: add example and clarify group-operations

--- src/views/open-api-docs-view/utils/group-operations.ts
+++ src/views/open-api-docs-view/utils/group-operations.ts
@@ -7,6 +7,42 @@ import { truncateHcpOperationPath } from '../utils'
  *
  * Operation groups are constructed by organizing the incoming
  * operation prop objects by their paths.
+ *
+ * EXAMPLE
+ *
+ * As an example, we might start with operation paths that look like:
+ *
+ * 1. ListApps - /secrets/2023-06-13/organizations/{organization_id}/projects/{project_id}/apps
+ * 2. CreateApp - /secrets/2023-06-13/organizations/{organization_id}/projects/{project_id}/apps
+ * 3. GetApp - /secrets/2023-06-13/organizations/{organization_id}/projects/{project_id}/apps/{app_name}
+ * 4. GetAppSecret - /secrets/2023-06-13/organizations/{organization_id}/projects/{project_id}/apps/{app_name}/secrets/{secret_name}
+ * 5. CreateAwsSmSyncIntegration - /secrets/2023-06-13/organizations/{organization_id}/projects/{project_id}/sync/aws-sm
+ *
+ * We truncate these paths to remove their common prefix, leaving us with:
+ *
+ * 1. ListApps - /apps
+ * 2. CreateApp - /apps
+ * 3. GetApp - /apps/{app_name}
+ * 4. GetAppSecret - /apps/{app_name}/secrets/{secret_name}
+ * 5. CreateAwsSmSyncIntegration - /sync/aws-sm
+ *
+ * Then, we use the **first two path segments** to group the operations,
+ * yielding something like the following::
+ *
+ * {
+ *	  '/apps': {
+ *		  heading: '/apps',
+ *		  items: [ 'ListApps', 'CreateApp' ]
+ *	  },
+ *	  '/apps/{app_name}': {
+ *		  heading: '/apps/{app_name}',
+ *		  items: [ 'GetApp', 'GetAppSecret' ]
+ *	  },
+ *	  '/sync/aws-sm': {
+ *		  heading: '/sync/aws-sm',
+ *		  items: [ 'CreateAwsSmSyncIntegration' ]
+ *	  },
+ * }
  */
 export function groupOperations(
 	operationObjects: OperationProps[]
@@ -17,15 +53,14 @@ export function groupOperations(
 			o: OperationProps
 		) => {
 			const truncatedPath = truncateHcpOperationPath(o._placeholder.__path)
-			const firstPathPart = truncatedPath.split('/').slice(0, 3).join('/')
-			const pathGroupSlug = firstPathPart
-			if (!acc[pathGroupSlug]) {
-				acc[pathGroupSlug] = {
-					heading: pathGroupSlug,
+			const firstTwoSegments = truncatedPath.split('/').slice(0, 3).join('/')
+			if (!acc[firstTwoSegments]) {
+				acc[firstTwoSegments] = {
+					heading: firstTwoSegments,
 					items: [],
 				}
 			}
-			acc[pathGroupSlug].items.push(o)
+			acc[firstTwoSegments].items.push(o)
 			return acc
 		},
 		{} as Record<string, OperationGroup>
