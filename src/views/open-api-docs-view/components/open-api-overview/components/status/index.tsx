c6db7d7a4aaf70ed882a525cf7f5d016cd1a0597
Kendall Strautman <kendallstrautman@gmail.com>
2023-07-26 21:45:27 -0700

    adds note to 'status' section

--- src/views/open-api-docs-view/components/open-api-overview/components/status/index.tsx
+++ src/views/open-api-docs-view/components/open-api-overview/components/status/index.tsx
@@ -4,6 +4,12 @@ import Badge from 'components/badge'
 import StandaloneLink from 'components/standalone-link'
 import s from './status.module.css'
 
+/**
+ * Skateboard component 🛹
+ * Will need to refactor to support multiple status 'states'.
+ * Currently this only renders a 'green' state.
+ */
+
 export function Status({ text, href }: { text: string; href: string }) {
 	return (
 		<div className={s.wrapper}>
@@ -21,6 +27,7 @@ export function Status({ text, href }: { text: string; href: string }) {
 				color="secondary"
 				href={href}
 				size="small"
+				opensInNewTab
 			/>
 		</div>
 	)
