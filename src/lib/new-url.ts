7a1e238c1bd3e89bc04009eacd3d070c9ba1cfa7
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2023-07-31 08:18:59 -0400

    docs: add comment to new-url

--- src/lib/new-url.ts
+++ src/lib/new-url.ts
@@ -4,10 +4,15 @@
  *
  * This function is intended as a light wrapper around the URL
  * constructor, with the sole purpose of not requiring a domain
- * name, and instead supply a default dummy domain name,
+ * name, and instead supply a default domain name,
  * since there are several uses through our codebase where we want
  * the functionality and reliability of URL objects, but do not
  * have a meaningful domain name to supply.
+ *
+ * Note that we use `example.com` here since we do _not_ want to rely
+ * on a specific domain name value here. If a specific domain name
+ * is needed, consumers should pass that domain to the `base` argument,
+ * or use `new URL(url, base)` directly.
  */
 export default function newUrl(
 	url: string | URL,
