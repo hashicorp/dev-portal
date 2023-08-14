7fcd429534961c7ef10832e27b6bf2974d9b90bd
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2023-08-14 19:31:54 -0400

    chore: tweak open-api property details (#2113)
    * experiment: tweak spacing, drop icons
    
    * feat: implement tree-style nesting indicators
    
    * chore: schema title as fallback for description
    
    * chore: tweak spacing based on sync with brayden
    
    * chore: further tweaks
    
    * chore: revert title fallback for consistency
    
    * chore: clean up unused css
    
    * chore: add todo comment
    
    * chore: rm nested icon
    
    * chore: clean up props, add description
    
    * chore: enable flags for dev & preview
    
    * chore: add comments
    
    * docs: update comment
    
    * chore: update naming for clarity
    
    * chore: tweak padding for consistency
    
    * chore: revert dev & preview flags

--- src/views/open-api-docs-view/components/property-details/index.tsx
+++ src/views/open-api-docs-view/components/property-details/index.tsx
@@ -4,9 +4,8 @@
  */
 
 // Third party
+import type { PropsWithChildren } from 'react'
 import classNames from 'classnames'
-// HDS
-import { IconCornerDownRight16 } from '@hashicorp/flight-icons/svg-react/corner-down-right-16'
 // Components
 import { MdxInlineCode } from 'components/dev-dot-content/mdx-components'
 import Badge from 'components/badge'
@@ -14,7 +13,7 @@ import Badge from 'components/badge'
 import s from './property-details.module.css'
 
 /**
- * Types are shared by the "base" and "deeper" variations of
+ * Types are shared by the "base" and "nested" variations of
  * the property details component.
  */
 export interface PropertyDetailsProps {
@@ -24,6 +23,7 @@ export interface PropertyDetailsProps {
 	description?: string // Plain text or HTML
 	nestedProperties?: PropertyDetailsProps[]
 	depth?: number
+	isLastItem?: boolean
 }
 
 /**
@@ -39,6 +39,7 @@ export function PropertyDetails({
 	nestedProperties,
 	depth = 0,
 }: PropertyDetailsProps) {
+	const hasNestedProperties = nestedProperties?.length > 0
 	return (
 		<div className={s.baseRoot}>
 			<div className={s.baseMetaAndDescription}>
@@ -47,18 +48,29 @@ export function PropertyDetails({
 					<span className={s.baseType}>{type}</span>
 					{isRequired ? <Badge text="Required" color="highlight" /> : null}
 				</div>
-				{description ? (
-					<div
-						className={s.baseDescription}
-						dangerouslySetInnerHTML={{ __html: description }}
-					/>
-				) : null}
 			</div>
-			{nestedProperties?.length > 0 ? (
-				<ListNestedProperties
-					nestedProperties={nestedProperties}
-					depth={depth}
-				/>
+			{description || hasNestedProperties ? (
+				<div>
+					{description ? (
+						<TreeContent
+							listItemStyle={!hasNestedProperties ? 'last' : null}
+							hideIndicator={!hasNestedProperties}
+						>
+							<div
+								className={classNames(s.baseDescription, {
+									[s.hasNestedProperties]: hasNestedProperties,
+								})}
+								dangerouslySetInnerHTML={{ __html: description }}
+							/>
+						</TreeContent>
+					) : null}
+					{hasNestedProperties ? (
+						<ListNestedProperties
+							nestedProperties={nestedProperties}
+							depth={depth}
+						/>
+					) : null}
+				</div>
 			) : null}
 		</div>
 	)
@@ -73,10 +85,15 @@ function ListNestedProperties({
 }: Pick<PropertyDetailsProps, 'nestedProperties' | 'depth'>) {
 	return (
 		<ul className={s.listNestedProperties}>
-			{nestedProperties.map((nestedProperty) => {
+			{nestedProperties.map((nestedProperty, idx) => {
+				const isLastItem = idx === nestedProperties.length - 1
 				return (
 					<li key={`${nestedProperty.name}_${depth}`}>
-						<PropertyDetailsNested {...nestedProperty} depth={depth + 1} />
+						<PropertyDetailsNested
+							{...nestedProperty}
+							depth={depth + 1}
+							isLastItem={isLastItem}
+						/>
 					</li>
 				)
 			})}
@@ -96,12 +113,14 @@ function PropertyDetailsNested({
 	description,
 	nestedProperties,
 	depth = 0,
+	isLastItem,
 }: PropertyDetailsProps) {
+	const hasNestedProperties = nestedProperties?.length > 0
 	return (
-		<div className={classNames(s.nestedRoot, s[`depth-${depth}`])}>
-			<div className={s.nestedMetaAndDescription}>
+		<div>
+			{/* Meta row */}
+			<TreeContent listItemStyle={isLastItem ? 'last' : 'middle'}>
 				<div className={s.nestedMeta}>
-					<IconCornerDownRight16 className={s.nestedIcon} />
 					<div className={s.nestedNameAndType}>
 						<code className={s.nestedName}>{name}</code>
 						<span className={s.nestedType}>{type}</span>
@@ -110,21 +129,71 @@ function PropertyDetailsNested({
 						<Badge text="Required" color="highlight" size="small" />
 					) : null}
 				</div>
-				{description ? (
+			</TreeContent>
+			{/* Optional description and nested properties */}
+			{description || hasNestedProperties ? (
+				<TreeContent hideBorder={isLastItem}>
+					{description ? (
+						<TreeContent
+							listItemStyle={!hasNestedProperties ? 'last' : null}
+							hideIndicator={!hasNestedProperties}
+						>
+							<div
+								className={s.nestedDescription}
+								dangerouslySetInnerHTML={{ __html: description }}
+							/>
+						</TreeContent>
+					) : null}
+					{hasNestedProperties ? (
+						<ListNestedProperties
+							nestedProperties={nestedProperties}
+							depth={depth}
+						/>
+					) : null}
+				</TreeContent>
+			) : null}
+		</div>
+	)
+}
+
+/**
+ * Display `children` with structured tree styles, including tree markers.
+ *
+ * Accepts optional props that affect the appearance of the tree styles:
+ * - `hideBorder` - hide the tree marker, but retain spacing
+ * - `hideIndicator` - hide the tree marker, which shifts positioning,
+ *    but still retains a smaller amount of consistent left padding.
+ * - `listItemStyle` - affects the tree marker style. By default,
+ *    the marker is a vertical line that extends to the bottom of the
+ * 	  container, creating the appearance of a continuous line for
+ * 		consecutive list items. The `middle` style adds a horizontal line,
+ *    which points to the children. The `last` style curves the vertical line
+ *    and ends it by pointing to the children.
+ */
+function TreeContent({
+	children,
+	listItemStyle,
+	hideBorder,
+	hideIndicator,
+}: PropsWithChildren<{
+	listItemStyle?: 'middle' | 'last'
+	hideBorder?: boolean
+	hideIndicator?: boolean
+}>) {
+	return (
+		<div className={s.treeContent}>
+			{hideIndicator ? null : (
+				<div className={s.treeIndicatorContainer}>
 					<div
-						className={s.nestedDescription}
-						dangerouslySetInnerHTML={{ __html: description }}
-					/>
-				) : null}
-			</div>
-			{nestedProperties?.length > 0 ? (
-				<div className={s.nestedBody}>
-					<ListNestedProperties
-						nestedProperties={nestedProperties}
-						depth={depth}
+						className={classNames(s.treeIndicator, {
+							[s.isLastItem]: listItemStyle === 'last',
+							[s.isMiddleItem]: listItemStyle === 'middle',
+							[s.hideBorder]: hideBorder,
+						})}
 					/>
 				</div>
-			) : null}
+			)}
+			<div>{children}</div>
 		</div>
 	)
 }
