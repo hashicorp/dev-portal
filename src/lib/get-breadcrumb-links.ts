70f08bb3d6984b56f3755adda010e804a7d3d37e
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2023-08-14 20:45:36 -0400

    chore: clean up get-breadcrumb-links

--- src/lib/get-breadcrumb-links.ts
+++ src/lib/get-breadcrumb-links.ts
@@ -1,4 +1,3 @@
-import { getPageTitleFromUrl } from './get-page-title-from-url'
 import type { BreadcrumbLink } from 'components/breadcrumb-bar'
 
 /**
@@ -11,11 +10,55 @@ const KNOWN_404_URLS = [
 ]
 
 /**
+ * Map a `urlPath` on `developer.hashicorp.com` to a consistent page title.
+ */
+const KNOWN_URL_TITLE: Record<string, string> = {
+	'/': 'Developer',
+	'/hcp': 'HashiCorp Cloud Platform',
+}
+
+/**
+ * Map the final segment of a `urlPath` to a generic page title.
+ * This is intended for segments that may appear frequently in many URLs.
+ *
+ * Rather than specify duplicative entries for specific URLs, we can
+ * specify a single entry for a common URL segment.
+ *
+ * For example:
+ * - `/waypoint/api-docs` breadcrumb title should be `API`
+ * - `/boundary/api-docs` breadcrumb title should be `API`
+ * - `/hcp/api-docs` breadcrumb title should be `API`
+ * - ...etc
+ */
+const KNOWN_URL_SEGMENT_TITLE: Record<string, string> = {
+	'api-docs': 'API',
+}
+
+/**
+ * Given a `urlPath` on `developer.hashicorp.com`,
+ * Return a page title we consistently use to refer to that URL.
+ *
+ * If the provided `urlPath` does not have a known breadcrumb title,
+ * we return the final URL segment of the path as the breadcrumb title.
+ */
+export function getBreadcrumbTitle(urlPath: string): string | null {
+	if (KNOWN_URL_TITLE[urlPath]) {
+		// If we have a URL-specific entry, favour that
+		return KNOWN_URL_TITLE[urlPath]
+	} else {
+		// If we have a segment-specific entry, return that, or `null` otherwise
+		const segments = urlPath.split('/')
+		const finalSegment = segments[segments.length - 1]
+		return KNOWN_URL_SEGMENT_TITLE[finalSegment] || null
+	}
+}
+
+/**
  * Given a `urlPath` on `developer.hashicorp.com`,
  * Return a set of breadcrumb link items representing each URL segment
  * leading to the page at `urlPath`.
  *
- * Note: if the URL does not have a known page title in `getPageTitleFromUrl`,
+ * Note: if the URL does not have a known page title in `getBreadcrumbTitle`,
  * we fallback to using the URL segment as the breadcrumb title.
  *
  * Note: if the URL is a known 404, then the resulting breadcrumb link
@@ -26,7 +69,7 @@ export function getBreadcrumbLinks(urlPath: string): BreadcrumbLink[] {
 	const segments = urlPath.split('/')
 	for (let i = 0; i < segments.length; i++) {
 		const url = i == 0 ? '/' : segments.slice(0, i + 1).join('/')
-		const title = getPageTitleFromUrl(url) || segments[i]
+		const title = getBreadcrumbTitle(url) || segments[i]
 		const isKnown404 = KNOWN_404_URLS.includes(url)
 		breadcrumbLinks.push({ title, url: isKnown404 ? null : url })
 	}
