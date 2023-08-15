d25206474600f5dffb336e1e0e594d383df6e6cc
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2023-08-15 17:44:02 -0400

    docs: describe ContentWithPermalink

--- src/views/open-api-docs-view/components/content-with-permalink/index.tsx
+++ src/views/open-api-docs-view/components/content-with-permalink/index.tsx
@@ -5,6 +5,19 @@ import type { PropsWithChildren } from 'react'
 // Styles
 import s from './content-with-permalink.module.css'
 
+/**
+ * Renders the provided `children` alongside a permalink `<a />` element.
+ *
+ * `ariaLabel` is required to ensure the permalink element has meaningful
+ * text for screen readers. For simple use cases where `children` is a
+ * string, the `ariaLabel` can be passed the same value as `children`.
+ * For more complex use cases, the consumer should determine what text
+ * would be appropriate to pass to `ariaLabel`.
+ *
+ * Note that this component does _not_ handle placing the provided `id`
+ * in the DOM. It requires the consumer to place the `id` on an appropriate
+ * element, typically an element rendered in the provided `children`.
+ */
 export function ContentWithPermalink({
 	id,
 	ariaLabel,
