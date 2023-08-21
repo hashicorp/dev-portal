heatlikeheatwave/hashicorp-dev-portal

feat: inject word breaks for prettier overflow-wrap

src/views/open-api-docs-view/components/open-api-operations/index.tsx
@@ -9,6 +9,55 @@ import type {
 	OperationProps,
 } from 'views/open-api-docs-view/types'
 import s from './open-api-operations.module.css'
+import { ReactNode } from 'react'
+
+/**
+ * Split a string on capital letters.
+ *
+ * Works like `string.split('')`, but splits on capital letters.
+ *
+ * TODO: split this out, put it somewhere else.
+ */
+function splitOnCapitalLetters(str: string): string[] {
+	const lowerCased = str.toLowerCase()
+	const result: string[] = []
+	let buffer = ''
+	for (let i = 0; i < str.length; i++) {
+		/**
+		 * If we have a lowercase character, add it to the buffer.
+		 * If we have an uppercase character, add the buffer to the result,
+		 * and reset the buffer.
+		 */
+		const isLowerCase = str[i] === lowerCased[i]
+		if (isLowerCase) {
+			buffer += str[i]
+		} else {
+			result.push(buffer)
+			buffer = str[i]
+		}
+	}
+	// Append any stray buffer, if it's non-empty
+	if (buffer !== '') {
+		result.push(buffer)
+	}
+	// Return the array of strings
+	return result
+}
+
+/**
+ * Given an array of strings,
+ * Return the array with `<wbr />` elements inserted between each string.
+ *
+ * TODO: split this out, put it somewhere else.
+ */
+function addWordBreaks(stringArray: string[]): ReactNode[] {
+	return stringArray
+		.map((v: string, idx: number) => {
+			// eslint-disable-next-line react/no-array-index-key
+			return idx === 0 ? [v] : [<wbr key={idx} />, v]
+		})
+		.flat()
+}
 
 /**
  * Render operation items for an OpenApiView.
@@ -41,9 +90,16 @@ export function OpenApiOperations({
 							<OperationHeader
 								className={s.header}
 								slug={operation.slug}
-								headerText={operation.operationId}
+								headerText={addWordBreaks(
+									splitOnCapitalLetters(operation.operationId)
+								)}
 								method={operation.type}
-								path={operation.path.truncated}
+								path={addWordBreaks(
+									operation.path.truncated
+										.split('/')
+										.map((v, idx) => (idx === 0 ? [v] : ['/', v]))
+										.flat()
+								)}
 							/>
 							<OperationSections
 								examplesSlot={<OperationExamples operation={operation} />}

hashicorp/dev-portal

src/views/open-api-docs-view/components/open-api-operations/index.tsx
@@ -14,6 +14,55 @@ import type {
 	OperationProps,
 } from 'views/open-api-docs-view/types'
 import s from './open-api-operations.module.css'
+import { ReactNode } from 'react'
+
+/**
+ * Split a string on capital letters.
+ *
+ * Works like `string.split('')`, but splits on capital letters.
+ *
+ * TODO: split this out, put it somewhere else.
+ */
+function splitOnCapitalLetters(str: string): string[] {
+	const lowerCased = str.toLowerCase()
+	const result: string[] = []
+	let buffer = ''
+	for (let i = 0; i < str.length; i++) {
+		/**
+		 * If we have a lowercase character, add it to the buffer.
+		 * If we have an uppercase character, add the buffer to the result,
+		 * and reset the buffer.
+		 */
+		const isLowerCase = str[i] === lowerCased[i]
+		if (isLowerCase) {
+			buffer += str[i]
+		} else {
+			result.push(buffer)
+			buffer = str[i]
+		}
+	}
+	// Append any stray buffer, if it's non-empty
+	if (buffer !== '') {
+		result.push(buffer)
+	}
+	// Return the array of strings
+	return result
+}
+
+/**
+ * Given an array of strings,
+ * Return the array with `<wbr />` elements inserted between each string.
+ *
+ * TODO: split this out, put it somewhere else.
+ */
+function addWordBreaks(stringArray: string[]): ReactNode[] {
+	return stringArray
+		.map((v: string, idx: number) => {
+			// eslint-disable-next-line react/no-array-index-key
+			return idx === 0 ? [v] : [<wbr key={idx} />, v]
+		})
+		.flat()
+}
 
 /**
  * Render operation items for an OpenApiView.
@@ -30,15 +79,26 @@ export function OpenApiOperations({
 				.flat()
 				.map((operation: OperationProps) => {
 					return (
-						<div key={operation.operationId}>
+						<div
+							key={operation.operationId}
+							style={{ border: '1px solid magenta', padding: '2px' }}
+						>
 							<OperationSections
 								headerSlot={
 									<OperationHeader
 										className={s.header}
 										slug={operation.slug}
-										headerText={operation.operationId}
+										headerAriaLabel={operation.operationId}
+										headerSlot={addWordBreaks(
+											splitOnCapitalLetters(operation.operationId)
+										)}
 										method={operation.type}
-										path={operation.path.truncated}
+										path={addWordBreaks(
+											operation.path.truncated
+												.split('/')
+												.map((v, idx) => (idx === 0 ? [v] : ['/', v]))
+												.flat()
+										)}
 									/>
 								}
 								examplesSlot={