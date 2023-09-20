64450f564d4fa3b3537f79901cc3cfbc7a0611e7
Kevin Wang <kwangsan@gmail.com>
2023-09-20 11:38:14 -0400

    update toast expiry logic

--- src/components/chatbox/ai-feature-toast.tsx
+++ src/components/chatbox/ai-feature-toast.tsx
@@ -1,15 +1,32 @@
-import { useState, useEffect } from 'react'
+import { useEffect } from 'react'
+import Cookies from 'js-cookie'
 import { IconWand24 } from '@hashicorp/flight-icons/svg-react/wand-24'
 
 import { toast, ToastColor } from 'components/toast'
 import useAuthentication from 'hooks/use-authentication'
 
-// TODO(kevinwang): see `make-dark-mode-notification.ts` for persistent-hiding
+const COOKIE_IGNORE_AI_TOAST = 'dev-dot-ignore-ai-toast'
+const COOKIE_HAS_SEEN_AI_TOAST = 'dev-dot-has-seen-ai-toast'
+const COOKIE_EXPIRES_AT = new Date('12/25/2024') // arbitrary date
+
+const TOAST_AUTO_DISMISS_MS = 15000
+
 export function AIFeatureToast() {
-	const [shown, setShown] = useState(false)
 	const { session } = useAuthentication()
+
 	useEffect(() => {
-		if (shown) {
+		// skip toast if we're past the expiration date
+		if (new Date() > COOKIE_EXPIRES_AT) {
+			return
+		}
+
+		// skip toast if user has previously dismissed it
+		if (Cookies.get(COOKIE_IGNORE_AI_TOAST)) {
+			return
+		}
+
+		// skip toast if it's already been seen
+		if (Cookies.get(COOKIE_HAS_SEEN_AI_TOAST)) {
 			return
 		}
 
@@ -19,10 +36,26 @@ export function AIFeatureToast() {
 				icon: <IconWand24 />,
 				title: 'Welcome to the Developer AI closed beta',
 				description: 'Try it out in our cmd+K menu!',
-				autoDismiss: 10000,
+				autoDismiss: TOAST_AUTO_DISMISS_MS,
+				onDismissCallback: () => {
+					// when user dismisses the toast, we should ignore it going forwards
+					Cookies.set(COOKIE_IGNORE_AI_TOAST, true, {
+						expires: COOKIE_EXPIRES_AT,
+					})
+					// remove extra cookie
+					Cookies.remove(COOKIE_HAS_SEEN_AI_TOAST)
+				},
+				dismissOnRouteChange: false,
 			})
-			setShown(true)
+
+			// if the user didn't explicitly dismiss the toast, show it again in 24 hours
+			// this is a simple means to boost discoverability / awareness
+			Cookies.set(COOKIE_HAS_SEEN_AI_TOAST, true, {
+				expires: 1, // https://github.com/js-cookie/js-cookie?tab=readme-ov-file#expires
+			})
 		}
-	}, [session?.meta.isAIEnabled, shown])
+	}, [session?.meta.isAIEnabled])
+
+	// no dom elements are needed for this component
 	return null
 }
