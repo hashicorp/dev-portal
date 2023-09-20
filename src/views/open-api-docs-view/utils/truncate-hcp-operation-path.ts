8f8604f50f0621e7cca54b744dc60a5d2c6c3f94
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2023-09-20 17:06:00 -0400

    feat: modify truncate fn to support global hcp services

--- src/views/open-api-docs-view/utils/truncate-hcp-operation-path.ts
+++ src/views/open-api-docs-view/utils/truncate-hcp-operation-path.ts
@@ -4,6 +4,24 @@
  */
 
 /**
+ * Specific HCP service paths start with the service name,
+ * followed by a date-based version number,
+ * and always include "organization" and "project" parameters.
+ *
+ * We remove all of this prefix from the path for better grouping
+ * of operations within the specific spec.
+ */
+const SPECIFIC_SERVICE_PATTERN =
+	/(\/[a-z]*\/\d\d\d\d-\d\d-\d\d\/organizations\/\{[a-z_.]*\}\/projects\/\{[a-z_.]*\})/
+
+/**
+ * Global HCP service paths start with the service name,
+ * and always include "organization" and "project" parameters.
+ *
+ * We remove the version prefix from the path for better grouping.
+ */
+const GLOBAL_SERVICE_PATTERN = /\/\d\d\d\d-\d\d-\d\d\//
+/**
  * Truncates HCP operation paths for clarity.
  *
  * TODO: this is a shim for development.
@@ -13,8 +31,7 @@
  * as grouping only really makes sense if we remove the extended prefix.
  */
 export function truncateHcpOperationPath(path: string) {
-	return path.replace(
-		/(\/[a-z]*\/\d\d\d\d-\d\d-\d\d\/organizations\/\{[a-z_.]*\}\/projects\/\{[a-z_.]*\})/,
-		''
-	)
+	return path
+		.replace(SPECIFIC_SERVICE_PATTERN, '')
+		.replace(GLOBAL_SERVICE_PATTERN, '')
 }
