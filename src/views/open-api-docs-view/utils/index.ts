703b95533e828e5c304f1d82631fe01e51d69a38
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2023-09-26 17:17:58 -0400

    feat: implement find-default-version

--- src/views/open-api-docs-view/utils/index.ts
+++ src/views/open-api-docs-view/utils/index.ts
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
+export { findDefaultVersion } from './find-default-version'
 export { findLatestStableVersion } from './find-latest-stable-version'
 export { getNavItems } from './get-nav-items'
 export { getOperationProps } from './get-operation-props'
