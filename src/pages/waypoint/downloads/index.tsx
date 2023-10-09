bfbdf110ca01a8cad6cab282937e77b66f6c1ac6
Kendall Strautman <kendallstrautman@gmail.com>
2023-10-09 15:34:54 -0700

    adds intall merchandising slot positioning

--- src/pages/waypoint/downloads/index.tsx
+++ src/pages/waypoint/downloads/index.tsx
@@ -10,28 +10,33 @@ import waypointData from 'data/waypoint.json'
 import { ProductData } from 'types/products'
 import ProductDownloadsView from 'views/product-downloads-view'
 import { generateGetStaticProps } from 'views/product-downloads-view/server'
+import s from './downloads.module.css'
 
 const WaypointDownloadsPage = (props) => {
 	return (
 		<ProductDownloadsView
 			{...props}
-			merchandisingSlot={
-				<InlineAlert
-					title="Archive Notice"
-					description="This download is part of the legacy version of Waypoint that is no longer actively maintained."
-					color="highlight"
-					ctaSlot={
-						<StandaloneLink
-							href="https://www.hashicorp.com/blog/a-new-vision-for-hcp-waypoint"
-							opensInNewTab
-							text="Read the blog"
-							color="secondary"
-							icon={<IconExternalLink16 />}
-							iconPosition="trailing"
-						/>
-					}
-				/>
-			}
+			merchandisingSlot={{
+				position: 'above',
+				slot: (
+					<InlineAlert
+						className={s.alert}
+						title="Archive Notice"
+						description="This download is part of the legacy version of Waypoint that is no longer actively maintained."
+						color="highlight"
+						ctaSlot={
+							<StandaloneLink
+								href="https://www.hashicorp.com/blog/a-new-vision-for-hcp-waypoint"
+								opensInNewTab
+								text="Read the blog"
+								color="secondary"
+								icon={<IconExternalLink16 />}
+								iconPosition="trailing"
+							/>
+						}
+					/>
+				),
+			}}
 		/>
 	)
 }
