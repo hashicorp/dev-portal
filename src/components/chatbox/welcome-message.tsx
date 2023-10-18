66ba2a4ff52397f493367eb165c70b74343fa565
Kevin Wang <kwangsan@gmail.com>
2023-10-18 16:03:38 -0400

    Developer AI VQA Cont. (#2186)
    * leave comment on `.arrowdown`
    
    * replace `mdxA` with `InlineLink`
    
    * add TODOs on a11y
    
    * update welcome text
    - same as https://github.com/hashicorp/dev-portal/pull/2181/commits/6e70cc116e631144622c400383e8e02ed387cc28
    
    * show finished element
    
    * add TODOs
    
    * lazyload `Finished` component
    
    * remove button overrides
    
    * update placeholder
    
    * Update src/components/chatbox/welcome-message.tsx
    
    * remove welcome message body text
    
    * update back button styles
    
    * fix: don't lazy import finished component
    
    * adjust button size
    
    * feat: adds ai feedback button to command bar header (#2203)
    
    * feat: adds command bar right header slot
    
    * update feedback form url
    
    * update buttons
    
    * adjut to arrow 16
    
    * adds ai link
    
    * open in new tab
    
    ---------
    
    Co-authored-by: Kendall Strautman <kendallstrautman@gmail.com>
    Co-authored-by: Kendall Strautman Swarthout <36613477+kendallstrautman@users.noreply.github.com>

--- src/components/chatbox/welcome-message.tsx
+++ src/components/chatbox/welcome-message.tsx
@@ -14,6 +14,23 @@ import Text from 'components/text'
 
 import s from './welcome-message.module.css'
 
+// this is an arbitrary, light abstraction to separate
+// the content/copy from the component
+const rightColumn = [
+	{
+		icon: <IconUser16 />,
+		heading: 'Personalized recommendations',
+	},
+	{
+		icon: <IconDiscussionCircle16 />,
+		heading: 'Natural language conversations',
+	},
+	{
+		icon: <IconBulb16 />,
+		heading: 'Tap into the HashiCorp knowledge base',
+	},
+]
+
 export const WelcomeMessage = () => {
 	return (
 		<div className={s.welcome}>
@@ -26,80 +43,36 @@ export const WelcomeMessage = () => {
 						weight="medium"
 						size={400}
 					>
-						Welcome to Developer AI
+						Developer AI
 					</Heading>
 					<Text /* Body/100/Medium */
 						className={s.faint}
 						size={100}
 						weight="medium"
 					>
-						Your personal AI-powered assistant, we’re ready to help you get the
-						most out of Developer. Let’s get started on this journey together.
+						Your AI-powered companion for finding reference materials,
+						architectural guidance, and product examples from HashiCorp. Ask me
+						anything to get started.
 					</Text>
 				</div>
 				<div className={s.spacer} />
 			</div>
 			<div className={classNames(s.col, s.right)}>
-				<div className={s.row}>
-					<IconUser16 />
-					<div>
-						<Heading /* Display/100/Medium */
-							className={s.strong}
-							level={4}
-							weight="medium"
-							size={100}
-						>
-							Personalized recommendations
-						</Heading>
-						<Text /* Body/100/Regular */
-							size={100}
-							weight="regular"
-							className={s.faint}
-						>
-							Coming soon...
-						</Text>
+				{rightColumn.map(({ icon, heading }, i) => (
+					<div className={s.row} key={i}>
+						{icon}
+						<div>
+							<Heading /* Display/100/Medium */
+								className={s.strong}
+								level={4}
+								weight="medium"
+								size={100}
+							>
+								{heading}
+							</Heading>
+						</div>
 					</div>
-				</div>
-				<div className={s.row}>
-					<IconDiscussionCircle16 />
-					<div>
-						<Heading /* Display/100/Medium */
-							className={s.strong}
-							level={4}
-							weight="medium"
-							size={100}
-						>
-							Natural language conversations
-						</Heading>
-						<Text /* Body/100/Regular */
-							className={s.faint}
-							size={100}
-							weight="regular"
-						>
-							Coming soon...
-						</Text>
-					</div>
-				</div>
-				<div className={s.row}>
-					<IconBulb16 />
-					<div>
-						<Heading /* Display/100/Medium */
-							className={s.strong}
-							level={4}
-							weight="medium"
-							size={100}
-						>
-							Knowledge base
-						</Heading>
-						<Text /* Body/100/Regular */
-							className={s.faint}
-							size={100}
-							weight="regular"
-						>
-							Yes
-						</Text>
-					</div>
-				</div>
+				))}
 			</div>
 		</div>
 	)
