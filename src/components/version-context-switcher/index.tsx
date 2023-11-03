aef384ca58ac1208a00f909535d67b299148ba71
Heat Hamilton <heat.hamilton@hashicorp.com>
2023-11-03 17:51:41 -0400

    Updated styling for version switcher; removed dead code

--- src/components/version-context-switcher/index.tsx
+++ src/components/version-context-switcher/index.tsx
@@ -6,9 +6,10 @@
 import { ChangeEventHandler, ReactElement, useState } from 'react'
 import { IconCaret16 } from '@hashicorp/flight-icons/svg-react/caret-16'
 import { useCurrentProduct } from 'contexts'
-import ProductIcon from 'components/product-icon'
 import { ContextSwitcherOption, VersionContextSwitcherProps } from './types'
 import s from './version-context-switcher.module.css'
+import { ReleaseVersion } from 'lib/fetch-release-data'
+import { IconChevronDown16 } from '@hashicorp/flight-icons/svg-react/chevron-down-16'
 
 /**
  * To be used as control that changes the content of a page or as form control.
@@ -25,26 +26,10 @@ const VersionContextSwitcher = ({
 	onChange,
 	options,
 }: VersionContextSwitcherProps): ReactElement => {
+	console.log({ options })
 	const currentProduct = useCurrentProduct()
-	let latestVersion = options[0].value
-	/** Update `latestVersion` to the latest version of the product
-	 * that has been released if it can be determined. This is needed
-	 * because not all products have `latest` listed as the first item
-	 * in the sorted array. E.g. `vault-enterprise` lists `+ent.hsm.fips1402`
-	 * variants first.
-	 */
-	for (let i = 0; i < options.length; i++) {
-		const versionLabel = options[i].label
-		const versionValue = options[i].value
-		if (versionLabel.indexOf('(latest)') >= 0) {
-			latestVersion = versionValue
-			break
-		}
-	}
-
-	const [selectedVersion, setSelectedVersion] = useState<
-		ContextSwitcherOption['value']
-	>(initialValue || latestVersion)
+	const [selectedVersion, setSelectedVersion] =
+		useState<ContextSwitcherOption['value']>(initialValue)
 
 	/**
 	 * Handle change event for switcher, invoking the `onChange` function last if
@@ -58,30 +43,15 @@ const VersionContextSwitcher = ({
 		}
 	}
 
-	/**
-	 * TODO: hopefully this is temporary. Because Sentinel does not have a
-	 * `ProductIcon` and the component intentionally returns `null` for if the
-	 * value for the `product` prop is `'sentinel'`, opting to pass a different
-	 * value for Sentinel here rather than affect all current references to
-	 * ProductIcon.
-	 *  - `ProductSwitcher` is one example that would be imacted by adding a
-	 *    ProductIcon for Sentinel. We currently do not want to render an icon
-	 *    for Sentinel in `ProductSwitcher`.
-	 */
-	const productIconSlug =
-		currentProduct.slug === 'sentinel' ? 'hcp' : currentProduct.slug
 	return (
 		<div className={s.root}>
-			<span className={s.productIcon}>
-				<ProductIcon productSlug={productIconSlug} />
-			</span>
 			<select
 				aria-label="Choose a different version to install"
 				className={s.select}
 				onChange={handleChange}
 				value={selectedVersion}
 			>
-				{options.map((option) => (
+				{options.map((option: HTMLOptionElement) => (
 					<option
 						aria-label={`${currentProduct.name} ${option.label}`}
 						className={s.option}
@@ -92,9 +62,7 @@ const VersionContextSwitcher = ({
 					</option>
 				))}
 			</select>
-			<span className={s.trailingIcon}>
-				<IconCaret16 />
-			</span>
+			<IconChevronDown16 className={s.trailingIcon} tab-index="-1" />
 		</div>
 	)
 }
