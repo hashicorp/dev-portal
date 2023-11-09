2b5f7e27ffdc361ba56374e9d34ae565d0e2c08b
Kendall Strautman Swarthout <36613477+kendallstrautman@users.noreply.github.com>
2023-11-09 09:22:54 -0800

    Scaffold hvd routing & feature flag (#2222)
    * add basic pages
    
    * add no-index
    
    * add feature flag
    
    * update comment
    
    * testing
    
    * rework the flag logic
    
    * add getStaticPaths
    
    * remove github fetch script
    
    * Remove the build dir adjustment
    
    * fixup
    
    * add test path
    
    * fix param

---
+++ src/pages/validated-designs/[slug]/index.tsx
@@ -0,0 +1,36 @@
+import Head from 'next/head'
+
+export default function ValidatedDesignPageTemplate() {
+	return (
+		<>
+			<Head>
+				<meta name="robots" content="noindex, nofollow" />
+			</Head>
+			<h1>HashiCorp Validated Design Page Template</h1>
+		</>
+	)
+}
+
+export async function getStaticPaths() {
+	return {
+		paths: [
+			{
+				params: {
+					slug: 'test',
+				},
+			},
+		],
+		fallback: false,
+	}
+}
+
+export async function getStaticProps() {
+	if (__config.flags.enable_hvd === false) {
+		return {
+			notFound: true,
+		}
+	}
+	return {
+		props: {},
+	}
+}
---
+++ src/pages/validated-designs/index.tsx
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+
+export default function ValidatedDesignsLanding() {
+	return (
+		<>
+			<Head>
+				<meta name="robots" content="noindex, nofollow" />
+			</Head>
+			<h1>HashiCorp Validated Designs</h1>
+		</>
+	)
+}
+
+export async function getStaticProps() {
+	if (__config.flags.enable_hvd === false) {
+		return {
+			notFound: true,
+		}
+	}
+	return {
+		props: {},
+	}
+}
