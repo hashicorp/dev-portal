8944bc503ca1034ed074bc8fee9e99463e5de290
Sarah Chavis <62406755+schavis@users.noreply.github.com>
2023-11-13 16:45:49 -0800

    switch vault to oas

--- src/pages/vault/api-docs/[[...page]].tsx
+++ src/pages/vault/api-docs/[[...page]].tsx
@@ -3,13 +3,101 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
-import DocsView from 'views/docs-view'
-import { getRootDocsPathGenerationFunctions } from 'views/docs-view/utils/get-root-docs-path-generation-functions'
+// Lib
+import { fetchCloudApiVersionData } from 'lib/api-docs/fetch-cloud-api-version-data'
+// View
+import OpenApiDocsView from 'views/open-api-docs-view'
+import {
+	getStaticPaths,
+	getStaticProps as getOpenApiDocsStaticProps,
+} from 'views/open-api-docs-view/server'
+// Types
+import type { GetStaticProps, GetStaticPropsContext } from 'next'
+import type { OpenAPIV3 } from 'openapi-types'
+import type {
+	OpenApiDocsParams,
+	OpenApiDocsViewProps,
+	OpenApiDocsPageConfig,
+} from 'views/open-api-docs-view/types'
 
-const { getStaticPaths, getStaticProps } = getRootDocsPathGenerationFunctions(
-	'vault',
-	'api-docs'
-)
+/**
+ * OpenApiDocsView server-side page configuration
+ */
+const PAGE_CONFIG: OpenApiDocsPageConfig = {
+	productSlug: 'vault',
+	serviceProductSlug: 'vault',
+	basePath: '/vault/api-docs',
+	githubSourceDirectory: {
+		owner: 'hashicorp',
+		repo: 'vault',
+		path: 'website/data/swagger',
+		ref: 'docs/oas-testing',
+	},
+	groupOperationsByPath: true,
+	statusIndicatorConfig: {
+		pageUrl: 'https://status.hashicorp.com',
+		endpointUrl:
+			'https://status.hashicorp.com/api/v2/components/hk67zg2j2rkd.json',
+	},
+	navResourceItems: [
+		{
+			title: 'Tutorial Library',
+			href: '/tutorials/library?product=vault&edition=hcp',
+		},
+		{
+			title: 'Certifications',
+			href: '/certifications/security-automation',
+		},
+		{
+			title: 'Community',
+			href: 'https://discuss.hashicorp.com/',
+		},
+		{
+			title: 'Support',
+			href: 'https://www.hashicorp.com/customer-success',
+		},
+	],
+	/**
+	 * Massage the schema data a little bit, replacing
+	 * "HashiCorp Cloud Platform" in the title with "HCP".
+	 */
+	massageSchemaForClient: (schemaData: OpenAPIV3.Document) => {
+		// Replace "HashiCorp Cloud Platform" with "HCP" in the title
+		const massagedTitle = schemaData.info.title.replace(
+			'HashiCorp Cloud Platform',
+			'HCP'
+		)
+		// Return the schema data with the revised title
+		const massagedInfo = { ...schemaData.info, title: massagedTitle }
+		return { ...schemaData, info: massagedInfo }
+	},
+}
 
-export { getStaticProps, getStaticPaths }
-export default DocsView
+/**
+ * Get static paths, using `versionData` fetched from GitHub.
+ */
+export { getStaticPaths }
+
+/**
+ * Get static props, using `versionData` fetched from GitHub.
+ *
+ * We need all version data for the version selector,
+ * and of course we need specific data for the current version.
+ */
+export const getStaticProps: GetStaticProps<
+	OpenApiDocsViewProps,
+	OpenApiDocsParams
+> = async ({ params }: GetStaticPropsContext<OpenApiDocsParams>) => {
+	// Fetch all version data, based on remote `stable` & `preview` subfolders
+	const versionData = await fetchCloudApiVersionData(
+		PAGE_CONFIG.githubSourceDirectory
+	)
+	// Generate static props based on page configuration, params, and versionData
+	return await getOpenApiDocsStaticProps({
+		...PAGE_CONFIG,
+		context: { params },
+		versionData,
+	})
+}
+
+export default OpenApiDocsView
