747b1c3ec26c62adce7825c59b39801880aed221
Kendall Strautman <kendallstrautman@gmail.com>
2023-11-13 16:13:27 -0800

    adds copyright header

--- src/views/validated-designs/guide/index.tsx
+++ src/views/validated-designs/guide/index.tsx
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 import Head from 'next/head'
 import BaseLayout from 'layouts/base-layout'
 import MobileMenuLevelsGeneric from 'components/mobile-menu-levels-generic'
