b85673aaae053d3c8d7d130d61faecfcdb464280
Heat Hamilton <heat.hamilton@hashicorp.com>
2023-11-20 19:19:42 -0500

    Styling added for desktop client downloads for Boundary; moved ReleaseInformation to ProductDownloads component so merchandising slot is above RI

--- src/views/product-downloads-view/components/downloads-section/index.tsx
+++ src/views/product-downloads-view/components/downloads-section/index.tsx
@@ -66,7 +66,7 @@ const PackageManagerSection = ({
 								key={label}
 								code={installCodeHtml}
 								language="shell-session"
-								options={{ showClipboard: true, wrapCode: true }}
+								options={{ showClipboard: true }}
 							/>
 						)
 					})}
