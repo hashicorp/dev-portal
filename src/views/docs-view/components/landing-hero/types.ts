e24c8ae710b17da851be93b5e1359189b8dbcf0e
Heat Hamilton <heat.hamilton@hashicorp.com>
2023-12-15 09:42:47 -0500

    Edited install-landing pages

---
+++ src/views/docs-view/components/landing-hero/types.ts
@@ -0,0 +1,11 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import { ProductRootDocsPathLandingProps } from 'views/product-root-docs-path-landing/types'
+
+export interface LandingHeroProps {
+	pageHeading: ProductRootDocsPathLandingProps['pageHeading']
+	pageSubtitle?: string
+}
