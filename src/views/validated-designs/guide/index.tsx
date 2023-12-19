dc3ded27feba47f620a64a96391db9ce42c6b996
Ruben Nic <ruben.nic@hashicorp.com>
2023-12-19 13:50:49 -0500

    quick style jam with kelly

--- src/views/validated-designs/guide/index.tsx
+++ src/views/validated-designs/guide/index.tsx
@@ -4,23 +4,27 @@
  */
 
 import Head from 'next/head'
-import DevDotContent from 'components/dev-dot-content'
-import { OutlineNavWithActive } from 'components/outline-nav/components'
-import SidebarSidecarLayout from 'layouts/sidebar-sidecar'
 
-import { HvdPage } from '../types'
+import DevDotContent from 'components/dev-dot-content'
+import getDocsMdxComponents from 'views/docs-view/utils/get-docs-mdx-components'
+
+import { OutlineNavWithActive } from 'components/outline-nav/components'
 import DirectionalLinkBox from 'components/directional-link-box'
 
-import { MDXRemoteSerializeResult } from 'next-mdx-remote'
-import { OutlineLinkItem } from 'components/outline-nav/types'
+import SidebarSidecarLayout from 'layouts/sidebar-sidecar'
 import {
+	SidebarHorizontalRule,
 	SidebarNavHighlightItem,
 	SidebarSectionBrandedHeading,
 } from 'components/sidebar/components'
 import SidebarNavList from 'components/sidebar/components/sidebar-nav-list'
+import SidebarBackToLink from 'components/sidebar/components/sidebar-back-to-link'
 
 import s from './detail-view.module.css'
-import getDocsMdxComponents from 'views/docs-view/utils/get-docs-mdx-components'
+
+import type { HvdPage } from '../types'
+import type { MDXRemoteSerializeResult } from 'next-mdx-remote'
+import type { OutlineLinkItem } from 'components/outline-nav/types'
 
 export interface ValidatedDesignsGuideProps {
 	title: string
@@ -44,6 +48,8 @@ export default function ValidatedDesignGuideView({
 	pages,
 }: ValidatedDesignsGuideProps) {
 	const docsMdxComponents = getDocsMdxComponents('hcp')
+	const backBtnTitle = 'HVD Home'
+	const backBtnUrl = basePath
 
 	function renderPreviousLink() {
 		if (currentPageIndex !== 0 && pages[currentPageIndex - 1]) {
@@ -82,8 +88,8 @@ export default function ValidatedDesignGuideView({
 			sidebarNavDataLevels={[
 				{
 					backToLinkProps: {
-						text: `HVD Home`,
-						href: `${basePath}`,
+						text: backBtnTitle,
+						href: backBtnUrl,
 					},
 					title,
 					visuallyHideTitle: true,
@@ -92,6 +98,7 @@ export default function ValidatedDesignGuideView({
 					children: (
 						<>
 							<SidebarSectionBrandedHeading text={title} theme={'hcp'} />
+							<SidebarHorizontalRule />
 							<SidebarNavList>
 								{pages.map((page: HvdPage, index: number) => {
 									return (
@@ -116,6 +123,9 @@ export default function ValidatedDesignGuideView({
 				<meta name="robots" content="noindex, nofollow" />
 				<meta name="description" content={markdown.description} />
 			</Head>
+			<div className={s.mobileBackButton}>
+				<SidebarBackToLink text={backBtnTitle} href={backBtnUrl} />
+			</div>
 			<DevDotContent
 				mdxRemoteProps={{
 					...markdown.mdxSource,
