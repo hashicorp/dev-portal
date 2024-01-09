62a4ff32a14ef6fa8b59929003fec5c5e966e93a
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2024-01-09 14:52:55 -0500

    docs: add comment for clarity

--- build-libs/docs-dot-hashicorp-redirects.js
+++ build-libs/docs-dot-hashicorp-redirects.js
@@ -60,7 +60,8 @@ function getDocsDotHashiCorpRedirects() {
 			destination: 'https://developer.hashicorp.com/sentinel/install',
 			permanent: true,
 		},
-		// Redirect the docs landing page
+		// Redirect the docs landing page (previously 404'd, but this redirect
+		// avoids redirecting `/sentinel/docs` to `/sentinel/docs/docs`)
 		{
 			source: '/sentinel/docs',
 			destination: 'https://developer.hashicorp.com/sentinel/docs',
