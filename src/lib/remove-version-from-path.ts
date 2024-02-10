d15a93fcf741353b2e0ab0dd7a17d70f035ab57d
Heat Hamilton <heat.hamilton@hashicorp.com>
2024-02-09 19:03:37 -0500

    Removed import of year & tfe version

--- src/lib/remove-version-from-path.ts
+++ src/lib/remove-version-from-path.ts
@@ -8,12 +8,16 @@ import {
 	TFE_VERSION_IN_PATH_REGEXP,
 } from 'constants/version-path'
 
+/** This REGEX is used to parse a Terraform Enterprise version and year from a URL
+ * e.g. /terraform/enterprise/releases/2024/v202401-1
+ */
+const YEAR_AND_TFE_VERSION_IN_PATH_REGEXP = /\/[0-9]{4}\/v[0-9]{6}-\d+/i
+
 /**
  * Removes a version string from a path, and returns the new path.
  * Returns the original string if no version is present.
  */
 export function removeVersionFromPath(path: string): string {
-	const YEAR_AND_TFE_VERSION_IN_PATH_REGEXP = /\/[0-9]{4}\/v[0-9]{6}-\d+/i
 	if (YEAR_AND_TFE_VERSION_IN_PATH_REGEXP.test(path)) {
 		path = path.replace(YEAR_AND_TFE_VERSION_IN_PATH_REGEXP, '')
 	}
