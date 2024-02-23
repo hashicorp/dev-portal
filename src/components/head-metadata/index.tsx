1ba4cd8398c18d5da1fd2bbfc6505d241c8bb5b5
Ruben Nic <ruben.nic@hashicorp.com>
2024-02-23 09:42:41 -0500

    Add a dev fav icon

--- src/components/head-metadata/index.tsx
+++ src/components/head-metadata/index.tsx
@@ -66,7 +66,15 @@ export default function HeadMetadata(props: HeadMetadataProps) {
 			image={ogImageUrl}
 			canonicalUrl={canonicalUrl}
 		>
-			<link rel="icon" href="/favicon.ico" sizes="any" />
+			<link
+				rel="icon"
+				href={
+					process.env.NODE_ENV === 'production'
+						? '/favicon.ico'
+						: '/favicon-dev.ico'
+				}
+				sizes="any"
+			/>
 			<link rel="icon" href="/icon.svg" type="image/svg+xml" />
 			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 			<meta
