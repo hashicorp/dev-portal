358e1f084ae8b1891c2397127cbbb6191b96341f
Heat Hamilton <heat.hamilton@hashicorp.com>
2024-02-27 16:50:19 -0500

    Remove install from waypoint

--- build-libs/redirects.js
+++ build-libs/redirects.js
@@ -182,6 +182,11 @@ async function buildDevPortalRedirects() {
 				'https://github.com/hashicorp/waypoint/blob/main/pkg/server/gen/server.swagger.json',
 			permanent: true,
 		},
+		{
+			source: '/waypoint/install',
+			destination: '/waypoint/tutorials/hcp-waypoint',
+			permanent: true,
+		},
 		/**
 		 * Redirect for Integration Component rework.
 		 * Further details in the file this is imported from.
