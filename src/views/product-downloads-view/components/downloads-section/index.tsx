0354e39f430bb0b7073b63671083d1eaf0fc8eb2
Dylan Staley <88163+dstaley@users.noreply.github.com>
2024-04-17 15:22:04 -0700

    feat: use react-design-system-components

--- src/views/product-downloads-view/components/downloads-section/index.tsx
+++ src/views/product-downloads-view/components/downloads-section/index.tsx
@@ -7,7 +7,7 @@
 import { ReactElement, useMemo } from 'react'
 
 // HashiCorp imports
-import CodeBlock from '@hashicorp/react-code-block'
+import CodeBlock from '@hashicorp/react-design-system-components/src/components/code-block'
 import { IconInfo16 } from '@hashicorp/flight-icons/svg-react/info-16'
 
 // Global imports
@@ -57,10 +57,11 @@ const PackageManagerSection = ({
 			<Heading {...SHARED_HEADING_LEVEL_3_PROPS}>Package manager</Heading>
 			{hasOnePackageManager && (
 				<CodeBlock
-					className={s.codeBlock}
-					code={packageManagers[0].installCodeHtml}
+					// className={s.codeBlock}
+					value={packageManagers[0].installCodeHtml}
 					language="shell-session"
-					options={{ showClipboard: true }}
+					hasCopyButton
+					hasLineNumbers={false}
 				/>
 			)}
 			{hasManyPackageManagers && (
@@ -70,9 +71,10 @@ const PackageManagerSection = ({
 							return (
 								<Tab key={label} heading={label}>
 									<CodeBlock
-										code={installCodeHtml}
+										value={installCodeHtml}
 										language="shell-session"
-										options={{ showClipboard: true }}
+										hasCopyButton
+										hasLineNumbers={false}
 									/>
 								</Tab>
 							)
