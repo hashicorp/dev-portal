959e19997757ae2d9868f9cfef6bb1b75f0734bc
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2024-05-15 13:26:31 -0400

    feat: massageRawSchema to rein in protobufAny docs

--- src/pages/hcp/api-docs/vault-secrets/[[...page]].tsx
+++ src/pages/hcp/api-docs/vault-secrets/[[...page]].tsx
@@ -57,6 +57,58 @@ const PAGE_CONFIG: OpenApiDocsPageConfig = {
 			href: 'https://www.hashicorp.com/customer-success',
 		},
 	],
+
+	/**
+	 * TEMPORARY FIX:
+	 * Ideally this content update would be made when the OpenAPI spec file is
+	 * generated, rather than hacked in here. This is intended to quickly
+	 * demo the type of change we might make in the original schema file.
+	 *
+	 * NOTE: this also modifies the incoming `schemaData` in place... would
+	 * generally prefer to take a more "immutable" kind of approach, and deep copy
+	 * and modify the schema data instead... but this is temporary, so taking
+	 * this more hacky and potential-side-effect-y approach for now.
+	 */
+	massageRawSchema: (schemaData: OpenAPIV3.Document) => {
+		console.log(Object.keys(schemaData))
+		/**
+		 * Check to see if we have the protobufAny definition we intend to modify,
+		 * if not then bail early.
+		 */
+		if (
+			!('definitions' in schemaData) ||
+			typeof schemaData.definitions !== 'object'
+		) {
+			return schemaData
+		}
+		if (
+			!('protobufAny' in schemaData.definitions) ||
+			typeof schemaData.definitions.protobufAny !== 'object'
+		) {
+			return schemaData
+		}
+		const { protobufAny } = schemaData.definitions
+		/**
+		 * Modify the description for the `protobufAny` schema (otherwise it's
+		 * very very long)
+		 */
+		if ('description' in protobufAny) {
+			protobufAny.description =
+				'An arbitrary serialized protocol buffer message. See the [protobufAny documentation](https://protobuf.dev/reference/protobuf/google.protobuf/#any) for more information.'
+		}
+		/**
+		 * Modify the description for protobufAny's @type property
+		 */
+		if ('properties' in protobufAny) {
+			if ('description' in protobufAny.properties['@type']) {
+				protobufAny.properties['@type'].description =
+					'A URL that describes the type of the serialized protocol buffer message.'
+			}
+		}
+		// Return the schema data (modifications were made in place)
+		return schemaData
+	},
+
 	/**
 	 * Massage the schema data a little bit, replacing
 	 * "HashiCorp Cloud Platform" in the title with "HCP".
