fe881c8efbe5ada7cba6d557aedbd5afb3aea4d9
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2024-05-23 17:17:01 -0400

    revert change for webhook api, not needed

--- src/pages/hcp/api-docs/webhook/[[...page]].tsx
+++ src/pages/hcp/api-docs/webhook/[[...page]].tsx
@@ -7,18 +7,13 @@
 import { fetchCloudApiVersionData } from 'lib/api-docs/fetch-cloud-api-version-data'
 // View
 import OpenApiDocsView from 'views/open-api-docs-view'
-import {
-	schemaModShortenHcp,
-	schemaModComponent,
-	shortenProtobufAnyDescription,
-} from 'views/open-api-docs-view/utils/massage-schema-utils'
+import { schemaModShortenHcp } from 'views/open-api-docs-view/utils/massage-schema-utils'
 import {
 	getStaticPaths,
 	getStaticProps as getOpenApiDocsStaticProps,
 } from 'views/open-api-docs-view/server'
 // Types
 import type { GetStaticProps, GetStaticPropsContext } from 'next'
-import type { OpenAPIV3 } from 'openapi-types'
 import type {
 	OpenApiDocsParams,
 	OpenApiDocsViewProps,
@@ -54,32 +49,10 @@ const PAGE_CONFIG: OpenApiDocsPageConfig = {
 		},
 	],
 	/**
-	 * Massage the schema data a little bit
+	 * Massage the schema data a little bit, replacing
+	 * "HashiCorp Cloud Platform" in the title with "HCP".
 	 */
-	massageSchemaForClient: (schemaData: OpenAPIV3.Document) => {
-		//  Replace "HashiCorp Cloud Platform" with "HCP" in the title
-		const withShortTitle = schemaModShortenHcp(schemaData)
-		/**
-		 * Shorten the description of the protobufAny schema
-		 *
-		 * Note: ideally this would be done at the content source,
-		 * but until we've got that work done, this shortening
-		 * seems necessary to ensure incremental static regeneration works
-		 * for past versions of the API docs. Without this shortening,
-		 * it seems the response size ends up crossing a threshold that
-		 * causes the serverless function that renders the page to fail.
-		 *
-		 * Related task:
-		 * https://app.asana.com/0/1207339219333499/1207339701271604/f
-		 */
-		const withShortProtobufDocs = schemaModComponent(
-			withShortTitle,
-			'google.protobuf.Any',
-			shortenProtobufAnyDescription
-		)
-		// Return the schema data with modifications
-		return withShortProtobufDocs
-	},
+	massageSchemaForClient: schemaModShortenHcp,
 }
 
 /**
