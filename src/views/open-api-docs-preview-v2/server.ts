5d0959364b6cd4f9b5755dfb925a31594803fd0f
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2024-06-18 15:24:29 -0400

    clean up preview-v2 server file a bit

--- src/views/open-api-docs-preview-v2/server.ts
+++ src/views/open-api-docs-preview-v2/server.ts
@@ -7,14 +7,28 @@
 import type { GetServerSidePropsContext, GetServerSidePropsResult } from 'next'
 import type { OpenApiDocsViewProps } from 'views/open-api-docs-view-v2/types'
 
+/**
+ * We expect the `HASHI_ENV` environment variable to be set to `production`
+ * only if we're actually in production.
+ */
 const IS_PRODUCTION = process.env.HASHI_ENV === 'production'
+
+/**
+ * For reference on Vercel system environment variables, see:
+ * https://vercel.com/docs/projects/environment-variables/system-environment-variables
+ */
 const IS_VERCEL_DEPLOY = process.env.VERCEL_ENV !== 'development'
 const BASE_URL = IS_VERCEL_DEPLOY
 	? `https://${process.env.VERCEL_URL}`
-	: 'http://localhost:3000'
+	: 'http://localhost:3000' // we assume port 3000... maybe a better way?
 
 /**
- * TODO: add description
+ * Build server-side props for a revised version of the OpenAPI docs view.
+ *
+ * This revised version splits each operation into its own URL. We also have
+ * an "overview" page when a specific operation isn't being viewed. There are
+ * common props between the overview and operation pages, such as the
+ * sidebarItemGroups. There are also props specific to each view.
  */
 export async function getServerSideProps({
 	params,
@@ -27,11 +41,13 @@ export async function getServerSideProps({
 		sidebarItemGroups?: $TSFixMe
 	}>
 > {
-	// In production, return a 404 not found for this page.
-	// In other environments (local, preview, and staging), show the page.
-	// if (IS_PRODUCTION) {
-	// 	return { notFound: true }
-	// }
+	/**
+	 * In production, return a 404 not found for this page.
+	 * In other environments (local, preview, and staging), show the page.
+	 */
+	if (IS_PRODUCTION) {
+		return { notFound: true }
+	}
 	// Determine which operation we're trying to render, if any
 	const operationSlug = params?.page?.length ? params.page[0] : ''
 	/**
@@ -44,8 +60,11 @@ export async function getServerSideProps({
 	 */
 	const uniqueFileId = req.cookies['open-api-docs-preview-v2_unique-file-id']
 
-	// Fetch the static props from our API route, which will read in a tmp
-	// file if it exists, or return null if it does not.
+	/**
+	 * Fetch the static props from our API route, which will read in a tmp
+	 * file based on the provided `uniqueFileId`. If this file does not
+	 * exist, we return `null` (and the user must re-upload an OpenAPI spec).
+	 */
 	let staticProps: OpenApiDocsViewProps | null = null
 	try {
 		const response = await fetch(
@@ -63,6 +82,12 @@ export async function getServerSideProps({
 	}
 
 	// Otherwise, we have static props, so we can process them
+
+	/**
+	 * First we set up `sidebarItemGroups`, which are used to render the sidebar.
+	 * These props are needed on the overview page, as well as on each individual
+	 * operation page.
+	 */
 	const { operationGroups } = staticProps
 	const sidebarItemGroups =
 		operationGroups?.map((group) => {
@@ -81,7 +106,10 @@ export async function getServerSideProps({
 				items,
 			}
 		}) || []
-	// If we have an operationSlug, try to get the associated operationProps
+
+	/**
+	 * If we have an operationSlug, try to get the associated operationProps
+	 */
 	let operationProps = null
 	if (operationSlug) {
 		operationProps = operationGroups
@@ -89,8 +117,8 @@ export async function getServerSideProps({
 			.flat()
 			.find((item) => item.operationId === operationSlug)
 	}
-	// Note that `staticProps` may be `null`, if the user has not yet provided
-	// an OpenAPI spec file to preview
+
+	// Return our bundle of props
 	return {
 		props: { staticProps, operationSlug, operationProps, sidebarItemGroups },
 	}
