69271abae8c91524a9b91b759106d3b32ded989b
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2024-07-26 15:19:20 -0400

    add upload-source-maps script

--- scripts/upload-source-maps.ts
+++ scripts/upload-source-maps.ts
@@ -5,49 +5,61 @@
 import { execSync } from 'child_process'
 
 /**
- * Uploads source maps to Datadog and then deletes source maps to prevent bundle size increase and leaking of source code.
+ * Set up constants based on environment variables
+ *
+ * Note that dd stands for DataDog, many constants are used for DataDog.
+ * Docs on Vercel System Environment Variables:
+ * https://vercel.com/docs/projects/environment-variables/system-environment-variables
  */
-const main = () => {
-	if (
-		typeof process.env.DD_API_KEY === 'undefined' ||
-		typeof process.env.VERCEL_ENV === 'undefined' ||
-		process.env.VERCEL_ENV === 'development'
-	) {
-		return
-	}
+const VERCEL_ENV = process.env.VERCEL_ENV
+const IS_DEV = VERCEL_ENV === 'development'
+const COMMIT_SHA = IS_DEV
+	? execSync('git rev-parse HEAD')
+	: process.env.VERCEL_GIT_COMMIT_SHA
+const DD_SERVICE_MAP = {
+	development: 'non-prod.developer.hashicorp.com',
+	preview: 'non-prod.developer.hashicorp.com',
+	production: 'developer.hashicorp.com',
+}
+const DD_API_KEY = process.env.DD_API_KEY
+const DD_DRY_RUN = IS_DEV ? '--dry-run' : ''
+const DD_SERVICE = DD_SERVICE_MAP[VERCEL_ENV]
 
-	const LATEST_SHA = process.env.VERCEL_GIT_COMMIT_SHA
-	const PATH_PREFIX =
-		process.env.VERCEL_ENV === 'production'
-			? 'https://developer.hashicorp.com/_next/static/'
-			: `https://${process.env.VERCEL_BRANCH_URL}/_next/static/`
-	const SERVICE =
-		process.env.VERCEL_ENV === 'production'
-			? 'developer.hashicorp.com'
-			: 'non-prod.developer.hashicorp.com'
-
-	const DATADOG_API_KEY = process.env.DD_API_KEY
-
+/**
+ * Upload source maps to DataDog using the `datadog-ci` CLI.
+ *
+ * The DataDog service to which we upload source maps will vary based on
+ * the environment we're in, to correspond with the DataDog setup we have
+ * in our `config` directory.
+ *
+ * - If we're in development, we'll execute a "dry run", no upload will occur.
+ * - If we're in preview, we'll use `non-prod.developer.hashicorp.com`
+ * - If we're in production, we'll use `developer.hashicorp.com`
+ */
+function main() {
 	try {
+		/**
+		 * For some reason, in Vercel build contexts, there don't seem to be any
+		 * git remotes available, and we get a warning from DataDog, and no links
+		 * back to source in the DataDog UI. To fix this, we manually add the
+		 * expected git remote here, solely for the purpose of DataDog.
+		 *
+		 * Note: we could in theory construct this from Vercel System env variables,
+		 * but it feels a little easier to hard-code it.
+		 */
 		execSync(
-			`DATADOG_API_KEY=${DATADOG_API_KEY} npx @datadog/datadog-ci sourcemaps upload .next/static/ --service=${SERVICE} --release-version=${LATEST_SHA} --minified-path-prefix=${PATH_PREFIX} --disable-git`,
+			`git remote add origin https://github.com/hashicorp/dev-portal.git`
+		)
+		// Run the upload command
+		execSync(
+			`DATADOG_API_KEY=${DD_API_KEY} ./node_modules/.bin/datadog-ci sourcemaps upload .next/static/ --service="${DD_SERVICE}" --release-version="${COMMIT_SHA}" --minified-path-prefix="/_next/static/" ${DD_DRY_RUN}`,
 			{ stdio: 'inherit' }
 		)
-
-		console.log('Source maps uploaded successfully')
+		// Remove source maps after uploading
+		execSync(`rm -f .next/static/**/*.map`)
 	} catch (error) {
+		console.error('Error: Failed to upload and then remove source maps.')
 		console.error(error)
-
-		console.log('Failed to upload sourcemaps')
-	}
-
-	// delete sourcemaps
-	try {
-		execSync(`rm -f .next/static/**/*.js.map`)
-	} catch (error) {
-		console.error(error)
-
-		console.log('Failed to delete sourcemaps')
 	}
 }
 
