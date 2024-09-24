bb9eff288c272739f285306494f074ce5fe479ee
Heat Hamilton <heat.hamilton@hashicorp.com>
2024-09-24 16:45:36 -0400

    Fix 404 error for Sentinel tutorials

--- src/pages/[productSlug]/tutorials/index.tsx
+++ src/pages/[productSlug]/tutorials/index.tsx
@@ -39,7 +39,7 @@ export async function getStaticProps({
 	 */
 	let props
 	try {
-		const props =
+		props =
 			productData.slug == 'hcp'
 				? await getCloudTutorialsViewProps()
 				: await getProductTutorialsViewProps(productData as LearnProductData)
