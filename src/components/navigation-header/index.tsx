0487b4c5b78b873ab89faa040314e4252eb3ab6d
Leah Bush <leah.bush@hashicorp.com>
2024-10-10 14:58:20 -0500

    WIP

--- src/components/navigation-header/index.tsx
+++ src/components/navigation-header/index.tsx
@@ -4,7 +4,7 @@
  */
 
 // Third-party imports
-import { useRouter } from 'next/router'
+// import { useRouter } from 'next/router'
 
 // HashiCorp imports
 import { IconMenu24 } from '@hashicorp/flight-icons/svg-react/menu-24'
@@ -82,13 +82,12 @@ const AuthenticationControls = () => {
  * `/{productSlug}.`
  */
 const NavigationHeader = () => {
-	const router = useRouter()
+	// const router = useRouter()
 	const currentProduct = useCurrentProduct()
 
-	console.log('### router', router)
+	// console.log('### router', router)
 
-	const shouldRenderGenericHeaderContent =
-		!currentProduct || router.route === '/_error'
+	const shouldRenderGenericHeaderContent = !currentProduct
 	const LeftSideHeaderContent = shouldRenderGenericHeaderContent
 		? HomePageHeaderContent
 		: ProductPageHeaderContent
