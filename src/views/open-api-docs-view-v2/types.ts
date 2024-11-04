3ca98db6130623703a892875c3e5c72d16455833
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2024-11-04 11:44:23 -0500

    OpenAPI V2 - implement landing content (#2606)
    * port over components from existing view
    
    * implement landing-content to match upstream
    
    * add demo data to preview

--- src/views/open-api-docs-view-v2/types.ts
+++ src/views/open-api-docs-view-v2/types.ts
@@ -4,11 +4,12 @@
  */
 
 // Types
-import type { LandingContentProps } from './components/landing-content'
 import type { OpenAPIV3 } from 'openapi-types'
 import type { OperationContentProps } from './components/operation-content'
+import type { LandingContentProps } from './components/landing-content'
 import type { OperationObject } from './utils/get-operation-objects'
 import type { ProductData, ProductSlug } from 'types/products'
+import type { BreadcrumbLink } from '@components/breadcrumb-bar'
 
 /**
  * Shared props are common to both the "landing" and "operation" views.
@@ -19,6 +20,32 @@ export interface SharedProps {
 		text: string
 		href: string
 	}
+	breadcrumbLinks: {
+		title: string
+		/**
+		 * Note: our BreadcrumbBar component has an interface that implies it
+		 * supports items without `url` values, but it actually filters out those
+		 * items and never renders them. We likely want to update either the
+		 * interface, to require URLs, or the component, to render items without
+		 * URLs. The latter might make sense if we're conceptualizing breadcrumb
+		 * links as serving both a navigational AND "orienting" purpose, as we
+		 * can then show an accurate hierarchical structure, even if a given page
+		 * in that structure doesn't exist (eg `/hcp/api-docs`).
+		 *
+		 * For example...
+		 *
+		 * When strictly requiring URLs, we might have:
+		 * URL: `/hcp/api-docs/vault-secrets`
+		 * Breadcrumb: `Developer > HCP > Vault Secrets`
+		 * This breadcrumb does NOT show that we're looking at API docs.
+		 *
+		 * When allowing items without URLs, we might have:
+		 * URL: `/hcp/api-docs/vault-secrets`
+		 * Breadcrumb: `Developer > HCP > API Docs > Vault Secrets`
+		 */
+		url?: string
+		isCurrentPage?: boolean
+	}[]
 	landingLink: {
 		text: string
 		href: string
@@ -42,13 +69,28 @@ export interface SharedProps {
 }
 
 /**
+ * Configure a status indicator for a status-page service.
+ */
+export interface StatusIndicatorConfig {
+	/**
+	 * A status-page component URL we can GET JSON data from, in the format
+	 * `https://status.hashicorp.com/api/v2/components/{componentId}.json`.
+	 */
+	endpointUrl: string
+	/**
+	 * A browser-friendly status page URL, like `https://status.hashicorp.com`
+	 */
+	pageUrl: string
+}
+
+/**
  * OpenApiDocsViewV2 props are used to render either a "landing" view, which
  * includes some introductory content to the API generally, or an "operation"
  * view, which includes details about specific operations.
  */
 export type OpenApiDocsViewV2Props =
 	| (SharedProps & { operationContentProps: OperationContentProps })
-	| (SharedProps & { landingContentProps: LandingContentProps })
+	| (SharedProps & { landingProps: LandingContentProps })
 
 /**
  * OpenApiDocsViewV2Config is used to set up and configure a set of
@@ -65,6 +107,24 @@ export interface OpenApiDocsViewV2Config {
 	 * `developer.hashicorp.com/hcp/api-docs/hcp-vault-secrets` has the basePath
 	 * `/hcp/api-docs/hcp-vault-secrets`. */
 	basePath: string
+	/**
+	 * Optional breadcrumb links to render before the generated breadcrumb links.
+	 * We build breadcrumbs like `[...breadcrumbLinksPrefix, ...generatedLinks]`,
+	 * where `generatedLinks` are:
+	 * - `<basePath>` - the OpenAPI spec's `title` will be used as text
+	 * - `<basePath>/<operationSlug>` - operation slugs used as text
+	 *
+	 * As an example, for HCP Vault Secrets, we might set `breadcrumbLinksPrefix`
+	 * to include links leading up to the basePath, like:
+	 * - { title: 'Developer', url: '/' },
+	 * - { title: 'HCP', url: '/hcp' },
+	 * - { title: 'API Docs', url: '/hcp/api-docs' }
+	 * And, for example, the generated breadcrumbs that would be added for a
+	 * `SetTier` operation page might look like:
+	 * - { title: 'HCP Vault Secrets', url: '/hcp/api-docs/vault-secrets' }
+	 * - { title: 'SetTier', url: '/hcp/api-docs/vault-secrets/SetTier' }
+	 */
+	breadcrumbLinksPrefix?: BreadcrumbLink[]
 	/**
 	 * Optional link to move up a level of context. Typically used to link
 	 * back to higher level documentation for the subject of the OpenAPI docs. k
@@ -111,4 +171,13 @@ export interface OpenApiDocsViewV2Config {
 		text: string
 		href: string
 	}[]
+	/**
+	 * Configuration to set up a status indicator.
+	 */
+	statusIndicatorConfig?: StatusIndicatorConfig
+	/**
+	 * Optional release stage to display in a badge at the top of the page.
+	 * Typical values include `Stable`, `Preview`, etc.
+	 */
+	releaseStage?: string
 }
