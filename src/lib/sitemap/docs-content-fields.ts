cb55e3299ac88067bbd1291fa3ab043b5502eb67
Leah Bush <leah.bush@hashicorp.com>
2024-12-13 15:22:44 -0600

    feat: fetch paths from UDR all-docs-paths

--- src/lib/sitemap/docs-content-fields.ts
+++ src/lib/sitemap/docs-content-fields.ts
@@ -6,14 +6,45 @@
 import { makeSitemapField } from './helpers'
 
 export async function allDocsFields() {
-	const getDocsPaths = await fetch(
+	const getContentAPIDocsPaths = await fetch(
 		`${process.env.MKTG_CONTENT_DOCS_API}/api/all-docs-paths`
 	)
-	const { result: docsResult } = await getDocsPaths.json()
-	return docsResult.map((page: { path: string; created_at: string }) =>
-		makeSitemapField({
-			slug: `${page.path}`,
-			lastmodDate: page.created_at,
-		})
+	const { result: contentAPIDocsResult } = await getContentAPIDocsPaths.json()
+
+	// If there are docs that have been migrated to the unified docs repo, get the paths for those docs
+	// and merge them with the paths for the docs that haven't been migrated from the content API
+	if (__config.flags?.unified_docs_migrated_repos.length > 0) {
+		const getUDRDocsPaths = await fetch(
+			`${process.env.UNIFIED_DOCS_API}/api/all-docs-paths`
+		)
+		const { result: udrDocsResult } = await getUDRDocsPaths.json()
+
+		// Filter the result from the content API to take out the duplicate paths between
+		// the content API and UDR
+		const filteredContentAPIDocsResult = contentAPIDocsResult.filter(
+			(item: { path: string; created_at: string }) => {
+				return !udrDocsResult.find(
+					(URDItem: { path: string; created_at: string }) => {
+						return URDItem.path === item.path
+					}
+				)
+			}
+		)
+
+		const allDocsData = [...filteredContentAPIDocsResult, ...udrDocsResult]
+		return allDocsData.map((page: { path: string; created_at: string }) =>
+			makeSitemapField({
+				slug: `${page.path}`,
+				lastmodDate: page.created_at,
+			})
+		)
+	}
+
+	return contentAPIDocsResult.map(
+		(page: { path: string; created_at: string }) =>
+			makeSitemapField({
+				slug: `${page.path}`,
+				lastmodDate: page.created_at,
+			})
 	)
 }
