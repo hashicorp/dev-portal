7fd1668e37dd9545e2e6248bfba600353cd0476f
Leah Bush <leah.bush@hashicorp.com>
2025-01-08 13:57:35 -0600

    filter results from content API

--- src/lib/sitemap/docs-content-fields.ts
+++ src/lib/sitemap/docs-content-fields.ts
@@ -6,41 +6,33 @@
 import { makeSitemapField } from './helpers'
 
 export async function allDocsFields() {
-	const getContentAPIDocsPaths = await fetch(
-		`${process.env.MKTG_CONTENT_DOCS_API}/api/all-docs-paths`
-	)
-	const { result: contentAPIDocsResult } = await getContentAPIDocsPaths.json()
-
 	// If there are docs that have been migrated to the unified docs repo, get the paths for those docs
 	// and merge them with the paths for the docs that haven't been migrated from the content API
 	if (
 		process.env.HASHI_ENV === 'unified-docs-sandbox' &&
 		__config.flags?.unified_docs_migrated_repos.length > 0
 	) {
-		const productsFilter = __config.flags.unified_docs_migrated_repos.map(
+		const contentAPIFilter = __config.flags.unified_docs_migrated_repos.map(
 			(repo) => {
-				return `products=${repo}`
+				return `filterOut=${repo}`
 			}
 		)
-		const productsFilterString = productsFilter.join('&')
+		const contentAPIFilterString = contentAPIFilter.join('&')
+		const getContentAPIDocsPaths = await fetch(
+			`${process.env.MKTG_CONTENT_DOCS_API}/api/all-docs-paths?${contentAPIFilterString}`
+		)
+		const { result: contentAPIDocsResult } = await getContentAPIDocsPaths.json()
+
+		const UDRFilter = __config.flags.unified_docs_migrated_repos.map((repo) => {
+			return `products=${repo}`
+		})
+		const UDRFilterString = UDRFilter.join('&')
 		const getUDRDocsPaths = await fetch(
-			`${process.env.UNIFIED_DOCS_API}/api/all-docs-paths?${productsFilterString}`
+			`${process.env.UNIFIED_DOCS_API}/api/all-docs-paths?${UDRFilterString}`
 		)
 		const { result: udrDocsResult } = await getUDRDocsPaths.json()
 
-		// Filter the result from the content API to take out the duplicate paths between
-		// the content API and UDR
-		const filteredContentAPIDocsResult = contentAPIDocsResult.filter(
-			(item: { path: string; created_at: string }) => {
-				return !udrDocsResult.find(
-					(URDItem: { path: string; created_at: string }) => {
-						return URDItem.path === item.path
-					}
-				)
-			}
-		)
-
-		const allDocsData = [...filteredContentAPIDocsResult, ...udrDocsResult]
+		const allDocsData = [...contentAPIDocsResult, ...udrDocsResult]
 		return allDocsData.map((page: { path: string; created_at: string }) =>
 			makeSitemapField({
 				slug: `${page.path}`,
@@ -48,6 +40,11 @@ export async function allDocsFields() {
 			})
 		)
 	}
+
+	const getContentAPIDocsPaths = await fetch(
+		`${process.env.MKTG_CONTENT_DOCS_API}/api/all-docs-paths`
+	)
+	const { result: contentAPIDocsResult } = await getContentAPIDocsPaths.json()
 
 	return contentAPIDocsResult.map(
 		(page: { path: string; created_at: string }) =>
