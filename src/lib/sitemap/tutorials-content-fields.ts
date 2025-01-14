2c8940bbcba5db782cf882ec04991e699764aef8
Leah Bush <leah.bush@hashicorp.com>
2025-01-14 16:09:03 -0600

    revert errors

--- src/lib/sitemap/tutorials-content-fields.ts
+++ src/lib/sitemap/tutorials-content-fields.ts
@@ -28,7 +28,6 @@ async function getCollectionPaths() {
 }
 
 export async function allTutorialsFields(config: typeof __config) {
-	throw new Error('tutorials error')
 	const landingSlugs = getTutorialLandingPaths()
 	const collectionSlugs = await getCollectionPaths()
 	const tutorialSlugs = Object.values(tutorialMap)
