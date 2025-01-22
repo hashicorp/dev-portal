6b0884697569be509e2013b5193e235bb803cd93
Tu Nguyen <im2nguyen@gmail.com>
2025-01-22 08:57:17 -0800

    Temp remove GS content from Vagrant to update Vagrant GS

--- src/pages/tutorials/library.tsx
+++ src/pages/tutorials/library.tsx
@@ -35,7 +35,6 @@ const DEFAULT_SLUGS = [
 	'vault/why-use-vault',
 	'consul/get-started',
 	'nomad/get-started-install',
-	'vagrant/development-environment',
 	'packer/get-started-install-cli',
 	'boundary/getting-started-intro',
 	'waypoint/get-started-intro',
