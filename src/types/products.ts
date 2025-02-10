7a101d50b011079b67a5b2f443eb2837eef5d012
Tu Nguyen <im2nguyen@gmail.com>
2025-02-10 13:46:17 -0800

    fix build error

--- src/types/products.ts
+++ src/types/products.ts
@@ -19,7 +19,7 @@ type ProductName =
 	| 'Waypoint'
 	| 'HCP Vault Secrets'
 
-type ProductSlug = Exclude<Products, 'hashicorp'> | 'hcp'
+type ProductSlug = Exclude<Products, 'hashicorp'> | 'hcp' | 'sentinel'
 
 interface Product extends ProductMeta {
 	name: ProductName
