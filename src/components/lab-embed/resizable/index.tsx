72ad51ec6efc81e92e1a812b6fad55f79c607b3b
Tu Nguyen <im2nguyen@gmail.com>
2025-03-20 00:09:53 -0700

    posthog tracking experiment

--- src/components/lab-embed/resizable/index.tsx
+++ src/components/lab-embed/resizable/index.tsx
@@ -33,7 +33,7 @@ export default function Resizable({
 	initialHeight = 400,
 }: ResizableProps) {
 	const { closeLab } = useInstruqtEmbed()
-
+	// State for resizable panel while in `panel`-mode
 	const minimumHeight = 300
 	const maximumHeight = 910
 	const [downMouseY, setDownMouseY] = useState(0)
@@ -41,7 +41,6 @@ export default function Resizable({
 	const [height, setHeight] = useState(initialHeight)
 	const [previousHeight, setPreviousHeight] = useState(initialHeight)
 	const [isResizing, setIsResizing] = useState(false)
-	const [isMobile, setIsMobile] = useState(false)
 
 	const resizableDiv = useRef<HTMLDivElement>(null)
 
@@ -80,17 +79,36 @@ export default function Resizable({
 		}
 	}, [moveMouseY, downMouseY, previousHeight, isMobile])
 
-	const resize = useCallback(
-		(e: MouseEvent) => {
-			if (isMobile) return
+	function enableResize(e) {
+		e.preventDefault()
+		// We need to know where the mouse started from since default state was just 0
+		setDownMouseY(e.screenY)
+		// Track the fact that we are resizing
+		// This keeps our cursor on our `<Resizer/>`
+		// This adds a class to the content our mouse may otherwise wander into via `pointer-events: none`
+		setIsResizing(true)
+		// Once we're clientside add the event listeners needed during a resize
+		addListeners()
+	}
 
 			setMoveMouseY(e.screenY)
 		},
 		[isMobile]
 	)
 
-	const stopResize = useCallback(() => {
+	function stopResize() {
+		// We stopped resizing so it'd be great to be able to use the content inside the resizable ref ;)
 		setIsResizing(false)
+		// We no longer want our event listeners
+		removeListeners()
+	}
+	// Adds the necessary event listeners during a resize
+	function addListeners() {
+		window.addEventListener('mousemove', resize)
+		window.addEventListener('mouseup', stopResize)
+	}
+	// Removes resize-related event listeners
+	function removeListeners() {
 		window.removeEventListener('mousemove', resize)
 		window.removeEventListener('mouseup', stopResize)
 	}, [resize])
