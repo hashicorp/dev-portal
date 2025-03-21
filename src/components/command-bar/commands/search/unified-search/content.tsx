62e017c773c16d573e4d5d98efa7c6fb3fa6ad27
Leah Bush <leah.bush@hashicorp.com>
2025-03-21 10:49:35 -0500

    update tab name and icon

--- src/components/command-bar/commands/search/unified-search/content.tsx
+++ src/components/command-bar/commands/search/unified-search/content.tsx
@@ -8,6 +8,7 @@ import { IconDocs16 } from '@hashicorp/flight-icons/svg-react/docs-16'
 import { IconGrid16 } from '@hashicorp/flight-icons/svg-react/grid-16'
 import { IconLearn16 } from '@hashicorp/flight-icons/svg-react/learn-16'
 import { IconPipeline16 } from '@hashicorp/flight-icons/svg-react/pipeline-16'
+import { IconSupport16 } from '@hashicorp/flight-icons/svg-react/support-16'
 // Types
 import type { ReactElement } from 'react'
 import type { UnifiedSearchableContentType } from './types'
@@ -39,7 +40,7 @@ export const tabContentByType: Record<
 		icon: <IconPipeline16 />,
 	},
 	knowledgebase: {
-		heading: 'Zendesk',
-		icon: <IconDocs16 />,
+		heading: 'Support',
+		icon: <IconSupport16 />,
 	},
 }
