68551a943ee9f7e72f361217aff6ffb7d8e8d256
Tu Nguyen <im2nguyen@gmail.com>
2025-03-24 14:03:50 -0700

    move posthog events to dedicated lib, use enum instead of string for consistency

--- src/lib/analytics.ts
+++ src/lib/analytics.ts
@@ -133,24 +133,4 @@ const trackProductDownload = ({
 	})
 }
 
-/**
- * Tracks sandbox events using PostHog
- */
-export enum SANDBOX_EVENT = {
-  SANDBOX_STARTED = 'sandbox_started',
-  SANDBOX_OPEN = 'sandbox_open',
-  SANDBOX_CLOSED = 'sandbox_closed',
-}
-export const trackSandboxEvent = (
-	eventName: `${SANDBOX_EVENT}`,
-	properties: {
-		labId: string
-		page: string
-	}
-): void => {
-	if (window?.posthog?.capture) {
-		window.posthog.capture(eventName, properties)
-	}
-}
-
 export { safeAnalyticsTrack, trackProductDownload }
