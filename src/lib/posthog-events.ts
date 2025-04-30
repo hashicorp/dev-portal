8595ff86da0bb37361120b35cece4071c1fb146c
Tu Nguyen <im2nguyen@gmail.com>
2025-04-30 16:38:41 -0700

    update broken tracking/merge error

--- src/lib/posthog-events.ts
+++ src/lib/posthog-events.ts
@@ -3,48 +3,6 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
-import posthog from 'posthog-js'
-
-/**
- * Enables PostHog video start tracking
- */
-export function trackVideoStart(url: string): void {
-	let video_host = ''
-	if (url.includes('wistia')) {
-		video_host = 'wistia'
-	} else if (url.includes('youtube')) {
-		video_host = 'youtube'
-	}
-
-	if (video_host) {
-		posthog.capture('video_start', {
-			video_host,
-			url,
-		})
-	}
-}
-
-/**
- * Enables PostHog video play tracking
- * This is different from the video start event because it is triggered
- * when the user actually plays the video (start and after play after pause).
- */
-export function trackVideoPlay(url: string): void {
-	let video_host = ''
-	if (url.includes('wistia')) {
-		video_host = 'wistia'
-	} else if (url.includes('youtube')) {
-		video_host = 'youtube'
-	}
-
-	if (video_host) {
-		posthog.capture('video_play', {
-			video_host,
-			url,
-		})
-	}
-}
-
 /**
  * Tracks sandbox events using PostHog
  */
@@ -63,5 +21,49 @@ export const trackSandboxEvent = (
 ): void => {
 	if (window?.posthog?.capture) {
 		window.posthog.capture(eventName, properties)
+	}
+}
+
+/**
+ * Enables PostHog video start tracking
+ */
+export function trackVideoStart(url: string): void {
+	if (!window?.posthog) return
+
+	let video_host = ''
+	if (url.includes('wistia')) {
+		video_host = 'wistia'
+	} else if (url.includes('youtube')) {
+		video_host = 'youtube'
+	}
+
+	if (video_host) {
+		window.posthog.capture('video_start', {
+			video_host,
+			url,
+		})
+	}
+}
+
+/**
+ * Enables PostHog video play tracking
+ * This is different from the video start event because it is triggered
+ * when the user actually plays the video (start and after play after pause).
+ */
+export function trackVideoPlay(url: string): void {
+	if (!window?.posthog) return
+
+	let video_host = ''
+	if (url.includes('wistia')) {
+		video_host = 'wistia'
+	} else if (url.includes('youtube')) {
+		video_host = 'youtube'
+	}
+
+	if (video_host) {
+		window.posthog.capture('video_play', {
+			video_host,
+			url,
+		})
 	}
 }
