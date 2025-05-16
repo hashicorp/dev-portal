b1d6617b4c6055252f009c419a77c62d810a4b11
Radek Simko <radeksimko@users.noreply.github.com>
2025-05-16 09:09:06 +0100

    views: Update Ubuntu/Debian command to support Linux Mint (#2753)

--- src/views/product-downloads-view/helpers.ts
+++ src/views/product-downloads-view/helpers.ts
@@ -48,7 +48,7 @@ export const generateDefaultPackageManagers = (
 			label: 'Ubuntu/Debian',
 			commands: [
 				`wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg`,
-				`echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list`,
+				`echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list`,
 				`sudo apt update && sudo apt install ${productSlug}`,
 			],
 			os: 'linux',
