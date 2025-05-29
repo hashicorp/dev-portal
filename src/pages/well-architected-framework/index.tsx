261bfda6ccd4d6431afd4369dd2a44ab6ec0ae5d
Tu Nguyen <im2nguyen@gmail.com>
2025-05-29 01:08:58 -0700

    Switch WAF to use docs layout instead of tutorials, migrate to UDR

--- src/pages/well-architected-framework/index.tsx
+++ src/pages/well-architected-framework/index.tsx
@@ -3,78 +3,19 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
-import slugify from 'slugify'
-import { getCollectionsBySection } from 'lib/learn-client/api/collection'
-import { stripUndefinedProperties } from 'lib/strip-undefined-props'
-import { PageSlugOption } from 'lib/learn-client/api/page'
-import WellArchitectedFrameworkLandingView from 'views/well-architected-framework'
-import { buildCategorizedWafSidebar } from 'views/well-architected-framework/utils/generate-sidebar-items'
-import { WellArchitectedFrameworkLandingProps } from 'views/well-architected-framework/types'
-import rawWafContent from 'content/well-architected-framework/index.json'
-import wafData from 'data/well-architected-framework.json'
-import getProcessedPageData from 'views/product-tutorials-view/helpers/page-data'
-import { TableOfContentsHeading } from 'components/table-of-contents'
-import outlineItemsFromHeadings from 'components/outline-nav/utils/outline-items-from-headings'
+import { getStaticProps } from 'views/product-root-docs-path-landing/server'
+import ProductRootDocsPathLanding from 'views/product-root-docs-path-landing'
 
-export async function getStaticProps(): Promise<{
-	props: WellArchitectedFrameworkLandingProps
-}> {
-	const { pageData, headings: pageHeadings } = await getProcessedPageData(
-		wafData.slug as PageSlugOption,
-		{ showOverviewHeading: false }
-	)
-	const wafCollections = await getCollectionsBySection(wafData.slug)
-
-	/**
-	 * Build and add the slug for the overview heading
-	 */
-	const wafContent = {
-		...rawWafContent,
-		landingPage: {
-			...rawWafContent.landingPage,
-			overview: {
-				...rawWafContent.landingPage.overview,
-				headingSlug: slugify(rawWafContent.landingPage.overview.heading, {
-					lower: true,
-				}),
-			},
-		},
-	}
-
-	const headings: TableOfContentsHeading[] = [
-		{
-			title: wafContent.landingPage.overview.heading,
-			level: 2,
-			slug: wafContent.landingPage.overview.headingSlug,
-		},
-		...pageHeadings,
-	]
-	const breadcrumbLinks = [
-		{ title: 'Developer', url: '/' },
-		{ title: wafData.name, url: `/${wafData.slug}`, isCurrentPage: true },
-	]
-
-	return {
-		props: stripUndefinedProperties({
-			metadata: {
-				title: wafData.name,
-				name: wafData.name,
-				slug: wafData.slug,
-			},
-			data: {
-				pageData,
-				wafContent: wafContent.landingPage,
-			},
-			outlineItems: outlineItemsFromHeadings(headings),
-			layoutProps: {
-				breadcrumbLinks,
-				sidebarSections: buildCategorizedWafSidebar(
-					wafCollections,
-					wafContent.sidebarCategories
-				),
-			},
-		}),
-	}
+// Pre-configured getStaticProps for well-architected-framework/docs
+const getStaticPropsWrapped = async (context) => {
+	return getStaticProps({
+		...context,
+		params: {
+			productSlug: 'well-architected-framework',
+			rootDocsPath: ''
+		}
+	})
 }
 
-export default WellArchitectedFrameworkLandingView
+export { getStaticPropsWrapped as getStaticProps }
+export default ProductRootDocsPathLanding 
