b13473ca956db461267c8fc9de1ddd27afc1fee7
Tu Nguyen <im2nguyen@gmail.com>
2025-06-16 09:22:00 -0700

    add commands to side nav

---
+++ src/pages/nomad/commands/[...page].tsx
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import DocsView from 'views/docs-view'
+import { getRootDocsPathGenerationFunctions } from 'views/docs-view/utils/get-root-docs-path-generation-functions'
+
+const { getStaticPaths, getStaticProps } = getRootDocsPathGenerationFunctions(
+	'nomad',
+	'commands'
+)
+
+export { getStaticProps, getStaticPaths }
+export default DocsView
