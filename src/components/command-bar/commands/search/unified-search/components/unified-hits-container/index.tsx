0e0380d6e48718ca5d5eb3a8316e68ac82951c0a
Leah Bush <157434496+LeahMarieBush@users.noreply.github.com>
2025-07-24 09:05:26 -0500

    chore: cleanup support text experiment (#2793)

--- src/components/command-bar/commands/search/unified-search/components/unified-hits-container/index.tsx
+++ src/components/command-bar/commands/search/unified-search/components/unified-hits-container/index.tsx
@@ -7,10 +7,6 @@
 import Tabs, { Tab } from 'components/tabs'
 import { CommandBarDivider } from 'components/command-bar/components'
 import { CommandBarList } from 'components/command-bar/components'
-// Experiment: support-tab-text START
-import { useFeatureFlagVariantKey } from 'posthog-js/react'
-import posthog from 'posthog-js'
-// Experiment: support-tab-text END
 // Unified search
 import TabHeadingWithCount from '../tab-heading-with-count'
 import NoResultsMessage from '../no-results-message'
@@ -36,44 +32,19 @@ export function UnifiedHitsContainer({
 	tabsData: UnifiedSearchTabContent[]
 	suggestedPages: SuggestedPageProps[]
 }) {
-	// Experiment: support-tab-text START
-	const featureFlagKey = useFeatureFlagVariantKey(
-		'support-tab-text'
-	)
-
-	const handleChange = (newActiveIndex: number) => {
-		// The index of the "Support" tab is 4, so we want to send an event to posthog
-		// when the users clicks this tab
-		if(newActiveIndex === 4) {
-			// Send event to posthog
-			posthog.capture('experiment_activated', {
-				id: 'support-tab-text',
-				flagKey: featureFlagKey,
-			})
-		}
-	}
-	// Experiment: support-tab-text END
 
 	return (
 		<div className={s.tabsWrapper}>
-			<Tabs showAnchorLine={false} variant="compact" onChange={handleChange}>
+			<Tabs showAnchorLine={false} variant="compact">
 				{tabsData.map((tabData: UnifiedSearchTabContent) => {
 					const { type, heading, icon, hits, hitCount, otherTabData } = tabData
 					const resultsLabelId = `${type}-search-results-label`
-
-					// Experiment: support-tab-text START
-					const tabHeading =
-						featureFlagKey === 'variant' &&
-						type === SearchContentTypes.KNOWLEDGEBASE
-							? 'Knowledge Base'
-							: heading
-					// Experiment: support-tab-text END
 					return (
 						<Tab
-							heading={tabHeading}
+							heading={heading}
 							headingSlot={
 								<TabHeadingWithCount
-									heading={tabHeading}
+									heading={heading}
 									count={
 										type === SearchContentTypes.GLOBAL ? undefined : hitCount
 									}
@@ -101,7 +72,7 @@ export function UnifiedHitsContainer({
 							) : (
 								<div className={s.noResultsWrapper}>
 									<NoResultsMessage
-										currentTabHeading={tabHeading}
+										currentTabHeading={heading}
 										tabsWithResults={otherTabData}
 									/>
 									<CommandBarDivider className={s.divider} />
