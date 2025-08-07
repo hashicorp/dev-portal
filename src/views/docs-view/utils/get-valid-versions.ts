8c04a55cb978ebf5db1dcf8047168f5b15caea47
Robert Main <50675045+rmainwork@users.noreply.github.com>
2025-08-07 13:33:30 -0400

    Return all versions without filtering
    Remove the version filtering logic for the time being and just return
    ALL versions. This will cause a TON of 404s and is only a temporary
    measure to get things going

--- src/views/docs-view/utils/get-valid-versions.ts
+++ src/views/docs-view/utils/get-valid-versions.ts
@@ -2,13 +2,9 @@
  * Copyright (c) HashiCorp, Inc.
  * SPDX-License-Identifier: MPL-2.0
  */
-
-// Utils
-import { getContentApiBaseUrl } from 'lib/unified-docs-migration-utils'
 // Types
 import type { VersionSelectItem } from '../loaders/remote-content'
 
-const VERSIONS_ENDPOINT = '/api/content-versions'
 
 /**
  * Given a list of all possible versions, as well as a document path and
@@ -44,35 +40,9 @@ export async function getValidVersions(
 	productSlugForLoader: string
 ): Promise<VersionSelectItem[]> {
 	// If versions are falsy or empty, we can skip the API calls and return []
-	if (!versions || versions.length === 0) return []
-	/**
-	 * We are currently migrating away from our existing content API, to a new
-	 * content API backed by a unified documentation repository. If the provided
-	 * `productSlugForLoader` has been flagged as migrated to unified docs, then
-	 * we use the new unified docs API to fetch known versions.
-	 */
-	const contentApiBaseUrl = getContentApiBaseUrl(productSlugForLoader)
-	try {
-		// Build the URL to fetch known versions of this document
-		const validVersionsUrl = new URL(VERSIONS_ENDPOINT, contentApiBaseUrl)
-		validVersionsUrl.searchParams.set('product', productSlugForLoader)
-		validVersionsUrl.searchParams.set('fullPath', fullPath)
-		const headers = process.env.UDR_VERCEL_AUTH_BYPASS_TOKEN
-			? new Headers({
-					'x-vercel-protection-bypass':
-						process.env.UDR_VERCEL_AUTH_BYPASS_TOKEN,
-			  })
-			: new Headers()
-		// Fetch known versions of this document
-		const response = await fetch(validVersionsUrl.toString(), { headers })
-		const { versions: knownVersions } = await response.json()
-		// Apply the filter, and return the valid versions
-		return versions.filter((option) => knownVersions.includes(option.version))
-	} catch (error) {
-		console.error(
-			`[docs-view/server] error fetching known versions for "${productSlugForLoader}" document "${fullPath}". Falling back to showing all versions.`,
-			error
-		)
+	if (!versions || versions.length === 0) {
+		return []
+	} else {
 		return versions
 	}
 }
