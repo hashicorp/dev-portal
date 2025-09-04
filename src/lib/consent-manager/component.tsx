837ef3666b8fd9f9841e73deaf596a64d4905b05
Leah Bush <leah.bush@hashicorp.com>
2025-09-04 16:21:14 -0500

    update consent manager to be in sync with www version

--- src/lib/consent-manager/component.tsx
+++ src/lib/consent-manager/component.tsx
@@ -4,17 +4,10 @@
  */
 
 import { useEffect } from 'react'
-import Cookies from 'js-cookie'
 import { GoogleTagManager } from '@next/third-parties/google'
 import {
-	showBanner,
-	addDatagrailEventListener,
 	onDatagrailMount,
-	removeDatagrailEventListeners,
-	CONSENT_COOKIE_KEY,
-	hasConfiguredConsent,
 } from './utils'
-import { DatagrailEvent, DatagrailPreferencesPayload } from './types'
 
 /**
  * Datagrail has its own logic for determining whether consent has been configured based on IP address.
@@ -47,28 +40,10 @@ export const ConsentManager = ({ gtmId }: ConsentManagerProps) => {
 			}
 			`
 			document.head.appendChild(styleNode)
-
-			if (!hasConfiguredConsent()) {
-				showBanner()
-			}
-
-			const datagrailEventHandler = (_: DatagrailPreferencesPayload) => {
-				const sixMonths = 365 / 2
-				Cookies.set(CONSENT_COOKIE_KEY, 'true', { expires: sixMonths })
-			}
-
-			const preferenceChangeEvents: DatagrailEvent[] = [
-				'initial_preference_callback',
-				'preference_callback',
-			]
-			preferenceChangeEvents.forEach((e) => {
-				addDatagrailEventListener(e, datagrailEventHandler)
-			})
 		})
 
 		return () => {
 			cleanupFn()
-			removeDatagrailEventListeners()
 			const styleNode = document.getElementById(STYLE_ID)
 			if (styleNode) {
 				styleNode.remove()
