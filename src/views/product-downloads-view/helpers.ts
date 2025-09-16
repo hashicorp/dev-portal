6f34f5b56f8c9c98f90831c2d555d5e7aace2fcc
Aimee Ukasick <aimee.ukasick@hashicorp.com>
2025-09-16 15:49:14 -0500

    remove whitespace to trigger checks

--- src/views/product-downloads-view/helpers.ts
+++ src/views/product-downloads-view/helpers.ts
@@ -82,7 +82,7 @@ export const generateDefaultPackageManagers = (
 			commands: [
 				`sudo yum install -y yum-utils shadow-utils`,
 				`sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo`,
-				`sudo yum install ${productSlug}`,     
+				`sudo yum install ${productSlug}`,
 			],
 			os: 'linux',
 		},
