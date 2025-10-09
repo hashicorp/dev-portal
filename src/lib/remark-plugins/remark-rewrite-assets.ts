f281bef0aad2cb458d2e5b69b2a2a050cf08ea24
Leah Bush <leah.bush@hashicorp.com>
2025-10-09 09:50:51 -0500

    fix: allow definitions as a type in remarkRewriteAssets

--- src/lib/remark-plugins/remark-rewrite-assets.ts
+++ src/lib/remark-plugins/remark-rewrite-assets.ts
@@ -28,48 +28,72 @@ export function remarkRewriteAssets(args: {
 
 	return function plugin() {
 		return function transform(tree) {
-			// @ts-expect-error Types Should be correct here
-			visitParents<Image>(tree, 'image', (node, ancestors: Array<Parent>) => {
-				let url
-				const originalUrl = node.url
+			visitParents<Image>(
+				// @ts-expect-error Types Should be correct here
+				tree,
+				['image', 'definition'],
+				// @ts-expect-error Types Should be correct here
+				(node, ancestors: Array<Parent>) => {
+					let url
+					const originalUrl = node.url
 
-				if (isInUDR) {
-					let domain
+					/**
+					 * The Definition node could be used by an image or link reference
+					 * This regex checks if the path starts with /img or /public/img. While
+					 * a brittle check, it prevents links references from breaking.
+					 *
+					 * We need to follow with a more comprehensive, long-term solution
+					 * to selectively delineate image from link.
+					 */
+					const regex = /^\/img|\/public\/img/ // Ensures non-image links aren't rewritten
+					const isImagePath = regex.test(originalUrl)
 
-					if (process.env.NODE_ENV === 'development') {
-						domain = `http://localhost:${process.env.UNIFIED_DOCS_PORT}`
-					} else {
-						domain = process.env.UNIFIED_DOCS_API
-					}
+					if (isImagePath) {
+						if (isInUDR) {
+							let domain
 
-					url = new URL(`${domain}/api/assets/${product}/${version}${node.url}`)
-				} else {
-					const asset = path.posix.join(...getAssetPathParts(originalUrl))
+							if (process.env.NODE_ENV === 'development') {
+								domain = `http://localhost:${process.env.UNIFIED_DOCS_PORT}`
+							} else {
+								domain = process.env.UNIFIED_DOCS_API
+							}
 
-					url = new URL(`${process.env.MKTG_CONTENT_DOCS_API}/api/assets`)
-					url.searchParams.append('asset', asset)
-					url.searchParams.append('version', version)
-					url.searchParams.append('product', product)
-				}
+							url = new URL(
+								`${domain}/api/assets/${product}/${version}${node.url}`
+							)
+						} else {
+							const asset = path.posix.join(...getAssetPathParts(originalUrl))
 
-				node.url = url.toString()
-				logOnce(
-					node.url,
-					`Rewriting asset url for local preview:
+							url = new URL(`${process.env.MKTG_CONTENT_DOCS_API}/api/assets`)
+							url.searchParams.append('asset', asset)
+							url.searchParams.append('version', version)
+							url.searchParams.append('product', product)
+						}
+
+						node.url = url.toString()
+						logOnce(
+							node.url,
+							`Rewriting asset url for local preview:
 - Found: ${originalUrl}
 - Replaced with: ${node.url}
 
 If this is a net-new asset, you'll need to commit and push it to GitHub.\n`
-				)
+						)
 
-				// if the image is wrapped in a link, and shares the same url as the original image, then update the link's url to the new asset url
-				if (isInUDR) {
-					const parent = ancestors[ancestors.length - 1]
-					if (parent && parent.type === 'link' && parent.url === originalUrl) {
-						parent.url = url.toString()
+						// if the image is wrapped in a link, and shares the same url as the original image, then update the link's url to the new asset url
+						if (isInUDR) {
+							const parent = ancestors[ancestors.length - 1]
+							if (
+								parent &&
+								parent.type === 'link' &&
+								parent.url === originalUrl
+							) {
+								parent.url = url.toString()
+							}
+						}
 					}
 				}
-			})
+			)
 		}
 	}
 }
