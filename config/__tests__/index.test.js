d5d837bb92df796c614817bf4db239c400c5be70
Robert Main <50675045+rmainwork@users.noreply.github.com>
2025-10-28 13:48:24 -0400

    Update snapshot

--- config/__tests__/index.test.js
+++ config/__tests__/index.test.js
@@ -20,32 +20,33 @@ describe('loadHashiConfigByEnvironment', () => {
 	test('loads configuration and handles extending', async () => {
 		vi.spyOn(process, 'cwd').mockReturnValue(fixtureDir)
 		expect(await loadHashiConfigForEnvironment()).toMatchInlineSnapshot(`
-      {
-        "alpha": "beta",
-        "deeply.nested.another_property": false,
-        "deeply.nested.property": true,
-        "extends": "base",
-        "flags.unified_docs_migrated_repos": [
-          "terraform-mcp-server",
-          "terraform-migrate",
-          "terraform-plugin-framework",
-          "terraform-plugin-log",
-          "terraform-plugin-mux",
-          "terraform-plugin-sdk",
-          "terraform-plugin-testing",
-          "terraform-docs-agents",
-          "terraform-cdk",
-          "terraform",
-          "terraform-docs-common",
-          "vault",
-          "ptfe-releases",
-          "well-architected-framework",
-          "terraform-enterprise",
-          "sentinel",
-          "hcp-docs",
-        ],
-        "foo": "bar",
-      }
-    `)
+			{
+			  "alpha": "beta",
+			  "deeply.nested.another_property": false,
+			  "deeply.nested.property": true,
+			  "extends": "base",
+			  "flags.unified_docs_migrated_repos": [
+			    "terraform-mcp-server",
+			    "terraform-migrate",
+			    "terraform-plugin-framework",
+			    "terraform-plugin-log",
+			    "terraform-plugin-mux",
+			    "terraform-plugin-sdk",
+			    "terraform-plugin-testing",
+			    "terraform-docs-agents",
+			    "terraform-cdk",
+			    "terraform",
+			    "terraform-docs-common",
+			    "vault",
+			    "ptfe-releases",
+			    "well-architected-framework",
+			    "terraform-enterprise",
+			    "sentinel",
+			    "hcp-docs",
+			    "boundary",
+			  ],
+			  "foo": "bar",
+			}
+		`)
 	})
 })
