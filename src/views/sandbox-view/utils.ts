aadcee0c2d96c54aedb84737f620048e3bf4afb7
williamdalessandro <will.dalessandro@hashicorp.com>
2025-10-29 15:51:17 -0400

    Debugging instruqt to fix ssr (#2887)
    * naive approach simply wrapping all posthog imports in an SSR safe wrapper- wont attempt to load until everything's available
    
    * adding in failing e2e tests
    
    * fixing circular dependency that was found through failing test case
    
    * fixing test that expects sandbox interaction to be called elsewhere
    
    * missed 2 spots
    
    * removing unused file
    
    * adding in more robust metadata tag tests along with content presence tests
    
    * fixing tests and making them actually robust

--- src/views/sandbox-view/utils.ts
+++ src/views/sandbox-view/utils.ts
@@ -1,6 +1,30 @@
-import posthog from 'posthog-js'
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 import { SANDBOX_EVENT } from 'lib/posthog-events'
 
+/**
+ * Dynamic import of PostHog to ensure SSR safety.
+ * This is loaded lazily only in the browser.
+ * This type of check is ok here since it's outside of the rendering logic
+ */
+let posthog: typeof import('posthog-js').default | null = null
+if (typeof window !== 'undefined') {
+	import('posthog-js').then((module) => {
+		posthog = module.default
+	})
+}
+
+// Before this was extracted, this function was causing circular dependency issues
+
+/**
+ * Track sandbox interaction events
+ * @param interactionType - Type of interaction (e.g., 'click', 'open')
+ * @param sandboxId - ID of the sandbox being interacted with
+ * @param additionalProps - Additional properties to track
+ */
 export const trackSandboxInteraction = (
 	interactionType: string,
 	sandboxId: string,
