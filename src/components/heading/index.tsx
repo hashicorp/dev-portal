f7d6c3e2fc65cf360baca394753bae8bf0cd9288
Mike Golus <mike.golus@hashicorp.com>
2025-12-05 12:03:36 -0500

    chore: deprecated hds-typography-display-600 class

--- src/components/heading/index.tsx
+++ src/components/heading/index.tsx
@@ -3,23 +3,42 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
+import { forwardRef } from 'react'
 import classNames from 'classnames'
 import { HeadingProps } from './types'
+import { Text } from '@hashicorp/mds-react'
 
-const Heading: React.FC<HeadingProps> = ({ level, size, weight, ...rest }) => {
+const Heading = forwardRef<HTMLHeadingElement, HeadingProps>(
+  ({ level, size, weight, ...rest }, ref) => {
+	if (size === 600) {
+		return (
+			<Text.DisplayExpressive
+				tag={`h${level}`}
+				size="400"
+				weight={weight}
+				ref={ref}
+				{...rest}
+			/>
+		)
+	}
+
 	const className = classNames(
 		`hds-typography-display-${size}`,
 		`hds-font-weight-${weight}`,
 		rest.className
 	)
+
 	const passableProps = {
 		...rest,
 		className,
 	}
 
 	const HeadingElement = `h${level}` as React.ElementType
-	return <HeadingElement {...passableProps} />
-}
+	return <HeadingElement ref={ref} {...passableProps} />
+  }
+)
+
+Heading.displayName = "Heading"
 
 export type { HeadingProps }
 export default Heading
