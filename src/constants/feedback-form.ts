8f2e5bab3be8b3a9666faf0fbfde406ccc6049c3
Robert Main <50675045+rmainwork@users.noreply.github.com>
2026-05-18 15:42:19 -0400

    Change feedback form URL

--- src/constants/feedback-form.ts
+++ src/constants/feedback-form.ts
@@ -3,4 +3,4 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
-export const FEEDBACK_FORM_URL = 'https://forms.gle/fnHLuNahLEhjuKvE6'
+export const FEEDBACK_FORM_URL = 'https://forms.office.com/Pages/ResponsePage.aspx?id=V3D2_MlQ1EqY8__KZK3Z6eJe9I0o9MhDqfZi9jhHb1NUQkFXNlNWT0RZNTNGNFQ1TTFJRUo1RUFEMi4u'
