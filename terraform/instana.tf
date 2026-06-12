8a523baa390a4fa87e2e5b2e81fc8b30b6063e9e
Robert Main <50675045+rmainwork@users.noreply.github.com>
2026-06-12 14:11:51 -0400

    Add slack notifications for RUM errors
    Notify slack channel for RUM errors

--- terraform/instana.tf
+++ terraform/instana.tf
@@ -2,3 +2,64 @@
 resource "instana_website_monitoring_config" "devdot" {
   name = var.website_name
 }
+
+resource "instana_alerting_channel" "slack" {
+  name = var.slack_channel_name
+  slack_app = {
+    app_id       = var.slack_app_id
+    team_id      = var.slack_team_id
+    team_name    = var.slack_team_name
+    channel_id   = var.slack_channel_id
+    channel_name = var.slack_channel_name
+  }
+}
+
+locals {
+  website_monitoring_id = instana_website_monitoring_config.devdot.id
+  alert_channel_ids = [
+    instana_alerting_channel.slack.id,
+  ]
+}
+
+resource "instana_website_alert_config" "js_errors" {
+  name        = "${var.website_name} JS error detected"
+  description = "${var.website_name} JS error detected"
+  enabled     = true
+  triggering  = false
+  website_id  = local.website_monitoring_id
+  granularity = var.granularity_minutes * 60000
+
+  alert_channel_ids = toset(local.alert_channel_ids)
+
+  rules = [
+    {
+      operator = ">"
+      rule = {
+        specific_js_error = {
+          metric_name = "errors"
+          aggregation = "SUM"
+          operator    = "GREATER_THAN"
+          value       = null
+        }
+      }
+      threshold = {
+        warning = {
+          static = {
+            value = var.warning_threshold
+          }
+        }
+        critical = {
+          static = {
+            value = var.critical_threshold
+          }
+        }
+      }
+    }
+  ]
+
+  time_threshold = {
+    violations_in_sequence = {
+      time_window = var.time_window_minutes * 60000
+    }
+  }
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -16,3 +16,55 @@ variable "website_name" {
   type        = string
   default     = "developer.hashicorp.com"
 }
+
+variable "slack_app_id" {
+  description = "Slack App ID (starts with A) for the Instana notification bot."
+  type        = string
+  default     = "A08AEK9NX41"
+}
+
+variable "slack_channel_name" {
+  description = "The name of the slack channel to send alerts to"
+  type        = string
+  default     = "devdot-instana-digital-alerts"
+}
+
+variable "slack_channel_id" {
+  description = "Slack channel ID"
+  default     = "C0B9Y3JSL58"
+  type        = string
+}
+
+variable "slack_team_name" {
+  description = "The name of the slack team that contains the channel to send alerts to"
+  type        = string
+}
+
+variable "slack_team_id" {
+  description = "Slack team ID. You can find this by inspecting the URL of your Slack workspace (e.g. https://app.slack.com/client/<team ID>)"
+  type        = string
+}
+
+variable "granularity_minutes" {
+  description = "Evaluation granularity in minutes."
+  type        = number
+  default     = 5
+}
+
+variable "time_window_minutes" {
+  description = "Violation sequence time window in minutes."
+  type        = number
+  default     = 5
+}
+
+variable "warning_threshold" {
+  description = "Warning threshold for JS error count. Use 1 to mimic Datadog y > 0 behavior."
+  type        = number
+  default     = 1
+}
+
+variable "critical_threshold" {
+  description = "Critical threshold for JS error count."
+  type        = number
+  default     = 2
+}
