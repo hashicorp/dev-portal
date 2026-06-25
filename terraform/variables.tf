4d001d99d47a038ba646f23e54d2f69f31448292
Robert Main <50675045+rmainwork@users.noreply.github.com>
2026-06-24 22:06:05 -0400

    Populate website ID to vercel with Terraform
    Since this value is generated by Instana when the resource is created,
    we can use Terraform to populate it to Vercel dynamically

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,22 @@ variable "website_name" {
   type        = string
   default     = "developer.hashicorp.com"
 }
+
+# -------------------------
+# Vercel
+# -------------------------
+variable "vercel_api_token" {
+  description = "Vercel API token. Can also be set via VERCEL_API_TOKEN env var."
+  type        = string
+  sensitive   = true
+}
+
+variable "vercel_team_slug" {
+  description = "Vercel team slug (the part after vercel.com/ in your team URL)."
+  type        = string
+}
+
+variable "vercel_project_id" {
+  description = "Vercel project ID. Find it in Project Settings → General."
+  type        = string
+}
