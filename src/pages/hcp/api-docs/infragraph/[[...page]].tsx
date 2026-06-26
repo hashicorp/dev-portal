3b3fc663f33c76038c76a289972d4bc779e9209a
Brian McClain <brianmmcclain@gmail.com>
2026-06-26 11:32:26 -0400

    Add Infragraph API page

---
+++ src/pages/hcp/api-docs/infragraph/[[...page]].tsx
@@ -0,0 +1,111 @@
+/**
+ * Copyright IBM Corp. 2021, 2025
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+// Lib
+import { fetchCloudApiVersionData } from 'lib/api-docs/fetch-cloud-api-version-data'
+// View
+import OpenApiDocsView from 'views/open-api-docs-view'
+import {
+	getStaticPaths,
+	getStaticProps as getOpenApiDocsStaticProps,
+} from 'views/open-api-docs-view/server'
+// Types
+import type { GetStaticProps, GetStaticPropsContext } from 'next'
+import type { OpenAPIV3 } from 'openapi-types'
+import type {
+	OpenApiDocsParams,
+	OpenApiDocsViewProps,
+	OpenApiDocsPageConfig,
+} from 'views/open-api-docs-view/types'
+import { schemaModShortenHcp } from 'views/open-api-docs-view/utils/massage-schema-utils'
+
+/**
+ * OpenApiDocsView server-side page configuration
+ */
+const PAGE_CONFIG: OpenApiDocsPageConfig = {
+	productSlug: 'hcp',
+	serviceProductSlug: 'hcp',
+	basePath: '/hcp/api-docs/infragraph',
+	githubSourceDirectory: {
+		owner: 'hashicorp',
+		repo: 'hcp-specs',
+		path: 'specs/cloud-infragraph-service',
+		ref: 'main',
+	},
+	groupOperationsByPath: true,
+	shouldNoIndex: true,
+	navResourceItems: [
+		{
+			title: 'Tutorials',
+			href: '/hcp/tutorials/infragraph',
+		},
+		{
+			title: 'Community',
+			href: 'https://discuss.hashicorp.com/',
+		},
+		{
+			title: 'Support',
+			href: 'https://www.ibm.com/mysupport',
+		},
+	],
+
+	/**
+	 * Massage the schema data a little bit
+	 */
+	massageSchemaForClient: (schemaData: OpenAPIV3.Document) => {
+		//  Replace "HashiCorp Cloud Platform" with "HCP" in the title
+		const withShortTitle = schemaModShortenHcp(schemaData)
+		// Return the schema data with modifications
+		return withShortTitle
+	},
+}
+
+/**
+ * Get static paths, using `versionData` fetched from GitHub.
+ */
+export { getStaticPaths }
+
+/**
+ * Get static props, using `versionData` fetched from GitHub.
+ *
+ * We need all version data for the version selector,
+ * and of course we need specific data for the current version.
+ */
+export const getStaticProps: GetStaticProps<
+	OpenApiDocsViewProps,
+	OpenApiDocsParams
+> = async ({ params }: GetStaticPropsContext<OpenApiDocsParams>) => {
+	// REMOVE
+	const versionData = [
+		{
+			versionId: '2024-01-01',
+			releaseStage: 'stable',
+			'sourceFile': JSON.stringify({
+				openapi: '3.0.0',
+				info: {
+					title: 'HCP Infragraph',
+					version: '2024-01-01',
+					description: 'Test data'
+				},
+				paths: {},
+				components: { schemas: {} }
+			})
+		}
+	]
+
+
+	// // Fetch all version data, based on remote `stable` & `preview` subfolders
+	// const versionData = await fetchCloudApiVersionData(
+	// 	PAGE_CONFIG.githubSourceDirectory
+	// )
+	// Generate static props based on page configuration, params, and versionData
+	return await getOpenApiDocsStaticProps({
+		...PAGE_CONFIG,
+		context: { params },
+		versionData,
+	})
+}
+
+export default OpenApiDocsView
