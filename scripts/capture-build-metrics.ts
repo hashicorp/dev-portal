b266ed1b0e751d46b1ed6b082a9b4e73755192ba
Ruben Nic <ruben.nic@hashicorp.com>
2026-06-26 13:50:22 -0400

    Add build metrics

--- scripts/capture-build-metrics.ts
+++ scripts/capture-build-metrics.ts
@@ -7,9 +7,6 @@ import path from 'path'
 import fs from 'fs'
 import { client, v1 } from '@datadog/datadog-api-client'
 
-const configuration = client.createConfiguration()
-const api = new v1.MetricsApi(configuration)
-
 const TRACE_FILE_PATH = './.next/trace'
 const EVENTS = [
 	'next-build',
@@ -17,6 +14,13 @@ const EVENTS = [
 	'static-generation',
 	'next-export',
 ]
+
+interface BuildEvent {
+	name: string
+	duration: number
+	timestamp?: number
+	tags: [string, string][]
+}
 
 async function readNextTrace(cwd: string) {
 	const filepath = path.join(cwd, TRACE_FILE_PATH)
@@ -31,24 +35,119 @@ async function readNextTrace(cwd: string) {
 	return parts.map((part) => JSON.parse(part)).flat()
 }
 
-function captureMetric({
-	name,
-	duration,
-	timestamp,
-	tags,
-}: {
-	name: string
-	duration: number
-	timestamp?: number
-	tags?: string[]
-}): v1.Series {
-	return {
-		host: '',
-		metric: name,
-		points: [[timestamp ?? Math.round(Date.now() / 1e3), duration]],
-		tags,
-		type: 'gauge',
+/**
+ * Submit build metrics to Datadog
+ */
+const submitDatadogMetrics = async (metrics: BuildEvent[]) => {
+	const configuration = client.createConfiguration()
+	const api = new v1.MetricsApi(configuration)
+
+	// Send metrics to Datadog API
+	await api.submitMetrics({
+		body: {
+			// Convert the build events into a format Datadog understands
+			series: metrics.map(({ timestamp, tags, ...event }) => {
+				return {
+					host: '',
+					metric: `build.${event.name}`,
+					points: [
+						[
+							timestamp ?? Math.round(Date.now() / 1e3),
+							Math.round(event.duration / 1e3),
+						],
+					],
+					tags: tags.map(([key, value]) => {
+						return `${key}:${value}`
+					}),
+					type: 'gauge',
+				}
+			}),
+		},
+	})
+	const tags = metrics[0].tags.map(([key, value]) => {
+		return `${key}:${value}`
+	})
+	console.log(
+		`〽️ Submitted build metrics to Datadog:\n${JSON.stringify(tags, null, 2)}`,
+	)
+}
+
+/**
+ * Submit build metrics to Instana OpenTelemetry endpoint
+ */
+const submitInstanaMetrics = async (metrics: BuildEvent[]) => {
+	const payload = {
+		resourceMetrics: [
+			{
+				resource: {
+					attributes: [
+						{
+							key: 'service.name',
+							value: {
+								stringValue: metrics[0].tags.find(([key]) => {
+									return key === 'app'
+								})?.[1],
+							},
+						},
+					],
+				},
+				scopeMetrics: [
+					{
+						scope: {
+							name: 'capture-build-metrics',
+						},
+						metrics: metrics.map(({ timestamp, tags, ...event }) => {
+							const unixTimeNs = (
+								BigInt(timestamp ?? Math.round(Date.now() / 1e3)) *
+								BigInt(1_000_000_000)
+							).toString()
+
+							return {
+								name: `build.${event.name}`,
+								description: 'Build event duration',
+								unit: 's',
+								gauge: {
+									dataPoints: [
+										{
+											attributes: tags.map(([key, value]) => {
+												return {
+													key,
+													value: { stringValue: value },
+												}
+											}),
+											asDouble: Math.round(event.duration / 1e3),
+											timeUnixNano: unixTimeNs,
+										},
+									],
+								},
+							}
+						}),
+					},
+				],
+			},
+		],
 	}
+	const response = await fetch(process.env.INSTANA_OTLP_ENDPOINT!, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'x-instana-key': process.env.INSTANA_OTLP_API_TOKEN!,
+		},
+		body: JSON.stringify(payload),
+	})
+	if (![200, 202].includes(response.status)) {
+		const responseText = await response.text()
+		throw new Error(
+			`Failed to submit metrics to Instana. Status: ${response.status}, Response: ${responseText}`,
+		)
+	}
+
+	const tags = metrics[0].tags.map(([key, value]) => {
+		return `${key}:${value}`
+	})
+	console.log(
+		`〽️ Submitted build metrics to Instana:\n${JSON.stringify(tags, null, 2)}`,
+	)
 }
 
 async function main() {
@@ -56,31 +155,52 @@ async function main() {
 		return
 	}
 
-	const [, , appName] = process.argv
+	const [, , appName = 'dev-portal'] = process.argv
 
 	try {
+		// It's important that this remains a constant variable rather than
+		// being incorporated into the `.map()` call as we don't want it to be
+		// re-evaluated for each run through the loop. We want it to remain fixed
 		const timestamp = Math.round(Date.now() / 1e3)
 		const trace = await readNextTrace(process.cwd())
 
-		const filteredEvents = trace.filter((event) => EVENTS.includes(event.name))
+		const environment = process.env.HASHI_ENV ?? 'local'
 
-		const structuredMetrics = filteredEvents.map((event) =>
-			captureMetric({
-				name: `build.${event.name}`,
-				duration: Math.round(event.duration / 1e3),
-				timestamp,
-				tags: [`app:${appName}`, `environment:${process.env.HASHI_ENV}`],
+		const filteredEvents: BuildEvent[] = trace
+			.filter((event) => EVENTS.includes(event.name))
+			.map((event) => {
+				return {
+					...event,
+					tags: [
+						['app', appName],
+						['environment', environment],
+					] as [string, string][],
+					timestamp,
+				}
 			})
-		)
 
-		await api.submitMetrics({
-			body: {
-				series: structuredMetrics,
-			},
-		})
-	} catch {
-		// Swallow errors here, we don't want to impact the build or make it seem like there's been an error in the actual app
-		// if something goes wrong when sending metrics
+		const results = await Promise.allSettled([
+			submitDatadogMetrics(filteredEvents),
+			submitInstanaMetrics(filteredEvents),
+		])
+
+		const failedSubmissions = results
+			.filter(({ status }) => {
+				return status === 'rejected'
+			})
+			.map((result) => {
+				return (result as PromiseRejectedResult).reason
+			})
+		if (failedSubmissions.length > 0) {
+			throw new AggregateError(failedSubmissions)
+		}
+	} catch (error) {
+		// Swallow errors
+		// we don't want to impact the build or make it seem like there's been
+		// an error in the actual app if something goes wrong when sending metrics
+		if (process.env.NODE_ENV === 'development' || process.env.CI) {
+			throw error
+		}
 	}
 }
 
