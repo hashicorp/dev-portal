7e17b03e4af307596a596a6f5f4f0a8ccdb64d0e
Robert Main <50675045+rmainwork@users.noreply.github.com>
2026-07-08 15:31:40 -0400

    Git repository variables
    Rather than hardcoding the github repository, we can pass it in
    programatically from the tf var already used to configure the github
    provider

--- terraform/github.tf
+++ terraform/github.tf
@@ -1,7 +1,7 @@
 module "github_repo" {
   source = "./github_repo"
 
-  repo = "dev-portal"
+  repo = var.github_repository
   variables = {
     INSTANA_OTLP_ENDPOINT = var.instana_otlp_endpoint
   }
