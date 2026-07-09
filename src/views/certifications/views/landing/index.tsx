0405b97f95ea81798c3dcdc198e0a46d150ce7be
Leah Bush <157434496+LeahMarieBush@users.noreply.github.com>
2026-07-09 16:17:12 -0500

    feat: create the announcement component (#3034)
    * feat: create the announcement component
    
    * cleanup
    
    * add copyright
    
    * export announcement

--- src/views/certifications/views/landing/index.tsx
+++ src/views/certifications/views/landing/index.tsx
@@ -9,6 +9,7 @@ import BaseLayout from 'layouts/base-layout'
 import MobileMenuLevelsGeneric from 'components/mobile-menu-levels-generic'
 import {
 	AccordionWithMdxContent,
+	Announcement,
 	CertificationsMaxWidth,
 	SignupFormArea,
 } from 'views/certifications/components'
@@ -29,8 +30,16 @@ function CertificationsLandingView({
 			<div className={s.root}>
 				{/* Hero */}
 				<LandingHero heading={hero.heading} description={hero.description} />
+				<CertificationsMaxWidth>
+					<Announcement
+						heading="TechXchange is Coming"
+						text="Learn from certified instructors on how to boost your enterprise adoption of HashiCorp"
+						cta="Read more"
+						ctaLink="https://developer.hashicorp.com"
+					/>
+				</CertificationsMaxWidth>
 				{/* Program Summaries */}
-				<div className={s.programsSection}>
+				{/* <div className={s.programsSection}>
 					{programSummaries.map(
 						(programSummary: CertificationProgramSummary) => {
 							const { slug, heading, description, exams } = programSummary
@@ -44,9 +53,9 @@ function CertificationsLandingView({
 									/>
 								</CertificationsMaxWidth>
 							)
-						}
+						},
 					)}
-				</div>
+				</div> */}
 				<div className={s.faqSignupSection}>
 					<CertificationsMaxWidth>
 						<h2 className={s.faqHeading}>{pageContent.faqHeading}</h2>
