4b7274d3a96ce5cdba6dd609b1e634bc164b2a07
Robert Main <50675045+rmainwork@users.noreply.github.com>
2026-07-09 12:20:12 -0400

    Remove github module
    We might not actually need a module here and can simplify this by just having the secrets and vars in a `locals` block.

--- terraform/github.tf
+++ terraform/github.tf
@@ -1,11 +1,24 @@
-module "github_repo" {
-  source = "./github_repo"
-
-  repo = var.github_repository
-  variables = {
-    INSTANA_OTLP_ENDPOINT = var.instana_otlp_endpoint
-  }
+locals {
   secrets = {
     INSTANA_OTLP_AGENT_TOKEN = var.instana_agent_key
   }
+  variables = {
+    INSTANA_OTLP_ENDPOINT = var.instana_otlp_endpoint
+  }
+}
+
+resource "github_actions_variable" "this" {
+  for_each    = local.variables
+  repository  = var.github_repository
+  variable_name = each.key
+  value = each.value
+}
+
+# Github Actions Secrets
+# These values are sensitive, and are redacted from the Terraform logs
+resource "github_actions_secret" "this" {
+  for_each    = local.secrets
+  repository  = var.github_repository
+  secret_name = each.key
+  value       = sensitive(each.value)
 }
