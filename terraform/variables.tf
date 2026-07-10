f78e8e44d4f469f152552588e9b44403f06455d0
Robert Main <50675045+rmainwork@users.noreply.github.com>
2026-07-10 12:14:48 -0400

    Instana metrics infrastructure (#3032)
    * Add github instana variables
    
    Add github env var and secret for Instana OTLP endpoint and agent key
    
    * Git repository variables
    
    Rather than hardcoding the github repository, we can pass it in
    programatically from the tf var already used to configure the github
    provider
    
    * Rename agent token secret
    
    The secret should be named INSTANA_OTLP_AGENT_TOKEN to better match the intent
    and the code in #3029
    
    * Remove github module
    
    We might not actually need a module here and can simplify this by just having the secrets and vars in a `locals` block.
    
    * Add Instana application and related smart alerts
    
    Import the existing instana application and related smart alerts into terraform.
    The import blocks generated will need to be removed later(just for cleanliness).
    
    * Add terraform code for custoim dashboard
    
    Adds the terraform code to bring the custom devdot dashboard under terraform
    control
    
    * Move OTLP endpoint to secret instead of var
    
    It's not technically a secret, but the code in #3029 expects it to be in
    `secrets.INSTANA_OTLP_ENDPOINT` in the github workflow. So for now it needs to
    be a secret for that code to work.
    
    * Exclude dashboard widgets from terraform
    
    We want to be able to manage the dashboard widgets via clickops in
    Instana without generating Terraform diffs. Therefore, the specific JSON
    to generate the widgets has been excluded (and removed) from Terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -61,3 +61,39 @@ variable "critical_threshold" {
   type        = number
   default     = 2
 }
+
+variable "instana_agent_key" {
+  description = "Agent key used to submit metrics to Instana. Obtained from: https://prod-hashicorp.instana.io/#/instanaagent/installation"
+  type        = string
+  sensitive   = true
+}
+
+variable "instana_otlp_endpoint" {
+  description = "Instana OTLP endpoint for sending metrics to Instana."
+  type        = string
+}
+
+
+variable "instana_application_violation_time_window_minutes" {
+  description = "Violation sequence time window in minutes for application alerting."
+  type        = number
+  default     = 10
+}
+
+variable "instana_repo_sync_failed_warning_threshold" {
+  description = "Warning threshold for repo-sync-failed application alert throughput."
+  type        = number
+  default     = 1
+}
+
+variable "instana_content_not_found_warning_threshold" {
+  description = "Warning threshold for content-not-found application alert throughput."
+  type        = number
+  default     = 500
+}
+
+
+variable "github_repository" {
+  description = "Canonical name of the GitHub repository to manage."
+  type        = string
+}
