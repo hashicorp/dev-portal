8148d1fe5c7b69229ffc92d8f79cb304bab6fae9
Ryan Yu <ryan.yu@ibm.com>
2026-07-20 16:22:22 -0700

    Added skeleton for Certification Details

---
+++ src/views/certifications/views/[slug]/components/certification-details/index.tsx
@@ -0,0 +1,22 @@
+// Components
+// import Heading from "views/certifications/components/heading";
+// import Text from "views/certifications/components/text";
+import { GradientCard } from "views/certifications/components/gradient-card";
+
+// Styles
+import s from "./certification-details.module.css";
+
+
+/* 
+    1. Need to figure out the structuring of the details 
+    (i.e. stick with the 1-2 block column layout or go with more flexible layout)
+    2. Think about what is going to be passed into the component
+    3. Address how the Figma design has each card with differing data
+*/
+export function CertificationDetails(props) {
+    return (
+        <div className={s.certDetails}>
+
+        </div>
+    )
+}
