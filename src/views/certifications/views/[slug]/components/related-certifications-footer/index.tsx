0ed619639bbfb60a36baad3dac63f399f67b85a8
Leah Bush <leah.bush@hashicorp.com>
2026-07-21 14:26:55 -0500

    adjust some styling

--- src/views/certifications/views/[slug]/components/related-certifications-footer/index.tsx
+++ src/views/certifications/views/[slug]/components/related-certifications-footer/index.tsx
@@ -8,6 +8,7 @@ import s from './related-certifications-footer.module.css'
 
 // Types
 import { CertificationCardProps } from 'views/certifications/components/certification-card/types'
+import { CertificationsMaxWidth } from 'views/certifications/components'
 
 interface relatedCertificationsFooterProps {
 	title: string
@@ -22,31 +23,35 @@ export function RelatedCertificationsFooter({
 }: relatedCertificationsFooterProps) {
 	return (
 		<footer className={s.relatedCertFooter}>
-			<div className={s.relatedCertFooterHeader}>
-				<Heading
-					level={2}
-					size={600}
-					weight={'bold'}
-					className={s.relatedCertFooterTitle}
-				>
-					{title}
-				</Heading>
-				<Text size={300} className={s.relatedCertFooterDesc}>
-					{desc}
-				</Text>
-			</div>
-			<div className={s.relatedCertFooterContent}>
-				{relatedCertifications &&
-					relatedCertifications.map((certification, index) => (
-						<CertificationCardDisplay
-							key={`related-certification-${index}`}
-							isReduced={true}
-							product={certification.product}
-							title={certification.title}
-							ctaLink={certification.ctaLink}
-						/>
-					))}
-			</div>
+			<CertificationsMaxWidth>
+				<div className={s.relatedCertFooterContainer}>
+					<div className={s.relatedCertFooterHeader}>
+						<Heading
+							level={2}
+							size={600}
+							weight={'bold'}
+							className={s.relatedCertFooterTitle}
+						>
+							{title}
+						</Heading>
+						<Text size={300} className={s.relatedCertFooterDesc}>
+							{desc}
+						</Text>
+					</div>
+					<div className={s.relatedCertFooterContent}>
+						{relatedCertifications &&
+							relatedCertifications.map((certification, index) => (
+								<CertificationCardDisplay
+									key={`related-certification-${index}`}
+									isReduced={true}
+									product={certification.product}
+									title={certification.title}
+									ctaLink={certification.ctaLink}
+								/>
+							))}
+					</div>
+				</div>
+			</CertificationsMaxWidth>
 		</footer>
 	)
 }
