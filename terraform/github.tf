2f828e4ba041e052599baeac21e818f9a7600547
Robert Main <50675045+rmainwork@users.noreply.github.com>
2026-08-07 14:15:24 -0400

    Consolidate env var names for the agent key
    There are various references to apiToken/AGENT_TOKEN/AGENT_KEY etc.
    in secrets and in the code.
    
    For simplicity and consistency, this should be consolidated into
    `INSTANA_AGENT_KEY` and variations thereof to match the nomenclature of
    Instana

--- terraform/github.tf
+++ terraform/github.tf
@@ -1,6 +1,6 @@
 locals {
   secrets = {
-    INSTANA_OTLP_AGENT_TOKEN = var.instana_agent_key
+    INSTANA_AGENT_KEY     = var.instana_agent_key
     INSTANA_OTLP_ENDPOINT    = var.instana_otlp_endpoint
   }
   variables = {}
