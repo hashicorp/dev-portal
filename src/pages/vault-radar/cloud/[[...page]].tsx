74d491097092e783d81bdf8f4c4834a62c0f0c33
William Dalessandro <will.dalessandro@hashicorp.com>
2026-08-27 20:52:36 -0400

    rename path here to use cloud

--- src/pages/vault-radar/cloud/[[...page]].tsx
+++ src/pages/vault-radar/cloud/[[...page]].tsx
@@ -1,4 +1,4 @@
-/**Add a comment on  lines R1 to R4Add diff commentMarkdown input:  edit mode selected.WritePreviewAdd a suggestionHeadingBoldItalicQuoteCodeLinkUnordered listNumbered listTask listMentionReferenceMore Formatting tools items 4Saved repliesAdd FilesPaste, drop, or click to add filesCancelCommentStart a review
+/**
  * Copyright IBM Corp. 2021, 2025
  * SPDX-License-Identifier: MPL-2.0
  */
@@ -8,7 +8,7 @@ import { getRootDocsPathGenerationFunctions } from 'views/docs-view/utils/get-ro
 
 const { getStaticPaths, getStaticProps } = getRootDocsPathGenerationFunctions(
 	'vault-radar',
-	'hcp-docs'
+	'cloud'
 )
 
 export { getStaticProps, getStaticPaths }
